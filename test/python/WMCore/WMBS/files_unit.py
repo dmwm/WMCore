1d310c9bc7f40a0f2233180c68f26bce5524cde2
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:41:24 +0000

    Unit test for File actions and associated SQL
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2663 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore/WMBS/files_unit.py
+++ test/python/WMCore/WMBS/files_unit.py
@@ -7,8 +7,8 @@ are database dialect neutral.
 
 """
 
-__revision__ = "$Id: files_unit.py,v 1.1 2008/06/10 16:51:14 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: files_unit.py,v 1.2 2008/06/10 17:05:25 metson Exp $"
+__version__ = "$Revision: 1.2 $"
 
 import unittest, logging, os, commands
 from pylint import lint
@@ -65,16 +65,19 @@ class CreateListDeleteTestCase(BaseFilesTestCase):
         
         from WMCore.WMBS.Actions.Fileset.New import NewFilesetAction
         newFS = NewFilesetAction(self.testlogger)
+        
         assert newFS.execute(name='fs001', dbinterface=self.dbf1.connect())
         assert newFS.execute(name='fs001', dbinterface=self.dbf2.connect())
         
         from WMCore.WMBS.Actions.Files.New import NewFileAction
         from WMCore.WMBS.Actions.Files.AddToFileset import AddFileToFilesetAction
         from WMCore.WMBS.Actions.Files.InFileset import InFilesetAction
+        from WMCore.WMBS.Actions.Files.SetLocation import SetFileLocationAction
         
         self.action1 = NewFileAction(self.testlogger)
         self.action2 = AddFileToFilesetAction(self.testlogger)
         self.action3 = InFilesetAction(self.testlogger)
+        self.action4 = SetFileLocationAction(self.testlogger)
         
     def testCreate(self, notest=False):
         file = "/store/data/Electrons/1234/5678/hkh123ghk12khj3hk123ljhkj1231.root"
@@ -107,9 +110,29 @@ class CreateListDeleteTestCase(BaseFilesTestCase):
         
         for conn in self.dbf1.connect(), self.dbf2.connect():
             list = self.action3.execute(fileset='fs001', dbinterface=conn)
-            assert len(list) == 3, \
+            assert len(list) == 3 or notest, \
                 "list has wrong length (%s not 3) \n \t %s" %(len(list), list)
+    
+    def testListDialectNeutral(self):
+        self.testListCreate(notest=True)
+        mysqllist = self.action3.execute(fileset='fs001', dbinterface=self.dbf1.connect())
+        sqllitelist = self.action3.execute(fileset='fs001', dbinterface=self.dbf2.connect())
         
+        assert mysqllist == sqllitelist, \
+            "Lists are not dialect neutral \n \t %s \n \t %s" % (mysqllist, sqllitelist)
         
+    def testLocateFiles(self):
+        self.testListCreate(notest=True)
+        
+        se1 = 'lcgse01.phy.bris.ac.uk'
+        se2 = 'lcgse02.phy.bris.ac.uk'
+        selist = ['se01.fnal.gov', 'se02.fnal.gov']
+        for conn in self.dbf1.connect(), self.dbf2.connect():
+            list = self.action3.execute(fileset='fs001', dbinterface=conn)
+            self.action4.execute(file=list, sename=se1, dbinterface=conn)
+            for i in list:
+                self.action4.execute(file=i, sename=se2, dbinterface=conn)
+            self.action4.execute(file=list, sename=selist, dbinterface=conn)
+            
 if __name__ == "__main__":
     unittest.main()

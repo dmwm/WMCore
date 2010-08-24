a21f7c533d1e2b78fc763481c568041c95f1285a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:42:34 +0000

    Remove the formatDict() method as the base class takes care of formatting the results.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3643 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Jobs/LoadFiles.py
+++ src/python/WMCore/WMBS/Oracle/Jobs/LoadFiles.py
@@ -6,8 +6,8 @@ Oracle implementation of Jobs.LoadFiles
 """
 
 __all__ = []
-__revision__ = "$Id: LoadFiles.py,v 1.3 2009/01/13 17:39:19 sfoulkes Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: LoadFiles.py,v 1.4 2009/01/16 22:32:14 sfoulkes Exp $"
+__version__ = "$Revision: 1.4 $"
 
 from WMCore.WMBS.MySQL.Jobs.LoadFiles import LoadFiles as LoadFilesMySQL
 
@@ -19,18 +19,3 @@ class LoadFiles(LoadFilesMySQL):
     database.
     """
     sql = "SELECT fileid FROM wmbs_job_assoc WHERE JOB = :jobid"
-
-    def formatDict(self, results):
-        """
-        _formatDict_
-
-        Change the name of the fileid key to be just file for compatibility
-        with the MySQL DAO object.
-        """
-        formattedResults = LoadFilesMySQL.formatDict(self, results)
-
-        for formattedResult in formattedResults:
-            formattedResult["file"] = formattedResult["fileid"]
-            del formattedResult["fileid"]
-
-        return formattedResults

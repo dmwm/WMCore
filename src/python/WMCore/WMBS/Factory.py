0ad31b1726df798a3897104a23d3bd1a13859fc5
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:36:45 +0000

    Added documentation, commented out the Oracle type for now, raise appropriate exception.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2589 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Factory.py
+++ src/python/WMCore/WMBS/Factory.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
+# pylint: disable-msg = R0903
+"""
+_SQLFactory_
+
+A factory to create the appropriate WMBS database dialect object
+for a given database engine. 
+
+"""
+
+__revision__ = "$Id: Factory.py,v 1.3 2008/05/02 14:15:18 metson Exp $"
+__version__ = "$Revision: 1.3 $"
+
 from sqlalchemy.databases.mysql import MySQLDialect
 from sqlalchemy.databases.sqlite import SQLiteDialect 
 from sqlalchemy.databases.oracle import OracleDialect
@@ -21,18 +33,17 @@ class SQLFactory(object):
         
         """
         dia = engine.dialect
-        if isinstance(dia, OracleDialect):
-            from WMCore.WMBS.Oracle import OracleDialect as WMBSOracle
-            return WMBSOracle (self.logger, engine)
-        elif isinstance(dia, SQLiteDialect):
+        #if isinstance(dia, OracleDialect):
+        #    from WMCore.WMBS.Oracle import OracleDialect as WMBSOracle
+        #    return WMBSOracle (self.logger, engine)
+        #el
+        if isinstance(dia, SQLiteDialect):
             from WMCore.WMBS.SQLite import SQLiteDialect as WMBSSQLite
             return WMBSSQLite (self.logger, engine)
         elif isinstance(dia, MySQLDialect):
             from WMCore.WMBS.MySQL import MySQLDialect as WMBSMySQL
             return WMBSMySQL (self.logger, engine)    
         else:
-            "Could return the wmbsSQL object and hope that the connection"
-            "can handle it but that might be a bit dangerous!"
-            raise "unknown connection type"
+            raise TypeError, "unknown connection type"
 
 #Make a similar factory for T0AST?

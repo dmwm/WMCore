eab45e141bb419f5e5ca2ab0be86c49a2ae9a41f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:08:57 +0000

    Updated mysql tests with DBSBufferTest
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3122 4525493e-7705-40b1-a816-d608a930855b

--- standards/conf_test_mysql.py
+++ standards/conf_test_mysql.py
@@ -4,13 +4,15 @@ from WMQuality.Test import Test
 
 
 from WMCore_t.WMException_t import WMExceptionTest        
+from WMComponent_t.DBSBuffer_t.DBSBuffer_t import DBSBufferTest
 from WMComponent_t.ErrorHandler_t.ErrorHandler_t import ErrorHandlerTest
+from WMComponent_t.Proxy_t.Proxy_t import ProxyTest
+
 from WMCore_t.ThreadPool_t.ThreadPool_t import ThreadPoolTest
 from WMCore_t.MsgService_t.MsgService_t import MsgServiceTest
 from WMCore_t.Agent_t.Configuration_t import ConfigurationTest
 from WMCore_t.Database_t.DBFormatter_t import DBFormatterTest
 from WMCore_t.Agent_t.Harness_t import HarnessTest
-from WMComponent_t.Proxy_t.Proxy_t import ProxyTest
 from WMCore_t.Trigger_t.Trigger_t import TriggerTest
 from WMCore_t.WMFactory_t.WMFactory_t import WMFactoryTest
 
@@ -22,6 +24,7 @@ tests = [\
      (ConfigurationTest(),'fvlingen'),\
      (DBFormatterTest(),'fvlingen'),\
      (HarnessTest(),'fvlingen'),\
+     (DBSBufferTest(),'afaq'),\
      (ErrorHandlerTest(),'fvlingen'),\
      (MsgServiceTest(),'fvlingen'),\
      (ProxyTest(),'fvlingen'),\

81006d8b0c96f054c89a5f6044bda0ace63b34ee
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:13:27 +0000

    Added two more tests to test bad Model/Formatter classes (those who are not implement all methods)
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3190 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/HTTPFrontEnd_t/REST_t/Rest_t.py
+++ test/python/WMCore_t/HTTPFrontEnd_t/REST_t/Rest_t.py
@@ -18,6 +18,63 @@ import unittest
 # load TestRestServer and TestRestClient
 from WMCore.HTTPFrontEnd.REST.services.test.TestRestServer import restservice
 from WMCore.HTTPFrontEnd.REST.services.test.TestRestClient import restclient
+from WMCore.HTTPFrontEnd.REST.services.test.TestFormatter import TestFormatter
+from WMCore.HTTPFrontEnd.REST.services.test.TestModel import TestModel
+from WMCore.HTTPFrontEnd.REST.services.rest.RestServer import RestServer
+
+class BadModel:
+    """
+       Example of bad model class implementation, who miss to
+       implement all required method (get, create, delete, update).
+    """
+    def __init__(self):
+        self.data = "DATA"
+    def getdata(self, method, params=None):
+        """Example of getdata implementation"""
+        self.data = "TestModel getdata method=%s params=%s" % \
+                    (str(method),str(params))
+        return self.data
+    def createdata(self, method, params=None):
+        """Example of createdata implementation"""
+        self.data = "TestModel createdata method=%s params=%s" % \
+                    (str(method),str(params))
+        return self.data
+
+class BadFormatter(object):
+    """Simple formatter class. It should format input data according
+       to returned MIME type
+    """
+    def __init__(self):
+        self._data = 0 # hold some data
+    def to_xml(self, data):
+        """This method shows how to convert input data into XML form"""
+        # you can do something with data
+        self._data = data
+    def to_txt(self, data):
+        """This method shows how to convert input data into TXT form"""
+        # you can do something with data
+        self._data = data
+
+
+def badrestmodel():
+    """REST service based on BadModel"""
+    url = 'http://localhost:8080/services/rest'
+    model = BadModel()
+    formatter = TestFormatter()
+    verbose = 0
+    rest = RestServer(model, formatter, url, verbose)
+    conf = {'/':{'request.dispatch':cherrypy.dispatch.MethodDispatcher()}}
+    cherrypy.quickstart(rest, '/services', config=conf)
+
+def badrestformatter():
+    """REST service based on BadModel"""
+    url = 'http://localhost:8080/services/rest'
+    model = TestModel()
+    formatter = BadFormatter()
+    verbose = 0
+    rest = RestServer(model, formatter, url, verbose)
+    conf = {'/':{'request.dispatch':cherrypy.dispatch.MethodDispatcher()}}
+    cherrypy.quickstart(rest, '/services', config=conf)
 
 class MyThread(Thread):
     """ My thread class which will run RestServer """
@@ -37,7 +94,7 @@ class RestTest(unittest.TestCase):
 
     def setUp(self):
         """
-        setup for test.
+        code to execute to in preparation for the test
         """
         if not RestTest._setup_done:
             logging.basicConfig(level=logging.NOTSET,
@@ -48,6 +105,10 @@ class RestTest(unittest.TestCase):
             myThread = threading.currentThread()
             myThread.logger = logging.getLogger('RestTest')
             RestTest._setup_done = True
+
+    def tearDown(self):
+        """code to execute to clean up after tests """
+        self._teardown = True
 
     def testA(self):
         """
@@ -66,11 +127,42 @@ class RestTest(unittest.TestCase):
         print('--- At this point all our requests should be completed')
         RestTest._teardown = True
 
+    def testB(self):
+        """
+        Test REST server if supplied model class does not implement
+        all required methods
+        """
+        try:
+            badrestmodel()
+        except AttributeError:
+            print('--- Test BadModel class, we expect ERROR here')
+            pass
+        else:
+            fail("expected a AttributeError")
+        self.assertRaises(AttributeError,badrestmodel)
+
+    def testC(self):
+        """
+        Test REST server if supplied formatter class does not implement
+        all required methods
+        """
+        try:
+            badrestformatter()
+        except AttributeError:
+            print('--- Test BadFormatter class, we expect ERROR here')
+            pass
+        else:
+            fail("expected a AttributeError")
+        self.assertRaises(AttributeError,badrestformatter)
+
     def runTest(self):
         """
         Run the proxy test
         """
         self.testA()
+        self.testB()
+        self.testC()
+
 if __name__ == '__main__':
     unittest.main()
 

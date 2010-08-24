06a79d95efa9f4db07305a644ffd582336de92ff
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:15:05 +0000

    Added the entire execute method to SQLite Subscriptions.Jobs. Some weird error of class inheritance didnt allow it to work.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3216 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Subscriptions/Jobs.py
+++ src/python/WMCore/WMBS/SQLite/Subscriptions/Jobs.py
@@ -7,10 +7,17 @@ SQLite implementation of Subscriptions.Jobs
 Return a list of all jobs that exist for a subscription.
 """
 __all__ = []
-__revision__ = "$Id: Jobs.py,v 1.1 2008/08/09 22:15:46 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Jobs.py,v 1.2 2008/10/16 15:38:11 jcgon Exp $"
+__version__ = "$Revision: 1.2 $"
 
-from WMCore.Database.DBFormatter import DBFormatter
+from WMCore.WMBS.SQLite.Base import SQLiteBase
 from WMCore.WMBS.MySQL.Subscriptions.Jobs import Jobs as JobsMySQL
-class Jobs(DBFormatter, JobsMySQL):
+#class Jobs(DBFormatter, JobsMySQL):
+class Jobs(SQLiteBase, JobsMySQL):
     sql = JobsMySQL.sql
+
+    def execute(self, subscription = 0, conn = None, transaction = False):
+        binds = self.getBinds(subscription=subscription)
+        result = self.dbi.processData(self.sql, binds, 
+                         conn = conn, transaction = transaction)
+        return self.format(result)

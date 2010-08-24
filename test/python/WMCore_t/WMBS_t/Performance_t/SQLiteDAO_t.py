8acb47481132c0e12a8613cbf5972586fb0b56d4
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:15:16 +0000

    New Version of SQLiteDAO_t, now integrated with the .ini file. Gets the instance name as filename for the .lite file.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3219 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WMBS_t/Performance_t/SQLiteDAO_t.py
+++ test/python/WMCore_t/WMBS_t/Performance_t/SQLiteDAO_t.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import commands,os
-
+import commands, os
+from ConfigParser import ConfigParser
 class SQLiteDAO_t():
     """
     __SQLiteDAO_t__
@@ -12,15 +12,21 @@ class SQLiteDAO_t():
     """
 
     def setUp(self):
+        cfg = ConfigParser()
+        cfg.read('sqlite.ini')
         #Set specific user for mysqladmin here        
-        self.logname = 'MySQL'
-        self.sqlURI = 'sqlite:///dbperftest.lite'
+        self.logname = 'SQLite'
+        self.dbuser = cfg.get('database', 'user')
+        self.dbhost = cfg.get('database', 'host')
+        self.dbinst = cfg.get('database', 'instance')
+        self.verbose = cfg.get('output','verbose')
+        self.sqlURI = 'sqlite:///%s' % (self.dbinst+'.lite') 
 
     def tearDown(self):
         #Call superclass tearDown method
         #DB Specific tearDown code        
         try:
-            self.logger.debug(os.remove('dbperftest.lite'))
+            self.logger.debug(os.remove(self.dbinst+'.lite'))
         except OSError:
             #Don't care if the file doesn't exist
             pass

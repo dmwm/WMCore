543f3674e20e819331033afefa54340a8cab4084
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:50:39 +0000

    It runs now and seems to do something.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2818 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBSFeeder/PhEDExNotifier/PhEDExNotifierComponent.py
+++ src/python/WMCore/WMBSFeeder/PhEDExNotifier/PhEDExNotifierComponent.py
@@ -6,8 +6,8 @@ ProdAgent Component to notify clients of new transfers
 
 """
 __all__ = []
-__revision__ = "$Id: PhEDExNotifierComponent.py,v 1.6 2008/07/23 10:37:18 gowdy Exp $"
-__version__ = "$Revision: 1.6 $"
+__revision__ = "$Id: PhEDExNotifierComponent.py,v 1.7 2008/07/23 11:32:57 gowdy Exp $"
+__version__ = "$Revision: 1.7 $"
 
 import logging
 
@@ -23,95 +23,80 @@ class PhEDExNotifierComponent(FeederImpl):
 
     """
     
-    def __init__( self, nodes, baseURL ):
+    def __init__( self, nodes, baseURL = "https://cmsweb.cern.ch/phedex/datasvc/json/prod/fileReplicas" ):
         
-        self.nodes = self.makelist( nodes )
-        if baseURL == None:
-            self.baseURL = "https://cmsweb.cern.ch/phedex/datasvc/json/prod/fileReplicas"
-        else
-            self.baseURL = baseURL
-        
+        # Add the node specification the URL
+        nodeList = self.makelist( nodes )
+        for node in nodeList:
+            # for the first node need to be a bit different
+            if node == nodeList[0]:
+                self.nodeURL = baseURL + "?node=%s" % node
+            else:
+                self.nodeURL += "&node=%s" % node
+                
             
-        # Get current state from PhEDEx or local file if it exists
-        baseFileName = self.args['ComponentDir'] + "/base.json"
-        if not os.path.isfile( baseFileName ):
-            logging.debug("PhEDExNotifier: downloading base.json file")
-            urlretrieve( self.baseURL, baseFileName )
-        f = open( baseFileName )
-        self.base = eval( f.read(), {}, {} )
-        f.close()
-        phedex = self.base[ 'phedex' ]
-
-
-    def __call__(self, fileset):
+    def __call__( self, filesets ):
         """
         _operator(message, payload)_
 
         Respond to messages from the message service
 
         """
+        filesetList = self.makelist( filesets )
+        for fileset in filesetList:
+            entity = fileset.name
 
-        # Add the node specification the URL
-        for node in self.nodes:
-            # for the first node need to be a bit different
-            if node == self.nodes[0]:
-                nodeURL = self.baseURL + "?node=%s" % node
+            # determine if we have a block or dataset
+            if entity.find("#") != -1:
+                self.doBlock( entity, fileset )
             else:
-                nodeURL += "&node=%s" % node
-                
-        entity = fileset.name
-
-        # determine if we have a block or dataset
-        if entity.find("#") != -1:
-            doBlock( entity, fileset, nodeURL )
-        else:
-            doDataset( entity, fileset, nodeURL )
+                self.doDataset( entity, fileset )
 
 
-    def doDataset( entity, fileset, nodeURL ):
+    def doDataset( self, entity, fileset ):
 
         # need to adjust the URL to get the list of blocks
-        datasetURL = nodeURL,replace( "fileReplicas", "blockReplicas" )
+        datasetURL = self.nodeURL.replace( "fileReplicas", "blockReplicas" )
 
         connection = urlopen( datasetURL + "&block=%s%s" % (entity,"*") )
         aString = connection.read()
         connection.close()
 
         if aString[2:8] != "phedex":
-            logging.debug( "PhEDExNotifier: bad updated string from server follows." )
-            logging.debug( "%s" % aString )
+            print "PhEDExNotifier: bad string from server follows."
+            print "%s" % aString
 
-        phedex = eval( aString, {}, {} )
+        phedex = eval( aString.replace( "null", "None" ), {}, {} )
         
         blocks = phedex[ 'phedex' ][ 'block' ]
         if len( blocks ) == 0:
-            logging.debug( "PhEDExNotifier: Found no blocks, expected one or more" )
+            print "PhEDExNotifier: Found no blocks, expected one or more"
         for block in blocks:
-            doBlock( block.name, fileset, nodeURL )
+            self.doBlock( block[ 'name' ], fileset )
 
-    def doBlock( entity, fileset, nodeURL ):
+    def doBlock( self, entity, fileset ):
     
-        connection = urlopen( nodeURL + "&block=%s" % entity )
+        connection = urlopen( self.nodeURL + "&block=%s" % entity.replace( "#", "%23" ) )        
         aString = connection.read()
         connection.close()
 
         if aString[2:8] != "phedex":
-            logging.debug( "PhEDExNotifier: bad updated string from server follows." )
-            logging.debug( "%s" % aString )
+            print "PhEDExNotifier: bad string from server follows."
+            print "%s" % aString
 
-        phedex = eval( aString, {}, {} )
+        phedex = eval( aString.replace( "null", "None" ), {}, {} )
         
         blocks = phedex[ 'phedex' ][ 'block' ]
         if len( blocks ) != 1:
-            logging.debug( "PhEDExNotifier: Found %d blocks, expected 1, will only consider first block" % len( updatedBlocks) )
+            print "PhEDExNotifier: Found %d blocks, expected 1, will only consider first block" % len( blocks)
 
-        files = blocks[0]
+        files = blocks[0][ 'file' ]
         for file in files:
-            fileToAdd = File( file.name, file.bytes )
+            fileToAdd = File( file[ 'name' ], file[ 'bytes'] )
             replicas = file[ 'replica' ]
             if len( replicas ) > 0:
                 locations = []
                 for replica in replicas:
-                    locations.append( replica.node )
-                fileToAdd.setlocation( locations )
+                    locations.append( replica[ 'node' ] )
+                fileToAdd.setLocation( locations )
                 fileset.addFile( fileToAdd )

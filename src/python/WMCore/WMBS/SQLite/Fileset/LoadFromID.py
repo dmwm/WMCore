999b6eb614cfae3d5b555ac85b57c7633e811045
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:46:09 +0000

    DAO Load objects
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2741 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/SQLite/Fileset/LoadFromID.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+_Load_
+
+SQLite implementation of LoadFileset
+
+"""
+__all__ = []
+__revision__ = "$Id: LoadFromID.py,v 1.1 2008/07/03 09:54:22 metson Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.WMBS.SQLite.Base import SQLiteBase
+from WMCore.WMBS.MySQL.Fileset.LoadFromName import LoadFromID as LoadFilesetMySQL
+
+class LoadFromID(LoadFilesetMySQL, SQLiteBase):
+    sql = LoadFilesetMySQL.sql
+    
+    def format(self, result):
+        result = result[0].fetchall()[0]
+        time = result[2]
+        open = result[1]
+        id = int(result[0])
+        return id, open, time

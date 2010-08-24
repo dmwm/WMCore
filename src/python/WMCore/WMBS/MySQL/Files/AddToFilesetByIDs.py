5531722d2ba0ce31739d0e37471652f964ff5907
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:55:12 +0000

    add ExistsByID and AddToFilesetByIDs DAO
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3845 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/MySQL/Files/AddToFilesetByIDs.py
@@ -0,0 +1,25 @@
+"""
+MySQL implementation of Files.AddToFilesetByIDs
+"""
+from WMCore.Database.DBFormatter import DBFormatter
+
+class AddToFilesetByIDs(DBFormatter):
+    sql = """
+            insert wmbs_fileset_files (file, fileset) 
+                select :file_id, wmbs_fileset.id 
+                from wmbs_fileset where wmbs_fileset.name = :fileset"""
+        
+    def getBinds(self, file = None, fileset = None):
+        return self.dbi.buildbinds(self.dbi.makelist(fileset), 'fileset',
+                                    self.dbi.buildbinds(self.dbi.makelist(file), 'file_id'))
+    
+    def format(self, result):
+        return True
+            
+    def execute(self, file=None, fileset=None, conn = None, transaction = False):
+        binds = self.getBinds(file, fileset)
+        
+        result = self.dbi.processData(self.sql, binds, 
+                         conn = conn, transaction = transaction)
+        
+        return self.format(result)

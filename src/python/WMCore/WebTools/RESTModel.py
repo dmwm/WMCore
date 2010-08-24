115c3dbef9eec2745b751fbbf56a2946dfe61cba
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:59:47 +0000

    The model should just return data, for formatting by type
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3918 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/RESTModel.py
+++ src/python/WMCore/WebTools/RESTModel.py
@@ -5,8 +5,8 @@ Rest Model abstract implementation
 """
 
 __author__ = "Valentin Kuznetsov <vkuznet at gmail dot com>"
-__revision__ = "$Id: RESTModel.py,v 1.1 2009/03/05 15:08:49 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: RESTModel.py,v 1.2 2009/03/10 02:02:48 metson Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WebTools.Page import DatabasePage
 
@@ -14,31 +14,31 @@ class RESTModel(DatabasePage):
    """Rest model class implementation"""
    def handle_get(self, args, kwargs):
        """Example of handle_get implementation"""
-       data = "RestModel handle_get method=%s params=%s" % (args, kwargs)
-       self.debug(data)
+       data = {"args": args, "kwargs": kwargs}
+       self.debug(str(data))
        return data
 
    def handle_post(self, args, kwargs):
        """Example of handle_post implementation"""
-       data = "RestModel handle_post method=%s params=%s" % (args, kwargs)
-       self.debug(data)
+       data = {"args": args, "kwargs": kwargs}
+       self.debug(str(data))
        return data
 
    def handle_put(self, args, kwargs):
        """Example of handle_put implementation"""
-       data = "RestModel handle_put method=%s params=%s" % (args, kwargs)
-       self.debug(data)
+       data = {"args": args, "kwargs": kwargs}
+       self.debug(str(data))
        return data
 
    def handle_delete(self, args, kwargs):
        """Example of handle_delete implementation"""
-       data = "RestModel handle_delete method=%s params=%s" % (args, kwargs)
-       self.debug(data)
+       data = {"args": args, "kwargs": kwargs}
+       self.debug(str(data))
        return data
    
    def handle_update(self, args, kwargs):
        """Example of handle_update implementation"""
-       data = "RestModel handle_update method=%s params=%s" % (args, kwargs)
-       self.debug(data)
+       data = {"args": args, "kwargs": kwargs}
+       self.debug(str(data))
        return data
    

1ccb07979d6dace0e3afa4f27bba4c4a66d99ac3
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:15:09 +0000

    Fixed wrong name (It was LoadFromSpecOwner instead of LoadSpecOwner)
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3217 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Workflow/LoadSpecOwner.py
+++ src/python/WMCore/WMBS/SQLite/Workflow/LoadSpecOwner.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 """
-_LoadFromSpecOwner_
+_LoadSpecOwner_
 
-SQLite implementation of Workflow.LoadFromSpecOwner
+SQLite implementation of Workflow.LoadSpecOwner
 
 """
 __all__ = []
-__revision__ = "$Id: LoadSpecOwner.py,v 1.2 2008/10/09 09:56:39 metson Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: LoadSpecOwner.py,v 1.3 2008/10/16 15:39:13 jcgon Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.WMBS.SQLite.Base import SQLiteBase
-from WMCore.WMBS.MySQL.Workflow.LoadFromSpecOwner import LoadFromSpecOwner as LoadWorkflowMySQL
+from WMCore.WMBS.MySQL.Workflow.LoadSpecOwner import LoadSpecOwner as LoadWorkflowMySQL
 
 class LoadSpecOwner(LoadWorkflowMySQL, SQLiteBase):
     sql = LoadWorkflowMySQL.sql

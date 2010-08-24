9515d57307d19b7f0dd87121b3df7b616b7d540a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:41:14 +0000

    Comment and line length clean ups.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3622 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Workflow/LoadFromName.py
+++ src/python/WMCore/WMBS/MySQL/Workflow/LoadFromName.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 """
-_Load_
+_LoadFromName_
 
-MySQL implementation of Workflow.Load
-
+MySQL implementation of Workflow.LoadFromName
 """
+
 __all__ = []
-__revision__ = "$Id: LoadFromName.py,v 1.1 2008/07/03 09:43:56 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: LoadFromName.py,v 1.2 2009/01/14 16:41:50 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WMBS.MySQL.Workflow.LoadFromID import LoadFromID
 
 class LoadFromName(LoadFromID):
-    sql = """select id, spec, name, owner from wmbs_workflow where name = :workflow"""
-    
+    sql = """SELECT id, spec, name, owner FROM wmbs_workflow
+             WHERE name = :workflow"""

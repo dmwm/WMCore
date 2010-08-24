05e1b7e46dc865c1ca6e5ba3b23aab67243ab9cd
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:44:14 +0000

    Set YUIHOME
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3670 4525493e-7705-40b1-a816-d608a930855b

--- etc/WMCore/WebTools/start.sh
+++ etc/WMCore/WebTools/start.sh
@@ -1,5 +1,5 @@
 export WTBASE=/Users/metson/Documents/Workspace/WMCORE/src
-
+export YUIHOME=/Users/metson/WT_Devel/osx105_ia32_gcc401/external/yui/2.2.2-wt/build
 export PYTHONPATH=$PYTHONPATH:$WTBASE/python
 
 export PATH=$PATH:$WTBASE/python/WMCore/WebTools

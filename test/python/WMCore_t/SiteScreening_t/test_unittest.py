d8205d33d591cdd52a7c261b63057fd1d0171fed
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:13:51 +0000

    Make pylint 'happy'
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3196 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/SiteScreening_t/test_unittest.py
+++ test/python/WMCore_t/SiteScreening_t/test_unittest.py
@@ -1,37 +1,63 @@
 #!/usr/bin/env python
 """
-Test case for wmbs
+Test case for SiteScreening
 """
-
+__revision__ = "$Id: test_unittest.py,v 1.2 2008/10/15 13:22:32 ewv Exp $"
+__version__  = "$Revision: 1.2 $"
+__author__   = "ewv@fnal.gov"
 
 import sets
 import unittest
 
-#import common
 from  WMCore.SiteScreening import BlackWhiteListParser
 
 class FakeLogger:
-
-    def debug(*args):
+    """
+    Fake logger class
+    """
+    def __init__(self):
         pass
 
-    def msg(*args):
+    def debug(self, *args):
+        """
+        Dummy method
+        """
+        pass
+
+    def msg(self, *args):
+        """
+        Dummy method
+        """
         pass
 
 class TestBlackWhiteList(unittest.TestCase):
+    """
+    Unit tests for SiteScreening module
+    """
 
     def setUp(self):
-        self.separser = BlackWhiteListParser.SEBlackWhiteListParser(cfgParams, fakeLogger)
-        self.ceparser = BlackWhiteListParser.CEBlackWhiteListParser(cfgParams, fakeLogger)
+        """
+        Setup for unit tests
+        """
+        self.separser = \
+            BlackWhiteListParser.SEBlackWhiteListParser(cfgParams, fakeLogger)
+        self.ceparser = \
+            BlackWhiteListParser.CEBlackWhiteListParser(cfgParams, fakeLogger)
 
-    def test_se_black_list(self):
+    def testSEBlackList(self):
+        """
+        Tests black list parsing for Storage Elements
+        """
         blacklist = ['ccsrm.in2p3.fr', 'cmssrm.fnal.gov']
         other = ['t2-srm-02.lnl.infn.it', 'se-dcache.hepgrid.uerj.br']
         results = self.separser.checkBlackList(other + blacklist)
         results = sets.Set(results)
         self.failUnless(results == sets.Set(other))
 
-    def test_se_white_list(self):
+    def testSEWhiteList(self):
+        """
+        Tests white list parsing for Storage Elements
+        """
         whitelist = ['srm.ihepa.ufl.edu', 'heplnx204.pp.rl.ac.uk',
             'cluster142.knu.ac.kr']
         other = ['f-dpm001.grid.sinica.edu.tw', 'cmsrm-se01.roma1.infn.it']
@@ -39,7 +65,10 @@ class TestBlackWhiteList(unittest.TestCase):
         results = sets.Set(results)
         self.failUnless(results == sets.Set(whitelist))
 
-    def test_ce_black_list(self):
+    def testCEBlackList(self):
+        """
+        Tests black list parsing for Compute Elements
+        """
         blacklist = ['lcg02.ciemat.es', 'lcgce01.phy.bris.ac.uk',
             'lcgce02.phy.bris.ac.uk', 'cmsosgce4.fnal.gov']
         other = ['osgce.hepgrid.uerj.br', 'egeece01.ifca.es',
@@ -48,7 +77,10 @@ class TestBlackWhiteList(unittest.TestCase):
         results = sets.Set(results)
         self.failUnless(results == sets.Set(other))
 
-    def test_ce_white_list(self):
+    def testCEWhiteList(self):
+        """
+        Tests white list parsing for Compute Elements
+        """
         whitelist = ['vampire.accre.vanderbilt.edu',
             'ic-kit-lcgce.rz.uni-karlsruhe.de']
         other = ['gridce2.pi.infn.it', 'lcg02.ciemat.es']

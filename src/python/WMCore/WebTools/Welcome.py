745ce3044b45c48d0394452739589265fd53e10b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:56:43 +0000

    Make prettier
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3869 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/Welcome.py
+++ src/python/WMCore/WebTools/Welcome.py
@@ -7,12 +7,19 @@ class Welcome(Page):
  
     @expose
     def index(self):
-        html = '<h1>Welcome to the DMWM web framework</h1>'
+        html = '<html>'
+        html += '<head>'
+        html += '<link rel="stylesheet" type="text/css" href="/controllers/css/reset/style"/>'
+        html += '</head>'
+        html += '<body>'
+        html += '<h1>Welcome to the DMWM web framework</h1>'
         html += "<table>\n"
-        html += "<tr><th>Service</th><th>Description</th></tr>\n"
+        html += "<tr><th><p>Service</p></th><th><p>Description</p></th></tr>\n"
         for name, docstring in self.namesAndDocstrings:
-            html += '<tr><td><a href="%s">%s</a></td><td>%s</td></tr>' % (name, name, docstring)
+            html += '<tr><td><p><a href="%s">%s</a></p></td><td><p>%s</p></td></tr>' % (name, name, docstring)
         html += '</table>'
+        html += '</body>'
+        html += '</html>'
         return html
     
     @expose

d5ab1e0a80251c152eae1de76212199427142e0a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:45:07 +0000

    Split Get method into GetByID and GetByLFN
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2723 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/MySQL/Files/GetByID.py
@@ -0,0 +1,30 @@
+"""
+MySQL implementation of File.Get
+"""
+from WMCore.WMBS.MySQL.Base import MySQLBase
+
+class GetByID(MySQLBase):
+    sql = """select id, lfn, size, events, run, lumi
+                 from wmbs_file_details where id = :file"""
+    
+    def getBinds(self, files=None):
+        binds = []
+        files = self.dbi.makelist(files)
+        for f in files:
+            binds.append({'file': f})
+        return binds
+    
+    def format(self, result):
+        out = result[0].fetchall()
+        out = out[0]
+        out = int(out[0]), str(out[1]), int(out[2]), int(out[3]), int(out[4]), int(out[5])
+        return out 
+        
+    def execute(self, files=None, conn = None, transaction = False):
+        binds = self.getBinds(files)
+        
+        self.logger.debug('File.Get binds: %s' % binds)
+        result = self.dbi.processData(self.sql, binds, 
+                         conn = conn, transaction = transaction)
+        self.logger.debug('File.Get result: %s' % result)
+        return self.format(result)
--- src/python/WMCore/WMBS/MySQL/Files/GetByLFN.py
+++ src/python/WMCore/WMBS/MySQL/Files/GetByLFN.py
@@ -3,7 +3,7 @@ MySQL implementation of File.Get
 """
 from WMCore.WMBS.MySQL.Base import MySQLBase
 
-class Get(MySQLBase):
+class GetByLFN(MySQLBase):
     sql = """select id, lfn, size, events, run, lumi
                  from wmbs_file_details where lfn = :file"""
     #select id, lfn, size, events, run, lumi from wmbs_file_details where id = :file
@@ -21,7 +21,7 @@ class Get(MySQLBase):
         out = int(out[0]), str(out[1]), int(out[2]), int(out[3]), int(out[4]), int(out[5])
         return out 
         
-    def execute(self, files=None, size=0, events=0, run=0, lumi=0, conn = None, transaction = False):
+    def execute(self, files=None, conn = None, transaction = False):
         binds = self.getBinds(files)
         
         self.logger.debug('File.Get binds: %s' % binds)

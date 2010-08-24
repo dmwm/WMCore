0100152d1972032357da71c2ff3540511d5c796f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:41:11 +0000

    Minor cleanups and use formatDict() to format the query results.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3621 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Workflow/LoadFromID.py
+++ src/python/WMCore/WMBS/MySQL/Workflow/LoadFromID.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
 """
-_Load_
+_LoadFromID_
 
-MySQL implementation of Workflow.Load
-
+MySQL implementation of Workflow.LoadFromID
 """
+
 __all__ = []
-__revision__ = "$Id: LoadFromID.py,v 1.2 2008/11/20 21:52:33 sryu Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: LoadFromID.py,v 1.3 2009/01/14 16:43:28 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class LoadFromID(DBFormatter):
-    sql = """select id, spec, name, owner from wmbs_workflow where id = :workflow"""
+    sql = "SELECT id, spec, name, owner FROM wmbs_workflow WHERE id = :workflow"
             
-    def getBinds(self, workflow = None):
-        return self.dbi.buildbinds(self.dbi.makelist(workflow), 'workflow')
-    
-    
     def execute(self, workflow = None, conn = None, transaction = False):
-        result = self.dbi.processData(self.sql, self.getBinds(workflow), 
+        result = self.dbi.processData(self.sql, {"workflow": workflow}, 
                          conn = conn, transaction = transaction)
-        return self.format(result)  
+        return self.formatDict(result)[0]
--- src/python/WMCore/WMBS/MySQL/Workflow/LoadFromSpecOwner.py
+++ src/python/WMCore/WMBS/MySQL/Workflow/LoadFromSpecOwner.py
@@ -2,24 +2,21 @@
 """
 _LoadFromSpecOwner_
 
-Oracle implementation of Workflow.LoadFromSpecOwner
-
+MySQL implementation of Workflow.LoadFromSpecOwner
 """
 
 __all__ = []
-__revision__ = "$Id: LoadFromSpecOwner.py,v 1.2 2008/11/24 21:47:12 sryu Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: LoadFromSpecOwner.py,v 1.3 2009/01/14 16:41:11 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 
-from WMCore.WMBS.MySQL.Workflow.LoadFromID import LoadFromID
+from WMCore.Database.DBFormatter import DBFormatter
     
-class LoadFromSpecOwner(LoadFromID):
-    sql = """select id, spec, name, owner from wmbs_workflow where spec = :spec and owner=:owner"""
+class LoadFromSpecOwner(DBFormatter):
+    sql = """SELECT id, spec, name, owner FROM wmbs_workflow
+             WHERE spec = :spec and owner = :owner"""
     
-    def getBinds(self, spec = None, owner=None):
-        return self.dbi.buildbinds(self.dbi.makelist(spec), 'spec', 
-                                   self.dbi.buildbinds(self.dbi.makelist(owner), 'owner'))
-    
-    def execute(self, spec = None, owner=None, conn = None, transaction = False):
-        result = self.dbi.processData(self.sql, self.getBinds(spec, owner), 
+    def execute(self, spec = None, owner = None, conn = None,
+                transaction = False):
+        result = self.dbi.processData(self.sql, {"spec": spec, "owner": owner},
                          conn = conn, transaction = transaction)
-        return self.format(result)
+        return self.formatDict(result)[0]

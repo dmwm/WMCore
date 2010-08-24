fe792f689fb3ca0d7a33e232188586f640c7da59
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:15:01 +0000

    Fixed the class name (was InFileset instead of ForFileset)
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3215 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Subscriptions/ForFileset.py
+++ src/python/WMCore/WMBS/SQLite/Subscriptions/ForFileset.py
@@ -4,5 +4,5 @@ SQLite implementation of Files.InFileset
 from WMCore.WMBS.SQLite.Base import SQLiteBase
 from WMCore.WMBS.MySQL.Subscriptions.ForFileset import ForFileset as ForFilesetMySQL
 
-class InFileset(ForFilesetMySQL, SQLiteBase):
+class ForFileset(ForFilesetMySQL, SQLiteBase):
     sql = ForFilesetMySQL.sql

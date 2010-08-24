737eb22441d6ae838dc4d0c30a1fffcd04a2913a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:55:56 +0000

    Use inner select with the insert
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2908 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Files/SetLocation.py
+++ src/python/WMCore/WMBS/SQLite/Files/SetLocation.py
@@ -1,8 +1,29 @@
 """
 SQLite implementation of SetFileLocation
 """
-from WMCore.WMBS.SQLite.Base import SQLiteBase
+
 from WMCore.WMBS.MySQL.Files.SetLocation import SetLocation as SetLocationMySQL
 
-class SetLocation(SetLocationMySQL, SQLiteBase):
-    sql = SetLocationMySQL.sql
+class SetLocation(SetLocationMySQL):
+    sql = """insert into wmbs_file_location (file, location) 
+                values ((select id from wmbs_file_details where lfn = :file),
+                (select id from wmbs_location where se_name = :location))"""
+                
+    def getBinds(self, file = None, location = None):
+        if type(location) == type('string'):
+            return self.dbi.buildbinds(self.dbi.makelist(file), 'file', 
+                   self.dbi.buildbinds(self.dbi.makelist(location), 'location'))
+        elif type(location) == type([]):
+            binds = []
+            for l in location:
+                binds.extend(self.dbi.buildbinds(self.dbi.makelist(file), 'file', 
+                   self.dbi.buildbinds(self.dbi.makelist(l), 'location')))
+            return binds
+        
+    def execute(self, file = None, sename = None, conn = None, transaction = False):
+        binds = self.getBinds(file, sename)
+        self.logger.debug('File.SetLocation binds: %s' % binds)
+        result = self.dbi.processData(self.sql, binds, 
+                         conn = conn, transaction = transaction)
+        
+        return self.format(result)

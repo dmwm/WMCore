9ed64bbb7e6eb57dcacb2f6be00578ac6bbcbda1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:42:23 +0000

    There is no need to overwrite the execute() function with one that does that same exact thing.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3640 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Subscriptions/Jobs.py
+++ src/python/WMCore/WMBS/SQLite/Subscriptions/Jobs.py
@@ -3,20 +3,13 @@
 _Jobs_
 
 SQLite implementation of Subscriptions.Jobs
-
-Return a list of all jobs that exist for a subscription.
 """
+
 __all__ = []
-__revision__ = "$Id: Jobs.py,v 1.4 2008/12/05 21:06:58 sryu Exp $"
-__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: Jobs.py,v 1.5 2009/01/16 22:27:37 sfoulkes Exp $"
+__version__ = "$Revision: 1.5 $"
 
 from WMCore.WMBS.MySQL.Subscriptions.Jobs import Jobs as JobsMySQL
 
 class Jobs(JobsMySQL):
     sql = JobsMySQL.sql
-
-    def execute(self, subscription = 0, conn = None, transaction = False):
-        binds = self.getBinds(subscription=subscription)
-        result = self.dbi.processData(self.sql, binds, 
-                         conn = conn, transaction = transaction)
-        return self.format(result)

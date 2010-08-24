c16561f2379e38e478f74bd154c914d42ea9a7b9
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:13:08 +0000

    Adding new module, SiteScreening for white and blacklisting
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3185 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/SiteScreening/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+__init__
+
+Module containing common components for filtering
+sites (white lists and black lists)
+
+"""
+
+__revision__ = "$Id: __init__.py,v 1.1 2008/10/14 13:19:29 ewv Exp $"
+__version__ = "$Revision: 1.1 $"
+__author__ = "fvlingen@caltech.edu"
+
+

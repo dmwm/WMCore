f7221cd85b221d6f3fa526513be57728fc69b099
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:01:38 +0000

    Add the SAM service
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3002 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/Services/SAM/SAM.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""
+_SAM_
+
+Talk to the SAM Service to get site status from the results of SAM tests.
+"""
+
+__revision__ = "$Id: SAM.py,v 1.1 2008/09/18 15:43:56 metson Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from BeautifulSoup import BeautifulSoup
+from WMCore.Services.AuthorisedService import AuthorisedService
+
+class SAM(AuthorisedService):
+    def getCMSSWInstalls(self, ce=None):
+        """
+        Contact the SAM test page and get the tested, installed software on a CE.
+        TODO: improved error handling
+        """
+        url = "%sfunct=TestResultLatest&nodename=%s&vo=cms&testname=CE-cms-swinst" % (self.endpoint, ce)
+        file = 'sam_cmssw_inst_%s.html' % ce
+        pageSoup = BeautifulSoup(self.refreshCache(file, url))
+        
+        swlists = pageSoup.findAll('ol', { "class" : "CMSSW-list"})
+        self.logger.debug(swlists)
+        
+        software = {}
+        for tag in swlists:
+            children=[]
+            for i in tag.contents:
+                child = i.string
+                if child != '\n' and child != 'error':
+                    children.append(child)
+                
+            children.sort()
+            children.reverse()
+            software[tag['id']] = children
+        return software
+        
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='/tmp/sam/samstatus.log',
+                    filemode='w')
+    
+    dict = {}
+    dict['endpoint'] = 'https://lcg-sam.cern.ch:8443/sam/sam.py?'
+    dict['cachepath'] = '/tmp/sam'
+    dict['type'] = 'text/json'
+    dict['logger'] = logging.getLogger('SAMtest')
+    
+    sam = SAM(dict = dict)
+    print sam.getCMSSWInstalls(ce='lcgce02.gridpp.rl.ac.uk')
---
+++ src/python/WMCore/Services/SAM/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+_WMCore/Services_
+
+Services for Workload Management Packages
+
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2008/09/18 15:43:56 metson Exp $"
+__version__ = "$Revision: 1.1 $"
+

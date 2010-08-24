335596dda24610c90cacfefc908a2fe24be8f7ec
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:58:14 +0000

    Add stub for T0AST and Oracle implementations
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2947 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/T0AST/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+_T0AST_
+
+T0AST backed implementation of WMBS
+
+
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2008/09/09 17:01:37 metson Exp $"
+__version__ = "$Revision: 1.1 $"
+

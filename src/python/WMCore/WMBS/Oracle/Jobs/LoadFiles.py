b89951ab36af8c3d0dcea84770a8d06f5a2f9e6e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:44:56 +0000

    move Oracle dependent part
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3681 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Jobs/LoadFiles.py
+++ src/python/WMCore/WMBS/Oracle/Jobs/LoadFiles.py
@@ -6,9 +6,10 @@ Oracle implementation of Jobs.LoadFiles
 """
 
 __all__ = []
-__revision__ = "$Id: LoadFiles.py,v 1.4 2009/01/16 22:32:14 sfoulkes Exp $"
-__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: LoadFiles.py,v 1.5 2009/01/21 22:01:30 sryu Exp $"
+__version__ = "$Revision: 1.5 $"
 
+from WMCore.Database.DBFormatter import DBFormatter
 from WMCore.WMBS.MySQL.Jobs.LoadFiles import LoadFiles as LoadFilesMySQL
 
 class LoadFiles(LoadFilesMySQL):
@@ -19,3 +20,20 @@ class LoadFiles(LoadFilesMySQL):
     database.
     """
     sql = "SELECT fileid FROM wmbs_job_assoc WHERE JOB = :jobid"
+
+    def formatDict(self, results):
+        """
+        _formatDict_
+
+        Cast the file attribute to an integer, 
+        """
+        formattedResults = DBFormatter.formatDict(self, results)
+        
+        dictResults = []
+        for formattedResult in formattedResults:
+            dictResult = {}
+            if "fileid" in formattedResult.keys():
+                dictResult["id"] = int(formattedResult["fileid"])
+                dictResults.append(dictResult)
+    
+        return dictResults

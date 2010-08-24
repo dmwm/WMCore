cfdce6ef6a590faf351750838f3313f3c2684ffe
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:16:07 +0000

    New Base DB Performance test class for WMBS. Base_t is deprecated
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3232 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/python/WMCore_t/WMBS_t/Performance_t/WMBSBase.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+import os, logging, time, random
+
+from WMCore_t.Database_t.Performance import Performance
+from WMCore.DAOFactory import DAOFactory
+from WMCore.WMBS.File import File
+from WMCore.WMBS.Fileset import Fileset
+from WMCore.WMBS.Job import Job
+from WMCore.WMBS.Subscription import Subscription
+from WMCore.WMBS.JobGroup import JobGroup
+from WMCore.WMBS.Workflow import Workflow
+from sets import Set
+
+class WMBSBase(Performance):
+    """
+    __WMBSBase__
+
+    Base class for Performance Tests at WMBS
+
+    This class is abstract, proceed to the DB specific testcase
+    to run the test
+
+
+    """
+
+    def setUp(self, dbf):
+        """
+        Common setUp for all Performance tests
+
+        """
+        
+        Performance.setUp(self)
+        #Place common execute method arguments here        
+        #TODO -Still to be implemented
+        self.baseexec=''
+
+        #possibly deprecated, need to use selist instead
+        self.sename='localhost'        
+        
+        self.tearDown()
+
+        self.dbf=dbf
+
+        self.dao = DAOFactory(package='WMCore.WMBS', logger=self.logger, 
+                        dbinterface=self.dbf.connect())
+        
+        assert self.dao(classname='CreateWMBS').execute()       
+
+        #Creating the Locations at the Database
+        self.selist = ['localhost']        
+        for se in self.selist:
+            self.dao(classname='Locations.New').execute(sename=se)      
+
+        # Create a File to be used as argument for the performance test
+        file_lfn = '/tmp/file/fileexample'
+        file_events = 1111
+        file_size = 1111
+        file_run = 111
+        file_lumi = 0
+        
+        self.testFile = File(lfn=file_lfn, size=file_size, events=file_events, run=file_run,
+                    lumi=file_lumi, logger=self.logger, dbfactory=self.dbf)
+        self.testFile.save()
+        self.testFile.load()
+
+        # Create a Fileset of random, parentless, childless, unlocatied file
+        filelist = []
+
+        #Generating Files        
+        for x in range(random.randint(1000,3000)):
+            file = File(lfn='/store/data/%s/%s/file.root' % (random.randint(1000, 9999), 
+                                                  random.randint(1000, 9999)),
+                        size=random.randint(1000, 2000),
+                        events = 1000,
+                        run = random.randint(0, 2000),
+                        lumi = random.randint(0, 8), 
+                        logger=self.logger, 
+                        dbfactory=self.dbf)
+            
+            filelist.append(file)
+    
+        #Creating mySQL Fileset        
+        self.testFileset = Fileset(name='testFileSet', 
+                            files=filelist, 
+                            logger=self.logger, 
+                            dbfactory=self.dbf) 
+        self.testFileset.create()     
+
+        #Creating mySQL Workflow
+        self.testWorkflow = Workflow(spec='Test', owner='PerformanceTestCase', name='TestWorkflow', logger=self.logger, dbfactory=self.dbf)
+        self.testWorkflow.create()
+
+        #Creating MySQL Subscription
+        self.testSubscription = Subscription(fileset=self.testFileset, 
+                        workflow=self.testWorkflow, logger=self.logger, 
+                        dbfactory=self.dbf)
+        self.testSubscription.create()
+
+        #Instatiating mySQL Job
+        self.testJob = Job(name='TestJob',files=self.testFileset, logger=self.logger, dbfactory=self.dbf)
+
+        #Creating mySQL JobGroup
+        testSet = Set()
+        testSet.add(self.testJob)
+        self.testJobGroup = JobGroup(subscription=self.testSubscription, jobs=testSet)
+
+        #Creating mySQL Job for testing
+        self.testJob.create(group=self.testJobGroup.id)
+
+
+    def tearDown(self):
+        #Base tearDown method for the DB Performance test
+        Performance.tearDown(self)
+        pass
+

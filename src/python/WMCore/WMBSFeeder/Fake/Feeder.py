75af83cf4737b63488c1ea3e627ba089f0d6ed33
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:50:01 +0000

    Changed to use Fileset properly
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2807 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBSFeeder/Fake/Feeder.py
+++ src/python/WMCore/WMBSFeeder/Fake/Feeder.py
@@ -4,10 +4,11 @@ _FakeFeeder_
 
 A feeder implementation that generates fake files. Make up random sizes, names and locations etc.
 
+Always returns new/unique files.
 """
 __all__ = []
-__revision__ = "$Id: Feeder.py,v 1.1 2008/07/21 15:42:33 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Feeder.py,v 1.2 2008/07/21 16:21:32 metson Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.DataStructs.WMObject import WMObject
 from WMCore.DataStructs.File import File
@@ -15,20 +16,20 @@ from WMCore.DataStructs.File import File
 import time, random, md5
 
 def uuid( *args ):
-  """
+    """
     Generates a universally unique ID.
     Any arguments only create more randomness.
-  """
-  t = long( time.time() * 1000 )
-  r = long( random.random()*100000000000000000L )
-  try:
-    a = socket.gethostbyname( socket.gethostname() )
-  except:
-    # if we can't get a network address, just imagine one
-    a = random.random()*100000000000000000L
-  data = str(t)+' '+str(r)+' '+str(a)+' '+str(args)
-  data = md5.md5(data).hexdigest()
-  return data
+    """
+    t = long( time.time() * 1000 )
+    r = long( random.random()*100000000000000000L )
+    try:
+      a = socket.gethostbyname( socket.gethostname() )
+    except:
+      # if we can't get a network address, just imagine one
+      a = random.random()*100000000000000000L
+    data = str(t)+' '+str(r)+' '+str(a)+' '+str(args)
+    data = md5.md5(data).hexdigest()
+    return data
   
 
 class Feeder(WMObject):
@@ -36,27 +37,28 @@ class Feeder(WMObject):
         self.max = max
         self.locations = ['fakese01.cern.ch','fakese02.cern.ch','fakese01.fnal.gov','fakese02.fnal.gov','fakese01.rl.ac.uk','fakese02.rl.ac.uk']
     
-    def __call__(self):
+    def __call__(self, fileset=["My Cool Fileset"]):
         """
         return a list of files (DataStructs.File) at locations (string)
         """
         num_files = random.randint(0 , self.max)
         
         list = []
-        for i in range(0, num_files):
-            # Decide where the file is
-            locs = []
-            for i in range(0, len(self.locations)):
-                if random.randint(0 , 1):
-                    locs.append(self.locations[i])
-            lfn='/store/data/fake-feeder-files/notreal/%s.root' % uuid(i) 
-            size=2000 + ((i-5) * 50) 
-            events=1000 + ((i-3) * 150) 
-            run = random.randint(0 , int(3.14159265 * i * self.max)) 
-            lumi = random.randint(0 ,10)
-            file = File(lfn, size, events, run, lumi)
-            list.append((file, locs))
+        for f in fileset:
+            for i in range(0, num_files):
+                # Decide where the file is
+                locs = []
+                for i in range(0, len(self.locations)):
+                    if random.randint(0 , 1):
+                        locs.append(self.locations[i])
+                lfn='/store/data/fake-feeder-files/notreal/%s.root' % uuid(i) 
+                size=2000 + ((i-5) * 50) 
+                events=1000 + ((i-3) * 150) 
+                run = random.randint(0 , int(3.14159265 * i * self.max)) 
+                lumi = random.randint(0 ,10)
+                file = File(lfn, size, events, run, lumi)
+                file.setLocation(locs)
+                f.addFile(file)
             
-        return list
                 
             

9170df9389fce2a5debd8e0f29e2bb4ad88e0250
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:17:52 +0000

    Add MarkLocation
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3260 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/MySQL/Subscriptions/MarkLocation.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+_MarkLocation_
+
+MySQL implementation of Subscription.MarkLocation
+
+Insert a record to wmbs_subscription_location
+
+CREATE TABLE wmbs_subscription_location (
+             subscription     INT(11)      NOT NULL,
+             location         INT(11)      NOT NULL,
+             valid            BOOLEAN      NOT NULL DEFAULT TRUE,
+             FOREIGN KEY(subscription)  REFERENCES wmbs_subscription(id)
+               ON DELETE CASCADE,
+             FOREIGN KEY(location)     REFERENCES wmbs_location(id)
+               ON DELETE CASCADE)
+"""
+__all__ = []
+__revision__ = "$Id: MarkLocation.py,v 1.1 2008/10/24 16:03:11 metson Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class MarkLocation(DBFormatter):
+    sql = """insert into wmbs_subscription_location 
+            (subscription, location, valid)
+        select :subscription, id, :valid from wmbs_location 
+        where sename = :location"""
+            
+    def execute(self, subscription=None, location=None, valid = None, 
+                conn = None, transaction = False):
+        
+        result = self.dbi.processData(self.sql, 
+                                      self.getBinds(subscription=subscription, 
+                                              location=location, valid=valid), 
+                         conn = conn, transaction = transaction)
+        return self.format(result)
---
+++ src/python/WMCore/WMBS/SQLite/Subscriptions/MarkLocation.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+_MarkLocation_
+
+MySQL implementation of Subscription.MarkLocation
+
+Insert a record to wmbs_subscription_location
+
+CREATE TABLE wmbs_subscription_location (
+             subscription     INT(11)      NOT NULL,
+             location         INT(11)      NOT NULL,
+             valid            BOOLEAN      NOT NULL DEFAULT TRUE,
+             FOREIGN KEY(subscription)  REFERENCES wmbs_subscription(id)
+               ON DELETE CASCADE,
+             FOREIGN KEY(location)     REFERENCES wmbs_location(id)
+               ON DELETE CASCADE)
+"""
+__all__ = []
+__revision__ = "$Id: MarkLocation.py,v 1.1 2008/10/24 16:03:25 metson Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class MarkLocation(DBFormatter):
+    sql = """insert into wmbs_subscription_location 
+            (subscription, location, valid)
+        values (
+            :subscription, 
+            (select id from wmbs_location where sename = :location),
+            :valid
+        )"""
+    
+    def execute(self, subscription=None, location=None, valid = None, 
+                conn = None, transaction = False):
+        
+        result = self.dbi.processData(self.sql, 
+                                      self.getBinds(subscription=subscription, 
+                                              location=location, valid=valid), 
+                         conn = conn, transaction = transaction)
+        return self.format(result)

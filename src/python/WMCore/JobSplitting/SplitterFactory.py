f9e9bff011b1b89b6ae1bcc25f649fa0f18da764
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:06:29 +0000

    Minor updates to comments
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3081 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/JobSplitting/SplitterFactory.py
+++ src/python/WMCore/JobSplitting/SplitterFactory.py
@@ -7,15 +7,17 @@ class SplitterFactory(WMObject):
     """
 
     def __init__(self, package='WMCore.JobSplitting'):
+        # package is the package the splitter will be loaded from 
         self.package = package
         
     def __call__(self, subscription=None, package='WMCore.DataStructs'):
+        # package is the package output of the splitter will be loaded from 
         """
         Instantiate an Subscription.split_algo and 
         run that algorithm on subscription
         """
         algorithm = subscription.split_algo
-        #TODO: Import the algorithm
+        
         module = "%s.%s" % (self.package, algorithm)
         module = __import__(module, globals(), locals(), [algorithm])#, -1)
         splitter = getattr(module, algorithm.split('.')[-1])

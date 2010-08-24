496058f41e03569a6f75694f1c8929e6dc26ef2c
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:38:58 +0000

    Add unit tests for transactions.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3589 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WMBS_t/Subscription_t.py
+++ test/python/WMCore_t/WMBS_t/Subscription_t.py
@@ -118,6 +118,124 @@ class Subscription_t(unittest.TestCase):
         testFileC.delete()
         testWorkflow.delete()        
         return
+
+    def testCreateTransaction(self):
+        """
+        _testCreateTransaction_
+
+        Create a subscription and commit it to the database.  Rollback the
+        database connection and verify that the subscription is no longer
+        there.
+        """
+        testWorkflow = Workflow(spec = "spec.xml", owner = "Simon",
+                                name = "wf001")
+        testWorkflow.create()
+
+        testFileA = File(lfn = "/this/is/a/lfnA", size = 1024,
+                         events = 20)
+        testFileB = File(lfn = "/this/is/a/lfnB", size = 1024,
+                         events = 20)
+        testFileC = File(lfn = "/this/is/a/lfnC", size = 1024,
+                         events = 20)
+        testFileA.create()
+        testFileB.create()
+        testFileC.create()
+        
+        testFilesetA = Fileset(name = "TestFileset")
+        testFilesetA.create()
+        
+        testFilesetA.addFile(testFileA)
+        testFilesetA.addFile(testFileB)
+        testFilesetA.addFile(testFileC)
+        testFilesetA.commit()
+
+        testSubscription = Subscription(fileset = testFilesetA,
+                                        workflow = testWorkflow)
+
+        assert testSubscription.exists() == False, \
+               "ERROR: Subscription exists before it was created"
+
+        myThread = threading.currentThread()
+        myThread.transaction.begin()
+
+        testSubscription.create()
+
+        assert testSubscription.exists() >= 0, \
+               "ERROR: Subscription does not exist after it was created"
+
+        myThread.transaction.rollback()
+
+        assert testSubscription.exists() == False, \
+               "ERROR: Subscription exists after transaction was rolled back."
+
+        testFilesetA.delete()
+        testFileA.delete()
+        testFileB.delete()
+        testFileC.delete()
+        testWorkflow.delete()        
+        return    
+
+    def testDeleteTransaction(self):
+        """
+        _testDeleteTransaction_
+
+        Create a subscription and commit it to the database.  Begin a new
+        transactions and delete the subscription.  Verify that the subscription
+        is no longer in the database and then roll back the transaction.  Verify
+        that the subscription is once again in the database.
+        """
+        testWorkflow = Workflow(spec = "spec.xml", owner = "Simon",
+                                name = "wf001")
+        testWorkflow.create()
+
+        testFileA = File(lfn = "/this/is/a/lfnA", size = 1024,
+                         events = 20)
+        testFileB = File(lfn = "/this/is/a/lfnB", size = 1024,
+                         events = 20)
+        testFileC = File(lfn = "/this/is/a/lfnC", size = 1024,
+                         events = 20)
+        testFileA.create()
+        testFileB.create()
+        testFileC.create()
+        
+        testFilesetA = Fileset(name = "TestFileset")
+        testFilesetA.create()
+        
+        testFilesetA.addFile(testFileA)
+        testFilesetA.addFile(testFileB)
+        testFilesetA.addFile(testFileC)
+        testFilesetA.commit()
+
+        testSubscription = Subscription(fileset = testFilesetA,
+                                        workflow = testWorkflow)
+
+        assert testSubscription.exists() == False, \
+               "ERROR: Subscription exists before it was created"
+
+        testSubscription.create()
+
+        assert testSubscription.exists() >= 0, \
+               "ERROR: Subscription does not exist after it was created"
+
+        myThread = threading.currentThread()
+        myThread.transaction.begin()
+
+        testSubscription.delete()
+
+        assert testSubscription.exists() == False, \
+               "ERROR: Subscription exists after it was deleted"
+
+        myThread.transaction.rollback()
+
+        assert testSubscription.exists() >= 0, \
+               "ERROR: Subscription does not exist after roll back."
+
+        testFilesetA.delete()
+        testFileA.delete()
+        testFileB.delete()
+        testFileC.delete()
+        testWorkflow.delete()        
+        return
     
     def testFailFiles(self):
         """
@@ -171,6 +289,70 @@ class Subscription_t(unittest.TestCase):
         testFileB.delete()
         testFileC.delete()
         return
+
+    def testFailFilesTransaction(self):
+        """
+        _testFailFilesTransaction_
+
+        Create a subscription and fail some files that are in it's input
+        fileset.  Rollback the subscription and verify that the files are
+        no longer failed.
+        """
+        testWorkflow = Workflow(spec = "spec.xml", owner = "Simon",
+                                name = "wf001")
+        testWorkflow.create()
+
+        testFileA = File(lfn = "/this/is/a/lfnA", size = 1024,
+                         events = 20)
+        testFileB = File(lfn = "/this/is/a/lfnB", size = 1024,
+                         events = 20)
+        testFileC = File(lfn = "/this/is/a/lfnC", size = 1024,
+                         events = 20)
+        testFileA.create()
+        testFileB.create()
+        testFileC.create()
+        
+        testFileset = Fileset(name = "TestFileset")
+        testFileset.create()
+        
+        testFileset.addFile(testFileA)
+        testFileset.addFile(testFileB)
+        testFileset.addFile(testFileC)        
+        testFileset.commit()
+
+        testSubscription = Subscription(fileset = testFileset,
+                                        workflow = testWorkflow)
+        testSubscription.create()
+
+        myThread = threading.currentThread()
+        myThread.transaction.begin()
+
+        testSubscription.failFiles([testFileA, testFileC])
+        failedFiles = testSubscription.filesOfStatus(status = "FailedFiles")
+
+        goldenFiles = [testFileA, testFileC]
+        for failedFile in failedFiles:
+            assert failedFile in goldenFiles, \
+                   "ERROR: Unknown failed files"
+            goldenFiles.remove(failedFile)
+
+        assert len(goldenFiles) == 0, \
+               "ERROR: Missing failed files"
+
+        myThread.transaction.rollback()
+
+        failedFiles = testSubscription.filesOfStatus(status = "FailedFiles")
+
+        assert len(failedFiles) == 0, \
+               "ERROR: Transaction did not roll back failed files"
+
+        testSubscription.delete()
+        testWorkflow.delete()
+        testFileset.delete()
+        testFileA.delete()
+        testFileB.delete()
+        testFileC.delete()
+        return    
     
     def testCompleteFiles(self):
         """
@@ -223,6 +405,70 @@ class Subscription_t(unittest.TestCase):
         testFileB.delete()
         testFileC.delete()        
         return
+
+    def testCompleteFilesTransaction(self):
+        """
+        _testCompleteFilesTransaction_
+
+        Create a subscription and complete some files that are in it's input
+        fileset.  Rollback the transaction and verify that the files are no
+        longer marked as complete.
+        """
+        testWorkflow = Workflow(spec = "spec.xml", owner = "Simon",
+                                name = "wf001")
+        testWorkflow.create()
+
+        testFileA = File(lfn = "/this/is/a/lfnA", size = 1024,
+                         events = 20)
+        testFileB = File(lfn = "/this/is/a/lfnB", size = 1024,
+                         events = 20)
+        testFileC = File(lfn = "/this/is/a/lfnC", size = 1024,
+                         events = 20)
+        testFileA.create()
+        testFileB.create()
+        testFileC.create()
+        
+        testFileset = Fileset(name = "TestFileset")
+        testFileset.create()
+        
+        testFileset.addFile(testFileA)
+        testFileset.addFile(testFileB)
+        testFileset.addFile(testFileC)        
+        testFileset.commit()
+
+        testSubscription = Subscription(fileset = testFileset,
+                                        workflow = testWorkflow)
+        testSubscription.create()
+
+        myThread = threading.currentThread()
+        myThread.transaction.begin()
+        
+        testSubscription.completeFiles([testFileA, testFileC])
+        completedFiles = testSubscription.filesOfStatus(status = "CompletedFiles")
+
+        goldenFiles = [testFileA, testFileC]
+        for completedFile in completedFiles:
+            assert completedFile in goldenFiles, \
+                   "ERROR: Unknown completed file"
+            goldenFiles.remove(completedFile)
+
+        assert len(goldenFiles) == 0, \
+               "ERROR: Missing completed files"
+
+        myThread.transaction.rollback()
+
+        completedFiles = testSubscription.filesOfStatus(status = "CompletedFiles")
+
+        assert len(completedFiles) == 0, \
+               "ERROR: Transaction didn't roll back completed files."
+
+        testSubscription.delete()
+        testWorkflow.delete()
+        testFileset.delete()
+        testFileA.delete()
+        testFileB.delete()
+        testFileC.delete()        
+        return    
     
     def testAcquireFiles(self):
         """
@@ -276,6 +522,69 @@ class Subscription_t(unittest.TestCase):
         testFileB.delete()
         testFileC.delete()
         return
+
+    def testAcquireFilesTransaction(self):
+        """
+        _testAcquireFilesTransaction_
+
+        Create a subscription and acquire some files from it's input fileset.
+        Rollback the transaction and verify that the files are no longer marked
+        as acquired.
+        """
+        testWorkflow = Workflow(spec = "spec.xml", owner = "Simon",
+                                name = "wf001")
+        testWorkflow.create()
+
+        testFileA = File(lfn = "/this/is/a/lfnA", size = 1024,
+                         events = 20)
+        testFileB = File(lfn = "/this/is/a/lfnB", size = 1024,
+                         events = 20)
+        testFileC = File(lfn = "/this/is/a/lfnC", size = 1024,
+                         events = 20)
+        testFileA.create()
+        testFileB.create()
+        testFileC.create()
+        
+        testFileset = Fileset(name = "TestFileset")
+        testFileset.create()
+        
+        testFileset.addFile(testFileA)
+        testFileset.addFile(testFileB)
+        testFileset.addFile(testFileC)        
+        testFileset.commit()
+
+        testSubscription = Subscription(fileset = testFileset,
+                                        workflow = testWorkflow)
+        testSubscription.create()
+
+        myThread = threading.currentThread()
+        myThread.transaction.begin()
+
+        testSubscription.acquireFiles([testFileA, testFileC])
+        acquiredFiles = testSubscription.filesOfStatus(status = "AcquiredFiles")
+
+        goldenFiles = [testFileA, testFileC]
+        for acquiredFile in acquiredFiles:
+            assert acquiredFile in goldenFiles, \
+                   "ERROR: Unknown acquired file"
+            goldenFiles.remove(acquiredFile)
+
+        assert len(goldenFiles) == 0, \
+               "ERROR: Missing acquired files"
+
+        myThread.transaction.rollback()
+        acquiredFiles = testSubscription.filesOfStatus(status = "AcquiredFiles")
+        
+        assert len(acquiredFiles) == 0, \
+               "ERROR: Transaction didn't roll back acquired files."
+
+        testSubscription.delete()
+        testWorkflow.delete()
+        testFileset.delete()
+        testFileA.delete()
+        testFileB.delete()
+        testFileC.delete()
+        return    
 
     def testAvailableFiles(self):
         """
@@ -336,6 +645,93 @@ class Subscription_t(unittest.TestCase):
 
         assert len(goldenFiles) == 0, \
                "ERROR: Missing available files"
+
+        testSubscription.delete()
+        testWorkflow.delete()
+        testFileset.delete()
+        testFileA.delete()
+        testFileB.delete()
+        testFileC.delete()
+        testFileD.delete()
+        testFileE.delete()
+        testFileF.delete()        
+        return
+
+    def testAvailableFilesTransaction(self):
+        """
+        _testAvailableFilesTransaction_
+
+        Create a subscription and mark a couple of it's input files as
+        complete, failed and acquired.  Rollback the transactions and verify
+        that all the files are listed as available.
+        """
+        testWorkflow = Workflow(spec = "spec.xml", owner = "Simon",
+                                name = "wf001")
+        testWorkflow.create()
+
+        testFileA = File(lfn = "/this/is/a/lfnA", size = 1024, events = 20,
+                         locations = Set(["goodse.cern.ch"]))
+        testFileB = File(lfn = "/this/is/a/lfnB", size = 1024, events = 20,
+                         locations = Set(["goodse.cern.ch"]))
+        testFileC = File(lfn = "/this/is/a/lfnC", size = 1024, events = 20,
+                         locations = Set(["goodse.cern.ch"]))
+        testFileD = File(lfn = "/this/is/a/lfnD", size = 1024, events = 20,
+                         locations = Set(["goodse.cern.ch"]))
+        testFileE = File(lfn = "/this/is/a/lfnE", size = 1024, events = 20,
+                         locations = Set(["goodse.cern.ch"]))
+        testFileF = File(lfn = "/this/is/a/lfnF", size = 1024, events = 20,
+                         locations = Set(["goodse.cern.ch"]))
+        testFileA.create()
+        testFileB.create()
+        testFileC.create()
+        testFileD.create()
+        testFileE.create()
+        testFileF.create()        
+        
+        testFileset = Fileset(name = "TestFileset")
+        testFileset.create()
+        
+        testFileset.addFile(testFileA)
+        testFileset.addFile(testFileB)
+        testFileset.addFile(testFileC)
+        testFileset.addFile(testFileD)
+        testFileset.addFile(testFileE)
+        testFileset.addFile(testFileF)
+        testFileset.commit()
+
+        testSubscription = Subscription(fileset = testFileset,
+                                        workflow = testWorkflow)
+        testSubscription.create()
+
+        myThread = threading.currentThread()
+        myThread.transaction.begin()
+
+        testSubscription.acquireFiles([testFileA])
+        testSubscription.completeFiles([testFileB])
+        testSubscription.failFiles([testFileC])
+        availableFiles = testSubscription.availableFiles()
+
+        goldenFiles = [testFileD, testFileE, testFileF]
+        for availableFile in availableFiles:
+            assert availableFile in goldenFiles, \
+                   "ERROR: Unknown available file"
+            goldenFiles.remove(availableFile)
+
+        assert len(goldenFiles) == 0, \
+               "ERROR: Missing available files"
+
+        myThread.transaction.rollback()
+        availableFiles = testSubscription.availableFiles()        
+
+        goldenFiles = [testFileA, testFileB, testFileC, testFileD, testFileE,
+                       testFileF]
+        for availableFile in availableFiles:
+            assert availableFile in goldenFiles, \
+                   "ERROR: Unknown available file"
+            goldenFiles.remove(availableFile)
+
+        assert len(goldenFiles) == 0, \
+               "ERROR: Missing available files after rollback."
 
         testSubscription.delete()
         testWorkflow.delete()
@@ -531,7 +927,6 @@ class Subscription_t(unittest.TestCase):
                (testWorkflow.owner == testSubscriptionB["workflow"].owner), \
                "ERROR: Subscription load by ID didn't load workflow correctly"
 
-        # Need to complete...
         return
        
 if __name__ == "__main__":

885813d1aa2c42d395d91d5a1fd00ffdde92a2f1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:53:30 +0000

    update the completion time when job is completed
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3817 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/Complete.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/Complete.py
@@ -3,20 +3,23 @@
 _Complete_
 MySQL implementation of Jobs.Complete
 
-move file into wmbs_group_job_acquired
+move file into wmbs_group_job_complete
 """
 __all__ = []
-__revision__ = "$Id: Complete.py,v 1.5 2009/01/27 16:47:15 sfoulkes Exp $"
-__version__ = "$Revision: 1.5 $"
+__revision__ = "$Id: Complete.py,v 1.6 2009/02/10 19:32:16 sryu Exp $"
+__version__ = "$Revision: 1.6 $"
 
+import time
 from WMCore.Database.DBFormatter import DBFormatter
 
 class Complete(DBFormatter):
-    sql = """insert into wmbs_group_job_complete (job, jobgroup) VALUES
-             (:job, (select jobgroup from wmbs_job where id = :job))"""
+    sql = ["""insert into wmbs_group_job_complete (job, jobgroup) VALUES
+             (:job, (select jobgroup from wmbs_job where id = :job))""",
+           " update wmbs_job set completion_time = %d where id = :job " 
+           % time.time()]
     
     def execute(self, job=0, conn = None, transaction = False):
-        binds = self.getBinds(job = job)
+        binds = self.getBinds(job = job) * 2
         self.dbi.processData(self.sql, binds, conn = conn,
                              transaction = transaction)
         return

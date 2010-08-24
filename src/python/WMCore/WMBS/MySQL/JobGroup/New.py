49552b37fb19a9ba46d36318836d92e9092db13a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:26:46 +0000

    Make sure we store the output fileset's id in the job group table.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3399 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/JobGroup/New.py
+++ src/python/WMCore/WMBS/MySQL/JobGroup/New.py
@@ -4,24 +4,24 @@ _New_
 MySQL implementation of JobGroup.New
 """
 __all__ = []
-__revision__ = "$Id: New.py,v 1.3 2008/10/01 22:10:05 metson Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: New.py,v 1.4 2008/11/20 17:02:21 sfoulkes Exp $"
+__version__ = "$Revision: 1.4 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 from WMCore.Services.UUID import makeUUID
 
 class New(DBFormatter):
     sql = []
-    sql.append("insert into wmbs_jobgroup (subscription, uid) values (:subscription, :uid)")
+    sql.append("insert into wmbs_jobgroup (subscription, uid, output) values (:subscription, :uid, :output)")
     sql.append("""select id from wmbs_jobgroup where uid=:uid""")
+
     def format(self, result, uid):
         result = DBFormatter.format(self, result)
         result = result[0][0]
         return result, uid
         
-    def execute(self, subscription=None, conn = None, transaction = False):
-        uid=makeUUID()
-        binds = self.getBinds(subscription=subscription, uid=uid)
+    def execute(self, uid, subscription=None, output=None, conn = None, transaction = False):
+        binds = self.getBinds(subscription=subscription, uid=uid, output=output)
         self.logger.debug('JobGroup.New sql: %s' % self.sql)
         self.logger.debug('JobGroup.New binds: %s' % binds)
         result = self.dbi.processData(self.sql[0], binds)

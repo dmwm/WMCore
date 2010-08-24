7d756727232738cd1465394cb2da7b3649a9fc67
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:54:52 +0000

    Added extra spaces in exception messages.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2890 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMExceptions.py
+++ src/python/WMCore/WMExceptions.py
@@ -7,12 +7,14 @@ mappings to a human readable message.
 
 """
 
-__revision__ = "$Id: WMExceptions.py,v 1.3 2008/08/20 08:55:31 fvlingen Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: WMExceptions.py,v 1.4 2008/08/26 11:05:28 fvlingen Exp $"
+__version__ = "$Revision: 1.4 $"
 __author__ = "fvlingen@caltech.edu"
 
 
-WMEXCEPTION = {'WMCore-1' : 'Not allowed to instantiate', \
-   'WMCore-2' : 'Problem creating database table',\
-   'WMCORE-3' : 'Could not find in library class',\
-   'WMCORE-4' : 'Problem with loading the class'}
+WMEXCEPTION = {'WMCore-1' : 'Not allowed to instantiate ', \
+   'WMCore-2' : 'Problem creating database table ',\
+   'WMCORE-3' : 'Could not find in library class ',\
+   'WMCORE-4' : 'Problem with loading the class ',\
+   'WMCORE-5' : 'Dialect not specified in configuration ',\
+   'WMCORE-6' : 'Message name is same as diagnostic message name! ',}

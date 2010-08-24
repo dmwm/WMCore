0bfdc4446ff2069e840ccf59597040e07ab2fac5
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:32:17 +0000

    group is oracle a key word, change bind variable name group -> job group
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3483 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/JobGroup/Status.py
+++ src/python/WMCore/WMBS/MySQL/JobGroup/Status.py
@@ -4,23 +4,23 @@ _New_
 MySQL implementation of JobGroup.Status
 """
 __all__ = []
-__revision__ = "$Id: Status.py,v 1.4 2008/10/01 22:20:19 metson Exp $"
-__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: Status.py,v 1.5 2008/12/10 23:01:46 sryu Exp $"
+__version__ = "$Revision: 1.5 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class Status(DBFormatter):
     sql = """select (
-        select count(id) from wmbs_job where jobgroup=:group 
+        select count(id) from wmbs_job where jobgroup=:jobgroup 
             and id not in (select job from wmbs_group_job_acquired) 
             and id not in (select job from wmbs_group_job_failed)
             and id not in (select job from wmbs_group_job_complete)
         ) as av, (
-        select count(job) from wmbs_group_job_acquired where jobgroup=:group
+        select count(job) from wmbs_group_job_acquired where jobgroup=:jobgroup
         ) as ac, (
-        select count(job) from wmbs_group_job_failed where jobgroup=:group
+        select count(job) from wmbs_group_job_failed where jobgroup=:jobgroup
         ) as fa, (
-        select count(job) from wmbs_group_job_complete where jobgroup=:group
+        select count(job) from wmbs_group_job_complete where jobgroup=:jobgroup
         ) as cm
     """
 
@@ -29,7 +29,7 @@ class Status(DBFormatter):
         return result[0]
         
     def execute(self, group=None, conn = None, transaction = False):
-        binds = self.getBinds(group=group)
+        binds = self.getBinds(jobgroup=group)
         self.logger.debug('JobGroup.Status sql: %s' % self.sql)
         self.logger.debug('JobGroup.Status binds: %s' % binds)
         result = self.dbi.processData(self.sql, binds)

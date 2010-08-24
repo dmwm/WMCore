401d8fba5c56c661b9219ae269e993b964b8b8e6
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:38:02 +0000

    Add SQLite implementation Set name correctly
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2610 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Actions/ListFileset.py
+++ src/python/WMCore/WMBS/Actions/ListFileset.py
@@ -1,16 +1,19 @@
 from WMCore.WMBS.Actions.Action import BaseAction
 class ListFilesetAction(BaseAction):
-    name = "NewFileset"
+    name = "ListFileset"
         
     def execute(self, dbinterface = None):
         """
         import the approriate SQL object and execute it
         """ 
-        #dia = dbinterface.engine.dialect
+        dia = dbinterface.engine.dialect
         #if isinstance(dia, OracleDialect):
         #    from WMCore.WMBS.Oracle import OracleDialect as WMBSOracle
         #    return WMBSOracle (self.logger, engine)
         #el
-        from WMCore.WMBS.MySQL.ListFilesetSQL import ListFileset
+        if isinstance(dia, self.dialects['sqlite']):
+            from WMCore.WMBS.SQLite.ListFilesetSQL import ListFileset
+        elif isinstance(dia, self.dialects['mysql']):
+            from WMCore.WMBS.MySQL.ListFilesetSQL import ListFileset
         action = ListFileset(self.logger, dbinterface)
         return action.execute()

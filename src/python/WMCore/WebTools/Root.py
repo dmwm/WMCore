2bd13183c1d107c9dbf05d0828d91a96e8d8f553
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:37:47 +0000

    Allow database per class, simplify configuration of pages. Allow logging to be set from the global configuration. Inherit from the WMObject baseclass (to use flatten and makelist).
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3570 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/Root.py
+++ src/python/WMCore/WebTools/Root.py
@@ -9,8 +9,8 @@ loaded dynamically and can be turned on/off via configuration file.
 
 """
 
-__revision__ = "$Id: Root.py,v 1.3 2009/01/10 14:08:41 metson Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: Root.py,v 1.4 2009/01/10 16:00:39 metson Exp $"
+__version__ = "$Revision: 1.4 $"
 
 # CherryPy
 from cherrypy import quickstart, expose, server, log
@@ -27,41 +27,48 @@ from WMCore.DAOFactory import DAOFactory
 # Logging
 import WMCore.WMLogging
 import logging 
+from WMCore.DataStructs.WMObject import WMObject
 
-class Root(object):
+class Root(WMObject):
     def __init__(self, opts):
-        self.config = self.loadConfig(opts.inifile)
-        self.app = self.config.get('root', 'application')
+        self.opts = opts
+        self.configfile = self.loadConfig(opts.inifile)
+        self.app = self.configfile.get('root', 'application')
         
-        #Configure the database if needed
-        try:
-            dburl = self.config.get('database', 'dburl')
-            conn = DBFactory(log.error_log, dburl).connect()
-            self.daofactory = DAOFactory(package=self.app, logger=log.error_log, dbinterface=conn)
-            opts.dao = self.daofactory
-        except:
-            log("No database defined for %s" % self.app, context=self.app, 
-                severity=logging.WARNING, traceback=False)
-            
         #Configure CherryPy
         try:
-            cpconfig.update ({"server.environment": self.config.get('root', 'environment')})
+            cpconfig.update ({"server.environment": self.configfile.get('root', 'environment')})
         except:
             cpconfig.update ({"server.environment": 'production'})
         try:
-            cpconfig.update ({"server.socket_port": int(self.config.get('root', 'port'))})
+            cpconfig.update ({"server.socket_port": int(self.configfile.get('root', 'port'))})
         except:
             cpconfig.update ({"server.socket_port": 8080})
         try:
-            cpconfig.update ({'tools.expires.secs': int(self.config.get('root', 'expires'))})
+            cpconfig.update ({'tools.expires.secs': int(self.configfile.get('root', 'expires'))})
         except:
             cpconfig.update ({'tools.expires.secs': 300})
-        cpconfig.update ({'tools.expires.on': True,
+        try:
+            cpconfig.update ({'log.screen': bool(self.configfile.get('root', 'log_screen'))})
+        except:
+            cpconfig.update ({'log.screen': True})
+        try:
+            cpconfig.update ({'log.access_file': self.configfile.get('root', 'access_log_file')})
+        except:
+            cpconfig.update ({'log.access_file': None})
+        try:
+            cpconfig.update ({'log.error_file': int(self.configfile.get('root', 'error_log_file'))})
+        except:
+            cpconfig.update ({'log.error_file': None})
+                                  
+        cpconfig.update ({
+                          'tools.expires.on': True,
                           'tools.response_headers.on':True,
                           'tools.etags.on':True,
                           'tools.etags.autotags':True,
                           'tools.encode.on': True, 
-                          'tools.gzip.on': True})
+                          'tools.gzip.on': True
+                          })
         #cpconfig.update ({'request.show_tracebacks': False})
         #cpconfig.update ({'request.error_response': self.handle_error})
         #cpconfig.update ({'tools.proxy.on': True})
@@ -69,43 +76,65 @@ class Root(object):
         
         log("loading config: %s" % cpconfig, context=self.app, severity=logging.DEBUG, traceback=False)
         
-        self.loadPages(opts, self.config)
+        self.loadPages()
         
-    def loadPages(self, opts, config):
+    def loadPages(self):
         factory = WMFactory('webtools_factory')
-        views = eval(config.get('views', 'active'))
+        views = eval(self.configfile.get('views', 'active'))
+        # Read in global configuration, to be over-ridden if needs be
+        globalconfig = {}
+        globalconfig['application'] = self.configfile.get('root', 'application')
+        try:
+            globalconfig['templates'] = self.configfile.get('root', 'templates')
+        except:
+            pass
+        try:
+            globalconfig['database'] = self.configfile.get('root', 'database')
+        except:
+            pass
         
         for i in views:
-            log("loading %s" % i, context=self.app, severity=logging.INFO, traceback=False)
-            theclass = config.get(i, 'class')
+            log("loading %s" % i, context=self.app, 
+                severity=logging.INFO, traceback=False)
             
-            args = [self.configToDict(config, i)]
-            args[0]['root'] = self.configToDict(config, 'root')
-            tmp = eval(config.get(i, 'init'))
-            if type(tmp) == type([]):
-                args.extend(tmp)
-            elif tmp:
-                args.append(tmp)
+            config = {} 
+            config.update(globalconfig)
+            config.update(self.configToDict(self.configfile, i))
             
-            database = eval(config.get(i, 'database'))
-            if database:
-                args.append(self.daofactory)
-            if len(args):
-                obj = factory.loadObject(theclass, args)
-                eval(compile("self.%s = obj" % i, '<string>', 'single'))
-            else:
-                obj = factory.loadObject(theclass)
-                eval(compile("self.%s = obj" % i, '<string>', 'single'))
+            log("configuration for %s: %s" % (i, config), context=self.app, 
+                severity=logging.INFO, traceback=False)
             
+            if 'database' in config.keys():
+                config['database'] = self.loadDatabase(config)
+            
+            theclass = self.configfile.get(i, 'class')
+                    
+            obj = factory.loadObject(theclass, config)
+            eval(compile("self.%s = obj" % i, '<string>', 'single'))
+        
             log("%s available on %s/%s" % (i, server.base(), i), 
                                         context=self.app, 
                                         severity=logging.DEBUG, 
                                         traceback=False)
         
-        maint = eval(config.get('views', 'maintenance'))
+        maint = eval(self.configfile.get('views', 'maintenance'))
         for i in maint:
             #TODO: Show a maintenance page
             pass
+    
+    def loadDatabase(self, config):
+        dblist = []
+        if 'database' in config.keys():
+            #Configure the database if needed, replace with thread style?
+            for dburl in self.makelist(config['database']):
+                try:
+                    conn = DBFactory(log.error_log, dburl).connect()
+                    daofactory = DAOFactory(package=self.app, logger=log.error_log, dbinterface=conn)
+                except:
+                    log("Cannot connect to %s" % config['database'], context=self.app, 
+                        severity=logging.WARNING, traceback=False)
+    
+        return self.flatten(dblist)
     
     def configToDict(self, config, section):
         dict = {}
@@ -125,12 +154,12 @@ class Root(object):
     
     @expose
     def index(self):
-        index = self.config.get('root', 'index')
+        index = self.configfile.get('root', 'index')
         return eval('self.%s.index()' % index)
     
     @expose
     def default(self, *args, **kwargs):
-        index = self.config.get('root', 'index')
+        index = self.configfile.get('root', 'index')
         return eval('self.%s.default(*args, **kwargs)' % index)
 
 if __name__ == "__main__":

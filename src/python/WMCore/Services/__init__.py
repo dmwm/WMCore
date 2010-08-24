1cc0533835a0ceeb42c47dadc656f9fa4463550e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:51:55 +0000

    Correct docstring
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2837 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Services/__init__.py
+++ src/python/WMCore/Services/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 """
-_WMCore_
+_WMCore/Services_
 
-Core libraries for Workload Management Packages
+Services for Workload Management Packages
 
 """
 __all__ = []
-__revision__ = "$Id: __init__.py,v 1.1 2008/08/12 21:47:19 ewv Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: __init__.py,v 1.2 2008/08/12 21:50:41 ewv Exp $"
+__version__ = "$Revision: 1.2 $"
 

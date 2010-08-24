cf1deee8a462f2578cb3820680e40eb5dda861b6
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:51:20 +0000

    COrrect docs, move to DBFormatter base class
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2828 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Subscriptions/GetFailedFiles.py
+++ src/python/WMCore/WMBS/MySQL/Subscriptions/GetFailedFiles.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 """
-_AcquireFiles_
+_GetFailedFiles_
 
 MySQL implementation of Subscription.GetFailedFiles
 
 Return a list of files that are available for processing
 """
 __all__ = []
-__revision__ = "$Id: GetFailedFiles.py,v 1.2 2008/06/30 18:00:19 metson Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: GetFailedFiles.py,v 1.3 2008/08/09 22:18:33 metson Exp $"
+__version__ = "$Revision: 1.3 $"
 
-from WMCore.WMBS.MySQL.Base import MySQLBase
+from WMCore.Database.DBFormatter import DBFormatter
 
-class GetFailedFiles(MySQLBase):
+class GetFailedFiles(DBFormatter):
 #    sql = """select lfn from wmbs_file_details
 #                where id in (
 #                     select file from wmbs_sub_files_failed where subscription=:subscription

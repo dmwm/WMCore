6e98461c318779191ea3891ab946eda4d848db79
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:44:52 +0000

    change insert work with oracle as well
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3680 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/Active.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/Active.py
@@ -6,14 +6,14 @@ MySQL implementation of Jobs.Active
 move file into wmbs_group_job_acquired
 """
 __all__ = []
-__revision__ = "$Id: Active.py,v 1.4 2009/01/12 19:26:03 sfoulkes Exp $"
-__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: Active.py,v 1.5 2009/01/21 22:00:27 sryu Exp $"
+__version__ = "$Revision: 1.5 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class Active(DBFormatter):
     sql = """insert into wmbs_group_job_acquired (job, jobgroup) 
-             values (select id, jobgroup from wmbs_job where id = :job)"""
+             (select id, jobgroup from wmbs_job where id = :job)"""
     
     def format(self, result):
         return True
--- src/python/WMCore/WMBS/MySQL/Jobs/Failed.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/Failed.py
@@ -6,14 +6,17 @@ MySQL implementation of Jobs.Failed
 move file into wmbs_group_job_acquired
 """
 __all__ = []
-__revision__ = "$Id: Failed.py,v 1.3 2009/01/12 19:26:03 sfoulkes Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: Failed.py,v 1.4 2009/01/21 22:00:27 sryu Exp $"
+__version__ = "$Revision: 1.4 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class Failed(DBFormatter):
     sql = """insert into wmbs_group_job_failed (job, jobgroup) 
-             values (select id, jobgroup from wmbs_job where id = :job)"""
+             (select id, jobgroup from wmbs_job where id = :job)"""
+    
+    def format(self, result):
+        return True
         
     def execute(self, job=0, conn = None, transaction = False):
         binds = self.getBinds(job=job)

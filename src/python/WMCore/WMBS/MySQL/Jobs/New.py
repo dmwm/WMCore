14aed32f692c778a94496bb3399c853e4dd93c73
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:27:01 +0000

    Add a job mask when creating a job.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3403 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/New.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/New.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 """
 _New_
+
 MySQL implementation of Jobs.New
 """
+
 __all__ = []
-__revision__ = "$Id: New.py,v 1.4 2008/10/01 21:43:16 metson Exp $"
-__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: New.py,v 1.5 2008/11/20 17:16:53 sfoulkes Exp $"
+__version__ = "$Revision: 1.5 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class New(DBFormatter):
     sql = ["insert into wmbs_job (jobgroup, name) values (:jobgroup, :name)",
-    "select id from wmbs_job where jobgroup = :jobgroup and name = :name"]
+           """insert into wmbs_job_mask (job) select id from wmbs_job
+              where jobgroup = :jobgroup and name = :name""",
+           "select id from wmbs_job where jobgroup = :jobgroup and name = :name"]
     
     def format(self, result):
         result = DBFormatter.format(self, result)
         return result[0][0]
         
-    def execute(self, jobgroup=0, name=None, conn = None, transaction = False):
-        binds = self.getBinds(jobgroup=jobgroup, name=name) * 2
+    def execute(self, jobgroup, name):
+        binds = self.getBinds(jobgroup=jobgroup, name=name) * 3
         self.logger.debug('Job.Add sql: %s' % self.sql)
         self.logger.debug('Job.Add binds: %s' % binds)
         

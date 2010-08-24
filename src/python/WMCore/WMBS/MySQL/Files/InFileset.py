45d43c0dc5c3478442d972d19e15a5b8d58f86d7
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:49:09 +0000

    Updated SQL to reflect the chnage in how run/lumi is stored
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2793 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Files/InFileset.py
+++ src/python/WMCore/WMBS/MySQL/Files/InFileset.py
@@ -4,9 +4,10 @@ MySQL implementation of Files.InFileset
 from WMCore.WMBS.MySQL.Base import MySQLBase
 
 class InFileset(MySQLBase):
-    sql = """select id, lfn, size, events, run, lumi from wmbs_file_details 
-                where id in (select file from wmbs_fileset_files where 
-                fileset = (select id from wmbs_fileset where name = :fileset))"""
+    sql = """select file.id, file.lfn, file.size, file.events, map.run, map.lumi
+            from wmbs_file_details as file join wmbs_file_runlumi_map as map on map.file = file.id 
+            where id in (select file from wmbs_fileset_files where 
+            fileset = (select id from wmbs_fileset where name = :fileset))"""
                 
     def getBinds(self, fileset = None):
         return self.dbi.buildbinds(self.dbi.makelist(fileset), 'fileset')

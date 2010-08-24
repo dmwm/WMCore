715c71445c7a4aa4a1a21e18359c00d6e7d8c50b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:05:26 +0000

    add a default name, don't set the name twice...
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3064 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/DataStructs/Workflow.py
+++ src/python/WMCore/DataStructs/Workflow.py
@@ -6,14 +6,13 @@ A class that describes some work to be undertaken on some files
 
 """
 __all__ = []
-__revision__ = "$Id: Workflow.py,v 1.1 2008/07/03 17:00:01 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Workflow.py,v 1.2 2008/09/29 16:07:04 metson Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.DataStructs.Pickleable import Pickleable
 class Workflow(Pickleable):
-    def __init__(self, spec=None, owner=None, name=None):        
+    def __init__(self, spec=None, owner=None, name='workflow'):        
         #TODO: define a url-like scheme for spec's and enforce it here
         self.spec = spec
         self.name = name
         self.owner = owner
-        self.name = name

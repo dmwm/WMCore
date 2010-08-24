f1c1ad7af2a978f822d21ea012ea6edcd6c87a09
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:23:05 +0000

    Now accepting DB user and password from .ini file
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3340 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WMBS_t/Performance_t/MySQLDAO_t.py
+++ test/python/WMCore_t/WMBS_t/Performance_t/MySQLDAO_t.py
@@ -18,19 +18,31 @@ class MySQLDAOTest():
         #Set specific user for mysqladmin here        
         self.logname = 'MySQL'
         self.dbuser = cfg.get('mysql', 'user')
+        self.dbpass = cfg.get('mysql', 'pass')
         self.dbhost = cfg.get('mysql', 'host')
         self.dbinst = cfg.get('mysql', 'instance')
         self.verbose = cfg.get('output','verbose')
-        self.sqlURI = 'mysql://%s@%s/%s' % (self.dbuser, self.dbhost, self.dbinst)
+        self.sqlURI = 'mysql://%s:%s@%s/%s' % (self.dbuser, self.dbpass, self.dbhost, self.dbinst)
 
     def tearDown(self):
         #Call superclass tearDown method
         #DB Specific tearDown code        
-        self.logger.debug(
-              commands.getstatusoutput('echo yes | mysqladmin -u %s drop %s' %\
+        if self.dbpass != '':        
+            self.logger.debug(
+              commands.getstatusoutput('echo yes | mysqladmin --user=%s \
+                                        --password=%s drop %s' %\
+                                       (self.dbuser, self.dbpass, self.dbinst)))
+            self.logger.debug(
+              commands.getstatusoutput('mysqladmin --user=%s --password=%s \
+                                        create %s' %\
+                                       (self.dbuser, self.dbpass, self.dbinst)))
+        else:
+            self.logger.debug(
+              commands.getstatusoutput('echo yes | mysqladmin --u %s drop %s' %\
                                        (self.dbuser, self.dbinst)))
-        self.logger.debug(
+            self.logger.debug(
               commands.getstatusoutput('mysqladmin -u %s create %s' %\
                                        (self.dbuser, self.dbinst)))
+
         self.logger.debug("WMBS MySQL database deleted")
 

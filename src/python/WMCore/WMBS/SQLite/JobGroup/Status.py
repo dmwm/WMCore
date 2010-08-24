36307769987a50c451ad0cc504a32e6799b4abc7
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:28:01 +0000

    Inherit from MySQL.JobGroup.Status instead of defining everything again.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3418 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/JobGroup/Status.py
+++ src/python/WMCore/WMBS/SQLite/JobGroup/Status.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 """
 _Status_
-MySQL implementation of JobGroup.Status
 
+SQLite implementation of JobGroup.Status
 """
 
-from WMCore.WMBS.MySQL.JobGroup.Status import Status as StatusJobGroupMySQL
+__all__ = []
+__revision__ = "$Id: Status.py,v 1.3 2008/11/21 17:11:25 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 
-class Status(StatusJobGroupMySQL):
-    sql = StatusJobGroupMySQL.sql
-        
-    def execute(self, group=None, conn = None, transaction = False):
-        StatusJobGroupMySQL.execute(self, group=group, conn=conn, transaction=transaction)
+from WMCore.WMBS.MySQL.JobGroup.Status import Status as StatusMySQL
+
+class Status(StatusMySQL):
+    sql = StatusMySQL.sql

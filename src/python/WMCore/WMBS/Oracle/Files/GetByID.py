ec446fdc7db65062b2d9afc22ee64645d4e90d23
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:35:28 +0000

    Fix this query so that it doesn't try to pull up run and lumi information.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3532 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Files/GetByID.py
+++ src/python/WMCore/WMBS/Oracle/Files/GetByID.py
@@ -11,8 +11,5 @@ class GetByID(GetByIDMySQL):
     """
     sql = """SELECT wmbs_file_details.id, wmbs_file_details.lfn,
              wmbs_file_details.filesize, wmbs_file_details.events,
-             wmbs_file_runlumi_map.run, wmbs_file_runlumi_map.lumi,
              wmbs_file_details.cksum FROM wmbs_file_details
-             INNER JOIN wmbs_file_runlumi_map ON
-             wmbs_file_details.id = wmbs_file_runlumi_map.fileid
              WHERE wmbs_file_details.id = :fileid"""

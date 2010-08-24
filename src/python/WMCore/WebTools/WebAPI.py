9ce755d2485225a030dca31b97f026d2058e4471
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:54:18 +0000

    A base class for a web api/das server. Inherit from this class, override the __init__ to append the methods you want to run and configure as:
     active.section_('myapp_das')
    active.myapp_das.object = CLASSNAME
    active.myapp_das.database = DATABASEURL
    active.myapp_das.templates = environ['WTBASE'] + '/templates/WMCore/WebTools/'
    
    for baseline functionality. Change the template if you so desire.
    
    This will expose all methds listed in the dictionary as json, xml and dasjson. Your methods *must* return a dictionary or list.
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3830 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WebTools/WebAPI.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+from WMCore.WebTools.Page import DatabasePage, exposexml, exposejson 
+from WMCore.Lexicon import sitetier, countrycode
+from cherrypy import expose
+import sys
+
+class WebAPI(DatabasePage):
+    def __init__(self, config = {}, database = ''):
+        DatabasePage.__init__(self, config, database) 
+        self.methods = {}
+    
+    @expose
+    def index(self):
+        return self.templatepage('API', methods = self.methods)
+    
+    @expose
+    def default(self, *args, **kwargs):
+        return self.index()
+    
+    @exposejson
+    def json(self, *args, **kwargs):
+        dict = self.runMethod(args[0], kwargs)
+        return dict
+    
+    @exposexml
+    def xml(self, *args, **kwargs):
+        dict = self.runMethod(args[0], kwargs)
+        return dict
+    
+    def runMethod(self, method, input):
+        """
+        Run the specified method with the provided input, return a dict 
+        containing the result of the call or an exception. 
+        """
+        kwargs=''
+        for i in input:
+            kwargs = kwargs + "%s='%s'," % (i, input[i])
+        str = "self.methods[method]['call']"
+        dict = {}
+        try:
+            method = eval(str)
+            dict = method(kwargs.strip(','))
+        except Exception, e:
+            error = e.__str__()
+            self.debug(error)
+            self.debug(str)
+            self.debug("%s:%s" % (sys.exc_type, sys.exc_value))
+            dict = {'Exception':{'Exception_thrown_in': method.__str__(),
+                       'Exception_type': '%s' % sys.exc_type,
+                       'Exception_detail':error, 
+                       'Exception_string': str, 
+                       'Exception_arguments': kwargs,
+                       'Exception_dict':dict}}
+        return dict

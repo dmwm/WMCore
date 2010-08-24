e15889e970b7253436c91ed654b6ae09d8d7454f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:32:21 +0000

    fix typo
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3484 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/JobGroup/Status.py
+++ src/python/WMCore/WMBS/Oracle/JobGroup/Status.py
@@ -4,10 +4,10 @@ _New_
 Oracle implementation of JobGroup.Status
 """
 __all__ = []
-__revision__ = "$Id: Status.py,v 1.2 2008/12/05 21:06:25 sryu Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: Status.py,v 1.3 2008/12/10 23:02:05 sryu Exp $"
+__version__ = "$Revision: 1.3 $"
 
-from WMCore.WMBS.MySQL.JobGroup.Status import New as StatusJobGroupMySQL
+from WMCore.WMBS.MySQL.JobGroup.Status import Status as StatusJobGroupMySQL
 
 class Status(StatusJobGroupMySQL):
     

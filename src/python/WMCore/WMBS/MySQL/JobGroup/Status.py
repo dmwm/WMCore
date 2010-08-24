03891f985a38201f3763ee43581469de776a2564
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:07:28 +0000

    Determine the status of the JobGroup from the DB, move to DBFormatter
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3097 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/JobGroup/Status.py
+++ src/python/WMCore/WMBS/MySQL/JobGroup/Status.py
@@ -4,14 +4,17 @@ _New_
 MySQL implementation of JobGroup.Status
 """
 __all__ = []
-__revision__ = "$Id: Status.py,v 1.1 2008/10/01 15:43:41 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Status.py,v 1.2 2008/10/01 21:57:08 metson Exp $"
+__version__ = "$Revision: 1.2 $"
 
-from WMCore.WMBS.MySQL.Base import MySQLBase
+from WMCore.Database.DBFormatter import DBFormatter
 
-class Status(MySQLBase):
+class Status(DBFormatter):
     sql = """select (
-        select count(id) from wmbs_job where jobgroup=:group
+        select count(id) from wmbs_job where jobgroup=:group and id not in 
+            ((select job from wmbs_group_job_acquired), 
+            (select job from wmbs_group_job_failed), 
+            (select job from wmbs_group_job_complete))
         ) as av, (
         select count(job) from wmbs_group_job_acquired where jobgroup=:group
         ) as ac, (

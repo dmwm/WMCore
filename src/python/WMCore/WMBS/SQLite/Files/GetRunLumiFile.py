e54d6cc150851e977c571ffbd5821bcb49703e6b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:35:14 +0000

    SQLLite implementation of GetRunLumiFile
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3528 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/SQLite/Files/GetRunLumiFile.py
@@ -0,0 +1,10 @@
+"""
+SQLLite implementation of GetRunLumiFile
+"""
+
+from WMCore.WMBS.MySQL.Files.GetRunLumiFile import GetRunLumiFile as GetRunLumiFileMySQL
+
+class GetRunLumiFile(GetRunLumiFileMySQL):
+    sql = GetRunLumiFileMySQL.sql
+
+

8589f295ca00fa93f8023797e84ddf0ad06d192f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:57:44 +0000

    Remove most of the code as this is exactly the same as the MySQL version.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3885 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Fileset/New.py
+++ src/python/WMCore/WMBS/SQLite/Fileset/New.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python
 """
-_Parentage_
+_New_
 
 SQLite implementation of Fileset.New
-
 """
+
 __all__ = []
-__revision__ = "$Id: New.py,v 1.2 2008/11/20 21:54:26 sryu Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: New.py,v 1.3 2009/03/03 14:49:10 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.WMBS.MySQL.Fileset.New import New as NewFilesetMySQL
 
 class New(NewFilesetMySQL):
-    sql = """insert into wmbs_fileset 
-            (name, last_update) values (:fileset, :timestamp)"""
-            
-    def getBinds(self, fileset = None):
-        binds = self.dbi.buildbinds(self.dbi.makelist(fileset), 'fileset',
-            self.dbi.buildbinds(
-                self.dbi.makelist(self.timestamp()), 'timestamp'))
-        return binds
+    sql = NewFilesetMySQL.sql

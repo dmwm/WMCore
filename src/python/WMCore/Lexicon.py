d98d2af9189318e165061ec3cba1227390142bcf
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:50:16 +0000

    Clean up and add some documentation, revision and version tags
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3768 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Lexicon.py
+++ src/python/WMCore/Lexicon.py
@@ -1,19 +1,28 @@
+#!/usr/bin/python
 """
-A set of regular expressions that we can use to validate input to other classes. 
+_Lexicon_
+
+A set of regular expressions  and other tests that we can use to validate input 
+to other classes. If a test fails an AssertionError should be raised, and 
+handled appropriately by the client methods, on success returns True. 
 """
+
+__revision__ = "$Id: Lexicon.py,v 1.2 2009/02/03 17:49:44 metson Exp $"
+__version__ = "$Revision: 1.2 $"
+
 import re
 
 def sitetier(candidate):
-    return check("^T[0-4]$", candidate)
+    return check("^T[0-3]", candidate)
     
 def cmsname(candidate):
-    return check("^T[0-4]_[A-Z]{2}_[A-Za-z]+", candidate)
+    return check("^T[0-3]_[A-Z]{2}_[A-Za-z]+", candidate)
 
 def countrycode(candidate):
     #TODO: do properly
     return check("^[A-Z]{2}$", candidate)
 
 def check(regexp, candidate):
-    assert re.compile(regexp).match(candidate) == True, \
-                  "'%s' does not match regular expression %s" % (candidate, regexp)
+    assert re.compile(regexp).match(candidate) != None , \
+              "'%s' does not match regular expression %s" % (candidate, regexp)
     return True

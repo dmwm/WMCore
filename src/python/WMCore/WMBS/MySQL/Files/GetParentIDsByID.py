45d15832c09c38f09f9a8cf9cde5cdf5f92e5102
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:58:10 +0000

    add code to handle mysql returning long. From DBFormmater
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3892 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Files/GetParentIDsByID.py
+++ src/python/WMCore/WMBS/MySQL/Files/GetParentIDsByID.py
@@ -15,16 +15,22 @@ class GetParentIDsByID(DBFormatter):
         for id in childIDs:
             binds.append({'child': id})
         return binds
-    
+        
     def format(self, result):        
         out = Set() 
         for r in result:
-            for f in r.fetchall():
-                out.add(f[0])
+            if type(1L) == type(r):
+                # deal with crappy mysql implementation
+                out.add(int(r))
+            else:
+                for f in r.fetchall():
+                    out.add(int(f[0]))
+        #print list(out)
         return list(out) 
-        
+  
     def execute(self, ids=None, conn = None, transaction = False):
         binds = self.getBinds(ids)
+        #print "Bind %s" % binds
         result = self.dbi.processData(self.sql, binds, 
                          conn = conn, transaction = transaction)
         return self.format(result)

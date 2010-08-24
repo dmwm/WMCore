5d4cd043a6190e3945c228b11256fb6b8c55aa6e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:59:40 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3916 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/JobSplitting/WMBSMergeBySize.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+"""
+_WMBSMergeBySize_
+
+Generic merging for WMBS.  This will correctly handle merging files that have
+been split up honoring the original file boundaries.
+"""
+
+__revision__ = "$Id: WMBSMergeBySize.py,v 1.1 2009/03/09 19:00:51 sfoulkes Exp $"
+__version__ = "$Revision: 1.1 $"
+
+import threading
+
+from WMCore.WMBS.File import File
+
+from WMCore.DAOFactory import DAOFactory
+from WMCore.JobSplitting.JobFactory import JobFactory
+from WMCore.Services.UUID import makeUUID
+
+def mergeUnitCompare(a, b):
+    """
+    _mergeUnitCompare_
+
+    Compare two merge units.  They will be sorted first by run ID and then by
+    lumi ID.
+    """
+    if a["run"] > b["run"]:
+        return 1
+    elif a["run"] == b["run"]:
+        if a["lumi"] > b["lumi"]:
+            return 1
+        elif a["lumi"] == b["lumi"]:
+            return 0
+        else:
+            return -1
+    else:
+        return -1
+
+def fileCompare(a, b):
+    """
+    _fileCompare_
+
+    Compare two files based on their "file_first_event" attribute.
+    """
+    if a["file_first_event"] > b["file_first_event"]:
+        return 1
+    if a["file_first_event"] == b["file_first_event"]:
+        return 0
+    else:
+        return -1
+
+def sortedFilesFromMergeUnits(mergeUnits):
+    """
+    _sortedFilesFromMergeUnits_
+
+    Given a list of merge units sort them and the files that they contain.
+    Return a list of sorted WMBS File structures.
+    """
+    mergeUnits.sort(mergeUnitCompare)
+
+    sortedFiles = []
+    for mergeUnit in mergeUnits:
+        mergeUnit["files"].sort(fileCompare)
+
+        for file in mergeUnit["files"]:
+            newFile = File(id = file["file_id"], lfn = file["file_lfn"])
+            sortedFiles.append(newFile)
+
+    return sortedFiles
+
+class WMBSMergeBySize(JobFactory):
+    """
+    _WMBSMergeBySize_
+
+    Generic merging for WMBS.  This will correctly handle merging files that
+    have been split up honoring the original file boundaries merging the files
+    in the correct order.
+    """
+    def defineMergeUnits(self, mergeableFiles):
+        """
+        _defineMergeUnits_
+
+        Split all the mergeable files into merge units.  A merge unit is a group
+        of files that must be merged together.  For example, the files that
+        result from event based splitting jobs need to be merged back together.
+        This method will return a list of merge units.  A merge unit is a
+        dictionary with the following keys: group_id, total_events, total_size,
+        run, lumi, and files.  The files in the merge group are stored in a list
+        under the files key.
+        """
+        mergeUnits = []
+
+        for mergeableFile in mergeableFiles:
+            newMergeFile = {}
+            for key in mergeableFile.keys():
+                newMergeFile[key] = mergeableFile[key]
+
+            if mergeableFile["group_id"] != None:
+                for mergeUnit in mergeUnits:
+                    if mergeUnit["group_id"] == mergeableFile["group_id"]:
+                        mergeUnit["files"].append(newMergeFile)
+                        mergeUnit["total_size"] += newMergeFile["file_size"]
+                        mergeUnit["total_events"] += newMergeFile["file_events"]
+
+                        if mergeableFile["file_run"] < mergeUnit["run"] or \
+                           (mergeableFile["file_run"] == mergeUnit["run"] and \
+                            mergeableFile["file_lumi"] < mergeUnit["lumi"]):
+                            newMergeUnit["run"] = newMergeFile["file_run"]
+                            newMergeUnit["lumi"] = newMergeFile["file_lumi"]
+                            
+                        break
+                else:
+                    newMergeUnit = {}
+                    newMergeUnit["group_id"] = newMergeFile["group_id"]
+                    newMergeUnit["total_events"] = newMergeFile["file_events"]
+                    newMergeUnit["total_size"] = newMergeFile["file_size"]
+                    newMergeUnit["run"] = newMergeFile["file_run"]
+                    newMergeUnit["lumi"] = newMergeFile["file_lumi"]
+                    newMergeUnit["files"] = []
+                    newMergeUnit["files"].append(newMergeFile)
+                    mergeUnits.append(newMergeUnit)
+            else:
+                newMergeUnit = {}
+                newMergeUnit["group_id"] = -1
+                newMergeUnit["total_events"] = newMergeFile["file_events"]
+                newMergeUnit["total_size"] = newMergeFile["file_size"]
+                newMergeUnit["run"] = newMergeFile["file_run"]
+                newMergeUnit["lumi"] = newMergeFile["file_lumi"]                
+                newMergeUnit["files"] = []
+                newMergeUnit["files"].append(newMergeFile)
+                mergeUnits.append(newMergeUnit)
+
+        return mergeUnits
+
+    def createMergeJob(self, mergeUnits):
+        """
+        _createMergeJob_
+
+        Create a merge job for the given merge units.  All the files contained
+        in the merge units will be associated to the job.
+        """
+        newJob = self.jobInstance(name = makeUUID())
+        sortedFiles = sortedFilesFromMergeUnits(mergeUnits)
+
+        for file in sortedFiles:
+            file.load()
+            newJob.addFile(file)
+
+        self.jobs.append(newJob)
+        return
+    
+    def defineMergeJobs(self, mergeUnits):
+        """
+        _defineMergeJobs_
+
+        Go through the list of merge units and try to combine them together into
+        merge jobs that fit within the min/max filesizes and under the maximum
+        number of events.  
+        """
+        mergeJobFileSize = 0
+        mergeJobEvents = 0
+        mergeJobFiles = []
+
+        for mergeUnit in mergeUnits:
+            if mergeUnit["total_size"] > self.maxMergeSize or \
+                   mergeUnit["total_events"] > self.maxMergeEvents:
+                self.createMergeJob([mergeUnit])
+                continue
+            elif mergeUnit["total_size"] + mergeJobFileSize > self.maxMergeSize or \
+                     mergeUnit["total_events"] + mergeJobEvents > self.maxMergeEvents:
+                if mergeJobFileSize > self.minMergeSize or \
+                       self.forceMerge == True:
+                    self.createMergeJob(mergeJobFiles)
+                    mergeJobFileSize = 0
+                    mergeJobEvents = 0
+                    mergeJobFiles = []
+                else:
+                    continue
+                    
+            mergeJobFiles.append(mergeUnit)
+            mergeJobFileSize += mergeUnit["total_size"]
+            mergeJobEvents += mergeUnit["total_events"]
+                        
+        if mergeJobFileSize > self.minMergeSize or self.forceMerge == True:
+            if len(mergeJobFiles) > 0:
+                self.createMergeJob(mergeJobFiles)
+
+        return
+    
+    def algorithm(self, groupInstance = None, jobInstance = None,
+                  *args, **kwargs):
+        """
+        _algorithm_
+
+        Try to merge any available files for the subscription provided.  This
+        accepts the following keyword arguments:
+          max_merge_size - The maximum size of merged files
+          min_merge_size - The minimum size of merged files
+          max_merge_events - The maximum number of events in a merge file
+        """
+        self.maxMergeSize = kwargs.get("max_merge_size", 1000000000)
+        self.minMergeSize = kwargs.get("min_merge_size", 1048576)
+        self.maxMergeEvents = kwargs.get("max_merge_events", 50000)
+        self.jobs = []
+
+        self.jobInstance = jobInstance
+        self.groupInstance = groupInstance
+
+        self.subscription["fileset"].load()
+
+        if self.subscription["fileset"].open == True:
+            self.forceMerge = False
+        else:
+            self.forceMerge = True
+
+        myThread = threading.currentThread()
+        daoFactory = DAOFactory(package = "WMCore.WMBS",
+                                logger = myThread.logger,
+                                dbinterface = myThread.dbi)
+        
+        mergeDAO = daoFactory(classname = "Subscriptions.GetFilesForMerge")
+        mergeableFiles = mergeDAO.execute(self.subscription["id"])
+
+        mergeUnits = self.defineMergeUnits(mergeableFiles)
+        mergeUnits.sort(mergeUnitCompare)
+        self.defineMergeJobs(mergeUnits)
+
+        jobGroup = groupInstance(subscription = self.subscription)
+        jobGroup.add(self.jobs)
+        jobGroup.commit()
+        jobGroup.recordAcquire(list(self.jobs))
+        
+        return [jobGroup]
---
+++ test/python/WMCore_t/WMBS_t/JobSplitting_t/WMBSMergeBySize_t.py
@@ -0,0 +1,459 @@
+#!/usr/bin/env python
+"""
+_WMBSMergeBySize_t
+
+Unit tests for generic WMBS merging.
+"""
+
+__revision__ = "$Id: WMBSMergeBySize_t.py,v 1.1 2009/03/09 19:04:53 sfoulkes Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from sets import Set
+import unittest
+import os
+import threading
+
+from WMCore.WMBS.File import File
+from WMCore.WMBS.Fileset import Fileset
+from WMCore.WMBS.Job import Job
+from WMCore.WMBS.JobGroup import JobGroup
+from WMCore.WMBS.Subscription import Subscription
+from WMCore.WMBS.Workflow import Workflow
+
+from WMCore.DataStructs.Run import Run
+
+from WMCore.DAOFactory import DAOFactory
+from WMCore.WMFactory import WMFactory
+from WMCore.JobSplitting.SplitterFactory import SplitterFactory
+from WMCore.Services.UUID import makeUUID
+from WMQuality.TestInit import TestInit
+
+class EventBasedTest(unittest.TestCase):
+    """
+    _EventBasedTest_
+
+    Test event based job splitting.
+    """
+    def runTest(self):
+        """
+        _runTest_
+
+        Run all the unit tests.
+        """
+        unittest.main()
+    
+    def setUp(self):
+        """
+        _setUp_
+
+        Create two subscriptions: One that contains a single file and one that
+        contains multiple files.
+        """
+        self.testInit = TestInit(__file__, os.getenv("DIALECT"))
+        self.testInit.setLogging()
+        self.testInit.setDatabaseConnection()
+        self.testInit.setSchema(customModules = ["WMCore.WMBS"],
+                                useDefault = False)
+        
+        return
+
+    def tearDown(self):
+        """
+        _tearDown_
+
+        Clear out WMBS.
+        """
+        myThread = threading.currentThread()
+
+        if myThread.transaction == None:
+            myThread.transaction = Transaction(self.dbi)
+            
+        myThread.transaction.begin()
+            
+        factory = WMFactory("WMBS", "WMCore.WMBS")
+        destroy = factory.loadObject(myThread.dialect + ".Destroy")
+        destroyworked = destroy.execute(conn = myThread.transaction.conn)
+        
+        if not destroyworked:
+            raise Exception("Could not complete WMBS tear down.")
+            
+        myThread.transaction.commit()
+        return
+
+    def stuffWMBS(self):
+        """
+        _stuffWMBS_
+
+        Insert some dummy jobs, jobgroups, filesets, files and subscriptions
+        into WMBS to test merging.  Three completed job groups each containing
+        several files are injected.  Another incomplete job group is also
+        injected.  Also files are added to the "Mergeable" subscription as well
+        as to the output fileset for their jobgroups.
+        """
+        bunkFileset = Fileset(name = "bunkFileset")
+        bunkFileset.create()
+
+        bunkWorkflow = Workflow(name = "bunkWorkflow", spec = "bunk",
+                                owner = "Steve")
+        bunkWorkflow.create()
+        
+        bunkSubscription = Subscription(fileset = bunkFileset,
+                                        workflow = bunkWorkflow)
+        bunkSubscription.create()
+
+        jobGroup1 = JobGroup(subscription = bunkSubscription)
+        jobGroup1.create()
+        newJob = Job()
+        newJob.create(jobGroup1)
+        newJob.changeStatus("COMPLETE")
+        jobGroup2 = JobGroup(subscription = bunkSubscription)
+        jobGroup2.create()
+        newJob = Job()
+        newJob.create(jobGroup2)
+        newJob.changeStatus("COMPLETE")        
+        jobGroup3 = JobGroup(subscription = bunkSubscription)
+        jobGroup3.create()
+        newJob = Job()
+        newJob.create(jobGroup3)
+        newJob.changeStatus("COMPLETE")
+        jobGroup4 = JobGroup(subscription = bunkSubscription)
+        jobGroup4.create()
+        newJob = Job()
+        newJob.create(jobGroup4)        
+
+        file1 = File(lfn = "file1", size = 1024, events = 1024, first_event = 0)
+        file1.addRun(Run(1, *[45]))
+        file2 = File(lfn = "file2", size = 1024, events = 1024, first_event = 1024)
+        file2.addRun(Run(1, *[45]))
+        file3 = File(lfn = "file3", size = 1024, events = 1024, first_event = 2048)
+        file3.addRun(Run(1, *[45]))
+        file4 = File(lfn = "file4", size = 1024, events = 1024, first_event = 3072)        
+        file4.addRun(Run(1, *[45]))
+
+        fileA = File(lfn = "fileA", size = 1024, events = 1024, first_event = 0)
+        fileA.addRun(Run(1, *[46]))
+        fileB = File(lfn = "fileB", size = 1024, events = 1024, first_event = 1024)
+        fileB.addRun(Run(1, *[46]))
+        fileC = File(lfn = "fileC", size = 1024, events = 1024, first_event = 2048)
+        fileC.addRun(Run(1, *[46]))
+
+        fileI = File(lfn = "fileI", size = 1024, events = 1024, first_event = 0)
+        fileI.addRun(Run(2, *[46]))
+        fileII = File(lfn = "fileII", size = 1024, events = 1024, first_event = 1024)
+        fileII.addRun(Run(2, *[46]))
+        fileIII = File(lfn = "fileIII", size = 1024, events = 1024, first_event = 2048)
+        fileIII.addRun(Run(2, *[46]))
+        fileIV = File(lfn = "fileIV", size = 1024, events = 1024, first_event = 3072)        
+        fileIV.addRun(Run(2, *[46]))        
+
+        fileX = File(lfn = "badFileA", size = 1024, events = 1024, first_event = 0)
+        fileX.addRun(Run(1, *[47]))
+        fileY = File(lfn = "badFileB", size = 1024, events = 1024, first_event = 1024)
+        fileY.addRun(Run(1, *[47]))
+        fileZ = File(lfn = "badFileC", size = 1024, events = 1024, first_event = 2048)
+        fileZ.addRun(Run(1, *[47]))
+
+        jobGroup1.groupoutput.addFile(file1)
+        jobGroup1.groupoutput.addFile(file2)
+        jobGroup1.groupoutput.addFile(file3)
+        jobGroup1.groupoutput.addFile(file4)        
+        jobGroup1.groupoutput.commit()
+
+        jobGroup2.groupoutput.addFile(fileA)
+        jobGroup2.groupoutput.addFile(fileB)
+        jobGroup2.groupoutput.addFile(fileC)
+        jobGroup2.groupoutput.commit()
+
+        jobGroup3.groupoutput.addFile(fileI)
+        jobGroup3.groupoutput.addFile(fileII)
+        jobGroup3.groupoutput.addFile(fileIII)
+        jobGroup3.groupoutput.addFile(fileIV)        
+        jobGroup3.groupoutput.commit()                
+
+        jobGroup4.groupoutput.addFile(fileX)
+        jobGroup4.groupoutput.addFile(fileY)
+        jobGroup4.groupoutput.addFile(fileZ)
+        jobGroup4.groupoutput.commit()
+
+        mergeFileset = Fileset(name = "mergeFileset")
+        mergeFileset.create()
+
+        mergeWorkflow = Workflow(name = "mergeWorkflow", spec = "bunk2",
+                                 owner = "Steve")
+        mergeWorkflow.create()
+        
+        self.mergeSubscription = Subscription(fileset = mergeFileset,
+                                              workflow = mergeWorkflow,
+                                              split_algo = "WMBSMergeBySize")
+        self.mergeSubscription.create()
+        
+        mergeFileset.addFile(file1)
+        mergeFileset.addFile(file2)
+        mergeFileset.addFile(file3)
+        mergeFileset.addFile(file4)
+        mergeFileset.addFile(fileA)
+        mergeFileset.addFile(fileB)
+        mergeFileset.addFile(fileC)
+        mergeFileset.addFile(fileI)
+        mergeFileset.addFile(fileII)
+        mergeFileset.addFile(fileIII)
+        mergeFileset.addFile(fileIV)
+        mergeFileset.addFile(fileX)
+        mergeFileset.addFile(fileY)
+        mergeFileset.addFile(fileZ)        
+        mergeFileset.commit()
+
+        return
+
+    def testMinMergeSize1(self):
+        """
+        _testMinMergeSize1_
+
+        Set the minimum merge size to be 20,000 bytes which is more than the
+        sum of all file sizes in the WMBS instance.  Verify that no merge jobs
+        will be produced.
+        """
+        self.stuffWMBS()
+
+        splitter = SplitterFactory()
+        jobFactory = splitter(package = "WMCore.WMBS",
+                              subscription = self.mergeSubscription)
+
+        result = jobFactory(min_merge_size = 20000, max_merge_size = 200000,
+                            max_merge_events = 20000)
+
+        assert len(result) == 1, \
+               "ERROR: More than one JobGroup returned."
+
+        assert len(result[0].jobs) == 0, \
+               "ERROR: No jobs should have been returned."
+
+        return
+
+    def testMinMergeSize2(self):
+        """
+        _testMinMergeSize2_
+
+        Set the minimum merge size to be 11,263 bytes which is one byte less
+        than the sum of all the file sizes in the WMBS instance.  Verify that
+        one merge job containing all the files in the WMBS instance is produced.
+        """
+        self.stuffWMBS()
+
+        splitter = SplitterFactory()
+        jobFactory = splitter(package = "WMCore.WMBS",
+                              subscription = self.mergeSubscription)
+
+        result = jobFactory(min_merge_size = 11263, max_merge_size = 20000,
+                            max_merge_events = 20000)
+
+        assert len(result) == 1, \
+               "ERROR: More than one JobGroup returned."
+
+        assert len(result[0].jobs) == 1, \
+               "ERROR: One job should have been returned."
+
+        fileset = list(result[0].jobs)[0].file_set
+
+        assert len(fileset.files) == 11, \
+               "ERROR: Merge job should contain 11 files."
+
+        goldenFiles = ["file1", "file2", "file3", "file4", "fileA", "fileB",
+                       "fileC", "fileI", "fileII", "fileIII", "fileIV"]
+
+        for file in fileset.files:
+            assert file["lfn"] in goldenFiles, \
+                   "ERROR: Unknown file: %s" % file["lfn"]
+
+            goldenFiles.remove(file["lfn"])
+
+        return
+
+    def testMaxMergeSize1(self):
+        """
+        _testMaxMergeSize1_
+
+        Set the maximum merge size to be two bytes.  Verify that three merge
+        jobs are created, one for each job group that exists inside the WMBS
+        instance.  Verify that each merge job contains the expected files.
+        """
+        self.stuffWMBS()
+
+        splitter = SplitterFactory()
+        jobFactory = splitter(package = "WMCore.WMBS",
+                              subscription = self.mergeSubscription)
+
+        result = jobFactory(min_merge_size = 1, max_merge_size = 2,
+                            max_merge_events = 20000)
+
+        assert len(result) == 1, \
+               "ERROR: More than one JobGroup returned."
+
+        assert len(result[0].jobs) == 3, \
+               "ERROR: Three jobs should have been returned."
+
+        goldenFilesA = ["file1", "file2", "file3", "file4"]
+        goldenFilesB = ["fileA", "fileB", "fileC"]
+        goldenFilesC = ["fileI", "fileII", "fileIII", "fileIV"]
+
+        for job in result[0].jobs:
+            fileset = job.file_set
+            
+            if list(fileset.files)[0]["lfn"] in goldenFilesA:
+                goldenFiles = goldenFilesA
+            elif list(fileset.files)[0]["lfn"] in goldenFilesB:
+                goldenFiles = goldenFilesB
+            else:
+                goldenFiles = goldenFilesC
+
+            for file in fileset.files:
+                assert file["lfn"] in goldenFiles, \
+                       "ERROR: Unknown file in merge jobs."
+
+                goldenFiles.remove(file["lfn"])
+
+        assert len(goldenFilesA) == 0 and len(goldenFilesB) == 0 and \
+               len(goldenFilesC) == 0, \
+               "ERROR: Files missing from merge jobs."
+
+        return
+
+    def testMaxMergeSize2(self):
+        """
+        _testMaxMergeSize2_
+
+        Set the minimum merge size to be one byte larger than the largest job
+        group in the WMBS instance and the max merge size to be one byte larger
+        than the total size of two of the groups.  Verify that one merge job
+        is produced with two of the job groups in it.
+        """
+        self.stuffWMBS()
+
+        splitter = SplitterFactory()
+        jobFactory = splitter(package = "WMCore.WMBS",
+                              subscription = self.mergeSubscription)
+
+        result = jobFactory(min_merge_size = 4097, max_merge_size = 7169,
+                            max_merge_events = 20000)
+
+        assert len(result) == 1, \
+               "ERROR: More than one JobGroup returned."
+
+        assert len(result[0].jobs) == 1, \
+               "ERROR: One job should have been returned."
+
+        goldenFilesA = ["file1", "file2", "file3", "file4"]
+        goldenFilesB = ["fileA", "fileB", "fileC"]
+        goldenFilesC = ["fileI", "fileII", "fileIII", "fileIV"]
+
+        fileset = list(result[0].jobs)[0].file_set
+            
+        for file in fileset.files:
+            if file["lfn"] in goldenFilesA:
+                goldenFilesA.remove(file["lfn"])
+            elif file["lfn"] in goldenFilesB:
+                goldenFilesB.remove(file["lfn"])
+            elif file["lfn"] in goldenFilesC:
+                goldenFilesC.remove(file["lfn"])
+
+        assert len(goldenFilesB) == 0 and \
+               (len(goldenFilesA) == 0 or len(goldenFilesC) == 0), \
+               "ERROR: Files not allocated to jobs correctly."
+
+        return
+
+    def testMaxEvents1(self):
+        """
+        _testMaxEvents1_
+
+        Set the maximum number of events per merge job to 1.  Verify that three
+        merge jobs are created, each one only containing the output of a single
+        job group.
+        """
+        self.stuffWMBS()
+
+        splitter = SplitterFactory()
+        jobFactory = splitter(package = "WMCore.WMBS",
+                              subscription = self.mergeSubscription)
+
+        result = jobFactory(min_merge_size = 1, max_merge_size = 20000,
+                            max_merge_events = 1)
+
+        assert len(result) == 1, \
+               "ERROR: More than one JobGroup returned."
+        
+        assert len(result[0].jobs) == 3, \
+               "ERROR: Three jobs should have been returned."
+
+        goldenFilesA = ["file1", "file2", "file3", "file4"]
+        goldenFilesB = ["fileA", "fileB", "fileC"]
+        goldenFilesC = ["fileI", "fileII", "fileIII", "fileIV"]
+
+        for job in result[0].jobs:
+            fileset = job.file_set
+            
+            if list(fileset.files)[0]["lfn"] in goldenFilesA:
+                goldenFiles = goldenFilesA
+            elif list(fileset.files)[0]["lfn"] in goldenFilesB:
+                goldenFiles = goldenFilesB
+            else:
+                goldenFiles = goldenFilesC
+
+            for file in fileset.files:
+                assert file["lfn"] in goldenFiles, \
+                       "ERROR: Unknown file in merge jobs."
+
+                goldenFiles.remove(file["lfn"])
+
+        assert len(goldenFilesA) == 0 and len(goldenFilesB) == 0 and \
+               len(goldenFilesC) == 0, \
+               "ERROR: Files missing from merge jobs."
+
+        return
+
+    def testMaxEvents2(self):
+        """
+        _testMaxEvents2_
+
+        Set the minimum merge size to be one byte larger than the largest job
+        group in the WMBS instance and the max events to be one event larger
+        than the total events in two of the groups.  Verify that one merge job
+        is produced with two of the job groups in it.
+        """
+        self.stuffWMBS()
+
+        splitter = SplitterFactory()
+        jobFactory = splitter(package = "WMCore.WMBS",
+                              subscription = self.mergeSubscription)
+
+        result = jobFactory(min_merge_size = 4097, max_merge_size = 20000,
+                            max_merge_events = 7169)
+
+        assert len(result) == 1, \
+               "ERROR: More than one JobGroup returned."
+
+        assert len(result[0].jobs) == 1, \
+               "ERROR: One job should have been returned."
+
+        goldenFilesA = ["file1", "file2", "file3", "file4"]
+        goldenFilesB = ["fileA", "fileB", "fileC"]
+        goldenFilesC = ["fileI", "fileII", "fileIII", "fileIV"]
+
+        fileset = list(result[0].jobs)[0].file_set
+            
+        for file in fileset.files:
+            if file["lfn"] in goldenFilesA:
+                goldenFilesA.remove(file["lfn"])
+            elif file["lfn"] in goldenFilesB:
+                goldenFilesB.remove(file["lfn"])
+            elif file["lfn"] in goldenFilesC:
+                goldenFilesC.remove(file["lfn"])
+
+        assert len(goldenFilesB) == 0 and \
+               (len(goldenFilesA) == 0 or len(goldenFilesC) == 0), \
+               "ERROR: Files not allocated to jobs correctly."
+
+        return
+    
+if __name__ == '__main__':
+    unittest.main()

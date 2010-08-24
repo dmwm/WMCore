6085f32d6a669b994a1d340d8c2cbec8fb3038a3
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:38:13 +0000

    Remove the creation of the job mask and the retrieving of the new job ID as these tasks are handled by different DAO objects.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3577 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/New.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/New.py
@@ -6,22 +6,16 @@ MySQL implementation of Jobs.New
 """
 
 __all__ = []
-__revision__ = "$Id: New.py,v 1.6 2008/11/25 17:21:43 sfoulkes Exp $"
-__version__ = "$Revision: 1.6 $"
+__revision__ = "$Id: New.py,v 1.7 2009/01/11 17:45:52 sfoulkes Exp $"
+__version__ = "$Revision: 1.7 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class New(DBFormatter):
-    sql = ["insert into wmbs_job (jobgroup, name) values (:jobgroup, :name)",
-           """insert into wmbs_job_mask (job) select id from wmbs_job
-              where jobgroup = :jobgroup and name = :name""",
-           "select id from wmbs_job where jobgroup = :jobgroup and name = :name"]
-    
-    def format(self, result):
-        result = DBFormatter.format(self, result)
-        return result[0][0]
+    sql = "insert into wmbs_job (jobgroup, name) values (:jobgroup, :name)"
         
-    def execute(self, jobgroup, name):
-        binds = self.getBinds(jobgroup=jobgroup, name=name) * 3
-        
-        return self.format(self.dbi.processData(self.sql, binds))
+    def execute(self, jobgroup, name, conn = None, transaction = False):
+        binds = self.getBinds(jobgroup = jobgroup, name = name)
+        self.dbi.processData(self.sql, binds, conn = conn,
+                             transaction = transaction)
+        return

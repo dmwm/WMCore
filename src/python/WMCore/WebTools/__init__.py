ee09215ecbcf9e6afca9cd5f5debbd440b5c9d8a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:36:31 +0000

    Core of DMWM web stack
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3549 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WebTools/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+__WebTools__
+
+Core of the DMWM web stack. Provides a basic set of features including security,
+performance optimisations (code minification, zipping and correct headers and 
+etags)
+"""
+
+__revision__ = "$Id: __init__.py,v 1.1 2009/01/07 11:55:50 metson Exp $"
+__version__ = "$Revision: 1.1 $"

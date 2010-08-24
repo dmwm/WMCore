6491be0cfecf5520ee77695fe3784a98648f3773
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:44:21 +0000

    Remove getBinds(), the version in the MySQL class is exactly the same.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3672 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Files/Add.py
+++ src/python/WMCore/WMBS/SQLite/Files/Add.py
@@ -6,22 +6,4 @@ from WMCore.WMBS.MySQL.Files.Add import Add as AddFileMySQL
 
 class Add(AddFileMySQL):
     sql = AddFileMySQL.sql
-    
-    
-    def getBinds(self, files=None, size=0, events=0, cksum=0):
-        # Can't use self.dbi.buildbinds here...
-        binds = {}
-        if type(files) == type('string'):
-            binds = {'lfn': files, 
-                     'filesize': size, 
-                     'events': events,
-                     'cksum': cksum} 
-        elif type(files) == type([]):
-        # files is a list of tuples containing lfn, size, events, run and lumi
-            binds = []
-            for f in files:
-                binds.append({'lfn': f[0], 
-                              'size': f[1], 
-                              'events': f[2],
-                              'cksum': f[3]})
-        return binds
+

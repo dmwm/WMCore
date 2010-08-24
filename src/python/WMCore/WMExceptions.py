91b606f7349c392d9b32521a2b382d48710be989
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:53:37 +0000

    Added exception
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2867 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMExceptions.py
+++ src/python/WMCore/WMExceptions.py
@@ -7,10 +7,12 @@ mappings to a human readable message.
 
 """
 
-__revision__ = "$Id: WMExceptions.py,v 1.2 2008/08/18 14:58:54 fvlingen Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: WMExceptions.py,v 1.3 2008/08/20 08:55:31 fvlingen Exp $"
+__version__ = "$Revision: 1.3 $"
 __author__ = "fvlingen@caltech.edu"
 
 
 WMEXCEPTION = {'WMCore-1' : 'Not allowed to instantiate', \
-   'WMCore-2' : 'Problem creating database table'}
+   'WMCore-2' : 'Problem creating database table',\
+   'WMCORE-3' : 'Could not find in library class',\
+   'WMCORE-4' : 'Problem with loading the class'}

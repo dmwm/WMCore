f7f255d37f427d437b973fd038985965f875f9c5
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:04:39 +0000

    added init file to create suite tests.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3051 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/python/WMCore_t/MsgService_t/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+_MsgService_t_
+
+MsgService test methods
+
+
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2008/09/25 14:36:57 fvlingen Exp $"
+__version__ = "$Revision: 1.1 $"
+

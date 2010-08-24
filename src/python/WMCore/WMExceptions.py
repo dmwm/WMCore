cecaff10ea1115de264d3f4d71b394a4e2b12299
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:52:50 +0000

    new file: list of exceptions used in WMCore
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2853 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMExceptions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""
+_WMExceptions_
+
+List of standard exception ids and their
+mappings to a human readable message.
+
+"""
+
+__revision__ = "$Id: WMExceptions.py,v 1.1 2008/08/16 05:06:25 fvlingen Exp $"
+__version__ = "$Revision: 1.1 $"
+__author__ = "fvlingen@caltech.edu"
+
+
+WMEXCEPTION = {'WMCore-1' : 'Not allowed to instantiate', \
+   'WMCore-2' : 'To be decided'}

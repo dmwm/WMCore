3ae1b46f3962139dca49a32aadf5c3c28e1a92a5
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:30:15 +0000

    No need to import SQLiteBase anymore...
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3453 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Workflow/LoadFromSpecOwner.py
+++ src/python/WMCore/WMBS/SQLite/Workflow/LoadFromSpecOwner.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 """
-_LoadSpecOwner_
+_LoadFromSpecOwner_
 
-SQLite implementation of Workflow.LoadSpecOwner
+SQLite implementation of Workflow.LoadFromSpecOwner
 
 """
-__all__ = []
-__revision__ = "$Id: LoadFromSpecOwner.py,v 1.1 2008/11/20 22:28:21 sfoulkes Exp $"
-__version__ = "$Revision: 1.1 $"
 
-from WMCore.WMBS.SQLite.Base import SQLiteBase
+__all__ = []
+__revision__ = "$Id: LoadFromSpecOwner.py,v 1.2 2008/11/26 19:52:02 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
+
 from WMCore.WMBS.MySQL.Workflow.LoadFromSpecOwner import LoadFromSpecOwner as LoadWorkflowMySQL
 
-class LoadFromSpecOwner(LoadWorkflowMySQL, SQLiteBase):
+class LoadFromSpecOwner(LoadWorkflowMySQL):
     sql = LoadWorkflowMySQL.sql

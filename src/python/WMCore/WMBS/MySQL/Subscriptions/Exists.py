f3d5254ba15ba8cb3a91da9481318001b39e979e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:26:35 +0000

    Better format the query result.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3396 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Subscriptions/Exists.py
+++ src/python/WMCore/WMBS/MySQL/Subscriptions/Exists.py
@@ -12,19 +12,22 @@ TABLE wmbs_subscription
     last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 """
 __all__ = []
-__revision__ = "$Id: Exists.py,v 1.1 2008/06/23 09:35:49 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Exists.py,v 1.2 2008/11/20 16:57:49 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WMBS.MySQL.Base import MySQLBase
 
 class Exists(MySQLBase):
     sql = """select id from wmbs_subscription
-            where fileset = :fileset and workflow = :workflow and type = :type"""
+             where fileset = :fileset and workflow = :workflow and type = :type"""
     
     def format(self, result):
         result = MySQLBase.format(self, result)
-        self.logger.debug( result )
-        return [x[0] for x in result]
+
+        if len(result) > 0:
+            return result[0][0]
+        else:
+            return False
         
     def execute(self, workflow=None, fileset=None, type = None, 
                 conn = None, transaction = False):

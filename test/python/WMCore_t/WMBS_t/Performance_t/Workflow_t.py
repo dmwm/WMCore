4d48595b69771be86045eb53e356070f4280fa98
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:12:31 +0000

    Working version of Workflow DAO Testcase
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3175 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WMBS_t/Performance_t/Workflow_t.py
+++ test/python/WMCore_t/WMBS_t/Performance_t/Workflow_t.py
@@ -32,38 +32,42 @@ class Workflow_t(Base_t):
     def testExists(self):         
         print "testExists"
 
-        time = self.perfTest(dao=self.dao, action='Workflow.Exists', execinput=['spec="TestDAO"', 'owner="PerformanceTestCaseDAO"', 'name="Test_Workflow"'])
+        time = self.perfTest(dao=self.dao, action='Workflow.Exists', spec=self.testWorkflow.spec, owner=self.testWorkflow.owner, name=self.testWorkflow.name)
         assert time <= self.threshold, 'Exists DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testNew(self):         
         print "testNew"
 
+        #Can't use testWorkflow from Base_t
+        spec='TestSpec'
+        owner='WorkflowTest'
+        name='NewTestWorkflow'
+
         #TODO - Confirm the exec input args
-        time = self.perfTest(dao=self.dao, action='Workflow.New', execinput=['workflow=5','fileset=1',
-'type=1'])
+        time = self.perfTest(dao=self.dao, action='Workflow.New', spec=spec, owner=owner, name=name)
         assert time <= self.threshold, 'New DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testDelete(self):         
         print "testDelete"
 
-        time = self.perfTest(dao=self.dao, action='Workflow.Delete', execinput=['id=self.testWorkflow.id'])
+        time = self.perfTest(dao=self.dao, action='Workflow.Delete', id=self.testWorkflow.id)
         assert time <= self.threshold, 'Delete DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testLoadFromID(self):         
         print "testLoadFromID"
 
-        time = self.perfTest(dao=self.dao, action='Workflow.LoadFromID', execinput=['workflow=self.testWorkflow.id'])
+        time = self.perfTest(dao=self.dao, action='Workflow.LoadFromID', workflow=self.testWorkflow.id)
         assert time <= self.threshold, 'LoadFromID DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testLoadFromName(self):         
         print "testLoadFromName"
 
-        time = self.perfTest(dao=self.dao, action='Workflow.LoadFromName', execinput=['workflow=self.testWorkflow.name'])
+        time = self.perfTest(dao=self.dao, action='Workflow.LoadFromName', workflow=self.testWorkflow.name)
         assert time <= self.threshold, 'LoadFromName DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testLoadSpecOwner(self):         
         print "testLoadSpecOwner"
 
-        time = self.perfTest(dao=self.dao, action='Workflow.LoadSpecOwner', execinput=['spec="Test", owner="PerformanceTestCase"'])
+        time = self.perfTest(dao=self.dao, action='Workflow.LoadSpecOwner', spec=self.testWorkflow.spec, owner=self.testWorkflow.owner)
         assert time <= self.threshold, 'LoadSpecOwner DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 

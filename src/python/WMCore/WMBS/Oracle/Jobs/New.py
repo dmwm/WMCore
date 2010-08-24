31a1c5113a204a2c1155cf314431bd915bf9d505
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:38:33 +0000

    Only add a row to the wmbs_job table.  Mask creation and the lookup of the ID is handled by other DAO objects.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3582 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Jobs/New.py
+++ src/python/WMCore/WMBS/Oracle/Jobs/New.py
@@ -2,30 +2,11 @@
 Oracle implementation of Jobs.New
 """
 
+import time
+
 from WMCore.WMBS.MySQL.Jobs.New import New as NewJobMySQL
 
 class New(NewJobMySQL):
-    sql = sql = [""" insert into wmbs_job (id, jobgroup, name, last_update) 
-                     values (wmbs_job_SEQ.nextval, :jobgroup, :name, 
-                             :timestamp)""",
-                 """insert into wmbs_job_mask (job, inclusivemask) values (
-                    (select id from wmbs_job 
-                     where jobgroup = :jobgroup and name = :name), 'Y')
-                    """,
-                 """ select id from wmbs_job 
-                     where jobgroup = :jobgroup and name = :name """]
-    
-    def execute(self, jobgroup=0, name=None, conn = None, transaction = False):
-        
-        # need to fix bind timestamp is not *2
-        binds = self.getBinds(jobgroup=jobgroup, name=name, 
-                              timestamp=self.timestamp())
-        result = self.dbi.processData(self.sql[0], binds)
-        
-        binds = self.getBinds(jobgroup=jobgroup, name=name)
-        result = self.dbi.processData(self.sql[1], binds)
-        
-        binds = self.getBinds(jobgroup=jobgroup, name=name)
-        result = self.dbi.processData(self.sql[2], binds)
-        
-        return self.format(result)
+    sql = """insert into wmbs_job (id, jobgroup, name, last_update) 
+              values (wmbs_job_SEQ.nextval, :jobgroup, :name,
+              %d)""" % time.time()

564adc0528ad226415e57a83a6a75b9f0fc46b93
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:39:09 +0000

    Clean out some dumb print statments
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2628 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Actions/AddLocation.py
+++ src/python/WMCore/WMBS/Actions/AddLocation.py
@@ -10,10 +10,7 @@ class AddLocationAction(BaseAction):
         myclass = self.loadDialect(self.name, dbinterface)
         action = myclass(self.logger, dbinterface)
         try:
-            print "Hello!"
-            value = action.execute(sename)
-            print value
-            return value
+            return action.execute(sename)
         except Exception, e:
             self.logger.exception(e)
             return False

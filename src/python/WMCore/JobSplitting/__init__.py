d9501e2128f2aa741062694a564c0b0aff1684ec
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:46:30 +0000

    First commit of the JobSplitting
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2747 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/JobSplitting/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+_JobSplitting_
+
+A set of algorithms to allocate work to a set of jobs split along lines 
+like event, run, lumi, file.
+
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2008/07/03 13:19:46 metson Exp $"
+__version__ = "$Revision: 1.1 $"
+

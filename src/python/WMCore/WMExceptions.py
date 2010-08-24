f9616a5bee9aba3a711046636bffc534c7c0d51b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:53:06 +0000

    Added new exception for table creation.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2858 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMExceptions.py
+++ src/python/WMCore/WMExceptions.py
@@ -7,10 +7,10 @@ mappings to a human readable message.
 
 """
 
-__revision__ = "$Id: WMExceptions.py,v 1.1 2008/08/16 05:06:25 fvlingen Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: WMExceptions.py,v 1.2 2008/08/18 14:58:54 fvlingen Exp $"
+__version__ = "$Revision: 1.2 $"
 __author__ = "fvlingen@caltech.edu"
 
 
 WMEXCEPTION = {'WMCore-1' : 'Not allowed to instantiate', \
-   'WMCore-2' : 'To be decided'}
+   'WMCore-2' : 'Problem creating database table'}

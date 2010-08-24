59d64c579e909e7278ba95526bd62c027bcb162d
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:58:14 +0000

    mysql has a different format to handle long type
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3893 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Files/GetParentIDsByID.py
+++ src/python/WMCore/WMBS/Oracle/Files/GetParentIDsByID.py
@@ -1,9 +1,17 @@
 """
 Oracle implementation of GetParentIDsByID
 """
-
+from sets import Set
 from WMCore.WMBS.MySQL.Files.GetParentIDsByID import GetParentIDsByID \
      as GetParentIDsMySQL
 
 class GetParentIDsByID(GetParentIDsMySQL):
     sql = GetParentIDsMySQL.sql
+    
+    def format(self, result):        
+        out = Set() 
+        for r in result:
+            for f in r.fetchall():
+                out.add(f[0])
+        return list(out) 
+    

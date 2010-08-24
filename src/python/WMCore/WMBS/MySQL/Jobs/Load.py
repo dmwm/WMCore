5815d718f2a80a56065ced4ec3a65871a781f458
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:51:41 +0000

    Override default format method
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2833 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/Load.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/Load.py
@@ -4,13 +4,20 @@ _Load_
 MySQL implementation of Jobs.Load
 """
 __all__ = []
-__revision__ = "$Id: Load.py,v 1.1 2008/08/05 17:59:30 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Load.py,v 1.2 2008/08/09 22:22:55 metson Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WMBS.MySQL.Base import MySQLBase
 
 class Load(MySQLBase):
     sql = "select file from wmbs_job_assoc where job = :jobid"
+    
+    def format(self, result):
+        out = []
+        for r in result:
+            for i in r.fetchall():
+                out.append(i[0])
+        return out
                
     def execute(self, id=0, conn = None, transaction = False):
         binds = self.getBinds(jobid = id)

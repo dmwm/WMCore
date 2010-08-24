a266614c53936b3fbb769f191b2e1759626e0bb1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:37:43 +0000

    Add flatten method
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3569 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/DataStructs/WMObject.py
+++ src/python/WMCore/DataStructs/WMObject.py
@@ -6,8 +6,8 @@ Helper class that other objects should inherit from
 
 """
 __all__ = []
-__revision__ = "$Id: WMObject.py,v 1.4 2008/11/11 11:26:47 metson Exp $"
-__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: WMObject.py,v 1.5 2009/01/10 15:49:16 metson Exp $"
+__version__ = "$Revision: 1.5 $"
 
 from sets import Set
 
@@ -32,3 +32,13 @@ class WMObject(object):
         if not isinstance(theset, Set):
             theset = Set(self.makelist(theset))
         return theset
+    
+    def flatten(self, list):
+        """
+        If a list has only one element return just that element, otherwise 
+        return the original list
+        """
+        if len(list) == 1:
+            return list[0]
+        return list
+    

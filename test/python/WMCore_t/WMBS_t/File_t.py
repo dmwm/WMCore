6df384eca3dcbf2004bf0a6edfc16adcbfd3a9f5
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:26:06 +0000

    Use Frank's TestInit class to do database and logger initializations.
    Also, add a couple more tests.
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3388 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WMBS_t/File_t.py
+++ test/python/WMCore_t/WMBS_t/File_t.py
@@ -5,225 +5,304 @@ _File_t_
 Unit tests for File creation, location and exists, including checks to see that calls 
 are database dialect neutral.
 
-Test creates one WMBS database instance which is used for all tests.
-
 """
 
-__revision__ = "$Id: File_t.py,v 1.2 2008/11/03 11:55:42 jacksonj Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: File_t.py,v 1.3 2008/11/20 16:41:15 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 
-import unittest, logging, os, commands
+import unittest
+import logging
+import os
+import commands
+import threading
+import random
 from sets import Set
+
 from WMCore.Database.DBCore import DBInterface
 from WMCore.Database.DBFactory import DBFactory
 from WMCore.DAOFactory import DAOFactory
 from WMCore.WMBS.File import File
-from unittest import TestCase
-import random
+from WMCore.WMFactory import WMFactory
+from WMQuality.TestInit import TestInit
 
-class FileClassTest(TestCase):
+class File_t(unittest.TestCase):
+    _setup = False
+    _teardown = False
+    
     def setUp(self):
-        logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%m-%d %H:%M',
-                    filename=__file__.replace('.py','.log'),
-                    filemode='w')
-            
-        mysqllogger = logging.getLogger('wmbs_mysql')
-        sqlitelogger = logging.getLogger('wmbs_sqlite')
+        """
+        _setUp_
+
+        Setup the database and logging connection.  Try to create all of the
+        WMBS tables.
+        """
+        if self._setup:
+            return
+
+        self.testInit = TestInit(__file__, os.getenv("DIALECT"))
+        self.testInit.setLogging()
+        self.testInit.setDatabaseConnection()
+        self.testInit.setSchema(customModules = ["WMCore.WMBS"],
+                                useDefault = False)
+
+        myThread = threading.currentThread()
+        daofactory = DAOFactory(package = "WMCore.WMBS",
+                                logger = myThread.logger,
+                                dbinterface = myThread.dbi)
+
+        locationAction = daofactory(classname = "Locations.New")
+        locationAction.execute(sename = "se1.cern.ch")
+        locationAction.execute(sename = "se1.fnal.gov")        
         
-        self.tearDown()
-        # Create the databases
-        
-        self.dbf1 = DBFactory(mysqllogger, 'mysql://metson@localhost/wmbs')
-        self.dbf2 = DBFactory(sqlitelogger, 'sqlite:///filetest.lite')
-        
-        self.daofactory1 = DAOFactory(package='WMCore.WMBS', 
-                                      logger=mysqllogger, 
-                                      dbinterface=self.dbf1.connect())
-        self.daofactory2 = DAOFactory(package='WMCore.WMBS', 
-                                      logger=sqlitelogger, 
-                                      dbinterface=self.dbf2.connect())
-        
-        theMySQLCreator = self.daofactory1(classname='CreateWMBS')
-        createworked = theMySQLCreator.execute()
-        if createworked:
-            mysqllogger.debug("WMBS MySQL database created")
-        else:
-            mysqllogger.debug("WMBS MySQL database could not be created, already exists?")
-            
-        theSQLiteCreator = self.daofactory2(classname='CreateWMBS')
-        createworked = theSQLiteCreator.execute()
-        if createworked:
-            sqlitelogger.debug("WMBS SQLite database created")
-        else:
-            sqlitelogger.debug("WMBS SQLite database could not be created, already exists?")
-        
-        self.selist = Set(['lcgse01.phy.bris.ac.uk', 
-                       'lcgse02.phy.bris.ac.uk', 
-                       'se01.fnal.gov', 
-                       'se02.fnal.gov'])
-        
-        self.daofactory1(classname='Locations.New').execute(sename=self.selist)  
-        self.daofactory2(classname='Locations.New').execute(sename=self.selist)
+        self._setup = True
+        return
           
     def tearDown(self):        
-        mysqllogger = logging.getLogger('wmbs_mysql')
-        sqlitelogger = logging.getLogger('wmbs_sqlite')
-        #Delete the databases
-    
-        mysqllogger.debug(commands.getstatusoutput('echo yes | mysqladmin -u root drop wmbs'))
-        mysqllogger.debug(commands.getstatusoutput('mysqladmin -u root create wmbs'))
-        mysqllogger.debug("WMBS MySQL database deleted")
-        try:
-            sqlitelogger.debug(os.remove('filetest.lite'))
-        except OSError:
-            #Don't care if the file doesn't exist
-            pass
-        sqlitelogger.debug("WMBS SQLite database deleted")
-              
-    def testInit(self, logger=None, dbf=None):
-        if not logger:
-            logger=logging.getLogger('testInit')
-        if not dbf:
-            dbf=self.dbf1
-        # Create a random, parentless, childless, unlocatied file
-        lfn = '/store/data/%s/%s/file.root' % (random.randint(1000, 9999), 
-                                              random.randint(1000, 9999))
-        size = random.randint(1000, 2000)
-        events = 1000
-        run = random.randint(0, 2000)
-        lumi = random.randint(0, 8)
+        """
+        _tearDown_
         
-        file = File(lfn=lfn, size=size, events=events, run=run, 
-                    lumi=lumi, logger=logger, dbfactory=dbf)
+        Drop all the WMBS tables.
+        """
+        myThread = threading.currentThread()
         
-        return file, lfn, size, events, run, lumi
+        if self._teardown:
+            return
         
-    def testGetInfo(self, logger=None, dbf=None):
-        if not logger:
-            logger=logging.getLogger('testGetInfo')
-        if not dbf:
-            dbf=self.dbf1
-        # Check that a file contains what it should
-        file, lfn, size, events, run, lumi = self.testInit(logger, dbf)
-        
-        info = file.getInfo()
-        assert info[0] == lfn, "lfn is wrong"
-        assert info[2] == size, "lfn is wrong"
-        assert info[3] == events, "lfn is wrong"
-        assert info[4] == run, "lfn is wrong"
-        assert info[5] == lumi, "lfn is wrong"
-        
-    def testGetParentLFNs(self, logger=None, dbf=None):
-        if not logger:
-            logger=logging.getLogger('testGetParentLFNs')
-        if not dbf:
-            dbf=self.dbf1
+        factory = WMFactory("WMBS", "WMCore.WMBS")
+        destroy = factory.loadObject(myThread.dialect + ".Destroy")
+        myThread.transaction.begin()
+        destroyworked = destroy.execute(conn = myThread.transaction.conn)
+        if not destroyworked:
+            raise Exception("Could not complete WMBS tear down.")
+        myThread.transaction.commit()
             
-        # Check that a file has the correct parents            
-        parent1, child = self.testAddParent()
-        
-        assert child.getParentLFNs()[0] == parent1.dict['lfn'], \
-                                                    "child has wrong parent"
-                                                    
-        parent2, child = self.testAddParent(child=child)
-        
-        parents = child.getParentLFNs()
-        assert len(parents) == 2, "child has %s parents, not 2" % len(parents) 
-        assert parent1 in parents and parent2 in parents, \
-                                                    "child has wrong parentage"
-    
-    def testLoad(self, logger=None, dbf=None, lfn=None):
-        if not logger:
-            logger=logging.getLogger('testLoad')
-        if not dbf:
-            dbf=self.dbf1
-        # Check that a file loads correctly 
-        if not lfn:
-            lfn = self.testSave(logger)[1]
-        file = File(lfn=lfn, logger=logger, dbfactory=dbf)
-        file.load()
-    
-    def testSave(self, logger=None, dbf=None):
-        if not logger:
-            logger=logging.getLogger('testSave')
-        if not dbf:
-            dbf=self.dbf1
-        # Check that a file saves correctly
-        file, lfn, size, events, run, lumi = self.testInit(logger, dbf)
-        file.save()
-        return file, lfn, size, events, run, lumi
-    
-    def testDelete(self, logger=None, dbf=None):
-        if not logger:
-            logger=logging.getLogger('testDelete')
-        if not dbf:
-            dbf=self.dbf1
-        # Check that a file deletes correctly
-        file, lfn, size, events, run, lumi = self.testSave(logger, dbf)
-        file.delete()
-        try:
-            self.testLoad(logger=logger, lfn=lfn)
-        except Exception, e:
-            str = e.message
-            str = str.lower()
-            if str == 'file not found':
-                pass
-            else:
-                raise e
+        self._teardown = True
+            
+    def testCreateDeleteExists(self):
+        """
+        _testCreateDeleteExists_
 
-    def testAddChild(self, logger=None, dbf=None):
-        if not logger:
-            logger=logging.getLogger('testAddChild')
-        if not dbf:
-            dbf=self.dbf1
-        # Check that children can be added correctly
-        # Return the parent and child
+        Test the create(), delete() and exists() methods of the file class
+        by creating and deleting a file.  The exists() method will be
+        called before and after creation and after deletion.
+        """
+        testFile = File(lfn = "/this/is/a/lfn", size = 1024, events = 10,
+                        run = 1, lumi = 45)
+
+        assert testFile.exists() == False, \
+               "ERROR: File exists before it was created"
+
+        testFile.create()
+
+        assert testFile.exists() > 0, \
+               "ERROR: File does not exist after it was created"
+
+        testFile.delete()
+
+        assert testFile.exists() == False, \
+               "ERROR: File exists after it has been deleted"
+        return
+
+    def testGetInfo(self):
+        """
+        _testGetInfo_
+
+        Test the getInfo() method of the File class to make sure that it
+        returns the correct information.
+        """
+        testFileParent = File(lfn = "/this/is/a/parent/lfn", size = 1024,
+                              events = 20, run = 1, lumi = 45)
+        testFileParent.create()
+
+        testFile = File(lfn = "/this/is/a/lfn", size = 1024, events = 10,
+                        run = 1, lumi = 45)        
+        testFile.create()
+        testFile.setLocation(se = "se1.fnal.gov", immediateSave = False)
+        testFile.setLocation(se = "se1.cern.ch", immediateSave = False)
+        testFile.addParent("/this/is/a/parent/lfn")
+
+        info = testFile.getInfo()
+        assert info[0] == testFile["lfn"], \
+               "ERROR: File returned wrong LFN"
         
-        child, child_lfn, size, events, run, lumi = self.testSave()
-        parent, parent_lfn, size, events, run, lumi = self.testSave()
+        assert info[1] == testFile["id"], \
+               "ERROR: File returned wrong ID"
         
-        parent.addChild(child_lfn)
-        return parent, child
+        assert info[2] == testFile["size"], \
+               "ERROR: File returned wrong size"
+        
+        assert info[3] == testFile["events"], \
+               "ERROR: File returned wrong events"
+        
+        assert info[4] == testFile["run"], \
+               "ERROR: File returned wrong run"
+
+        assert info[5] == testFile["lumi"], \
+               "ERROR: File returned wrong lumi"
+
+        assert len(info[6]) == 2, \
+               "ERROR: File returned wrong locations"
+
+        for testLocation in info[6]:
+            assert testLocation in ["se1.fnal.gov", "se1.cern.ch"], \
+                   "ERROR: File returned wrong locations"                   
+
+        assert len(info[7]) == 1, \
+               "ERROR: File returned wrong parents"
+
+        assert info[7][0] == testFileParent, \
+               "ERROR: File returned wrong parents"
+
+        testFile.delete()
+        testFileParent.delete()
+        return
+        
+    def testGetParentLFNs(self):
+        """
+        _testGetParentLFNs_
+
+        Create three files and set them to be parents of a fourth file.  Check
+        to make sure that getParentLFNs() on the child file returns the correct
+        LFNs.
+        """
+        testFileParentA = File(lfn = "/this/is/a/parent/lfnA", size = 1024,
+                               events = 20, run = 1, lumi = 45)
+        testFileParentB = File(lfn = "/this/is/a/parent/lfnB", size = 1024,
+                               events = 20, run = 1, lumi = 45)
+        testFileParentC = File(lfn = "/this/is/a/parent/lfnC", size = 1024,
+                               events = 20, run = 1, lumi = 45)
+        testFileParentA.create()
+        testFileParentB.create()
+        testFileParentC.create()
+
+        testFile = File(lfn = "/this/is/a/lfn", size = 1024,
+                               events = 10, run = 1, lumi = 45)
+        testFile.create()
+
+        testFile.addParent(testFileParentA["lfn"])
+        testFile.addParent(testFileParentB["lfn"])
+        testFile.addParent(testFileParentC["lfn"])        
+
+        parentLFNs = testFile.getParentLFNs()
+        
+        assert len(parentLFNs) == 3, \
+               "ERROR: Child does not have the right amount of parents"
+
+        goldenLFNs = ["/this/is/a/parent/lfnA",
+                      "/this/is/a/parent/lfnB",
+                      "/this/is/a/parent/lfnC"]
+        for parentLFN in parentLFNs:
+            assert parentLFN in goldenLFNs, \
+                   "ERROR: Unknown parent lfn"
+            goldenLFNs.remove(parentLFN)
+                   
+        testFile.delete()
+        testFileParentA.delete()
+        testFileParentB.delete()
+        testFileParentC.delete()
+        return
     
-    def testAddParent(self, logger=None, dbf=None, child=None, parent=None):
-        if not logger:
-            logger=logging.getLogger('testAddParent')
-        if not dbf:
-            dbf=self.dbf1
-        # Check that parents can be added correctly
-        # Return the parent and child
-        if not child:
-            child, child_lfn, size, events, run, lumi = self.testSave()
-        if not parent:
-            parent, parent_lfn, size, events, run, lumi = self.testSave()
-        
-        child.addParent(parent_lfn)
-        return parent, child
+    def testLoad(self):
+        """
+        _testLoad_
+
+        Create a file, add two parents and two locations and save the file
+        to the database.  Load the file using the ID and using the LFN and
+        compare the loaded file to the original to make sure everything loaded
+        correctly.
+        """
+        testFileParentA = File(lfn = "/this/is/a/parent/lfnA", size = 1024,
+                              events = 20, run = 1, lumi = 45)
+        testFileParentB = File(lfn = "/this/is/a/parent/lfnB", size = 1024,
+                              events = 20, run = 1, lumi = 45)
+        testFileParentA.create()
+        testFileParentB.create()
+
+        testFileA = File(lfn = "/this/is/a/lfn", size = 1024, events = 10,
+                        run = 1, lumi = 45)
+        testFileA.create()
+        testFileA.setLocation(se = "se1.fnal.gov", immediateSave = False)
+        testFileA.setLocation(se = "se1.cern.ch", immediateSave = False)
+        testFileA.addParent("/this/is/a/parent/lfnA")
+        testFileA.addParent("/this/is/a/parent/lfnB")
+        testFileA.updateLocations()
+                                                        
+        testFileB = File(lfn = testFileA["lfn"])
+        testFileB.load(parentage = 1)
+        testFileC = File(id = testFileA["id"])
+        testFileC.load(parentage = 1)
+
+        assert testFileA == testFileB, \
+               "ERROR: File load by LFN didn't work"
+
+        assert testFileA == testFileC, \
+               "ERROR: File load by ID didn't work"
+
+        testFileA.delete()
+        testFileParentA.delete()
+        testFileParentB.delete()
+        return
+
+    def testAddChild(self):
+        """
+        _testAddChild_
+
+        """
+        testFileParentA = File(lfn = "/this/is/a/parent/lfnA", size = 1024,
+                              events = 20, run = 1, lumi = 45)
+        testFileParentB = File(lfn = "/this/is/a/parent/lfnB", size = 1024,
+                              events = 20, run = 1, lumi = 45)
+        testFileParentA.create()
+        testFileParentB.create()
+
+        testFileA = File(lfn = "/this/is/a/lfn", size = 1024, events = 10,
+                         run = 1, lumi = 45)
+        testFileA.create()
+
+        testFileParentA.addChild("/this/is/a/lfn")
+        testFileParentB.addChild("/this/is/a/lfn")
+
+        testFileB = File(id = testFileA["id"])
+        testFileB.load(parentage = 1)
+
+        goldenFiles = [testFileParentA, testFileParentB]
+
+        for parentFile in testFileB["parents"]:
+            assert parentFile in goldenFiles, \
+                   "ERROR: Unknown parent file"
+            goldenFiles.remove(parentFile)
+
+        assert len(goldenFiles) == 0, \
+              "ERROR: Some parents are missing"
+
+        return
     
-    def testSetLocation(self, logger=None, dbf=None):
-        if not logger:
-            logger=logging.getLogger('testSetLocation')
-        if not dbf:
-            dbf=self.dbf1
-        # Check that a file is located properly
-        file, lfn, size, events, run, lumi = self.testSave(logger, dbf)
-        
-        assert len(file.dict['locations']) == 0, \
-                                        "file has a location before it should"
-        file.setLocation(self.selist)
-        assert len(file.dict['locations']) == len(self.selist), \
-                                        "file has incorrect number of locations"
-    
-    def testDialectNeutral(self, logger=None, dbf=None):
-        if not logger:
-            logger=logging.getLogger('testDialectNeutral')
-        if not dbf:
-            dbf=self.dbf1
-        # Test that File object behave the same for MySQL and SQLitepass
-        pass
-   
-     
+    def testSetLocation(self):
+        """
+        _testSetLocation_
+
+        Create a file and add a couple locations.  Load the file from the
+        database to make sure that the locations were set correctly.
+        """
+        testFileA = File(lfn = "/this/is/a/lfn", size = 1024, events = 10,
+                        run = 1, lumi = 45)
+        testFileA.create()
+        testFileA.setLocation(["se1.fnal.gov", "se1.cern.ch"])
+        testFileA.setLocation(["bunkse1.fnal.gov", "bunkse1.cern.ch"],
+                              immediateSave = False)        
+
+        testFileB = File(id = testFileA["id"])
+        testFileB.load()
+
+        goldenLocations = ["se1.fnal.gov", "se1.cern.ch"]
+
+        for location in testFileB["locations"]:
+            assert location in goldenLocations, \
+                   "ERROR: Unknown file location"
+            goldenLocations.remove(location)
+
+        assert len(goldenLocations) == 0, \
+              "ERROR: Some locations are missing"    
+
 if __name__ == "__main__":
     unittest.main() 

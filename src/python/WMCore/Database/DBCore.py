e458229eced0c7a7bda42acce2f3e53861fce77e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:37:25 +0000

    Updated DBInterface, with a few helper methods (makelist and buildbinds) and no SQLAlchemy imports
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2600 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Database/DBCore.py
+++ src/python/WMCore/Database/DBCore.py
@@ -6,12 +6,10 @@ Core Database APIs
 
 
 """
-__revision__ = "$Id: DBCore.py,v 1.5 2008/05/12 11:58:08 swakef Exp $"
-__version__ = "$Revision: 1.5 $"
+__revision__ = "$Id: DBCore.py,v 1.6 2008/05/21 14:26:28 metson Exp $"
+__version__ = "$Revision: 1.6 $"
 
-
-from sqlalchemy.databases.mysql import MySQLDialect
-    
+from copy import copy   
 class DBInterface(object):    
     """
     Base class for doing SQL operations using a SQLAlchemy engine, or
@@ -36,13 +34,33 @@ class DBInterface(object):
         self.logger.info ("Instantiating base WM SQL object")
         self.engine = engine
     
+    def makelist(self, thelist):
+        """
+        Simple method to ensure thelist is a list
+        """
+        if not isinstance(thelist, list):
+            thelist = [thelist]
+        return thelist
+    
+    def buildbinds(self, sequence, thename, therest = [{}]):
+        """
+        Build a list of binds. Can be used recursively, e.g.:
+        buildbinds(file, 'file', buildbinds(sename, 'location'), {'lumi':123})
+        TODO: replace with an appropriate map function 
+        """
+        binds = []
+        for r in sequence:
+            for i in self.makelist(therest):
+                    thebind = copy(i)
+                    thebind[thename] = r
+                    binds.append(thebind)
+        return binds
+
     def executebinds(self, s=None, b=None, connection=None):
         """
         _executebinds_
         """
         try:
-            if isinstance(self.engine.dialect, MySQLDialect) and b is not None:
-                s, b = self.substitute(s, b)
             self.logger.debug (s)
             self.logger.debug (b)
             if b == None: 
@@ -55,17 +73,6 @@ class DBInterface(object):
             self.logger.exception(s)
             self.logger.exception(b)
             raise e
-    
-    def substitute(self, sql, binds):
-        """
-        Horrible hacky thing to handle MySQL's lack of bind variable support
-        takes a single sql statment and its associated binds
-        """
-        newsql = sql
-        if isinstance(self.engine.dialect, MySQLDialect) and binds is not None:
-            for k, v in binds.items():
-                newsql = newsql.replace(':%s' % k, "'%s'" % v)
-            return newsql, None
             
     def processData(self, sqlstmt, binds = None, conn = None,
                     transaction = False):
@@ -78,10 +85,6 @@ class DBInterface(object):
             connection = self.engine.connect()
         else: 
             connection = conn
-            
-        if not transaction: 
-                trans = connection.begin()
-            
         result = []
         # Can take either a single statement or a list of statements and binds
         if type(sqlstmt) == type("string") and binds is None:
@@ -95,15 +98,14 @@ class DBInterface(object):
                                             connection=connection))
         elif type(sqlstmt) == type("string") and isinstance(binds, list):
             #Run single SQL statement for a list of binds
-            #if not transaction: 
-            #    trans = connection.begin()
-            #    print "start transaction"
+            if not transaction: 
+                trans = connection.begin()
             try:
                 for b in binds:
                     result.append(self.executebinds(sqlstmt, b,
                                             connection=connection))
-                #if not transaction: 
-                #    trans.commit()
+                if not transaction: 
+                    trans.commit()
             except Exception, e:
                 if not transaction: 
                     trans.rollback()
@@ -111,54 +113,31 @@ class DBInterface(object):
         elif isinstance(sqlstmt, list) and isinstance(binds, list) \
                 and len(binds) == len(sqlstmt):            
             # Run a list of SQL for a list of binds
-            #if not transaction: 
-             #   trans = connection.begin()
+            if not transaction: 
+                trans = connection.begin()
             try:
                 for i, s in enumerate(sqlstmt):
                     b = binds[i]
                     result.append(self.executebinds(sqlstmt, b,
                                             connection=connection))
-                #if not transaction:
-                #    trans.commit()
+                if not transaction: 
+                    trans.commit()
             except Exception, e:
                 if not transaction: 
                     trans.rollback()
-                raise e
+                raise e         
         else:
             self.logger.exception(
                 "Nothing executed, problem with your arguments")
             self.logger.debug('sql is %s items long' % len(sqlstmt))
             self.logger.debug('binds are %s items long' % len(binds))
             self.logger.debug('are binds and sql same length? : %s' % (
-                len(binds) == len(sqlstmt)),)
+                (len(binds) == len(sqlstmt))))
             self.logger.debug( sqlstmt, binds, connection, transaction)
             self.logger.debug( type(sqlstmt), type(binds),
                                type("string"), type({}),
                                type(connection), type(transaction))
-            raise 'some clever exception type'
-        
-#        if not transaction:
-#            #print "commiting transaction"
-#            trans.commit()
-#        if not conn: 
-#            connection.close() # Return connection to the pool
-        
-        # change resultset to list
-        #return result
-        temp = []
-        for row in result:
-            for subrow in row.fetchall():
-                #print "have %s" % subrow
-                #temp.extend(subrow.values())
-                temp.append(subrow.values())
-            temp.extend(row.fetchall())
-            #temp.append(row.fetchall())
-#        print temp
-        if not transaction:
-            #print "commiting transaction"
-            trans.commit()
         if not conn: 
             connection.close() # Return connection to the pool
-            
-        return temp
+        return result
         

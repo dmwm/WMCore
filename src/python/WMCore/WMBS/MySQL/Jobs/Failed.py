844f4ca5b27196b9f156d21493abc1bfc6d1e969
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:54:47 +0000

    update the completion_time when job is failed as well
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3838 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/Failed.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/Failed.py
@@ -6,17 +6,19 @@ MySQL implementation of Jobs.Failed
 move file into wmbs_group_job_acquired
 """
 __all__ = []
-__revision__ = "$Id: Failed.py,v 1.5 2009/01/27 16:47:15 sfoulkes Exp $"
-__version__ = "$Revision: 1.5 $"
-
+__revision__ = "$Id: Failed.py,v 1.6 2009/02/13 21:17:22 sryu Exp $"
+__version__ = "$Revision: 1.6 $"
+import time
 from WMCore.Database.DBFormatter import DBFormatter
 
 class Failed(DBFormatter):
-    sql = """insert into wmbs_group_job_failed (job, jobgroup) VALUES
-             (:job, (select jobgroup from wmbs_job where id = :job))"""
+    sql = ["""insert into wmbs_group_job_failed (job, jobgroup) VALUES
+             (:job, (select jobgroup from wmbs_job where id = :job))""",
+           " update wmbs_job set completion_time = %d where id = :job " 
+           % time.time()]
     
     def execute(self, job=0, conn = None, transaction = False):
-        binds = self.getBinds(job=job)
+        binds = self.getBinds(job=job) * 2
         self.dbi.processData(self.sql, binds, conn = conn,
                              transaction = transaction)
         return

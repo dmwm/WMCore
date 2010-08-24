1f75a09b6507bb569f1246118d64331f11b47d34
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:37:22 +0000

    First pass of a DBInterface factory
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2599 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/Database/DBFactory.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy import __version__ as sqlalchemy_version
+from WMCore.Database.Dialects import MySQLDialect
+from WMCore.Database.Dialects import SQLiteDialect
+from WMCore.Database.Dialects import OracleDialect
+
+class DBFactory(object):
+    def __init__(self, logger, dburl):
+        self.logger = logger
+        self.dburl = dburl
+        
+    def connect(self):
+        self.logger.debug("Using SQLAlchemy v.%s" % sqlalchemy_version)
+        self.logger.debug("creating DB engine %s" % dburl)
+        engine = create_engine(self.dburl, convert_unicode=True, encoding='utf-8', pool_size=10, pool_recycle=30)
+
+        dia = dbinterface.engine.dialect
+        if isinstance(dia, MySQLDialect):
+            from WMCore.Database.MySQLCore import MySQLInterface as DBInterface
+        else:
+            from WMCore.Database.DBCore import DBInterface
+            
+        return DBInterface(self.logger, engine)

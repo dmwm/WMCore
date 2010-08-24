7a2d3ba5a41bd8782ac5c8b4582542a50e9b0fe4
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:15:56 +0000

    Removed the argument formatting from perfTest to a new method. Added the number of times a dao class will be loaded for performance tests at perfTest.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3229 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WMBS_t/Performance_t/Base_t.py
+++ test/python/WMCore_t/WMBS_t/Performance_t/Base_t.py
@@ -118,8 +118,23 @@ class BaseTest():
         #Base tearDown method for the DB Performance test
         pass
 
-    
-    def perfTest(self, dao, action, **input):
+    def formatExecInput(self, input):
+        """
+        Method that format the string received as an argument
+        for the execute method of each DAO class.
+
+        """
+        fmtstring = ""
+        for i in input.keys():
+            if type(input[i]) == type('string'):
+                fmtstring = "%s %s='%s'," % (fmtstring, i, input[i])
+            else:
+                fmtstring = "%s %s=%s," % (fmtstring, i, input[i])
+        fmtstring = fmtstring.strip()
+        fmtstring = fmtstring.rstrip(",")
+        return fmtstring
+
+    def perfTest(self, dao, action, times=1, **input):
         """
         Method that executes a dao class operation and measures its
         execution time.
@@ -127,23 +142,21 @@ class BaseTest():
         """
         #Test each of the DAO classes of the specific WMBS class directory        
         action = dao(classname=action)
-        string = ""
-        for i in input.keys():
-            if type(input[i]) == type('string'):
-                string = "%s %s='%s'," % (string, i, input[i])
-            else:
-                string = "%s %s=%s," % (string, i, input[i])
-        string = string.strip()
-        string = string.rstrip(",")
+        string = self.formatExecInput(input=input)        
         string = "action.execute(%s)" % string
         self.logger.debug('the final string: %s' % string)
-        startTime = time.time()               
-        #Place execute method of the specific classname here
-        #string = compile(string)
-        eval(string)
+        diffTotal = 0
+
+        for i in range(0,times):
+            #Performance testing block START        
+            startTime = time.time()               
+            #Place execute method of the specific classname here
+            #string = compile(string)
+            eval(string)
+            endTime = time.time()
+            diffTime = endTime - startTime
+            #Performance testing block END        
+            diffTotal = diffTotal + diffTime
         if self.verbose == 'True':
-            print string
-        endTime = time.time()
-        diffTime = endTime - startTime
-        
-        return diffTime
+            print string + " - " + str(times) +" time(s) " 
+        return diffTotal

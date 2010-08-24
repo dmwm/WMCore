9bec4489f68e3d32b073cc3da342002e6708ef28
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:50:54 +0000

    Turn it into a table of all the active views
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3776 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/Welcome.py
+++ src/python/WMCore/WebTools/Welcome.py
@@ -2,12 +2,18 @@ from cherrypy import expose
 from WMCore.WebTools.Page import Page
 
 class Welcome(Page):
+    def __init__(self, namesAndDocstrings):
+        self.namesAndDocstrings = namesAndDocstrings
+ 
     @expose
     def index(self):
-        return """
-<h1>Welcome to the DMWM web framework</h1>
-<p><a href="documentation">Documentation</a></p>
-        """
+        html = '<h1>Welcome to the DMWM web framework</h1>'
+        html += "<table>\n"
+        html += "<tr><th>Service</th><th>Description</th></tr>\n"
+        for name, docstring in self.namesAndDocstrings:
+            html += '<tr><td><a href="%s">%s</a></td><td>%s</td></tr>' % (name, name, docstring)
+        html += '</table>'
+        return html
     
     @expose
     def default(self, *args, **kwargs):

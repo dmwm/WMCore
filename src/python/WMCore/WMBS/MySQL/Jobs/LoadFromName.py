312ed7f277fc4f2ad4f85369560a8dcf62c4a994
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:41:18 +0000

    Minor cleanups and use formatDict() to format query results.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3623 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/LoadFromName.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/LoadFromName.py
@@ -6,8 +6,8 @@ MySQL implementation of Jobs.LoadFromName.
 """
 
 __all__ = []
-__revision__ = "$Id: LoadFromName.py,v 1.3 2009/01/12 19:26:03 sfoulkes Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: LoadFromName.py,v 1.4 2009/01/14 16:44:06 sfoulkes Exp $"
+__version__ = "$Revision: 1.4 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -16,32 +16,12 @@ class LoadFromName(DBFormatter):
     _LoadFromName_
 
     Retrieve meta data for a job given it's name.  This includes the name,
-    job group and last update time.  This will also retrieve the job mask
-    if one exists.
+    job group and last update time. 
     """
     sql = """SELECT ID, NAME, JOBGROUP, LAST_UPDATE FROM wmbs_job WHERE
              NAME = :name"""
     
-    def format(self, resultProxy):
-        """
-        _format_
-
-        Format the results of the SQL query into a dictionary with the
-        following keys:
-          NAME, JOB_GROUP, LAST_UPDATE
-        """
-        results = resultProxy[0].fetchall()
-
-        out = {}
-        out["ID"] = results[0][0]
-        out["NAME"] = results[0][1]
-        out["JOBGROUP"] = results[0][2]
-        out["LAST_UPDATE"] = results[0][3]
-        
-        return out
-               
     def execute(self, name, conn = None, transaction = False):
-        binds = self.getBinds(name = name)
-        result = self.dbi.processData(self.sql, binds, conn = conn,
+        result = self.dbi.processData(self.sql, {"name": name}, conn = conn,
                                       transaction = transaction)
-        return self.format(result)
+        return self.formatDict(result)[0]

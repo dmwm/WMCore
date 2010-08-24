fe887d8aa4d24d4f239496f148b0f643702365c1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:59:47 +0000

    Add directories for WMBS JobGroup SQL
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2974 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/SQLite/JobGroup/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+_WMBS_
+
+Workload Management Book-keeping Service
+
+
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2008/09/12 17:07:45 metson Exp $"
+__version__ = "$Revision: 1.1 $"
+

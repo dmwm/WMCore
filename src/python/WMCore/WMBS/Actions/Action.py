7577ec7e0ee7ce3bf4ebaa9fd2484f2a3bf2f8fe
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:41:12 +0000

    Update to reflect move of original action object to WMCore root
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2660 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Actions/Action.py
+++ src/python/WMCore/WMBS/Actions/Action.py
@@ -1,49 +1,20 @@
 """
+
+
 A basic action is a thing that will run a SQL statement
 
 A more complex one would be something that ran multiple SQL 
 objects to produce a single output.
 """
-from WMCore.Database.Dialects import MySQLDialect
-from WMCore.Database.Dialects import SQLiteDialect
-from WMCore.Database.Dialects import OracleDialect
 
-class BaseAction(object):
+from WMCore.Action import BaseAction as CoreBaseAction
+from WMCore.Action import BoundAction as CoreBoundAction
+
+class BaseAction(CoreBaseAction):
     name = "BaseAction"
     def __init__(self, logger):
-        self.logger = logger
-        self.logger.debug("Instantiating %s Action object" % self.name)
-        self.dialects = {"Oracle" : OracleDialect,
-                    "MySQL" : MySQLDialect,
-                    "SQLite" : SQLiteDialect}
-    
-    def loadDialect(self, classname, dbinterface):
-        """
-        Somewhat fugly method to load generic SQL classes...
-        """
-        
-        dia = dbinterface.engine.dialect
-        #TODO: Make good
-        dialect = None
-        for i in self.dialects.keys():
-            if isinstance(dia, self.dialects[i]): 
-                dialect = i
-        if not dialect: 
-            raise TypeError, "unknown connection type: %s" % dia
-        
-        module = "WMCore.WMBS.%s.%sSQL" % (dialect, classname)
-        self.logger.debug("importing %s, %s" % (module, classname))
-        module = __import__(module, globals(), locals(), [classname], -1)
-        instance = getattr(module, classname.split('.')[-1])
-        return instance
+        CoreBaseAction.__init__(self, package='WMCore.WMBS', logger=logger)
 
-    
-    def execute(self, dbinterface = None):
-        myclass = self.loadDialect(self.name, dbinterface)
-        action = myclass(self.logger, dbinterface)
-        try:
-            return action.execute()
-        except Exception, e:
-            self.logger.exception(e)
-            return False
-# Fin       
+class BoundAction(CoreBoundAction):
+    def __init__(self, logger):
+        CoreBoundAction.__init__(package='WMCore.WMBS', logger=logger)

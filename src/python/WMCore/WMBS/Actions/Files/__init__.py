2b4aa803b3458288b1e69373ce3f245316730ff3
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:49:17 +0000

    Store run/lumi in separate table, interact with both tables in one go
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2795 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/Actions/Files/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+_WMBS_
+
+Workload Management Book-keeping Service
+
+
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.3 2008/07/21 14:18:53 metson Exp $"
+__version__ = "$Revision: 1.3 $"
+

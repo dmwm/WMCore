dec4645cdcaa8a5302924f1878cbf7459e4e7efc
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:27:57 +0000

    Inherit from MySQL.JobGroup.New instead of defining everything again.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3417 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/JobGroup/New.py
+++ src/python/WMCore/WMBS/SQLite/JobGroup/New.py
@@ -1,30 +1,17 @@
 #!/usr/bin/env python
 """
 _New_
+
 SQLite implementation of JobGroup.New
 """
+
 __all__ = []
-__revision__ = "$Id: New.py,v 1.1 2008/10/16 15:34:12 jcgon Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: New.py,v 1.2 2008/11/21 17:10:28 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
 
-from WMCore.Database.DBFormatter import DBFormatter
-from WMCore.Services.UUID import makeUUID
+from WMCore.WMBS.MySQL.JobGroup.New import New as NewMySQL
 
-class New(DBFormatter):
+class New(NewMySQL):
     sql = []
-    sql.append("insert into wmbs_jobgroup (subscription, uid, last_update) values (:subscription, :uid, :timestamp)")
+    sql.append("""insert into wmbs_jobgroup (subscription, uid, output, last_update) values (:subscription, :uid, :output, strftime('%s', 'now'))""")
     sql.append("""select id from wmbs_jobgroup where uid=:uid""")
-    def format(self, result, uid):
-        result = DBFormatter.format(self, result)
-        result = result[0][0]
-        return result, uid
-        
-    def execute(self, subscription=None, conn = None, transaction = False):
-        uid=makeUUID()
-        binds = self.getBinds(subscription=subscription, uid=uid, timestamp=self.timestamp())
-        self.logger.debug('JobGroup.New sql: %s' % self.sql)
-        self.logger.debug('JobGroup.New binds: %s' % binds)
-        result = self.dbi.processData(self.sql[0], binds)
-        binds = self.getBinds(uid=uid)
-        result = self.dbi.processData(self.sql[1], binds)
-        return self.format(result, uid)

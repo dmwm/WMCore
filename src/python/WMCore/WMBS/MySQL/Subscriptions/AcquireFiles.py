a155c240a470d06e310fc884d17f18479369ed7f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:19:29 +0000

    If no files are passed in write a warning to the log and return
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3286 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Subscriptions/AcquireFiles.py
+++ src/python/WMCore/WMBS/MySQL/Subscriptions/AcquireFiles.py
@@ -5,8 +5,8 @@ _AcquireFiles_
 MySQL implementation of Subscription.AcquireFiles
 """
 __all__ = []
-__revision__ = "$Id: AcquireFiles.py,v 1.1 2008/06/24 15:53:16 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: AcquireFiles.py,v 1.2 2008/10/28 18:48:10 metson Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WMBS.MySQL.Base import MySQLBase
 
@@ -16,7 +16,9 @@ class AcquireFiles(MySQLBase):
         
     def execute(self, subscription=None, file=None,
                 conn = None, transaction = False):
-        
+        if len(file) == 0:
+            self.logger.warning('No files acquired for subscription id %s' % subscription)
+            return 0
         result = self.dbi.processData(self.sql, self.getBinds(subscription=subscription, file=file), 
                          conn = conn, transaction = transaction)
         return self.format(result)

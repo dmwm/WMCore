e42dc612666fdacc898a3656653e845c5f06feae
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:05:51 +0000

    More generic version of the Base_t class, to be used as a superclass for each DAO class testcases
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3071 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WMBS_t/Performance_t/Base_t.py
+++ test/python/WMCore_t/WMBS_t/Performance_t/Base_t.py
@@ -8,7 +8,7 @@ from WMCore.Database.DBCore import DBInterface
 from WMCore.Database.DBFactory import DBFactory
 from WMCore.DAOFactory import DAOFactory
 
-class Base_t(TestCase):
+class Base_t():
     """
     __Base_t__
 
@@ -17,7 +17,6 @@ class Base_t(TestCase):
 
     """
     def setUp(self):        
-        self.classType = 'FilePerfTest'
         logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
@@ -25,64 +24,47 @@ class Base_t(TestCase):
                     filemode='w')
         
         self.logger = logging.getLogger('DBPerformanceTest')
+        
+        self.DBList = ['MySQL','SQLite']
+        mysqlURI = 'mysql://jcg@localhost/wmbs'
+        sqliteURI = 'sqlite:///dbperftest.lite'
+
+        self.mysqldbf = DBFactory(self.logger, mysqlURI)
+        self.sqlitedbf = DBFactory(self.logger, sqliteURI)        
+
+        self.mysqldao = DAOFactory(package='WMCore.WMBS', logger=daologger, dbinterface=mysqldbf.connect())
+        self.sqlitedao = DAOFactory(package='WMCore.WMBS', logger=daologger, dbinterface=sqlitedbf.connect())        
+        
+    def tearDown(self)
+    #Base tearDown method for the DB Performance test
+    #TODO - Database deleting,etc,etc
+    pass
 
     def getClassNames(self, dirname='.'):
         files = os.listdir(dirname)
         list = []
         for x in files:
-            #Hack - Only get .py files and no __init__.py,etc...
+            #Hack - Only get relevant .py files (no __init__.py, CVS stuff, etc...)
             if (x[-3:] == '.py') and (x[0] != '_'):
                 parts = x.split('.')
                 list.append(parts[0])
         return list
-            
-    def perfDAOClassName(self,classname=None, threshold=1, DBType='MySQL'):
-        
-        # Make up the URI, different for each database you may want tif __name__ == "__main__":
-        #TODO - get a function to ignore case for DBType    
-        if DBType == 'MySQL':
-            sqlURI = 'mysql://jcg@localhost/wmbs'
-        elif DBType == 'SQLite':
-            sqlURI = 'sqlite:///fileperftest.lite'
-        #elif DBType == 'Oracle':
-            #sqlURI = 'Add Oracle URI here'
-        #TODO - Alter Exception to make it uniform with WMException
-        else:        
-            raise Exception, "Invalid DBType: "+DBType
     
-        daologger = logging.getLogger(DBType+'PerformanceLogger')
+    def perfTest(self, dao, execinput=''):
+        #POTENTIAL PITFALL:
+        #dao object must be given as an argument ALREADY WITH A CLASSNAME LOADED ON IT
+        #TODO - Exception that deals with uninitialized DAOs given as an argument
 
-        dbf = DBFactory(self.logger, sqlURI)        
-
-        daofactory = DAOFactory(package='WMCore.WMBS', logger=daologger, dbinterface=dbf.connect())        
-        
         #Test each of the DAO classes of the specific WMBS class directory        
-        print classname+' Performance Test ( '+DBType+' )'     
         startTime = time.time()               
-
-        #Place execute method of the specific classname here
-        #TODO - Figure out how to handle the specific arguments for each class            
-        #daofactory(classname=x).execute()
-            
+        #Place execute method of the specific classname here            
+        dao.execute(execinput)
         endTime = time.time()
         diffTime = endTime - startTime
-        print classname+' DAO class performance test:'+str(diffTime)
-        assert diffTime <= threshold, classname+' DAO class - Operation too slow ( elapsed time:'+str(diffTime)+', threshold:'+str(threshold)+' )'
-     
-    def runTest(self):
-        #Construct the directory name string, based on DB type and WMBS class you may want to test        
-        ClassList = ['Files', 'Fileset', 'Jobs', 'JobGroup', 'Workflow', 'Subscriptions', 'Locations']          
-        DBList = ['MySQL','SQLite']        
-        for wmbsclass in ClassList:
-            print wmbsclass+' WMBS Class Performance Test'
-            for dbtype in DBList:
-                dirname = '/home/jcg/workspace/WMCoreCERN2/src/python/WMCore/WMBS/'+dbtype+'/'+wmbsclass
-                daoclasslist = self.getClassNames(dirname)
-                for daoclass in daoclasslist:                
-                    self.perfDAOClassName(daoclass, dbtype)
+        
+        return diffTime        
 
-if __name__ == "__main__":
-    unittest.main() 
+ 
             
         
         

2ecd2b9199d1569ba15bd493362d7b803182b481
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:48:37 +0000

    Set size correctly (could be a filelist for WMBS Subscription)
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2784 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/JobSplitting/FileBased.py
+++ src/python/WMCore/JobSplitting/FileBased.py
@@ -8,6 +8,6 @@ class FileBased(JobFactory):
     def algorithm(self, job_instance=None, *args, **kwargs):
         jobs = Set()
         while len(self.subscription.availableFiles()) > 0:
-            job = job_instance(self.subscription, self.subscription.acquireFiles(kwargs['files_per_job']))
+            job = job_instance(self.subscription, self.subscription.acquireFiles(size=kwargs['files_per_job']))
             jobs.add(job)
         return jobs

a26cbbf52c039544228e1067012fea2699cafdac
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:44:37 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3676 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMComponent/HTTPFrontend/Downloader.py
@@ -0,0 +1,26 @@
+from cherrypy.lib.static import serve_file
+import os
+
+class Downloader:
+    """
+    _Downloader_
+
+    Serve files from the JobCreator Cache via HTTP
+
+    """
+    def __init__(self, rootDir):
+        self.rootDir = rootDir
+
+    def index(self, filepath):
+        """
+        _index_
+
+        index response to download URL, serves the file
+        requested
+
+        """
+        pathInCache = os.path.join(self.rootDir, filepath)
+        logging.debug("Download Agent serving file: %s" % pathInCache)
+        return serve_file(pathInCache, "application/x-download", "attachment")
+    index.exposed = True
+

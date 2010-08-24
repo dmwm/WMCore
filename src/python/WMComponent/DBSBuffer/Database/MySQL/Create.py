53828ea3983b81a1da4864fa4bc4fdc78c00aeb1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:14:23 +0000

    Fixed the self.create sort bug
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3205 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSBuffer/Database/MySQL/Create.py
+++ src/python/WMComponent/DBSBuffer/Database/MySQL/Create.py
@@ -4,7 +4,7 @@ _Create_DBSBuffer_
 Implementation of Create_DBSBuffer for MySQL.
 """
 
-__revision__ = "$Id: Create.py,v 1.4 2008/10/15 12:53:37 afaq Exp $"
+__revision__ = "$Id: Create.py,v 1.5 2008/10/16 08:36:41 afaq Exp $"
 __version__ = "$Reivison: $"
 __author__ = "anzar@fnal.gov"
 
@@ -23,12 +23,11 @@ class Create(DBCreator):
 
         Call the base class's constructor and create all necessary tables,
         constraints and inserts.
-        """        
-
+        """
         myThread = threading.currentThread()
         DBCreator.__init__(self, myThread.logger, myThread.dbi)
-
-	self.create["01dbsbuf_dataset"] = \
+        
+        self.create["01dbsbuf_dataset"] = \
                 """CREATE TABLE dbsbuffer_dataset
 			(
 			   ID     BIGINT UNSIGNED not null auto_increment,
@@ -38,7 +37,7 @@ class Create(DBCreator):
 			) ENGINE=InnoDB"""
 
         self.create["02dbsbuf_file"] = \
-		"""CREATE TABLE dbsbuffer_file
+		      """CREATE TABLE dbsbuffer_file
 			( 
 			    ID                    BIGINT UNSIGNED not null auto_increment,
 			    LFN                   varchar(500)      unique not null,
@@ -51,15 +50,14 @@ class Create(DBCreator):
 			    RunLumiInfo           varchar(500),
 			    LastModificationDate  BIGINT,
 			    primary key(ID)
-		) ENGINE=InnoDB"""
+		    ) ENGINE=InnoDB"""
 
-	self.constraints["FK_dbsbuf_file_ds"]=\
-		"""ALTER TABLE dbsbuf_file ADD CONSTRAINT FK_dbsbuf_file_ds
+        self.constraints["FK_dbsbuf_file_ds"]=\
+		      """ALTER TABLE dbsbuf_file ADD CONSTRAINT FK_dbsbuf_file_ds
     			 foreign key(Dataset) references dbsbuffer_dataset(ID) on delete CASCADE"""
 
-	#self.triggers IS NOT a member so I will just use self.create for now
-	
-	self.create["03TR_dbsbuf_file_lud"]=\
+	    #self.triggers IS NOT a member so I will just use self.create for now
+        self.create["03TR_dbsbuf_file_lud"]=\
                 """CREATE TRIGGER TR_dbsbuf_file_lud BEFORE INSERT ON dbsbuf_file
                         FOR EACH ROW SET NEW.LastModificationDate = UNIX_TIMESTAMP();"""
 
@@ -67,7 +65,7 @@ class Create(DBCreator):
                 """CREATE TRIGGER TR_dbsbuf_ds_lud BEFORE INSERT ON dbsbuf_dataset
                         FOR EACH ROW SET NEW.LastModificationDate = UNIX_TIMESTAMP();"""
 
-	self.create.sort()
+        self.create=sorted(self.create.iteritems(), key=lambda (k,v):(v,k))
 
 
         

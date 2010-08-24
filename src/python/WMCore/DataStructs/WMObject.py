6153490631b8c3a44c7bb52fbfcad346f0d23e8a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:59:11 +0000

    Add makeset helper method - to be improved, e.g. remove duplicates
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2964 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/DataStructs/WMObject.py
+++ src/python/WMCore/DataStructs/WMObject.py
@@ -6,8 +6,11 @@ Helper class that other objects should inherit from
 
 """
 __all__ = []
-__revision__ = "$Id: WMObject.py,v 1.1 2008/07/03 16:59:46 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: WMObject.py,v 1.2 2008/09/10 19:56:19 metson Exp $"
+__version__ = "$Revision: 1.2 $"
+
+from sets import Set
+
 class WMObject(object):
     """
     Helper class that other objects should inherit from
@@ -19,3 +22,11 @@ class WMObject(object):
         if not isinstance(thelist, list):
             thelist = [thelist]
         return thelist
+    
+    def makeset(self, theset):
+        """
+        Simple method to ensure thelist is a list
+        """
+        if not isinstance(theset, Set):
+            theset = Set(self.makelist(theset))
+        return theset

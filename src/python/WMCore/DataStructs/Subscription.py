ae7004fc64ead31cedf958f52ec4c14c9ba08fc2
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:47:52 +0000

    Remove Get from file status
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2771 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/DataStructs/Subscription.py
+++ src/python/WMCore/DataStructs/Subscription.py
@@ -8,8 +8,8 @@ TODO: Add some kind of tracking for state of files - though if too much is
 added becomes counter productive
 """
 __all__ = []
-__revision__ = "$Id: Subscription.py,v 1.2 2008/07/04 16:45:47 metson Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: Subscription.py,v 1.3 2008/07/04 16:47:08 metson Exp $"
+__version__ = "$Revision: 1.3 $"
 from WMCore.DataStructs.Pickleable import Pickleable
 from WMCore.DataStructs.Fileset import Fileset 
 class Subscription(Pickleable):
@@ -48,13 +48,13 @@ class Subscription(Pickleable):
         return self.acquiredFiles.listNewFiles()  
     
     def filesOfStatus(self, status=None):
-        if status == 'GetAvailableFiles':
+        if status == 'AvailableFiles':
             return self.fileset - (self.acquired | self.completed | self.failed)
-        elif status == 'GetAcquiredFiles':
+        elif status == 'AcquiredFiles':
             return self.acquired
-        elif status == 'GetCompletedFiles':
+        elif status == 'CompletedFiles':
             return self.completed
-        elif status == 'GetFailedFiles':
+        elif status == 'FailedFiles':
             return self.failed
         
     def availableFiles(self):
@@ -62,22 +62,22 @@ class Subscription(Pickleable):
         Return a Set of files that are available for processing 
         (e.g. not already in use)
         """
-        return self.filestatus(status='GetAvailableFiles')
+        return self.filestatus(status='AvailableFiles')
             
     def acquiredFiles(self):
         """
         Set of files marked as acquired.
         """
-        return self.filestatus(status='GetAcquiredFiles')
+        return self.filestatus(status='AcquiredFiles')
         
     def completedFiles(self):
         """
         Set of files marked as completed.
         """
-        return self.filestatus(status='GetCompletedFiles')
+        return self.filestatus(status='CompletedFiles')
     
     def failedFiles(self):
         """
         Set of files marked as failed. 
         """
-        return self.filestatus(status='GetFailedFiles')
+        return self.filestatus(status='FailedFiles')

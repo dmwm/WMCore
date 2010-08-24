efe5b7702fecc5d76a1a48c8899708a34bd5e806
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:49:06 +0000

    Updated SQL to reflect the chnage in how run/lumi is sotred
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2792 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Files/GetByID.py
+++ src/python/WMCore/WMBS/MySQL/Files/GetByID.py
@@ -4,8 +4,9 @@ MySQL implementation of File.Get
 from WMCore.WMBS.MySQL.Base import MySQLBase
 
 class GetByID(MySQLBase):
-    sql = """select id, lfn, size, events, run, lumi
-                 from wmbs_file_details where id = :file"""
+    sql = """select file.id, file.lfn, file.size, file.events, map.run, map.lumi
+             from wmbs_file_details as file join wmbs_file_runlumi_map as map on map.file = file.id 
+             where file.id = :file"""
     
     def getBinds(self, files=None):
         binds = []

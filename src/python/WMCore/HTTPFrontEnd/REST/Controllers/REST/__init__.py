36b61e7adc72c96d871410ce264062dde04b3652
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:11:13 +0000

    Code clean-up to confirm with pylint standard, reach level 10/10
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3154 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/HTTPFrontEnd/REST/Controllers/REST/__init__.py
+++ src/python/WMCore/HTTPFrontEnd/REST/Controllers/REST/__init__.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 #-*- coding: ISO-8859-1 -*-
+"""
+Plugin declaration for WEBTOOLS
+"""
 
 from Framework import Controller
 from Framework.PluginManager import DeclarePlugin
 from cherrypy import expose
 
-from services.rest.RestServer import *
+from services.rest.RestServer import RestServer
 
-DeclarePlugin ("/Controllers/REST/RestServer", RestServer, {"baseUrl": "/services"})
+DeclarePlugin ("/Controllers/REST/RestServer", 
+                RestServer, {"baseUrl": "/services"})
--- src/python/WMCore/HTTPFrontEnd/REST/utils/Utils.py
+++ src/python/WMCore/HTTPFrontEnd/REST/utils/Utils.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 #-*- coding: ISO-8859-1 -*-
-#
-# Copyright 2006 Cornell University, Ithaca, NY 14853. All rights reserved.
-#
 # Author:  Valentin Kuznetsov, 2006
 """
 Common utilities module used by REST services
@@ -11,20 +8,20 @@ Common utilities module used by REST services
 # import system modules
 import logging, logging.handlers
 
-def setSQLAlchemyLogger(hdlr,logLevel):
-    # set up logging for SQLAlchemy
-    logging.getLogger('sqlalchemy.engine').setLevel(logLevel)
-    logging.getLogger('sqlalchemy.orm.unitofwork').setLevel(logLevel)
-    logging.getLogger('sqlalchemy.pool').setLevel(logLevel)
+def setsqlalchemylogger(hdlr, level):
+    """Set up logging for SQLAlchemy"""
+    logging.getLogger('sqlalchemy.engine').setLevel(level)
+    logging.getLogger('sqlalchemy.orm.unitofwork').setLevel(level)
+    logging.getLogger('sqlalchemy.pool').setLevel(level)
 
     logging.getLogger('sqlalchemy.engine').addHandler(hdlr)
     logging.getLogger('sqlalchemy.orm.unitofwork').addHandler(hdlr)
     logging.getLogger('sqlalchemy.pool').addHandler(hdlr)
 
-def setCherryPyLogger(hdlr,logLevel):
-    # set up logging for SQLAlchemy
-    logging.getLogger('cherrypy.error').setLevel(logLevel)
-    logging.getLogger('cherrypy.access').setLevel(logLevel)
+def setcherrypylogger(hdlr, level):
+    """Set up logging for CherryPy"""
+    logging.getLogger('cherrypy.error').setLevel(level)
+    logging.getLogger('cherrypy.access').setLevel(level)
 
     logging.getLogger('cherrypy.error').addHandler(hdlr)
     logging.getLogger('cherrypy.access').addHandler(hdlr)

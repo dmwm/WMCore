75695b2005af9755ff68d3a18cc57c95bdeb5c4f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:47:49 +0000

    add filesOfStatus method and use that for all calls to get sets of file of a certain status
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2770 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/DataStructs/Subscription.py
+++ src/python/WMCore/DataStructs/Subscription.py
@@ -4,23 +4,80 @@ _Subscription_
 
 workflow + fileset = subscription
 
-TODO: Add some kind of tracking for state of files
+TODO: Add some kind of tracking for state of files - though if too much is 
+added becomes counter productive
 """
 __all__ = []
-__revision__ = "$Id: Subscription.py,v 1.1 2008/07/03 17:00:21 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Subscription.py,v 1.2 2008/07/04 16:45:47 metson Exp $"
+__version__ = "$Revision: 1.2 $"
 from WMCore.DataStructs.Pickleable import Pickleable
-
+from WMCore.DataStructs.Fileset import Fileset 
 class Subscription(Pickleable):
     def __init__(self, fileset = None, workflow = None, 
-               split_algo = 'File', type = "Processing"):
+               split_algo = 'FileBased', type = "Processing"):
         self.fileset = fileset
         self.workflow = workflow
         self.type = type
         self.split_algo = split_algo
+        self.acquired = Fileset()
+        self.completed = Fileset()
+        self.failed = Fileset()
         
     def getWorkflow(self):
         return self.workflow
     
     def getFileset(self):
         return self.fileset
+
+    def availableFiles(self, parents=0):
+        """
+        Return a Set of files that are available for processing 
+        (e.g. not already in use)
+        """
+        return self.fileset.listFiles() - self.acquiredFiles
+    
+    def acquireFiles(self, size=1):
+        print "DataStruct Subscription acquireFiles"
+        files = self.availableFiles()
+        self.acquiredFiles.commit()
+        if len(files) < size or size == 0:
+            size = len(files)
+        for i in range(size):
+            self.acquiredFiles.addFile(files.pop())
+            i = i + 1
+        return self.acquiredFiles.listNewFiles()  
+    
+    def filesOfStatus(self, status=None):
+        if status == 'GetAvailableFiles':
+            return self.fileset - (self.acquired | self.completed | self.failed)
+        elif status == 'GetAcquiredFiles':
+            return self.acquired
+        elif status == 'GetCompletedFiles':
+            return self.completed
+        elif status == 'GetFailedFiles':
+            return self.failed
+        
+    def availableFiles(self):
+        """
+        Return a Set of files that are available for processing 
+        (e.g. not already in use)
+        """
+        return self.filestatus(status='GetAvailableFiles')
+            
+    def acquiredFiles(self):
+        """
+        Set of files marked as acquired.
+        """
+        return self.filestatus(status='GetAcquiredFiles')
+        
+    def completedFiles(self):
+        """
+        Set of files marked as completed.
+        """
+        return self.filestatus(status='GetCompletedFiles')
+    
+    def failedFiles(self):
+        """
+        Set of files marked as failed. 
+        """
+        return self.filestatus(status='GetFailedFiles')

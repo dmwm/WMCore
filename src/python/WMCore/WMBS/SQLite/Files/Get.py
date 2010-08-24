82d92c6b22c5aefbdcb13a337ed704d8eabc7236
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:45:42 +0000

    Remove Get.py
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2733 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Files/Get.py
+++
@@ -1,8 +0,0 @@
-"""
-SQLite implementation of GetFile
-"""
-from WMCore.WMBS.SQLite.Base import SQLiteBase
-from WMCore.WMBS.MySQL.Files.Get import Get as GetFileMySQL
-
-class Get(GetFileMySQL, SQLiteBase):
-    sql = GetFileMySQL.sql

44927651263e6ea0aafe65b3b195b22f7c49654e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:14:43 +0000

    pylint
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3210 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/Services_t/SiteDB_t/SiteDB_t.py
+++ test/python/WMCore_t/Services_t/SiteDB_t/SiteDB_t.py
@@ -2,12 +2,11 @@
 """
 Test case for SiteDB
 """
-__revision__ = "$Id: SiteDB_t.py,v 1.2 2008/10/16 11:28:06 ewv Exp $"
-__version__  = "$Revision: 1.2 $"
+__revision__ = "$Id: SiteDB_t.py,v 1.3 2008/10/16 12:49:56 ewv Exp $"
+__version__  = "$Revision: 1.3 $"
 __author__   = "ewv@fnal.gov"
 
 import unittest
-import logging
 
 from WMCore.Services.SiteDB.SiteDB import SiteDBJSON
 
@@ -16,17 +15,24 @@ class SiteDBJSONTest(unittest.TestCase):
     Unit tests for SiteScreening module
     """
 
+
     def setUp(self):
         """
         Setup for unit tests
         """
         self.mySiteDB = SiteDBJSON()
 
+
     def testCmsNametoSE(self):
         """
         Tests CmsNametoSE
         """
-        target = ['cmssrm.fnal.gov', 'ralsrmb.rl.ac.uk', 'gridka-dCache.fzk.de', 'srm-cms.cern.ch', 'castorsrm.cr.cnaf.infn.it', 'ccsrm.in2p3.fr', 'srm.grid.sinica.edu.tw', 'srm2.grid.sinica.edu.tw', 'ralsrma.rl.ac.uk', 'srm-cms.gridpp.rl.ac.uk', 'ralsrme.rl.ac.uk', 'ralsrmf.rl.ac.uk']
+        target = ['cmssrm.fnal.gov', 'ralsrmb.rl.ac.uk',
+                  'gridka-dCache.fzk.de', 'srm-cms.cern.ch',
+                  'castorsrm.cr.cnaf.infn.it', 'ccsrm.in2p3.fr',
+                  'srm.grid.sinica.edu.tw', 'srm2.grid.sinica.edu.tw',
+                  'ralsrma.rl.ac.uk', 'srm-cms.gridpp.rl.ac.uk',
+                  'ralsrme.rl.ac.uk', 'ralsrmf.rl.ac.uk']
         results = self.mySiteDB.cmsNametoSE("T1")
         self.failUnless(results == target)
 
@@ -35,7 +41,9 @@ class SiteDBJSONTest(unittest.TestCase):
         """
         Tests CmsNametoCE
         """
-        target = ['cclcgceli04.in2p3.fr', 'cclcgceli03.in2p3.fr', 'w-ce02.grid.sinica.edu.tw', 'w-ce01.grid.sinica.edu.tw', 'lcgce02.gridpp.rl.ac.uk', 'lcg00125.grid.sinica.edu.tw']
+        target = ['cclcgceli04.in2p3.fr', 'cclcgceli03.in2p3.fr',
+                  'w-ce02.grid.sinica.edu.tw', 'w-ce01.grid.sinica.edu.tw',
+                  'lcgce02.gridpp.rl.ac.uk', 'lcg00125.grid.sinica.edu.tw']
         results = self.mySiteDB.cmsNametoCE("T1")
         self.failUnless(results == target)
 
@@ -50,6 +58,7 @@ class SiteDBJSONTest(unittest.TestCase):
                                   name=cmsName)
         self.failUnless(results['0']['name'] == "T2_US_Nebraska")
 
+
     def testDNUserName(self):
         """
         Tests DN to Username lookup
@@ -59,6 +68,8 @@ class SiteDBJSONTest(unittest.TestCase):
         testUserName = "metson"
         userName = self.mySiteDB.dnUserName(dn=testDn)
         self.failUnless(testUserName == userName)
+
+
 
 if __name__ == '__main__':
     unittest.main()

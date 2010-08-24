f1a414c48b558090c17b598844c9b5823ef860bb
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:46:50 +0000

    First commit of DataStructs
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2753 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/DataStructs/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+_DataStructs_
+
+A set of base objects to be used/inherited elsewhere
+
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2008/07/03 16:40:30 metson Exp $"
+__version__ = "$Revision: 1.1 $"
+

bce8088718993475edd32078e12412497ad1ed33
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:43:19 +0000

    Add file actions
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2692 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Actions/Files/New.py
+++ src/python/WMCore/WMBS/Actions/Files/New.py
@@ -5,11 +5,23 @@ _NewFilesAction_
 Add a (list of) new file(s) to WMBS
 """
 
-__revision__ = "$Id: New.py,v 1.2 2008/06/12 10:05:44 metson Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: New.py,v 1.3 2008/06/19 11:48:14 swakef Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.WMBS.Actions.Action import BoundAction
 from WMCore.DAOFactory import DAOFactory
 
 class NewFileAction(BoundAction):
     name = "Files.Add"
+    
+    def execute(self, files=None, size=0, events=0, run=0, lumi=0, dbinterface=None):
+        """
+        import the approriate SQL object and execute it
+        """ 
+        myclass = self.loadDialect(self.name, dbinterface)
+        action = myclass(self.logger, dbinterface)
+        try:
+            return action.execute(files, size, events, run, lumi)
+        except Exception, e:
+            self.logger.exception(e)
+            return False

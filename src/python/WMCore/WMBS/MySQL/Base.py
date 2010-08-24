5034389360e9fefb0dde35b201af8f4077abc043
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:37:51 +0000

    Some simple "formatting"
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2607 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Base.py
+++ src/python/WMCore/WMBS/MySQL/Base.py
@@ -1,8 +1,12 @@
+"""
+Class to define the standardised formatting of MySQL results.
+"""
+
 class MySQLBase(object):
     def __init__(self, logger, dbinterface):
         self.logger = logger
         self.dbi = dbinterface
-    
+        
     def format(self, result):
         """
         Some standard formatting
@@ -12,11 +16,18 @@ class MySQLBase(object):
             for i in r.fetchall():
                 out.append(i)
         return out
-        
+    
+    def getBinds(self):
+        """
+        Return the appropriately formatted binds for the sql
+        """
+        return {}
+    
     def execute(self, conn = None, transaction = False):
         """
         A simple select with no binds/arguments is the default
         """
         result = self.dbi.processData(self.sql, self.getBinds(), 
                          conn = conn, transaction = transaction)
+        print result
         return self.format(result)
--- src/python/WMCore/WMBS/SQLite/Base.py
+++ src/python/WMCore/WMBS/SQLite/Base.py
@@ -1,6 +1,9 @@
 import datetime
 import time
-class SQLiteBase(object):
+from WMCore.WMBS.MySQL.Base import MySQLBase
+
+class SQLiteBase(MySQLBase):
+    
     def timestamp(self):
         """
         generate a timestamp
@@ -12,4 +15,22 @@ class SQLiteBase(object):
         """
         Some standard formatting making allowances for the difference with MySQL
         """
-        return result
+        out = []
+        for r in result:
+            for i in r.fetchall():
+                out.append(i)
+        return out
+    
+    def getBinds(self):
+        """
+        Return the appropriately formatted binds for the sql
+        """
+        return {}
+        
+    def execute(self, conn = None, transaction = False):
+        """
+        A simple select with no binds/arguments is the default
+        """
+        result = self.dbi.processData(self.sql, self.getBinds(), 
+                         conn = conn, transaction = transaction)
+        return self.format(result)

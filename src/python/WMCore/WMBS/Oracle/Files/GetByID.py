8cbd7811f44207319ac5119b4b27dc8efca54a0e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:42:42 +0000

    Remove the formatDict() method as this is now handled by the base class.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3645 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Files/GetByID.py
+++ src/python/WMCore/WMBS/Oracle/Files/GetByID.py
@@ -7,16 +7,3 @@ from WMCore.WMBS.MySQL.Files.GetByID import GetByID as GetByIDMySQL
 class GetByID(GetByIDMySQL):
     sql = """SELECT id, lfn, filesize, events, cksum
              FROM wmbs_file_details WHERE id = :fileid"""
-
-    def formatDict(self, result):
-        """
-        _formatDict_
-
-        Override the formatDict() method so that we can change the name
-        of the filesize column to just be size.
-        """
-        formattedResult = GetByIDMySQL.formatDict(self, result)
-        formattedResult[0]["size"] = formattedResult[0]["filesize"]
-        del formattedResult[0]["filesize"]
-
-        return formattedResult
--- src/python/WMCore/WMBS/Oracle/Files/GetByLFN.py
+++ src/python/WMCore/WMBS/Oracle/Files/GetByLFN.py
@@ -6,16 +6,3 @@ from WMCore.WMBS.MySQL.Files.GetByLFN import GetByLFN as GetByLFNMySQL
 class GetByLFN(GetByLFNMySQL):
     sql = """SELECT id, lfn, filesize, events, cksum
              FROM wmbs_file_details WHERE lfn = :lfn"""
-
-    def formatDict(self, result):
-        """
-        _formatDict_
-
-        Override the formatDict() method so that we can change the name
-        of the filesize column to just be size.
-        """
-        formattedResult = GetByLFNMySQL.formatDict(self, result)
-        formattedResult[0]["size"] = formattedResult[0]["filesize"]
-        del formattedResult[0]["filesize"]
-
-        return formattedResult

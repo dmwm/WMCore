f6c4f927176d2eee5dc3d32c829ea86ab85d268d
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:09:29 +0000

    3 tests to fix - added some debugging print out
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3131 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WMBS_t/Performance_t/File_t.py
+++ test/python/WMCore_t/WMBS_t/Performance_t/File_t.py
@@ -26,61 +26,73 @@ class File_t(Base_t,TestCase):
         file_size = 1111
         file_run = 111
         file_lumi = 0
+        
         self.testFile = File(lfn=file_lfn, size=file_size, events=file_events, run=file_run,
                     lumi=file_lumi, logger=self.logger, dbfactory=self.mysqldbf)
+        self.testFile.save()
         # Create a Fileset of random, parentless, childless, unlocatied file
         filelist = []
         for x in range(random.randint(1000,3000)):
-            lfn = '/store/data/%s/%s/file.root' % (random.randint(1000, 9999), 
-                                                  random.randint(1000, 9999))
-            self.size = random.randint(1000, 2000)
-            self.events = 1000
-            self.run = random.randint(0, 2000)
-            self.lumi = random.randint(0, 8)
-        
-            file = File(lfn=file_lfn, size=file_size, events=file_events, run=file_run, 
-                    lumi=file_lumi, logger=self.logger, dbfactory=self.mysqldbf)
+            file = File(lfn='/store/data/%s/%s/file.root' % (random.randint(1000, 9999), 
+                                                  random.randint(1000, 9999)),
+                        size=random.randint(1000, 2000),
+                        events = 1000,
+                        run = random.randint(0, 2000),
+                        lumi = random.randint(0, 8), 
+                        logger=self.logger, 
+                        dbfactory=self.mysqldbf)
+            
             filelist.append(file)
+        
         self.testFileset = Fileset(name='testFileSet', 
                             files=filelist, 
                             logger=self.logger, 
                             dbfactory=self.mysqldbf) 
-            
+        self.testFileset.create()    
 
     def tearDown(self):
         #Call superclass tearDown method
         Base_t.tearDown(self)
 
     def testAdd(self):
-        time = self.perfTest(dao=self.mysqldao, action='Files.Add', execinput='files=self.testFileset, size=self.size, events=self.events '+self.baseexec)
+        print "testAdd"
+        print self.testFile['lfn'], self.testFile['size'], self.testFile['events']
+        time = self.perfTest(dao=self.mysqldao, action='Files.Add', execinput=['files=self.testFile["lfn"]', 'size=self.testFile["size"]', 'events=self.testFile["events"]'])
         assert time <= self.threshold, 'Add DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testAddRunLumi(self): 
-        time = self.perfTest(dao=self.mysqldao, action='Files.AddRunLumi', execinput='files=self.testFileset, run=self.run, lumi=self.lumi '+self.baseexec)
+        print "testAddRunLumi"
+        time = self.perfTest(dao=self.mysqldao, action='Files.AddRunLumi', execinput=['files=self.testFileset.name', 'run=self.run', 'lumi=self.lumi'])
         assert time <= self.threshold, 'AddRunLumi DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testAddToFileset(self):
-        time = self.perfTest(dao=self.mysqldao, action='Files.AddToFileset', execinput='file=self.testFile, fileset=self.testFileset '+self.baseexec)
+        print "testAddToFileset"
+        time = self.perfTest(dao=self.mysqldao, action='Files.AddToFileset', execinput=['file=self.testFile.lfn', 'fileset=self.testFileset.name '])
         assert time <= self.threshold, 'AddToFileset DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testDelete(self):
-        time = self.perfTest(dao=self.mysqldao, action='Files.Delete', execinput='file=self.testFile '+self.baseexec)
+        print "testDelete"
+        time = self.perfTest(dao=self.mysqldao, action='Files.Delete', execinput='file=self.testFile')
         assert time <= self.threshold, 'Delete DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testGetByID(self):
-        time = self.perfTest(dao=self.mysqldao, action='Files.GetByID', execinput='files=self.testFileset '+self.baseexec)
+        print "testGetByID"
+        time = self.perfTest(dao=self.mysqldao, action='Files.GetByID', execinput='files=self.testFile.id')
         assert time <= self.threshold, 'GetByID DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
-    def testGetByLFN(self): 
-        time = self.perfTest(dao=self.mysqldao, action='Files.GetByLFN', execinput='files=self.testFileset '+self.baseexec)
+    def testGetByLFN(self):
+        print "testGetByLFN" 
+        time = self.perfTest(dao=self.mysqldao, action='Files.GetByLFN', execinput='files=self.testFile.lfn')
         assert time <= self.threshold, 'GetByLFN DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testGetLocation(self):
-        time = self.perfTest(dao=self.mysqldao, action='Files.GetLocation', execinput='file=self.testFile '+self.baseexec)
+        print "testGetLocation"
+        time = self.perfTest(dao=self.mysqldao, action='Files.GetLocation', execinput='files=self.testFileset')
         assert time <= self.threshold, 'GetLocation DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testGetParents(self):
-        time = self.perfTest(dao=self.mysqldao, action='Files.GetParents', execinput='files=self.testFileset '+self.baseexec)
+        print "testGetParents"
+        time = self.perfTest(dao=self.mysqldao, action='Files.GetParents', execinput='files=self.testFileset')
         assert time <= self.threshold, 'GetParents DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
 #    def testHeritage(self):
@@ -90,10 +102,12 @@ class File_t(Base_t,TestCase):
 #        assert time <= self.threshold, 'Heritage DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testInFileset(self):
+        print "testInFileset"
         time = self.perfTest(dao=self.mysqldao, action='Files.InFileset', execinput='files=self.testFileset '+self.baseexec)
         assert time <= self.threshold, 'InFileset DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 
     def testSetLocation(self):
+        print "testSetLocation"
         time = self.perfTest(dao=self.mysqldao, action='Files.SetLocation', execinput='file=self.testFile, sename=self.sename '+self.baseexec)
         assert time <= self.threshold, 'SetLocation DAO class - Operation too slow ( elapsed time:'+str(time)+', threshold:'+str(self.threshold)+' )'
 

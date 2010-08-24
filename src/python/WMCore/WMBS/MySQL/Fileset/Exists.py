dc966c9c70d582f615f327f838b52823ab92532c
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:39:38 +0000

    change to return id instead of count
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3599 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Fileset/Exists.py
+++ src/python/WMCore/WMBS/MySQL/Fileset/Exists.py
@@ -5,22 +5,21 @@ _Exists_
 MySQL implementation of Fileset.Exists
 """
 __all__ = []
-__revision__ = "$Id: Exists.py,v 1.2 2008/11/20 21:52:33 sryu Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: Exists.py,v 1.3 2009/01/13 16:39:02 sryu Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class Exists(DBFormatter):
-    sql = """select count(*) from wmbs_fileset 
+    sql = """select id from wmbs_fileset 
             where name = :name"""
     
     def format(self, result):
         result = DBFormatter.format(self, result)
-        self.logger.debug( result )
-        if result[0][0] > 0:
-            return True
-        else:
+        if len(result) == 0:
             return False
+        else:
+            return result[0][0]
         
     def getBinds(self, name = None):
         return self.dbi.buildbinds(self.dbi.makelist(name), 'name')

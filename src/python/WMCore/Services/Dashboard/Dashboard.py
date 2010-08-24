49c54c667d0627a6a9125cc1f3573d2107bfcfca
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:01:46 +0000

    Get site status from Dashboard
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3004 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/Services/Dashboard/Dashboard.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+_Dashboard_
+
+Talk to the Dashboard Service to get site status.
+"""
+
+__revision__ = "$Id: Dashboard.py,v 1.1 2008/09/18 18:56:48 metson Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.Services.Service import Service
+class Dashboard(Service):
+    """
+    Dashboard provides a service that gives a site's status. A site can be in 
+    the follow states: "MAINTENANCE", "ERROR", "WARNING", "UP"
+    """
+    
+    def getStatus(self, name):
+        summaryfile = "db_sam_summary_%s.csv" % (name)
+        self.logger.debug('writing to %s/%s for site %s' % (self.path, 
+                                                            summaryfile, 
+                                                            name))
+        
+        url = '/latestresults?sites=%s' % (name)
+        summaryfile = self.refreshCache(summaryfile, url)
+        
+        status = []
+        for l in summaryfile.readlines():
+            status.append(l.strip().rsplit(',',1)[1])
+        self.logger.debug(status)
+        for i in ["MAINTENANCE", "ERROR", "WARNING", "UP"]:
+            if i in status:
+                 return {'status':i, 'id':name}
+        return {'status':'UNKNOWN', 'id':name}
+    
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='/tmp/sam/samstatus.log',
+                    filemode='w')
+    
+    dict = {}
+    dict['endpoint'] = 'http://lxarda16.cern.ch/dashboard/request.py'
+    dict['cachepath'] = '/tmp/dashboard'
+    dict['type'] = 'text/csv'
+    dict['logger'] = logging.getLogger('SAMtest')
+    
+    dashboard = Dashboard(dict = dict)
+    print dashboard.getStatus(name='T1_UK_RAL')    

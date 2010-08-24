294cb201d27395c907e78c02c4b9e47c897345e5
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:57:21 +0000

    keep the inner select sql
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2932 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Files/AddRunLumi.py
+++ src/python/WMCore/WMBS/SQLite/Files/AddRunLumi.py
@@ -5,4 +5,5 @@ from WMCore.WMBS.SQLite.Base import SQLiteBase
 from WMCore.WMBS.MySQL.Files.AddRunLumi import AddRunLumi as AddRunLumiMySQL
 
 class AddRunLumi(AddRunLumiMySQL, SQLiteBase):
-    sql = AddRunLumiMySQL.sql
+    sql = """insert into wmbs_file_runlumi_map (file, run, lumi) 
+                values ((select id from wmbs_file_details where lfn = :lfn), :run, :lumi)"""

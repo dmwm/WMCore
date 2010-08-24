a8a44e4f4230e2ac0e8e98b7869834b5cf20d5c2
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:55:59 +0000

    use insert ... select instead of insert with inner select
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2909 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Files/SetLocation.py
+++ src/python/WMCore/WMBS/MySQL/Files/SetLocation.py
@@ -1,17 +1,28 @@
 """
 MySQL implementation of SetLocation
 """
-from WMCore.WMBS.MySQL.Base import MySQLBase
+from WMCore.Database.DBFormatter import DBFormatter
 
-class SetLocation(MySQLBase):
-    sql = """insert into wmbs_file_location (file, location) 
-                values ((select id from wmbs_file_details where lfn = :file),
-                (select id from wmbs_location where se_name = :location))"""
+class SetLocation(DBFormatter):
+    
+    #NSERT newtable (user,age,os) SELECT table1.user,table1.age,table2.os FROM table1,table2 WHERE table1.user=table2.user;
+    
+    sql = """insert wmbs_file_location (file, location) 
+            select wmbs_file_details.id, wmbs_location.id from wmbs_file_details, wmbs_location 
+            where wmbs_file_details.lfn = :file
+            and wmbs_location.se_name = :location"""
                 
     def getBinds(self, file = None, location = None):
-        return self.dbi.buildbinds(self.dbi.makelist(file), 'file', 
-                                   self.dbi.buildbinds(self.dbi.makelist(location), 'location'))
-    
+        if type(location) == type('string'):
+            return self.dbi.buildbinds(self.dbi.makelist(file), 'file', 
+                   self.dbi.buildbinds(self.dbi.makelist(location), 'location'))
+        elif type(location) == type([]):
+            binds = []
+            for l in location:
+                binds.extend(self.dbi.buildbinds(self.dbi.makelist(file), 'file', 
+                   self.dbi.buildbinds(self.dbi.makelist(l), 'location')))
+            return binds
+        
     def execute(self, file = None, sename = None, conn = None, transaction = False):
         binds = self.getBinds(file, sename)
         self.logger.debug('File.SetLocation binds: %s' % binds)

cdf0991b6d27e76408009c8819c0cec27ae5de57
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:08:21 +0000

    Added Database backend info.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3112 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMComponent/ErrorHandler/Database/MySQL/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+_MySQL_
+
+Implementations for the mysql backend.
+
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2008/10/02 11:10:58 fvlingen Exp $"
+__version__ = "$Revision: 1.1 $"
+__author__ = "fvlingen@caltech.edu"
+

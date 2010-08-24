470319dc8ba926e819282b08d8fee39a279a0800
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:51:24 +0000

    Correct SQL
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2829 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/AddFiles.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/AddFiles.py
@@ -4,13 +4,13 @@ _AddFiles_
 MySQL implementation of Jobs.AddFiles
 """
 __all__ = []
-__revision__ = "$Id: AddFiles.py,v 1.1 2008/08/05 17:59:30 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: AddFiles.py,v 1.2 2008/08/09 22:19:39 metson Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WMBS.MySQL.Base import MySQLBase
 
 class AddFiles(MySQLBase):
-    sql = "insert into wmbs_job_assoc (id, file) values (:jobid, :fileid)"
+    sql = "insert into wmbs_job_assoc (job, file) values (:jobid, :fileid)"
                
     def execute(self, id=0, file=0, conn = None, transaction = False):
         binds = self.getBinds(jobid = id, fileid=file)

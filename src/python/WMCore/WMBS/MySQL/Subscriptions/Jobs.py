63039e76e69af84f5ce29817391c48090fb6a04b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:13:31 +0000

    Fixed the sql query and added self.sql (instead of sql) at the self.dbi.processData call
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3191 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Subscriptions/Jobs.py
+++ src/python/WMCore/WMBS/MySQL/Subscriptions/Jobs.py
@@ -7,16 +7,16 @@ MySQL implementation of Subscriptions.Jobs
 Return a list of all jobs that exist for a subscription.
 """
 __all__ = []
-__revision__ = "$Id: Jobs.py,v 1.1 2008/08/09 22:15:45 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Jobs.py,v 1.2 2008/10/15 07:56:36 jcgon Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class Jobs(DBFormatter):
-    sql = "select id from wmbs_job where subscription = :subscription"
-    
+    sql = "select id from wmbs_job where jobgroup in (select id from wmbs_jobgroup where subscription = :subscription)"
+
     def execute(self, subscription = 0, conn = None, transaction = False):
         binds = self.getBinds(subscription=subscription)
-        result = self.dbi.processData(sql, binds, 
+        result = self.dbi.processData(self.sql, binds, 
                          conn = conn, transaction = transaction)
         return self.format(result)

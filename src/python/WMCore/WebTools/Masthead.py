a4e81f93ab58e0cfe84d3873fb9393dcab804980
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:45:50 +0000

    First go at the new masthead service. Linked services should be picked up from the configuration
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3696 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WebTools/Masthead.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""
+__Controllers__
+
+Controllers return java script and/or css from a static directory, after 
+minimising setting appropriate headers and etags and gzip.  
+"""
+
+__revision__ = "$Id: Masthead.py,v 1.1 2009/01/24 01:32:24 metson Exp $"
+__version__ = "$Revision: 1.1 $"
+
+import os
+from Page import TemplatedPage, exposejs, exposecss
+from Controllers import Controllers
+from cherrypy import expose
+
+class Masthead(TemplatedPage, Controllers):
+    def __init__(self, config):
+        TemplatedPage.__init__(self, config)
+        Controllers.__init__(self, config)
+        
+    @expose
+    def index(self):
+        return "this is the web tools masthead"
+     
+    @exposejs
+    def masthead (self):
+        return self.templatepage ('masthead')
+
+    @exposejs
+    def masthead_table(self):
+        return self.templatepage ('masthead_table')
+    
+    @exposecss  
+    def mastheadcss(self, site=None, *args, **kwargs):
+        files = ['dmwt_main.css', 'dmwt_masthead.css']
+        if site:
+          files+= ['dmwt_masthead_%s.css' % site]
+        path = __file__.rsplit('/',1)[0]
+        data = ""
+        for f in files:
+          filename = "%s/css/%s" % (path, f)
+          if os.path.exists(filename):
+            lines = file(filename).readlines()
+            for l in lines:
+              data = data + l
+    
+        return self.minify(self.templatepage ('data', {'data':data}))
+  
+    @exposecss  
+    def mastheadcss_table(self, site=None, *args, **kwargs):
+        files = ['dmwt_main_table.css', 'dmwt_masthead.css']
+        if site:
+          files+= ['dmwt_masthead_table_%s.css' % site]
+        path = __file__.rsplit('/',1)[0]
+        data = ""
+        for f in files:
+          filename = "%s/css/%s" % (path, f)
+          if os.path.exists(filename):
+            lines = file(filename).readlines()
+            for l in lines:
+              data = data + l
+
+        return self.minify(self.templatePage ('data', {'data':data}))

98d18de34af0b53ea770d9b690405157d8967f24
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:37:32 +0000

    Added new expose decorators for variety of mime-types, e.g. exposexml.  Updated SecuredPage and added authenticateviacert method as an example what to extract from request header
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3566 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/Page.py
+++ src/python/WMCore/WebTools/Page.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-__revision__ = "$Id: Page.py,v 1.1 2009/01/07 11:56:51 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Page.py,v 1.2 2009/01/09 16:31:22 valya Exp $"
+__version__ = "$Revision: 1.2 $"
 
+import cherrypy
 from cherrypy import log
 from Cheetah.Template import Template
-import logging, os
+from simplejson import JSONEncoder
+import logging, os, types
 
 class Page(object):
     """
@@ -56,5 +58,77 @@ class TemplatedPage(Page):
 class SecuredPage(Page):
     def authenticate(self):
         pass
+
+    def authenticateviahn(self):
+        userdn = ""
+        return userdn
+
+    def authenticateviacert(self):
+        userdn = ""
+        try:
+            userdn  = cherrypy.request.headers['Cms-Client-S-Dn']
+            access  = cherrypy.request.headers['Cms-Auth-Status']
+            if  userdn != '(null)' and access == 'OK':
+                log("Found user cert", context=self.app, 
+                    severity=logging.DEBUG, traceback=False)
+        except:
+            log("No cert found in a browser", context=self.app, 
+                severity=logging.DEBUG, traceback=False)
+        return userdn
+
     
-    
+def exposexml (func):
+    def wrapper (self, *args, **kwds):
+        data = func (self, *args, **kwds)
+        cherrypy.response.headers['Content-Type'] = "application/xml"
+        return data
+    wrapper.__doc__ = func.__doc__
+    wrapper.__name__ = func.__name__
+    wrapper.exposed = True
+    return wrapper
+
+def exposetext (func):
+    def wrapper (self, *args, **kwds):
+        data = func (self, *args, **kwds)
+        cherrypy.response.headers['Content-Type'] = "text/plain"
+        return data
+    wrapper.__doc__ = func.__doc__
+    wrapper.__name__ = func.__name__
+    wrapper.exposed = True
+    return wrapper
+
+def exposejson (func):
+    def wrapper (self, *args, **kwds):
+        encoder = JSONEncoder()
+        data = func (self, *args, **kwds)
+        cherrypy.response.headers['Content-Type'] = "application/json"
+        try:
+            jsondata = encoder.iterencode(data)
+            return jsondata
+        except:
+            Exception("Fail to jsontify obj '%s' type '%s'" % (data, type(data)))
+#        return data
+    wrapper.__doc__ = func.__doc__
+    wrapper.__name__ = func.__name__
+    wrapper.exposed = True
+    return wrapper
+
+def exposejs (func):
+    def wrapper (self, *args, **kwds):
+        data = func (self, *args, **kwds)
+        cherrypy.response.headers['Content-Type'] = "application/javascript"
+        return data
+    wrapper.__doc__ = func.__doc__
+    wrapper.__name__ = func.__name__
+    wrapper.exposed = True
+    return wrapper
+
+def exposecss (func):
+    def wrapper (self, *args, **kwds):
+        data = func (self, *args, **kwds)
+        cherrypy.response.headers['Content-Type'] = "text/css"
+        return data
+    wrapper.__doc__ = func.__doc__
+    wrapper.__name__ = func.__name__
+    wrapper.exposed = True
+    return wrapper

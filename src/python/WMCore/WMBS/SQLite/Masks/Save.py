c2a2dec2305753282ba5c8fc395b705a79f26301
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:37:58 +0000

    Use the execute() method from the MySQL version of this object.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3573 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Masks/Save.py
+++ src/python/WMCore/WMBS/SQLite/Masks/Save.py
@@ -6,11 +6,10 @@ SQLite implementation of Masks.Save
 """
 
 __all__ = []
-__revision__ = "$Id: Save.py,v 1.1 2008/11/21 17:13:35 sfoulkes Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Save.py,v 1.2 2009/01/11 17:40:00 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WMBS.MySQL.Masks.Save import Save as SaveMySQL
 
 class Save(SaveMySQL):
-    def execute(self, jobid, mask):
-        SaveMySQL.execute(self, jobid, mask)
+    sqlBeginning = SaveMySQL.sqlBeginning

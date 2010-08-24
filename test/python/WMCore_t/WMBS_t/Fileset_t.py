76cec41c0f0da5bca3a6822a387b8522b115d6a8
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:26:17 +0000

    Use Frank's TestInit class to do all initializations.
    Also, add some more tests.
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3391 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WMBS_t/Fileset_t.py
+++ test/python/WMCore_t/WMBS_t/Fileset_t.py
@@ -7,60 +7,219 @@ its access methods and additional file insert methods are tested
 
 """
 
-import unittest, logging, random
-import Fileset
+__revision__ = "$Id: Fileset_t.py,v 1.2 2008/11/20 16:49:54 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
 
-class FilesetClassTest (TestCase):
+import unittest
+import logging
+import random
+import os
+import threading
+
+from WMCore.WMFactory import WMFactory
+from WMCore.WMBS.File import File
+from WMCore.WMBS.Fileset import Fileset
+
+from WMQuality.TestInit import TestInit
+
+class Fileset_t(unittest.TestCase):
+    _setup = False
+    _teardown = False
+    
     def setUp(self):
-        logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%m-%d %H:%M',
-                    filename=__file__.replace('.py','.log'),
-                    filemode='w')
-        self.logger = logging.getLogger('FilesetClassTest')
+        """
+        _setUp_
         
-        #Setup the initial testcase environment:
-        self.initialfile= File('/tmp/lfn1',1000,1,1,1)
-        self.initialSet = Set(initialfile)
+        Setup the database and logging connection.  Try to create all of the
+        WMBS tables.
+        """
+        if self._setup:
+            return
         
-        #A Fileset, containing a initial file on it.
-        fileset = Fileset(name = 'testFileSet', files = self.initialSet, logger = self.logger )
-        #Populating the fileset
-        for i in range(1,1000):
-            lfn = '/store/data/%s/%s/file.root' % (random.randint(1000, 9999),
-                                              random.randint(1000, 9999))
-            size = random.randint(1000, 2000)
-            events = 1000
-            run = random.randint(0, 2000)
-            lumi = random.randint(0, 8)
-
-            file = File(lfn=lfn, size=size, events=events, run=run, lumi=lumi)
-            fileset.addFile(file)
+        self.testInit = TestInit(__file__, os.getenv("DIALECT"))
+        self.testInit.setLogging()
+        self.testInit.setDatabaseConnection()
+        self.testInit.setSchema(customModules = ["WMCore.WMBS"],
+                                useDefault = False)
         
+        self._setup = True
+        return
+                                                                
     def tearDown(self):
-        #Is there a need for a tearDown in our fileset testcase?
-        pass
-    def testAddFile(self):
-        #First test - Add file and check if its there
-        #Second test - Add file that was already at Fileset.files , and check if its updated
-        #Third test - Add file that was already at Fileset.newfiles , and check if its updated
-        pass
-    def testListFiles(self):
-        filestemp = fileSet.listFiles()
-        assert( filesettemp in (testFileSet._files | testFileSet._newfiles), 'Message' )
-        pass
-    def testListLFNs(self):
-        #Im a bit confused with this method, leave to discuss it at the meeting with Simon
-        pass
-    def testListNewFiles(self):
-        newfilestemp = testFileSet.ListNewFiles()
-        assert(newfilestemp == testFileSet._newfiles, 'Message')
-    def testCommit(self):
-        localTestFileSet = Fileset('LocalTestFileset', initialSet)
-        localTestFileSet.add(file3)
-        newfilestemp = localTestFileSet.ListNewFiles()
-        localTestFileSet.commit()
-        #First, testing if the new file is present at file Set object attribute of the Fileset object
-        assert( newfilestemp in localTestFileSet.listFiles(), 'Message' )
-        #Second, testing if the newfile Set object attribute is empty
-        assert (localTestFileSet._newfiles == None, 'Message')
+        """
+        _tearDown_
+        
+        Drop all the WMBS tables.
+        """
+        myThread = threading.currentThread()
+        
+        if self._teardown:
+            return
+        
+        factory = WMFactory("WMBS", "WMCore.WMBS")
+        destroy = factory.loadObject(myThread.dialect + ".Destroy")
+        myThread.transaction.begin()
+        destroyworked = destroy.execute(conn = myThread.transaction.conn)
+        if not destroyworked:
+            raise Exception("Could not complete WMBS tear down.")
+        myThread.transaction.commit()
+        
+        self._teardown = True
+        return                                                                                
+
+    def testCreateDeleteExists(self):
+        """
+        _testCreateDeleteExists_
+
+        Create a delete a fileset object while also using the exists() method
+        to determine the the create() and delete() methods succeeded.
+        """
+        testFileset = Fileset(name = "TestFileset")
+
+        assert testFileset.exists() == False, \
+               "ERROR: Fileset exists before it was created"
+
+        testFileset.create()
+
+        assert testFileset.exists() >= 0, \
+               "ERROR: Fileset does not exist after it was created"
+
+        testFileset.delete()
+
+        assert testFileset.exists() == False, \
+               "ERROR: Fileset exists after it was deleted"
+
+        return
+
+    def testLoad(self):
+        """
+        _testLoad_
+
+        Create a fileset and load it from the database using the two
+        load methods.
+        """
+        testFileA = File(lfn = "/this/is/a/lfnA", size = 1024,
+                         events = 20, run = 1, lumi = 45)
+        testFileB = File(lfn = "/this/is/a/lfnB", size = 1024,
+                         events = 20, run = 1, lumi = 45)
+        testFileC = File(lfn = "/this/is/a/lfnC", size = 1024,
+                         events = 20, run = 1, lumi = 45)
+        testFileA.create()
+        testFileB.create()
+        testFileC.create()
+
+        testFilesetA = Fileset(name = "TestFileset")
+        testFilesetA.create()
+
+        testFilesetA.addFile(testFileA)
+        testFilesetA.addFile(testFileB)
+        testFilesetA.addFile(testFileC)
+        testFilesetA.commit()
+
+        testFilesetB = Fileset(name = testFilesetA.name)
+        testFilesetB.load(method = "Fileset.LoadFromName")        
+        testFilesetC = Fileset(id = testFilesetA.id)
+        testFilesetC.load(method = "Fileset.LoadFromID")
+
+        assert testFilesetB.id == testFilesetA.id, \
+               "ERROR: Load from name didn't load id"
+
+        assert testFilesetC.name == testFilesetA.name, \
+               "ERROR: Load from id didn't load name"
+
+        goldenFiles = [testFileA, testFileB, testFileC]
+        for filesetFile in testFilesetB.files:
+            assert filesetFile in goldenFiles, \
+                   "ERROR: Unknown file in fileset"
+            goldenFiles.remove(filesetFile)
+
+        assert len(goldenFiles) == 0, \
+               "ERROR: Fileset is missing files"
+
+        goldenFiles = [testFileA, testFileB, testFileC]
+        for filesetFile in testFilesetC.files:
+            assert filesetFile in goldenFiles, \
+                   "ERROR: Unknown file in fileset"
+            goldenFiles.remove(filesetFile)
+
+        assert len(goldenFiles) == 0, \
+               "ERROR: Fileset is missing files"
+        
+        testFilesetA.delete()
+        testFileA.delete()
+        testFileB.delete()
+        testFileC.delete()
+        
+    def testGetFiles(self):
+        """
+        _testGetFiles_
+
+        Create a fileset with three files and exercise the getFiles() method to
+        make sure that all the results it returns are consistant.
+        """
+        testFileA = File(lfn = "/this/is/a/lfnA", size = 1024,
+                         events = 20, run = 1, lumi = 45)
+        testFileB = File(lfn = "/this/is/a/lfnB", size = 1024,
+                         events = 20, run = 1, lumi = 45)
+        testFileC = File(lfn = "/this/is/a/lfnC", size = 1024,
+                         events = 20, run = 1, lumi = 45)
+        testFileA.create()
+        testFileB.create()
+        testFileC.create()
+
+        testFilesetA = Fileset(name = "TestFileset")
+        testFilesetA.create()
+        
+        testFilesetA.addFile(testFileA)
+        testFilesetA.addFile(testFileB)
+        testFilesetA.addFile(testFileC)
+        testFilesetA.commit()
+
+        filesetFiles = testFilesetA.getFiles(type = "list")
+
+        goldenFiles = [testFileA, testFileB, testFileC]
+        for filesetFile in filesetFiles:
+            assert filesetFile in goldenFiles, \
+                   "ERROR: Unknown file in fileset"
+            goldenFiles.remove(filesetFile)
+
+        assert len(goldenFiles) == 0, \
+               "ERROR: Not all files in fileset"
+
+        filesetFiles = testFilesetA.getFiles(type = "set")
+
+        goldenFiles = [testFileA, testFileB, testFileC]
+        for filesetFile in filesetFiles:
+            assert filesetFile in goldenFiles, \
+                   "ERROR: Unknown file in fileset"
+            goldenFiles.remove(filesetFile)
+
+        assert len(goldenFiles) == 0, \
+               "ERROR: Not all files in fileset"
+
+        filesetLFNs = testFilesetA.getFiles(type = "lfn")
+
+        goldenLFNs = [testFileA["lfn"], testFileB["lfn"], testFileC["lfn"]]
+        for filesetLFN in filesetLFNs:
+            assert filesetLFN in goldenLFNs, \
+                   "ERROR: Unknown lfn in fileset"
+            goldenLFNs.remove(filesetLFN)
+
+        assert len(goldenLFNs) == 0, \
+               "ERROR: Not all lfns in fileset"
+
+        filesetIDs = testFilesetA.getFiles(type = "id")
+
+        goldenIDs = [testFileA["id"], testFileB["id"], testFileC["id"]]
+        for filesetID in filesetIDs:
+            assert filesetID in goldenIDs, \
+                   "ERROR: Unknown id in fileset"
+            goldenIDs.remove(filesetID)
+
+        assert len(goldenIDs) == 0, \
+               "ERROR: Not all ids in fileset"        
+        
+    # test parentage
+    
+if __name__ == "__main__":
+        unittest.main()

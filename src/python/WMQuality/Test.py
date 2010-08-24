507a9a9e5f29dde1a1b43965a8c92ab064fdeb0a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:04:54 +0000

    - Updated Code.py and Test.py to parse cvs files and map test and modules to the actual developers - Created a test-generate which uses the parse methods to generate the conf_test_mysql.py, conf_test_oracle.py and test_style scripts
    These scripts can then be edited (to ensure that the modules/tests
    are not wrapped to the wrong developer), after which the
    scripts can run.
    
    If no editing is needed these scripts can be used for fully automated
    testing and style checking.
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3055 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMQuality/Test.py
+++ src/python/WMQuality/Test.py
@@ -1,8 +1,10 @@
+
+import os
 import unittest
 
 class Test:
 
-    def __init__(self, tests):
+    def __init__(self, tests = []):
         self.tests = tests
 
 
@@ -13,6 +15,123 @@ class Test:
             testSuite.addTest(test[0])
         testResult = unittest.TestResult()
         self.testResult = testSuite.run(testResult)
+
+    def parseCVS(self, cvsLog, pathCut, moduleCut, maxVotes):
+        """
+        Parses a cvs log to information to generate a style quality
+        test.
+        """
+        self.cvsLog = cvsLog
+        # cuts the path from e.g. cvmsserver/repositories/CMSSW/WMCore/src/python/WMCore)
+        # to src/python/WMCore
+        self.pathCut = pathCut
+        # ensures that non relevant modules are not incorporated. e.g.
+        # src/python/WMCore becomes WMCore
+        self.moduleCut = moduleCut
+        # maximum number of authors for voting.
+        self.maxVotes = maxVotes
+        # files used for testing.
+        self.testFile = {}
+
+        logFile = open(self.cvsLog, 'r')
+        nl = logFile.readline()
+        state = 'file'
+        # reset the vote
+        vote = 0
+        # a list on who we should vote
+        curFile = ''
+        while nl:
+            # locate the cvs files
+            if nl.find('RCS file:') == 0:
+                # reset our vote structure
+                vote = 0
+                # filter the file path:
+                path = nl.split(' ')[2].split(',')[0]
+                # check if it is .py file
+                if path.endswith('_t.py'):
+                    # split it and start building modules
+                    parts = path.split('/')
+                    moduleName = ''
+                    # do not include the actual file for style testing
+                    # (only modules)
+                    for index in xrange(0,len(parts)):
+                        # we cut of part of the path
+                        if index > self.pathCut:
+                            moduleName = os.path.join(moduleName, parts[index])
+                            if not self.testFile.has_key(moduleName) and \
+                                index > self.pathCut+self.moduleCut and\
+                                index == (len(parts)-1):
+                                self.testFile[moduleName] = {}
+                                curFile = moduleName
+                    # now we need to find authors and let them vote.
+                    state = 'authors'
+                    # reset the vote
+                vote = 0
+            if nl.find('date:') == 0 and state == 'authors':
+                author = nl.split(' ')[6].split(';')[0]
+                # start voting:
+                if not self.testFile[curFile].has_key(author):
+                    self.testFile[curFile][author] = 0
+                self.testFile[curFile][author] += 1
+                # we voted
+                vote += 1
+                # if we reach maxVotes where done
+                if vote < self.maxVotes:
+                   state = 'file'
+                   vote = 0
+            nl = logFile.readline()
+        # we are done voting
+
+    def generate(self, filename):
+        """
+        Generates a python file that uses the result of parsing
+        and this class to generate a script that does suite tests.
+        """
+        testsFile = open(filename, 'w')
+        head = """#!/usr/bin/env python
+
+from WMQuality.Test import Test
+
+        """
+        testsFile.writelines(head)
+        testsFile.writelines('\n')
+        winners = {}
+        # make the import statements
+        for testFile in self.testFile.keys():
+            # find the one with the most votes per module:
+            votes = 0
+            winner = ''
+            for voter in self.testFile[testFile].keys():
+                if self.testFile[testFile][voter] > votes:
+                    votes = self.testFile[testFile][voter]
+                    winner = voter
+            # make the import:
+            parts = testFile.split('/')
+            importStmt = 'from '
+            for part in xrange(0,len(parts)-1):
+                if part > self.moduleCut:
+                    importStmt +=parts[part]+"."
+            importStmt += parts[-1].split('.')[0]
+            importStmt += ' import '
+            testObject = parts[-1].split('_t.py')[0] + 'Test'
+            importStmt += testObject
+            winners[testObject] = winner
+            testsFile.writelines(importStmt+'\n')
+
+        testsFile.writelines('\n\n')
+        testsFile.writelines('tests = [\\\n')
+        # make the object instantiations.
+        for testObject in winners:
+            testsFile.writelines('     ('+testObject+"(),'"+winners[testObject]+"'),\\\n")
+        testsFile.writelines('    ]\n')
+        tail = """
+test = Test(tests)
+test.run()
+test.summaryText()
+        """
+        testsFile.writelines(tail)
+        testsFile.close()
+ 
  
     def summaryText(self):
         """

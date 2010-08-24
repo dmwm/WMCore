f0a3dabd4ce56984d1bec1e661aa93c581e49119
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:10:43 +0000

    New version of Base_t, with the common dummy objects
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3147 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WMBS_t/Performance_t/Base_t.py
+++ test/python/WMCore_t/WMBS_t/Performance_t/Base_t.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
-import os, unittest, logging, commands, time
+import os, unittest, logging, commands, time, random
 
 
 from unittest import TestCase
 from WMCore.Database.DBCore import DBInterface
 from WMCore.Database.DBFactory import DBFactory
 from WMCore.DAOFactory import DAOFactory
+from WMCore.WMBS.File import File
+from WMCore.WMBS.Fileset import Fileset
+from WMCore.WMBS.Job import Job
+from WMCore.WMBS.Subscription import Subscription
+from WMCore.WMBS.JobGroup import JobGroup
+from WMCore.WMBS.Workflow import Workflow
+from sets import Set
 
 class Base_t():
     """
@@ -21,10 +28,8 @@ class Base_t():
         Common setUp for all Performance tests
 
         """
-        #Place common execute method arguments here        
-        self.baseexec=' '
-        
-        self.sename='localhost'        
+
+        #Setting up logger
         logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
@@ -32,29 +37,167 @@ class Base_t():
                     filemode='w')
         
         self.logger = logging.getLogger('DBPerformanceTest')
+
+        #Place common execute method arguments here        
+        self.baseexec=' '
+
+        self.sename='localhost'        
         
         self.tearDown()
         
         self.DBList = ['MySQL','SQLite']
-        mysqlURI = 'mysql://metson@localhost/wmbs'
-        sqliteURI = 'sqlite:///dbperftest.lite'
+        mysqlURI = 'mysql://jcg@localhost/wmbs'
+#        sqliteURI = 'sqlite:///dbperftest.lite'
 
         self.mysqldbf = DBFactory(self.logger, mysqlURI)
-        self.sqlitedbf = DBFactory(self.logger, sqliteURI)        
+#        self.sqlitedbf = DBFactory(self.logger, sqliteURI)        
 
         self.mysqldao = DAOFactory(package='WMCore.WMBS', logger=self.logger, 
                         dbinterface=self.mysqldbf.connect())
-        self.sqlitedao = DAOFactory(package='WMCore.WMBS', logger=self.logger, 
-                        dbinterface=self.sqlitedbf.connect())        
+#        self.sqlitedao = DAOFactory(package='WMCore.WMBS', logger=self.logger, 
+#                        dbinterface=self.sqlitedbf.connect())        
         
         assert self.mysqldao(classname='CreateWMBS').execute()
 
-        assert self.sqlitedao(classname='CreateWMBS').execute()
+#        assert self.sqlitedao(classname='CreateWMBS').execute()
+
+        # Create a File to be used as argument for the performance test
+        file_lfn = '/tmp/file/fileexample'
+        file_events = 1111
+        file_size = 1111
+        file_run = 111
+        file_lumi = 0
+        
+        #Create File - mySQL
+        self.testmysqlFile = File(lfn=file_lfn, size=file_size, events=file_events, run=file_run,
+                    lumi=file_lumi, logger=self.logger, dbfactory=self.mysqldbf)
+        self.testmysqlFile.save()
+
+        #Create File - SQLite
+#        self.testsqliteFile = File(lfn=file_lfn, size=file_size, events=file_events, run=file_run,
+#                    lumi=file_lumi, logger=self.logger, dbfactory=self.sqlitedbf)
+#        self.testsqliteFile.save()
+
+
+        # Create a Fileset of random, parentless, childless, unlocatied file
+        mysqlfilelist = []
+#        sqlitefilelist = []
+
+        #Generating Files - mySQL DBFactory        
+        for x in range(random.randint(1000,3000)):
+            file = File(lfn='/store/data/%s/%s/file.root' % (random.randint(1000, 9999), 
+                                                  random.randint(1000, 9999)),
+                        size=random.randint(1000, 2000),
+                        events = 1000,
+                        run = random.randint(0, 2000),
+                        lumi = random.randint(0, 8), 
+                        logger=self.logger, 
+                        dbfactory=self.mysqldbf)
+            
+            mysqlfilelist.append(file)
+
+        #Generating Files - SQLite DBFactory        
+#        for x in range(random.randint(1000,3000)):
+#            file = File(lfn='/store/data/%s/%s/file.root' % (random.randint(1000, 9999), 
+#                                                  random.randint(1000, 9999)),
+#                        size=random.randint(1000, 2000),
+#                        events = 1000,
+#                        run = random.randint(0, 2000),
+#                        lumi = random.randint(0, 8), 
+#                        logger=self.logger, 
+#                        dbfactory=self.sqlitedbf)
+#            
+#            sqlitefilelist.append(file)
+    
+        #Creating mySQL Fileset        
+        self.testmysqlFileset = Fileset(name='testFileSet', 
+                            files=mysqlfilelist, 
+                            logger=self.logger, 
+                            dbfactory=self.mysqldbf) 
+        self.testmysqlFileset.create()     
+
+        #Creating SQLite Fileset        
+#        self.testsqliteFileset = Fileset(name='testFileSet', 
+#                            files=sqlitefilelist, 
+#                            logger=self.logger, 
+#                            dbfactory=self.sqlitedbf) 
+#        self.testsqliteFileset.create()     
+
+        #Creating mySQL Workflow
+        self.testmysqlWorkflow = Workflow(spec='Test', owner='PerformanceTestCase', name='Test_mysqlWorkflow', logger=self.logger, dbfactory=self.mysqldbf)
+        self.testmysqlWorkflow.create()
+
+        #Creating SQLite Workflow
+#        self.testsqliteWorkflow = Workflow(spec='Test', owner='PerformanceTestCase', name='Test_mysqlWorkflow', logger=self.logger, dbfactory=self.sqlitedbf)
+#        self.testsqliteWorkflow.create()
+
+        #Creating MySQL Subscription
+        self.testmysqlSubscription = Subscription(fileset=self.testmysqlFileset, 
+                        workflow=self.testmysqlWorkflow, logger=self.logger, 
+                        dbfactory=self.mysqldbf)
+        self.testmysqlSubscription.create()
+
+        #Creating SQLite Subscription
+#        self.testsqliteSubscription = Subscription(fileset=self.testsqliteFileset, 
+#                        workflow=self.testsqliteWorkflow, logger=self.logger, 
+#                        dbfactory=self.sqlitedbf)
+#        self.testsqliteSubscription.create()
+
+        #Instatiating mySQL Job
+        self.testmysqlJob = Job(name='TestmysqlJob',files=self.testmysqlFileset, logger=self.logger, dbfactory=self.mysqldbf)
+
+        #Instatiating SQLite Job
+#        self.testsqliteJob = Job(name='TestsqliteJob',files=self.testsqliteFileset, logger=self.logger, dbfactory=self.sqlitedbf)
+
+        #Creating mySQL JobGroup
+        testsetmysql = Set()
+        testsetmysql.add(self.testmysqlJob)
+        self.testmysqlJobGroup = JobGroup(subscription=self.testmysqlSubscription, jobs=testsetmysql)
+
+        #Creating SQLite JobGroup
+#        testsetsqlite = Set()
+#        testsetsqlite.add(self.testsqliteJob)
+#        self.testsqliteJobGroup = JobGroup(subscription=self.testsqliteSubscription, jobs=testsetsqlite)
+
+        #Creating mySQL Job for testing
+        self.testmysqlJob.create(group=self.testmysqlJobGroup.id)
+
+        #Creating SQLite Job for testing
+#        self.testsqliteJob.create(group=self.testsqliteJobGroup.id)
+
+        #Setting the available SEs        
+        self.sename='localhost'        
+
+        #Assuring the database is reinitialized        
+        self.tearDown()
+        
+        self.DBList = ['MySQL','SQLite']
+        mysqlURI = 'mysql://jcg@localhost/wmbs'
+#        sqliteURI = 'sqlite:///dbperftest.lite'
+
+        #Creating the DB Factories and DAOs
+        self.mysqldbf = DBFactory(self.logger, mysqlURI)
+#        self.sqlitedbf = DBFactory(self.logger, sqliteURI)        
+
+        self.mysqldao = DAOFactory(package='WMCore.WMBS', logger=self.logger, 
+                        dbinterface=self.mysqldbf.connect())
+#        self.sqlitedao = DAOFactory(package='WMCore.WMBS', logger=self.logger, 
+#                        dbinterface=self.sqlitedbf.connect())        
+        
+        assert self.mysqldao(classname='CreateWMBS').execute()
+
+#        assert self.sqlitedao(classname='CreateWMBS').execute()
+
+        #Creating the Locations at the Database
+        self.selist = ['localhost']        
+        for se in self.selist:
+            self.mysqldao(classname='Locations.New').execute(sename=se)
+            #self.sqlitedao(classname='Locations.New').execute(sename=se)      
 
     def tearDown(self):
         #Base tearDown method for the DB Performance test
-        self.logger.debug(commands.getstatusoutput('echo yes | mysqladmin -u root drop wmbs'))
-        self.logger.debug(commands.getstatusoutput('mysqladmin -u root create wmbs'))
+        self.logger.debug(commands.getstatusoutput('echo yes | mysqladmin -u jcg drop wmbs'))
+        self.logger.debug(commands.getstatusoutput('mysqladmin -u jcg create wmbs'))
         self.logger.debug("WMBS MySQL database deleted")
         try:
             self.logger.debug(os.remove('dbperftest.lite'))

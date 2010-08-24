55ec2bc152c2a85f0afc54397cfd3194a720a774
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:49:35 +0000

    Don't need to override SQL
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2800 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Subscriptions/GetAvailableFiles.py
+++ src/python/WMCore/WMBS/SQLite/Subscriptions/GetAvailableFiles.py
@@ -8,18 +8,11 @@ Return a list of files that are available for processing.
 Available means not acquired, complete or failed.
 """
 __all__ = []
-__revision__ = "$Id: GetAvailableFiles.py,v 1.2 2008/07/21 14:27:06 metson Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: GetAvailableFiles.py,v 1.3 2008/07/21 14:28:18 metson Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.WMBS.SQLite.Base import SQLiteBase
 from WMCore.WMBS.MySQL.Subscriptions.GetAvailableFiles import GetAvailableFiles as GetAvailableFilesMySQL
 
 class GetAvailableFiles(GetAvailableFilesMySQL, SQLiteBase):
-    sql = """select file from wmbs_fileset_files where
-        fileset = (select fileset from wmbs_subscription where id=:subscription)
-        and file not in 
-            (select file from wmbs_sub_files_acquired where subscription=:subscription)
-        and file not in 
-            (select file from wmbs_sub_files_failed where subscription=:subscription)
-        and file not in 
-            (select file from wmbs_sub_files_complete where subscription=:subscription)"""
+    sql = GetAvailableFilesMySQL.sql

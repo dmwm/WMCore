a84876666544afc7f39e34a8b9b58b65e6905a4b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:54:39 +0000

    Generalized factory object. This object has not yet a test, as the pythonpath root for tests and src files is the same and this class depends on dynamic imports which would lead to import conflicts.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2886 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMFactory.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+"""
+A factory Class that is 'not thread safe' but is intended to work in threads
+(no sharing). The class dynamically loads objects from files when needed and 
+caches them (or not). It is a generalized factory object. If needed this class
+can be made threadsafe.
+"""
+
+__revision__ = "$Id: WMFactory.py,v 1.1 2008/08/22 13:20:10 fvlingen Exp $"
+__version__ = "$Revision: 1.1 $"
+__author__ = "fvlingen@caltech.edu"
+
+#FIXME: need a test for this. But this can only be done if 
+#FIXME: ../test/python/WMCORE is moved to ../test/python/WMCORE_t 
+#FIXME: to prevent similar inclusion roots for the pythonpath.
+
+import logging
+import threading
+
+from WMCore.WMException import WMException
+from WMCore.WMExceptions import WMEXCEPTION 
+
+class WMFactory:
+    """
+    A factory Class that is 'not thread safe' but is intended to work in 
+    threads (no sharing). The class dynamically loads objects from files 
+    when needed and caches them.
+    """
+
+    def __init__(self, name, namespace):
+        """
+        Initializes the factory, and checks if this thread already
+        has an attribute for storing registries. It uses the reserved 
+        'registries' attribute in the thread.
+        """
+        self.namespace = namespace 
+        self.objectList = {}
+        msg = """
+Creating factory with name: %s associated to 
+namespace (package): %s """ % (name, str(namespace))
+        logging.debug(msg)
+        myThread = threading.currentThread()
+        if not hasattr(myThread, "factory"):
+            myThread.factory = {}
+        myThread.factory[name] = self 
+
+    def loadObject(self, classname, args = None, storeInCache = True, \
+        getFromCache = False):
+        """
+        Dynamically loads the object from file.        
+        For this to work the class name has to 
+        be the same as the file name (minus the .py)
+        
+        Be default objects are loaded from cache. However if you 
+        want several different instances of the same object in one
+        thread, you set cache to False.
+        """
+        if getFromCache:
+            logging.debug("Check cache")
+            if self.objectList.has_key(classname):
+                logging.debug("Object in cache")
+                return self.objectList[classname]
+            logging.debug("Not in cache")
+        logging.debug("Searching class name: "+ classname)
+        try:
+          
+            module = "%s.%s" % (self.namespace, classname)
+            module = __import__(module, globals(), locals(), [classname])
+            obj = getattr(module, classname.split('.')[-1])
+            if args == None:
+                classinstance = obj()
+            else:  
+                classinstance = obj(args)
+            if storeInCache:
+                self.objectList[classname] = classinstance
+            logging.debug("Created instance for class: "+classname)
+            return classinstance
+        except Exception,ex:
+            raise WMException(WMEXCEPTION['WMCORE-4'] + str(ex), 'WMCORE-4')

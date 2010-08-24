ebfc1aa42530782c5efd74a102b2db605f06c50d
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:39:56 +0000

    Cleaned code a bit and added in CVS header
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2641 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/FilesetParentageSQL.py
+++ src/python/WMCore/WMBS/SQLite/FilesetParentageSQL.py
@@ -1,8 +1,12 @@
 """
-MySQL implementation of FilesetParentage
+_FilesetParentage_
+MySQL implementation of FilesetParentage 
 """
-from WMCore.WMBS.MySQL.Base import MySQLBase
 
+__revision__ = "$Id: FilesetParentageSQL.py,v 1.2 2008/06/02 23:58:52 metson Exp $"
+__version__ = "$Revision: 1.2 $"
+
+from WMCore.WMBS.MySQL.Base import MySQLBase
 from WMCore.WMBS.MySQL.FilesetParentageSQL import FilesetParentage as FilesetParentageMySQL
 
 class FilesetParentage(FilesetParentageMySQL, SQLiteBase):

9cde47ccb3a92c6f9932a23065d65c47c30e5283
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:07:10 +0000

    Begining the sqlite support
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3092 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/SQLite/Jobs/Active.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+_Active_
+MySQL implementation of Jobs.Active
+
+move file into wmbs_group_job_acquired
+"""
+__all__ = []
+__revision__ = "$Id: Active.py,v 1.1 2008/10/01 21:34:25 metson Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class Active(MySQLBase):
+    sql = """insert into wmbs_group_job_acquired (job, jobgroup) 
+        values (select id, jobgroup from wmbs_job where id = :job)"""
+        
+    def execute(self, job=0, conn = None, transaction = False):
+        binds = self.getBinds(job=job)
+        self.logger.debug('Job.Active sql: %s' % self.sql)
+        self.logger.debug('Job.Active binds: %s' % binds)
+        
+        return self.format(self.dbi.processData(self.sql, binds))

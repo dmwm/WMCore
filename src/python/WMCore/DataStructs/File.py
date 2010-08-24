9fc3b38d3ed8915b4f843e06de77aaeb3e162bec
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:02:57 +0000

    Switch to inheriting from dict, should be backwards compatible
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3024 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/DataStructs/File.py
+++ src/python/WMCore/DataStructs/File.py
@@ -6,31 +6,33 @@ Data object that contains details for a single file
 
 """
 __all__ = []
-__revision__ = "$Id: File.py,v 1.7 2008/09/09 16:52:54 metson Exp $"
-__version__ = "$Revision: 1.7 $"
+__revision__ = "$Id: File.py,v 1.8 2008/09/19 16:28:19 metson Exp $"
+__version__ = "$Revision: 1.8 $"
 from sets import Set
 from WMCore.DataStructs.WMObject import WMObject 
-class File(WMObject):
+class File(WMObject, dict):
     """
     _File_
     Data object that contains details for a single file
     """
     def __init__(self, lfn='', size=0, events=0, run=0, lumi=0, parents=Set()):
-        self.dict = {}
-        self.dict["lfn"] = lfn
-        self.dict["size"] = size
-        self.dict["events"] = events
-        self.dict["run"] = run
-        self.dict["lumi"] = lumi
-        self.dict["parents"] = parents
-        self.dict['locations'] = Set()
-    
+        dict.__init__(self)
+        self.setdefault("lfn", lfn)
+        self.setdefault("size", size)
+        self.setdefault("events", events)
+        self.setdefault("run", run)
+        self.setdefault("lumi", lumi)
+        self.setdefault("parents", parents)
+        self.setdefault('locations', Set())
+        self.dict = self
+        
     def load(self):
         """
         A DataStructs file has nothing to load from, other implementations will
         over-ride this method.
         """
         pass
+    
     def save(self): 
         """
         A DataStructs file has nothing to save to, other implementations will
@@ -39,16 +41,16 @@ class File(WMObject):
         pass   
     
     def setLocation(self, se):
-        self.dict['locations'] = self.dict['locations'] | Set(self.makelist(se))
+        self['locations'] = self['locations'] | Set(self.makelist(se))
+        self.dict = self
 
     def __cmp__(self, rhs):
         """
         Sort files in run number and lumi section order
         """
-        if self.dict['run'] == rhs.dict['run']:
-            return cmp(self.dict['lumi'], rhs.dict['lumi'])
-        return cmp(self.dict['run'], rhs.dict['run'])
-    
+        if self['run'] == rhs.dict['run']:
+            return cmp(self['lumi'], rhs.dict['lumi'])
+        return cmp(self['run'], rhs.dict['run'])
     
     def __eq__(self, rhs):
         """
@@ -56,22 +58,22 @@ class File(WMObject):
         """
         eq = False
         if type(rhs) == type(self):
-            eq = self.dict['lfn'] == rhs.dict['lfn'] 
-            eq = eq and self.dict['size'] == rhs.dict['size']
-            eq = eq and self.dict['events'] == rhs.dict['events']
-            eq = eq and self.dict['run'] == rhs.dict['run']
-            eq = eq and self.dict['lumi'] == rhs.dict['lumi']
+            eq = self['lfn'] == rhs.dict['lfn'] 
+            eq = eq and self['size'] == rhs.dict['size']
+            eq = eq and self['events'] == rhs.dict['events']
+            eq = eq and self['run'] == rhs.dict['run']
+            eq = eq and self['lumi'] == rhs.dict['lumi']
         elif type(rhs) == type('string'):
-            eq = self.dict['lfn'] == rhs
+            eq = self['lfn'] == rhs
         return eq
     
     def __ne__(self, rhs):
         return not self.__eq__(rhs)
     
     def __hash__(self):
-        hash = self.dict['lfn'].__hash__() 
-        hash = hash + self.dict['size'].__hash__()
-        hash = hash + self.dict['events'].__hash__()
-        hash = hash + self.dict['run'].__hash__()
-        hash = hash + self.dict['lumi'].__hash__()
+        hash = self['lfn'].__hash__() 
+        hash = hash + self['size'].__hash__()
+        hash = hash + self['events'].__hash__()
+        hash = hash + self['run'].__hash__()
+        hash = hash + self['lumi'].__hash__()
         return hash
--- src/python/WMCore/WMBS/File.py
+++ src/python/WMCore/WMBS/File.py
@@ -6,8 +6,8 @@ A simple object representing a file in WMBS
 
 """
 
-__revision__ = "$Id: File.py,v 1.20 2008/09/18 20:42:14 metson Exp $"
-__version__ = "$Revision: 1.20 $"
+__revision__ = "$Id: File.py,v 1.21 2008/09/19 16:29:14 metson Exp $"
+__version__ = "$Revision: 1.21 $"
 
 from WMCore.WMBS.BusinessObject import BusinessObject
 from WMCore.DataStructs.File import File as WMFile
@@ -21,28 +21,30 @@ class File(BusinessObject, WMFile):
     A simple object representing a file in WMBS
     """
     def __init__(self, lfn='', id=-1, size=0, events=0, run=0, lumi=0,
-                 parents=Set(), locations=Set(), logger=None, dbfactory=None):
+                 parents=Set(), locations=None, logger=None, dbfactory=None):
         BusinessObject.__init__(self, logger=logger, dbfactory=dbfactory)
         WMFile.__init__(self, lfn=lfn, size=size, events=events, run=run, lumi=lumi, parents=parents)
         """
         Create the file object
         """
-        self.dict["id"] = int(id)
-        self.dict["locations"] = locations
+        self.setdefault("id", int(id))
+        if locations != None:
+            self.setdefault("locations", locations)
+        self.dict = self
         
     def getInfo(self):
         """
         Return the files attributes as a tuple
         """
-        return self.dict['lfn'], self.dict['id'], self.dict['size'], self.dict['events'], self.dict['run'], \
-                                    self.dict['lumi'], list(self.dict['locations']), list(self.dict['parents'])
+        return self['lfn'], self['id'], self['size'], self['events'], self['run'], \
+                                    self['lumi'], list(self['locations']), list(self['parents'])
                                     
     def getParentLFNs(self):
         """
         get a flat list of parent LFN's
         """
         result = []
-        parents = self.dict['parents']
+        parents = self['parents']
         while parents:
             result.extend(parents)
             temp = []
@@ -50,33 +52,33 @@ class File(BusinessObject, WMFile):
                 temp.extend(parent.dict["parents"])
             parents = temp
         result.sort()   # ensure SecondaryInputFiles are in order
-        return [x.dict['lfn'] for x in result]
+        return [x['lfn'] for x in result]
     
     def load(self, parentage=0):
         """
         use lfn to load file info from db
         """
         result = None 
-        if self.dict['id'] > 0:
-            result = self.daofactory(classname='Files.GetByID').execute(self.dict['id'])
+        if self['id'] > 0:
+            result = self.daofactory(classname='Files.GetByID').execute(self['id'])
         else:
-            result = self.daofactory(classname='Files.GetByLFN').execute(self.dict['lfn'])
-        self.dict['id'] = result[0]
-        self.dict['lfn'] = result[1]
-        self.dict['size'] = result[2]
-        self.dict['events'] = result[3]
-        self.dict['run'] = result[4]
-        self.dict['lumi'] = result[5]
+            result = self.daofactory(classname='Files.GetByLFN').execute(self['lfn'])
+        self['id'] = result[0]
+        self['lfn'] = result[1]
+        self['size'] = result[2]
+        self['events'] = result[3]
+        self['run'] = result[4]
+        self['lumi'] = result[5]
         
-        self.dict['locations'] = self.daofactory(classname='Files.GetLocation').execute(self.dict['lfn']) 
+        self['locations'] = self.daofactory(classname='Files.GetLocation').execute(self['lfn']) 
         
-        self.dict['parents'] = Set()
+        self['parents'] = Set()
         
         if parentage > 0:
-            for lfn in self.daofactory(classname='Files.GetParents').execute(self.dict['lfn']):
-                self.dict['parents'].add( \
+            for lfn in self.daofactory(classname='Files.GetParents').execute(self['lfn']):
+                self['parents'].add( \
                         File(lfn=lfn, logger=self.logger, dbfactory=self.dbfactory).load(parentage=parentage-1))
-        
+        self.dict = self
         return self
     
     def save(self):
@@ -86,19 +88,19 @@ class File(BusinessObject, WMFile):
         trans = Transaction(dbinterface = self.dbfactory.connect())
         try:
             try:
-                self.daofactory(classname='Files.Add').execute(files=self.dict['lfn'], 
-                                                       size=self.dict['size'], 
-                                                       events=self.dict['events'],
+                self.daofactory(classname='Files.Add').execute(files=self['lfn'], 
+                                                       size=self['size'], 
+                                                       events=self['events'],
                                                        conn = trans.conn, 
                                                        transaction = True)
             except IntegrityError, e:
-                self.logger.exception('File %s exists' % (self.dict['lfn']))
+                self.logger.exception('File %s exists' % (self['lfn']))
             except Exception, e:
                 raise e
             try:
-                self.daofactory(classname='Files.AddRunLumi').execute(files=self.dict['lfn'],  
-                                                       run=self.dict['run'], 
-                                                       lumi=self.dict['lumi'],
+                self.daofactory(classname='Files.AddRunLumi').execute(files=self['lfn'],  
+                                                       run=self['run'], 
+                                                       lumi=self['lumi'],
                                                        conn = trans.conn, 
                                                        transaction = True)
             except IntegrityError, e:
@@ -109,14 +111,12 @@ class File(BusinessObject, WMFile):
         except Exception, e:
             trans.rollback()
             raise e
-        
-        #self.load()
     
     def delete(self):
         """
         Remove a file from WMBS
         """
-        self.daofactory(classname='Files.Delete').execute(file=self.dict['lfn'])
+        self.daofactory(classname='Files.Delete').execute(file=self['lfn'])
         
     def addChild(self, lfn):
         """
@@ -124,12 +124,12 @@ class File(BusinessObject, WMFile):
         """
         child = File(lfn=lfn, logger=self.logger, dbfactory=self.dbfactory)
         child.load()
-        if not self.dict['id'] > 0:
-            raise Exception, "Parent file doesn't have an id %s" % self.dict['lfn']
+        if not self['id'] > 0:
+            raise Exception, "Parent file doesn't have an id %s" % self['lfn']
         if not child.dict['id'] > 0:
             raise Exception, "Child file doesn't have an id %s" % child.lfn
         
-        self.daofactory(classname='Files.Heritage').execute(child=child.dict['id'], parent=self.dict['id'])
+        self.daofactory(classname='Files.Heritage').execute(child=child.dict['id'], parent=self['id'])
         
     def addParent(self, lfn):
         """
@@ -137,14 +137,16 @@ class File(BusinessObject, WMFile):
         """
         parent = File(lfn=lfn, logger=self.logger, dbfactory=self.dbfactory)
         parent.load()
-        self.dict['parents'].add(parent)
-        if not self.dict['id'] > 0:
-            raise Exception, "Child file doesn't have an id %s" % self.dict['lfn']
+        self['parents'].add(parent)
+        if not self['id'] > 0:
+            raise Exception, "Child file doesn't have an id %s" % self['lfn']
         if not parent.dict['id'] > 0:
             raise Exception, "Parent file doesn't have an id %s" % parent.dict['lfn']
         
-        self.daofactory(classname='Files.Heritage').execute(child=self.dict['id'], parent=parent.dict['id'])
-
+        self.daofactory(classname='Files.Heritage').execute(child=self['id'], parent=parent.dict['id'])
+        self.dict = self
+        
     def setLocation(self, se):
-        self.daofactory(classname='Files.SetLocation').execute(file=self.dict['lfn'], sename=se)
-        self.dict['locations'] = self.daofactory(classname='Files.GetLocation').execute(self.dict['lfn']) 
+        self.daofactory(classname='Files.SetLocation').execute(file=self['lfn'], sename=se)
+        self['locations'] = self.daofactory(classname='Files.GetLocation').execute(self['lfn']) 
+        self.dict = self

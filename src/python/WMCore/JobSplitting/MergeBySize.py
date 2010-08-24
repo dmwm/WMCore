30285c67ac1885614350e221ffb52ef627349b5e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:03:52 +0000

    Add Merging based on size job splitting
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3039 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/JobSplitting/MergeBySize.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+"""
+_MergeBySize_
+
+"Splitting" algorithm to combine a fileset into merge job definitions
+based on the size of the files
+
+"""
+
+__revision__ = "$Id: MergeBySize.py,v 1.1 2008/09/22 10:02:54 evansde Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from sets import Set
+from WMCore.JobSplitting.JobFactory import JobFactory
+
+
+
+class MergeBySize(JobFactory):
+    """
+    _MergeBySize_
+
+    Process a fileset to generate merge jobs based on size.
+    Files will be combined based on size,  grouped by
+    run,and sorted by lumisection
+
+    an option is provided to include all files in overflow jobs
+
+
+    """
+    def algorithm(self, job_instance = None, *args, **kwargs):
+        """
+        _algorithm_
+
+        Implement merge algorithm for the subscription provided
+
+        """
+        jobs = Set()
+
+        fileset = list(self.subscription.availableFiles())
+
+        mergeSize = kwargs['merge_size']
+        overflow  = kwargs.get('all_files', False)
+        fileset.sort()
+
+        accumSize = 0
+        accumFiles = []
+        for f in fileset:
+            accumSize += f['size']
+            accumFiles.append(f)
+            if accumSize >= mergeSize:
+                job  = job_instance(subscription = self.subscription)
+                job.addFile(accumFiles)
+                job.mask.setMaxAndSkipEvents(-1, 0)
+                jobs.add(job)
+                accumSize = 0
+                accumFiles = []
+
+        if len(accumFiles) > 0:
+            if overflow:
+                job =  job_instance(subscription = self.subscription)
+                job.addFile(accumFiles)
+                job.mask.setMaxAndSkipEvents(-1, 0)
+                jobs.add(job)
+
+
+        return jobs
+
+
+
+
+
+
+

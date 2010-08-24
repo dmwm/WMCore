215f3be7e96bad3bf8a5862244b4d871ea28f1e3
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:17:01 +0000

    Updated as WMFBS.File contains file parentage - rely on feeder to setup relations where required
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3246 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/JobSplitting/RunBased.py
+++ src/python/WMCore/JobSplitting/RunBased.py
@@ -9,13 +9,10 @@ creation and/or tracking.
 
 If file spans a run will need to create a mask for that file.
 """
-__revision__ = "$Id: RunBased.py,v 1.2 2008/10/23 11:04:17 jacksonj Exp $"
-__version__  = "$Revision: 1.2 $"
-
-
+__revision__ = "$Id: RunBased.py,v 1.3 2008/10/23 15:58:30 jacksonj Exp $"
+__version__  = "$Revision: 1.3 $"
 
 from sets import Set
-from re import match
 from WMCore.JobSplitting.JobFactory import JobFactory
 
 class RunBased(JobFactory):
@@ -24,16 +21,14 @@ class RunBased(JobFactory):
         _algorithm_
         
         Implement run splitting algorithm. Assumes that if a file with a run is
-        present, then all files for that run are also present
+        present, then all files for that run are also present.
+        
+        kwargs can take:
+        files_per_job - e.g. 20 - Number of files per each split job
         """
         # Set default inputs if required
         if 'files_per_job' not in kwargs.keys():
-            kwargs['files_per_job'] = 50
-        primaryRe = ".*"
-        doPrimaryLinkage = False
-        if 'primary_data_tier' in kwargs.keys():
-            primaryRe = "^/store/data/.*/.*/%s/.*$" % kwargs['primary_data_tier']
-            doPrimaryLinkage = True
+            kwargs['files_per_job'] = 300
         logger = None
         dbf = None
         try:
@@ -45,43 +40,38 @@ class RunBased(JobFactory):
         # Resulting job set
         jobs = Set()
         
-        # Get the available FileSet
-        fileset = self.subscription.availableFiles()
+        # Get the available Fileset
+        fileset = list(self.subscription.availableFiles())
         
         # Select all primary files for the first present run
         curRun = None
         primaryFiles = []
         for f in fileset:
-            # Determine if this file is of primary interest
-            if re.match(primaryRe, f['lfn']):
-                # Check file doesn't span runs
-                if len(f['runs']) != 1:
-                    msg = "File %s contains %s runs, should be 1" % \
-                        (f['lfn'], len(f['runs']))
-                    raise RuntimeError, msg
-                # Acquire run of interest if required
-                elif not curRun:
-                    curRun = f['run']
+            # Check file doesn't span runs
+            if len(f['runs']) != 1:
+                msg = "File %s contains %s runs, should be 1" % \
+                    (f['lfn'], len(f['runs']))
+                raise RuntimeError, msg
+            # Acquire run of interest if required
+            elif not curRun:
+                curRun = f['run']
                 
-                # Add file to primary interest list if contains current run
-                if f['run'] == curRun:
-                    primaryFiles.add(f)
-                    
-        # Resolve secondary file linkages
-        secFiles = {}
-        if doPrimaryLinkage:
-            for f in primaryFiles:
-                secFiles[primaryFiles['lfn']] = []
+            # Add file to primary interest list if contains current run
+            if f['run'] == curRun:
+                primaryFiles.add(f)
         
         # Create jobs for the available files, split by number of files
         num_files = len(primaryFiles)
         while num_files > 0:
-            thefiles = Fileset(files=primaryFiles[:kwargs['files_per_job']])
+            # Extract a subset of primary files
+            jobFiles = Fileset(files=primaryFiles[:kwargs['files_per_job']])
             primaryFiles = primaryFiles[kwargs['files_per_job']:]
+            num_files = num_files - len(jobFiles)
+
+            # Create the job
             job = job_instance(name = '%s-%s' % (jobname, len(jobs) + 1), 
-                               files=thefiles, logger=logger, dbfactory=dbf)
+                               files=jobFiles, logger=logger, dbfactory=dbf)
             jobs.add(job)
-            num_files = num_files - len(thefiles)
         
         # Return the jobs
         return jobs

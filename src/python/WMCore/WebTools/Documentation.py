851b0a0dde2601c68815627873679bfc9bfcd3bd
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:36:45 +0000

    Only show pages with templates (i.e. ignore the CVS directory)
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3553 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/Documentation.py
+++ src/python/WMCore/WebTools/Documentation.py
@@ -7,7 +7,8 @@ class Documentation(TemplatedPage):
         templates = listdir(self.templatedir)
         index = "<h1>Documentation</h1>\n<ol>"
         for t in templates:
-            index = "%s\n<li><a href='%s'>%s</a></li>" % (index, 
+            if '.tmpl' in t:
+                index = "%s\n<li><a href='%s'>%s</a></li>" % (index, 
                                                       t.replace('.tmpl', ''), 
                                                       t.replace('.tmpl', ''))
         index = "%s\n<ol>" % (index)

6e0c3fa824ae5a638498d8f41d4cb9db6d57fd62
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:40:22 +0000

    Remove some inaccurate comments.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3611 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Jobs/LoadFromID.py
+++ src/python/WMCore/WMBS/Oracle/Jobs/LoadFromID.py
@@ -6,17 +6,10 @@ Oracle implementation of Jobs.LoadFromID.
 """
 
 __all__ = []
-__revision__ = "$Id: LoadFromID.py,v 1.2 2009/01/12 19:26:04 sfoulkes Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: LoadFromID.py,v 1.3 2009/01/13 17:39:55 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.WMBS.MySQL.Jobs.LoadFromID import LoadFromID as LoadFromIDJobMySQL
 
 class LoadFromID(LoadFromIDJobMySQL):
-    """
-    _LoadFromID_
-
-    Retrieve meta data for a job given it's ID.  This includes the name,
-    job group and last update time.  This will also retrieve the job mask
-    if one exists.
-    """
     sql = LoadFromIDJobMySQL.sql
--- src/python/WMCore/WMBS/Oracle/Jobs/LoadFromName.py
+++ src/python/WMCore/WMBS/Oracle/Jobs/LoadFromName.py
@@ -6,17 +6,10 @@ Oracle implementation of Jobs.LoadFromName.
 """
 
 __all__ = []
-__revision__ = "$Id: LoadFromName.py,v 1.2 2009/01/12 19:26:04 sfoulkes Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: LoadFromName.py,v 1.3 2009/01/13 17:39:55 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.WMBS.MySQL.Jobs.LoadFromName import LoadFromName as LoadFromNameJobMySQL
 
 class LoadFromName(LoadFromNameJobMySQL):
-    """
-    _LoadFromName_
-
-    Retrieve meta data for a job given it's name.  This includes the name,
-    job group and last update time.  This will also retrieve the job mask
-    if one exists.
-    """
     sql = LoadFromNameJobMySQL.sql

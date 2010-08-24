a8273b6a840e5b11616b2f3b05daf36be608d367
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:38:37 +0000

    Fixup the handling of the timestamp and only insert a row into the wmbs_jobgroup table.  Looking up the ID for the nex group is handled by another DAO class.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3583 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/JobGroup/New.py
+++ src/python/WMCore/WMBS/Oracle/JobGroup/New.py
@@ -1,31 +1,19 @@
 #!/usr/bin/env python
 """
 _New_
+
 Oracle implementation of JobGroup.New
 """
+
 __all__ = []
-__revision__ = "$Id: New.py,v 1.1 2008/11/24 21:51:45 sryu Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: New.py,v 1.2 2009/01/11 17:51:39 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
+
+import time
 
 from WMCore.WMBS.MySQL.JobGroup.New import New as NewJobGroupMySQL
 
 class New(NewJobGroupMySQL):
-    """
-    _New_
-    Oracle specific sql: 'uid' is a reserved word in Oracle 
-    """
-    sql = []
-    sql.append("""insert into wmbs_jobgroup (id, subscription, guid, output, last_update) 
-                  values (wmbs_jobgroup_SEQ.nextval, :subscription, 
-                          :guid, :output, :timestamp)""")
-    sql.append("""select id from wmbs_jobgroup where guid=:guid""")
-    
-    def execute(self, uid, subscription=None, output=None, conn = None, transaction = False):
-        binds = self.getBinds(subscription=subscription, guid=uid, output=output, 
-                              timestamp=self.timestamp())
-        self.logger.debug('JobGroup.New sql: %s' % self.sql)
-        self.logger.debug('JobGroup.New binds: %s' % binds)
-        result = self.dbi.processData(self.sql[0], binds)
-        binds = self.getBinds(guid=uid)
-        result = self.dbi.processData(self.sql[1], binds)
-        return self.format(result, uid)
+    sql = """INSERT INTO wmbs_jobgroup (id, subscription, guid, output,
+             last_update) VALUES (wmbs_jobgroup_SEQ.nextval, :subscription,
+             :guid, :output, %d)""" % time.time()

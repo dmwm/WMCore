66811af3dc6903cd4b2b90254397edeecb7734e9
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:53:10 +0000

    Added some more exception handling. Including throwing an exception in case of an error to help operators trouble shoot the problem.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2859 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Database/DBCreator.py
+++ src/python/WMCore/Database/DBCreator.py
@@ -7,13 +7,15 @@ Base class for formatters that create tables.
 
 """
 
-__revision__ = "$Id: DBCreator.py,v 1.1 2008/08/16 05:08:56 fvlingen Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: DBCreator.py,v 1.2 2008/08/18 14:59:34 fvlingen Exp $"
+__version__ = "$Revision: 1.2 $"
 __author__ = "fvlingen@caltech.edu"
 
 
 
 from WMCore.Database.DBFormatter import DBFormatter
+from WMCore.WMException import WMException
+from WMCore.WMExceptions import WMEXCEPTION
 
 
 class DBCreator(DBFormatter):
@@ -42,16 +44,26 @@ class DBCreator(DBFormatter):
         
         Generic method to create tables and constraints by execute
         sql statements in the create, and constraints dictionaries.
-        """
         
-        for i in self.create.keys():
+        Before execution the keys assigned to the tables in the self.create
+        dictionary are sorted, to offer the possibilitiy of executing 
+        table creation in a certain order.
+        """
+        # get the keys for the table mapping:
+        tableKeys = self.create.keys()
+        tableKeys.sort()
+        
+        for i in tableKeys:
             self.logger.debug( i )
             try:
                 self.dbi.processData(self.create[i], 
                                      conn = conn, 
                                      transaction = transaction)
             except Exception, e:
-                self.logger.debug( e )
+                msg = WMEXCEPTION['WMCore-2'] + '\n\n' +\
+                                  str(self.create[i]) +'\n\n' +str(e)
+                self.logger.debug( msg )
+                raise WMException(msg,'WMCore-2')
             
             keys = self.constraints.keys()
             self.logger.debug( keys )

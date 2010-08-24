d5aa48231f6f894c136eef14db17c5c28cc11eac
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:42:00 +0000

    Use DAOFactory instead of loadDialect Generally deprecated
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2672 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Action.py
+++ src/python/WMCore/Action.py
@@ -3,10 +3,16 @@ A basic action is a thing that will run a SQL statement
 
 A more complex one would be something that ran multiple SQL 
 objects to produce a single output.
+
+This class should generally not be used unless you know what 
+you are doing. DAOFactory is the preferred method for 
+accessing WM DAO classes.
+
 """
 from WMCore.Database.Dialects import MySQLDialect
 from WMCore.Database.Dialects import SQLiteDialect
 from WMCore.Database.Dialects import OracleDialect
+from WMCore.DAOFactory import DAOFactory
 
 class BaseAction(object):
     name = "BaseAction"
@@ -14,34 +20,9 @@ class BaseAction(object):
         self.package = package
         self.logger = logger
         self.logger.debug("Instantiating %s Action object" % self.name)
-        self.dialects = {"Oracle" : OracleDialect,
-                    "MySQL" : MySQLDialect,
-                    "SQLite" : SQLiteDialect}
-    
-    def loadDialect(self, classname, dbinterface):
-        """
-        Somewhat fugly method to load generic SQL classes...
-        """
-        
-        dia = dbinterface.engine.dialect
-        #TODO: Make good
-        dialect = None
-        for i in self.dialects.keys():
-            if isinstance(dia, self.dialects[i]): 
-                dialect = i
-        if not dialect: 
-            raise TypeError, "unknown connection type: %s" % dia
-        
-        module = "%s.%s.%sSQL" % (self.package, dialect, classname)
-        self.logger.debug("importing %s, %s" % (module, classname))
-        module = __import__(module, globals(), locals(), [classname], -1)
-        instance = getattr(module, classname.split('.')[-1])
-        return instance
-
-    
+       
     def execute(self, dbinterface = None):
-        myclass = self.loadDialect(self.name, dbinterface)
-        action = myclass(self.logger, dbinterface)
+        action = DAOFactory(package=self.package, logger=self.logger, dbinterface=dbinterface)
         try:
             return action.execute()
         except Exception, e:
@@ -56,8 +37,7 @@ class BoundAction(BaseAction):
     def execute(self, **kwargs):
         if not 'dbinterface' in kwargs.keys():
             raise ValueError, "You must pass a dbinterface to a BoundAction's execute method"
-        myclass = self.loadDialect(self.name, dbinterface)
-        action = myclass(self.logger, dbinterface)
+        action = DAOFactory(package=self.package, logger=self.logger, dbinterface=kwargs['dbinterface'])
         try:
             return action.execute(kwargs)
         except Exception, e:

5fe2ae2e640eb877ec7dbd8922cb016bf16e9a2f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:05:02 +0000

    Subclass the GetByID object (which does the same thing with different SQL) instead of maintaining it in both classes
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3057 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Files/GetByLFN.py
+++ src/python/WMCore/WMBS/MySQL/Files/GetByLFN.py
@@ -1,34 +1,11 @@
 """
 MySQL implementation of File.Get
 """
-from WMCore.WMBS.MySQL.Base import MySQLBase
+from WMCore.WMBS.MySQL.Files import GetByID
 
-class GetByLFN(MySQLBase):
+class GetByLFN(GetByID):
     sql = """select file.id, file.lfn, file.size, file.events, map.run, map.lumi
              from wmbs_file_details as file join wmbs_file_runlumi_map as map on map.file = file.id 
              where lfn = :file"""
     #select id, lfn, size, events, run, lumi from wmbs_file_details where id = :file
     
-    def getBinds(self, files=None):
-        binds = []
-        files = self.dbi.makelist(files)
-        for f in files:
-            binds.append({'file': f})
-        return binds
-    
-    def format(self, result):
-        out = result[0].fetchall()
-        self.logger.debug('File.GetByLFN format result: %s' % out)
-        if len(out) > 0:
-            out = out[0]
-            out = int(out[0]), str(out[1]), int(out[2]), int(out[3]), int(out[4]), int(out[5])
-            return out
-        else:
-            raise Exception, "File not found"  
-        
-    def execute(self, files=None, conn = None, transaction = False):
-        binds = self.getBinds(files)
-        
-        result = self.dbi.processData(self.sql, binds, 
-                         conn = conn, transaction = transaction)
-        return self.format(result)

25c9faecc98f2b65a62f5b68e3b2d6ac70c7ed19
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 22:49:42 +0000

    Default to 10 files/job
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@2802 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/JobSplitting/FileBased.py
+++ src/python/WMCore/JobSplitting/FileBased.py
@@ -6,6 +6,8 @@ class FileBased(JobFactory):
     Return a set of jobs split along file boundaries
     """
     def algorithm(self, job_instance=None, *args, **kwargs):
+        if 'files_per_job' not in kwargs.keys():
+            kwargs['files_per_job'] = 10
         jobs = Set()
         while len(self.subscription.availableFiles()) > 0:
             job = job_instance(self.subscription, self.subscription.acquireFiles(size=kwargs['files_per_job']))

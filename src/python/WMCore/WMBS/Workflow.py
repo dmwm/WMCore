9c864c734736ab58847fcbe9445c94fed02dbcd1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:37:21 +0000

    Add support for lumping together multiple operations in a single transaction.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3563 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Workflow.py
+++ src/python/WMCore/WMBS/Workflow.py
@@ -13,18 +13,15 @@ filesets and subscriptions (e.g. repeating the same task on a
 bunch of data).
 
 workflow + fileset = subscription
-
 """
 
-__revision__ = "$Id: Workflow.py,v 1.15 2008/11/20 16:15:07 sfoulkes Exp $"
-__version__ = "$Revision: 1.15 $"
+__revision__ = "$Id: Workflow.py,v 1.16 2009/01/08 22:47:35 sfoulkes Exp $"
+__version__ = "$Revision: 1.16 $"
 
-import threading
-
+from WMCore.WMBS.WMBSBase import WMBSBase
 from WMCore.DataStructs.Workflow import Workflow as WMWorkflow
-from WMCore.DAOFactory import DAOFactory
 
-class Workflow(WMWorkflow):
+class Workflow(WMBSBase, WMWorkflow):
     """
     A simple object representing a Workflow in WMBS.
 
@@ -38,19 +35,12 @@ class Workflow(WMWorkflow):
     
     workflow + fileset = subscription
     """
-
     def __init__(self, spec = None, owner = None, name = None, id = -1):
+        WMBSBase.__init__(self)
         WMWorkflow.__init__(self, spec = spec, owner = owner, name = name)
 
-        myThread = threading.currentThread()
-        self.logger = myThread.logger
-        self.dialect = myThread.dialect
-        self.dbi = myThread.dbi        
-        self.daofactory = DAOFactory(package = "WMCore.WMBS",
-                                     logger = self.logger,
-                                     dbinterface = self.dbi)
-        
         self.id = id
+        return
         
     def exists(self):
         """
@@ -58,25 +48,32 @@ class Workflow(WMWorkflow):
         """
         action = self.daofactory(classname = "Workflow.Exists")
         return action.execute(spec = self.spec, owner = self.owner,
-                              name = self.name)
+                              name = self.name, conn = self.getReadDBConn(),
+                              transaction = self.existingTransaction())
     
     def create(self):
         """
         Write a workflow to the database
         """
         action = self.daofactory(classname = "Workflow.New")
-        action.execute(spec = self.spec, owner = self.owner, name = self.name)
+        action.execute(spec = self.spec, owner = self.owner, name = self.name,
+                       conn = self.getWriteDBConn(),
+                       transaction = self.existingTransaction())
+        
         self.id = self.exists()
-
+        self.commitIfNew()
+        return
+    
     def delete(self):
         """
         Remove this workflow from WMBS
         """
-        self.logger.warning(
-        'You are removing the following workflow from WMBS %s (%s) owned by %s'
-                                 % (self.name, self.spec, self.owner))
         action = self.daofactory(classname = "Workflow.Delete")
-        action.execute(id = self.id)
+        action.execute(id = self.id, conn = self.getWriteDBConn(),
+                       transaction = self.existingTransaction())
+
+        self.commitIfNew()
+        return
         
     def load(self, method = "Workflow.LoadFromName"):
         """
@@ -84,15 +81,19 @@ class Workflow(WMWorkflow):
         """
         action = self.daofactory(classname = method)
         if method == "Workflow.LoadFromName":
-            result = action.execute(workflow = self.name)
+            result = action.execute(workflow = self.name,
+                                    conn = self.getReadDBConn(),
+                                    transaction = self.existingTransaction())
         elif method == "Workflow.LoadFromID":
-            result = action.execute(workflow = self.id)
+            result = action.execute(workflow = self.id,
+                                    conn = self.getReadDBConn(),
+                                    transaction = self.existingTransaction())
         elif method == "Workflow.LoadFromSpecOwner":
-            result = action.execute(spec = self.spec, owner = self.owner)
+            result = action.execute(spec = self.spec, owner = self.owner,
+                                    conn = self.getReadDBConn(),
+                                    transaction = self.existingTransaction())                                    
         else:
             raise TypeError, "load method not supported"
-
-        self.logger.debug("RESULT: %s" % result)
 
         self.id = result[0][0]
         self.spec = result[0][1]

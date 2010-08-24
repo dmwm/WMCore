7dd2010dec8a63bf256bb712a8fd489f65a7bee9
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:03:21 +0000

    Minor tweak to make output fileset use the logger reference from self.file_set, since the files arg to the ctor can now be None
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3030 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/DataStructs/Job.py
+++ src/python/WMCore/DataStructs/Job.py
@@ -4,14 +4,14 @@ _Job_
 
 Data object that describes a job
 
-Jobs know their status (active, failed, complete) and know the files they run on 
+Jobs know their status (active, failed, complete) and know the files they run on
 but don't know the subscription or group. They do know their workflow.
 """
 __all__ = []
-__revision__ = "$Id: Job.py,v 1.7 2008/09/19 17:26:19 metson Exp $"
-__version__ = "$Revision: 1.7 $"
+__revision__ = "$Id: Job.py,v 1.8 2008/09/20 12:02:48 evansde Exp $"
+__version__ = "$Revision: 1.8 $"
 
-from WMCore.DataStructs.Pickleable import Pickleable 
+from WMCore.DataStructs.Pickleable import Pickleable
 from WMCore.DataStructs.Fileset import Fileset
 from WMCore.DataStructs.Subscription import Subscription
 from sets import Set
@@ -26,14 +26,14 @@ class Job(Pickleable):
         """
         self.subscription = subscription
         self.workflow = subscription.workflow
-        if files == None: 
+        if files == None:
             self.file_set = Fileset()
         else:
             self.file_set = files
         self.last_update = datetime.datetime.now()
         self.status = 'QUEUED'
         self.name = None
-        self.output = Fileset(name='output', logger=files.logger)
+        self.output = Fileset(name = 'output', logger = self.file_set.logger)
         self.report = None
 
     def listFiles(self):
@@ -41,54 +41,54 @@ class Job(Pickleable):
         return the list of files associated with the job
         """
         return self.file_set.listFiles()
-    
+
     def listLFNs(self):
         """
         return the list of lfns associated with the job
         """
         return self.file_set.listLFNs()
-    
+
     def addFile(self, file):
         """
         add a file to self.file_set
         """
-        self.file_set.addFile(file) 
-            
+        self.file_set.addFile(file)
+
     def addOutput(self, file):
         """
         add a file to self.output
         """
-        self.output.addFile(file) 
-        
+        self.output.addFile(file)
+
     def changeStatus(self, status):
         self.last_update = datetime.datetime.now()
         self.status = status
-        
+
     def submit(self, name):
         """
-        Once submitted to a batch queue set status to active and set the job's 
+        Once submitted to a batch queue set status to active and set the job's
         name to some id from the batch system
         """
         self.name = name
         self.changeStatus('ACTIVE')
-        
+
     def resubmit(self, name):
         """
         Reset the file status to acquired for files associated to this job
         """
-        self.submit(name)        
-    
+        self.submit(name)
+
     def fail(self, report):
         """
-        Job has failed, mark all files associated with it as failed, and store 
+        Job has failed, mark all files associated with it as failed, and store
         the job report
         """
         self.changeStatus('FAILED')
         self.report = report
-    
+
     def complete(self, report):
         """
-        Job has completed successfully, mark all files associated 
+        Job has completed successfully, mark all files associated
         with it as complete, and store the job report
         """
         self.changeStatus('COMPLETE')

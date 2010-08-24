42c13c1666e3491564093fb8cbe0c48311aea6f1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:11:25 +0000

    Fix broken import
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3157 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Files/GetByLFN.py
+++ src/python/WMCore/WMBS/MySQL/Files/GetByLFN.py
@@ -1,11 +1,10 @@
 """
 MySQL implementation of File.Get
 """
-from WMCore.WMBS.MySQL.Files import GetByID
+from WMCore.WMBS.MySQL.Files.GetByID import GetByID
 
 class GetByLFN(GetByID):
     sql = """select file.id, file.lfn, file.size, file.events, map.run, map.lumi
              from wmbs_file_details as file join wmbs_file_runlumi_map as map on map.file = file.id 
              where lfn = :file"""
     #select id, lfn, size, events, run, lumi from wmbs_file_details where id = :file
-    

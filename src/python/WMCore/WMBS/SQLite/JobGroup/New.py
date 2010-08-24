743c8ba1652466499c4b68719509df503d1bbb2d
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-24 23:38:01 +0000

    Don't return the ID of the newly created job group, this is taken care of by another DAO object.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3574 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/JobGroup/New.py
+++ src/python/WMCore/WMBS/SQLite/JobGroup/New.py
@@ -6,12 +6,12 @@ SQLite implementation of JobGroup.New
 """
 
 __all__ = []
-__revision__ = "$Id: New.py,v 1.2 2008/11/21 17:10:28 sfoulkes Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: New.py,v 1.3 2009/01/11 17:40:46 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.WMBS.MySQL.JobGroup.New import New as NewMySQL
 
 class New(NewMySQL):
-    sql = []
-    sql.append("""insert into wmbs_jobgroup (subscription, uid, output, last_update) values (:subscription, :uid, :output, strftime('%s', 'now'))""")
-    sql.append("""select id from wmbs_jobgroup where uid=:uid""")
+    sql = """INSERT INTO wmbs_jobgroup (subscription, uid, output,
+             last_update) VALUES (:subscription, :guid, :output,
+             strftime('%s', 'now'))"""

75b5001257e44dc68da862db0eed199b27b33b28
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:44:55 +0000

    new test: testG_JobRunningJob.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7169 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/BossLite_t/DBObjects_t.py
+++ test/python/WMCore_t/BossLite_t/DBObjects_t.py
@@ -328,6 +328,56 @@ class DBObjectsTest(unittest.TestCase):
         
         
         return
+    
+    
+    def testG_JobRunningJob(self):
+        """
+        Test load/save RunningJob correctly (draft)
+
+        """
+        
+        myThread = threading.currentThread()
+        
+        parameters = {'name': 'Bishop'}
+        task = Task(parameters)
+        
+        # without this nothing works!
+        task.create()
+        
+        parameters = {'name': 'Walter', 'events' : 42 }
+        job = Job( parameters )
+        job.newRunningInstance(  )
+        task.addJob(job)
+        task.save()
+        
+        #print task.jobs
+        #print task.jobLoaded
+        #print task.jobIndex
+        
+        task2 = Task(parameters = {'id': 1})  
+        task2.load()
+        
+        #print task2.jobs
+        #print task2.jobLoaded
+        #print task2.jobIndex
+        
+        self.assertEqual(task.data['name'], task2.data['name'])
+        
+        # 'loadedJob = task2.getJob(1)' doesn't work! 
+        loadedJob = task2.jobs[0]
+        
+        self.assertNotEqual(loadedJob.runningJob, None)
+        
+        loadedJob.runningJob['wrapperReturnCode'] = "60303"
+        
+        loadedJob.updateRunningInstance()
+        
+        loadedJob.closeRunningInstance()
+        
+        # print loadedJob.runningJob
+        
+        
+        return
 
 if __name__ == "__main__":
     unittest.main()

b3fb693317774cb8f1228547eeaff17a066126e6
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:22:23 +0000

    Added new mime types
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6024 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/DASRESTFormatter.py
+++ src/python/WMCore/WebTools/DASRESTFormatter.py
@@ -4,8 +4,8 @@ _DASRESTFormatter_
 
 A REST formatter that appends the DAS headers to the result data
 """
-__revision__ = "$Id: DASRESTFormatter.py,v 1.5 2009/12/27 11:43:10 metson Exp $"
-__version__ = "$Revision: 1.5 $"
+__revision__ = "$Id: DASRESTFormatter.py,v 1.6 2010/01/19 15:54:05 valya Exp $"
+__version__ = "$Revision: 1.6 $"
 
 from WMCore.WebTools.Page import exposedasjson, exposedasxml
 from WMCore.WebTools.Page import exposedasplist
@@ -15,7 +15,9 @@ from WMCore.WebTools.Page import DEFAULT_EXPIRE
 class DASRESTFormatter(RESTFormatter):
     def __init__(self, config):
         RESTFormatter.__init__(self, config)
-        self.supporttypes.update({'text/json+das':self.dasjson})
+        mimes = {'text/json+das':self.dasjson, 'application/xml+das':self.xml,
+                 'application/plist':self.plist}
+        self.supporttypes.update(mimes)
 
     @exposedasjson
     def dasjson(self, data, expires=DEFAULT_EXPIRE):

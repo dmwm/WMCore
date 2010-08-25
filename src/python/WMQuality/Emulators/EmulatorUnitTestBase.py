898d2e0f4372770055ad0b61aaa24154d1aaf03e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:08:38 +0000

    add EmulatorUnitTestBase to use in unit test
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6678 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMQuality/Emulators/EmulatorUnitTestBase.py
@@ -0,0 +1,37 @@
+import unittest
+import os
+import tempfile
+import sys
+
+from WMCore.Configuration import Configuration, saveConfigurationFile
+
+class EmulatorUnitTestBase(unittest.TestCase):
+    
+    def setUp(self):
+        fd, self.configFile = tempfile.mkstemp(".py", "Emulator_Config",)
+        os.environ["EMULATOR_CONFIG"] = self.configFile
+        self.setEmulator()
+        self._emulatorCofig()
+        
+    def tearDown(self):
+        os.remove(self.configFile)
+        print "file deleted: %s" % self.configFile
+        
+    def _emulatorCofig(self):
+        
+        config = Configuration()
+        config.section_("Emulator")
+        config.Emulator.PhEDEx = self.phedexFlag
+        config.Emulator.DBSReader = self.dbsFlag
+        config.Emulator.RequestMgr = self.requestMgrFlag
+        saveConfigurationFile(config, self.configFile)
+        print "create config file PhEDEx: %s, DBS: %s, RequestManager: %s, with flag %s" \
+               % (self.configFile, self.phedexFlag, self.dbsFlag, self.requestMgrFlag)
+               
+    def setEmulator(self):
+        """
+        overwrite this in the child class if diffent setting is needed.
+        """
+        self.phedexFlag = True
+        self.dbsFlag = True
+        self.requestMgrFlag = False

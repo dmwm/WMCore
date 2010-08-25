a86cea363b6dc210cd11af4184c5908d184e43fc
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:52:11 +0000

    Make this work in Oracle.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4711 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Subscriptions/GetJobGroups.py
+++ src/python/WMCore/WMBS/MySQL/Subscriptions/GetJobGroups.py
@@ -6,8 +6,8 @@ MySQL implementation of Subscription.GetJobGroups
 """
 
 __all__ = []
-__revision__ = "$Id: GetJobGroups.py,v 1.3 2009/07/17 20:21:35 meloam Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: GetJobGroups.py,v 1.4 2009/08/03 19:46:37 sfoulkes Exp $"
+__version__ = "$Revision: 1.4 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 from WMCore.WMBS.JobGroup import JobGroup
@@ -18,18 +18,20 @@ class GetJobGroups(DBFormatter):
         # 1) foreach jobgroup within the subscription
         #  2) do a subquery to find the number of jobs that are acquired/failed
         #  3) check to see if this number of jobs is less than the total number                           
-        newstep = """SELECT jobgroup.id
-                        FROM  wmbs_jobgroup jobgroup
-                        WHERE jobgroup.subscription = :subscription
-                            AND (SELECT COUNT(*) 
-                                FROM wmbs_job job
-                                WHERE   job.state = (select id from wmbs_job_state where name = "new")
-                                    AND job.jobgroup = jobgroup.id)
-                                <> 0
+        newstep = """SELECT wmbs_jobgroup.id  FROM wmbs_jobgroup
+                       INNER JOIN
+                         (SELECT wmbs_job.jobgroup AS jobgroup, COUNT(*) AS new_jobs FROM wmbs_job
+                          WHERE wmbs_job.state = (SELECT id FROM wmbs_job_state WHERE name = 'new')
+                          GROUP BY wmbs_job.jobgroup) new_count ON
+                          wmbs_jobgroup.id = new_count.jobgroup
+                        WHERE wmbs_jobgroup.subscription = :subscription
+                        AND new_count.new_jobs != 0
         """                                                 
         allJobs = self.formatFlat(self.dbi.processData(newstep, 
                            {"subscription": subscription},
                            conn = conn, transaction = transaction))
+
+        return allJobs
                                   
                                        
                                      

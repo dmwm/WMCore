7dff57fa04bed393a76f75cad1f790ccfa9a2d86
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:15:37 +0000

    A REST formatter that appends the DAS headers to the result data
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4160 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WebTools/DASRESTFormatter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+_DASRESTFormatter_
+
+A REST formatter that appends the DAS headers to the result data
+"""
+
+from WMCore.WebTools.Page import exposedasjson, exposedasxml
+from WMCore.WebTools.RESTFormatter import RESTFormatter
+
+class DASRESTFormatter(RESTFormatter):
+    @exposedasjson
+    def json(self, data):
+        return data
+
+    @exposedasxml
+    def xml(self, data):
+        return data

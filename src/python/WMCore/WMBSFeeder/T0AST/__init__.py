52636f75f708096081d73ebc7cb99d2b0ff7459e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:45:02 +0000

    initial commit
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5495 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBSFeeder/T0AST/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+_Feeder_
+
+Feeder to populate the WMBS with a set of files from T0 
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2009/11/06 11:05:25 riahi Exp $"
+__version__ = "$Revision: 1.1 $"

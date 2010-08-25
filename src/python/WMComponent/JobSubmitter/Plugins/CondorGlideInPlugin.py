630d978d04458712e85c5f0e54b0f6f37121c2dd
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:53:25 +0000

    Rename report; attempt #3
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7284 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/JobSubmitter/Plugins/CondorGlideInPlugin.py
+++ src/python/WMComponent/JobSubmitter/Plugins/CondorGlideInPlugin.py
@@ -12,8 +12,8 @@ A plug-in that should submit directly to condor glide-in nodes
 
 """
 
-__revision__ = "$Id: CondorGlideInPlugin.py,v 1.5 2010/05/03 18:37:54 mnorman Exp $"
-__version__ = "$Revision: 1.5 $"
+__revision__ = "$Id: CondorGlideInPlugin.py,v 1.6 2010/05/03 19:22:32 mnorman Exp $"
+__version__ = "$Revision: 1.6 $"
 
 import os
 import os.path
@@ -131,7 +131,6 @@ class CondorGlideInPlugin(PluginBase):
         jdl.append('(Memory >= 1 && OpSys == \"LINUX\" ) && (Arch == \"INTEL\" || Arch == \"X86_64\")\n')
         jdl.append("should_transfer_executable = TRUE\n")
         jdl.append("transfer_output_files = Report.pkl\n")
-        jdl.append("transfer_output_remaps = \"Report.pkl = Report.$(Cluster).$(Process).pkl\"\n")
         jdl.append("should_transfer_files = YES\n")
         jdl.append("when_to_transfer_output = ON_EXIT\n")
         jdl.append("log_xml = True\n" )
@@ -184,13 +183,9 @@ class CondorGlideInPlugin(PluginBase):
                         % (os.path.basename(job['sandbox']), index)
             jdl.append(argString)
 
-            #jobCE = self.getCEName(jobSite = job['location'])
-            #if not jobCE:
-                # Then we ended up with a site that doesn't exist?
-            #    logging.error("Job for non-existant site %s" \
-            #                  % (job['location']))
-            #    continue
-            #jdl.append("globusscheduler = %s\n" % (jobCE))
+            # Transfer the output files into new names
+            jdl.append("transfer_output_remaps = \"Report.pkl = Report.%i.pkl\"\n" \
+                       % (job["retry_count"]))
 
             jdl.append("+WMAgent_JobName = \"%s\"\n" % job['name'])
             jdl.append("+WMAgent_JobID = %s\n" % job['id'])

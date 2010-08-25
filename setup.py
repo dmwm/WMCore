cfba63097159c476da4043108a4de1347b82ba77
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 05:16:44 +0000

    a hopeful fix to get buildbot to properly ignore integration and performance tests
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8379 4525493e-7705-40b1-a816-d608a930855b

--- setup.py
+++ setup.py
@@ -92,11 +92,11 @@ if can_nose:
                 time.sleep(4)
             
             if not self.buildBotMode:
-                retval =  nose.run(argv=[__file__,'--with-xunit', '--all-modules','-v','test/python'])
+                retval =  nose.run(argv=[__file__,'--with-xunit', '-v','test/python', '-m', '(_t.py$)|(_t$)|(^test)'])
             else:    
                 print "### We are in buildbot mode ###"
                 sys.stdout.flush()
-                retval =  nose.run(argv=[__file__,'--with-xunit', '--all-modules','-v','test/python','-a','!integration,!performance,!__integration__,!__performance__'],
+                retval =  nose.run(argv=[__file__,'--with-xunit', '-v','test/python','-m', '(_t.py$)|(_t$)|(^test)','-a','!integration,!performance,!__integration__,!__performance__'],
                                     addplugins=[DetailedOutputter(), AttributeSelector()])
                 
             if retval:

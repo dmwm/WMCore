5613b08d017f71c0607b5e46610c3cb22cf42549
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:07:45 +0000

    Fix handling of submission time.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4041 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/Active.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/Active.py
@@ -6,8 +6,8 @@ MySQL implementation of Jobs.Active
 """
 
 __all__ = []
-__revision__ = "$Id: Active.py,v 1.8 2009/04/10 15:42:20 sryu Exp $"
-__version__ = "$Revision: 1.8 $"
+__revision__ = "$Id: Active.py,v 1.9 2009/04/23 16:50:57 sfoulkes Exp $"
+__version__ = "$Revision: 1.9 $"
 import time
 
 from WMCore.Database.DBFormatter import DBFormatter
@@ -18,12 +18,13 @@ class Active(DBFormatter):
                        FROM dual WHERE NOT EXISTS
                          (SELECT job FROM wmbs_group_job_acquired WHERE job = :job)"""    
     
-    updateSQL = "UPDATE wmbs_job SET submission_time = %s WHERE id = :job" % int(time.time())
+    updateSQL = "UPDATE wmbs_job SET submission_time = :time WHERE id = :job"
     
     def execute(self, job, conn = None, transaction = False):
         binds = {"job": job}
         self.dbi.processData(self.insertSQL, binds, conn = conn,
                              transaction = transaction)
+        binds["time"] = time.time()
         self.dbi.processData(self.updateSQL, binds, conn = conn,
                              transaction = transaction)         
         return 

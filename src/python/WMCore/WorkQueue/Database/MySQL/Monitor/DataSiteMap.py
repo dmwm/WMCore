4195ae7f0f9fcbcd88d79e3090682ba48068cae2
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:20:50 +0000

    add Data site table monitor
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6842 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WorkQueue/Database/MySQL/Monitor/DataSiteMap.py
@@ -0,0 +1,27 @@
+"""
+WMCore/WorkQueue/Database/MySQL/Monitor/Sites.py
+
+DAO object for WorkQueue
+
+"""
+
+__all__ = []
+__revision__ = "$Id: DataSiteMap.py,v 1.1 2010/03/24 19:40:12 sryu Exp $"
+__version__ = "$Revision: 1.1 $"
+
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+
+
+class Sites(DBFormatter):
+    sql = """SELECT d.id, d.name, s.name FROM wq_data_site_assoc
+             INNER JOIN wq_data d ON (d.id = data_id)
+             INNER JOIN wq_site s ON (s.id = site_id)
+             ORDER BY d.id"""
+    
+    def execute(self, conn = None, transaction = False):
+        results = self.dbi.processData(self.sql, conn = conn,
+                                       transaction = transaction)
+        
+        return self.formatDict(results)

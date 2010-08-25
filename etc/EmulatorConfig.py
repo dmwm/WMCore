f6affe7cc6d27708fd2213e91170800dcd6e98e5
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:55:43 +0000

    add Emulator configuration
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6500 4525493e-7705-40b1-a816-d608a930855b

---
+++ etc/EmulatorConfig.py
@@ -0,0 +1,11 @@
+'''
+Created on Feb 15, 2010
+
+'''
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+config.section_("Emulator")
+config.Emulator.PhEDEx = True
+config.Emulator.DBSReader = True
+config.Emulator.RequestMgr = True

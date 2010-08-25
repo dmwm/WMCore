9deb9da3dee09e555363ae41bdfd5025269576b0
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:56:02 +0000

    Function to retrieve the checksum from DBSBuffer
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5652 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMComponent/DBSBuffer/Database/MySQL/DBSBufferFiles/GetChecksum.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+"""
+MySQL implementation of GetChecksum
+"""
+
+
+__revision__ = "$Id: GetChecksum.py,v 1.1 2009/12/02 20:04:59 mnorman Exp $"
+__version__  = "$Revision: 1.1 $"
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class GetChecksum(DBFormatter):
+    sql = """SELECT cst.type AS cktype, fcs.cksum AS cksum FROM
+               dbsbuffer_file_checksums fcs INNER JOIN
+               dbsbuffer_checksum_type cst
+               ON fcs.typeid = cst.id
+               WHERE fcs.fileid = :fileid"""
+
+    def formatResult(self, result):
+        """
+        I need the result in a reasonable list.
+        This will return None if there is no cksum
+
+        """
+        formattedResult = {}
+
+        dictVersion = DBFormatter.formatDict(self, result)
+        if type(dictVersion) == type([]):
+            if len(dictVersion) == 0:
+                #Then it's empty
+                return None
+            elif len(dictVersion) == 1:
+                #There's only one entry
+                formattedResult['cktype'] = dictVersion[0].get('cktype', None)
+                formattedResult['cksum']  = dictVersion[0].get('cksum', None)
+            else:
+                #Otherwise there are several, and we have to record each one
+                #I don't know how to do this yet.
+                tmpList = []
+                for entry in dictVersion:
+                    tmpList.append({'cktype': entry.get('cktype', None), 'cksum': entry.get('cksum', None)})
+                formattedResult['cktype'] = tmpList
+                formattedResult['cksum']  = tmpList
+        else:
+            formattedResult['cktype'] = dictVersion.get('cktype', None)
+            formattedResult['cksum']  = dictVersion.get('cksum', None)
+            if formattedResult == {'cktype': None, 'cksum': None}:
+                #Then the thing was empty anyway
+                return None
+
+        return formattedResult
+                
+                
+    def execute(self, fileid = None, bulkList = None, conn = None, transaction = False):
+
+        if bulkList:
+            #Would need to accept a bulk list of form [{fileid: fileid}]
+            binds = bulkList
+        else:
+            binds = {'fileid': fileid}
+
+        result = self.dbi.processData(self.sql, binds, 
+                                      conn = conn, transaction = transaction)
+
+        return self.formatResult(result)
---
+++ src/python/WMComponent/DBSBuffer/Database/SQLite/DBSBufferFiles/GetChecksum.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+SQLite implementation of GetChecksum
+"""
+
+
+__revision__ = "$Id: GetChecksum.py,v 1.1 2009/12/02 20:04:59 mnorman Exp $"
+__version__  = "$Revision: 1.1 $"
+
+from WMComponent.DBSBuffer.Database.MySQL.DBSBufferFiles.GetChecksum import GetChecksum as MySQLGetChecksum
+
+class GetChecksum(MySQLGetChecksum):
+    """
+    Identical to MySQL Version
+
+    """

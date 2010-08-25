9fbbe2c19f9cbe0f1b1999eee727abaa49b2d3cf
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:08:27 +0000

    Seperate table create statements from everything else.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4959 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/ThreadPool/Oracle/Create.py
+++ src/python/WMCore/ThreadPool/Oracle/Create.py
@@ -7,8 +7,8 @@ Class for creating Oracle specific schema for persistent messages.
 
 """
 
-__revision__ = "$Id: Create.py,v 1.3 2009/06/16 14:44:06 mnorman Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: Create.py,v 1.4 2009/08/31 20:35:25 sfoulkes Exp $"
+__version__ = "$Revision: 1.4 $"
 __author__ = "mnorman@fnal.gov"
 
 import threading
@@ -32,112 +32,114 @@ class Create(DBCreator):
     trigger_tables.append('tp_buffer_in_trg')
     trigger_tables.append('tp_buffer_out_trg')
     
-    
-    
-    def __init__(self):
+    def __init__(self, logger = None, dbi = None, params = None):
         myThread = threading.currentThread()
         DBCreator.__init__(self, myThread.logger, myThread.dbi)
         self.create = {}
         self.constraints = {}
 
+        tablespaceTable = ""
+        tablespaceIndex = ""
 
-#Disabled for now: Oracle doesn't seem to support SET AUTOCOMMIT on non-interactive runs.
-#        self.create['a_transaction'] = """
-#SET AUTOCOMMIT OFF; """
+        if params:
+            if params.has_key("tablespace_table"):
+                tablespaceTable = "TABLESPACE %s" % params["tablespace_table"]
+            if params.has_key("tablespace_index"):
+                tablespaceIndex = "USING INDEX TABLESPACE %s" % params["tablespace_index"]
+                                                            
+        self.create["tp_threadpool"] = \
+          """CREATE TABLE tp_threadpool (
+               id             NUMBER(11)    NOT NULL ENABLE,
+               event          VARCHAR2(255) NOT NULL ENABLE,
+               component      VARCHAR2(255) NOT NULL ENABLE,
+               payload        CLOB          NOT NULL ENABLE,
+               thread_pool_id VARCHAR2(255) NOT NULL ENABLE,
+               state          VARCHAR(20)   DEFAULT 'queued'  NOT NULL ENABLE
+               ) %s""" % tablespaceTable
 
-        #tp_threadpool
+        self.create["tp_threadpool_seq"] = \
+          """CREATE SEQUENCE tp_threadpool_seq
+               START WITH 1
+               INCREMENT BY 1
+               NOMAXVALUE"""
 
-        self.create['threadpool'] = """      
-CREATE TABLE tp_threadpool(
-   id                      NUMBER(11)    NOT NULL ENABLE,
-   event                   varchar2(255) NOT NULL ENABLE,
-   component               varchar2(255) NOT NULL ENABLE,
-   payload                 clob          NOT NULL ENABLE,
-   thread_pool_id          varchar2(255) NOT NULL ENABLE,
-   state                   varchar2(20)  default 'queued'  NOT NULL ENABLE,
-   CONSTRAINT tp_threadpool_state CHECK(state IN ('queued', 'process')),
-   CONSTRAINT tp_threadpool_pk    PRIMARY KEY (id)
-   )
-"""
-        #Because Oracle does not implement AUTOINCREMENT, we have to do it ourselves.
-        #Create a sequence of integers and a trigger to do it ourselves.
-        
-        self.create['threadpool_seq'] = """
-CREATE SEQUENCE tp_threadpool_seq
-        start with 1
-        increment by 1
-        nomaxvalue
-"""
+        self.indexes["tp_threadpool_pk"] = \
+          """ALTER TABLE tp_threadpool ADD
+               (CONSTRAINT tp_threadpool_pk PRIMARY KEY (id) %s)""" % tablespaceIndex
 
-        self.create['threadpool_trg'] = """
-CREATE TRIGGER tp_threadpool_trg
-BEFORE INSERT ON tp_threadpool
-FOR EACH ROW
-     DECLARE m_no INTEGER;
-     BEGIN
-        SELECT tp_threadpool_seq.nextval INTO :new.id FROM dual;
-     END;        """
+        self.constraints["tp_threadpool_fk"] = \
+          """ALTER TABLE tp_threadpool ADD
+               (CONSTRAINT tp_threadpool_state CHECK(state IN ('queued', 'process')))"""
 
+        self.create["tp_threadpool_trg"] = \
+          """CREATE TRIGGER tp_threadpool_trg
+               BEFORE INSERT ON tp_threadpool
+               FOR EACH ROW
+                 DECLARE m_no INTEGER;
+                 BEGIN
+                   SELECT tp_threadpool_seq.nextval INTO :new.id FROM dual;
+                 END;"""
 
+        self.create["tp_threadpool_buffer_in"] = \
+          """CREATE TABLE tp_threadpool_buffer_in (
+               id             NUMBER(11)   NOT NULL ENABLE,
+               event          VARCHAR(255) NOT NULL ENABLE,
+               component      VARCHAR(255) NOT NULL ENABLE,
+               payload        CLOB         NOT NULL ENABLE,
+               thread_pool_id VARCHAR(255) NOT NULL ENABLE,
+               state          VARCHAR2(20) DEFAULT 'queued' NOT NULL ENABLE
+               ) %s""" % tablespaceTable
 
-        #tp_threadpool_buffer_in
+        self.create["tp_threadpool_buffer_in_seq"] = \
+          """CREATE SEQUENCE tp_buffer_in_seq
+               START WITH 1
+               INCREMENT BY 1
+               NOMAXVALUE"""
 
-        self.create['threadpool_buffer_in'] = """      
-CREATE TABLE tp_threadpool_buffer_in(
-   id                      NUMBER(11)      NOT NULL ENABLE,
-   event                   varchar(255)    NOT NULL ENABLE,
-   component               varchar(255)    NOT NULL ENABLE,
-   payload                 clob            NOT NULL ENABLE,
-   thread_pool_id          varchar(255)    NOT NULL ENABLE,
-   state                   varchar2(20)    default 'queued'  NOT NULL ENABLE,
-   CONSTRAINT tp_threadpool_buffer_in_state CHECK(state IN ('queued', 'process')),
-   CONSTRAINT tp_threadpool_buffer_in_pk    PRIMARY KEY (id)
-   )
-"""
-        self.create['threadpool_buffer_in_seq'] = """
-CREATE SEQUENCE tp_buffer_in_seq
-        start with 1
-        increment by 1
-        nomaxvalue
-"""
+        self.create["tp_threadpool_buffer_in_trg"] = \
+          """CREATE TRIGGER tp_buffer_in_trg
+               BEFORE INSERT ON tp_threadpool_buffer_in
+               FOR EACH ROW
+                 BEGIN
+                   SELECT tp_buffer_in_seq.nextval INTO :new.id FROM dual;
+                 END;"""
 
-        self.create['threadpool_buffer_in_trg'] = """
-CREATE TRIGGER tp_buffer_in_trg
-BEFORE INSERT ON tp_threadpool_buffer_in
-FOR EACH ROW
-     BEGIN
-        SELECT tp_buffer_in_seq.nextval INTO :new.id FROM dual;
-     END;        """
+        self.constraints["tp_threadpool_buffer_in_fk"] = \
+          """ALTER TABLE tp_threadpool_buffer_in
+               (CONSTRAINT tp_threadpool_buffer_in_state CHECK(state IN ('queued', 'process')))"""
 
+        self.indexes["tp_threadpool_buffer_in_pk"] = \
+          """ALTER TABLE tp_threadpool_buffer_in
+               (CONSTRAINT tp_threadpool_buffer_in_pk PRIMARY KEY (id) %s)""" % tablespaceIndex
 
-        #tp_threadpool_buffer_out
-        
-        self.create['threadpool_buffer_out'] = """      
-CREATE TABLE tp_threadpool_buffer_out(
-   id                      NUMBER(11)   NOT NULL ENABLE,
-   event                   varchar(255) NOT NULL ENABLE,
-   component               varchar(255) NOT NULL ENABLE,
-   payload                 clob         NOT NULL ENABLE,
-   thread_pool_id          varchar(255) NOT NULL ENABLE,
-   state                   varchar2(20) default 'queued'  NOT NULL ENABLE,
-   CONSTRAINT tp_threadpool_buffer_out_state CHECK(state IN ('queued', 'process')),
-   CONSTRAINT tp_threadpool_buffer_out_pk    PRIMARY KEY (id)
-   )
-"""
+        self.create["tp_threadpool_buffer_out"] = \
+          """CREATE TABLE tp_threadpool_buffer_out (
+               id             NUMBER(11)   NOT NULL ENABLE,
+               event          varchar(255) NOT NULL ENABLE,
+               component      varchar(255) NOT NULL ENABLE,
+               payload        clob         NOT NULL ENABLE,
+               thread_pool_id varchar(255) NOT NULL ENABLE,
+               state          varchar2(20) DEFAULT 'queued' NOT NULL ENABLE
+               ) %s""" % tablespaceTable
 
+        self.create["threadpool_buffer_out_seq"] = \
+          """CREATE SEQUENCE tp_buffer_out_seq
+               START WITH 1
+               INCREMENT BY 1
+               NOMAXVALUE"""
 
-        self.create['threadpool_buffer_out_seq'] = """
-CREATE SEQUENCE tp_buffer_out_seq
-        start with 1
-        increment by 1
-        nomaxvalue
-"""
+        self.create["tp_threadpool_buffer_out_trg"] = \
+          """CREATE TRIGGER tp_buffer_out_trg
+               BEFORE INSERT ON tp_threadpool_buffer_out
+               FOR EACH ROW
+                 BEGIN
+                   SELECT tp_buffer_out_seq.nextval INTO :new.id FROM dual;
+                 END;"""
 
-        self.create['threadpool_buffer_out_trg'] = """
-CREATE TRIGGER tp_buffer_out_trg
-BEFORE INSERT ON tp_threadpool_buffer_out
-FOR EACH ROW
-     BEGIN
-        SELECT tp_buffer_out_seq.nextval INTO :new.id FROM dual;
-     END;        """
- 
+        self.indexes["tp_threadpool_buffer_out_pk"] = \
+          """ALTER TABLE tp_threadpool_buffer_out ADD
+               (CONSTRAINT tp_threadpool_buffer_out_pk PRIMARY KEY (id) %s)""" % tablespaceIndex
+
+        self.constraints["tp_threadpool_buffer_out_ck"] = \
+          """ALTER TABLE tp_threadpool_buffer_out ADD
+               (CONSTRAINT tp_threadpool_buffer_out_state CHECK(state IN ('queued', 'process')))"""

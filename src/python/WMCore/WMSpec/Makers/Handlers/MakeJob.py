c52ca4ba1d86165d4f96c2d8afe1c00575c30334
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:28:22 +0000

    Handler for the JobMaker threaded approach; initial check-in. Called by JobMaker, and runs the Interface modules.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4352 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMSpec/Makers/Handlers/MakeJob.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""
+Default function for Job Maker
+"""
+__all__ = []
+__revision__ = "$Id: MakeJob.py,v 1.1 2009/06/19 18:06:15 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+
+from WMCore.Agent.BaseHandler import BaseHandler
+from WMCore.ThreadPool.ThreadPool import ThreadPool
+
+import exceptions
+import threading
+
+
+
+class MakeJob(BaseHandler):
+    """
+    Default handler for Make Job Events.
+    """
+
+    def __init__(self, component):
+        BaseHandler.__init__(self, component)
+        # define a slave threadpool (this is optional
+        # and depends on the developer deciding how he/she
+        # wants to implement certain logic.
+
+        self.nThreads = 5
+
+        self.threadpool = ThreadPool(\
+            "WMCore.WMSpec.Makers.Handlers.MakeJobSlave", \
+            self.component, 'MakeJob', \
+            self.nThreads)
+        myThread = threading.currentThread()
+        myThread.msgService.purgeMessages()
+
+     # this we overload from the base handler
+    def __call__(self, event, payload):
+        """
+        Handles the event with payload, by sending it to the threadpool.
+        """
+        # as we defined a threadpool we can enqueue our item
+        # and move to the next.
+        self.threadpool.enqueue(event, payload)
+        print "Thread with payload " + str(payload) + " is enqueued"
---
+++ src/python/WMCore/WMSpec/Makers/Handlers/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+_WMCore.WMSpec.Makers_
+
+"""
+__revision__ = "$Id: __init__.py,v 1.1 2009/06/19 18:06:15 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+__all__ = []

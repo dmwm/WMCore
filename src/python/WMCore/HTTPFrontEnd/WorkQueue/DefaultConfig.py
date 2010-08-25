9315f36df18107102f33a5b2dff304dc0999571d
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:57:27 +0000

    Config changes plus take queueParams from WorkQueueManager (mandatory to run both WorkQueue REST & Manager together)
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6525 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/HTTPFrontEnd/WorkQueue/DefaultConfig.py
+++ src/python/WMCore/HTTPFrontEnd/WorkQueue/DefaultConfig.py
@@ -4,13 +4,12 @@ import WMCore.WMInit
 
 config = Configuration()
 
-config.component_('Webtools')
-config.Webtools.application = 'WorkQueueService'
-config.Webtools.port = 8080
-config.Webtools.host = "hostname.fnal.gov"
+config.webapp_('WorkQueueService')
+config.WorkQueueService.port = 8080
+config.WorkQueueService.host = "hostname.fnal.gov"
 config.component_('WorkQueueService')
 
-config.WorkQueueService.templates = path.join( WMCore.WMInit.getWMBASE(), 'src/templates/WMCore/WebTools')
+config.WorkQueueService.templates = path.join(WMCore.WMInit.getWMBASE(), 'src/templates/WMCore/WebTools')
 config.WorkQueueService.admin = 'your@email.com'
 config.WorkQueueService.title = 'WorkQueue Data Service'
 config.WorkQueueService.description = 'Provide WorkQueue related service call'
@@ -21,8 +20,8 @@ active = config.WorkQueueService.views.section_('active')
 workqueue = active.section_('workqueue')
 # The class to load for this view/page
 workqueue.object = 'WMCore.WebTools.RESTApi'
-workqueue.templates = path.join( WMCore.WMInit.getWMBASE(),  'src/templates/WMCore/WebTools/')
-workqueue.database = 'mysql://username@hostname.fnal.gov:3306/TestDB'
+workqueue.templates = path.join(WMCore.WMInit.getWMBASE(), 'src/templates/WMCore/WebTools/')
+#workqueue.database = 'mysql://username@hostname.fnal.gov:3306/TestDB'
 #only needs to specify when mysql db is used
 #if it is not specified gets the value from environment variable. (DBSOCK)
 #workqueue.dbsocket = '/var//mysql.sock'
@@ -32,5 +31,5 @@ workqueue.section_('formatter')
 workqueue.formatter.object = 'WMCore.WebTools.DASRESTFormatter'
 workqueue.serviceModules = ['WMCore.HTTPFrontEnd.WorkQueue.Services.WorkQueueService']
 
-# if workqueue is a global queue this needs to be set True. for a local queue False
-workqueue.queueParams = {'PopulateFilesets' : True}
+# take queueParams from WorkQueueManager - specify here to override
+workqueue.queueParams = getattr(config.WorkQueueManager, 'queueParams', {})

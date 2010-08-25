e2a4633c1c6ff7d839b71492222d7de8e41d0359
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:23:10 +0000

    Hopefully straightening out the Inner Join mess left in the initial version by simplifying the SQL code.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4273 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSBuffer/Database/Oracle/UpdateDSFileCount.py
+++ src/python/WMComponent/DBSBuffer/Database/Oracle/UpdateDSFileCount.py
@@ -5,8 +5,8 @@ _DBSBuffer.NewFile_
 Add a new file to DBS Buffer: Oracle version
 
 """
-__revision__ = "$Id: UpdateDSFileCount.py,v 1.1 2009/05/15 16:19:13 mnorman Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: UpdateDSFileCount.py,v 1.2 2009/06/04 21:52:14 mnorman Exp $"
+__version__ = "$Revision: 1.2 $"
 __author__ = "mnorman@fnal.gov"
 
 #This has been updated for use with Oracle
@@ -22,14 +22,23 @@ class UpdateDSFileCount(MySQLUpdateDSFileCount):
 	
 	"""
 
-	sql = """
-UPDATE dbsbuffer_dataset a
-        SET UnMigratedFiles = (SELECT count(*) FROM dbsbuffer_file f
-	   WHERE f.status  = 'NOTUPLOADED'
-	   AND   f.dataset = a.ID
-	)
-	WHERE ID IN (SELECT ID from dbsbuffer_dataset WHERE Path=:path)
+
+	sql = """UPDATE dbsbuffer_dataset a
+	      SET UnMigratedFiles = (SELECT count(*) FROM dbsbuffer_file f
+	      WHERE f.status  = 'NOTUPLOADED'
+	      AND   f.dataset = a.ID
+	      AND   a.Path    = :path)
 	"""
+
+#	sql = """
+#UPDATE dbsbuffer_dataset a
+#        SET UnMigratedFiles = (SELECT count(*) FROM dbsbuffer_file f
+#	   WHERE f.status  = 'NOTUPLOADED'
+#	   AND   f.dataset = a.ID
+#	)
+#	WHERE ID IN (SELECT ID from dbsbuffer_dataset WHERE Path=:path)
+#	"""
+
 
 
 

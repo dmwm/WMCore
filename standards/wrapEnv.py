99ed3b23ba3fdb9b7d4163fc9d73d57974aaa55a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:18:24 +0000

    adding wrapper scripts for buildbot so he can be made aware of paths and databases.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5966 4525493e-7705-40b1-a816-d608a930855b

---
+++ standards/wrapEnv.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+""" wrapEnv.py [python version] [database schema] [command line]
+        handles munging PATH, PYTHONPATH, DATABASE variables for tests
+        read configuration from standards/buildslave.py .. there is an 
+        example file at buildslave.py.sample"""
+    
+import sys
+import os
+if (len(sys.argv) < 3):
+    print "Usage: %s <python version> <database type> <command> [arg1] [arg2] .." %\
+                sys.argv[0]
+    sys.exit(1)
+
+requestedPython = sys.argv[1]
+requestedDB     = sys.argv[2]
+import buildslave
+
+if ( not (requestedPython in buildslave.conf) ):
+    print "Requested python version isn't in the slave configuration"
+    sys.exit(1)
+
+os.environ['PYTHONPATH'] = "%s:%s" % (buildslave.conf[requestedPython]['PYTHONPATH'], os.environ['PYTHONPATH'])
+os.environ['PATH'] = "%s:%s" % (buildslave.conf[requestedPython]['PATH'], os.environ['PATH'])
+
+if ( not (requestedDB in buildslave.conf) ):
+    if ( requestedDB == 'sqlite'):
+        # give a default for sqlite, since it doesnt matter
+        os.environ['DATABASE'] = 'sqlite://temp.db'
+    else:
+        print "Requested database type isn't in the slave configuration"
+        sys.exit(1)
+else:
+    os.environ['DATABASE'] = buildslave.conf[requestedDB]
+
+commandLine = sys.argv[3:]
+
+# actually run
+sys.execvp( commandLine[0], commandLine[1:] )
+
+        
+
+    
+    

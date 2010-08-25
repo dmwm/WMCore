ea7246282f4e7dbcc93c3479705f12422c56e4c3
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 05:07:01 +0000

    Update views to use the GroupUser couchap views for owner
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8255 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapps/ACDC/views/collection_name/map.js
+++ src/couchapps/ACDC/views/collection_name/map.js
@@ -1,5 +1,5 @@
 function(doc) {  
     if (doc.collection) {
-        emit([doc.collection.name, doc.collection.owner_id] , { '_id': doc._id, 'collection' : doc.collection} )
+        emit([doc.collection.name, doc.owner.group, doc.owner.user] , { '_id': doc._id, 'collection' : doc.collection} )
     }
 }

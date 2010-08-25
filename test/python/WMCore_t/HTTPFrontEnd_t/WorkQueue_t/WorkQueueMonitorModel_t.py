cb9b53414e56f0f1a5f4e0d796b0bba9a0c12b00
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:27:51 +0000

    initial files for WorkQueue REST monitoring capability - REST unit test file
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6106 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/python/WMCore_t/HTTPFrontEnd_t/WorkQueue_t/WorkQueueMonitorModel_t.py
@@ -0,0 +1,91 @@
+"""
+WMCore_t/HTTPFrontEnd_t/WorkQueue_t/WorkQueueMonitorModel_t.py
+
+unittest for
+WMCore/HTTPFrontEnd/WorkQueue/WorkQueueMonitoringModel.py
+
+writing unittests / testing details:
+https://twiki.cern.ch/twiki/bin/view/CMS/RESTModelUnitTest
+"""
+
+
+
+__revision__ = "$Id: WorkQueueMonitorModel_t.py,v 1.1 2010/01/26 15:19:17 maxa Exp $"
+__version__ = "$Revision: 1.1 $"
+
+
+import os
+import unittest
+from WMCore.HTTPFrontEnd.WorkQueue.WorkQueueMonitorModel import WorkQueueMonitorModel
+from WMCore.WebTools.RESTFormatter import RESTFormatter 
+
+from WMQuality.WebTools.RESTBaseUnitTest import RESTBaseUnitTest
+from WMQuality.WebTools.RESTClientAPI import methodTest
+from WMQuality.WebTools.RESTServerSetup import DefaultConfig
+
+from WMCore.WorkQueue.WorkQueue import globalQueue
+from WMCore_t.Services_t.WorkQueue_t.WorkQueuePopulator import createProductionSpec, getGlobalQueue
+
+
+
+# TODO need to populate database, properly instantiate WorkQueue
+# getGlobalWorkQueue fails
+class WorkQueueMonitorModelTest(RESTBaseUnitTest):    
+    def initialize(self):
+        print "initialize()"
+        self.config = DefaultConfig("WMCore.HTTPFrontEnd.WorkQueue.WorkQueueMonitorModel")
+        
+        # if module "WorkQueueMonitoringModel" needs database access, set:
+        self.schemaModules = ["WMCore.WorkQueue.Database"]
+        
+        dbUrl = os.environ["DATABASE"] or "'sqlite:////tmp/resttest.db"
+        self.config.setDBUrl(dbUrl)        
+        self.urlbase = self.config.getServerUrl()
+        
+                
+    
+    def setUp(self):
+        print "setUp()"
+        RESTBaseUnitTest.setUp(self) # calls self.initialize()         
+        # TestInit stuff happens in RESTBaseUnitTest if self.schemaModules are set
+      
+        # TODO
+        # set up WorkQueue - taken from Servies_t/WorkQueue_t/WorkQueue_t.py
+        # which fails - email to Seangchan ...
+        # self.params = {}
+        # self.params['endpoint'] = self.config.getServerUrl()        
+        # self.globalQueue = getGlobalQueue(dbi = self.testInit.getDBInterface(),
+        #                                   CacheDir = 'global',
+        #                                   NegotiationTimeout = 0,
+        #                                   QueueURL = self.config.getServerUrl())    
+        
+    
+    
+    def tearDown(self):
+        print "tearDown()"
+        RESTBaseUnitTest.tearDown(self)
+        # happens in RESTBaseUnitTest if self.schemaModules is set
+        # self.testInit.clearDatabase()
+
+        
+    
+    def testMyAPI(self):
+        print "testMyAPI()"
+        # this call fails
+        # AttributeError: 'module' object has no attribute 'Database'
+        self.testInit.initializeSchema(["WMCore.WorkQueue.Database"])
+        # test not accepted type should return 406 error, in fact getting 404, then OK
+        url = self.urlbase + 'testApi/'
+        methodTest('GET', url,  accept='text/json', output={'code':404})
+        
+
+        
+    def testDummy(self):
+        print "testDummy()"
+        assert True
+
+             
+        
+if __name__ == "__main__":
+    unittest.main()        
+     

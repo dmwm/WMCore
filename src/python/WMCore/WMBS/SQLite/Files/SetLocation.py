df1c41e3e88c88507995fe950ad57c963321a3e1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:07:12 +0000

    use mysql bind and format
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4032 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Files/SetLocation.py
+++ src/python/WMCore/WMBS/SQLite/Files/SetLocation.py
@@ -7,24 +7,4 @@ from sets import Set
 
 class SetLocation(SetLocationMySQL):
     sql = SetLocationMySQL.sql
-                
-    def getBinds(self, file = None, location = None):
-        if type(location) == type('string'):
-            return self.dbi.buildbinds(self.dbi.makelist(file), 'file', 
-                   self.dbi.buildbinds(self.dbi.makelist(location), 'location'))
-        elif isinstance(location, (list, Set, set)):
-            binds = []
-            for l in location:
-                binds.extend(self.dbi.buildbinds(self.dbi.makelist(file), 'file', 
-                   self.dbi.buildbinds(self.dbi.makelist(l), 'location')))
-            return binds
-        else:
-            raise Exception, "Type of location argument is not allowed: %s" \
-                                % type(location)
         
-    def execute(self, file = None, location = None, conn = None, transaction = False):
-        binds = self.getBinds(file, location)
-        result = self.dbi.processData(self.sql, binds, 
-                         conn = conn, transaction = transaction)
-        
-        return self.format(result)

d962424d2573ac24c2fd8b17165c7d79f2fbf4a0
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:13:40 +0000

    Silence warning messgae if emulator config missing.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6745 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMQuality/Emulators/__init__.py
+++ src/python/WMQuality/Emulators/__init__.py
@@ -7,24 +7,20 @@ import os
 from WMCore.Configuration import loadConfigurationFile
 
 def _getConfiguration():
-    configFile = os.environ["EMULATOR_CONFIG"]
-    config = loadConfigurationFile(configFile)
-    return config
-    
+    configFile = os.environ.get("EMULATOR_CONFIG")
+    if configFile:
+        return loadConfigurationFile(configFile)
+    return None
+
 def emulatorSwitch(name):
     try:
         config = _getConfiguration()
-         
-        if getattr(config.Emulator, name, False):
-            logging.info("Using %s Emulator" %  name)
-            return True
-        else:
-            return False    
-    
+        if config:
+            if getattr(config.Emulator, name, False):
+                logging.info("Using %s Emulator" %  name)
+                return True
+        return False
+
     except Exception, e:
         logging.warning("Emulator Config is not set:  %s"  % str(e))
         return False
-        
-        
-    
-    

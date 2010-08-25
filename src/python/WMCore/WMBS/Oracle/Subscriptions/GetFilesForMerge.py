4eb9da04f621f95c4b2df74270f2368f2135ef58
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:46:31 +0000

    No more close out state for jobs.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5517 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Subscriptions/GetFilesForMerge.py
+++ src/python/WMCore/WMBS/Oracle/Subscriptions/GetFilesForMerge.py
@@ -6,8 +6,8 @@ Oracle implementation of Subscription.GetFilesForMerge
 """
 
 __all__ = []
-__revision__ = "$Id: GetFilesForMerge.py,v 1.5 2009/09/01 19:33:56 sfoulkes Exp $"
-__version__ = "$Revision: 1.5 $"
+__revision__ = "$Id: GetFilesForMerge.py,v 1.6 2009/11/09 15:27:12 sfoulkes Exp $"
+__version__ = "$Revision: 1.6 $"
 
 from WMCore.WMBS.MySQL.Subscriptions.GetFilesForMerge import GetFilesForMerge as GetFilesForMergeMySQL
 
@@ -42,7 +42,7 @@ class GetFilesForMerge(GetFilesForMergeMySQL):
                  (SELECT jobgroup FROM
                    (SELECT wmbs_job.jobgroup AS jobgroup , COUNT(*) AS total FROM wmbs_job
                       INNER JOIN wmbs_job_state ON wmbs_job.state = wmbs_job_state.id
-                    WHERE wmbs_job.outcome = 0 OR wmbs_job_state.name != 'closeout'
+                    WHERE wmbs_job.outcome = 0 OR wmbs_job_state.name != 'cleanout'
                     GROUP BY wmbs_job.jobgroup) incomplete
                   WHERE incomplete.total != 0)  
                AND NOT EXISTS

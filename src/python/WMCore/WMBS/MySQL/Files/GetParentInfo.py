ba8003729673b27dd83f72ef34629cceaa483338
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:32:29 +0000

    Simplify to get rid of redneck info
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7000 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Files/GetParentInfo.py
+++ src/python/WMCore/WMBS/MySQL/Files/GetParentInfo.py
@@ -8,42 +8,23 @@ lfn, id and whether or not the file is merged.  This will also determine
 whether or not the file is a redneck parent or redneck child.
 """
 
-__revision__ = "$Id: GetParentInfo.py,v 1.2 2009/12/23 17:49:36 sfoulkes Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: GetParentInfo.py,v 1.3 2010/04/07 18:57:01 mnorman Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class GetParentInfo(DBFormatter):
-    sql = """SELECT wmbs_file_details.id,
-                    wmbs_file_details.lfn,
-                    wmbs_file_details.merged,
-                    wmbs_file_gpdetails.lfn AS gplfn,
-                    wmbs_file_gpdetails.merged AS gpmerged,
-                    redneck_parent_output.output_fileset AS redneck_parent_fileset,
-                    redneck_child_output.output_fileset AS redneck_child_fileset
-                    FROM wmbs_file_details
-               INNER JOIN wmbs_file_parent ON
-                 wmbs_file_details.id = wmbs_file_parent.parent
-               LEFT OUTER JOIN wmbs_file_parent wmbs_file_gparent ON
-                 wmbs_file_parent.parent = wmbs_file_gparent.child
-               LEFT OUTER JOIN wmbs_file_details wmbs_file_gpdetails ON
-                 wmbs_file_gparent.parent = wmbs_file_gpdetails.id
-               INNER JOIN wmbs_fileset_files ON
-                 wmbs_file_parent.child = wmbs_fileset_files.file
-               INNER JOIN wmbs_workflow_output ON
-                 wmbs_fileset_files.fileset = wmbs_workflow_output.output_fileset
-               INNER JOIN wmbs_subscription ON
-                 wmbs_subscription.workflow = wmbs_workflow_output.workflow_id
-               LEFT OUTER JOIN wmbs_workflow_output wmbs_pworkflow_output ON
-                 wmbs_subscription.fileset = wmbs_pworkflow_output.output_fileset
-               LEFT OUTER JOIN wmbs_workflow_output redneck_parent_output ON
-                 wmbs_pworkflow_output.workflow_id = redneck_parent_output.workflow_id AND
-                 wmbs_pworkflow_output.output_parent = redneck_parent_output.output_identifier
-               LEFT OUTER JOIN wmbs_workflow_output redneck_child_output ON
-                 wmbs_pworkflow_output.workflow_id = redneck_child_output.workflow_id AND
-                 wmbs_pworkflow_output.output_identifier = redneck_child_output.output_parent
-               WHERE wmbs_file_parent.child =
-                 (SELECT id FROM wmbs_file_details WHERE lfn = :child_lfn)"""
+    sql = """SELECT wfp.id, wfp.lfn, wfp.merged,
+                    wfgp.lfn AS gplfn, wfgp.merged AS gpmerged
+             FROM wmbs_file_details wfp
+             INNER JOIN wmbs_file_parent wfpa ON wfpa.parent = wfp.id
+             INNER JOIN wmbs_file_details wfd ON wfd.id = wfpa.child
+             LEFT OUTER JOIN wmbs_file_parent wfpb ON wfpb.child = wfp.id
+             LEFT OUTER JOIN wmbs_file_details wfgp ON wfgp.id = wfpb.parent
+             WHERE wfd.lfn = :child_lfn
+    """
+
+    
     
     def execute(self, childLFNs, conn = None, transaction = False):
         bindVars = []

8808ea0a4b0d690ab5a51e4be363822bc3026526
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:50:07 +0000

    PlotFairy examples for WQMonitor, using only hard-coded values, some details yet to be clarified
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8039 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/html/WorkQueue/examples/PlotFairyLineChart.py
@@ -0,0 +1,85 @@
+
+# HOWTO run:
+# 1) set up WMCore
+# 2) db init
+#    bin/wmcore-db-init --config src/python/WMCore/examples/WorkQueue/GlobalWQConfig.py --create --modules=WMCore.WorkQueue.Database,WMCore.MsgService,WMCore.Agent.Database
+# 3) run compoments:
+#    bin/wmcoreD --start --config src/python/WMCore/examples/WorkQueue/MonitorServiceConfig.py
+# 4) python __file__
+#     -> this prints out html file, normally resulting html snippet will be used ...
+
+
+import random, urllib, math
+try:
+    # Python 2.6
+    import json
+except:
+    # Prior to 2.6 requires simplejson
+    import simplejson as json
+    
+
+from WMCore.WorkQueue.DataStructs.WorkQueueElement import STATES
+from WMQuality.WebTools.RESTClientAPI import makeRequest
+from WMCore.Wrappers import JsonWrapper
+
+
+# random colour chooser (or use matplotlib colourmap)
+def random_colour():
+    return '#%02x%02x%02x'%(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+
+def random_data():
+    return [random.randint(10, 20) for x in range(4)]
+
+
+# output file name
+FILE_NAME = "PlotFairyLineChart.html"
+PLOT_SERVICE_URL = "http://localhost:8888"
+DATA_SOURCE_URL = "http://localhost:8888/workqueue/elementsinfo"
+
+
+pageTemplate = \
+"""
+<html>\n<body>
+<p>
+<img src=%(url)s/plotfairy/plot/?type=%(plotType)s&data=%(hardCodedData)s>
+</p>
+<br><br>
+</body>\n</html>
+"""
+
+
+# hard-coded data
+series = [{"label": x, "colour": random_colour(), 'x': random_data(), 'y': random_data(), "marker": '*'} for x in STATES]
+plotDefinition = \
+{
+    "title": "Job element recent evolution",
+    "shadow": True,
+    "series": series,
+    "sort": "value",
+    "legend": "right",
+    "draw_lines": True,
+    # TODO
+    # time axis format doesn't seem to work properly - even in PlotFairy:Example.py
+    "xaxis": {"label": "time", "format": "time"},
+    # TODO
+    # how to set y-axis description just a text label?
+    "yaxis": {"label": "thousands of jobs", "format": "si"}
+}
+hardCodedPlotData = urllib.quote(json.dumps(plotDefinition, ensure_ascii = True))
+
+
+# TODO
+# get data from the system (URL) ... should have particular visualisation request ...
+# see PlotFairyPieChart for reference ...
+# retrieve data from the system
+# URL http://localhost:8888/workqueue/elementsinfo
+
+
+
+# write result file / generate result html snippet
+outputFile = open(FILE_NAME, 'w')
+finalPage = pageTemplate % {"url": PLOT_SERVICE_URL, "plotType": "Scatter", 
+                            "hardCodedData": hardCodedPlotData}
+outputFile.write(finalPage)
+outputFile.close()
+print "result html page '%s' written" % FILE_NAME

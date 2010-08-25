f75f6d52a908d4695249f14135218a53bdfbf406
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:38:39 +0000

    Modify the DAO to work even if the location already exists.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4508 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSBuffer/Database/MySQL/DBSBufferFiles/AddLocation.py
+++ src/python/WMComponent/DBSBuffer/Database/MySQL/DBSBufferFiles/AddLocation.py
@@ -1,37 +1,19 @@
+#!/usr/bin/env python
 """
-MySQL implementation of AddLocation, Adds a Location to database if it doesn't exists already
+_AddLocation_
+
+MySQL implementation of DBSBufferFiles.AddLocation
 """
+
 from WMCore.Database.DBFormatter import DBFormatter
-from sets import Set
 
 class AddLocation(DBFormatter):
-
-    sql = """insert dbsbuffer_location(se_name) values (:location)""" 
-
-    def getBinds(self, location = None):
-
-        if type(location) == type('string'):
-            return self.dbi.buildbinds(self.dbi.makelist(location), 'location')
-
-        elif isinstance(location, (list, Set, set)):
-            binds = []
-            for l in location:
-                binds.extend(self.dbi.buildbinds(self.dbi.makelist(file), 'lfn',
-                   self.dbi.buildbinds(self.dbi.makelist(l), 'location')))
-            return binds
-        else:
-            raise Exception, "Type of location argument is not allowed: %s" \
-                                % type(location)
-
-    def execute(self, location, conn = None, transaction = None):
-        binds = self.getBinds(location)
-	try:
-	        result = self.dbi.processData(self.sql, binds, conn = conn,
-                                      transaction = transaction)
-        except Exception, ex:
-            if ex.__str__().find("Duplicate entry") != -1 :
-                return
-            else:
-                raise ex
+    sql = """INSERT INTO dbsbuffer_location (se_name) 
+               SELECT :location AS se_name FROM DUAL WHERE NOT EXISTS
+                (SELECT se_name FROM dbsbuffer_location WHERE se_name = :location)"""
+    
+    def execute(self, siteName, conn = None, transaction = False):
+        binds = {"location": siteName}
+        self.dbi.processData(self.sql, binds, conn = conn, 
+                             transaction = transaction)
         return
-

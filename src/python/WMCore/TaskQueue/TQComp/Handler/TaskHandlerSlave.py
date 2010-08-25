2e40b1cfc18012262a7a39b4a3e11d4321c44f6b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:10:01 +0000

    Keep handlers for reference (not used by now)
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4076 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/TaskQueue/TQComp/Handler/TaskHandlerSlave.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+"""
+Slave used for NewTask handler.
+"""
+
+__all__ = []
+__revision__ = "$Id: TaskHandlerSlave.py,v 1.1 2009/04/30 09:24:03 delgadop Exp $"
+__version__ = "$Revision: 1.1 $"
+
+import logging
+import threading
+import time
+import random
+
+
+# inherit from our default slave implementation
+from TQComp.Handler.ParentSlave import ParentSlave
+
+class TaskHandlerSlave(ParentSlave):
+    """
+    The slave handler for a NewTask message
+    """
+
+    
+    def initInThread(self):
+        """
+        Called during thread initialization. Loads the
+        backend for this instance.
+        """
+        # make sure you instantiate the super class method.
+        ParentSlave.initInThread(self)
+
+        logging.debug("TaskHandlerSlave initialized")
+
+
+    def __call__(self, parameters):
+        logging.debug("I am the TaskHandler slave called with parameters: "\
+            +str(parameters))
+
+        # first call the super class to do the default work.
+        ParentSlave.__call__(self, parameters)
+
+        # Now handle the message
+        myThread = threading.currentThread()
+        if parameters['event'] in ['NewTask']:
+
+            # Extract the task attributes
+            # Here we should check that all arguments are given correctly...
+            parts = parameters["payload"].split(",")  
+            logging.debug('TaskHandler:NewTask:parts'+str(parts))
+
+            # Extract sandbox path if not known yet
+            sandbox = parts[1]
+            if not sandbox:
+                sandbox = self.__getSandbox__(parts[0])
+
+            # Insert job and its characteristics in the database
+            myThread.transaction.begin()
+            self.queries.add(*parts)
+
+            # Say how many we have
+            print "Number of tasks in queue: %s" % (self.queries.count())
+            myThread.transaction.commit()
+
+        else:
+            # unexpected message, scream!
+            pass
+
+
+        # Typical ending
+        sleepTime = random.randint(0, 5)
+        myThread = threading.currentThread()
+        # we need to do this in our slave otherwise the failure
+        # messages that might have been published, will not be send.
+        myThread.msgService.finish()
+
+        time.sleep(sleepTime) 
+
+
+    def __getSandbox__(self, xmlfile):
+        dom = xml.dom.minidom.parse(xmlfile)
+        params = dom.getElementsByTagName("JobSpec")[0].getElementsByTagName("Parameter")
+        for param in params:
+            if param.getAttribute("Name") == "BulkInputSandbox":
+                result = self.__getText__(param.childNodes)
+        return result
+
+
+    def __getText__(self, nodelist):
+        rc = ""
+        for node in nodelist:
+            if node.nodeType == node.TEXT_NODE:
+                rc = rc + node.data
+        return rc
+
+
---
+++ src/python/WMCore/TaskQueue/TQComp/Handler/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+_Handler_
+
+Main Queue maintains tasks and pilot information component
+
+"""
+#__all__ = []

bff16669a533f270ca98746c3816760cc1e9d065
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:09:57 +0000

    Expand on the test cases
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5845 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WebTools_t/REST_t.py
+++ test/python/WMCore_t/WebTools_t/REST_t.py
@@ -7,29 +7,31 @@ etc..
 
 """
 
-__revision__ = "$Id: REST_t.py,v 1.3 2009/11/23 18:09:12 metson Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: REST_t.py,v 1.4 2009/12/22 16:46:55 metson Exp $"
+__version__ = "$Revision: 1.4 $"
 
 import unittest
 import os
 import threading
-
+import logging 
+import cherrypy
 import urllib, urllib2
 from httplib import HTTPConnection
-
+from cherrypy import HTTPError
 from WMQuality.TestInit import TestInit
 from WMCore.WebTools.RESTModel import RESTModel
 from WMCore.Configuration import Configuration
+from WMCore.WebTools.Root import Root
 
-class DummyDAO1():
+class DummyDAO1:
     def execute(self, input={}):
         return {'data': 123, 'input': input}
 
-class DummyDAO2():
+class DummyDAO2:
     def execute(self, input={}):
         return {'data': 456, 'input': input}
 
-class DummyDAOFac():
+class DummyDAOFac:
     def __call__(self, classname='DummyDAO1'):
         dao = None
         if classname == 'DummyDAO1':
@@ -88,30 +90,130 @@ class DummyRESTModel(RESTModel):
         
 class RESTTest(unittest.TestCase):
     
+    def configureServer(self):
+        dummycfg = Configuration()
+        dummycfg.component_('Webtools')
+        dummycfg.Webtools.application = 'UnitTests'
+        dummycfg.Webtools.log_screen = False
+        dummycfg.Webtools.access_file = '/dev/null'
+        dummycfg.Webtools.error_file = '/dev/null'
+        dummycfg.component_('UnitTests')
+        dummycfg.UnitTests.title = 'CMS WMCore/WebTools Unit Tests'
+        dummycfg.UnitTests.description = 'Dummy server for the running of unit tests' 
+        dummycfg.UnitTests.section_('views')
+        
+        active = dummycfg.UnitTests.views.section_('active')
+        active.section_('rest')
+        active.rest.object = 'WMCore.WebTools.RESTApi'
+        active.rest.templates = '/tmp'
+        active.rest.database = 'sqlite://'
+        active.rest.section_('model')
+        active.rest.model.object = 'WMCore.WebTools.RESTModel'
+        active.rest.section_('formatter')
+        active.rest.formatter.object = 'WMCore.WebTools.RESTFormatter'
+        active.rest.formatter.templates = '/tmp'
+        
+        rt = Root(dummycfg)
+        return rt
+    
+    def testGoodEcho(self):
+        rt = self.configureServer()
+        rt.start(blocking=False)
+        cherrypy.log.error_log.setLevel(logging.WARNING)
+        cherrypy.log.access_log.setLevel(logging.WARNING)
+        
+        for method in ['POST']:
+            data, code, type = self.makeRequest('/rest/echo', 
+                                                   {'data': 'unit test'}, 
+                                                   method, 'text/json')
+            assert code == 200, \
+                 'Got a return code != 200 (got %s)' % code
+            assert type == 'text/json'
+            assert data == '{"args": [], "kwargs": {"data": "unit test"}}', 'got unexpected response %s' % data
+    
+    def testGoodEchoWithPosArg(self):
+        rt = self.configureServer()
+        rt.start(blocking=False)
+        cherrypy.log.error_log.setLevel(logging.WARNING)
+        cherrypy.log.access_log.setLevel(logging.WARNING)
+        for method in ['POST']:
+            data, code, type = self.makeRequest('/rest/echo/stuff', 
+                                                {'data': 'unit test'},
+                                                method, 
+                                                'text/json')
+            assert code == 200, \
+                 'Got a return code != 200 (got %s)' % code
+            assert type == 'text/json'
+            assert data == '{"args": ["stuff"], "kwargs": {"data": "unit test"}}', 'got unexpected response %s' % data
+            
+    def testBadMethodEcho(self):
+        rt = self.configureServer()
+        rt.start(blocking=False)
+        cherrypy.log.error_log.setLevel(logging.WARNING)
+        cherrypy.log.access_log.setLevel(logging.WARNING)
+        
+        for method in ['GET']:
+            data, code, type = self.makeRequest('/rest/echo', {'data': 'unit test'}, 
+                                          method, 'text/json')
+            assert int(code) == 405, "Didn't get a 'Method Not Allowed' response for %s (got %s)" % (method, code)
+            assert type == 'text/json' 
+        rt.stop()
+          
+    def testBadVerbEcho(self):
+        rt = self.configureServer()
+        rt.start(blocking=False)
+        cherrypy.log.error_log.setLevel(logging.WARNING)
+        cherrypy.log.access_log.setLevel(logging.WARNING)
+        
+        for method in ['DELETE', 'PUT']:
+            data, code, type = self.makeRequest('/rest/echo', {'data': 'unit test'}, 
+                                          method, 'text/json')
+            assert int(code) == 501, "Didn't get a 'Not Implemented' response for %s (got %s), message: %s" % (method, code, data)
+            assert type == 'text/json'
+        rt.stop()
+    
+    def testPing(self):
+        rt = self.configureServer()
+        rt.start(blocking=False)
+        cherrypy.log.error_log.setLevel(logging.WARNING)
+        cherrypy.log.access_log.setLevel(logging.WARNING)
+        data, code, type = self.makeRequest('/rest/ping', 
+                                          type='GET', accept='text/json')
+        assert code == 200, 'Got a return code != 200 (got %s), message: %s' % (code, data)
+        assert type == 'text/json'
+        assert data == '"ping"', 'got unexpected response %s' % data
+        rt.stop()
+    
+    def makeRequest(self, uri='/rest/', values=None, type='GET', accept="text/plain"):
+        headers = {}
+        headers = {"Content-type": "application/x-www-form-urlencoded",
+                   "Accept": accept}
+        data = None
+        if values:
+            data = urllib.urlencode(values)
+        if type != 'POST':
+            uri = '%s?%s' % (uri, data)
+        conn = HTTPConnection('localhost:8080')
+        conn.connect()
+        conn.request(type, uri, data, headers)
+        response = conn.getresponse()
+        
+        data = response.read()
+        conn.close()
+        return data, response.status, response.getheader('content-type').split(';')[0]
     
     def testA(self):
-        def makeRequest(uri='/rest/', values=None, type='GET'):
-            headers = {}
-            headers = {"Content-type": "application/x-www-form-urlencoded",
-                       "Accept": "text/plain"}
-            
-            data = urllib.urlencode(values)
-            if type != 'POST':
-                uri = '%s?%s' % (uri, data)
-            conn = HTTPConnection('localhost:8080')
-            conn.connect()
-            conn.request(type, uri, data, headers)
-            response = conn.getresponse()
-                
-            data = response.read()
-            conn.close()
-            return data, response.status
-            
+        rt = self.configureServer()
+        rt.start(blocking=False)
+        cherrypy.log.error_log.setLevel(logging.WARNING)
+        cherrypy.log.access_log.setLevel(logging.WARNING)
         for t in ['GET', 'POST', 'PUT', 'DELETE', 'UPDATE']:
-                response = makeRequest(values={'value':1234})
+                response = self.makeRequest(values={'value':1234})
                 assert response[1] == 200, \
-                    "%s failed: %s - %s" % (t, response[1], response[2])
-    
+                 'Got a return code != 200 (got %s)' % response[1]
+        
+        rt.stop()
+        
     def testSanitisePass(self):
         config = Configuration()
         component = config.component_('UnitTests')
@@ -141,19 +243,19 @@ class RESTTest(unittest.TestCase):
         drm = DummyRESTModel(component)
         
         # Wrong type for input args
-        self.assertRaises(AssertionError, drm.list, [123, 123], {})
-        self.assertRaises(AssertionError, drm.list, ['abc', 'abc'], {})
-        self.assertRaises(AssertionError, drm.list, [], {'str':123, 'int':'abc'})
-        self.assertRaises(AssertionError, drm.list, [], {'str':'abc', 'int':'abc'})
-        self.assertRaises(AssertionError, drm.list, ['abc', 123], {})
-        self.assertRaises(AssertionError, drm.list, ['abc', 'abc'], {})
-        self.assertRaises(AssertionError, drm.list, [], {'str':123, 'int':'abc'})
-        self.assertRaises(AssertionError, drm.list, [], {'str':123, 'int':123})
-        self.assertRaises(AssertionError, drm.list, [], {'str':'abc', 'int':'abc'})
+        self.assertRaises(HTTPError, drm.list, [123, 123], {})
+        self.assertRaises(HTTPError, drm.list, ['abc', 'abc'], {})
+        self.assertRaises(HTTPError, drm.list, [], {'str':123, 'int':'abc'})
+        self.assertRaises(HTTPError, drm.list, [], {'str':'abc', 'int':'abc'})
+        self.assertRaises(HTTPError, drm.list, ['abc', 123], {})
+        self.assertRaises(HTTPError, drm.list, ['abc', 'abc'], {})
+        self.assertRaises(HTTPError, drm.list, [], {'str':123, 'int':'abc'})
+        self.assertRaises(HTTPError, drm.list, [], {'str':123, 'int':123})
+        self.assertRaises(HTTPError, drm.list, [], {'str':'abc', 'int':'abc'})
         
         # Incorrect values for input args
-        self.assertRaises(AssertionError, drm.list, [1234, 'abc'], {})
-        self.assertRaises(AssertionError, drm.list, [123, 'abcd'], {})
+        self.assertRaises(HTTPError, drm.list, [1234, 'abc'], {})
+        self.assertRaises(HTTPError, drm.list, [123, 'abcd'], {})
         
     def testSanitiseKeyFail(self):
         config = Configuration()
@@ -164,9 +266,9 @@ class RESTTest(unittest.TestCase):
         drm = DummyRESTModel(component)
         
         # Empty input data, when data is required
-        self.assertRaises(KeyError, drm.list, [], {})
+        self.assertRaises(HTTPError, drm.list, [], {})
         # Out of order input data
-        self.assertRaises(KeyError, drm.list, ['abc'], {'int':123})
+        self.assertRaises(HTTPError, drm.list, ['abc'], {'int':123})
     
     def testDAOBased(self):
         config = Configuration()

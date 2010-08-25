b5ad6e3946546277073ea884a71835118659b0cf
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:07:38 +0000

    update query for sqlite
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4039 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/JobGroup/Status.py
+++ src/python/WMCore/WMBS/SQLite/JobGroup/Status.py
@@ -6,10 +6,24 @@ SQLite implementation of JobGroup.Status
 """
 
 __all__ = []
-__revision__ = "$Id: Status.py,v 1.3 2008/11/21 17:11:25 sfoulkes Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: Status.py,v 1.4 2009/04/23 00:03:21 sryu Exp $"
+__version__ = "$Revision: 1.4 $"
 
 from WMCore.WMBS.MySQL.JobGroup.Status import Status as StatusMySQL
 
 class Status(StatusMySQL):
-    sql = StatusMySQL.sql
+    sql = """select (
+        select count(id) from wmbs_job wj
+            left outer join wmbs_group_job_acquired wa on wj.id = wa.job
+            left outer join wmbs_group_job_failed wf on wj.id = wf.job
+            left outer join wmbs_group_job_complete wc on wj.id = wc.job
+            where jobgroup=:jobgroup and wa.job is null
+                  and wf.job is null and wc.job is null
+        ) as av, (
+        select count(job) from wmbs_group_job_acquired where jobgroup=:jobgroup
+        ) as ac, (
+        select count(job) from wmbs_group_job_failed where jobgroup=:jobgroup
+        ) as fa, (
+        select count(job) from wmbs_group_job_complete where jobgroup=:jobgroup
+        ) as cm 
+    """

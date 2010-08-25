2b67989b3773bf98966e9a955a0e62b1230bffe7
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:48:35 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8020 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/RequestManager/RequestDB/Interface/ProdSystem/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+_Interface.ProdSystem_
+
+ReqMgr DB Interface for production system components
+
+"""
+__all__ = []
---
+++ src/python/WMCore/RequestManager/RequestDB/Interface/User/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+_Interface.User_
+
+API Libraries for interacting with Request DB for user related functionality
+
+
+"""
+__all__ = []

0578358a6fa6ed18c74b61095d68001a3934f9fe
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:52:13 +0000

    Added settings for global DBS
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5597 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSUpload/DefaultConfig.py
+++ src/python/WMComponent/DBSUpload/DefaultConfig.py
@@ -5,8 +5,8 @@ Defines default config values for DBSUpload specific
 parameters.
 """
 __all__ = []
-__revision__ = "$Id: DefaultConfig.py,v 1.6 2009/09/02 16:16:10 mnorman Exp $"
-__version__ = "$Revision: 1.6 $"
+__revision__ = "$Id: DefaultConfig.py,v 1.7 2009/11/24 16:34:45 mnorman Exp $"
+__version__ = "$Revision: 1.7 $"
 
 
 from WMCore.Agent.Configuration import Configuration
@@ -14,7 +14,7 @@ from WMCore.Agent.Configuration import Configuration
 config = Configuration()
 config.component_("DBSUpload")
 #The log level of the component. 
-config.DBSUpload.logLevel = 'INFO'
+config.DBSUpload.logLevel = 'DEBUG'
 
 # maximum number of threads we want to deal
 # with messages per pool.
@@ -39,9 +39,12 @@ config.DBSUpload.dbsversion = \
                             'DBS_2_0_6'
 #    'DBS_2_0_4'
 # Number of files taht will be Batch inserted into DBS
-config.DBSUpload.uploadFileMax = 10
+config.DBSUpload.uploadFileMax = 500
 
 config.DBSUpload.pollInterval = 10
+config.DBSUpload.globalDBSUrl = 'http://cmssrv49.fnal.gov:8989/DBS209/servlet/DBSServlet'
+#config.DBSUpload.globalDBSUrl = 'http://cmssrv49.fnal.gov:8989/DBS/servlet/DBSServlet'
+config.DBSUpload.globalDBSVer = 'DBS_2_0_8'
 
 #Config variables for block sizes in DBS
 config.DBSUpload.DBSMaxSize  = 999999999

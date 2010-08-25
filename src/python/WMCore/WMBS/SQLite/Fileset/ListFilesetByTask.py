f17706efa4bdf2c9596fd401169d3df6888288e9
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:22:47 +0000

    Minor fix of SQLite implementation of Fileset.ListFilesetByTask
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7681 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Fileset/ListFilesetByTask.py
+++ src/python/WMCore/WMBS/SQLite/Fileset/ListFilesetByTask.py
@@ -6,13 +6,13 @@ SQLite implementation of Fileset.ListFilesetByTask
 """
 
 __all__ = []
-__revision__ = "$Id: ListFilesetByTask.py,v 1.1 2010/06/01 17:15:51 riahi Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: ListFilesetByTask.py,v 1.2 2010/06/01 21:17:35 riahi Exp $"
+__version__ = "$Revision: 1.2 $"
 
 
 from WMCore.WMBS.MySQL.Fileset.ListFilesetByTask import ListFilesetByTask as ListFilesetByTaskMySQL
 
-class LoadFromName(ListFilesetByTaskMySQL):
+class ListFilesetByTask(ListFilesetByTaskMySQL):
     sql = ListFilesetByTaskMySQL.sql
 ~                                     
 

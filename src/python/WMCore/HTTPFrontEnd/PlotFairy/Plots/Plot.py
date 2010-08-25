e98610c83b3c4a3aefe4db987cb9f6359a5629ea
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:29:25 +0000

    Add in a dummy validation function for per plot input validation
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5268 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/HTTPFrontEnd/PlotFairy/Plots/Plot.py
+++ src/python/WMCore/HTTPFrontEnd/PlotFairy/Plots/Plot.py
@@ -13,6 +13,7 @@ class Plot(object):
         We use the __call__ method as a way of enforcing good behaviour, and 
         hopefully minimising memory leakage. 
         '''
+        input = self.validate_input(input)
         plot = self.plot(input)
         pyplot.close()
         return plot
@@ -23,8 +24,18 @@ class Plot(object):
         '''
         return None
     
+    def validate_input(self, input):
+        """
+        Dummy function to override to validate specific input for a plot.
+        """
+        return input
+    
     def siformat(self, val, unit='', long=False):
-        suffix = [(1e18,'E','exa'),(1e15,'P','peta'),(1e12,'T','tera'),(1e9,'G','giga'),(1e6,'M','mega'),(1e3,'k','kilo'),(1,'',''),(1e-3,'m','mili'),(1e-6,'u','micro'),(1e-9,'n','nano'),(1e-12,'p','pico'),(1e-15,'f','femto'),(1e-18,'a','atto')]
+        suffix = [(1e18,'E','exa'), (1e15,'P','peta'), (1e12,'T','tera'),
+                  (1e9,'G','giga'), (1e6,'M','mega'), (1e3,'k','kilo'),
+                  (1,'',''), (1e-3,'m','mili'), (1e-6,'u','micro'),
+                  (1e-9,'n','nano'),(1e-12,'p','pico'),(1e-15,'f','femto'),
+                  (1e-18,'a','atto')]
         use = 1
         if long:
             use = 2

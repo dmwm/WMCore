90a3c9e7119a26df23c25b59359e7722da60605b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:41:25 +0000

    Use DBSBufferFile to add algorithms and datasets to the buffer.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4548 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSBuffer/Handler/JobSuccessSlave.py
+++ src/python/WMComponent/DBSBuffer/Handler/JobSuccessSlave.py
@@ -4,21 +4,19 @@ DBS Buffer handler for JobSuccess event
 """
 __all__ = []
 
-__revision__ = "$Id: JobSuccessSlave.py,v 1.2 2009/06/10 16:29:16 mnorman Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: JobSuccessSlave.py,v 1.3 2009/07/15 20:41:30 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 __author__ = "mnorman@fnal.gov"
 
 from WMCore.Agent.BaseHandler import BaseHandler
 from WMCore.ThreadPool.ThreadPool import ThreadPool
 from WMCore.Agent.Configuration import loadConfigurationFile
 
-#from WMComponent.DBSBuffer.Database.Interface.addToBuffer import AddToBuffer
+from WMComponent.DBSBuffer.Database.Interface.DBSBufferFile import DBSBufferFile
 from WMCore.WMFactory import WMFactory
 
-#from WMComponent.ErrorHandler.Handler.DefaultSlave import DefaultSlave
 from WMCore.ThreadPool.ThreadSlave import ThreadSlave
 
-#import cPickle
 import os
 import string
 import logging
@@ -28,27 +26,24 @@ import threading
 
 from ProdCommon.FwkJobRep.ReportParser import readJobReport
 
-
-
 class JobSuccessSlave(ThreadSlave):
     """
     Default handler for create failures: slave version.
     """
 
-    def readJobReportInfo(self,jobReportFile):
+    def readJobReportInfo(self, jobReportFile):
         """
         _readJobReportInfo_
 
         Read the info from jobReport file
 
         """
-
-        jobReportFile=string.replace(jobReportFile,'file://','')
+        jobReportFile = string.replace(jobReportFile, "file://", "")
         if not os.path.exists(jobReportFile):
             logging.error("JobReport Not Found: %s" %jobReportFile)
             raise InvalidJobReport(jobReportFile)
         try:
-            jobreports=readJobReport(jobReportFile)
+            jobreports = readJobReport(jobReportFile)
         except:
             logging.debug("Invalid JobReport File: %s" %jobReportFile)
             raise InvalidJobReport(jobReportFile)
@@ -63,40 +58,38 @@ class JobSuccessSlave(ThreadSlave):
         # as we defined a threadpool we can enqueue our item
         # and move to the next.
 
+        jobReports = self.readJobReportInfo(parameters)
         myThread = threading.currentThread()
         myThread.transaction.begin()
 
-
-
         payload = parameters #I think this is correct; it's all that I've got to go on
         jobReports = self.readJobReportInfo(payload)
-        factory = WMFactory("dbsBuffer", "WMComponent.DBSBuffer.Database.Interface")
-        addToBuffer=factory.loadObject("AddToBuffer")
 
         for aFJR in jobReports:
-            for aFile in aFJR.files:
-                # This shouldn't be required any more
-                # Dataset is being added with workflow spec
-                for dataset in aFile.dataset:
-                    addToBuffer.addAlgo(dataset)
-                    addToBuffer.addDataset(dataset, algoInDBS=0)
-                    addToBuffer.updateDSFileCount(dataset)
-                    #Lets see if ALGO info is present in the dataset
-                    #### Lets fake test it
-                    #pset=aFile['PSetHash']="ABCDEFGHIJKL12345676"
-                    #print "FAKING PSetHash in file <<<<<<<<<"
-                    if aFile.has_key('PSetHash') and aFile['PSetHash'] != None:
-                       #Pass in the dataset, that contains all info about Algo
-                       # 
-                       addToBuffer.updateAlgo(dataset, pset)
-		dataset=aFile.dataset[0]
-		datasetPath='/'+dataset['PrimaryDataset']+'/'+ \
-                                        dataset['ProcessedDataset']+'/'+ \
-                                        dataset['DataTier']
-		addToBuffer.addFile(aFile, datasetPath)
+            for fwjrFile in aFJR.files:
+                bufferFile = DBSBufferFile(lfn = fwjrFile["LFN"], size = fwjrFile["Size"],
+                                           events = fwjrFile["TotalEvents"],
+                                           cksum = fwjrFile["Checksum"])
+                
+                datasetPath = "/%s/%s/%s" % (fwjrFile.dataset[0]["PrimaryDataset"],
+                                             fwjrFile.dataset[0]["ProcessedDataset"],
+                                             fwjrFile.dataset[0]["DataTier"])
+                bufferFile.setDatasetPath(datasetPath)
+
+                if fwjrFile.dataset[0]["PSetHash"] == None:
+                    fwjrFile.dataset[0]["PSetHash"] = "BOGUS"
+
+                myThread.logger.debug("Dataset Path567: %s" % datasetPath)
+                myThread.logger.debug("ALGO: %s %s %s %s" % (fwjrFile.dataset[0]["ApplicationName"], fwjrFile.dataset[0]["ApplicationVersion"], fwjrFile.dataset[0]["ApplicationFamily"], fwjrFile.dataset[0]["PSetHash"]))
+
+                bufferFile.setAlgorithm(appName = fwjrFile.dataset[0]["ApplicationName"],
+                                        appVer = fwjrFile.dataset[0]["ApplicationVersion"],
+                                        appFam = fwjrFile.dataset[0]["ApplicationFamily"],
+                                        psetHash = fwjrFile.dataset[0]["PSetHash"])
+                
+                bufferFile.create()
 
         myThread.transaction.commit()
-
         return
 
 

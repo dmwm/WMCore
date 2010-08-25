93ca4021b5276552a9641e377afdd04b36762969
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:42:49 +0000

    Don't need to subclass SecureRequests, just over ride Requests
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4569 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Services/AuthorisedService.py
+++ src/python/WMCore/Services/AuthorisedService.py
@@ -6,26 +6,26 @@ An AuthorisedService is the same as a Service but sends a cert/key with the url
 opener to access secured resources.
 """
 
-__revision__ = "$Id: AuthorisedService.py,v 1.9 2009/07/13 17:55:47 sryu Exp $"
-__version__ = "$Revision: 1.9 $"
+__revision__ = "$Id: AuthorisedService.py,v 1.10 2009/07/16 21:57:26 metson Exp $"
+__version__ = "$Revision: 1.10 $"
 
 import datetime, os, urllib, time
 
 from WMCore.WMException import WMException
 from WMCore.Services.Service import Service
-from WMCore.Services.Requests import SecureRequests
+from WMCore.Services.Requests import SecureRequests as Requests
 
-class AuthorisedService(SecureRequests, Service):
+class AuthorisedService(Service):
     """
     _AuthorisedService_
     
     (Warning: Order of inheritance is important since both has parent have 
      the same method (_getURLOpener))
-    TODO: better exception handling - make clear what exception is throwms
+    TODO: better exception handling - make clear what exception is thrown
     """
     def __init__(self, dict={}):
         try:
-            Service.__init__(self, dict)    
+            Service.__init__(self, dict)
         except WMException, ex:
             msg = str(ex)
             self["logger"].exception(msg)

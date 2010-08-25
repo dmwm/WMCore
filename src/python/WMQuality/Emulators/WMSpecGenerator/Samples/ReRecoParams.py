25e60314f2bb03652bb359da7e663ebd07c2789d
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:17:32 +0000

    update the ReReco Params
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7611 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMQuality/Emulators/WMSpecGenerator/Samples/ReRecoParams.py
+++ src/python/WMQuality/Emulators/WMSpecGenerator/Samples/ReRecoParams.py
@@ -1,13 +1,25 @@
 MinBiasWithoutEmulator = {
-    "Owner" : "DWMWTest",                         
-    "OutputTiers" : ["RECO", "ALCARECO"],
-    "AcquisitionEra" : "WMAgentCommissioining10",
-    "GlobalTag" :"GR10_P_V4::All",
-    "LFNCategory" : "/store/backfill/2",
-    "TemporaryLFNCategory": "/store/backfill/2/unmerged",
-    "ProcessingVersion" : "v1",
-    "Scenario" : "cosmics",
-    "CMSSWVersion" : "CMSSW_3_5_6",
-    "InputDatasets" : "/MinimumBias/BeamCommissioning09-v1/RAW",
-    "Emulate" : False,
+    "CmsPath": "/uscmst1/prod/sw/cms",
+    "AcquisitionEra": "WMAgentCommissioning10",
+    "Requester": "sfoulkes@fnal.gov",
+    "InputDataset": "/MinimumBias/Commissioning10-v4/RAW",
+    "CMSSWVersion": "CMSSW_3_5_8_patch3",
+    "ScramArch": "slc5_ia32_gcc434",
+    "ProcessingVersion": "v20scf",
+    "SkimInput": "output",
+    "GlobalTag": "GR10_P_v4::All",
+    
+    "ProcessingConfig": "http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/Configuration/GlobalRuns/python/rereco_FirstCollisions_MinimumBias_35X.py?revision=1.8",
+    "SkimConfig": "http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/Configuration/DataOps/python/prescaleskimmer.py?revision=1.1",
+    
+    "CouchUrl": "http://dmwmwriter:gutslap!@cmssrv52.fnal.gov:5984",
+    "CouchDBName": "wmagent_config_cache",
+    "Scenario": ""
+#     "scenario": "cosmics",
+#     "processingOutputModules": {"outputRECORECO": {"dataTier": "RECO", "filterName": ""},
+#                                 "outputALCARECOALCARECO": {"dataTier": "ALCARECO", "filterName": ""}},
+#     "skimOutputModules": {},
+#     "processingConfig": "",
+#     "skimConfig": ""
+    
     }

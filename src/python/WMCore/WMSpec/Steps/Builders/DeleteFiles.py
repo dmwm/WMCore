c4cf4f60e9f5203a39704950730f85188f14537e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:42:17 +0000

    Initial check-in of a step to delete files passed in from the stageOut system
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7133 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMSpec/Steps/Builders/DeleteFiles.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""
+_DeleteFiles_
+
+Builder implementation for DeleteFiles
+
+"""
+from WMCore.WMSpec.Steps.Builder import Builder
+from WMCore.WMSpec.ConfigSectionTree import nodeName
+
+
+
+
+
+class DeleteFiles(Builder):
+    """
+    _DeleteFiles_
+
+    Build a working area for a DeleteFiles step
+
+    """
+
+    def build(self, step, workingDir, **args):
+        """
+        _build_
+
+        implement build interface for DeleteFiles Step
+
+        """
+        stepName = nodeName(step)
+        stepWorkingArea = "%s/%s" % (workingDir, stepName)
+        self.installWorkingArea(step, stepWorkingArea)
+        print "Builders.DeleteFiles.build called on %s" % stepName
---
+++ src/python/WMCore/WMSpec/Steps/Executors/DeleteFiles.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+"""
+_Step.Executor.DeleteFiles_
+
+Implementation of an Executor for a Delete step
+
+"""
+
+__revision__ = "$Id: DeleteFiles.py,v 1.1 2010/04/21 16:16:40 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+import os
+import os.path
+import logging
+import signal
+
+from WMCore.WMSpec.Steps.Executor           import Executor
+from WMCore.FwkJobReport.Report             import Report
+
+import WMCore.Storage.StageOutMgr as StageOutMgr
+import WMCore.Storage.DeleteMgr as DeleteMgr
+        
+from WMCore.WMSpec.Steps.Executors.LogArchive import Alarm, alarmHandler
+
+class DeleteFiles(Executor):
+    """
+    A step run to clean up the unmerged files in a job
+
+    """
+
+
+    def pre(self, emulator = None):
+        """
+        _pre_
+
+        Pre execution checks
+
+        """
+
+        # Are we using an emulator?
+        if (emulator != None):
+            return emulator.emulatePre( self.step )
+
+
+        
+        print "Steps.Executors.DeleteFiles.pre called"
+        return None
+
+
+
+    def execute(self, emulator = None):
+        """
+        _execute_
+
+        """
+
+        # Are we using emulators again?
+        if (emulator != None):
+            return emulator.emulate( self.step, self.job )
+
+
+
+        # Look!  I can steal from StageOut
+        # DeleteMgr uses the same manager structure as StageOutMgr
+
+        overrides = {}
+        if hasattr(self.step, 'override'):
+            overrides = self.step.override.dictionary_()
+
+        # Set wait to 15 minutes
+        waitTime = overrides.get('waitTime', 900)
+
+        # Pull out StageOutMgr Overrides
+        stageOutCall = {}
+        if overrides.has_key("command") and overrides.has_key("option") \
+               and overrides.has_key("se-name") and overrides.has_key("lfn-prefix"):
+            stageOutCall['command']    = overrides.get('command')
+            stageOutCall['option']     = overrides.get('option')
+            stageOutCall['se-name']    = overrides.get('se-name')
+            stageOutCall['lfn-prefix'] = overrides.get('lfn-prefix')
+
+        # naw man, this is real
+        # iterate over all the incoming files
+        manager = DeleteMgr.DeleteMgr(**stageOutCall)
+        manager.numberOfRetries = self.step.retryCount
+        manager.retryPauseTime  = self.step.retryDelay
+
+
+        # This is where the deleted files go
+        filesDeleted = []
+
+        for file in self.job['input_files']:
+            fileForTransfer = {'LFN': file.get('lfn'),
+                               'PFN': None,  # PFNs are assigned in the Delete Manager
+                               'SEName' : None,  # SEName is assigned in the delete manager
+                               'StageOutCommand': None}
+
+            signal.signal(signal.SIGALRM, alarmHandler)
+            signal.alarm(waitTime)
+
+            try:
+                manager(fileToDelete = fileForTransfer)
+                #Afterwards, the file should have updated info.
+                filesDeleted.append(fileForTransfer)
+
+            except Alarm:
+                msg = "Indefinite hang during stageOut of logArchive"
+                logging.error(msg)
+            except:
+                print "Exception raised in stageout executor, how do we handle that?"
+                raise
+            signal.alarm(0)
+
+
+        # Now we've got to put things in the report
+        for file in filesDeleted:
+            self.report.addRemovedCleanupFile(**file)
+
+                
+        return
+
+
+    def post(self, emulator = None):
+        """
+        _post_
+
+        Post execution checkpointing
+
+        """
+        #Another emulator check
+        if (emulator != None):
+            return emulator.emulatePost( self.step )
+        
+        print "Steps.Executors.DeleteFiles.post called"
+        return None

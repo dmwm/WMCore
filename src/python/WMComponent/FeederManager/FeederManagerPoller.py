7534b7d56a0f07da398ec2339f08c58cfa93481f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:48:40 +0000

    Add condition to remove a fileset from the FeederManager management, the fileset must be closed
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4660 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/FeederManager/FeederManagerPoller.py
+++ src/python/WMComponent/FeederManager/FeederManagerPoller.py
@@ -3,9 +3,10 @@
 """
 Filesets and Feeders manager
 """
+
 __all__ = []
-__revision__ = "$Id: FeederManagerPoller.py,v 1.1 2009/07/14 13:37:22 riahi Exp $" 
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: FeederManagerPoller.py,v 1.2 2009/07/25 11:01:57 riahi Exp $" 
+__version__ = "$Revision: 1.2 $"
 
 import threading
 import logging
@@ -15,6 +16,12 @@ from WMCore.WMBSFeeder.DBS.Feeder import Feeder
 from WMCore.WMBS.Fileset import Fileset
 #from Registry import retrieveFeederImpl, RegistryError
 from ProdCommon.ThreadTools import WorkQueue
+
+#from sqlalchemy import create_engine
+#import sqlalchemy.pool as pool
+#from sqlalchemy.exceptions import IntegrityError, OperationalError
+#from WMCore.WMBS.Factory import SQLFactory
+
 
 # Tracks filesets watched:{filesetName:filesetObject}
 FILESET_WATCH = {}
@@ -37,7 +44,7 @@ class FeederManagerPoller(BaseWorkerThread):
     def setup(self, parameters):
         """
         Load DB objects required for queries
-        """
+        """ 
         myThread = threading.currentThread()
         factory = WMFactory("default", \
             "WMComponent.FeederManager.Database." + myThread.dialect)
@@ -59,57 +66,64 @@ class FeederManagerPoller(BaseWorkerThread):
         for fileset in range(len(managedFilesets)):
 
             # Poll for the new fileset only
-            # FIXME: Add (and the fileset is closed)  
             if managedFilesets[fileset]['name'] in FILESET_WATCH:
                 logging.debug \
        ('fileset %s is in mn_filesets' % managedFilesets[fileset]['name']) 
 
             else: 
 
-                logging.debug("I m processing %s %s" % \
+                logging.debug("Processing %s %s" % \
   ( managedFilesets[fileset]['id'] , managedFilesets[fileset]['name'] ) )
                 filesetToUpdate = Fileset(id=managedFilesets[fileset]['id'])
                 filesetToUpdate.loadData()
-                logging.debug("%s is now processed:" % filesetToUpdate.name)
                 # lock me!
                 FILESET_WATCH[filesetToUpdate.name] = filesetToUpdate
-
-
 
         # WorkQueue worker
         for name, fileset in FILESET_WATCH.items():
 
             logging.debug("Will poll %s : %s" % (name, fileset.id))
             self.workq.enqueue(name, fileset)
-            logging.debug("%s is enqueued" % name)
 
         for key, filesets in self.workq.__iter__():
 
             fileset = FILESET_WATCH[key]
             logging.debug \
-      ("the poll key %s result %s is ready !" % (key,fileset.id))
+      ("the poll key %s result %s is ready !" % (key,str(fileset.id)))
+
+
+            # FIXME: Get it more generic 
+            feederId = self.queries.getFeederId("Feeder")
+            logging.debug("the Feeder %s has processed %s and is \
+                  removing it if closed" % (feederId , fileset.name ) ) 
+
 
             # Finally delete fileset 
-            # lock me!
-            del FILESET_WATCH[key]
+            # If the fileset is closed remove it
+            if fileset.open == False: 
+                self.queries.removeManagedFilesets(fileset.id, feederId)
+                # lock me!
+                del FILESET_WATCH[key]
 
     # Switch to static method if needed
     def pollExternal(self, fileset):
         """
         Call relevant external source and get file details
         """
+
         # Looking for!  
-        ##try:
+        #try:
              # fileset.source can be dataset feeder
-        ##     feeder = retrieveFeederImpl(fileset.source)
-        ##except RegistryError:
-        ##     msg = "WMBSFeeder plugin \'%s\' unknown" % fileset.source
-        ##     logging.error(msg)
-        ##     logging.error("aborting poll for...")
-        ##     raise RuntimeError, msg
+             #fileset.source 
+        #     feeder = retrieveFeederImpl("Feeder")
+        #except RegistryError:
+        #     msg = "WMBSFeeder plugin \'%s\' unknown" % fileset.source
+        #     logging.error(msg)
+        #     logging.error("aborting poll for...")
+        #     raise RuntimeError, msg
     
         # do we have any parents we need
-        ##if fileset.parents and fileset.listNewFiles()\ 
+        #if fileset.parents and fileset.listNewFiles()\ 
                  # and not fileset.listNewFiles()[0].parents(): 
         # get parentage from dbs
         ##     try:
@@ -122,15 +136,11 @@ class FeederManagerPoller(BaseWorkerThread):
         
         ##fileset = parentFeeder.getParentsForNewFiles(fileset)
 
-        # FIXME: Get it more generique
-        feederId = self.queries.getFeederId("Feeder")
-        logging.debug("the Feeder %s is processing %s" % \
-             (feederId , fileset.name ) )
-        logging.debug("Feeder called for %s" % fileset.name)
+        # FIXME: Get it more generic
+        logging.debug("feeder called for %s" % fileset.name)
         feeder = Feeder()
         try:
             feeder(fileset)
-            self.queries.removeManagedFilesets(fileset.id, feederId)
         except:
             logging.debug("ERROR when calling Feeder")
 

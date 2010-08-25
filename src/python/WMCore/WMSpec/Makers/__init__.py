3c0f40caf984f4c598c2beb6204ef183b9a9682a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:26:22 +0000

    init file for Makers
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4321 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMSpec/Makers/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+_WMCore.WMSpec.Makers_
+
+"""
+__revision__ = "$Id: __init__.py,v 1.1 2009/06/12 21:24:51 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+__all__ = []

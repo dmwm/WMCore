5c5d10c70f1c670aec0d0f751a482e34a6a3ba02
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:38:07 +0000

    Preliminary destructor for ThreadPool for MySQL
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4500 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/ThreadPool/MySQL/Destroy.py
@@ -0,0 +1,35 @@
+#/usr/bin/env python2.4
+"""
+_Destroy_
+
+"""
+
+__revision__ = "$Id: Destroy.py,v 1.1 2009/07/13 19:37:40 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+import threading
+
+from WMCore.Database.DBCreator import DBCreator
+
+from WMCore.WMException import WMException
+from WMCore.WMExceptions import WMEXCEPTION
+
+class Destroy(DBCreator):    
+    def __init__(self, logger = None, dbi = None):
+        """
+        _init_
+
+        Call the DBCreator constructor and create the list of required tables.
+        """
+        myThread = threading.currentThread()
+
+        if logger == None:
+            logger = myThread.logger
+        if dbi == None:
+            dbi = myThread.dbi
+            
+        DBCreator.__init__(self, logger, dbi)
+
+        self.create["03tp_threadpool"]             = "DROP TABLE tp_threadpool"
+        self.create["02tp_threadpool_buffer_in"]   = "DROP TABLE tp_threadpool_buffer_in"
+        self.create["01tp_threadpool_buffer_out"]  = "DROP TABLE tp_threadpool_buffer_out"

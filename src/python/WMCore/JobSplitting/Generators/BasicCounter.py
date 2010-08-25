2d91b7ac60a8d499c8a5337128704257df87275f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:06:00 +0000

    Placeholder for including job count information used to manage LFN group settings
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5789 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/JobSplitting/Generators/BasicCounter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""
+_BasicCounter_
+
+Count jobs created so far for a task and insert the count into the job
+
+"""
+from WMCore.JobSplitting.Generators.GeneratorInterface import GeneratorInterface
+
+
+
+class BasicCounter(GeneratorInterface):
+    """
+    _BasicCounter_
+
+    Incremental job counter
+
+    TODO: Use WMAgent couch to get count for the particular task as starting value in ctor
+          Or use options for better control
+    
+    """
+    def __init__(self, **options):
+        GeneratorInterface.__init__(self, **options)
+        self.count = 0
+
+    def __call__(self, wmbsJob):
+        
+
+        wmbsJob['counter'] = self.count
+        self.count += 1
+
+

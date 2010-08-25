9cd3fd19193f61ff2dd491c23d30a4dd1f3b607a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:35:55 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5361 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/Oracle/Jobs/GetOutputMap.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+_GetOutputMap_
+
+Oracle implementation of Jobs.GetOutputMap
+"""
+
+__revision__ = "$Id: GetOutputMap.py,v 1.1 2009/10/14 16:47:18 sfoulkes Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.WMBS.MySQL.Jobs.GetOutputMap import GetOutputMap as MySQLGetOutputMap
+
+class GetOutputMap(MySQLGetOutputMap):
+    """
+    Identical to MySQL version for now.
+    """
+    pass

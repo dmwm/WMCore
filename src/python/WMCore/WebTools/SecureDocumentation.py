f288ffa75b49d7d5922bc5c0293c6e8402d44279
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:58:36 +0000

    Changes for new style OID
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5689 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/SecureDocumentation.py
+++ src/python/WMCore/WebTools/SecureDocumentation.py
@@ -3,13 +3,6 @@ import cherrypy
 from WMCore.WebTools.Page import TemplatedPage
 from os import listdir
 
-def check_authz(permissions, fullname, dn):
-    if (permissions.get('Admin',None) == 'T2_BR_UERJ') or \
-           (permissions.get('L2',None) == 'CMS DMWM'):
-        if fullname != 'Bad guy':
-            return True # authorized
-    return False
-
 class SecureDocumentation(TemplatedPage):
     """
     The documentation for the framework
@@ -31,6 +24,8 @@ class SecureDocumentation(TemplatedPage):
         for k in oidinfo['permissions'].keys():
             index += "<li><b>%s</b>: %s</li>\n" % (k, oidinfo['permissions'][k])
         index += "</ul></li></ul>\n"
+        index += "<a href='CernDocs'>CERN docs</a>\n"
+        index += "<a href='UerjDocs'>UERJ docs</a>\n"
         index += "</div>\n"
         index += "<ol>"
         for t in templates:
@@ -40,20 +35,16 @@ class SecureDocumentation(TemplatedPage):
                                                       t.replace('.tmpl', ''))
         index = "%s\n<li><a href='https://twiki.cern.ch/twiki/bin/view/CMS/DMWebtools'>twiki</a>" % (index)
         index = "%s\n</ol>" % (index)
+        
         return index
 
     @expose
-    @tools.cernoid(authzfunc=check_authz)
-    def SpecialDocs(self):
-        return "This is a secret documentation for special docs"
-
-    @expose
-    @tools.cernoid()
+    @tools.cernoid(site='T2_BR_UERJ')
     def UerjDocs(self):
         return "This is a secret documentation about the T2 UERJ site"
 
     @expose
-    @tools.cernoid()
+    @tools.cernoid(site='T1_CH_CERN')
     def CernDocs(self):
         return "This is a secret documentation about the T1 CERN site"
 

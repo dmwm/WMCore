a3c137bd64178562abd5d6386f98a25f0300cabb
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:47:52 +0000

    Change how we call the DAO's from the Model class
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4648 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/HTTPFrontEnd/WMBS/WMBSRESTModel.py
+++ src/python/WMCore/HTTPFrontEnd/WMBS/WMBSRESTModel.py
@@ -18,9 +18,21 @@ class WMBSRESTModel(RESTModel):
         self.daofactory = DAOFactory(package = "WMCore.WMBS",
                                      logger = self,
                                      dbinterface = self.dbi)
+        self.methods = {'GET':{'jobs':{'dao': JobsByState(self, self.dbi),
+                                       'call': self.sanitise,
+                                       'version': 1}}}
 
-    def handle_get(self, args=[], kwargs={}):
-        if args[0].lower() == 'jobs':
-            jbs = JobsByState(self, self.dbi)
-
-            return jbs.execute()
+    def sanitise(self, verb, args, kwargs):
+        """
+        Take the arguments and ignore everything apart from the first value in 
+        args - this is the method name. We can then use this to pull out 
+        configuration from self.methods.
+        
+        I'm not sure how secure this is. There is presumably a way that you 
+        could inject a different dao into the methods dictionary, but I don't 
+        know how you would do that without compromising the machine running the 
+        server, and even then I don't know how you'd do it without changing code
+        and disrupting the service.
+        """
+        return self.methods[verb][args[0]]['dao'].execute()
+        

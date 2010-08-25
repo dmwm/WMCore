b9ebbb4ecaa4be3d0cd15abd9704df94741724ae
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 05:27:38 +0000

    Fix so that no attempts at duplicates
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8521 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Files/SetParentage.py
+++ src/python/WMCore/WMBS/MySQL/Files/SetParentage.py
@@ -5,13 +5,13 @@ MySQL implementation of File.SetParentage
 Make the parentage link between two file lfns in bulk
 """
 __all__ = []
-__revision__ = "$Id: SetParentage.py,v 1.1 2010/08/13 16:41:56 mnorman Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: SetParentage.py,v 1.2 2010/08/13 18:52:50 mnorman Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class SetParentage(DBFormatter):
-    sql = """INSERT INTO wmbs_file_parent (child, parent)
+    sql = """INSERT IGNORE INTO wmbs_file_parent (child, parent)
              SELECT DISTINCT wfd1.id, wfd2.id
              FROM wmbs_file_details wfd1 INNER JOIN wmbs_file_details wfd2
              WHERE wfd1.lfn = :child
--- src/python/WMCore/WMBS/Oracle/Files/SetParentage.py
+++ src/python/WMCore/WMBS/Oracle/Files/SetParentage.py
@@ -5,8 +5,8 @@ Oracle implementation of File.SetParentage
 Make the parentage link between two file lfns in bulk
 """
 __all__ = []
-__revision__ = "$Id: SetParentage.py,v 1.1 2010/08/13 16:41:57 mnorman Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: SetParentage.py,v 1.2 2010/08/13 18:52:51 mnorman Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WMBS.MySQL.Files.SetParentage import SetParentage as MySQLSetParentage
 
@@ -16,4 +16,12 @@ class SetParentage(MySQLSetParentage):
 
 
     Identical to MySQL version
+    """
+
+    sql = """INSERT IGNORE INTO wmbs_file_parent (child, parent)
+             SELECT DISTINCT wfd1.id, wfd2.id
+             FROM wmbs_file_details wfd1 INNER JOIN wmbs_file_details wfd2
+             WHERE wfd1.lfn = :child
+             AND wfd2.lfn = :parent
+             AND NOT EXISTS (SELECT child FROM wmbs_file_parent WHERE child = wfd1.id AND parent = wfd2.id)
     """

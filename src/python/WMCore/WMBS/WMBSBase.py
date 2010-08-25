b69eb1bb5e2c7b6492535a325fee4d99d6d1acf5
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:25:59 +0000

    inherit from WMConnectionBase class
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4315 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/WMBSBase.py
+++ src/python/WMCore/WMBS/WMBSBase.py
@@ -5,15 +5,12 @@ _WMBSBase_
 Generic methods used by all of the WMBS classes.
 """
 
-__revision__ = "$Id: WMBSBase.py,v 1.2 2009/05/08 16:04:10 sfoulkes Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: WMBSBase.py,v 1.3 2009/06/12 16:37:54 sryu Exp $"
+__version__ = "$Revision: 1.3 $"
 
-import threading
+from WMCore.WMConnectionBase import WMConnectionBase
 
-from WMCore.Database.Transaction import Transaction
-from WMCore.DAOFactory import DAOFactory
-
-class WMBSBase:
+class WMBSBase(WMConnectionBase):
     """
     Generic methods used by all of the WMBS classes.
     """
@@ -26,60 +23,5 @@ class WMBSBase:
         check to see if a transaction object has been created.  If none exists,
         create one but leave the transaction closed.
         """
-        self.myThread = threading.currentThread()
-        self.logger = self.myThread.logger
-        self.dialect = self.myThread.dialect
-        self.dbi = self.myThread.dbi
-        self.daofactory = DAOFactory(package = "WMCore.WMBS",
-                                     logger = self.logger,
-                                     dbinterface = self.dbi)
-
-        if "transaction" not in dir(self.myThread):
-            self.myThread.transaction = Transaction(self.dbi)
-            self.myThread.transaction.commit()
-
-        return
-
-    def getDBConn(self):
-        """
-        _getDBConn_
-
-        Retrieve the database connection that is associated with the current
-        dataabase transaction.
-        """
-        return self.myThread.transaction.conn
-
-    def beginTransaction(self):
-        """
-        _beginTransaction_
-
-        Begin a database transaction if one does not already exist.
-        """
-        if self.myThread.transaction.conn == None:
-            self.myThread.transaction.begin()
-            return False
-
-        return True
-
-    def existingTransaction(self):
-        """
-        _existingTransaction_
-
-        Return True if there is an open transaction, False otherwise.
-        """
-        if self.myThread.transaction.conn != None:
-            return True
-
-        return False
-
-    def commitTransaction(self, existingTransaction):
-        """
-        _commitTransaction_
-
-        Commit a database transaction that was begun by self.beginTransaction().
-        """
-        if not existingTransaction:
-            self.myThread.transaction.commit()
-            self.myThread.transaction.conn = None
-
-        return
+        WMConnectionBase.__init__(self, daoPackage = "WMCore.WMBS")
+        

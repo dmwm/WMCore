9468b14c87ad904065bef2485dd874e28d07ae77
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:16:38 +0000

    Lose the dependancy on BeautifulSoup Make an abridged cache file to minimise work the next time around
    some truly hateful code....
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5940 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Services/SAM/SAM.py
+++ src/python/WMCore/Services/SAM/SAM.py
@@ -5,37 +5,62 @@ _SAM_
 Talk to the SAM Service to get site status from the results of SAM tests.
 """
 
-__revision__ = "$Id: SAM.py,v 1.3 2009/10/12 15:05:40 swakef Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: SAM.py,v 1.4 2010/01/08 23:26:36 metson Exp $"
+__version__ = "$Revision: 1.4 $"
 
-from BeautifulSoup import BeautifulSoup
 from WMCore.Services.Service import Service
 
 class SAM(Service):
     def getCMSSWInstalls(self, ce = None):
         """
         Contact the SAM test page and get the tested, installed software on a CE.
-        TODO: improved error handling
+        
+        Can't use an XML parser because it's not compliant XML...
         """
-        url = "funct=TestResultLatest&nodename=%s&vo=cms&testname=CE-cms-swinst" % ce
+        inputdata = {"funct":"TestResultLatest",
+                    "nodename":ce,
+                    "vo":"cms",
+                    "testname":"CE-cms-swinst"}
         file = 'sam_cmssw_inst_%s.html' % ce
-        pageSoup = BeautifulSoup(self.refreshCache(file, url))
+        
+        f = self.refreshCache(file, url = '/sam/sam.py', inputdata = inputdata)
+        lines = f.readlines()
+        f.close()
+        
+        start = []
+        end = []
+        for i in lines:
+            if i.startswith('<ol'):
+                idx = lines.index(i)
+                start.append(idx)
+            elif i.endswith('</ol>\n'):
+                idx = lines.index(i)
+                end.append(idx)
+                # Change lines[idx] so we don't find it next time...
+                lines[idx] = '----'
+        
+        # Here I overwrite the cached file with an abridged version, to speed 
+        # things up next access, so long as the file hasn't expired.
+        f = open(file, 'w')
+        releases = {}
+        assert len(start) == len(end)
+        for s,e in zip(start, end):
+            arch = ""
+            try:
+                #sometimes the class is quoted...
+                arch = lines[s].split('"')[3]
+            except:
+                #and sometimes its not!
+                arch = lines[s].split('"')[1]
+            f.write(lines[s])
+            releases[arch] = []
+            for rel in lines[s+1: e]:
+                releases[arch].append(rel[5:-6])
+            f.write(rel)
+            f.write('</ol>\n')
+        f.close()
 
-        swlists = pageSoup.findAll('ol', { "class" : "CMSSW-list"})
-        self.logger.debug(swlists)
-
-        software = {}
-        for tag in swlists:
-            children = []
-            for i in tag.contents:
-                child = i.string
-                if child != '\n' and child != 'error':
-                    children.append(child)
-
-            children.sort()
-            children.reverse()
-            software[tag['id']] = children
-        return software
+        return releases
 
 if __name__ == '__main__':
     import logging
@@ -46,10 +71,10 @@ if __name__ == '__main__':
                     filemode = 'w')
 
     dict = {}
-    dict['endpoint'] = 'https://lcg-sam.cern.ch:8443/sam/sam.py?'
+    dict['endpoint'] = 'https://lcg-sam.cern.ch:8443'
     dict['cachepath'] = '/tmp/sam'
     dict['type'] = 'text/json'
     dict['logger'] = logging.getLogger('SAMtest')
 
     sam = SAM(dict = dict)
-    print sam.getCMSSWInstalls(ce = 'lcgce02.gridpp.rl.ac.uk')
+    print sam.getCMSSWInstalls(ce = 'lcgce01.phy.bris.ac.uk')

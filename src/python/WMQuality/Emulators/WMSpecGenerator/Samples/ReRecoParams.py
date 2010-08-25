762bd91c7388a2015178f32304addb09fafd2513
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:19:02 +0000

    disable couchDB for emulator test
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7631 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMQuality/Emulators/WMSpecGenerator/Samples/ReRecoParams.py
+++ src/python/WMQuality/Emulators/WMSpecGenerator/Samples/ReRecoParams.py
@@ -1,3 +1,32 @@
+# this is for actual test
+#MinBiasWithoutEmulator = {
+#    "CmsPath": "/uscmst1/prod/sw/cms",
+#    "AcquisitionEra": "WMAgentCommissioning10",
+#    "Requester": "sfoulkes@fnal.gov",
+#    "InputDataset": "/MinimumBias/Commissioning10-v4/RAW",
+#    "CMSSWVersion": "CMSSW_3_5_8_patch3",
+#    "ScramArch": "slc5_ia32_gcc434",
+#    "ProcessingVersion": "v20scf",
+#    "SkimInput": "output",
+#    "GlobalTag": "GR10_P_v4::All",
+#    
+#    "ProcessingConfig": "http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/Configuration/GlobalRuns/python/rereco_FirstCollisions_MinimumBias_35X.py?revision=1.8",
+#    "SkimConfig": "http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/Configuration/DataOps/python/prescaleskimmer.py?revision=1.1",
+#    
+#    "CouchUrl": "http://dmwmwriter:gutslap!@cmssrv52.fnal.gov:5984",
+#    "CouchDBName": "wmagent_config_cache",
+#    "Scenario": ""
+##     "scenario": "cosmics",
+##     "processingOutputModules": {"outputRECORECO": {"dataTier": "RECO", "filterName": ""},
+##                                 "outputALCARECOALCARECO": {"dataTier": "ALCARECO", "filterName": ""}},
+##     "skimOutputModules": {},
+##     "processingConfig": "",
+##     "skimConfig": ""
+#    
+#    }
+
+
+# This for the emulator test.
 MinBiasWithoutEmulator = {
     "CmsPath": "/uscmst1/prod/sw/cms",
     "AcquisitionEra": "WMAgentCommissioning10",
@@ -12,8 +41,8 @@ MinBiasWithoutEmulator = {
     "ProcessingConfig": "http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/Configuration/GlobalRuns/python/rereco_FirstCollisions_MinimumBias_35X.py?revision=1.8",
     "SkimConfig": "http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/Configuration/DataOps/python/prescaleskimmer.py?revision=1.1",
     
-    "CouchUrl": "http://dmwmwriter:gutslap!@cmssrv52.fnal.gov:5984",
-    "CouchDBName": "wmagent_config_cache",
+    "CouchUrl": None,
+    "CouchDBName": None,
     "Scenario": ""
 #     "scenario": "cosmics",
 #     "processingOutputModules": {"outputRECORECO": {"dataTier": "RECO", "filterName": ""},

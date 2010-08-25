040c5c25c1741c6431e6502a39125d513b99f3ce
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:43:57 +0000

    added changestate DAO shims for oracle/sqlite
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4587 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Jobs/ChangeState.py
+++ src/python/WMCore/WMBS/Oracle/Jobs/ChangeState.py
@@ -6,8 +6,8 @@ Sqlite implementation of Subscription.GetJobGroups
 """
 
 __all__ = []
-__revision__ = "$Id: ChangeState.py,v 1.2 2009/07/17 20:21:17 meloam Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: ChangeState.py,v 1.1 2009/07/17 20:21:17 meloam Exp $"
+__version__ = "$Revision: 1.1 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 from WMCore.WMBS.MySQL.Jobs.ChangeState import ChangeState as BaseDAO

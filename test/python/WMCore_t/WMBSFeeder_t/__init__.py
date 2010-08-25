7d250af9f6953c6537fc2b47760af4f448cace60
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:35:00 +0000

    need to have __init__.py
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5348 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/python/WMCore_t/WMBSFeeder_t/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+_MsgService_t_
+
+MsgService test methods
+
+
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2009/10/13 23:05:35 meloam Exp $"
+__version__ = "$Revision: 1.1 $"
+

71323a5385e16beb0ca99be3390672ec459cf554
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:36:23 +0000

    Add tests for seToCMSName and cmsNametoPhEDExNode, make CE test pass
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4473 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/Services_t/SiteDB_t/SiteDB_t.py
+++ test/python/WMCore_t/Services_t/SiteDB_t/SiteDB_t.py
@@ -2,8 +2,8 @@
 """
 Test case for SiteDB
 """
-__revision__ = "$Id: SiteDB_t.py,v 1.6 2009/03/31 14:52:02 metson Exp $"
-__version__  = "$Revision: 1.6 $"
+__revision__ = "$Id: SiteDB_t.py,v 1.7 2009/07/10 22:17:57 ewv Exp $"
+__version__  = "$Revision: 1.7 $"
 __author__   = "ewv@fnal.gov"
 
 import unittest
@@ -27,35 +27,46 @@ class SiteDBTest(unittest.TestCase):
         self.testCmsNametoCE()
         self.testJSONParser()
         self.testDNUserName()
+        self.testSEtoCmsName()
+        self.testCmsNametoPhEDExNode()
+
+
+    def testCmsNametoPhEDExNode(self):
+        """
+        Tests CmsNametoSE
+        """
+        target = ['T1_US_FNAL_MSS','T1_US_FNAL_Buffer']
+        results = self.mySiteDB.cmsNametoPhEDExNode("T1_US_FNAL")
+        self.failUnless(results.sort() == target.sort())
 
 
     def testCmsNametoSE(self):
         """
         Tests CmsNametoSE
         """
-        target = ['ralsrmb.rl.ac.uk',
-                  'ralsrma.rl.ac.uk', 'srm-cms.gridpp.rl.ac.uk',
-                  'ralsrme.rl.ac.uk', 'ralsrmf.rl.ac.uk']
+        target = ['srm-cms.gridpp.rl.ac.uk','ralsrma.rl.ac.uk',
+                  'ralsrmf.rl.ac.uk','ralsrme.rl.ac.uk','ralsrmb.rl.ac.uk']
         results = self.mySiteDB.cmsNametoSE("T1_UK_RAL")
         self.failUnless(results.sort() == target.sort())
+
+
+    def testSEtoCmsName(self):
+        """
+        Tests CmsNametoSE
+        """
+        target = 'T1_US_FNAL'
+        results = self.mySiteDB.seToCMSName("cmssrm.fnal.gov")
+        self.failUnless(results == target)
 
 
     def testCmsNametoCE(self):
         """
         Tests CmsNametoCE
         """
-        target = ['cclcgceli03.in2p3.fr', 'ce-2-fzk.gridka.de', 
-                  'ce-1-fzk.gridka.de', 'cclcgceli04.in2p3.fr', 
-                  'ce-4-fzk.gridka.de', 'ce-3-fzk.gridka.de', 'ce06.pic.es', 
-                  'ce-5-fzk.gridka.de', 'ce07.pic.es', 
-                  'w-ce01.grid.sinica.edu.tw', 'ce04-lcg.cr.cnaf.infn.it', 
-                  'lcg00125.grid.sinica.edu.tw', 'w-ce02.grid.sinica.edu.tw', 
-                  'ce05-lcg.cr.cnaf.infn.it ', 'ce06-lcg.cr.cnaf.infn.it', 
-                  'cmsosgce4.fnal.gov', 'lcgce02.gridpp.rl.ac.uk', 
-                  'cmsosgce.fnal.gov', 'cmsosgce2.fnal.gov']
+        target = ['lcgce02.gridpp.rl.ac.uk']
 
-        results = self.mySiteDB.cmsNametoCE("T1")
-        
+        results = self.mySiteDB.cmsNametoCE("T1_UK_RAL")
+
         self.failUnless(results == target)
 
 

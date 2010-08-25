5ed0bb49933a9b1d545a1dbb5face51dc3caf494
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:33:28 +0000

    Algo for finding the Dataset-Algo pair needed for upload
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7822 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMComponent/DBSUpload/Database/Oracle/FindDASToUpload.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+This code should load the necessary information regarding
+dataset-algo combinations from the DBSBuffer.
+
+Oracle version
+
+"""
+
+__revision__ = "$Id: FindDASToUpload.py,v 1.1 2010/06/14 16:38:52 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMComponent.DBSUpload.Database.MySQL.FindDASToUpload import FindDASToUpload as MySQLFindDASToUpload
+
+
+class FindDASToUpload(MySQLFindDASToUpload):
+    """
+    Find Uploadable DAS
+
+    """

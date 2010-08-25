52eed47b50540912345163ecf70b5ac0644cc5b5
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:58:38 +0000

    Lists converted to strings before create the object into the database.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7356 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/BossLite/MySQL/Job/New.py
+++ src/python/WMCore/BossLite/MySQL/Job/New.py
@@ -6,11 +6,13 @@ MySQL implementation of BossLite.Jobs.Create
 """
 
 __all__ = []
-__revision__ = "$Id: New.py,v 1.1 2010/03/30 10:13:38 mnorman Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: New.py,v 1.2 2010/05/09 14:55:55 spigafi Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
+from WMCore.BossLite.Common.System import strToList, listToStr
 
+    
 class New(DBFormatter):
     sql = """INSERT INTO bl_job (job_id, task_id, name, executable, events,
                 arguments, stdin, stdout, stderr, input_files, output_files,
@@ -20,6 +22,31 @@ class New(DBFormatter):
                 :outputFiles, :dlsDestination, :submissionNumber, :closed)
                 """
 
+    def preFormat(self, entry):
+        """
+        This method maps database fields with object dictionary and 
+        it translate python List and timestamps in well formatted string
+        """
+        
+        result = {}  
+        
+        # result['id']               = entry['id']
+        result['jobId']            = entry['jobId']
+        result['taskId']           = entry['taskId']
+        result['name']             = entry['name']
+        result['executable']       = entry['executable']
+        result['events']           = entry['events']
+        result['arguments']        = entry['arguments']
+        result['standardInput']    = entry['standardInput']
+        result['standardOutput']   = entry['standardOutput']
+        result['standardError']    = entry['standardError']
+        result['inputFiles']       = listToStr(entry['inputFiles'])
+        result['outputFiles']      = listToStr(entry['outputFiles'])
+        result['dlsDestination']   = listToStr(entry['dlsDestination'])
+        result['submissionNumber'] = entry['submissionNumber']
+        result['closed']           = entry['closed']
+            
+        return result
 
     def execute(self, binds, conn = None, transaction = False):
         """
@@ -29,7 +56,9 @@ class New(DBFormatter):
         parsing the binds
         """
         
-        self.dbi.processData(self.sql, binds, conn = conn,
+        ppBinds = self.preFormat(binds)
+        
+        self.dbi.processData(self.sql, ppBinds, conn = conn,
                              transaction = transaction)
         return
     

4639ed5735e5515b0a8b41324171bd74e79cd3b8
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:39:53 +0000

    Added poll interval configuration
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4527 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/FeederManager/DefaultConfig.py
+++ src/python/WMComponent/FeederManager/DefaultConfig.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+#pylint: disable-msg=E1101,E1103,C0103,R0902
+"""
+Defines default config values for FeederManager specific
+parameters.
+"""
+__all__ = []
+__revision__ = "$Id: DefaultConfig.py,v 1.2 2009/07/14 16:30:15 riahi Exp $"
+__version__ = "$Revision: 1.2 $"
+
 import os
 
 from WMCore.Agent.Configuration import Configuration
@@ -14,3 +23,6 @@ config.FeederManager.addDatasetWatchHandler = \
 
 # The maximum number of threads to process each message type
 config.FeederManager.maxThreads = 10
+
+# The poll interval at which to look for new fileset/feeder association
+config.FeederManager.pollInterval = 60

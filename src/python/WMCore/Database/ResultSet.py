577a5356ba8244a8e80e645d56f0fcba4bb57b93
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:04:49 +0000

    Verify that the cursor is open before pulling rows from it.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3994 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Database/ResultSet.py
+++ src/python/WMCore/Database/ResultSet.py
@@ -6,8 +6,8 @@ SQLAlchemy result sets (aka cursors) can be closed. Make this class look as much
 like the SQLAlchemy class to minimise the impact of adding this class.
 """
 
-__revision__ = "$Id: ResultSet.py,v 1.2 2009/03/31 14:35:47 sfoulkes Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: ResultSet.py,v 1.3 2009/03/31 14:56:21 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 
 class ResultSet:
     def __init__(self):
@@ -24,6 +24,9 @@ class ResultSet:
         return self.data
     
     def add(self, resultproxy):
+        if resultproxy.closed:
+            return
+        
         for r in resultproxy:
             if len(self.keys) == 0:
                 self.keys.extend(r.keys())

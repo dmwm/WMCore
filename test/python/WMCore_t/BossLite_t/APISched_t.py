d7fea23cdb8a302d46c91ff27d6404fddd0f3192
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:17:15 +0000

    Refactoring. Added 'Status' test-case.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7607 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/BossLite_t/APISched_t.py
+++ test/python/WMCore_t/BossLite_t/APISched_t.py
@@ -4,10 +4,11 @@ _APISched_t_
 
 """
 
-__revision__ = "$Id: APISched_t.py,v 1.1 2010/05/24 14:14:52 spigafi Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: APISched_t.py,v 1.2 2010/05/25 22:15:05 spigafi Exp $"
+__version__ = "$Revision: 1.2 $"
 
 import unittest
+import time
         
 # Import key features
 from WMQuality.TestInit import TestInit
@@ -28,11 +29,11 @@ def fakeTask(db, numjob):
 
     taskParams = {'name' : 'testTask' }
 
-    jobParams = {'executable' : '/bin/echo',
-                 'arguments' : 'ciao',
-                 'standardError' : 'err.txt',
-                 'standardOutput' : 'out.txt',
-                 'outputFiles' : ['out.txt'] }
+    jobParams = {'executable' : '/bin/hostname',
+                 'arguments' : '-f',
+                 'standardError' : 'hostname.err',
+                 'standardOutput' : 'hostname.out',
+                 'outputFiles' : ['hostname.out'] }
 
 
     task = Task(taskParams)
@@ -49,73 +50,145 @@ def fakeTask(db, numjob):
     
     return
 
+
+def printDebug(task):
+    """
+    printDebug
+    """
+    
+    print '\n >> schedulerParentId: ' + \
+                str(task.jobs[0].runningJob['schedulerId']) + '\n' 
+    print 'schedulerId'.ljust(52), 'status'.ljust(6), \
+                'statusScheduler'.ljust(20), 'destination'.ljust(20)
+    print ('-'*52).ljust(52), ('-'*6).ljust(6), \
+                ('-'*20).ljust(20), ('-'*20).ljust(20)
+    for job in task.jobs :
+        if job.runningJob is None :
+            print str(job.runningJob['schedulerId']).ljust(52), \
+                    'No running job'.rjust(10)
+        else :
+            print str(job.runningJob['schedulerId']).ljust(52), \
+                     str(job.runningJob['status']).ljust(6), \
+                     str(job.runningJob['statusScheduler']).ljust(20), \
+                     str(job.runningJob['destination']).ljust(20)
+        # print '\n'
+
+
 class APISched(unittest.TestCase):
     """
     Unit-test for BossLiteAPISched
     """
     
-    def setUp(self):
+    myBossLiteAPI = None
+    mySchedConfig =  {'name' : 'SchedulerGLite' }
+    
+    def testA_databaseStartup(self):
         """
-        _setUp_
+        testA_databaseStartup
         """
         
-        self.testInit = TestInit(__file__)
-        self.testInit.setLogging()
-        self.testInit.setDatabaseConnection()
-        self.testInit.setSchema(customModules = ["WMCore.BossLite"],
+        testInit = TestInit(__file__)
+        testInit.setLogging()
+        testInit.setDatabaseConnection()
+        
+        testInit.setSchema(customModules = ["WMCore.BossLite"],
                                 useDefault = False)
         
-        return
-    
-    
-    def tearDown(self):
-        """
-        Tear down database
-        """
-
-        self.testInit.clearDatabase()
-
+        # populate DB
+        self.myBossLiteAPI = BossLiteAPI()
+        fakeTask(self.myBossLiteAPI.db, numjob= 3)
+        
         return
 
     
-    def testA_SubmissionGLite(self):
+    def testB_Submission(self):
+        """
+        Simple submission operation
+        """
+        
+        mySchedAPI = BossLiteAPISched( bossLiteSession = self.myBossLiteAPI, 
+                                       schedulerConfig = self.mySchedConfig )
+        
+        task = mySchedAPI.submit( 1 )
+        
+        #### DEBUG ####
+        printDebug(task)
+        
+        return
+    
+    
+    
+    def testC_Status(self):
+        """
+        Simple status check operation
+        """
+        
+        mySchedAPI = BossLiteAPISched( bossLiteSession = self.myBossLiteAPI, 
+                                       schedulerConfig = self.mySchedConfig )
+        
+        # polling status... the test ends when all jobs reach the status 'SD'
+        while True:
+            
+            task = mySchedAPI.query( 1 )
+            
+            #### DEBUG ####
+            printDebug(task)
+            
+            exitCondition = True
+            
+            for x in task.jobs :
+                if x.runningJob['status'] != 'SD' :
+                    exitCondition = False
+                    
+            if exitCondition:
+                break
+            
+            # sleeping for a while...
+            time.sleep(60)
+        
+        return
+    
+    
+    def testD_GetOutput(self):
+        """
+        Simple getOutput operation
+        """
+        
+        mySchedAPI = BossLiteAPISched( bossLiteSession = self.myBossLiteAPI, 
+                                       schedulerConfig = self.mySchedConfig )
+        
+        # do something ...
+        
+        return
+    
+    
+    def testD_Kill(self):
+        """
+        Simple kill operation
+        """
+        
+        mySchedAPI = BossLiteAPISched( bossLiteSession = self.myBossLiteAPI, 
+                                       schedulerConfig = self.mySchedConfig )
+        
+        # do something ...
+        
+        return
+    
+    
+    def testZ_dropDatabase(self):
         """
         Simple submission through SchedulerGLite
         """
         
-        myBossLiteAPI = BossLiteAPI()
+        testInit = TestInit(__file__)
+        testInit.setLogging()
+        testInit.setDatabaseConnection()
         
-        mySchedConfig =  {'name' : 'SchedulerGLite' }
-        mySchedAPI = BossLiteAPISched( bossLiteSession = myBossLiteAPI, 
-                                       
-                                       schedulerConfig = mySchedConfig )
-
-        fakeTask(myBossLiteAPI.db, numjob= 5)
-        
-        task = mySchedAPI.submit( 1 )
-        
-        
-        #### DEBUG ####
-        print '\n >> schedulerParentId: ' + \
-                    str(task.jobs[0].runningJob['schedulerId']) + '\n' 
-        print 'schedulerId'.ljust(52), 'status'.ljust(6), \
-                    'statusScheduler'.ljust(20), 'destination'.ljust(20)
-        print ('-'*52).ljust(52), ('-'*6).ljust(6), \
-                    ('-'*20).ljust(20), ('-'*20).ljust(20)
-        for job in task.jobs :
-            if job.runningJob is None :
-                print str(job.runningJob['schedulerId']).ljust(52), \
-                        'No running job'.rjust(10)
-            else :
-                print str(job.runningJob['schedulerId']).ljust(52), \
-                         str(job.runningJob['status']).ljust(6), \
-                         str(job.runningJob['statusScheduler']).ljust(20), \
-                         str(job.runningJob['destination']).ljust(20)
-            # print '\n'
+        testInit.clearDatabase()
         
         return
     
-
+    
 if __name__ == "__main__":
     APISchedSuite = unittest.TestLoader().loadTestsFromTestCase(APISched)
 

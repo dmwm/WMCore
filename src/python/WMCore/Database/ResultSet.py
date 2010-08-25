9140893f07f41698d56a4defa689c3fbaf6d6ec0
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:16:30 +0000

    Add lastrowid param - needed for backwards compatibility for ProdAgent - may not work with all databases.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4174 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Database/ResultSet.py
+++ src/python/WMCore/Database/ResultSet.py
@@ -6,14 +6,16 @@ SQLAlchemy result sets (aka cursors) can be closed. Make this class look as much
 like the SQLAlchemy class to minimise the impact of adding this class.
 """
 
-__revision__ = "$Id: ResultSet.py,v 1.4 2009/04/02 13:23:11 sfoulkes Exp $"
-__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: ResultSet.py,v 1.5 2009/05/12 16:38:46 swakef Exp $"
+__version__ = "$Revision: 1.5 $"
 
 class ResultSet:
     def __init__(self):
         self.data = []
         self.keys = []
         self.rowcount = 0
+        # DO NOT use with Oracle, provide backwards compatibility for prodagent
+        self.lastrowid = None
 
     def close(self):
         return
@@ -33,5 +35,7 @@ class ResultSet:
                 self.keys.extend(r.keys())
             self.data.append(r)
 
+        self.lastrowid = resultproxy.lastrowid
         self.rowcount = len(self.data)
+        
         return

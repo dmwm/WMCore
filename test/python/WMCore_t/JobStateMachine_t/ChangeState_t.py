9e005c2938e54f98097162668d57b4ab8f0e3338
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:14:17 +0000

    Working testCheck test
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4139 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/python/WMCore_t/JobStateMachine_t/ChangeState_t.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+import unittest
+import sys
+import os
+import logging
+import threading
+
+from WMQuality.TestInit import TestInit
+from WMCore.DAOFactory import DAOFactory
+from WMCore.WMFactory import WMFactory
+from WMCore.JobStateMachine.ChangeState import ChangeState, Transitions
+from WMCore.JobStateMachine import DefaultConfig
+# Framework for this code written automatically by Inspect.py
+
+
+class TestChangeState(unittest.TestCase):
+    _setup = False
+    _teardown = False
+    transitions = None
+    change = None
+    def setUp(self):
+        """
+        _setUp_
+        """
+        if self._setup:
+            return
+        self.transitions = Transitions()
+        # TODO: write a config here
+        self.change = ChangeState(DefaultConfig.config)
+        self._setup = True
+
+
+    def tearDown(self):
+        """
+        _tearDown_
+        """
+        if self._teardown:
+            return
+
+        self._teardown = True
+
+
+    def testCheck(self):
+    	"""
+    	This is the test class for function Check from module ChangeState
+    	"""
+        # Run through all good state transitions and assert that they work
+        for state in self.transitions.keys():
+            for dest in self.transitions[state]:
+                self.change.check(dest, state)
+        dummystates = ['dummy1', 'dummy2', 'dummy3', 'dummy4']
+
+        # Then run through some bad state transistions and assertRaises(AssertionError)
+        for state in self.transitions.keys():
+            for dest in dummystates:
+                self.assertRaises(AssertionError, self.change.check, dest, state)
+    	return
+
+    def testPersist(self):
+    	"""
+    	This is the test class for function Persist from module ChangeState
+    	"""
+        return
+
+
+    def testPropagate(self):
+    	"""
+    	This is the test class for function Propagate from module ChangeState
+    	"""
+        return
+
+
+
+
+    def testRecordInCouch(self):
+    	"""
+    	This is the test class for function RecordInCouch from module ChangeState
+    	"""
+        return
+
+
+
+
+    def testStates(self):
+    	"""
+    	This is the test class for function States from module ChangeState
+    	"""
+        return
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()

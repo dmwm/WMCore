7c8ec81e91104ef4127076cced1557973c0d50c4
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:44:01 +0000

    Changed to reflect new purpose of JobArchiver; only remove job cache areas
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5480 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/JobArchiver/JobArchiverPoller.py
+++ src/python/WMComponent/JobArchiver/JobArchiverPoller.py
@@ -3,19 +3,22 @@
 The actual jobArchiver algorithm
 """
 __all__ = []
-__revision__ = "$Id: JobArchiverPoller.py,v 1.1 2009/09/29 16:33:46 mnorman Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: JobArchiverPoller.py,v 1.2 2009/10/30 13:47:07 mnorman Exp $"
+__version__ = "$Revision: 1.2 $"
 
 import threading
 import logging
 import re
 import os
 import os.path
+import shutil
 from sets import Set
 
 from subprocess import Popen, PIPE
 
 from WMCore.WorkerThreads.BaseWorkerThread import BaseWorkerThread
+
+from WMCore.JobStateMachine.ChangeState    import ChangeState
 
 from WMCore.WMBS.Subscription import Subscription
 from WMCore.WMBS.Fileset      import Fileset
@@ -34,6 +37,7 @@ class JobArchiverPoller(BaseWorkerThread):
         """
         BaseWorkerThread.__init__(self)
         self.config = config
+        self.changeState = ChangeState(self.config)
     
     def setup(self, parameters):
         """
@@ -79,69 +83,52 @@ class JobArchiverPoller(BaseWorkerThread):
         """
         _archiveJobs_
         
-        archiveJobs will handle the master task of looking for finished subscriptions,
-        checking to see if they've finished, and then notifying the workQueue and
-        finishing things up.
+        archiveJobs will handle the master task of looking for finished jobs,
+        and running the code that cleans them out.
         """
 
 
         myThread = threading.currentThread()
 
-        subList  = self.findFinishedSubscriptions()
-        doneList = self.notifyWorkQueue(subList)
+        doneList  = self.findFinishedJobs()
         self.cleanWorkArea(doneList)
 
+        successList = []
+        failList    = []
 
-    def findFinishedSubscriptions(self):
-        """
-        _findFinishedSubscriptions_
-        
-        Figures out which one of the subscriptions is actually finished.
-        """
-        subList = []
-
-        myThread = threading.currentThread()
-
+        for job in doneList:
+            if job["outcome"] == "success":
+                successList.append(job)
+            else:
+                failList.append(job)
+                
         myThread.transaction.begin()
+        self.changeState.propagate(successList, "cleanout", "success")
+        self.changeState.propagate(failList, "cleanout", "exhausted")
+        myThread.transaction.commit()
+
+
+    def findFinishedJobs(self):
+
+        jobList = []
 
         jobListAction = self.daoFactory(classname = "Jobs.GetAllJobs")
-        jobList  = jobListAction.execute(state = "Closeout")
+        jobList1  = jobListAction.execute(state = "success")
+        jobList2  = jobListAction.execute(state = "exhausted")
 
-        subscriptionList = self.daoFactory(classname = "Subscriptions.List")
-        subscriptions    = subscriptionList.execute()
-
-        for subscription in subscriptions:
-            wmbsSubscription = Subscription(id = subscription)
-            jobs             = wmbsSubscription.getJobs()
-            finished = True
-            if len(jobs) == 0:
-                finished = False
-            for job in jobs:
-                if not job['id'] in jobList:
-                    finished = False
-                    break
-            if finished:
-                subList.append(wmbsSubscription)
-
-        myThread.transaction.commit()
-                
-        return subList
-
-    def notifyWorkQueue(self, subList):
-        """
-        _notifyWorkQueue_
-        
-        Tells the workQueue component that a particular subscription,
-        or set of subscriptions, is done.  Receives confirmation
-        """
+        jobList.extend(jobList1)
+        jobList.extend(jobList2)
 
         doneList = []
+        
+        for jobID in jobList:
+            tmpJob = Job(id = jobID)
+            tmpJob.load()
+            doneList.append(tmpJob)
 
-        #In the future, this will talk to the workQueue
-        #Right now it doesn't because I'm not sure how to do it
-        doneList = subList
 
         return doneList
+
 
     def cleanWorkArea(self, doneList):
         """
@@ -152,20 +139,13 @@ class JobArchiverPoller(BaseWorkerThread):
         """
 
         myThread = threading.currentThread()
-        #myThread.transaction.commit()
 
-        for subscription in doneList:
-            wmbsSubscription = Subscription(id = subscription["id"])
-            wmbsSubscription.load()
-            for job in wmbsSubscription.getJobs():
-                wmbsJob = Job(id = job['id'])
-                wmbsJob.load()
-                self.cleanJobCache(wmbsJob.getCache())
-            wmbsSubscription.deleteEverything()
+        for job in doneList:
+            self.cleanJobCache(job)
         
         return
 
-    def cleanJobCache(self, cacheDir):
+    def cleanJobCache(self, job):
         """
         _cleanJobCache_
 
@@ -174,28 +154,32 @@ class JobArchiverPoller(BaseWorkerThread):
 
         myThread = threading.currentThread()
 
+        cacheDir = job.getCache()
+
         if not cacheDir or not os.path.isdir(cacheDir):
             logging.error("Could not find jobCacheDir %s" %(cacheDir))
             return
 
         if os.listdir(cacheDir) == []:
-            #Directory is empty.
-            logging.error("jobCacheDir %s empty" %(cacheDir))
+            os.rmdir(cacheDir)
             return
 
         #Otherwise we have something in there
+        tarName = 'Job_%s.tar' %(job['name'])
         tarString = ["tar"]
         tarString.append("-cvf")
-        tarString.append('%s/Job_%i.tar ' %(job["cache_dir"], job['id']))
-        for file in os.listdir(job["cache_dir"]):
-            tarString.append('%s' %(os.path.join(job["cache_dir"], file)))
+        tarString.append('%s/%s' %(cacheDir, tarName))
+        for file in os.listdir(cacheDir):
+            tarString.append('%s' %(os.path.join(cacheDir, file)))
 
         #Now we should have all the files together.  Tar them up
         pipe = Popen(tarString, stdout = PIPE, stderr = PIPE, shell = False)
         pipe.wait()
 
-        #This should wait for the result, and then move the file
-        #Then delete
+        shutil.move('%s/%s' %(cacheDir, tarName), '%s/%s' %(self.config.JobArchiver.logDir, tarName))
+
+        shutil.rmtree('%s' %(cacheDir))
+
         return
 
         

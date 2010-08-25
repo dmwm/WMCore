d3b7ca148d77290505ff8d0cc974def5a362f8a1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:13:50 +0000

    Schema changes make these DAOs obsolete.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4132 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Jobs/Load.py
+++
@@ -1,7 +0,0 @@
-"""
-Oracle implementation of Jobs.Load
-"""
-from WMCore.WMBS.MySQL.Jobs.Load import Load as LoadJobMySQL
-
-class Load(LoadJobMySQL):
-    sql = LoadJobMySQL.sql
--- src/python/WMCore/WMBS/SQLite/Jobs/Load.py
+++
@@ -1,7 +0,0 @@
-"""
-SQLite implementation of Jobs.Load
-"""
-from WMCore.WMBS.MySQL.Jobs.Load import Load as LoadJobMySQL
-
-class Load(LoadJobMySQL):
-    sql = LoadJobMySQL.sql

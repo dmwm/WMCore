50ac8f2385d8351294c824d2af94cb5809fb35fb
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:39:29 +0000

    Use the MySQL version of this DAO.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5415 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSBuffer/Database/Oracle/DBSBufferFiles/SetLocation.py
+++ src/python/WMComponent/DBSBuffer/Database/Oracle/DBSBufferFiles/SetLocation.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 """
-Oracle implementation of SetLocation
+_SetLocation_
+
+Oracle implementation of DBSBufferFiles.SetLocation
 """
 
-#Modified for Oracle
+__revision__ = "$Id: SetLocation.py,v 1.3 2009/10/22 14:46:43 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 
-from WMComponent.DBSBuffer.Database.MySQL.DBSBufferFiles.SetLocation import SetLocation as MySQLSetLocation
-
-__revision__ = "$Id: SetLocation.py,v 1.2 2009/05/18 20:14:03 mnorman Exp $"
-__version__ = "$Revision: 1.2 $"
+from WMComponent.DBSBuffer.Database.MySQL.DBSBufferFiles.SetLocation import SetLocation as \
+     MySQLSetLocation
 
 class SetLocation(MySQLSetLocation):
-    sql = """insert into dbsbuffer_file_location (filename, location) 
-             select dbsbuffer_file.id, dbsbuffer_location.id from dbsbuffer_file, dbsbuffer_location 
-             where dbsbuffer_file.lfn = :lfn
-             and dbsbuffer_location.se_name = :location"""
+    pass
+
                 

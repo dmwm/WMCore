dd62db0ab667a0aaf2f61e0251124077b0c8e452
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:27:35 +0000

    better formatting
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6102 4525493e-7705-40b1-a816-d608a930855b

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ def runUnitTests():
             __import__(oneModule)
             modules.append(sys.modules[oneModule])
         except Exception, e:
-            print "ERROR: Can't load test case %s - %s" % (oneModule, e)
+            sys.stderr.write("ERROR: Can't load test case %s - %s\n" % (oneModule, e)
             loadFail.append(oneModule)    
 
     load = unittest.defaultTestLoader.loadTestsFromModule  

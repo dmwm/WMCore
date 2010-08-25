de1c538ca3485fe6f809ede77bb4144845fd73c6
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:06:20 +0000

    Advanced DAO: load Jobs starting from RunningJob attributes.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7460 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/BossLite/MySQL/Job/LoadByRunningJobAttr.py
+++ src/python/WMCore/BossLite/MySQL/Job/LoadByRunningJobAttr.py
@@ -6,65 +6,56 @@ MySQL implementation of BossLite.Jobs.LoadByRunningJobAttr
 """
 
 __all__ = []
-__revision__ = "$Id: LoadByRunningJobAttr.py,v 1.1 2010/04/28 21:14:40 mnorman Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: LoadByRunningJobAttr.py,v 1.2 2010/05/17 19:08:56 spigafi Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
+from WMCore.BossLite.DbObjects.Job import JobDBFormatter
 
 class LoadByRunningJobAttr(DBFormatter):
-    sql = """SELECT bl_job.job_id as jobId, bl_job.task_id as taskId,
-                bl_job.name as name, bl_job.executable as executable, bl_job.events as events,
-                bl_job.arguments as arguments, bl_job.stdin as StandardInput,
-                bl_job.stdout as StandardOutput, bl_job.stderr as StandardError,
-                bl_job.input_files as inputFiles, bl_job.output_files as outputFiles,
+    """
+    BossLite.Jobs.LoadByRunningJobAttr
+    """
+    
+    sql = """SELECT bl_job.id as id,
+                bl_job.job_id as jobId, 
+                bl_job.task_id as taskId,
+                bl_job.name as name, 
+                bl_job.executable as executable, 
+                bl_job.events as events,
+                bl_job.arguments as arguments, 
+                bl_job.stdin as StandardInput,
+                bl_job.stdout as StandardOutput, 
+                bl_job.stderr as StandardError,
+                bl_job.input_files as inputFiles, 
+                bl_job.output_files as outputFiles,
                 bl_job.dls_destination as dlsDestination,
                 bl_job.submission_number as submissionNumber,
                 bl_job.closed as closed
                 FROM bl_job
-                INNER JOIN bl_runningjob ON bl_runningjob.job_id = bl_job.job_id
-                WHERE bl_runningjob.submission = (SELECT MAX(submission) FROM bl_runningjob
-                     WHERE bl_runningjob.job_id = bl_job.job_id )
-                AND bl_runningjob.%s = :value
-                """
-
-    def format(self, res):
-        """
-        Format the results into the right output
-        """
-
-        form = self.formatDict(res)
-        final = []
-        for entry in form:
-            result = {}
-            result['jobId']            = entry['jobid']
-            result['taskId']           = entry['taskid']
-            result['name']             = entry['name']
-            result['executable']       = entry['executable']
-            result['events']           = entry['events']
-            result['arguments']        = entry['arguments']
-            result['standardInput']    = entry['standardinput']
-            result['standardOutput']   = entry['standardoutput']
-            result['standardError']    = entry['standarderror']
-            result['inputFiles']       = entry['inputfiles']
-            result['outputFiles']      = entry['outputfiles']
-            result['dlsDestination']   = entry['dlsdestination']
-            result['submissionNumber'] = entry['submissionnumber']
-            result['closed']           = entry['closed']
-
-            final.append(result)
-
-        return final
+                INNER JOIN bl_runningjob ON 
+                        bl_runningjob.job_id = bl_job.job_id
+                WHERE bl_runningjob.submission = 
+                        ( SELECT MAX(submission) FROM bl_runningjob 
+                            WHERE bl_runningjob.job_id = bl_job.job_id )
+                        AND bl_runningjob.%s = :value """
 
     def execute(self, column, value, conn = None, transaction = False):
         """
         Load a job based on the attributes of the most recent runningJob
         """
+        
+        objFormatter = JobDBFormatter()
+        
         if type(value) == list:
             binds = value
         else:
             binds = {'value': value}
 
-        sql = self.sql %(column)
+        sql = self.sql % (column)
+        
         result = self.dbi.processData(sql, binds, conn = conn,
                                       transaction = transaction)
-        return self.format(result)
+        
+        ppResult = self.formatDict(result)
+        return objFormatter.postFormat(ppResult)

9c030898c4989e8388c1fe60f96f7ff4639e7940
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:59:10 +0000

    bring up-to-date with MySQL version, no longer checkign for exisiting records (this is being taken care of in application code)
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8155 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Files/SetLocation.py
+++ src/python/WMCore/WMBS/Oracle/Files/SetLocation.py
@@ -4,14 +4,12 @@ _SetLocation_
 Oracle implementation of Files.SetLocation
 """
 
-__revision__ = "$Id: SetLocation.py,v 1.8 2010/04/08 20:09:09 sfoulkes Exp $"
-__version__ = "$Revision: 1.8 $"
+__revision__ = "$Id: SetLocation.py,v 1.9 2010/07/14 11:41:45 hufnagel Exp $"
+__version__ = "$Revision: 1.9 $"
 
 from WMCore.WMBS.MySQL.Files.SetLocation import SetLocation as SetLocationMySQL
 
 class SetLocation(SetLocationMySQL):
-    sql = """INSERT INTO wmbs_file_location (fileid, location) 
-                SELECT :fileid, wmbs_location.id from wmbs_location 
-                  WHERE wmbs_location.se_name = :location AND NOT EXISTS
-                    (SELECT * FROM wmbs_file_location
-                       WHERE fileid = :fileid AND location = wmbs_location.id)"""
+    sql = """INSERT INTO wmbs_file_location (fileid, location)
+             SELECT :fileid, wmbs_location.id FROM wmbs_location 
+             WHERE wmbs_location.se_name = :location"""

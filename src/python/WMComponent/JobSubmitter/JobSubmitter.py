5a4761ba0a2981144eae48364180f7ea6e13b839
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:53:42 +0000

    Did some tightening of code
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6471 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/JobSubmitter/JobSubmitter.py
+++ src/python/WMComponent/JobSubmitter/JobSubmitter.py
@@ -1,59 +1,49 @@
 #!/usr/bin/env python
+#pylint: disable-msg=W6501
+# W6501: pass information to logging using string arguments
 
 """
 Creates jobs for new subscriptions
 
 """
 
-__revision__ = "$Id: JobSubmitter.py,v 1.2 2009/10/07 19:33:34 mnorman Exp $"
-__version__ = "$Revision: 1.2 $"
-__author__ = "mnorman@fnal.gov"
+__revision__ = "$Id: JobSubmitter.py,v 1.3 2010/02/12 21:14:45 mnorman Exp $"
+__version__ = "$Revision: 1.3 $"
 
 import logging
 import threading
-import time
-import os.path
-#import common
 
-#WMBS objects
-from WMCore.WMBS.Subscription import Subscription
-from WMCore.WMBS.JobGroup     import JobGroup
-from WMCore.WMBS.Job          import Job as WMBSJob
-from WMCore.WMBS.Workflow     import Workflow
-from WMCore.DAOFactory        import DAOFactory
-from WMCore.WMFactory         import WMFactory
 from WMCore.Agent.Harness     import Harness
-
-from WMCore.WMSpec.WMWorkload                 import WMWorkload, WMWorkloadHelper
-from WMCore.WMSpec.WMTask                     import WMTask, WMTaskHelper
 
 from WMComponent.JobSubmitter.JobSubmitterPoller import JobSubmitterPoller
 
 
 class JobSubmitter(Harness):
+    """
+    Creates jobs for new subscriptions
+
+    """
 
     def __init__(self, config):
         # call the base class
         Harness.__init__(self, config)
         self.pollTime = 1
 
-        #myThread = threading.currentThread()
-        #myThread.database = os.getenv("DATABASE")
-        
-	logging.info("JobSubmitter.__init__")
+        logging.info("JobSubmitter.__init__")
 
     def preInitialization(self):
-	logging.info("JobSubmitter.preInitialization")
+        """
+        Setup the worker thread for jobSubmitter
 
-        # use a factory to dynamically load handlers.
-        factory = WMFactory('generic')
-        
+        """
+        logging.info("JobSubmitter.preInitialization")
 
         # Add event loop to worker manager
         myThread = threading.currentThread()
         
         pollInterval = self.config.JobSubmitter.pollInterval
         logging.info("Setting poll interval to %s seconds" % pollInterval)
-        myThread.workerThreadManager.addWorker(JobSubmitterPoller(self.config), pollInterval)
+        myThread.workerThreadManager.addWorker(JobSubmitterPoller(self.config),
+                                               pollInterval)
 
         return

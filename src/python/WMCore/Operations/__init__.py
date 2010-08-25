a79ecfdfe0429439854741659305ed44cec35f96
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:55:12 +0000

    Base for Operations directory
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5640 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/Operations/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+_WMBS_
+
+Workload Management Book-keeping Service
+
+
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2009/12/02 19:45:18 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+

a81e2ee305ba9017e0acb215b3ceb20b1a7f8419
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:40:38 +0000

    Don't check for duplicate entries as that causes deadlocks.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5432 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Files/SetLocation.py
+++ src/python/WMCore/WMBS/MySQL/Files/SetLocation.py
@@ -1,38 +1,38 @@
+#!/usr/bin/env python
 """
-MySQL implementation of SetLocation
+_SetLocation_
+
+MySQL implementation of Files.SetLocation
 """
+
+__revision__ = "$Id: SetLocation.py,v 1.11 2009/10/22 18:40:53 sfoulkes Exp $"
+__version__ = "$Revision: 1.11 $"
+
 from WMCore.Database.DBFormatter import DBFormatter
 from sets import Set
 
 class SetLocation(DBFormatter):
     sql = """INSERT INTO wmbs_file_location (file, location) 
-             SELECT wmbs_file_details.id, wmbs_location.id from wmbs_file_details, wmbs_location 
-             WHERE wmbs_file_details.lfn = :lfn
-             AND wmbs_location.site_name = :location 
-             AND NOT EXISTS (SELECT * FROM wmbs_file_location WHERE file = wmbs_file_details.id
-             and location = wmbs_location.id)"""
+             SELECT :fileid, wmbs_location.id FROM wmbs_location 
+             WHERE wmbs_location.site_name = :location"""
                 
     def getBinds(self, file = None, location = None):
         if type(location) == type('string'):
-            return self.dbi.buildbinds(self.dbi.makelist(file), 'lfn', 
+            return self.dbi.buildbinds(self.dbi.makelist(file), 'fileid', 
                    self.dbi.buildbinds(self.dbi.makelist(location), 'location'))
         elif isinstance(location, (list, Set, set)):
             binds = []
             for l in location:
-                binds.extend(self.dbi.buildbinds(self.dbi.makelist(file), 'lfn', 
+                binds.extend(self.dbi.buildbinds(self.dbi.makelist(file), 'fileid', 
                    self.dbi.buildbinds(self.dbi.makelist(l), 'location')))
             return binds
         else:
             raise Exception, "Type of location argument is not allowed: %s" \
                                 % type(location)
     
-    def format(self, result):
-        return True
-        
     def execute(self, file, location, conn = None, transaction = None):
         binds = self.getBinds(file, location)
 
         result = self.dbi.processData(self.sql, binds, conn = conn,
                                       transaction = transaction)
-        
-        return self.format(result)
+        return

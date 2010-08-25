f4d2a143332f3cb9d9f81ef02705460f07fd15a7
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:01:38 +0000

    Called Bootstrap logging function
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5729 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMRuntime/Startup.py
+++ src/python/WMCore/WMRuntime/Startup.py
@@ -11,9 +11,10 @@ import WMCore.WMRuntime.Bootstrap as Bootstrap
 
 if __name__ == '__main__':
 
-
     job = Bootstrap.loadJobDefinition()
     task = Bootstrap.loadTask(job)
+
+    Bootstrap.setupLogging(os.getcwd())
 
     task.build(os.getcwd())
 

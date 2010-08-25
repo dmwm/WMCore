a8b3d2d8ec325e5f05a72c5c83f248b57848c8d9
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:34:13 +0000

    Well, SQLAlchemy decided to reorganize their class tree in the most recent revision. Which means we get a nice try-except block to catch it
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7832 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Database/Dialects.py
+++ src/python/WMCore/Database/Dialects.py
@@ -1,6 +1,17 @@
-from sqlalchemy.databases.mysql import MySQLDialect
-from sqlalchemy.databases.sqlite import SQLiteDialect 
-from sqlalchemy.databases.oracle import OracleDialect
+
+
+# sqlalchemy breaks this because they reorganized things. Fail.
+# see: http://www.mail-archive.com/sqlalchemy@googlegroups.com/msg18392.html
+#  -AMM 6/15/10
+
+try:
+    from sqlalchemy.databases.mysql import MySQLDialect
+    from sqlalchemy.databases.sqlite import SQLiteDialect 
+    from sqlalchemy.databases.oracle import OracleDialect
+except:
+    from sqlalchemy.dialects.mysql.base import MySQLDialect
+    from sqlalchemy.dialects.databases.sqlite.base import SQLiteDialect 
+    from sqlalchemy.dialects.databases.oracle.base import OracleDialect
 
 
 __all__ = [MySQLDialect, SQLiteDialect, OracleDialect]

eaaf7f3a56156578789edf6368d244bb4af77811
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:08:23 +0000

    Initial check-in of the base class for monitoring plug-ins
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5823 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMRuntime/Monitors/WMRuntimeMonitor.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+"""
+_WMRuntimeMonitor_
+
+This is the base class for monitors
+"""
+
+__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: WMRuntimeMonitor.py,v 1.1 2009/12/21 15:59:09 mnorman Exp $"
+
+import threading
+import os
+
+from WMCore.WMException                         import WMException
+from WMCore.WMSpec.Steps.Executor               import getStepSpace
+from WMCore.WMSpec.WMStep                       import WMStepHelper
+
+getStepName = lambda step: WMStepHelper(step).name()
+
+class WMRuntimeMonitorException(WMException):
+    """
+    _StepFactortyException_
+
+    It's like an exception class that does nothing
+
+    """
+    pass
+
+class WMRuntimeMonitor:
+
+
+    def __init__(self):
+        self.currentStep      = None
+        self.currentStepName  = None
+        self.currentStepSpace = None
+        return
+
+
+
+    def initMonitor(self, task, job, args = {}):
+        return
+
+
+    def shutdown(self):
+        """
+        Shutdown method, will be called before object is deleted
+        at end of job.
+        """
+        pass
+
+
+    def periodicUpdate(self, monitorState):
+        """
+        Periodic update.
+        """
+        pass
+
+
+    def jobStart(self, task):
+        """
+        Job start notifier.
+        """
+        pass
+
+
+    def stepStart(self, step):
+        """
+        Tasked started notifier. 
+        """
+        self.currentStep      = step
+        self.currentStepName  = getStepName(step)
+        self.currentStepSpace = getStepSpace(self.currentStepName)
+        return
+
+    
+    def stepEnd(self, step):
+        """
+        Tasked ended notifier.
+        """
+        self.currentStep      = None
+        self.currentStepName  = None
+        self.currentStepSpace = None
+        return
+
+
+
+    def jobEnd(self, task):
+        """
+        Job ended notifier.
+        """
+        pass
+
+    def jobKilled(self):
+        """
+        Job killed notifier.
+        """
+        pass
+
+    def stepKilled(self):
+        """
+        Task killed notifier.
+        """
+        pass
+
+    def killCMSRun(self):
+        """
+        Kills all cmsRun processes owned by user
+
+        """
+
+        uid = os.getuid()

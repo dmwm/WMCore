9d51e5a8141e65bd1bff6dfddfc3eefefc85b2a2
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:11:12 +0000

    start server in the unit test. - this in away test both REST server and client code. still needs to refine the test to actually check correctness.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5861 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/Services_t/WorkQueue_t/WorkQueue_t.py
+++ test/python/WMCore_t/Services_t/WorkQueue_t/WorkQueue_t.py
@@ -2,14 +2,60 @@
 
 import unittest
 import logging
+import cherrypy
 
+from WMCore.Configuration import Configuration
 from WMCore.Services.WorkQueue.WorkQueue import WorkQueue
+from WMCore.WebTools.Root import Root
 
 class WorkQueueServiceTest(unittest.TestCase):
     """
-    Provide setUp and tearDown for Reader package module
-
+    Test WorkQueue Service client
+    It will start WorkQueue RESTService
+    Server DB is SQlite.
+    Client DB sets from environment variable. 
     """
+    def configureWorkQueueServer(self):
+        config = Configuration()
+
+        config.component_('Webtools')
+        config.Webtools.application = 'WorkQueueService'
+        config.Webtools.log_screen = False
+        config.Webtools.access_file = '/dev/null'
+        config.Webtools.error_file = '/dev/null'
+        config.Webtools.port = 6660
+        config.Webtools.host = "cmssrv18.fnal.gov"
+        config.component_('WorkQueueService')
+        
+        config.WorkQueueService.templates = '/tmp'
+        config.WorkQueueService.admin = 'your@email.com'
+        config.WorkQueueService.title = 'CMS WMCore/HTTPFrontEnd/WorkQueue Unit Tests'
+        config.WorkQueueService.description = 'Dummy server for the running of unit tests' 
+        
+        config.WorkQueueService.section_('views')
+        # These are all the active pages that Root.py should instantiate
+        active = config.WorkQueueService.views.section_('active')
+        workqueue = active.section_('workqueue')
+        # The class to load for this view/page
+        workqueue.object = 'WMCore.WebTools.RESTApi'
+        workqueue.templates = '/tmp'
+        workqueue.database = 'sqlite://'
+        workqueue.section_('model')
+        workqueue.model.object = 'WMCore.HTTPFrontEnd.WorkQueue.WorkQueueRESTModel'
+        workqueue.section_('formatter')
+        workqueue.formatter.object = 'WMCore.WebTools.DASRESTFormatter'
+        workqueue.formatter.templates = '/tmp'
+        
+        rt = Root(config)
+        return rt
+    
+    def startWQServer(self):
+        server = self.configureWorkQueueServer()
+        server.start(blocking=False)
+        cherrypy.log.error_log.setLevel(logging.WARNING)
+        cherrypy.log.access_log.setLevel(logging.WARNING)
+        return server
+    
     def setUp(self):
         """
         setUP global values
@@ -19,14 +65,21 @@ class WorkQueueServiceTest(unittest.TestCase):
         self.params = {}
         self.params['endpoint'] = wmbsTestDS
         
+    def testStartServer(self):
+        server = self.startWQServer()    
+        server.stop()
         
-
     def testGetWork(self):
+        
+        server = self.startWQServer()
         wqApi = WorkQueue(self.params)
 
         print wqApi.getWork({'SiteB' : 15, 'SiteA' : 15}, "http://test.url")
-
+        server.stop()
+        
     def testSynchronize(self):
+        server = self.startWQServer()
+        
         wqApi = WorkQueue(self.params)
         childUrl = "http://test.url"
         childResources = [{'ParentQueueId' : 1, 'Status' : 'Available'}]
@@ -35,8 +88,11 @@ class WorkQueueServiceTest(unittest.TestCase):
         childUrl = "http://test.url"
         childResources = []
         print wqApi.synchronize(childUrl, childResources)
-    
+        server.stop()
+        
     def testStatusChange(self):
+        
+        server = self.startWQServer()
         
         wqApi = WorkQueue(self.params)
 
@@ -48,6 +104,8 @@ class WorkQueueServiceTest(unittest.TestCase):
         print wqApi.status()
         print wqApi.cancelWork([1])
         print wqApi.status()
+        server.stop()
+        
 if __name__ == '__main__':
 
     unittest.main()

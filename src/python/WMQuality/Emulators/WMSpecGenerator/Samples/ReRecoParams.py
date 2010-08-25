bd12ee6df495d682c17c8775ba6ff7bce12a1536
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:36:43 +0000

    change arguments to test
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7057 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMQuality/Emulators/WMSpecGenerator/Samples/ReRecoParams.py
+++ src/python/WMQuality/Emulators/WMSpecGenerator/Samples/ReRecoParams.py
@@ -1,13 +1,13 @@
 MinBiasWithoutEmulator = {
     "Owner" : "DWMWTest",                         
-    "OutputTiers" : ["RECO", "ALCA", "AOD"],
+    "OutputTiers" : ["RECO", "ALCARECO"],
     "AcquisitionEra" : "WMAgentCommissioining10",
     "GlobalTag" :"GR09_R_34X_V5::All",
     "LFNCategory" : "/store/backfill/2",
     "TemporaryLFNCategory": "/store/backfill/2/unmerged",
     "ProcessingVersion" : "v1",
     "Scenario" : "cosmics",
-    "CMSSWVersion" : "CMSSW_3_4_2_patch1",
+    "CMSSWVersion" : "CMSSW_3_5_6",
     "InputDatasets" : "/MinimumBias/BeamCommissioning09-v1/RAW",
     "Emulate" : False,
     }

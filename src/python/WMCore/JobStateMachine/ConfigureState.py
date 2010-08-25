ea0c36b2b26eeac05a72c7035825702c9bc68efb
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:14:13 +0000

    Start filling in the ConfigureState
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4138 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/JobStateMachine/ConfigureState.py
+++ src/python/WMCore/JobStateMachine/ConfigureState.py
@@ -4,7 +4,18 @@ _ConfigureState_
 
 Populate the states tables with all known states, and set the max retries for
 each state. Default to one retry.
+Create the CouchDB and associated views if needed.
 """
 
-__revision__ = "$Id: ConfigureState.py,v 1.1 2009/05/08 14:15:31 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: ConfigureState.py,v 1.2 2009/05/11 11:58:57 metson Exp $"
+__version__ = "$Revision: 1.2 $"
+
+from WMCore.Database.CMSCouch import CouchServer
+from WMCore.DataStructs.WMObject import WMObject
+
+class ConfigureState(WMObject):
+    def configure(self):
+        server = CouchServer(self.config.JobStateMachine.couchurl)
+        dbname = 'JSM/JobHistory'
+        if dbname in server.listDatabases():
+            server.createDatabase(dbname)

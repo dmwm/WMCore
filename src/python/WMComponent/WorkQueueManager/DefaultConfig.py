5d40eb4a3f49df6bfbeac48913fde8fc4dc20b4d
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:25:33 +0000

    change the config according to the configuration setting change
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6071 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/WorkQueueManager/DefaultConfig.py
+++ src/python/WMComponent/WorkQueueManager/DefaultConfig.py
@@ -5,8 +5,8 @@ Defines default config values for JobAccountant specific
 parameters.
 """
 __all__ = []
-__revision__ = "$Id: DefaultConfig.py,v 1.1 2009/12/01 21:55:21 sryu Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: DefaultConfig.py,v 1.2 2010/01/22 22:07:26 sryu Exp $"
+__version__ = "$Revision: 1.2 $"
 
 import os
 
@@ -14,15 +14,18 @@ from WMCore.Agent.Configuration import Configuration
 
 config = Configuration()
 config.component_("WorkQueueManager")
-#accessType is either REST or DB. REST access is REST access to remote server
-#DB access is direct DB access (doens't need for DB reside in the same machine) 
-config.WorkQueueManager.accessType = 'REST'
-# unit can be either dataset or block
-config.WorkQueueManager.unit = 'dataset'
-config.WorkQueueManager.team = 'team_usa'
-config.WorkQueueManager.requestMgrHost = 'cmssrv49.fnal.gov:8585'
+# either request Mgr host url or global workqueue das service url needs to be set
+# depending on the level of work queue
+# accessing Request Manager.
+config.WorkQueueManager.level = "GlobalQueue"
+
+# serviceUrl is either upper level queue Url or request manager url in case of global queue 
+config.WorkQueueManager.serviceUrl = 'cmssrv49.fnal.gov:8585'
+config.WorkQueueManager.componentDir = config.General.WorkDir + "/WorkQueueManager"
+config.WorkQueueManager.namespace = "WMComponent.WorkQueueManager.WorkQueueManager"
+# accessing global queue
+#config.WorkQueueManager.level = "LocalQueue"
+#config.WorkQueueManager.serviceUrl = "http://cmssrv18.fnal.gov:6660"
 
 config.WorkQueueManager.logLevel = 'INFO'
 config.WorkQueueManager.pollInterval = 10
-config.WorkQueueManager.level = "GlobalQueue"
-config.WorkQueueManager.serviceUrl = "http://cmssrv18.fnal.gov:6660"

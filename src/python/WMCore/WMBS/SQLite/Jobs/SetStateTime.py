2c83d031fed689b1393ed29a09a6e480ef2c9a0e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:42:29 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6314 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/SQLite/Jobs/SetStateTime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+_SetStateTime_
+
+SQLite implementation of Jobs.SetStateTime
+"""
+
+__revision__ = "$Id: SetStateTime.py,v 1.1 2010/02/05 16:51:53 sfoulkes Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.WMBS.MySQL.Jobs.SetStateTime import SetStateTime as MySQLSetStateTime
+
+class SetStateTime(MySQLSetStateTime):
+    pass

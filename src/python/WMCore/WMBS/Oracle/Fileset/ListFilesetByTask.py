85a98ec738969be95c19a141da235534508b0bdc
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:41:54 +0000

    Remove an unwanted ~
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7933 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Fileset/ListFilesetByTask.py
+++ src/python/WMCore/WMBS/Oracle/Fileset/ListFilesetByTask.py
@@ -5,15 +5,14 @@ _ListFilesetByTask_
 Oracle implementation of Fileset.ListFilesetByTask
 """
 
-__all__ = []
-__revision__ = "$Id: ListFilesetByTask.py,v 1.2 2010/06/01 21:19:41 riahi Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: ListFilesetByTask.py,v 1.3 2010/06/24 16:33:04 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 
 
 from WMCore.WMBS.MySQL.Fileset.ListFilesetByTask import ListFilesetByTask as ListFilesetByTaskMySQL
 
 class ListFilesetByTask(ListFilesetByTaskMySQL):
-    sql = ListFilesetByTaskMySQL.sql
-~                                     
+    pass
+
 
 

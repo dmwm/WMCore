da4dd7c61f09a212b642da2c922fe30397e66a79
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:44:09 +0000

    remove dead code path
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5482 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Services/UUID.py
+++ src/python/WMCore/Services/UUID.py
@@ -1,29 +1,6 @@
 #!/usr/bin/env python
 
-import time
-import random
-import md5
-import socket
-import popen2
-
 import WMCore.Algorithms.External.uuid75421 as uuid
-
-def ourUUID( *args ):
-    """
-    Generates a universally unique ID.
-    Any arguments only create more randomness.
-    """
-    t = long( time.time() * 1000 )
-    r = long( random.random()*100000000000000000L )
-    try:
-        a = socket.gethostbyname( socket.gethostname() )
-    except:
-        # if we can't get a network address, just imagine one
-        a = random.random()*100000000000000000L
-    data = str(t)+' '+str(r)+' '+str(a)+' '+str(args)
-    data = md5.md5(data).hexdigest()
-    return data
-
 
 def makeUUID():
     """
@@ -31,9 +8,4 @@ def makeUUID():
     
     Makes a UUID from the uuid class, returns it
     """
-
-    guid = uuid.uuid1()
-    if not guid:
-        guid = ourUUID()
-    return str(guid)
-    
+    return str(uuid.uuid1())

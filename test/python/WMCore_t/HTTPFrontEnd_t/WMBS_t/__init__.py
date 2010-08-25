2388fe92e0d86b45178a9727d1663c0b516357dc
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:49:38 +0000

    add initial basic test. -- need to improve to test more details
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4675 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/python/WMCore_t/HTTPFrontEnd_t/WMBS_t/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+_Rest_t_
+
+REST component test methods
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2009/07/28 19:40:19 sryu Exp $"
+__version__ = "$Revision: 1.1 $"
+
+

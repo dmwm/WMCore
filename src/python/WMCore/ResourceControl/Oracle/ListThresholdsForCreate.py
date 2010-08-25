54cba318ab50b01854f27430f61803507b70b44f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 05:09:12 +0000

    Remove an extra group by.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8283 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/ResourceControl/Oracle/ListThresholdsForCreate.py
+++ src/python/WMCore/ResourceControl/Oracle/ListThresholdsForCreate.py
@@ -5,8 +5,8 @@ _ListThresholdsForCreate_
 Oracle implementation of ResourceControl.ListThresholdsForCreate
 """
 
-__revision__ = "$Id: ListThresholdsForCreate.py,v 1.2 2010/07/01 19:52:08 sfoulkes Exp $"
-__version__  = "$Revision: 1.2 $"
+__revision__ = "$Id: ListThresholdsForCreate.py,v 1.3 2010/07/28 20:11:17 sfoulkes Exp $"
+__version__  = "$Revision: 1.3 $"
 
 from WMCore.ResourceControl.MySQL.ListThresholdsForCreate \
      import ListThresholdsForCreate as MySQLListThresholdsForCreate
@@ -21,7 +21,6 @@ class ListThresholdsForCreate(MySQLListThresholdsForCreate):
                                 wmbs_job_assoc.fileid = wmbs_file_location.fileid
                               INNER JOIN wmbs_job ON
                                 wmbs_job_assoc.job = wmbs_job.id
-                            WHERE wmbs_job.location IS NULL    
-                            GROUP BY wmbs_job_assoc.job, wmbs_file_location.location) job_location ON
+                            WHERE wmbs_job.location IS NULL) job_location ON
                             wmbs_location.id = job_location.location
                        GROUP BY wmbs_location.site_name, wmbs_location.job_slots"""

04c21ddf6e40d1df9f99f092908ce34709f6ca3a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:44:27 +0000

    Start with clean dictionary
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4595 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/ThreadPool/MySQL/Destroy.py
+++ src/python/WMCore/ThreadPool/MySQL/Destroy.py
@@ -4,8 +4,8 @@ _Destroy_
 
 """
 
-__revision__ = "$Id: Destroy.py,v 1.1 2009/07/13 19:37:40 mnorman Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Destroy.py,v 1.2 2009/07/20 17:35:13 mnorman Exp $"
+__version__ = "$Revision: 1.2 $"
 
 import threading
 
@@ -30,6 +30,9 @@ class Destroy(DBCreator):
             
         DBCreator.__init__(self, logger, dbi)
 
+        self.create = {}
+
         self.create["03tp_threadpool"]             = "DROP TABLE tp_threadpool"
         self.create["02tp_threadpool_buffer_in"]   = "DROP TABLE tp_threadpool_buffer_in"
         self.create["01tp_threadpool_buffer_out"]  = "DROP TABLE tp_threadpool_buffer_out"
+

cca4f52c4d55194ae4f115ce0ca97ab53c97adb1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:09:30 +0000

    add failed status count
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4068 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/JobGroup/IsComplete.py
+++ src/python/WMCore/WMBS/MySQL/JobGroup/IsComplete.py
@@ -5,8 +5,8 @@ MySQL implementation of JobGroup.IsComplete
 Checks all the jobs are completed in  the given Job Group
 """
 __all__ = []
-__revision__ = "$Id: IsComplete.py,v 1.1 2009/03/24 21:59:33 sryu Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: IsComplete.py,v 1.2 2009/04/29 23:16:59 sryu Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -15,15 +15,17 @@ class IsComplete(DBFormatter):
                 (select count(id) from wmbs_job where jobgroup=:jobgroup 
                  ) as ac, 
                 (select count(job) from wmbs_group_job_complete where jobgroup=:jobgroup
-                ) as cm 
-            from dual
+                ) as cm,
+                (select count(job) from wmbs_group_job_failed where jobgroup=:jobgroup
+                ) as fa 
+             from dual
           """
 
     def format(self, result):
         result = DBFormatter.format(self, result)
         return result[0]
         
-    def execute(self, group=None, conn = None, transaction = False):
+    def execute(self, group, conn = None, transaction = False):
         binds = self.getBinds(jobgroup=group)
         result = self.dbi.processData(self.sql, binds, conn = conn,
                                       transaction = transaction)
--- src/python/WMCore/WMBS/SQLite/JobGroup/IsComplete.py
+++ src/python/WMCore/WMBS/SQLite/JobGroup/IsComplete.py
@@ -5,13 +5,14 @@ Sqlite implementation of JobGroup.IsComplete
 """
 
 __all__ = []
-__revision__ = "$Id: IsComplete.py,v 1.2 2009/03/31 16:25:16 sfoulkes Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: IsComplete.py,v 1.3 2009/04/29 23:18:25 sryu Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.WMBS.MySQL.JobGroup.IsComplete import IsComplete as IsCompleteJobGroupMySQL
 
 class IsComplete(IsCompleteJobGroupMySQL):
     sql = """select
                (select count(id) from wmbs_job where jobgroup=:jobgroup) as ac,
-               (select count(job) from wmbs_group_job_complete where jobgroup=:jobgroup) as cm
+               (select count(job) from wmbs_group_job_complete where jobgroup=:jobgroup) as cm,
+               (select count(job) from wmbs_group_job_failed where jobgroup=:jobgroup) as fa
           """

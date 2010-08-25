1673a9c975b725d8e49a245cb53f15ebc1710905
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:51:43 +0000

    Add in expired retry exception
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5590 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMExceptions.py
+++ src/python/WMCore/WMExceptions.py
@@ -7,20 +7,21 @@ mappings to a human readable message.
 
 """
 
-__revision__ = "$Id: WMExceptions.py,v 1.9 2008/11/13 16:05:37 fvlingen Exp $"
-__version__ = "$Revision: 1.9 $"
+__revision__ = "$Id: WMExceptions.py,v 1.10 2009/11/23 20:50:02 metson Exp $"
+__version__ = "$Revision: 1.10 $"
 __author__ = "fvlingen@caltech.edu"
 
 
-WMEXCEPTION = {'WMCore-1' : 'Not allowed to instantiate ', \
-   'WMCore-2' : 'Problem creating database table ',\
-   'WMCORE-3' : 'Could not find in library class ',\
-   'WMCORE-4' : 'Problem with loading the class ',\
-   'WMCORE-5' : 'Dialect not specified in configuration ',\
-   'WMCORE-6' : 'Message name is same as diagnostic message name! ',\
-   'WMCORE-7' : 'Component name is reserved word. ',\
-   'WMCORE-8' : 'No config section for this component! ',\
-   'WMCORE-9' : 'Problem inserting a trigger flag. ',\
-   'WMCORE-10': 'Problem setting trigger action. ',\
-   'WMCORE-11': 'Security exception. ',\
-   'WMCORE-12': 'Database connection problem '}
+WMEXCEPTION = {'WMCore-1' : 'Not allowed to instantiate ',
+   'WMCore-2' : 'Problem creating database table ',
+   'WMCORE-3' : 'Could not find in library class ',
+   'WMCORE-4' : 'Problem with loading the class ',
+   'WMCORE-5' : 'Dialect not specified in configuration ',
+   'WMCORE-6' : 'Message name is same as diagnostic message name! ',
+   'WMCORE-7' : 'Component name is reserved word. ',
+   'WMCORE-8' : 'No config section for this component! ',
+   'WMCORE-9' : 'Problem inserting a trigger flag. ',
+   'WMCORE-10': 'Problem setting trigger action. ',
+   'WMCORE-11': 'Security exception. ',
+   'WMCORE-12': 'Database connection problem ',
+   'WMCORE-13': 'Number of retries exceeded'}

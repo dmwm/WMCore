7490fb6ee1a3e54315f99ced67e4004f47e4fd33
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:45:26 +0000

    Inherit from WMException.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7979 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Storage/StageOutError.py
+++ src/python/WMCore/Storage/StageOutError.py
@@ -6,17 +6,13 @@ General Exception class for JC modules
 
 """
 
-__version__ = "$Revision: 1.1 $"
-__revision__ = "$Id: StageOutError.py,v 1.1 2009/06/18 21:14:34 meloam Exp $"
+__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: StageOutError.py,v 1.2 2010/06/29 19:01:25 sfoulkes Exp $"
 
-import exceptions
 import inspect
-#  //
-# // No logging module at runtime until SL4 becomes standard
-#//  use sys.stdout instead
-#import logging
 import sys
 
+from WMCore.WMException import WMException
 
 ErrorDefinitions = {
     60311 : "GeneralStageOutFailure",
@@ -24,7 +20,7 @@ ErrorDefinitions = {
     }
 
 
-class StageOutError(exceptions.Exception):
+class StageOutError(WMException):
     """
     _StageOutError_
 
@@ -33,94 +29,10 @@ class StageOutError(exceptions.Exception):
 
     """
     def __init__(self, message, **data):
-        self.name = str(self.__class__.__name__)
-        exceptions.Exception.__init__(self, self.name,
-                                      message)
+        WMException.__init__(self, message, **data)
 
-        #  //
-        # // Init data dictionary with defaults
-        #// 
-        self.data = {}
-        self.data.setdefault("ClassName", None)
-        self.data.setdefault("ModuleName", None)
-        self.data.setdefault("MethodName", None)
-        self.data.setdefault("ClassInstance", None)
-        self.data.setdefault("FileName", None)
-        self.data.setdefault("LineNumber", None)
         self.data.setdefault("ErrorCode", 60311)
         self.data.setdefault("ErrorType", ErrorDefinitions[60311])
-        
-        self.message = message
-        self.data.update(data)
-
-        #  //
-        # // Automatically determine the module name
-        #//  if not set
-        if self['ModuleName'] == None:
-            frame = inspect.currentframe()
-            lastframe = inspect.getouterframes(frame)[1][0]
-            excepModule = inspect.getmodule(lastframe)
-            if excepModule != None:
-                modName = excepModule.__name__
-                self['ModuleName'] = modName
-
-                
-        #  //
-        # // Find out where the exception came from
-        #//
-        stack = inspect.stack(1)[1]
-        self['FileName'] = stack[1]
-        self['LineNumber'] = stack[2]
-        self['MethodName'] = stack[3]
-
-        #  //
-        # // ClassName if ClassInstance is passed
-        #//
-        if self['ClassInstance'] != None:
-            self['ClassName'] = \
-              self['ClassInstance'].__class__.__name__
-
-        #logging.error(str(self))
-        
-        
-
-    def __getitem__(self, key):
-        """
-        make exception look like a dictionary
-        """
-        return self.data[key]
-
-    def __setitem__(self, key, value):
-        """
-        make exception look like a dictionary
-        """
-        self.data[key] = value
-        
-    def addInfo(self, **data):
-        """
-        _addInfo_
-
-        Add key=value information pairs to an
-        exception instance
-        """
-        for key, value in data.items():
-            self[key] = value
-        return
-
-
-    def __str__(self):
-        """create a string rep of this exception"""
-        strg = "%s\n" % self.name
-        strg += "Message: %s\n" % self.message
-        for key, value in self.data.items():
-            strg += "\t%s : %s\n" % (key, value, )
-        return strg
-
-
-
-
-
-
 
 class StageOutFailure(StageOutError):
     """
@@ -134,9 +46,7 @@ class StageOutFailure(StageOutError):
         self.data.setdefault("ErrorCode", 60311)
         self.data.setdefault("ErrorType", ErrorDefinitions[60311])
         
-        sys.stdout.write(str(self))
-        sys.stdout.write("\n")
-
+        return
           
 class StageOutInitError(StageOutError):
     """
@@ -150,10 +60,6 @@ class StageOutInitError(StageOutError):
         StageOutError.__init__(self, message, **data)
         self.data["ErrorCode"] =  60315
         self.data["ErrorType"] = ErrorDefinitions[60315]
-
-        sys.stdout.write(str(self))
-        sys.stdout.write("\n")
-
 
 class StageOutInvalidPath(StageOutError):
     """

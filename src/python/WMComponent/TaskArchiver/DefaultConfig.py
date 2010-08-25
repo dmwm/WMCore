1041968c17d1d4540fc467dff4858fad92d775fb
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:02:51 +0000

    Just for kicks, updated with timeOut param
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5746 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/TaskArchiver/DefaultConfig.py
+++ src/python/WMComponent/TaskArchiver/DefaultConfig.py
@@ -5,21 +5,22 @@ Defines default config values for JobAccountant specific
 parameters.
 """
 __all__ = []
-__revision__ = "$Id: DefaultConfig.py,v 1.1 2009/10/30 13:43:05 mnorman Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: DefaultConfig.py,v 1.2 2009/12/14 22:27:35 mnorman Exp $"
+__version__ = "$Revision: 1.2 $"
 
 import os
 
 from WMCore.Agent.Configuration import Configuration
 
 config = Configuration()
-config.component_("JobArchiver")
-config.JobArchiver.logLevel = 'INFO'
-config.JobArchiver.pollInterval = 10
+config.component_("TaskArchiver")
+config.TaskArchiver.logLevel = 'SQLDEBUG'
+config.TaskArchiver.pollInterval = 10
+config.TaskArchiver.timeOut      = 0
 
 
 config.component_('JobStateMachine')
-config.JobStateMachine.couchurl        = os.getenv('COUCHURL', 'cmssrv48.fnal.gov:5984')
+config.JobStateMachine.couchurl        = os.getenv('COUCHURL', 'cmssrv52.fnal.gov:5984')
 config.JobStateMachine.default_retries = 1
 
 

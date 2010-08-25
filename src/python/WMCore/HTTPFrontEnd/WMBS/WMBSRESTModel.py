7c3d427804492c84c32017342379f03ccc3d9da5
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:49:19 +0000

    Add a monitoring webpage and a couple new REST methods.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5557 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/HTTPFrontEnd/WMBS/WMBSRESTModel.py
+++ src/python/WMCore/HTTPFrontEnd/WMBS/WMBSRESTModel.py
@@ -7,33 +7,86 @@ Rest Model for WMBS Monitoring.
 from WMCore.WebTools.RESTModel import RESTModel
 from WMCore.DAOFactory import DAOFactory
 
+class MonitoringWebpage():
+    """
+    _MonitoringWebpage_
+
+    Container class for the WMBS monitoring webpage.  This will store references
+    to the WMBS REST model and use that to render templates into HTML.
+    """
+    def __init__(self, restModel):
+        self.restModel = restModel
+        return
+
+    def execute(self, *args, **kwargs):
+        """
+        _execute_
+
+        Return the correct webpage template given any parameters passed to the
+        monitoring REST method.
+        """
+        # If no subscription type is specified we'll display all subscriptions
+        # on the monitoring webpage.
+        if not kwargs.has_key("subType"):
+            kwargs["subType"] = "All"
+            
+        return self.restModel.templatepage("WMBS", **kwargs)
+
 class WMBSRESTModel(RESTModel):
     """
     A REST Model for WMBS. Currently only supports monitoring, so only
     implementing the GET verb.
     """
     def __init__(self, config = {}):
+        self.version = "$Revision: 1.8 $"        
         RESTModel.__init__(self, config)
         self.daofactory = DAOFactory(package = "WMCore.WMBS",
                                      logger = self,
                                      dbinterface = self.dbi)
-        
-        #only support get for now
-        self.methods = {'GET':{}}
+
+        # The following three methods are used by the monitoring webpages.
+        self.addDAO("GET", "listsubtypes", "Monitoring.ListSubTypes")
+        self.addDAO("GET", "subscriptionstatus",
+                    "Monitoring.SubscriptionStatus", ["subscriptionType"])
+        self.addDAO("GET", "listjobsbysub", "Monitoring.ListJobsBySub", 
+                    ["subscriptionId"], validation = [self.validateArgs])
+
+        wmbsMonitoringPage = MonitoringWebpage(self)
+        self.addRESTObject("GET", "monitoring", wmbsMonitoringPage)
+
         self.addDAO('GET', 'jobs', 'Monitoring.JobsByState')
         self.addDAO('GET', 'jobcount', 'Monitoring.JobCountByState')
-        self.addDAO('GET', 'jobsbysubs', 'Monitoring.JobsBySubscription', 
-                    ['fileset_name', 'workflow_name', 'state_time'],
-                     validation=[self.validateArgs])
         self.addDAO('GET', 'jobcountbysubs', 'Monitoring.JobCountBySubscriptionAndRun', 
                     ['subscription', 'run'],
                     validation=[self.validateArgs])
-        
+        return
+
+    def addRESTObject(self, verb, method, restObject, args = [],
+                      validation = [], version = 1):
+        """
+        _addRESTObject_
+
+        Add a REST object for a particular verb and key to the model.  As far as
+        this method is concerned, a REST object is any class instance that has
+        an execute() method.  The execute method should gather all positional
+        and keyword arguments that will be passed in by the calling code.
+        """
+        if not self.methods.has_key(verb):
+            self.methods[verb] = {}
+
+        self.methods[verb][method] = {"args": args,
+                                      "call": restObject.execute,
+                                      "validation": validation,
+                                      "version": version}
+        return
+
     def addDAO(self, verb, methodKey, daoStr, args=[], validation=[], version=1):
         """
         add dao (or any other method handler) in self.methods
         self.method need to be initialize if sub class doesn't want to take provide by
         """
+        if not self.methods.has_key(verb):
+            self.methods[verb] = {}
 
         dao = self.daofactory(classname=daoStr) 
                 

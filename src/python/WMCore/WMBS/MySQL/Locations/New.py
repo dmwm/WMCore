2bbafd0156b29d05d201b3492fa599c7f4138415
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 05:38:21 +0000

    Allow you to insert plugins into new sites
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8641 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Locations/New.py
+++ src/python/WMCore/WMBS/MySQL/Locations/New.py
@@ -8,16 +8,22 @@ MySQL implementation of Locations.New
 from WMCore.Database.DBFormatter import DBFormatter
 
 class New(DBFormatter):
-    sql = """INSERT INTO wmbs_location (site_name, se_name, ce_name, job_slots) 
+    sql = """INSERT INTO wmbs_location (site_name, se_name, ce_name, job_slots, plugin) 
                SELECT :location AS site_name, :sename AS se_name,
-                      :cename AS ce_name, :slots AS job_slots
+                      :cename AS ce_name, :slots AS job_slots, :plugin as plugin
                       FROM DUAL WHERE NOT EXISTS
                 (SELECT site_name FROM wmbs_location WHERE site_name = :location)"""
     
     def execute(self, siteName, jobSlots = 0, seName = None,
-                ceName = None, conn = None, transaction = False):
+                ceName = None, plugin = None, conn = None,
+                transaction = False):
+        """
+        _execute_
+
+        Now with 100% more plugin support
+        """
         binds = {"location": siteName, "slots": jobSlots, "sename": seName,
-                 "cename": ceName}
+                 "cename": ceName, "plugin": plugin}
         self.dbi.processData(self.sql, binds, conn = conn, 
                              transaction = transaction)
         return

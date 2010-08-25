747e011c7adedaac2f74e830e9b59d099718dcb8
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:05:46 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4009 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/JobSplitting/TwoFileBased.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+_TwoFileBased_
+
+File based job splitting for two file read workflows.  This works the same as
+normal file based splitting except that the input files will also have their
+parentage information loaded so that the parents can be included in the job.
+"""
+
+__revision__ = "$Id: TwoFileBased.py,v 1.1 2009/04/03 15:23:38 sfoulkes Exp $"
+__version__  = "$Revision: 1.1 $"
+
+from sets import Set
+
+from WMCore.JobSplitting.JobFactory import JobFactory
+from WMCore.Services.UUID import makeUUID
+
+class TwoFileBased(JobFactory):
+    def algorithm(self, groupInstance = None, jobInstance = None, *args,
+                  **kwargs):
+        """
+        _algorithm_
+
+        Split up all the available files such that each job will process a
+        maximum of "files_per_job".  If the "files_per_job" parameters is not
+        passed in jobs will process a maximum of 10 files.
+        """
+        filesPerJob = kwargs.get("files_per_job", 10)
+        filesInJob = 0
+        jobs = Set()
+
+        baseName = makeUUID()
+
+        for availableFile in self.subscription.availableFiles():
+            availableFile.loadData(parentage = 1)
+            if filesInJob == 0 or filesInJob == filesPerJob:
+                job = jobInstance(name = "%s-%s" % (baseName, len(jobs) + 1))
+                jobs.add(job)
+                filesInJob = 0
+
+            filesInJob += 1
+            job.addFile(availableFile)
+
+        if len(jobs) == 0:
+            return []
+
+        jobGroup = groupInstance(subscription = self.subscription)
+        jobGroup.add(jobs)
+        jobGroup.commit()
+        jobGroup.recordAcquire(list(jobs))
+
+        return [jobGroup]

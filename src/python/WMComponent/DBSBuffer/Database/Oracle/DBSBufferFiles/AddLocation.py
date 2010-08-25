227318a4f2f2501d1c4dfe4e3702088146d4d131
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:39:32 +0000

    Sync with changes to the MySQL version of this DAO.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5416 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSBuffer/Database/Oracle/DBSBufferFiles/AddLocation.py
+++ src/python/WMComponent/DBSBuffer/Database/Oracle/DBSBufferFiles/AddLocation.py
@@ -5,11 +5,17 @@ _AddLocation_
 Oracle implementation of DBSBufferFiles.AddLocation
 """
 
-__revision__ = "$Id: AddLocation.py,v 1.3 2009/10/13 19:40:21 sfoulkes Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: AddLocation.py,v 1.4 2009/10/22 14:47:30 sfoulkes Exp $"
+__version__ = "$Revision: 1.4 $"
 
-from WMComponent.DBSBuffer.Database.MySQL.DBSBufferFiles.AddLocation \
-     import AddLocation as MySQLAddLocation
+from WMComponent.DBSBuffer.Database.MySQL.DBSBufferFiles.AddLocation import AddLocation as \
+     MySQLAddLocation
 
 class AddLocation(MySQLAddLocation):
-    pass
+    existsSQL = """SELECT se_name, id FROM dbsbuffer_location
+                         WHERE se_name = :location"""
+
+    sql = """INSERT INTO dbsbuffer_location (se_name) 
+               SELECT :location AS se_name FROM DUAL WHERE NOT EXISTS
+                (SELECT se_name FROM dbsbuffer_location WHERE se_name = :location)"""
+    

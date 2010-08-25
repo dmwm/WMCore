063c6f0ad297f6f9ff30caf8414f2e0a154e1c72
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:09:56 +0000

    Use the rownum column correctly.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4982 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSUpload/Database/Oracle/FindUploadableFiles.py
+++ src/python/WMComponent/DBSUpload/Database/Oracle/FindUploadableFiles.py
@@ -5,8 +5,8 @@ _DBSUpload.FindUploadableFiles_
 Find the files in a datasets that needs to be uploaded to DBS
 
 """
-__revision__ = "$Id: FindUploadableFiles.py,v 1.2 2009/07/20 18:07:26 mnorman Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: FindUploadableFiles.py,v 1.3 2009/09/03 12:53:47 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 __author__ = "anzar@fnal.gov"
 
 from WMComponent.DBSUpload.Database.MySQL.FindUploadableFiles import FindUploadableFiles as MySQLFindUploadableFiles
@@ -16,9 +16,10 @@ class FindUploadableFiles(MySQLFindUploadableFiles):
     """
 Oracle implementation to find files in datasets that need to be uploaded to DBS
     """
-    
-
-    sql = """SELECT id as ID FROM (SELECT id, status, dataset_algo, ROWNUM rn FROM dbsbuffer_file)
-             WHERE dataset_algo = (SELECT ID FROM dbsbuffer_algo_dataset_assoc WHERE dataset_id =:dataset)
-             AND status =:status
-             AND rn <= :maxfiles""" 
+    sql = """SELECT * FROM
+               (SELECT dbsbuffer_file.id FROM dbsbuffer_file
+                  INNER JOIN dbsbuffer_algo_dataset_assoc ON
+                    dbsbuffer_file.dataset_algo = dbsbuffer_algo_dataset_assoc.id
+                WHERE status = :status AND
+                      dbsbuffer_algo_dataset_assoc.dataset_id = :dataset)
+             WHERE rownum <= :maxfiles"""

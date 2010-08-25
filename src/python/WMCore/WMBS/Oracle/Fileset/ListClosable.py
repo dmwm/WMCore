f1226c7472f66a8d0779121e316db678f49efd68
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:10:57 +0000

    Don't return filesets which have been closed previously.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4997 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Fileset/ListClosable.py
+++ src/python/WMCore/WMBS/Oracle/Fileset/ListClosable.py
@@ -6,8 +6,8 @@ MySQL implementation of Fileset.ListClosable
 """
 
 __all__ = []
-__revision__ = "$Id: ListClosable.py,v 1.1 2009/04/28 13:59:17 sfoulkes Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: ListClosable.py,v 1.2 2009/09/03 19:47:26 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WMBS.MySQL.Fileset.ListClosable import ListClosable as ListFilesetClosableMySQL
 
@@ -17,6 +17,8 @@ class ListClosable(ListFilesetClosableMySQL):
                  wmbs_subscription.fileset = wmbs_fileset.id
                INNER JOIN wmbs_workflow_output ON
                  wmbs_subscription.workflow = wmbs_workflow_output.workflow_id
+               INNER JOIN (SELECT id, open FROM wmbs_fileset) output_fileset ON
+                 wmbs_workflow_output.output_fileset = output_fileset.id                 
                INNER JOIN (SELECT fileset, COUNT(fileid) AS total_files
                             FROM wmbs_fileset_files GROUP BY fileset) fileset_size ON
                  wmbs_subscription.fileset = fileset_size.fileset
@@ -26,7 +28,7 @@ class ListClosable(ListFilesetClosableMySQL):
                LEFT OUTER JOIN (SELECT subscription, COUNT(fileid) AS total_files
                    FROM wmbs_sub_files_failed GROUP BY subscription) sub_failed ON
                  wmbs_subscription.id = sub_failed.subscription
-             WHERE wmbs_fileset.open = '0' AND
+             WHERE wmbs_fileset.open = '0' AND output_fileset.open = 1 AND 
                fileset_size.total_files = (COALESCE(sub_complete.total_files, 0) +
                                            COALESCE(sub_failed.total_files, 0))
     """                            

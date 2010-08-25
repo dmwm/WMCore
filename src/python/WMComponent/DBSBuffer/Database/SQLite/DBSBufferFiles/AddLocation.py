b4dbef2899d164f9daaa2f0bc20726bbd74c98ef
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:19:42 +0000

    Mimick MySQL behaviour.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7640 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSBuffer/Database/SQLite/DBSBufferFiles/AddLocation.py
+++ src/python/WMComponent/DBSBuffer/Database/SQLite/DBSBufferFiles/AddLocation.py
@@ -6,13 +6,18 @@ SQLite implementation of DBSBufferFiles.AddLocation
 """
 
 from WMCore.Database.DBFormatter import DBFormatter
+from WMCore.Database.Transaction import Transaction
 
 class AddLocation(DBFormatter):
     sql = """INSERT INTO dbsbuffer_location (se_name) 
                SELECT :location AS se_name WHERE NOT EXISTS
                 (SELECT se_name FROM dbsbuffer_location WHERE se_name = :location)"""
-    
+
+    existsSQL = """SELECT se_name, id FROM dbsbuffer_location
+                   WHERE se_name = :location"""
+
     def execute(self, siteName, conn = None, transaction = False):
+
         if type(siteName) == str:
             binds = {"location": siteName}
         else:
@@ -20,6 +25,18 @@ class AddLocation(DBFormatter):
             for aLocation in siteName:
                 binds.append({"location": aLocation})
 
+        myTransaction = Transaction(self.dbi)
+        myTransaction.begin()
+
+        nameMap = {}
         self.dbi.processData(self.sql, binds, conn = conn, 
                              transaction = transaction)
-        return
+        results = self.dbi.processData(self.existsSQL, binds,
+                                           conn = myTransaction.conn,
+                                           transaction = True)
+        results = self.format(results)
+        for result in results:
+            nameMap[result[0]] = int(result[1])
+
+        myTransaction.commit()
+        return nameMap

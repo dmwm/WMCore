ab0caf58fa8e8411921d80d30c4bc03d2190c370
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:23:41 +0000

    Add placeholder/notes for supporting pileup file lists within the input sandbox
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5183 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMSpec/Steps/Fetchers/PileupFetcher.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+_PileupFetcher_
+
+Given a pileup dataset, pull the information required to cache the list
+of pileup files in the job sandbox for the dataset.
+
+"""
+
+
+from WMCore.WMSpec.Steps.Fetchers.FetcherInterface import FetcherInterface
+import WMCore.WMSpec.WMStep as WMStep
+
+
+
+class PileupFetcher(FetcherInterface):
+    """
+    _PileupFetcher_
+
+    Pull dataset block/SE : LFN list from DBS for the
+    pileup datasets required by the steps in the job.
+
+    Archive these maps as files in the sandbox
+
+    """
+
+    def __call__(self, wmTask):
+        """
+        Trip through the input pileup datasets required by the
+        task and pull the information from DBS.
+
+        Stack the information in a file that maps block/SE to a list
+        of LFNs at that site so that the available LFNs can be
+        looked up on arrival at a site within the runtime environment
+
+        """
+        #TODO: Implement this
+        raise NotImplementedError, "Mail Evans and tell him to write this"

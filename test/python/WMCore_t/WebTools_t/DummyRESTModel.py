ecac2e0dc1843ae7178f1e6ccb0f4fb6d4e935e6
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:12:35 +0000

    test addDAO changes. also test default ping sent 400 errors when argument doesn't match.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5881 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/python/WMCore_t/WebTools_t/DummyRESTModel.py
@@ -0,0 +1,83 @@
+from WMCore.WebTools.RESTModel import RESTModel
+
+class DummyDAO1:
+    def execute(self):
+        return 123
+
+class DummyDAO2:
+    def execute(self, num):
+        return {'num': num}
+
+class DummyDAOFac:
+    def __call__(self, classname='DummyDAO1'):
+        dao = None
+        if classname == 'DummyDAO1':
+            dao = DummyDAO1()
+        elif classname == 'DummyDAO2':
+            dao = DummyDAO2()
+        return dao
+    
+class DummyRESTModel(RESTModel):
+    def __init__(self, config):
+        '''
+        Initialise the RESTModel and add some methods to it.
+        '''
+        RESTModel.__init__(self, config)
+        self.methods['GET'] = {'list':{'args':['int', 'str'],
+                                        'call': self.list,
+                                        'version': 2,
+                                        'validation': [self.val_1, 
+                                                       self.val_2, 
+                                                       self.val_3, 
+                                                       self.val_4]},
+                                'list1':{'args':[],
+                                         'call': self.list1,
+                                         'version': 2,
+                                         'validation': []},
+                                         
+                                'list2':{'args':['num0', 'num1', 'num2'],
+                                         'call': self.list2,
+                                         'version': 2,
+                                         'validation': []}}
+        self.daofactory = DummyDAOFac()
+        self.addDAO('GET', 'data1', 'DummyDAO1', [])
+        self.addDAO('GET', 'data2', 'DummyDAO2', ['num'])
+        
+    def list(self, int, str):
+        input = self.sanitise_input('list', int, str)
+        return input
+
+#TODO necessary to Test this?    
+#    def list1(self):
+#        return 'No arguement'
+#    
+#    def list2(self, num0, num1, num2):
+#        input = self.sanitise_input(num0, num1, num3, 'list')
+#        return input
+    
+    def val_1(self, input):
+        # Convert the input data to an int (will be a string), ignore if it 
+        # fails as the next validation will kill that, and it makes the unit test
+        # trickier...
+        try:
+            input['int'] = int(input['int'])
+        except:
+            pass
+        # Checks its first input contains a int
+        assert type(input['int']) == type(123)
+        return input
+    
+    def val_2(self, input):
+        # Checks its second input is a string
+        assert type(input['str']) == type('abc')
+        return input
+    
+    def val_3(self, input):
+        # Checks the int is 123
+        assert input['int'] == 123
+        return input
+    
+    def val_4(self, input):
+        # Checks the str is 'abc'
+        assert input['str'] == 'abc'
+        return input
--- test/python/WMCore_t/WebTools_t/REST_t.py
+++ test/python/WMCore_t/WebTools_t/REST_t.py
@@ -7,8 +7,8 @@ etc..
 
 """
 
-__revision__ = "$Id: REST_t.py,v 1.7 2009/12/28 17:45:28 sryu Exp $"
-__version__ = "$Revision: 1.7 $"
+__revision__ = "$Id: REST_t.py,v 1.8 2009/12/28 21:37:06 sryu Exp $"
+__version__ = "$Revision: 1.8 $"
 
 import unittest
 import os
@@ -21,79 +21,14 @@ from httplib import HTTPConnection
 from cherrypy import HTTPError
 from wsgiref.handlers import format_date_time
 from WMQuality.TestInit import TestInit
-from WMCore.WebTools.RESTModel import RESTModel
 from WMCore.Configuration import Configuration
 from WMCore.WebTools.Root import Root
 from WMCore.WebTools.Page import make_rfc_timestamp
-
-class DummyDAO1:
-    def execute(self, input={}):
-        return {'data': 123, 'input': input}
-
-class DummyDAO2:
-    def execute(self, input={}):
-        return {'data': 456, 'input': input}
-
-class DummyDAOFac:
-    def __call__(self, classname='DummyDAO1'):
-        dao = None
-        if classname == 'DummyDAO1':
-            dao = DummyDAO1()
-        elif classname == 'DummyDAO2':
-            dao = DummyDAO2()
-        return dao
-
-class DummyRESTModel(RESTModel):
-    def __init__(self, config):
-        '''
-        Initialise the RESTModel and add some methods to it.
-        '''
-        RESTModel.__init__(self, config)
-        self.methods['GET'] = {'list':{'args':['int', 'str'],
-                                        'call': self.list,
-                                        'version': 2,
-                                        'validation': [self.val_1, 
-                                                       self.val_2, 
-                                                       self.val_3, 
-                                                       self.val_4]}}
-        self.daofactory = DummyDAOFac()
-        self.addDAO('GET', 'data1', 'DummyDAO1', ['num'])
-        self.addDAO('GET', 'data2', 'DummyDAO2', ['num'])
-        
-    def list(self, args, kwargs):
-        input = self.sanitise_input(args, kwargs, 'list')
-        return input
-    
-    def val_1(self, input):
-        # Convert the input data to an int (will be a string), ignore if it 
-        # fails as the next validation will kill that, and it makes the unit test
-        # trickier...
-        try:
-            input['int'] = int(input['int'])
-        except:
-            pass
-        # Checks its first input contains a int
-        assert type(input['int']) == type(123)
-        return input
-    
-    def val_2(self, input):
-        # Checks its second input is a string
-        assert type(input['str']) == type('abc')
-        return input
-    
-    def val_3(self, input):
-        # Checks the int is 123
-        assert input['int'] == 123
-        return input
-    
-    def val_4(self, input):
-        # Checks the str is 'abc'
-        assert input['str'] == 'abc'
-        return input
+from DummyRESTModel import DummyRESTModel
         
 class RESTTest(unittest.TestCase):
     
-    def configureServer(self, das=False):
+    def configureServer(self, restModel='WMCore.WebTools.RESTModel', das=False):
         dummycfg = Configuration()
         dummycfg.component_('Webtools')
         dummycfg.Webtools.application = 'UnitTests'
@@ -111,7 +46,7 @@ class RESTTest(unittest.TestCase):
         active.rest.templates = '/tmp'
         active.rest.database = 'sqlite://'
         active.rest.section_('model')
-        active.rest.model.object = 'WMCore.WebTools.RESTModel'
+        active.rest.model.object = restModel
         active.rest.section_('formatter')
         if das:
             active.rest.formatter.object = 'WMCore.WebTools.DASRESTFormatter'
@@ -191,7 +126,7 @@ class RESTTest(unittest.TestCase):
         rt.start(blocking=False)
         cherrypy.log.error_log.setLevel(logging.WARNING)
         cherrypy.log.access_log.setLevel(logging.WARNING)
-        data, code, type, response = self.makeRequest('/rest/ping', 
+        data, code, type, response = self.makeRequest('/rest/ping/',
                                           type='GET', accept='text/json')
         assert code == 200, 'Got a return code != 200 (got %s), message: %s' % (code, data)
         assert type == 'text/json'
@@ -201,12 +136,26 @@ class RESTTest(unittest.TestCase):
         
         rt.stop()
         
-    def testDasPing(self):
-        rt = self.configureServer(True)
+    def testBadPing(self):
+        rt = self.configureServer()
         rt.start(blocking=False)
         cherrypy.log.error_log.setLevel(logging.WARNING)
         cherrypy.log.access_log.setLevel(logging.WARNING)
-        data, code, type, response = self.makeRequest('/rest/ping', 
+        data, code, type, response = self.makeRequest('/rest/ping/wrong/',
+                                          type='GET', accept='text/json')
+        assert code == 400, 'Got a return code != 400 (got %s), message: %s' % (code, data)
+        assert type == 'text/json'
+        expires = response.getheader('Expires')
+        assert expires == make_rfc_timestamp(300), 'Expires header incorrect (%s)' % expires 
+
+        rt.stop()
+        
+    def testDasPing(self):
+        rt = self.configureServer(das=True)
+        rt.start(blocking=False)
+        cherrypy.log.error_log.setLevel(logging.WARNING)
+        cherrypy.log.access_log.setLevel(logging.WARNING)
+        data, code, type, response = self.makeRequest('/rest/ping',
                                           type='GET', accept='text/json+das')
         assert code == 200, 'Got a return code != 200 (got %s), message: %s' % (code, data)
         assert type == 'text/json+das'
@@ -222,6 +171,19 @@ class RESTTest(unittest.TestCase):
         
         rt.stop()
     
+    def testList(self):
+        rt = self.configureServer(restModel='DummyRESTModel')
+        rt.start(blocking=False)
+        cherrypy.log.error_log.setLevel(logging.WARNING)
+        cherrypy.log.access_log.setLevel(logging.WARNING)
+        data, code, type, response = self.makeRequest('/rest/list/', {'int':123, 'str':'abc'},
+                                          type='GET', accept='text/json')
+        assert code == 200, 'Got a return code != 200 (got %s), message: %s' % (code, data)
+        assert type == 'text/json'
+        expires = response.getheader('Expires')
+        assert expires == make_rfc_timestamp(300), 'Expires header incorrect (%s)' % expires
+        assert data == '{"int": 123, "str": "abc"}', 'data is not correct %s' % data
+        
     def makeRequest(self, uri='/rest/', values=None, type='GET', accept="text/plain"):
         headers = {}
         headers = {"Content-type": "application/x-www-form-urlencoded",
@@ -262,14 +224,14 @@ class RESTTest(unittest.TestCase):
         drm = DummyRESTModel(component)
         
         # 2 positional args
-        result = drm.list([123, 'abc'], {})
+        result = drm.list(*[123, 'abc'], **{})
         assert result == {'int':123, 'str':'abc'}
         # 2 query string args
-        result = drm.list([], {'int':123, 'str':'abc'})
+        result = drm.list(*[], **{'int':123, 'str':'abc'})
         assert result == {'int':123, 'str':'abc'}
         
         # 1 positional, 1 keyword
-        result = drm.list([123], {'str':'abc'})
+        result = drm.list(*[123], **{'str':'abc'})
         assert result == {'int':123, 'str':'abc'}
         
     def testSanitiseAssertFail(self):
@@ -282,19 +244,19 @@ class RESTTest(unittest.TestCase):
         drm = DummyRESTModel(component)
         
         # Wrong type for input args
-        self.assertRaises(HTTPError, drm.list, [123, 123], {})
-        self.assertRaises(HTTPError, drm.list, ['abc', 'abc'], {})
-        self.assertRaises(HTTPError, drm.list, [], {'str':123, 'int':'abc'})
-        self.assertRaises(HTTPError, drm.list, [], {'str':'abc', 'int':'abc'})
-        self.assertRaises(HTTPError, drm.list, ['abc', 123], {})
-        self.assertRaises(HTTPError, drm.list, ['abc', 'abc'], {})
-        self.assertRaises(HTTPError, drm.list, [], {'str':123, 'int':'abc'})
-        self.assertRaises(HTTPError, drm.list, [], {'str':123, 'int':123})
-        self.assertRaises(HTTPError, drm.list, [], {'str':'abc', 'int':'abc'})
+        self.assertRaises(HTTPError, drm.list, *[123, 123], **{})
+        self.assertRaises(HTTPError, drm.list, *['abc', 'abc'], **{})
+        self.assertRaises(HTTPError, drm.list, *[], **{'str':123, 'int':'abc'})
+        self.assertRaises(HTTPError, drm.list, *[], **{'str':'abc', 'int':'abc'})
+        self.assertRaises(HTTPError, drm.list, *['abc', 123], **{})
+        self.assertRaises(HTTPError, drm.list, *['abc', 'abc'], **{})
+        self.assertRaises(HTTPError, drm.list, *[], **{'str':123, 'int':'abc'})
+        self.assertRaises(HTTPError, drm.list, *[], **{'str':123, 'int':123})
+        self.assertRaises(HTTPError, drm.list, *[], **{'str':'abc', 'int':'abc'})
         
         # Incorrect values for input args
-        self.assertRaises(HTTPError, drm.list, [1234, 'abc'], {})
-        self.assertRaises(HTTPError, drm.list, [123, 'abcd'], {})
+        self.assertRaises(HTTPError, drm.list, *[1234, 'abc'], **{})
+        self.assertRaises(HTTPError, drm.list, *[123, 'abcd'], **{})
         
     def testSanitiseKeyFail(self):
         config = Configuration()
@@ -308,7 +270,7 @@ class RESTTest(unittest.TestCase):
         self.assertRaises(HTTPError, drm.list, [], {})
         # Out of order input data
         self.assertRaises(HTTPError, drm.list, ['abc'], {'int':123})
-    
+        
     def testDAOBased(self):
         config = Configuration()
         component = config.component_('UnitTests')
@@ -317,21 +279,14 @@ class RESTTest(unittest.TestCase):
                 
         drm = DummyRESTModel(component)
         
-        result = drm.methods['GET']['data1']['call']([], {})
-        assert result['data'] == 123
-        assert result['input'] == {}
+        result = drm.methods['GET']['data1']['call']()
+        assert result == 123, 'Error default value is set to 123 but returns %s' % result
         
-        result =  drm.methods['GET']['data1']['call']([123], {})
-        assert result['data'] == 123
-        assert result['input'] == {'num': 123}
+        result =  drm.methods['GET']['data2']['call'](456)
+        assert result['num'] == 456
         
-        result =  drm.methods['GET']['data2']['call']([], {})
-        assert result['data'] == 456
-        assert result['input'] == {}
-        
-        result =  drm.methods['GET']['data2']['call']([456], {})
-        assert result['data'] == 456
-        assert result['input'] == {'num': 456}
+        result =  drm.methods['GET']['data2']['call'](num = 456)
+        assert result['num'] == 456
         
         
 if __name__ == "__main__":

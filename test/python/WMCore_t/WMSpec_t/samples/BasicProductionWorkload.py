99cbfa18a1c3c77b7a7180ecb6568e40ee82d50e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:26:06 +0000

    start working repository of major new workflow types for testing & development
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4317 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/python/WMCore_t/WMSpec_t/samples/BasicProductionWorkload.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+"""
+_BasicProductionWorkload_
+
+Sample/Tester for a simple production workflow and associated merge.
+
+Production task produces a single output dataset,
+Merge task is used to merge that dataset
+
+"""
+from WMCore.WMSpec.WMWorkload import newWorkload
+from WMCore.WMSpec.WMStep import makeWMStep
+from WMCore.WMSpec.Steps.StepFactory import getStepTypeHelper
+
+#  //
+# // Set up the basic workload task and step structure
+#//
+workload = newWorkload("BasicProduction")
+
+#  //
+# // set up the production task
+#//
+production = workload.newTask("Production")
+prodCmssw = production.makeStep("cmsRun1")
+prodCmssw.setStepType("CMSSW")
+prodStageOut = prodCmssw.addStep("stageOut1")
+prodStageOut.setStepType("StageOut")
+production.applyTemplates()
+
+#  //
+# // set up the merge task
+#//
+merge = workload.newTask("Merge")
+mergeCmssw = merge.makeStep("cmsRun1")
+mergeCmssw.setStepType("CMSSW")
+mergeStageOut = mergeCmssw.addStep("stageOut1")
+mergeStageOut.setStepType("StageOut")
+merge.applyTemplates()
+
+
+#  //
+# // populate the details of the production tasks
+#//
+#  //
+# // production cmssw step
+#//
+#
+# TODO: Anywhere helper.data is accessed means we need a method added to the
+# type based helper class to provide a clear API.
+prodCmsswHelper = prodCmssw.getTypeHelper()
+
+prodCmsswHelper.data.application.setup.cmsswVersion = "CMSSW_X_Y_Z"
+prodCmsswHelper.data.application.setup.softwareEnvironment = " . /uscmst1/prod/sw/cms/bashrc prod"
+
+prodCmsswHelper.data.application.configuration.configCacheUrl = "http://whatever"
+
+prodCmsswHelper.addOutputModule("writeData", primaryDataset = "Primary",
+                                processedDataset = "Processed",
+                                dataTier = "TIER")
+#print prodCmsswHelper.data
+
+
+
+#  //
+# // production stage out step
+#//
+prodStageOutHelper =prodStageOut.getTypeHelper()
+
+
+#  //
+# // merge cmssw step
+#//
+# point it at the input step from the previous task
+merge.setInputReference(prodCmssw, outputModule = "writeData")
+
+
+#  //
+# // populate the details of the merge tasks
+#//
+
+
+
+print workload.data

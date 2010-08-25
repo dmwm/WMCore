ae2f11ba79bd0f3aed1db773716665c2b08b02b5
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:39:40 +0000

    Fix this up so that it doesn't cause database deadlocks:   - Remove the duplicate entry check, code that calls this shouldn't be     setting the location of a file multiple times anyway.   - Modify this to only take file/location IDs instead of LFNs to avoid     unnecessary joins.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5418 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSBuffer/Database/MySQL/DBSBufferFiles/SetLocation.py
+++ src/python/WMComponent/DBSBuffer/Database/MySQL/DBSBufferFiles/SetLocation.py
@@ -5,33 +5,16 @@ _SetLocation_
 MySQL implementation of DBSBuffer.SetLocation
 """
 
+__revision__ = "$Id: SetLocation.py,v 1.4 2009/10/22 14:55:57 sfoulkes Exp $"
+__version__ = "$Revision: 1.4 $"
+
 from WMCore.Database.DBFormatter import DBFormatter
-from sets import Set
 
 class SetLocation(DBFormatter):
-    sql = """INSERT INTO dbsbuffer_file_location (filename, location) 
-               SELECT dbsbuffer_file.id, dbsbuffer_location.id from dbsbuffer_file, dbsbuffer_location 
-             WHERE dbsbuffer_file.lfn = :lfn
-             AND dbsbuffer_location.se_name = :location
-             AND NOT EXISTS (SELECT * FROM dbsbuffer_file_location WHERE filename = dbsbuffer_file.id
-             and location = dbsbuffer_location.id)"""
-                
-    def getBinds(self, file = None, location = None):
-        if type(location) == type('string'):
-            return self.dbi.buildbinds(self.dbi.makelist(file), 'lfn', 
-                   self.dbi.buildbinds(self.dbi.makelist(location), 'location'))
-        elif isinstance(location, (list, Set, set)):
-            binds = []
-            for l in location:
-                binds.extend(self.dbi.buildbinds(self.dbi.makelist(file), 'lfn', 
-                   self.dbi.buildbinds(self.dbi.makelist(l), 'location')))
-            return binds
-        else:
-            raise Exception, "Type of location argument is not allowed: %s" \
-                                % type(location)
+    sql = """INSERT INTO dbsbuffer_file_location (filename, location)
+               VALUES (:fileid, :locationid)"""
     
-    def execute(self, file, location, conn = None, transaction = None):
-        binds = self.getBinds(file, location)
-        result = self.dbi.processData(self.sql, binds, conn = conn,
-                                      transaction = transaction)
+    def execute(self, binds, conn = None, transaction = None):
+        self.dbi.processData(self.sql, binds, conn = conn,
+                             transaction = transaction)
         return

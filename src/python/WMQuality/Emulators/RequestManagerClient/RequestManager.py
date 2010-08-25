caadeea13e2c0cddddb341b9e6991d82ec95ab55
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:24:13 +0000

    limit the number of spec generated. - ToDo make it configurable.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6887 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMQuality/Emulators/RequestManagerClient/RequestManager.py
+++ src/python/WMQuality/Emulators/RequestManagerClient/RequestManager.py
@@ -6,18 +6,24 @@ class RequestManager(object):
         print "Using RequestManager Emulator"
         self.specGenerator = WMSpecGenerator()
         self.count = 0
+        self.maxWmSpec = 10
     
     def getAssignment(self, teamName=None, request=None):
-        #specName = "FakeProductionSpec_%s" % self.count
-        #specUrl =self.specGenerator.createProductionSpec(specName, "file")
-        #specName = "FakeProcessingSpec_%s" % self.count
-        #specUrl =self.specGenerator.createProcessingSpec(specName, "file")
-        specName = "MinBiasProcessingSpec_Test_%s" % self.count
-        specUrl =self.specGenerator.createReRecoSpec(specName, "file")
+        if self.count < self.maxWmSpec:
+            #specName = "FakeProductionSpec_%s" % self.count
+            #specUrl =self.specGenerator.createProductionSpec(specName, "file")
+            #specName = "FakeProcessingSpec_%s" % self.count
+            #specUrl =self.specGenerator.createProcessingSpec(specName, "file")
         
-        self.count += 1
-        return {specName:specUrl}
+            specName = "MinBiasProcessingSpec_Test_%s" % self.count
+            specUrl =self.specGenerator.createReRecoSpec(specName, "file")
+            self.count += 1
+            return {specName:specUrl}
+        else:
+            return {}
     
+        
+        
     def postAssignment(self, requestName, prodAgentUrl=None):
         # do not thing or return success of fail massage 
         return 

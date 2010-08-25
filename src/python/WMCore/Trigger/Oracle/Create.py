d8a886030eccfc66ee339939b3dd4e369de4d2a9
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:08:16 +0000

    Move constraint statements out of the table create statements so that we can load indexes into different table spaces.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4956 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Trigger/Oracle/Create.py
+++ src/python/WMCore/Trigger/Oracle/Create.py
@@ -8,9 +8,8 @@ Class for creating MySQL specific schema for the trigger
 
 """
 
-__revision__ = "$Id: Create.py,v 1.1 2008/09/19 15:34:35 fvlingen Exp $"
-__version__ = "$Revision: 1.1 $"
-__author__ = "fvlingen@caltech.edu"
+__revision__ = "$Id: Create.py,v 1.2 2009/08/31 19:12:40 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
 
 import threading
 
@@ -22,28 +21,40 @@ class Create(DBCreator):
     
     Class for creating MySQL specific schema for the trigger.
     """
-    
-    
-    
-    def __init__(self):
+    def __init__(self, logger = None, dbi = None, params = None):
         myThread = threading.currentThread()
         DBCreator.__init__(self, myThread.logger, myThread.dbi)
         self.create = {}
         self.constraints = {}
-        self.create['a_tr_trigger'] = """
-CREATE TABLE tr_action
-(	id VARCHAR2(32 BYTE) NOT NULL ENABLE, 
-	trigger_id VARCHAR2(32 BYTE) NOT NULL ENABLE, 
-	action_name VARCHAR2(255 BYTE) NOT NULL ENABLE, 
-	payload CLOB NOT NULL ENABLE, 
-	 CONSTRAINT TR_ACTION_UK1 UNIQUE (id, trigger_id, action_name)
-)
-"""
-        self.create['b_tr_action'] = """
-CREATE TABLE tr_trigger 
-(	id VARCHAR2(32 BYTE) NOT NULL ENABLE, 
-	trigger_id VARCHAR2(32 BYTE) NOT NULL ENABLE, 
-	flag_id VARCHAR2(32 BYTE) NOT NULL ENABLE, 
-	 CONSTRAINT TR_TRIGGER_PK PRIMARY KEY (id, trigger_id, flag_id)
-)  
-"""
+
+        tablespaceTable = ""
+        tablespaceIndex = ""
+
+        if params:
+            if params.has_key("tablespace_table"):
+                tablespaceTable = "TABLESPACE %s" % params["tablespace_table"]
+            if params.has_key("tablespace_index"):
+                tablespaceIndex = "USING INDEX TABLESPACE %s" % params["tablespace_index"]
+                                                            
+        self.create["01_tr_action"] = \
+          """CREATE TABLE tr_action (
+               id          VARCHAR2(32 BYTE)  NOT NULL ENABLE, 
+	       trigger_id  VARCHAR2(32 BYTE)  NOT NULL ENABLE, 
+	       action_name VARCHAR2(255 BYTE) NOT NULL ENABLE, 
+	       payload     CLOB               NOT NULL ENABLE 
+               ) %s""" % tablespaceTable
+
+        self.indexes["01_pk_tr_action"] = \
+          """ALTER TABLE tr_action ADD                            
+	       (CONSTRAINT tr_action_pk PRIMARY KEY (id, trigger_id, action_name) %s)""" % tablespaceIndex
+
+        self.create["01_tr_trigger"] = \
+          """CREATE TABLE tr_trigger (
+               id         VARCHAR2(32 BYTE) NOT NULL ENABLE, 
+	       trigger_id VARCHAR2(32 BYTE) NOT NULL ENABLE, 
+	       flag_id    VARCHAR2(32 BYTE) NOT NULL ENABLE
+               ) %s""" % tablespaceTable
+
+        self.indexes["01_pk_tr_action"] = \
+          """ALTER TABLE tr_action ADD
+               (CONSTRAINT TR_TRIGGER_PK PRIMARY KEY (id, trigger_id, flag_id) %s)""" % tablespaceIndex

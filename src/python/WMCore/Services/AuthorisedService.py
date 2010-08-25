c8d072379722d9a7091f48ab9cd0dbc7eaaf7630
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:34:25 +0000

    add code looking for key, cert from environment if they are not specified  - some refactoring
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4443 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Services/AuthorisedService.py
+++ src/python/WMCore/Services/AuthorisedService.py
@@ -6,8 +6,8 @@ An AuthorisedService is the same as a Service but sends a cert/key with the url
 opener to access secured resources.
 """
 
-__revision__ = "$Id: AuthorisedService.py,v 1.4 2009/07/07 18:53:20 sryu Exp $"
-__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: AuthorisedService.py,v 1.5 2009/07/08 14:40:35 sryu Exp $"
+__version__ = "$Revision: 1.5 $"
 
 import datetime, os, urllib, time
 
@@ -15,38 +15,62 @@ from WMCore.WMException import WMException
 from WMCore.WMExceptions import WMEXCEPTION
 from WMCore.Services.Service import Service
 
+def getKeyCert():
+    """
+       _getKeyCert_
+       
+       Gets the User Proxy if it exists otherwise throws an exception
+       This code is bollowed from DBSAPI/dbsHttpService.py with Anzar's permission
+    """
+
+    # First presendence to HOST Certificate, This is how it set in Tier0
+    if os.environ.has_key('X509_HOST_CERT'):
+        cert = os.environ['X509_HOST_CERT']
+        key = os.environ['X509_HOST_KEY']
+        
+    # Second preference to User Proxy, very common
+    elif os.environ.has_key('X509_USER_PROXY'):
+        cert = os.environ['X509_USER_PROXY']
+        key = cert
+
+    # Third preference to User Cert/Proxy combinition
+    elif os.environ.has_key('X509_USER_CERT'):
+        cert = os.environ['X509_USER_CERT']
+        key = os.environ['X509_USER_KEY']
+    
+    #TODO: only in linux, unix case, add other os case
+    # Worst case, look for proxy at default location /tmp/x509up_u$uid
+    else :
+        uid = os.getuid()
+        cert = '/tmp/x509up_u'+str(uid)
+        key = cert
+
+    #Set but not found
+    if not os.path.exists(cert) or not os.path.exists(key):
+        msg = "Certificate is not found"
+        raise Exception, msg
+    
+    # All looks OK, still doesn't gurantee proxy's validity etc.
+    return key, cert
+
 class AuthorisedService(Service):
     def __init__(self, dict={}):
         Service.__init__(self, dict)
         try:
-            self.cert = dict['cert']
-            self.key = dict['key']
+            if dict.has_key('key') and dict.has_key('cert'):
+                self.cert = dict['cert']
+                self.key = dict['key']
+            else:
+                dict['key'], dict['cert'] = getKeyCert()
         except:
             self.logger.exception('Service requires a host certificate and key')
             raise WMException('Service requires a host certificate and key', 
                               "WMCORE-11") 
-        
-    def refreshCache(self, cachefile, url):
-        
-        cachefile = "%s/%s" % (self.path, cachefile)
-        url = self.endpoint + url
-        
-        t = datetime.datetime.now() - datetime.timedelta(hours=self.cacheduration)
-        if not os.path.exists(cachefile) or os.path.getmtime(cachefile) < time.mktime(t.timetuple()):
-            self.logger.debug("%s expired, refreshing cache" % cachefile)
-            u = urllib.URLopener(cert_file=self.cert, key_file=self.key)
-            u.addheader('Accept', self.type)
-            u.retrieve(url, cachefile)
-        return open(cachefile, 'r')
     
-    def forceRefresh(self, cachefile, url):
-        
-        cachefile = "%s/%s" % (self.path, cachefile)
-        url = self.endpoint + url
-        
-        self.logger.debug("%s expired, refreshing cache" % cachefile)
-        u = urllib.URLopener(cert_file=self.cert, key_file=self.key)
-        u.addheader('Accept', self.type)
-        u.retrieve(url, cachefile)
-        return open(cachefile, 'r')        
-
+    def _getURLOpener(self):
+        """
+        method getting url opener, it is used by getData method
+        sub class can override this to have different URL opener
+        i.e. - if it needs authentication
+        """
+        return urllib.URLopener(cert_file=self.cert, key_file=self.key)

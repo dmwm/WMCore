bb0893e1b7630cc62456c0340c3db5eb4db28955
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:32:27 +0000

    Comment out the sendSRegFields - seems to be a typo/bit of noise
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6170 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/cms_sreg.py
+++ src/python/WMCore/WebTools/cms_sreg.py
@@ -3,12 +3,12 @@
 Extend the default sreg library to support permissions and DN for a user
 """
 
-__revision__ = "$Id: cms_sreg.py,v 1.4 2010/01/28 16:44:35 metson Exp $"
-__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: cms_sreg.py,v 1.5 2010/01/28 17:55:27 metson Exp $"
+__version__ = "$Revision: 1.5 $"
 
 from openid.extensions.sreg import SRegRequest
 from openid.extensions.sreg import SRegResponse
-from openid.extensions.sreg import sendSRegFields
+#from openid.extensions.sreg import sendSRegFields
 from openid.extensions.sreg import data_fields
 from openid.extensions.sreg import ns_uri
 from openid.extensions.sreg import supportsSReg
@@ -21,7 +21,7 @@ data_fields.update({'permissions': 'Permissions',
 __all__ = [
     'SRegRequest',
     'SRegResponse',
-    'sendSRegFields',
+#    'sendSRegFields',
     'data_fields',
     'ns_uri',
     'supportsSReg',

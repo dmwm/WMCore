c4fbda603cda75489d25b018402357dfc6238af1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:38:54 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4512 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMComponent/DBSBuffer/Database/MySQL/AlgoDatasetAssoc.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""
+_AlgoDatasetAssoc_
+
+MySQL implementation of DBSBuffer.AlgoDatasetAssoc
+"""
+
+__revision__ = "$Id: AlgoDatasetAssoc.py,v 1.1 2009/07/13 19:57:44 sfoulkes Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class AlgoDatasetAssoc(DBFormatter):
+    """
+    _AlgoDatasetAssoc_
+
+    Associtate an algorithm to a dataset and return the ID of the assocation.
+    """
+    sql = """INSERT INTO dbsbuffer_algo_dataset_assoc (algo_id, dataset_id)
+               SELECT (SELECT id FROM dbsbuffer_algo WHERE app_name = :app_name AND
+                         app_ver = :app_ver AND app_fam = :app_fam AND
+                         pset_hash = :pset_hash) AS algo_id,
+                      (SELECT id FROM dbsbuffer_dataset WHERE path = :path) AS dataset_id
+               FROM DUAL WHERE NOT EXISTS
+                 (SELECT * FROM dbsbuffer_algo_dataset_assoc WHERE algo_id =
+                   (SELECT id FROM dbsbuffer_algo WHERE app_name = :app_name AND
+                      app_ver = :app_ver AND app_fam = :app_fam AND
+                      pset_hash = :pset_hash) AND dataset_id =
+                   (SELECT id FROM dbsbuffer_dataset WHERE path = :path))"""
+
+    idsql = """SELECT id FROM dbsbuffer_algo_dataset_assoc WHERE algo_id =
+                 (SELECT id FROM dbsbuffer_algo WHERE app_name = :app_name AND
+                    app_ver = :app_ver AND app_fam = :app_fam AND
+                    pset_hash = :pset_hash) AND dataset_id =
+                 (SELECT id FROM dbsbuffer_dataset WHERE path = :path)"""
+
+    def execute(self, appName = None, appVer = None, appFam = None,
+                psetHash = None, datasetPath = None, conn = None,
+                transaction = False):
+        binds = {"app_name": appName, "app_ver": appVer, "app_fam": appFam,
+                 "pset_hash": psetHash, "path": datasetPath}
+
+        self.dbi.processData(self.sql, binds, conn = conn,
+                             transaction = transaction)
+
+        result = self.dbi.processData(self.idsql, binds, conn = conn,
+                                      transaction = transaction)
+        formattedResult = self.formatDict(result)
+        return formattedResult[0]["id"]
---
+++ src/python/WMComponent/DBSBuffer/Database/MySQL/Destroy.py
@@ -0,0 +1,33 @@
+#/usr/bin/env python
+"""
+_Destroy_
+
+Implementation of DBSBuffer.Destroy for SQLite
+"""
+
+__revision__ = "$Id: Destroy.py,v 1.1 2009/07/13 19:57:44 sfoulkes Exp $"
+__version__ = "$Revision: 1.1 $s"
+
+import threading
+
+from WMCore.Database.DBCreator import DBCreator
+
+class Destroy(DBCreator):
+    def __init__(self):
+        """
+        _init_
+
+        Call the base class's constructor and create all necessary tables,
+        constraints and inserts.
+        """
+        myThread = threading.currentThread()
+        DBCreator.__init__(self, myThread.logger, myThread.dbi)
+        
+        self.create["08dbsbuffer_dataset"] = "DROP TABLE dbsbuffer_dataset" 
+        self.create["07dbsbuffer_algo"] = "DROP TABLE dbsbuffer_algo"
+        self.create["06dbsbuffer_algo_dataset_assoc"] = "DROP TABLE dbsbuffer_algo_dataset_assoc"
+        self.create["05dbsbuffer_file"] = "DROP TABLE dbsbuffer_file"
+        self.create["04dbsbuffer_file_parent"] = "DROP TABLE dbsbuffer_file_parent"
+        self.create["03dbsbuffer_file_runlumi_map"] = "DROP TABLE dbsbuffer_file_runlumi_map"
+        self.create["02dbsbuffer_location"] = "DROP TABLE dbsbuffer_location"
+        self.create["01dbsbuffer_file_location"] = "DROP TABLE dbsbuffer_file_location"

9069ee1a6acca18f3b17d05fc5dbce23cd436874
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:21:14 +0000

    Upload files, not blocks.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5147 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/PhEDExInjector/PhEDExInjectorPoller.py
+++ src/python/WMComponent/PhEDExInjector/PhEDExInjectorPoller.py
@@ -2,18 +2,18 @@
 """
 _PhEDExInjectorPoller_
 
-Poll the DBSBuffer database and inject blocks as they are created.
+Poll the DBSBuffer database and inject files as they are created.
 """
 
-__revision__ = "$Id: PhEDExInjectorPoller.py,v 1.7 2009/09/18 18:53:03 sfoulkes Exp $"
-__version__ = "$Revision: 1.7 $"
+__revision__ = "$Id: PhEDExInjectorPoller.py,v 1.8 2009/09/24 20:29:06 sfoulkes Exp $"
+__version__ = "$Revision: 1.8 $"
 
 import threading
 import logging
 
 from WMCore.WorkerThreads.BaseWorkerThread import BaseWorkerThread
 
-from WMCore.Services.PhEDEx.PhEDEx import PhEDEx as phedexApi
+from WMCore.Services.PhEDEx.PhEDEx import PhEDEx
 from WMCore.Services.Requests import JSONRequests
 from WMCore.Services.Requests import SSLJSONRequests
 
@@ -23,7 +23,7 @@ class PhEDExInjectorPoller(BaseWorkerThread):
     """
     _PhEDExInjectorPoller_
 
-    Poll the DBSBuffer database and inject blocks as they are created.
+    Poll the DBSBuffer database and inject files as they are created.
     """
     def __init__(self, config):
         """
@@ -33,7 +33,7 @@ class PhEDExInjectorPoller(BaseWorkerThread):
         """
         BaseWorkerThread.__init__(self)
         self.config = config
-        self.phedex = phedexApi({"endpoint": config.PhEDExInjector.phedexurl}, "json")
+        self.phedex = PhEDEx({"endpoint": config.PhEDExInjector.phedexurl}, "json")
         self.dbsUrl = config.DBSUpload.dbsurl 
 
         # This will be used to map SE names which are stored in the DBSBuffer to
@@ -55,8 +55,12 @@ class PhEDExInjectorPoller(BaseWorkerThread):
                                 logger = self.logger,
                                 dbinterface = myThread.dbi)
 
-        self.getUninjected = daofactory(classname = "GetUninjectedBlocks")
-        self.setInjected = daofactory(classname = "SetBlocksInjected")
+        self.getUninjected = daofactory(classname = "GetUninjectedFiles")
+
+        daofactory = DAOFactory(package = "WMComponent.DBSBuffer.Database",
+                                logger = self.logger,
+                                dbinterface = myThread.dbi)   
+        self.setStatus = daofactory(classname = "DBSBufferFiles.SetStatus")
 
         (protocol, blank, host, path) = self.config.PhEDExInjector.phedexurl.split("/", 3)
         if protocol == "https:":
@@ -80,51 +84,51 @@ class PhEDExInjectorPoller(BaseWorkerThread):
         """
         _algorithm_
 
-        Poll the database for uninjected blocks and attempt to inject them into
+        Poll the database for uninjected files and attempt to inject them into
         PhEDEx.
         """
         myThread = threading.currentThread()
         myThread.transaction.begin()
 
-        uninjectedBlocks = self.getUninjected.execute()
+        uninjectedFiles = self.getUninjected.execute()
 
-        injectedBlocks = []
-        for uninjectedBlock in uninjectedBlocks:
+        injectedFiles = []
+        for siteName in uninjectedFiles.keys():
             # SE names can be stored in DBSBuffer as that is what is returned in
             # the framework job report.  We'll try to map the SE name to a
             # PhEDEx node name here. 
             location = None
 
-            if uninjectedBlock["location"] in self.nodeNames:
-                location = uninjectedBlock["location"]
+            if siteName in self.nodeNames:
+                location = siteName
             else:
                 if self.seMap.has_key("MSS"):
-                    if self.seMap["MSS"].has_key(uninjectedBlock["location"]):
-                        location = self.seMap["MSS"][uninjectedBlock["location"]]
+                    if self.seMap["MSS"].has_key(siteName):
+                        location = self.seMap["MSS"][siteName]
                 elif self.seMap.has_key("Disk"):
-                    if self.seMap["Disk"].has_key(uninjectedBlock["location"]):
-                        location = self.seMap["Disk"][uninjectedBlock["location"]]
+                    if self.seMap["Disk"].has_key(siteName):
+                        location = self.seMap["Disk"][siteName]
 
             if location == None:
                 logging.error("Could not map SE %s to PhEDEx node." % \
-                              uninjectedBlock["location"])
+                              siteName)
                 continue
 
-            logging.debug("Going to inject: %s, %s" % \
-                          (location, uninjectedBlock["blockname"]))
-            datasetPath = uninjectedBlock["blockname"].rsplit("#")[0]
-            injectRes = self.phedex.injectBlocks(self.dbsUrl, location, datasetPath,
-                                                 0, 0, uninjectedBlock["blockname"])
+            injectRes = self.phedex.injectBlocksFromDB(self.dbsUrl,
+                                                       uninjectedFiles[siteName],
+                                                       location, 1, 0)
 
             if not injectRes.has_key("error"):
-                injectedBlocks.append({"location": uninjectedBlock["location"],
-                                       "blockname": uninjectedBlock["blockname"]})
+                for datasetName in uninjectedFiles[siteName]:
+                    for blockName in uninjectedFiles[siteName][datasetName]:
+                        for file in uninjectedFiles[siteName][datasetName][blockName]["files"]:
+                            injectedFiles.append(file["lfn"])
             else:
-                logging.error("Error injecting block %s: %s" % \
-                              (uninjectBlock["blockname"], injectRes["error"]))
+                logging.error("Error injecting data %s: %s" % \
+                              (uninjectedFiles[siteName], injectRes["error"]))
 
-        if len(injectedBlocks) > 0:
-            self.setInjected.execute(injectedBlocks,
+        if len(injectedFiles) > 0:
+            self.setStatus.execute(injectedFiles, "InPhEDEx", 
                                      conn = myThread.transaction.conn,
                                      transaction = myThread.transaction)
 

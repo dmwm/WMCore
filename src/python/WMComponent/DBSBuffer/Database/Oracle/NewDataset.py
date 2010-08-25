dce9287d3b6fa9a65fcdef8a3b7ebb4ea041a96a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:40:17 +0000

    Use the base class's execute method.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4533 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSBuffer/Database/Oracle/NewDataset.py
+++ src/python/WMComponent/DBSBuffer/Database/Oracle/NewDataset.py
@@ -1,36 +1,19 @@
 #!/usr/bin/env python
 """
-_DBSBuffer.NewDataset_
+_NewDataset_
 
-Add a new dataset to DBS Buffer: Oracle version
-
+Oracle implementation of DBSBuffer.NewDataset
 """
-__revision__ = "$Id: NewDataset.py,v 1.1 2009/05/15 16:19:13 mnorman Exp $"
-__version__ = "$Revision: 1.1 $"
-__author__ = "mnorman@fnal.gov"
 
-import threading
-import exceptions
+__revision__ = "$Id: NewDataset.py,v 1.2 2009/07/14 19:15:46 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
+
 from WMComponent.DBSBuffer.Database.MySQL.NewDataset import NewDataset as MySQLNewDataset
 
 class NewDataset(MySQLNewDataset):
     """
-    _DBSBuffer.NewDataset_
-    
-    Add a new dataset to DBS Buffer: Oracle version
-    
+    _NewDataset_
+
+    Add a new dataset to DBS Buffer
     """
-
-    def execute(self, dataset=None, algoInDBS=0, conn=None, transaction = False):
-        binds = self.getBinds(dataset, algoInDBS)
-
-	try:
-            result = self.dbi.processData(self.sql, binds,
-                                          conn = conn, transaction = transaction)
-	except Exception, ex:
-            #Ignore duplicate rows
-            if ex.__str__().find("unique") != -1 :
-                return
-            else:
-                raise ex
-        return 
+    pass

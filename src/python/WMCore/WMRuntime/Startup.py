32ace2450f7c88e3744de7979fdc4c56e7a7caa7
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:57:26 +0000

    Add job runtime startup tool
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5672 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMRuntime/Startup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+_Startup_
+
+Runtime environment startup script
+
+"""
+import os
+import WMCore.WMRuntime.Bootstrap as Bootstrap
+
+
+if __name__ == '__main__':
+
+
+    job = Bootstrap.loadJobDefinition()
+    task = Bootstrap.loadTask(job)
+
+    task.build(os.getcwd())
+
+
+    task.execute(job)
+
+
+
+
+
+
+
+
+

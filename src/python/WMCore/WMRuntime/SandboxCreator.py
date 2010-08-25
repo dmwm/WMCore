0fce49069c497604e7c8e1450da1ceef497e6ffb
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:09:02 +0000

    Barfs on having None as first arg to tarfile.open, no clue why, committed workaround. Change name of parameter in WMTask that defines the url to pull the config from the ConfigCache
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4968 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMRuntime/SandboxCreator.py
+++ src/python/WMCore/WMRuntime/SandboxCreator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 """
     _SandboxCreator_
-    
+
     Given a path, workflow and task, create a sandbox within the path
 """
-__revision__ = "$Id: SandboxCreator.py,v 1.7 2009/06/17 16:15:51 meloam Exp $"
-__version__ = "$Revision: 1.7 $"
+__revision__ = "$Id: SandboxCreator.py,v 1.8 2009/09/01 19:41:45 evansde Exp $"
+__version__ = "$Revision: 1.8 $"
 import os
 import re
 import tarfile
@@ -15,45 +15,45 @@ import urllib
 import WMCore
 
 class SandboxCreator:
-    
+
     def __init__(self):
         self.packageWMCore = True
-    
+
     def disableWMCorePackaging(self):
-        """ 
+        """
             __disableWMCorePackaging__
-            
+
             use to keep the sandboxer from adding WMCore/* to the sandbox.
             testing would take forever otherwise
         """
         self.packageWMCore = False
-    
+
     def extractSandbox(self, archivePath, targetPath):
         """
             __extractSandbox__
-            
+
             extracts a sandbox at the given archivePath to the given targetPath
         """
         os.makedirs(targetPath)
         archive = tarfile.TarFile(archivePath)
         archive.extractall(targetPath)
         archive.close()
-        
-    
+
+
     def makeSandbox(self, buildItHere, workload, task):
         """
             __makeSandbox__
-            
+
             MakeSandbox creates and archives a sandbox in buildItHere,
-            returning the path to the archive and putting it in the 
+            returning the path to the archive and putting it in the
             task
         """
-        # generate the real path and make it 
+        # generate the real path and make it
         workloadName = workload.name()
         taskName     = task.name()
         path = "%s/%s/%s/WMSandbox" % (buildItHere, workloadName, taskName)
         os.makedirs( path )
-        
+
         # for each step in the task, make a directory
         for t in task.steps().nodeIterator():
             t = WMStep.WMStepHelper(t)
@@ -62,15 +62,16 @@ class SandboxCreator:
             initHandle = open(stepPath + "/__init__.py", 'w')
             initHandle.write("# dummy file for now")
             initHandle.close()
-            
+
             # the CMSSW has a special case with its ConfigCache argument
-            if (hasattr(t.data.application.configuration,'configCacheUrl')):
-                fileTarget = "%s/%s" % (stepPath,
-                                    t.data.application.command.configuration)
+            if (hasattr(t.data.application.configuration,'retrieveConfigUrl')):
+                fileTarget = "%s/%s" % (
+                    stepPath,
+                    t.data.application.command.configuration)
                 urllib.urlretrieve(
-                                t.data.application.configuration.configCacheUrl
-                               ,fileTarget)
-            
+                    t.data.application.configuration.retrieveConfigUrl,
+                    fileTarget)
+
             # within the step, the sandbox section has files to be imported
             # TODO: do we need a function to wrap this data call?
             for fileInfo in t.data.sandbox:
@@ -81,40 +82,43 @@ class SandboxCreator:
                     fileSuffix = match.group(1)
                 else:
                     fileSuffix = "sandboxFile.dat"
-                
+
                 fileTarget = "%s/%s" % (stepPath, fileSuffix)
                 urllib.urlretrieve(fileInfo.src, fileTarget)
                 fileInfo.injob = fileTarget
-                
+
         # and generate the __init__.py
         # TODO: find out what else should go in this init. I don't understand
-        #   python's module system all the way yet        
+        #   python's module system all the way yet
         initHandle = open(path + "/__init__.py", 'w')
         initHandle.write("# dummy file for now")
         initHandle.close()
-        
+
         # pickle up the workload for storage in the sandbox
         workload.save(path + "/WMWorkload.pcl")
-        
+
         # now, tar everything up and put it somewhere special
         (archiveHandle,archivePath) = tempfile.mkstemp('.tar.bz2','sbox',
                                                       buildItHere)
+
         task.data.sandboxArchivePath = archivePath
         pythonHandle = os.fdopen(archiveHandle,'w+b')
-        archive = tarfile.open(None,'w:bz2', pythonHandle)
+
+        #TODO: Tweak so the name of the sandbox is a bit more sensible
+        archive = tarfile.open(archivePath,'w:bz2', pythonHandle)
         archive.add("%s/%s/%s/" % (buildItHere, workloadName, taskName),'/')
         if (self.packageWMCore):
             # package up the WMCore distribution
             # hopefully messing with this magic isn't a recipie for disaster
             wmcorePath = WMCore.__path__[0]
             archive.add(wmcorePath, '/WMCore/')
-            
+
         archive.close()
         pythonHandle.close()
 
-        
-        
+
+
         return archivePath
-        
-        
-        
+
+
+

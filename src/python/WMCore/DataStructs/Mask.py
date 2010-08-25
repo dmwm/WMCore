6fbad667cb82e1e543d4ca87a4d491cfb1698805
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:23:21 +0000

    Added functions for Lumi and Run based skip and max. Also added generic function for finding the max in each mask.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4276 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/DataStructs/Mask.py
+++ src/python/WMCore/DataStructs/Mask.py
@@ -10,8 +10,8 @@ job in two ways:
 
 """
 __author__ = "evansde@fnal.gov"
-__revision__ = "$Id: Mask.py,v 1.2 2008/09/20 12:50:16 evansde Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: Mask.py,v 1.3 2009/06/05 16:36:22 mnorman Exp $"
+__version__ = "$Revision: 1.3 $"
 
 
 
@@ -45,6 +45,33 @@ class Mask(dict):
         self['LastEvent']  = skipEvents + maxEvents
         return
 
+    def setMaxAndSkipLumis(self, maxLumis, skipLumi):
+        """
+        _setMaxAndSkipLumis
+
+        Set the Maximum number of lumi sections and the starting point
+
+        """
+
+        self['FirstLumi'] = skipLumi
+        self['LastLumi']  = skipLumi + maxLumis
+
+        return
+
+
+    def setMaxAndSkipRuns(self, maxRuns, skipRun):
+        """
+        _setMaxAndSkipRuns
+
+        Set the Maximum number of runss and the starting point
+
+        """
+
+        self['FirstRun'] = skipRun
+        self['LastRun']  = skipRun + maxRuns
+
+        return
+
     def getMaxEvents(self):
         """
         _getMaxEvents_
@@ -55,6 +82,21 @@ class Mask(dict):
         if (self['LastEvent'] == None) or (self['FirstEvent'] == None):
             return None
         return self['LastEvent'] - self['FirstEvent']
+
+
+    def getMax(self, type = None):
+        """
+        _getMax_
+
+        returns the maximum number of runs/events/etc of the type of the type string
+
+        """
+
+        if not self.has_key('First%s' %(type)):
+            return None
+        if (self['First%s'%(type)] == None) or (self['Last%s'%(type)] == None):
+            return None
+        return self['Last%s'%(type)] - self['First%s'%(type)]
 
 
 

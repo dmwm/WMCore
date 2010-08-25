50d8d6815bbdf6db932aea8451a36f197840d5cb
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:27:23 +0000

    Fixed to work with Oracle
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4337 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/MsgService/Oracle/Destroy.py
+++ src/python/WMCore/MsgService/Oracle/Destroy.py
@@ -2,35 +2,45 @@
 #pylint: disable-msg=E1103
 
 """
-_Create_
+_Destroy_
 
-Class for creating MySQL specific schema for the trigger
+Class for destroying Oracle specific schema for the trigger
 
 """
 
-__revision__ = "$Id: Destroy.py,v 1.1 2008/10/02 11:33:03 fvlingen Exp $"
-__version__ = "$Revision: 1.1 $"
-__author__ = "fvlingen@caltech.edu"
+__revision__ = "$Id: Destroy.py,v 1.2 2009/06/16 14:46:17 mnorman Exp $"
+__version__ = "$Revision: 1.2 $"
+__author__ = "mnorman@fnal.gov"
 
 import threading
 
 from WMCore.Database.DBCreator import DBCreator
+from WMCore.MsgService.MySQL.Destroy import Destroy as MySQLDestroy
+from WMCore.MsgService.Oracle.Create import Create
 
-class Destroy(DBCreator):
+class Destroy(MySQLDestroy):
     """
     _Create_
     
-    Class for creating MySQL specific schema for the trigger.
+    Class for destroying Oracle specific schema for the trigger.
     """
     
     
     
-    def __init__(self):
+    def __init__(self, logger = None, dbi = None):
         myThread = threading.currentThread()
-        DBCreator.__init__(self, myThread.logger, myThread.dbi)
-        self.create = {}
-        self.constraints = {}
-        self.create['z_ms_process'] = "DROP TABLE ms_process"
-        self.create['y_ms_process_seq'] = "DROP SEQUENCE MS_PROCESS_SEQ1"
-        self.create['x_ms_type'] = "DROP TABLE ms_type"
-        self.create['w_ms_type_seq'] = "DROP SEQUENCE MS_TYPE_SEQ1"
+        MySQLDestroy.__init__(self, logger, dbi)
+
+
+
+
+        #self.create['z_ms_process'] = "DROP TABLE ms_process"
+        #self.create['y_ms_process_seq'] = "DROP SEQUENCE MS_PROCESS_SEQ1"
+        #self.create['x_ms_type'] = "DROP TABLE ms_type"
+        #self.create['w_ms_type_seq'] = "DROP SEQUENCE MS_TYPE_SEQ1"
+
+        j = 50
+        for i in Create.sequence_tables:
+            seqname = i
+            self.create["%s%s" % (j, seqname)] = \
+                           "DROP SEQUENCE %s"  % seqname

ff77211e488fb5596e83562c1e523d8aea888fbb
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:06:55 +0000

    __init__ commit
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7468 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMRuntime/Tools/Plugins/ApMonLite/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+_ApMonLite_
+
+Lighter more API friendly way to send data to ApMon
+
+"""
+__revision__ = "$Id: __init__.py,v 1.1 2010/05/17 20:41:07 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+__all__ = []
---
+++ src/python/WMCore/WMRuntime/Tools/Plugins/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+_Tools.Plugins_
+
+Plugins used alongside runtime tools
+"""
+__all__ = []
+__revision__ = "$Id: __init__.py,v 1.1 2010/05/17 20:41:05 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+
+
+

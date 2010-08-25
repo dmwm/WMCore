3095656c9dbeba1f79eac4a4e53cae7658cf06a2
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:24:54 +0000

    misunderstood the problem in previous commit     # duplicate insert can happen when two different workspec using the same block
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6896 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WorkQueue/Database/Oracle/Site/UpdateDataSiteMapping.py
+++ src/python/WMCore/WorkQueue/Database/Oracle/Site/UpdateDataSiteMapping.py
@@ -3,14 +3,22 @@ SQLite implementation of site.UpdateDataSiteMapping
 """
 
 __all__ = []
-__revision__ = "$Id: UpdateDataSiteMapping.py,v 1.2 2010/03/29 21:12:14 sryu Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: UpdateDataSiteMapping.py,v 1.3 2010/03/30 15:42:40 sryu Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.WorkQueue.Database.MySQL.Site.UpdateDataSiteMapping \
     import UpdateDataSiteMapping as UpdateDataSiteMappingMySQL
 
 class UpdateDataSiteMapping(UpdateDataSiteMappingMySQL):
+    #TODO need to improve the query
+    # duplicate insert can happen when two different workspec using the same block
     insertSQL = """INSERT INTO wq_data_site_assoc (data_id, site_id)
-                        VALUES(
+                        SELECT 
                             (SELECT id FROM wq_data WHERE name = :data),
-                            (SELECT id from wq_site WHERE name = :site))"""
+                            (SELECT id from wq_site WHERE name = :site)
+                        FROM DUAL 
+                        WHERE NOT EXISTS
+                            (SELECT * FROM wq_data_site_assoc
+                             WHERE data_id = (SELECT id FROM wq_data WHERE name = :data)
+                              AND
+                                site_id = (SELECT id from wq_site WHERE name = :site))"""

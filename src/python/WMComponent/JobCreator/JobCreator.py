f0989fa45e244c76cdc463a23b46b517327256f1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:52:48 +0000

    Did some tightening of code
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6458 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/JobCreator/JobCreator.py
+++ src/python/WMComponent/JobCreator/JobCreator.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#pylint: disable-msg=W0613, W6501
 
 """
 Creates jobs for new subscriptions
@@ -6,19 +7,14 @@ Handler implementation for polling
 
 """
 
-__revision__ = "$Id: JobCreator.py,v 1.1 2009/07/09 22:12:14 mnorman Exp $"
-__version__ = "$Revision: 1.1 $"
-__author__ = "anzar@fnal.gov"
+__revision__ = "$Id: JobCreator.py,v 1.2 2010/02/12 15:41:48 mnorman Exp $"
+__version__ = "$Revision: 1.2 $"
 
 import logging
 import threading
-import os
 
 # harness class that encapsulates the basic component logic.
 from WMCore.Agent.Harness import Harness
-# we do not import failure handlers as they are dynamicly 
-# loaded from the config file.
-from WMCore.WMFactory import WMFactory
 
 from WMComponent.JobCreator.JobCreatorPoller import JobCreatorPoller
 
@@ -26,6 +22,11 @@ from WMComponent.JobCreator.JobCreatorPoller import JobCreatorPoller
 
 
 class JobCreator(Harness):
+    """
+    Creates jobs for new subscriptions
+    Handler implementation for polling
+    
+    """
 
     def __init__(self, config):
         # call the base class
@@ -35,20 +36,22 @@ class JobCreator(Harness):
         #myThread = threading.currentThread()
         #myThread.database = os.getenv("DATABASE")
         
-	print "JobCreator.__init__"
+        print "JobCreator.__init__"
 
     def preInitialization(self):
-	print "JobCreator.preInitialization"
+        """
+        Step that actually adds the worker thread properly
 
-        # use a factory to dynamically load handlers.
-        factory = WMFactory('generic')
-        
+        """
+        print "JobCreator.preInitialization"
+
 
         # Add event loop to worker manager
         myThread = threading.currentThread()
         
         pollInterval = self.config.JobCreator.pollInterval
         logging.info("Setting poll interval to %s seconds" % pollInterval)
-        myThread.workerThreadManager.addWorker(JobCreatorPoller(self.config), pollInterval)
+        myThread.workerThreadManager.addWorker(JobCreatorPoller(self.config), \
+                                               pollInterval)
 
         return

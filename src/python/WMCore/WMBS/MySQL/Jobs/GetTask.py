3edd08843c6bfa73a66a2a6cdeb229e299afc134
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:40:58 +0000

    DAO object for grabbing the task from a given job by going up the chain through jobgroup/subscription/workflow
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5436 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/MySQL/Jobs/GetTask.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+"""
+_GetTask_
+
+MySQL implementation of Jobs.Task
+"""
+
+__all__ = []
+__revision__ = "$Id: GetTask.py,v 1.1 2009/10/23 19:24:06 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+import logging
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class GetTask(DBFormatter):
+
+    sql = """SELECT wmwf.task AS task, wmj.id AS jobid FROM wmbs_workflow wmwf
+               INNER JOIN wmbs_subscription wms ON wmwf.id = wms.workflow
+               INNER JOIN wmbs_jobgroup wmjg ON wms.id = wmjg.subscription
+               INNER JOIN wmbs_job wmj ON wmjg.id = wmj.jobgroup
+               WHERE wmj.id = :jobid
+               """
+
+
+    def format(self, results):
+        """
+        Should create one dictionary of the form {'jobid':'taskName'}
+
+        """
+        dictionary = self.formatDict(results)
+
+        final = {}
+
+        for entry in dictionary:
+            final[entry['jobid']] = entry['task']
+
+        return final
+
+
+    def execute(self, jobID, conn = None, transaction = False):
+        """
+        Should handle bulk and regular attempts to find the task
+        """
+
+
+        if type(jobID) == list:
+            if len(jobID) == 0:
+                return {}
+            binds = []
+            for id in jobID:
+                binds.append({'jobid': int(id)})
+        elif type(jobID) == int:
+            binds = {'jobid': jobID}
+        else:
+            logging.error('Incompatible jobid in GetTask')
+            return
+
+        results = self.dbi.processData(self.sql, binds, conn = conn, transaction = transaction)
+
+        return self.format(results)
---
+++ src/python/WMCore/WMBS/SQLite/Jobs/GetTask.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+_GetTask_
+
+SQLite implementation of Jobs.Task
+"""
+
+__all__ = []
+__revision__ = "$Id: GetTask.py,v 1.1 2009/10/23 19:24:07 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+import logging
+
+from WMCore.WMBS.MySQL.Jobs.GetTask import GetTask as MySQLGetTask
+
+class GetTask(MySQLGetTask):
+    """
+    Identical to MySQL version
+    """

9c7f3304346f2578d1a4663f4a9be1381315698f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 05:27:29 +0000

    DAO object for setting bulk LFN based parentage
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8519 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/SQLite/Files/SetParentage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+SQLite implementation of File.SetParentage
+
+Make the parentage link between two file lfns in bulk
+"""
+__all__ = []
+__revision__ = "$Id: SetParentage.py,v 1.1 2010/08/13 16:41:57 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.WMBS.MySQL.Files.SetParentage import SetParentage as MySQLSetParentage
+
+class SetParentage(MySQLSetParentage):
+    """
+    _SetParentage_
+
+
+    Identical to MySQL version
+    """

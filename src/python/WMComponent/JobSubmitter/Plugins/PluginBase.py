1bfaf27e36a1cb3e01cc7bba2d730fcc54b95973
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:55:02 +0000

    Base changes to create changeState features in all plugin classes
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8102 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/JobSubmitter/Plugins/PluginBase.py
+++ src/python/WMComponent/JobSubmitter/Plugins/PluginBase.py
@@ -12,6 +12,9 @@ import threading
 
 from WMCore.WMBS.Job import Job
 
+from WMCore.Agent.Configuration           import Configuration
+from WMCore.JobStateMachine.ChangeState   import ChangeState
+
 class PluginBase(object):
     """
     _BasicPlugin: Submitter_
@@ -20,9 +23,46 @@ class PluginBase(object):
     """
 
     def __init__(self, config):
+
+
+
+        csConfig = Configuration()
+        csConfig.section_("JobStateMachine")
+        csConfig.JobStateMachine.couchurl      = config["couchURL"]
+        csConfig.JobStateMachine.couch_retries = config["defaultRetries"]
+        csConfig.JobStateMachine.couchDBName   = config["couchDBName"]
+
+
+        self.changeState = ChangeState(csConfig)
+        
         self.config = config
 
     def submitJobs(self, jobList, localConfig):
         """
         Overwrite this function with something that works
         """
+
+
+    def passJobs(self, jobList):
+        """
+        _passJobs_
+        
+        Run the jobs through ChangeState
+        """
+
+        self.changeState.propagate(jobList, 'executing',    'created')
+
+        return
+
+
+    def failJobs(self, jobList):
+        """
+        _failJobs_
+
+        Fail the jobs that failed in the submitter
+        """
+
+        self.changeState.propagate(jobList, 'submitfailed', 'created')
+
+        return
+    

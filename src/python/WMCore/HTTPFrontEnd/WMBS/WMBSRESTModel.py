3bc310482b048960444308677db637fd13f20d0f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:22:32 +0000

    update according to the interface change in RESTModel
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5166 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/HTTPFrontEnd/WMBS/WMBSRESTModel.py
+++ src/python/WMCore/HTTPFrontEnd/WMBS/WMBSRESTModel.py
@@ -22,32 +22,27 @@ class WMBSRESTModel(RESTModel):
         self.methods = {'GET':{}}
         self.addDAO('GET', 'jobs', 'Monitoring.JobsByState')
         self.addDAO('GET', 'jobcount', 'Monitoring.JobCountByState')
-        self.addDAO('GET', 'jobsbysubs', 'Monitoring.JobsBySubscription')
-        self.addDAO('GET', 'jobcountbysubs', 'Monitoring.JobCountBySubscriptionAndRun')
+        self.addDAO('GET', 'jobsbysubs', 'Monitoring.JobsBySubscription', ['subscription'])
+        self.addDAO('GET', 'jobcountbysubs', 'Monitoring.JobCountBySubscriptionAndRun', ['subscription', 'run'])
         
-        
-        
-    def _sanitise(self, verb, args, kwargs):
-        """
-        don't override this in subclass
-        Take the arguments and ignore everything apart from the first value in 
-        args - this is the method name. We can then use this to pull out 
-        configuration from self.methods.
-        
-        I'm not sure how secure this is. There is presumably a way that you 
-        could inject a different dao into the methods dictionary, but I don't 
-        know how you would do that without compromising the machine running the 
-        server, and even then I don't know how you'd do it without changing code
-        and disrupting the service.
-        """
-        dao = self.daofactory(classname=self.methods[verb][args[0]]['dao'])
-        return dao.execute(**kwargs)
-        
-    def addDAO(self, verb, methodKey, dao, version=1):
+    def addDAO(self, verb, methodKey, daoStr, args=[], validation=[], version=1):
         """
         add dao (or any other method handler) in self.methods
         self.method need to be initialize if sub class doesn't want to take provide by
         """
-        self.methods[verb][methodKey] = {'dao': dao,
-                                         'call': self._sanitise,
+        
+        
+        dao = self.daofactory(classname=daoStr) 
+                
+        self.methods[verb][methodKey] = {'args': args,
+                                         'call': dao.execute,
+                                         'validation': [],
                                          'version': version}
+        
+
+        
+        
+        
+        
+        
+        

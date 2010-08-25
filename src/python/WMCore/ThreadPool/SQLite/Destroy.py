25158a6c716e581b8be3e9e2ae331c6c762910e3
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:44:31 +0000

    Add the enum table to teardown
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4596 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/ThreadPool/SQLite/Destroy.py
+++ src/python/WMCore/ThreadPool/SQLite/Destroy.py
@@ -2,17 +2,22 @@
 """
 _ChangeState_
 
-Sqlite implementation of Job.ChangeState
+Sqlite implementation of Destroy for ThreadPool
 """
 
 __all__ = []
-__revision__ = "$Id: Destroy.py,v 1.2 2009/07/15 21:47:13 meloam Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: Destroy.py,v 1.3 2009/07/20 17:39:11 mnorman Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 from WMCore.ThreadPool.MySQL.Destroy import Destroy as BaseDAO
 
 class Destroy(BaseDAO):
-    pass
+
+    def __init__(self):
+
+        BaseDAO.__init__(self)
+
+        self.create["04tp_queued_process"] = "DROP TABLE tp_queued_process_enum"
 
 

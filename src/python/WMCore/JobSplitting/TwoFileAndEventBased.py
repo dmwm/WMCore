2f8a9370903f1e32cb2c724781e6698d3efe0ab4
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:06:22 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4019 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/JobSplitting/TwoFileAndEventBased.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+"""
+_TwoFileAndEventBased_
+
+"""
+
+__revision__ = "$Id: TwoFileAndEventBased.py,v 1.1 2009/04/09 16:40:20 sfoulkes Exp $"
+__version__  = "$Revision: 1.1 $"
+
+from sets import Set
+
+from WMCore.JobSplitting.JobFactory import JobFactory
+from WMCore.Services.UUID import makeUUID
+
+class TwoFileAndEventBased(JobFactory):
+    """
+    Split jobs by number of events
+    """
+    def algorithm(self, groupInstance = None, jobInstance = None, *args,
+                  **kwargs):
+        """
+        _algorithm_
+
+        An event base splitting algorithm.  All available files are split into a
+        set number of events per job.  
+        """
+        jobGroups = []
+        fileset = self.subscription.availableFiles()
+
+        #  //
+        # // get the event total
+        #//
+        eventsPerJob = kwargs.get("events_per_job", 5000)
+        try:
+            selectionAlgorithm = kwargs['selection_algorithm']
+        except KeyError, e:
+            selectionAlgorithm = None
+        carryOver = 0
+
+        for f in fileset:
+            if selectionAlgorithm:
+                if not selectionAlgorithm( f ):
+                    self.subscription.completeFiles( [ f ] )
+                    continue
+            f.loadData(parentage = 1)
+            jobGroup = groupInstance(subscription = self.subscription)
+            jobGroups.append(jobGroup)
+            eventsInFile = f['events']
+
+            if eventsInFile == 0:
+                currentJob = jobInstance(name = makeUUID())
+                currentJob.addFile(f)
+                jobGroup.add(currentJob)
+                jobGroup.commit()
+                jobGroup.recordAcquire()                
+                continue
+
+            currentEvent = 0
+            while currentEvent < eventsInFile:
+                currentJob = jobInstance(name = makeUUID())
+                currentJob.addFile(f)
+                currentJob.mask.setMaxAndSkipEvents(eventsPerJob, currentEvent)
+                jobGroup.add(currentJob)
+                currentEvent += eventsPerJob
+
+            jobGroup.commit()
+            jobGroup.recordAcquire()
+
+        return jobGroups

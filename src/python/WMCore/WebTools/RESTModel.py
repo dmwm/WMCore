84f404f535b35ab2f3197723cf587fb1dda2be83
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:15:15 +0000

    Give handle methods appropriate default input
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4154 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/RESTModel.py
+++ src/python/WMCore/WebTools/RESTModel.py
@@ -5,41 +5,41 @@ Rest Model abstract implementation
 """
 
 __author__ = "Valentin Kuznetsov <vkuznet at gmail dot com>"
-__revision__ = "$Id: RESTModel.py,v 1.4 2009/03/25 13:34:32 metson Exp $"
-__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: RESTModel.py,v 1.5 2009/05/12 11:28:22 metson Exp $"
+__version__ = "$Revision: 1.5 $"
 
 from WMCore.WebTools.WebAPI import WebAPI
 
 class RESTModel(WebAPI):
    """Rest model class implementation"""
-   def handle_get(self, args, kwargs):
+   def handle_get(self, args=[], kwargs={}):
        """Example of handle_get implementation"""
-       data = {"message": "Unsupported verb method: GET", 
-               "args": args, 
+       data = {"message": "Unsupported verb method: GET",
+               "args": args,
                "kwargs": kwargs}
        self.debug(str(data))
        return data
 
-   def handle_post(self, args, kwargs):
+   def handle_post(self, args=[], kwargs={}):
        """Example of handle_post implementation"""
-       data = {"message": "Unsupported verb method: POST", 
-               "args": args, 
+       data = {"message": "Unsupported verb method: POST",
+               "args": args,
                "kwargs": kwargs}
        self.debug(str(data))
        return data
 
-   def handle_put(self, args, kwargs):
+   def handle_put(self, args=[], kwargs={}):
        """Example of handle_put implementation"""
-       data = {"message": "Unsupported verb method: PUT", 
-               "args": args, 
+       data = {"message": "Unsupported verb method: PUT",
+               "args": args,
                "kwargs": kwargs}
        self.debug(str(data))
        return data
 
-   def handle_delete(self, args, kwargs):
+   def handle_delete(self, args=[], kwargs={}):
        """Example of handle_delete implementation"""
-       data = {"message": "Unsupported verb method: DELETE", 
-               "args": args, 
+       data = {"message": "Unsupported verb method: DELETE",
+               "args": args,
                "kwargs": kwargs}
        self.debug(str(data))
        return data

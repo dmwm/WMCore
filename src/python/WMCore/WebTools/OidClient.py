0df14cff06f627e9e84d12e450231d233a4ee7d7
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:43:41 +0000

    documented open and added arguments to allow POSTs and set timeouts
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7152 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/OidClient.py
+++ src/python/WMCore/WebTools/OidClient.py
@@ -9,15 +9,29 @@ class OidClient():
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
         #urllib2.install_opener(opener)
 
-    def open(self, url):
-        resp = self.opener.open(url)
+    def open(self, url, data=None, timeout=3):
+        """
+        Opens a url (which can be a urllib2.Request object or a string)
+
+        data is a string specifying additional data to send to the server
+        If provided, the HTTP request will be a POST (instead of GET).
+        It should be a buffer in the standard application/x-www-form-urlencoded
+        format (use urllib.urlencode() for that)
+
+        timeout in seconds for blocking operations like the connection attempt
+        Defaults to 3 seconds. Note that an openid request 
+        may require several step (thus, several timeouts).
+
+        returns an file-like object (same as urllib2.urlopen())
+        """
+        resp = self.opener.open(url,data,timeout)
         if resp.geturl().startswith(self.oidserver+'/openidserver'):
             # Means that the oidserver responded with the login form
             login_data = urllib.urlencode({'identifier' : self.username, 'password' : self.password, 'submit' : 'login'})
-            self.opener.open(self.oidserver+'/loginsubmit', login_data)
+            self.opener.open(self.oidserver+'/loginsubmit', login_data, timeout)
 
             # Continues from the point it was...
-            resp = self.opener.open(resp.geturl())
+            resp = self.opener.open(resp.geturl(),data,timeout)
         return resp
 
 if __name__ == "__main__":

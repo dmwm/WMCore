de3758c9ad1289f6890040269b0d1ba3b2966e28
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:03:18 +0000

    use MySql format
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3970 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Files/GetChildIDsByID.py
+++ src/python/WMCore/WMBS/Oracle/Files/GetChildIDsByID.py
@@ -8,11 +8,3 @@ from WMCore.WMBS.MySQL.Files.GetChildIDsByID import GetChildIDsByID \
 
 class GetChildIDsByID(GetChildIDsMySQL):
     sql = GetChildIDsMySQL.sql
-    
-    def format(self, result):        
-        out = Set() 
-        for r in result:
-            for f in r.fetchall():
-                out.add(f[0])
-            r.close()
-        return list(out)
--- src/python/WMCore/WMBS/Oracle/Files/GetParentIDsByID.py
+++ src/python/WMCore/WMBS/Oracle/Files/GetParentIDsByID.py
@@ -8,11 +8,3 @@ from WMCore.WMBS.MySQL.Files.GetParentIDsByID import GetParentIDsByID \
 class GetParentIDsByID(GetParentIDsMySQL):
     sql = GetParentIDsMySQL.sql
     
-    def format(self, result):        
-        out = Set() 
-        for r in result:
-            for f in r.fetchall():
-                out.add(f[0])
-            r.close()
-        return list(out) 
-    

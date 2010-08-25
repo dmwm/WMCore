849bfa599351c13fca7bd680ca19e24ff8563b22
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:02:33 +0000

    return id as well
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7408 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WorkQueue/Database/MySQL/WMSpec/GetWMSpecInfo.py
+++ src/python/WMCore/WorkQueue/Database/MySQL/WMSpec/GetWMSpecInfo.py
@@ -4,13 +4,13 @@ _GetWMSpecInfo_
 MySQL implementation of WMSpec.GetWMSpecInfo
 """
 __all__ = []
-__revision__ = "$Id: GetWMSpecInfo.py,v 1.2 2010/02/08 19:05:45 sryu Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: GetWMSpecInfo.py,v 1.3 2010/05/12 19:13:55 sryu Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class GetWMSpecInfo(DBFormatter):
-    sql = """SELECT ws.name as wmspec_name, ws.url, ws.owner, 
+    sql = """SELECT ws.id, ws.name as wmspec_name, ws.url, ws.owner, 
                     wt.name as wmtask_name, wt.type as wmtask_type,
                     wt.dbs_url 
              FROM wq_wmspec ws 

9ce059928f25d5e812fe60262c997890d65d120b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:12:37 +0000

    Added cache_dir
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5020 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/Save.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/Save.py
@@ -6,18 +6,19 @@ MySQL implementation of Jobs.Save
 """
 
 __all__ = []
-__revision__ = "$Id: Save.py,v 1.7 2009/08/27 16:49:40 sfoulkes Exp $"
-__version__ = "$Revision: 1.7 $"
+__revision__ = "$Id: Save.py,v 1.8 2009/09/09 21:22:00 mnorman Exp $"
+__version__ = "$Revision: 1.8 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class Save(DBFormatter):
     sql = """UPDATE wmbs_job SET jobgroup = :jobgroup, name = :name, 
-               couch_record = :couch_record, outcome = :outcome, location = 
+               couch_record = :couch_record, outcome = :outcome, cache_dir = :cache_dir,
+               location = 
                  (SELECT id FROM wmbs_location WHERE site_name = :location)
              WHERE id = :jobid"""
     
-    def execute(self, jobid, jobgroup, name, couch_record, location, outcome, 
+    def execute(self, jobid, jobgroup, name, couch_record, location, outcome, cache_dir,  
                 conn = None, transaction = False):
         if outcome == 'success':
             boolOutcome = 1
@@ -26,7 +27,7 @@ class Save(DBFormatter):
         
         binds = {"jobid": jobid, "jobgroup": jobgroup, "name": name, 
                  "couch_record": couch_record, "location": location, 
-                 "outcome": boolOutcome}
+                 "outcome": boolOutcome, "cache_dir": cache_dir}
 
         self.dbi.processData(self.sql, binds, conn = conn,
                              transaction = transaction)

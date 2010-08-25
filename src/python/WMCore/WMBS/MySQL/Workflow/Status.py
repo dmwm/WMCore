345403203e21f3c849f4f2b6e0f6767772ba224b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:18:45 +0000

    Make this work for workflows that have not produced jobs.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7627 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Workflow/Status.py
+++ src/python/WMCore/WMBS/MySQL/Workflow/Status.py
@@ -5,8 +5,8 @@ _Status_
 MySQL implementation of Workflow.Status
 """
 
-__revision__ = "$Id: Status.py,v 1.1 2010/05/26 21:04:20 sfoulkes Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Status.py,v 1.2 2010/05/26 21:55:13 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -19,11 +19,11 @@ class Status(DBFormatter):
                     COUNT(wmbs_job.id) AS jobs, SUM(wmbs_job.outcome) AS success FROM wmbs_workflow
                INNER JOIN wmbs_subscription ON
                  wmbs_workflow.id = wmbs_subscription.workflow
-               INNER JOIN wmbs_jobgroup ON
+               LEFT OUTER JOIN wmbs_jobgroup ON
                  wmbs_subscription.id = wmbs_jobgroup.subscription
-               INNER JOIN wmbs_job ON
+               LEFT OUTER JOIN wmbs_job ON
                  wmbs_jobgroup.id = wmbs_job.jobgroup
-               INNER JOIN wmbs_job_state ON
+               LEFT OUTER JOIN wmbs_job_state ON
                  wmbs_job.state = wmbs_job_state.id
                GROUP BY wmbs_workflow.owner, wmbs_workflow.task, wmbs_job_state.name"""
         

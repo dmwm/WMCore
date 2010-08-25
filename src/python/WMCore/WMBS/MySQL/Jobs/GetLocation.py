cb9ea3fb6c811dff8245050ea99086f2acaa2499
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:24:53 +0000

    This is what happens when you write a SQL query and then never use it
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6061 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/GetLocation.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/GetLocation.py
@@ -6,8 +6,8 @@ MySQL implementation of Jobs.Location
 """
 
 __all__ = []
-__revision__ = "$Id: GetLocation.py,v 1.1 2009/10/02 21:25:22 mnorman Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: GetLocation.py,v 1.2 2010/01/22 18:00:57 mnorman Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -18,36 +18,38 @@ class GetLocation(DBFormatter):
     Retrieve all files that are associated with the given job from the
     database.
     """
-    sql = "SELECT site_name FROM wmbs_location WHERE id = (SELECT location FROM wmbs_job WHERE JOB = :jobid)"
-    bulkSQL = """SELECT wmbs_location.site_name as site_name, wmbs_job.name as name
+    sql = "SELECT site_name FROM wmbs_location WHERE id = (SELECT location FROM wmbs_job WHERE id = :jobid)"
+    bulkSQL = """SELECT wmbs_location.site_name as site_name, wmbs_job.id as id
                  FROM wmbs_location
                  INNER JOIN wmbs_job
                  ON wmbs_location.id = wmbs_job.location
                  WHERE wmbs_job.id = :jobid"""
 
-    def format(self, results):
-        """
-        _formatDict_
-
-        """
-
-        if len(results) == 0:
-            return False
-        else:
-            return results[0][0]
+    #def format(self, results):
+    #    """
+    #    _formatDict_
+    #
+    #    """
+    #
+    #    if len(results) == 0:
+    #        return False
+    #    else:
+    #        return results[0][0]
 
         
-    def execute(self, id, conn = None, transaction = False):
+    def execute(self, jobid, conn = None, transaction = False):
         """
         _execute_
 
         Execute the SQL for the given job ID and then format and return
         the result.
         """
-        if type(id) == list:
-            result = self.dbi.processData(self.bulkSQL, id, conn = conn, transaction = transaction)
-            return self.formatDict(result)
-        result = self.dbi.processData(self.sql, {"jobid": id}, conn = conn,
+
+        if type(jobid) == list:
+            result = self.dbi.processData(self.bulkSQL, jobid, conn = conn, transaction = transaction)
+            tmp = self.formatDict(result)
+            return tmp
+        result = self.dbi.processData(self.sql, {"jobid": jobid}, conn = conn,
                                       transaction = transaction)
         
         return self.format(result)

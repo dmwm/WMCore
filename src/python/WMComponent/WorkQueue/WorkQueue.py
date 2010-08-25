ba4949611efe17a323b970567bfd3f82004384ac
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:11:41 +0000

    Added functions to Add Elements to the workqueue, to set priority, and the reorder the list
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4099 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/WorkQueue/WorkQueue.py
+++ src/python/WMComponent/WorkQueue/WorkQueue.py
@@ -6,11 +6,11 @@ _WorkQueue_
 container representing work queue
 
 """
-__revision__ = "$Id: WorkQueue.py,v 1.1 2009/05/08 10:49:06 fisk Exp $"
-__version__  = "$Revision: 1.1 $"
+__revision__ = "$Id: WorkQueue.py,v 1.2 2009/05/08 13:59:59 fisk Exp $"
+__version__  = "$Revision: 1.2 $"
 
 from WMCore.DataStructs.WMObject import WMObject
-
+import time
 
 class WQElement(WMObject):
     """
@@ -28,12 +28,37 @@ class WQElement(WMObject):
         self.priority = priority
         self.online = online
         self.njobs = njobs
-        self.next = "Null"
+        self.time = time.time()
+
+    def __cmp__(x, y):
+        tfactor = x.time
+        current = time.time()
+        weight = 0.01
+        return (x.priority + weight*(current - x.time)) - (y.priority + weight*(current - y.time))
+        
 
 class WorkQueue(WMObject):
     def __init__(self):
         self.elements = [ ]
- 
-       
 
+    def AddElement(self, idnumber = None, blocks = [ ],  priority = 0,  njobs = 0):
+        locations = [ ] # Should be automated 
+        online = 1 # Should be automated 
+        x = WQElement(idnumber, blocks, locations, priority, online, njobs)
+        self.elements = self.elements + [ x ] 
 
+    def ReorderList(self):
+        self.elements.sort()    
+
+    def SetPriority(self, idnumber, newpriority):
+        found = 0
+        count = 0
+        while (len(self.elements) < count or found):
+            if (self.elements[count].idnumber == idnumber):
+                found=1
+                self.elements[count].priority = newpriority
+            if (not found):
+                print "Element not found nothing changed"
+            else:
+                self.ReorderList
+             

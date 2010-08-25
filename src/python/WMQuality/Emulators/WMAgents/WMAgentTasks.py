af971d4eee3e2244ca86e1447a9dfc98a5d6255a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:07:18 +0000

    fix typos and bug
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6660 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMQuality/Emulators/WMAgents/WMAgentTasks.py
+++ src/python/WMQuality/Emulators/WMAgents/WMAgentTasks.py
@@ -3,8 +3,8 @@
 The actual taskArchiver algorithm
 """
 __all__ = []
-__revision__ = "$Id: WMAgentTasks.py,v 1.2 2010/03/03 22:16:15 sryu Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: WMAgentTasks.py,v 1.3 2010/03/05 15:45:10 sryu Exp $"
+__version__ = "$Revision: 1.3 $"
 
 import threading
 import logging
@@ -31,14 +31,14 @@ class WMAgentTasks(BaseWorkerThread):
         
         data = self.wq.getWork(self.resources)
         print "Data back from workqueue"
-        print "%s, %s" % data
+        print "%s: %s" % (self.resources, data)
         if len(data) != 0:
             elementIDs = []
             for element in data:
                 elementIDs.append(element['element_id'])
             self.wq.gotWork(elementIDs)
-        time.sleep(5)
-        self.wq.doneWork(elementIDs)
-        #self.wq.failWork(elementIDs)
-        #self.wq.cancelWork(elementIDs)
+            time.sleep(5)
+            self.wq.doneWork(elementIDs)
+            #self.wq.failWork(elementIDs)
+            #self.wq.cancelWork(elementIDs)
         

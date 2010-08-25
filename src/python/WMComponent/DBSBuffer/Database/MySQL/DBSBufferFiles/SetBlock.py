06feb021a3411396b2508781eaf2e331de043b9c
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:17:54 +0000

    Updated for new DBSUploader
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7616 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSBuffer/Database/MySQL/DBSBufferFiles/SetBlock.py
+++ src/python/WMComponent/DBSBuffer/Database/MySQL/DBSBufferFiles/SetBlock.py
@@ -13,8 +13,19 @@ class SetBlock(DBFormatter):
              WHERE dbsbuffer_file.lfn = :filelfn"""  
     
     def execute(self, lfn, blockName, conn = None, transaction = None):
-        result = self.dbi.processData(self.sql, {"block": blockName,
-                                                 "filelfn": lfn},
-                                      conn = conn,
+        """
+        Insert block name into files
+
+        """
+
+        if type(lfn) == list:
+            binds = []
+            for entry in lfn:
+                binds.append({'block': blockName, 'filelfn': entry})
+        else:
+            binds = {"block": blockName, "filelfn": lfn}
+
+        
+        result = self.dbi.processData(self.sql, binds, conn = conn,
                                       transaction = transaction)
         return

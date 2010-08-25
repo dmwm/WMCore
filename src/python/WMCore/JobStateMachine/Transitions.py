e0e915b634bdc46c68dff29321b167afb3977b5d
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:41:17 +0000

    Splitting JSM state definitions out of ChangeState. ChangeState will get pushed once I make sure nothing breaks
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4546 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/JobStateMachine/Transitions.py
@@ -0,0 +1,38 @@
+'''
+Created on Jul 15, 2009
+
+@author: meloam
+'''
+
+class Transitions(dict):
+    """
+    All allowed state transitions in the JSM.
+    """
+    def __init__(self):
+        self.setdefault('none', ['new'])
+        self.setdefault('new', ['created', 'createfailed'])
+        self.setdefault('created', ['executing', 'submitfailed'])
+        self.setdefault('executing', ['complete'])
+        self.setdefault('complete', ['jobfailed', 'success'])
+        self.setdefault('createfailed', ['createcooloff', 'exhausted'])
+        self.setdefault('submitfailed', ['submitcooloff', 'exhausted'])
+        self.setdefault('jobfailed', ['jobcooloff', 'exhausted'])
+        self.setdefault('createcooloff', ['new'])
+        self.setdefault('submitcooloff', ['created'])
+        self.setdefault('jobcooloff', ['created'])
+        self.setdefault('success', ['closeout'])
+        self.setdefault('exhausted', ['closeout'])
+        self.setdefault('closeout', ['cleanout'])
+
+    def states(self):
+        """
+        Return a list of all known states, derive it in case we add new final
+        states other than cleanout.
+        """
+        knownstates = Set(self.keys())
+        for possiblestates in self.values():
+            for i in possiblestates:
+                knownstates.add(i)
+        return list(knownstates)
+
+

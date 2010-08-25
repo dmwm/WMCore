b822ab331483e420058ee5ed3073a1514ca0d7dd
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:51:11 +0000

    Initial checkin of a function that searches through all the jobs
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4696 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/MySQL/Jobs/GetAllJobs.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+_GetLocation_
+
+MySQL implementation of Jobs.GetAllJobs
+"""
+
+__all__ = []
+__revision__ = "$Id: GetAllJobs.py,v 1.1 2009/07/30 19:29:42 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class GetAllJobs(DBFormatter):
+    """
+    _GetLocation_
+
+    Retrieve all files that are associated with the given job from the
+    database.
+    """
+    sql_all   = "SELECT id FROM wmbs_job"
+    
+    sql_state = "SELECT id FROM wmbs_job WHERE state = (SELECT id FROM wmbs_job_state WHERE name = :state)"
+
+    def format(self, results):
+        """
+        _formatDict_
+
+        Cast the file attribute to an integer, and also handle changing the
+        column name in Oracle from FILEID to FILE.
+        """
+
+        if len(results) == 0:
+            return False
+        else:
+            tempList = results[0].fetchall()
+            final = []
+            for i in tempList:
+                final.append(i.values()[0])
+            return final
+
+        
+    def execute(self, state = None, conn = None, transaction = False):
+        """
+        _execute_
+
+        Execute the SQL for the given job ID and then format and return
+        the result.
+        """
+        if state == None:
+            result = self.dbi.processData(self.sql_all, {}, conn = conn,
+                                          transaction = transaction)
+        else:
+            result = self.dbi.processData(self.sql_state, {'state':state.lower()}, conn = conn,
+                                          transaction = transaction)
+        
+        return self.format(result)
---
+++ src/python/WMCore/WMBS/SQLite/Jobs/GetAllJobs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+_GetAllJobs_
+
+SQLite implementation of Jobs.GetAllJobs
+"""
+
+__all__ = []
+__revision__ = "$Id: GetAllJobs.py,v 1.1 2009/07/30 19:29:43 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.WMBS.MySQL.Jobs.GetAllJobs import GetAllJobs as MySQLGetAllJobs
+
+class GetAllJobs(MySQLGetAllJobs):
+    """
+    Right now the same as the MySQL version
+
+    """

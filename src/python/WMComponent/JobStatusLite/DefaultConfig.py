50c3e7c96793b0818521d1615f3f52c9f91d88a8
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 05:06:38 +0000

    Better default config
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8250 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/JobStatusLite/DefaultConfig.py
+++ src/python/WMComponent/JobStatusLite/DefaultConfig.py
@@ -4,16 +4,15 @@
 Defines default config values for JobStatusLite specific parameters.
 """
 __all__ = []
-__revision__ = "$Id: DefaultConfig.py,v 1.2 2010/06/09 20:28:48 mcinquil Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: DefaultConfig.py,v 1.3 2010/07/23 15:49:51 mcinquil Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.Agent.Configuration import Configuration
 
 config = Configuration()
 config.component_('JobStatusLite')
 config.JobStatusLite.namespace       = 'WMComponent.JobStatusLite.JobStatusLite'
-config.JobStatusLite.componentDir    = '/home/cinquilli/Development/DMWM/WMAgent/work/JobStatusLite'
-config.JobStatusLite.ComponentDir    = '/tmp/application/JobStatusLite'
+config.JobStatusLite.componentDir    = os.path.join(os.getcwd(), 'Components')
 config.JobStatusLite.logLevel        = 'INFO'
 config.JobStatusLite.pollInterval    = 10
 config.JobStatusLite.queryInterval   = 2

64ec879a0842af96652e4dcdcccf0f45f7af2d98
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:03:07 +0000

    Fix a syntax error in the query.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3967 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Subscriptions/FailFiles.py
+++ src/python/WMCore/WMBS/MySQL/Subscriptions/FailFiles.py
@@ -6,8 +6,8 @@ MySQL implementation of Subscription.FailFiles
 """
 
 __all__ = []
-__revision__ = "$Id: FailFiles.py,v 1.5 2009/03/23 19:05:09 sfoulkes Exp $"
-__version__ = "$Revision: 1.5 $"
+__revision__ = "$Id: FailFiles.py,v 1.6 2009/03/24 16:31:26 sfoulkes Exp $"
+__version__ = "$Revision: 1.6 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -15,10 +15,11 @@ class FailFiles(DBFormatter):
     sql = """INSERT INTO wmbs_sub_files_failed (subscription, file)
                SELECT :subscription, :fileid FROM dual WHERE NOT EXISTS
                  (SELECT file FROM wmbs_sub_files_failed
-                    WHERE file = :fileid AND subscription = subscription)"""    
+                    WHERE file = :fileid AND subscription = :subscription)"""    
 
     def execute(self, subscription = None, file = None, conn = None,
                 transaction = False):
-        self.dbi.processData(self.sql, self.getBinds(subscription=subscription, fileid=file),
+        binds = self.getBinds(subscription=subscription, fileid=file)
+        self.dbi.processData(self.sql, binds, 
                              conn = conn, transaction = transaction)
         return

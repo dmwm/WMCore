7c7d1441fb366ef225ba54ec67fb34617e81c9e5
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:06:47 +0000

    Mark sucessful jobs as being successful instead of marking them as failed.  This should reduce _some_ confusion.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4933 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/Save.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/Save.py
@@ -6,8 +6,8 @@ MySQL implementation of Jobs.Save
 """
 
 __all__ = []
-__revision__ = "$Id: Save.py,v 1.6 2009/05/11 14:47:49 sfoulkes Exp $"
-__version__ = "$Revision: 1.6 $"
+__revision__ = "$Id: Save.py,v 1.7 2009/08/27 16:49:40 sfoulkes Exp $"
+__version__ = "$Revision: 1.7 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -20,9 +20,9 @@ class Save(DBFormatter):
     def execute(self, jobid, jobgroup, name, couch_record, location, outcome, 
                 conn = None, transaction = False):
         if outcome == 'success':
-            boolOutcome = 0
-        else:
             boolOutcome = 1
+        else:
+            boolOutcome = 0
         
         binds = {"jobid": jobid, "jobgroup": jobgroup, "name": name, 
                  "couch_record": couch_record, "location": location, 

899f7406ec7955804cd16a0d31e0f6d96b4df92a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:12:01 +0000

    Cleanups.  Also map SE names to PhEDEx node names before trying to inject blocks.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5011 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/PhEDExInjector/PhEDExInjectorPoller.py
+++ src/python/WMComponent/PhEDExInjector/PhEDExInjectorPoller.py
@@ -1,119 +1,113 @@
 #!/usr/bin/env python
-#pylint: disable-msg=W0613
 """
-The PhEDExInjector algorithm
+_PhEDExInjectorPoller_
+
 """
-__all__ = []
-__revision__ = "$Id: PhEDExInjectorPoller.py,v 1.4 2009/08/24 11:10:03 meloam Exp $"
-__version__ = "$Revision: 1.4 $"
-__author__ = "mnorman@fnal.gov"
+
+__revision__ = "$Id: PhEDExInjectorPoller.py,v 1.5 2009/09/08 18:28:15 sfoulkes Exp $"
+__version__ = "$Revision: 1.5 $"
 
 import threading
 import logging
-import re
-import os
-import time
-from sets import Set
-
-import inspect
 
 from WMCore.WorkerThreads.BaseWorkerThread import BaseWorkerThread
 
-from WMCore.WMFactory import WMFactory
-
-from WMCore.WMBS.Subscription import Subscription
-from WMCore.WMBS.Fileset import Fileset
-from WMCore.WMBS.Workflow import Workflow
-
-from DBSAPI.dbsApi import DbsApi
-#from DBSAPI.dbsException import *
-#from DBSAPI.dbsApiException import *
-#from DBSAPI.dbsAlgorithm import DbsAlgorithm
-
-from WMComponent.DBSBuffer.Database.Interface.DBSBufferFile import DBSBufferFile
-
-from WMCore.Services.DBS.DBSWriter import DBSWriter
-from WMCore.Services.DBS           import DBSWriterObjects
-from WMCore.Services.DBS.DBSErrors import DBSWriterError, formatEx,DBSReaderError
-from WMCore.Services.DBS.DBSReader import DBSReader
 from WMCore.Services.PhEDEx.PhEDEx import PhEDEx as phedexApi
-from WMCore.DAOFactory import DAOFactory
+from WMCore.Services.Requests import JSONRequests
 
+from WMCore.DAOFactory import DAOFactory
 
 class PhEDExInjectorPoller(BaseWorkerThread):
     """
-    Handles poll-based DBSUpload
+    _PhEDExInjectorPoller_
 
     """
-
-
     def __init__(self, config, noclue = None):
         """
+        ___init___
+        
         Initialise class members
         """
-        myThread = threading.currentThread()
-        myThread.dialect = os.getenv('DIALECT')
         BaseWorkerThread.__init__(self)
-        self.config     = config
-        self.phedex     = phedexApi({'endpoint': config.PhEDExInjector.phedexurl}, 'json' )
-        self.dbsUrl     = config.DBSUpload.dbsurl 
-        self.uploadFileMax = 10
+        self.config = config
+        self.phedex = phedexApi({"endpoint": config.PhEDExInjector.phedexurl}, "json")
+        self.dbsUrl = config.DBSUpload.dbsurl 
+
+        # This will be used to map SE names which are stored in the DBSBuffer to
+        # PhEDEx node names.  The first key will be the "kind" which consists
+        # of one of the following: MSS, Disk, Buffer.  The next key will be the
+        # SE name.
+        self.seMap = {}
     
     def setup(self, parameters):
         """
-        Load DB objects required for queries
+        _setup_
+        
+        Create a DAO Factory for the PhEDExInjector.  Also load the SE names to
+        PhEDEx node name mappings from the data service.
         """
         myThread = threading.currentThread()
+        daofactory = DAOFactory(package = "WMComponent.PhEDExInjector.Database",
+                                logger = self.logger,
+                                dbinterface = myThread.dbi)
 
-        # The WMBS base class creates a DAO factory for WMBS, we'll need to
-        # overwrite that so we can use the factory for PhEDExInjector objects.
-        self.daofactory = DAOFactory(package = "WMComponent.PhEDExInjector.Database",
-                                     logger = self.logger,
-                                     dbinterface = myThread.dbi)
+        self.getUninjected = daofactory(classname = "GetUninjectedBlocks")
+        self.setInjected = daofactory(classname = "SetBlocksInjected")
+
+        cmswebReq = JSONRequests(url = "cmsweb.cern.ch")
+        cmswebResp = cmswebReq.get(uri = "/phedex/datasvc/json/prod/nodes")[0]
+
+        for node in cmswebResp:
+            if not seMap.has_key(node["kind"]):
+                seMap[node["kind"]] = {}
+
+            seMap[node["kind"]][node["se"]] = node["name"]
 
         return
 
-    def inject(self):
-        myThread = threading.currentThread()
-
-        action = self.daofactory(classname = "GetUninjectedBlocks")
-        setAction = self.daofactory(classname = "SetBlocksInjected")
-        rows = action.execute( )
-        print rows
-        condensed = {}
-        for row in rows:
-            if not row['location'] in condensed:
-                condensed[row['location']] = []
-            condensed[row['location']].append(row['blockname'])
-            self.phedex.injectBlocks(self.dbsUrl, row['location'], row['blockname'])
-            setAction.execute( [ {'location': row['location'], 'blockname':row['blockname']} ],
-                               conn= myThread.transaction.conn,
-                               transaction=myThread.transaction )
-            
-#        for loc,blocks in condensed.iteritems():
-#            print self.phedex.injectBlocks(self.dbsUrl, 
-#                                         loc,
-#                                         0,
-#                                         1,
-#                                         *blocks)   
-        pass
-    
-    def terminate(self,params):
-        logging.debug("terminating. doing one more pass before we die")
-        self.algorithm(params)
-        
     def algorithm(self, parameters):
         """
-        Queries DB for all watched filesets, if matching filesets become
-        available, create the subscriptions. Wraps in transaction.
-        """
-        logging.debug("Running subscription / fileset matching algorithm")
-        myThread = threading.currentThread()
-        try:
-            myThread.transaction.begin()
-            self.inject()
-            myThread.transaction.commit()
-        except:
-            myThread.transaction.rollback()
-            raise
+        _algorithm_
 
+        Poll the database for uninjected blocks and attempt to inject them into
+        PhEDEx.
+        """
+        myThread = threading.currentThread()
+        myThread.transaction.begin()
+
+        uninjectedBlocks = self.getUninjected.execute()
+
+        injectedBlocks = []
+        for uninjectedBlock in uninjectedBlocks:
+            # SE names are stored in DBSBuffer as that is what is returned in
+            # the framework job report.  We'll try to map the SE name to a
+            # PhEDEx node name here. 
+            location = None
+            if self.seMap.has_key("MSS"):
+                if self.seMap["MSS"].has_key(uninjectedBlock["location"]):
+                    location = self.seMap["MSS"][uninjectedBlock["location"]]
+            elif self.seMap.has_key("Disk"):
+                if self.seMap["Disk"].has_key(uninjectedBlock["location"]):
+                    location = self.seMap["Disk"][uninjectedBlock["location"]]
+
+            if location == None:
+                logging.error("Could not map SE %s to PhEDEx node." % \
+                              uninjectedBlock["location"])
+                continue
+                    
+            result = self.phedex.injectBlocks(self.dbsUrl,
+                                              uninjectedBlock["location"],
+                                              uninjectedBlock["blockname"])
+
+            if not result.has_key("error"):
+                injectedBlocks.append({"location": uninjectedBlock["location"],
+                                       "blockname": uninjectedBlock["blockname"])
+            else:
+                logging.error("Error injecting block %s: %s" % \
+                              (uninjectBlock["blockname"], result["error"]))
+
+        setAction.execute(injectedBlocks, conn = myThread.transaction.conn,
+                          transaction = myThread.transaction)
+
+        myThread.transaction.commit()
+        return

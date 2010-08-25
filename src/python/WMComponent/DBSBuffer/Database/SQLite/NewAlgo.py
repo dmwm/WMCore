db9471c716589a02c512ea2e7d11685e20ad9b11
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:46:33 +0000

    add appropriate sql for sqlite
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6372 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSBuffer/Database/SQLite/NewAlgo.py
+++ src/python/WMComponent/DBSBuffer/Database/SQLite/NewAlgo.py
@@ -5,8 +5,8 @@ _NewAlgo_
 SQLite implementation of DBSBuffer.NewAlgo
 """
 
-__revision__ = "$Id: NewAlgo.py,v 1.2 2009/07/13 19:39:01 sfoulkes Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: NewAlgo.py,v 1.3 2010/02/09 17:12:52 meloam Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMComponent.DBSBuffer.Database.MySQL.NewAlgo import NewAlgo as MySQLNewAlgo
 
@@ -24,3 +24,7 @@ class NewAlgo(MySQLNewAlgo):
                    (SELECT * FROM dbsbuffer_algo WHERE app_name = :app_name AND
                       app_ver = :app_ver AND app_fam = :app_fam AND
                       pset_hash = :pset_hash)"""    
+    
+    existsSQL = """SELECT id FROM dbsbuffer_algo WHERE app_name = :app_name AND
+                     app_ver = :app_ver AND app_fam = :app_fam AND
+                     pset_hash = :pset_hash"""

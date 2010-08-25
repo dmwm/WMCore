ef088ecb09b2f0c479b477a4eebd358a09ce1faa
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:21:36 +0000

    change env name WMCOREBASE -> WMCORE_ROOT, and YUIHOME -> YUI_ROOT but leave old name as well for now. - should remove
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6012 4525493e-7705-40b1-a816-d608a930855b

--- standards/setup.sh
+++ standards/setup.sh
@@ -11,16 +11,24 @@ export STYLE=/home/fvlingen/programFiles/CMS_CVS/WMCORE/standards/.pylintrc
 # directory used for tests.
 export TESTDIR=/home/fvlingen/programFiles/CMS_CVS/WMCORE_TEST
 # location of WMCORE
-export WMCOREBASE=/home/fvlingen/programFiles/CMS_CVS/WMCORE
+export WMCORE_ROOT=/home/fvlingen/programFiles/CMS_CVS/WMCORE
+#TODO: replace to WMCORE_ROOT - remove this line - backward compatibility
+export WMCOREBASE=$WMCORE_ROOT
+
 # webtools base (only for webtools related files and modules)
-export WTBASE=$WMCOREBASE/src
+#TODO: remove this as well - backward compatibility
+export WTBASE=$WMCORE_ROOT/src
 # yui home (only for webtools related files and modules)
-export YUIHOME=/put/your/value/here
+export YUI_ROOT=/put/your/value/here
+#TODO: remove this as well - backward compatibility
+export YUIHOME=$YUI_ROOT
+
 # path settings for incorporating wmcore binaries.
-export PATH=$WMCOREBASE/bin:$PATH
+
+export PATH=$WMCORE_ROOT/bin:$PATH
 # add wmcore library and tests to pythonpath.
-export PYTHONPATH=$WMCOREBASE/src/python:$PYTHONPATH
-export PYTHONPATH=$WMCOREBASE/test/python:$PYTHONPATH
+export PYTHONPATH=$WMCORE_ROOT/src/python:$PYTHONPATH
+export PYTHONPATH=$WMCORE_ROOT/test/python:$PYTHONPATH
 
 #TODO: this should eventually defined with in WMCore envrioment. but for not get it from prodagent setting.
 echo "-->Sourcing CMS environment"

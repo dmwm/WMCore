437d8ed08672274d9ba3fd3d9206030218d7443e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:38:48 +0000

    Fix python setup.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7893 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/JobSubmitter/submit.sh
+++ src/python/WMComponent/JobSubmitter/submit.sh
@@ -34,7 +34,7 @@ if [[ $rc != 0 ]]
 then
 	if [ -e $VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/python/2.6.4/etc/profile.d/init.sh ]
 	then
-		. $VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/python/2.6.4-cms2/etc/profile.d/init.sh 
+		. $VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/python/2.6.4/etc/profile.d/init.sh 
 		export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/openssl/0.9.7m/lib:$VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/bz2lib/1.0.5/lib
 	else
 		echo "WMAgent Error: Python2.6 isn't available on this worker node." >&2

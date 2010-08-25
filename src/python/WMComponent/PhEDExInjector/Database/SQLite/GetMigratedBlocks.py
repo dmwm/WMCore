160a1f110ac64c279db2506111c5628005c48c2d
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:54:34 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5631 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMComponent/PhEDExInjector/Database/SQLite/GetMigratedBlocks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+_GetMigratedBlocks_
+
+SQLite implementation on PhEDExInjector.GetMigratedBlocks.
+"""
+
+__revision__ = "$Id: GetMigratedBlocks.py,v 1.1 2009/12/02 17:56:32 sfoulkes Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMComponent.PhEDExInjector.Database.MySQL.GetMigratedBlocks import GetMigratedBlocks as MySQLBase
+
+class GetMigratedBlocks(MySQLBase):
+    """
+    _GetMigratedBlocks_
+
+    """
+    pass

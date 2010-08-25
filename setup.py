57067bb1a7c850c8fe110b21bfb4b0cd2647000d
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:05:06 +0000

    Add test and clean commands, can now automate the testing in buildbot. Currently not doing so well:
    "FAILED (failures=5, errors=468)"
    
    oops ;)
    
    TODO: Pull database URL's from environment, and skip tests where database URL is not present (e.g. for a slave without Oracle connection)
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4906 4525493e-7705-40b1-a816-d608a930855b

--- setup.py
+++ setup.py
@@ -1,12 +1,71 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from distutils.core import setup, Command
+from unittest import TextTestRunner, TestLoader, TestSuite
+from glob import glob
+from os.path import splitext, basename, join as pjoin, walk
+import os
 
-#TODO: derive a test suite (from packages) and run it if someone calls
-#setup.py test. Deriving the list of tests should also point out missing tests
-  
+class TestCommand(Command):
+    user_options = [ ]
+
+    def initialize_options(self):
+        self._dir = os.getcwd()
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        '''
+        Finds all the tests modules in tests/, and runs them.
+        '''
+        testfiles = [ ]
+        # Walk the directory tree
+        for dirpath, dirnames, filenames in os.walk('./test/python/WMCore_t'):
+            # skipping CVS directories and their contents
+            pathelements = dirpath.split('/')
+            if not 'CVS' in pathelements:
+                # to build up a list of file names which contain tests
+                for file in filenames:
+                    if file not in ['__init__.py']:
+                        if file.endswith('_t.py'):
+                            testmodpath = pathelements[3:]
+                            testmodpath.append(file.replace('.py',''))
+                            testfiles.append('.'.join(testmodpath))
+                            
+        testsuite = TestSuite()
+        for test in testfiles:
+            try:
+                testsuite.addTest(TestLoader().loadTestsFromName(test))
+            except Exception, e:
+                print "Could not load %s test - fix it!\n %s" % (test, e)
+        print "Running %s tests" % testsuite.countTestCases()
+        
+        t = TextTestRunner(verbosity = 1)
+        t.run(testsuite)
+
+class CleanCommand(Command):
+    user_options = [ ]
+
+    def initialize_options(self):
+        self._clean_me = [ ]
+        for root, dirs, files in os.walk('.'):
+            for f in files:
+                if f.endswith('.pyc'):
+                    self._clean_me.append(pjoin(root, f))
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for clean_me in self._clean_me:
+            try:
+                os.unlink(clean_me)
+            except:
+                pass
+            
 setup (name='wmcore',
        version='1.0',
-       test_suite = '',
+       cmdclass = { 'test': TestCommand, 'clean': CleanCommand },
        package_dir={'WMCore': 'src/python/WMCore','WMComponent' : 'src/python/WMComponent','WMQuality' : 'src/python/WMQuality'},
        packages=['WMComponent.DBSBuffer.Handler',
                  'WMComponent.DBSBuffer.Database.SQLite.DBSBufferFiles',

c7621e12a1269c7cfc424fc5b3bf1dc3c3e2d586
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 05:28:49 +0000

    Successfully pass limit option.  Also did some housecleaning.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8536 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/JobSplitting/TwoFileBased.py
+++ src/python/WMCore/JobSplitting/TwoFileBased.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+#pylint: disable-msg=E1103
+# E1103:  Attach objects to threading
 """
 _TwoFileBased_
 
@@ -7,24 +9,28 @@ normal file based splitting except that the input files will also have their
 parentage information loaded so that the parents can be included in the job.
 """
 
-__revision__ = "$Id: TwoFileBased.py,v 1.12 2010/06/08 20:05:57 mnorman Exp $"
-__version__  = "$Revision: 1.12 $"
+__revision__ = "$Id: TwoFileBased.py,v 1.13 2010/08/16 16:04:53 mnorman Exp $"
+__version__  = "$Revision: 1.13 $"
 
 import logging
 import threading
 
 from WMCore.JobSplitting.JobFactory import JobFactory
-from WMCore.Services.UUID           import makeUUID
 from WMCore.DAOFactory              import DAOFactory
 
 from WMCore.WMBS.File  import File
 
 class TwoFileBased(JobFactory):
+    """
+    Two file read workflow splitting
+
+    """
 
 
     def __init__(self, package='WMCore.DataStructs',
                  subscription=None,
-                 generators=[]):
+                 generators=[],
+                 limit = None):
         """
         __init__
 
@@ -35,7 +41,8 @@ class TwoFileBased(JobFactory):
 
         JobFactory.__init__(self, package = 'WMCore.WMBS',
                             subscription = subscription,
-                            generators = generators)
+                            generators = generators,
+                            limit = limit)
 
 
         self.daoFactory = DAOFactory(package = "WMCore.WMBS",
@@ -76,6 +83,7 @@ class TwoFileBased(JobFactory):
             if len(fileList) == 0:
                 #No files for this location
                 #This isn't supposed to happen, but better safe then sorry
+                logging.debug("Have location %s with no files" % (location))
                 continue
             for file in fileList:
                 parentLFNs = self.findParent(lfn = file['lfn'])
@@ -98,6 +106,9 @@ class TwoFileBased(JobFactory):
                 self.currentJob.addFile(file)
                 
                 listOfFiles.append(file)
+
+
+            
 
         return
 

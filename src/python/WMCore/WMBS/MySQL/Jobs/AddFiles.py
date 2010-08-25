f5de79fe001b6def44cd8f65493e8c7aa4d81ca8
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:13:30 +0000

    Added ability to do bulk addition by sending a dictionary of form {jid: fileid} named jobDict.  Should not interfere with current functionality
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5033 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Jobs/AddFiles.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/AddFiles.py
@@ -5,8 +5,10 @@ MySQL implementation of Jobs.AddFiles
 """
 
 __all__ = []
-__revision__ = "$Id: AddFiles.py,v 1.9 2009/03/20 14:29:19 sfoulkes Exp $"
-__version__ = "$Revision: 1.9 $"
+__revision__ = "$Id: AddFiles.py,v 1.10 2009/09/10 16:22:59 mnorman Exp $"
+__version__ = "$Revision: 1.10 $"
+
+import logging
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -16,8 +18,29 @@ class AddFiles(DBFormatter):
                  (SELECT * FROM wmbs_job_assoc
                   WHERE job = :jobid AND file = :fileid)"""
     
-    def execute(self, id = None, file = None, conn = None, transaction = False):
-        binds = self.getBinds(jobid = id, fileid = file)
+    def getBinds(self, jobDict):
+        binds = []
+        for jid in jobDict.keys():
+            #For each job
+            for fileID in jobDict[jid]:
+                #For each file in each job
+                binds.append({'jobid': jid, 'fileid': fileID})
+
+        return binds
+    
+    def execute(self, id = None, file = None, conn = None, transaction = False, jobDict = None):
+
+        #Adding jobDict activates bulk mode
+        #Bulk mode expect jobDict of form {jid: fileid}
+
+        if jobDict:
+            binds = self.getBinds(jobDict)
+        elif id and file:
+            binds = DBFormatter.getBinds(self, jobid = id, fileid = file)
+        else:
+            logging.error('Jobs.AddFiles called with insufficient arguments')
+            return
+        
         self.dbi.processData(self.sql, binds, conn = conn,
                              transaction = transaction)
         return

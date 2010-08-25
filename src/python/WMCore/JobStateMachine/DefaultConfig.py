31a711f1d626bb5236b9293507963770202cad47
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:33:03 +0000

    CouchURL doesn't need the protocol in front of it
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4422 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/JobStateMachine/DefaultConfig.py
+++ src/python/WMCore/JobStateMachine/DefaultConfig.py
@@ -6,7 +6,7 @@ config = Configuration()
 jsm = config.component_('JobStateMachine')
 
 if (os.getenv('COUCHURL') != None):
-    jsm.couchurl = os.getenv('COUCHURL')
+    couchurl = os.getenv('COUCHURL')
 else:
-    jsm.couchurl = 'http://localhost:5984'
+    couchurl = 'localhost:5984'
 jsm.default_retries = 1

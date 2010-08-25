49aba659581946567841cddb4d2a4fd418d4b7cf
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:29:01 +0000

    change column name to insert_time
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4362 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WorkQueue/Database/MySQL/WorkQueueElement/New.py
+++ src/python/WMCore/WorkQueue/Database/MySQL/WorkQueueElement/New.py
@@ -5,24 +5,24 @@ MySQL implementation of WorkQueueElement.New
 """
 
 __all__ = []
-__revision__ = "$Id: New.py,v 1.2 2009/06/15 20:56:59 sryu Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: New.py,v 1.3 2009/06/19 22:14:56 sryu Exp $"
+__version__ = "$Revision: 1.3 $"
 
 import time
 from WMCore.Database.DBFormatter import DBFormatter
 
 class New(DBFormatter):
     sql = """INSERT INTO wq_element (wmspec_id, block_id, num_jobs, priority,
-                         parent_flag, last_updated) 
+                         parent_flag, insert_time) 
                  VALUES (:wmSpecID, :blockID, :numJobs, :priority, :parentFlag,
-                         :lastUpdated)
+                         :insertTime)
           """
 
     def execute(self, wmSpecID, blockID, numJobs, priority, parentFlag = 0,
                 conn = None, transaction = False):
         binds = {"wmSpecID":wmSpecID, "blockID":blockID, "numJobs":numJobs, 
                  "priority":priority, "parentFlag":parentFlag, 
-                 "lastUpdated": int(time.time())}
+                 "insertTime": int(time.time())}
 
         self.dbi.processData(self.sql, binds, conn = conn,
                              transaction = transaction)            

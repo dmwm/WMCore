716e27695d8aed28b309e2da1408b281ffe9c24c
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:31:43 +0000

    Make more appealing front web page
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4402 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/Welcome.py
+++ src/python/WMCore/WebTools/Welcome.py
@@ -1,3 +1,4 @@
+import time
 from cherrypy import expose
 from cherrypy import __version__ as cherrypy_version
 from Cheetah import Version as cheetah_version
@@ -15,15 +16,22 @@ class Welcome(Page):
         html += '</head>'
         html += '<body>'
         html += '<h1>Welcome to the DMWM web framework</h1>'
+        html += '<hr style="width:100%;border-top: 1px dotted #CCCCCC;" />'
         html += "<table>\n"
-        html += "<tr><th><h3>Service</h3></th><th><h3>Description</h3></th></tr>\n"
+        html += '<tr><th align="left"><h3>Service</h3></th>\n'
+        html += '<th align="left"><h3>Description</h3></th></tr>\n'
         self.namesAndDocstrings.sort()
         for name, docstring in self.namesAndDocstrings:
-            html += '<tr><td><p><a href="%s">%s</a></p></td><td><p>%s</p></td></tr>' % (name, name, docstring)
-        html += "<tr><td><h3>Server info</h3</td></tr>\n"
-        html += '<tr><td><p>CherryPy: v.%s</p>' % cherrypy_version
-        html += '<p>Cheetah: v.%s</p></td></tr>' % cheetah_version
-        html += '</table>'
+            html += '<tr><td><p><a href="%s">%s</a></p></td>\n' \
+                % (name, name)
+            html += '<td><p>%s</p></td></tr>\n' % docstring
+        html += '</table><br />'
+        html += '<hr style="width:100%;border-top: 1px dotted #CCCCCC;" />'
+        html += '<div style="font-size: 12px;font-weight: normal;font-family: helvetica;">'
+        timestamp = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime())
+        html += '<b>Server info:</b> CherryPy v%s, Cheetah: v%s, %s' \
+                % (cherrypy_version, cheetah_version, timestamp)
+        html += '</div>'
         html += '</body>'
         html += '</html>'
         return html

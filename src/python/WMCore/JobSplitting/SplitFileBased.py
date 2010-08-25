f8bbe64f402fdce296a6b1a1fc858c3510909593
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:32:24 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4412 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/JobSplitting/SplitFileBased.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+"""
+_SplitFileBased_
+
+JobSplitting algorithm for creating jobs that run over the results of split
+processing jobs.  This borrows heavily from the generic WMBS merging algorithm
+but instead of combining multiple merge units together this will create a
+single job for each merge unit.
+"""
+
+__revision__ = "$Id: SplitFileBased.py,v 1.2 2009/07/02 15:46:08 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
+
+import threading
+
+from WMCore.WMBS.File import File
+
+from WMCore.DAOFactory import DAOFactory
+from WMCore.JobSplitting.JobFactory import JobFactory
+from WMCore.Services.UUID import makeUUID
+
+def mergeUnitCompare(a, b):
+    """
+    _mergeUnitCompare_
+
+    Compare two merge units.  They will be sorted first by run ID and then by
+    lumi ID.
+    """
+    if a["run"] > b["run"]:
+        return 1
+    elif a["run"] == b["run"]:
+        if a["lumi"] > b["lumi"]:
+            return 1
+        elif a["lumi"] == b["lumi"]:
+            return 0
+        else:
+            return -1
+    else:
+        return -1
+
+def fileCompare(a, b):
+    """
+    _fileCompare_
+
+    Compare two files based on their "file_first_event" attribute.
+    """
+    if a["file_first_event"] > b["file_first_event"]:
+        return 1
+    if a["file_first_event"] == b["file_first_event"]:
+        return 0
+    else:
+        return -1
+
+def sortedFilesFromMergeUnits(mergeUnits):
+    """
+    _sortedFilesFromMergeUnits_
+
+    Given a list of merge units sort them and the files that they contain.
+    Return a list of sorted WMBS File structures.
+    """
+    mergeUnits.sort(mergeUnitCompare)
+
+    sortedFiles = []
+    for mergeUnit in mergeUnits:
+        mergeUnit["files"].sort(fileCompare)
+
+        for file in mergeUnit["files"]:
+            newFile = File(id = file["file_id"], lfn = file["file_lfn"])
+            sortedFiles.append(newFile)
+
+    return sortedFiles
+
+class SplitFileBased(JobFactory):
+    """
+    _SplitFileBased_
+
+    JobSplitting algorithm for creating jobs that run over the results of split
+    processing jobs.
+    """
+    def defineMergeUnits(self, mergeableFiles):
+        """
+        _defineMergeUnits_
+
+        Split all the of files into merge units.  A merge unit is a group
+        of files that must be merged together.  For example, the files that
+        result from event based splitting jobs need to be processed together and
+        in the correct order.  This method will return a list of merge units.
+        A merge unit is a dictionary with the following keys: group_id,
+        total_events, total_size, run, lumi, and files.  The files in the merge
+        group are stored in a list under the files key.
+        """
+        mergeUnits = []
+
+        for mergeableFile in mergeableFiles:
+            newMergeFile = {}
+            for key in mergeableFile.keys():
+                newMergeFile[key] = mergeableFile[key]
+
+            if mergeableFile["group_id"] != None:
+                for mergeUnit in mergeUnits:
+                    if mergeUnit["group_id"] == mergeableFile["group_id"]:
+                        mergeUnit["files"].append(newMergeFile)
+                        mergeUnit["total_size"] += newMergeFile["file_size"]
+                        mergeUnit["total_events"] += newMergeFile["file_events"]
+
+                        if mergeableFile["file_run"] < mergeUnit["run"] or \
+                           (mergeableFile["file_run"] == mergeUnit["run"] and \
+                            mergeableFile["file_lumi"] < mergeUnit["lumi"]):
+                            newMergeUnit["run"] = newMergeFile["file_run"]
+                            newMergeUnit["lumi"] = newMergeFile["file_lumi"]
+                            
+                        break
+                else:
+                    newMergeUnit = {}
+                    newMergeUnit["group_id"] = newMergeFile["group_id"]
+                    newMergeUnit["total_events"] = newMergeFile["file_events"]
+                    newMergeUnit["total_size"] = newMergeFile["file_size"]
+                    newMergeUnit["run"] = newMergeFile["file_run"]
+                    newMergeUnit["lumi"] = newMergeFile["file_lumi"]
+                    newMergeUnit["files"] = []
+                    newMergeUnit["files"].append(newMergeFile)
+                    mergeUnits.append(newMergeUnit)
+            else:
+                newMergeUnit = {}
+                newMergeUnit["group_id"] = -1
+                newMergeUnit["total_events"] = newMergeFile["file_events"]
+                newMergeUnit["total_size"] = newMergeFile["file_size"]
+                newMergeUnit["run"] = newMergeFile["file_run"]
+                newMergeUnit["lumi"] = newMergeFile["file_lumi"]                
+                newMergeUnit["files"] = []
+                newMergeUnit["files"].append(newMergeFile)
+                mergeUnits.append(newMergeUnit)
+
+        return mergeUnits
+
+    def createProcJobs(self, mergeUnits):
+        """
+        _createProcJob_
+
+        Given a list of merge units, create a job that processes all the files
+        in the merge unit.  Each job will be placed in its own JobGroup so that
+        merging will work correctly.
+        """
+        for mergeUnit in mergeUnits:
+            newJobGroup = self.groupInstance(subscription = self.subscription)
+            newJob = self.jobInstance(name = makeUUID())            
+            sortedFiles = sortedFilesFromMergeUnits([mergeUnit])
+
+            for file in sortedFiles:
+                file.load()
+                newJob.addFile(file)
+
+            newJobGroup.add(newJob)
+            newJobGroup.commit()
+            newJobGroup.recordAcquire(newJob)
+
+            self.jobGroups.append(newJobGroup)
+            
+        return
+    
+    def algorithm(self, groupInstance = None, jobInstance = None,
+                  *args, **kwargs):
+        """
+        _algorithm_
+
+        Use the generic WMBS merging DAO to get a list of files in our fileset
+        that correspond to completed job groups.  Create jobs for these files.
+        """
+        self.jobGroups = []
+
+        self.jobInstance = jobInstance
+        self.groupInstance = groupInstance
+
+        myThread = threading.currentThread()
+        daoFactory = DAOFactory(package = "WMCore.WMBS",
+                                logger = myThread.logger,
+                                dbinterface = myThread.dbi)
+        
+        mergeDAO = daoFactory(classname = "Subscriptions.GetFilesForMerge")
+        mergeableFiles = mergeDAO.execute(self.subscription["id"])
+
+        mergeUnits = self.defineMergeUnits(mergeableFiles)
+        mergeUnits.sort(mergeUnitCompare)
+        self.createProcJobs(mergeUnits)
+
+        return self.jobGroups
---
+++ test/python/WMCore_t/WMBS_t/JobSplitting_t/SplitFileBased_t.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python
+"""
+_SplitFileBased_t_
+
+Unit tests for the split file job splitting algorithm.
+"""
+
+__revision__ = "$Id: SplitFileBased_t.py,v 1.2 2009/07/02 15:46:08 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
+
+from sets import Set
+import unittest
+import os
+import threading
+
+from WMCore.WMBS.File import File
+from WMCore.WMBS.Fileset import Fileset
+from WMCore.WMBS.Job import Job
+from WMCore.WMBS.JobGroup import JobGroup
+from WMCore.WMBS.Subscription import Subscription
+from WMCore.WMBS.Workflow import Workflow
+
+from WMCore.DataStructs.Run import Run
+
+from WMCore.DAOFactory import DAOFactory
+from WMCore.WMFactory import WMFactory
+from WMCore.JobSplitting.SplitterFactory import SplitterFactory
+from WMCore.Services.UUID import makeUUID
+from WMQuality.TestInit import TestInit
+
+class SplitFileBasedTest(unittest.TestCase):
+    """
+    _SplitFileBasedTest_
+
+    Unit tests for the split file job splitting algorithm.
+    """
+    def runTest(self):
+        """
+        _runTest_
+
+        Run all the unit tests.
+        """
+        unittest.main()
+    
+    def setUp(self):
+        """
+        _setUp_
+
+        Create database connection and load up the WMBS schema.
+        """
+        self.testInit = TestInit(__file__, os.getenv("DIALECT"))
+        self.testInit.setLogging()
+        self.testInit.setDatabaseConnection()
+        self.testInit.setSchema(customModules = ["WMCore.WMBS"],
+                                useDefault = False)
+        
+        return
+
+    def tearDown(self):
+        """
+        _tearDown_
+
+        Clear out WMBS.
+        """
+        myThread = threading.currentThread()
+
+        if myThread.transaction == None:
+            myThread.transaction = Transaction(self.dbi)
+            
+        myThread.transaction.begin()
+            
+        factory = WMFactory("WMBS", "WMCore.WMBS")
+        destroy = factory.loadObject(myThread.dialect + ".Destroy")
+        destroyworked = destroy.execute(conn = myThread.transaction.conn)
+        
+        if not destroyworked:
+            raise Exception("Could not complete WMBS tear down.")
+            
+        myThread.transaction.commit()
+        return
+
+    def stuffWMBS(self):
+        """
+        _stuffWMBS_
+
+        Insert some dummy jobs, jobgroups, filesets, files and subscriptions
+        into WMBS to test job creation.  Three completed job groups each
+        containing several files are injected.  Another incomplete job group is
+        also injected.  Also files are added to the "Mergeable" subscription as
+        well as to the output fileset for their jobgroups.
+        """
+        bunkFileset = Fileset(name = "bunkFileset")
+        bunkFileset.create()
+
+        bunkWorkflow = Workflow(name = "bunkWorkflow", spec = "bunk",
+                                owner = "Steve")
+        bunkWorkflow.create()
+        
+        bunkSubscription = Subscription(fileset = bunkFileset,
+                                        workflow = bunkWorkflow)
+        bunkSubscription.create()
+
+        jobGroup1 = JobGroup(subscription = bunkSubscription)
+        jobGroup1.create()
+        newJob = Job()
+        newJob.create(jobGroup1)
+        newJob.changeStatus("COMPLETE")
+        jobGroup2 = JobGroup(subscription = bunkSubscription)
+        jobGroup2.create()
+        newJob = Job()
+        newJob.create(jobGroup2)
+        newJob.changeStatus("COMPLETE")        
+        jobGroup3 = JobGroup(subscription = bunkSubscription)
+        jobGroup3.create()
+        newJob = Job()
+        newJob.create(jobGroup3)
+        newJob.changeStatus("COMPLETE")
+        jobGroup4 = JobGroup(subscription = bunkSubscription)
+        jobGroup4.create()
+        newJob = Job()
+        newJob.create(jobGroup4)        
+
+        file1 = File(lfn = "file1", size = 1024, events = 1024, first_event = 0)
+        file1.addRun(Run(1, *[45]))
+        file2 = File(lfn = "file2", size = 1024, events = 1024, first_event = 1024)
+        file2.addRun(Run(1, *[45]))
+        file3 = File(lfn = "file3", size = 1024, events = 1024, first_event = 2048)
+        file3.addRun(Run(1, *[45]))
+        file4 = File(lfn = "file4", size = 1024, events = 1024, first_event = 3072)        
+        file4.addRun(Run(1, *[45]))
+
+        fileA = File(lfn = "fileA", size = 1024, events = 1024, first_event = 0)
+        fileA.addRun(Run(1, *[46]))
+        fileB = File(lfn = "fileB", size = 1024, events = 1024, first_event = 1024)
+        fileB.addRun(Run(1, *[46]))
+        fileC = File(lfn = "fileC", size = 1024, events = 1024, first_event = 2048)
+        fileC.addRun(Run(1, *[46]))
+
+        fileI = File(lfn = "fileI", size = 1024, events = 1024, first_event = 0)
+        fileI.addRun(Run(2, *[46]))
+        fileII = File(lfn = "fileII", size = 1024, events = 1024, first_event = 1024)
+        fileII.addRun(Run(2, *[46]))
+        fileIII = File(lfn = "fileIII", size = 1024, events = 1024, first_event = 2048)
+        fileIII.addRun(Run(2, *[46]))
+        fileIV = File(lfn = "fileIV", size = 1024, events = 1024, first_event = 3072)        
+        fileIV.addRun(Run(2, *[46]))        
+
+        fileX = File(lfn = "badFileA", size = 1024, events = 1024, first_event = 0)
+        fileX.addRun(Run(1, *[47]))
+        fileY = File(lfn = "badFileB", size = 1024, events = 1024, first_event = 1024)
+        fileY.addRun(Run(1, *[47]))
+        fileZ = File(lfn = "badFileC", size = 1024, events = 1024, first_event = 2048)
+        fileZ.addRun(Run(1, *[47]))
+
+        jobGroup1.groupoutput.addFile(file1)
+        jobGroup1.groupoutput.addFile(file2)
+        jobGroup1.groupoutput.addFile(file3)
+        jobGroup1.groupoutput.addFile(file4)        
+        jobGroup1.groupoutput.commit()
+
+        jobGroup2.groupoutput.addFile(fileA)
+        jobGroup2.groupoutput.addFile(fileB)
+        jobGroup2.groupoutput.addFile(fileC)
+        jobGroup2.groupoutput.commit()
+
+        jobGroup3.groupoutput.addFile(fileI)
+        jobGroup3.groupoutput.addFile(fileII)
+        jobGroup3.groupoutput.addFile(fileIII)
+        jobGroup3.groupoutput.addFile(fileIV)        
+        jobGroup3.groupoutput.commit()                
+
+        jobGroup4.groupoutput.addFile(fileX)
+        jobGroup4.groupoutput.addFile(fileY)
+        jobGroup4.groupoutput.addFile(fileZ)
+        jobGroup4.groupoutput.commit()
+
+        self.mergeFileset = Fileset(name = "mergeFileset")
+        self.mergeFileset.create()
+
+        mergeWorkflow = Workflow(name = "mergeWorkflow", spec = "bunk2",
+                                 owner = "Steve")
+        mergeWorkflow.create()
+        
+        self.mergeSubscription = Subscription(fileset = self.mergeFileset,
+                                              workflow = mergeWorkflow,
+                                              split_algo = "SplitFileBased")
+        self.mergeSubscription.create()
+        
+        self.mergeFileset.addFile(file1)
+        self.mergeFileset.addFile(file2)
+        self.mergeFileset.addFile(file3)
+        self.mergeFileset.addFile(file4)
+        self.mergeFileset.addFile(fileA)
+        self.mergeFileset.addFile(fileB)
+        self.mergeFileset.addFile(fileC)
+        self.mergeFileset.addFile(fileI)
+        self.mergeFileset.addFile(fileII)
+        self.mergeFileset.addFile(fileIII)
+        self.mergeFileset.addFile(fileIV)
+        self.mergeFileset.addFile(fileX)
+        self.mergeFileset.addFile(fileY)
+        self.mergeFileset.addFile(fileZ)        
+        self.mergeFileset.commit()
+
+        return
+
+    def testSplitAlgo(self):
+        """
+        _testSplitAlgo_
+
+        Run the SplitFileBased splitting algorithm over the data created in
+        the merge subscription.  This should produce three job groups each
+        containing one job.  The files in the job should be ordered correctly.
+        """
+        self.stuffWMBS()
+
+        splitter = SplitterFactory()
+        jobFactory = splitter(package = "WMCore.WMBS",
+                              subscription = self.mergeSubscription)
+
+        result = jobFactory()
+
+        assert len(result) == 3, \
+               "ERROR: Wrong number of job groups returned."
+
+        for jobGroup in result:
+            assert len(jobGroup.jobs) == 1, \
+                   "ERROR: One job should be in a job group."
+
+        goldenFilesA = ["file1", "file2", "file3", "file4"]
+        goldenFilesB = ["fileA", "fileB", "fileC"]
+        goldenFilesC = ["fileI", "fileII", "fileIII", "fileIV"]
+
+        for jobGroup in result:
+            jobFiles = jobGroup.jobs.pop().getFiles()
+            
+            if jobFiles[0]["lfn"] in goldenFilesA:
+                goldenFiles = goldenFilesA
+            elif jobFiles[0]["lfn"] in goldenFilesB:
+                goldenFiles = goldenFilesB
+            else:
+                goldenFiles = goldenFilesC
+
+            currentRun = 0
+            currentLumi = 0
+            currentEvent = 0
+            for file in jobFiles:
+                file.loadData()
+                assert file["lfn"] in goldenFiles, \
+                       "ERROR: Unknown file in merge jobs."
+
+                goldenFiles.remove(file["lfn"])
+
+            fileRun = list(file["runs"])[0].run
+            fileLumi = min(list(file["runs"])[0])
+            fileEvent = file["first_event"]
+
+            if currentRun == 0:
+                currentRun = fileRun
+                currentLumi = fileLumi
+                currentEvent = fileEvent
+                continue
+
+            assert fileRun >= currentRun, \
+                   "ERROR: Files not sorted by run."
+
+            if fileRun == currentRun:
+                assert fileLumi >= currentLumi, \
+                       "ERROR: Files not ordered by lumi"
+
+                if fileLumi == currentLumi:
+                    assert fileEvent > currentEvent, \
+                           "ERROR: Files not ordered by first event"
+
+            currentRun = fileRun
+            currentLumi = fileLumi
+            currentEvent = fileEvent
+
+        assert len(goldenFilesA) == 0 and len(goldenFilesB) == 0 and \
+               len(goldenFilesC) == 0, \
+               "ERROR: Files missing from merge jobs."
+
+        return
+
+if __name__ == '__main__':
+    unittest.main()

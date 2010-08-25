793fd6fc9f4460a7ed543210d5e5ccf4b4ac9c6f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:27:11 +0000

    Added destroy module for ThreadPool Oracle
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4334 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/ThreadPool/Oracle/Destroy.py
@@ -0,0 +1,34 @@
+#/usr/bin/env python2.4
+"""
+_Destroy_
+
+"""
+
+__revision__ = "$Id: Destroy.py,v 1.1 2009/06/16 14:42:53 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.ThreadPool.MySQL.Destroy import Destroy as MySQLDestroy
+from WMCore.ThreadPool.Oracle.Create import Create
+
+class Destroy(MySQLDestroy):    
+    def __init__(self, logger = None, dbi = None):
+        """
+        _init_
+
+        Call the base class's constructor and add all necessary tables for 
+        deletion,
+        """        
+        MySQLDestroy.__init__(self, logger, dbi)
+        
+        j=50
+        for i in Create.sequence_tables:
+            seqname = i
+            self.create["%s%s" % (j, seqname)] = \
+                           "DROP SEQUENCE %s"  % seqname
+
+
+        #for i in Create.trigger_tables:
+        #    seqname = i
+        #    self.create["%s%s" % (j, seqname)] = \
+        #                   "DROP TRIGGER %s"  % seqname 
+

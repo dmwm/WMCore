64eb6b233ddb3da83737a4c6247f6d9b4854470e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:26:14 +0000

    Add hacks for the broken python at FNAL and hacks for finding FWJRs from various steps.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6915 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMComponent_t/JobSubmitter_t/submit.sh
+++ test/python/WMComponent_t/JobSubmitter_t/submit.sh
@@ -7,14 +7,25 @@ INDEX=$2
 ls
 . /uscmst1/prod/sw/cms/slc5_amd64_gcc434/external/python/2.6.4-cms2/etc/profile.d/init.sh 
 . /uscmst1/prod/sw/cms/setup/shrc prod
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/uscmst1/prod/sw/cms/slc5_amd64_gcc434/external/openssl/0.9.7m/lib" > ~/.bashrc
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/uscmst1/prod/sw/cms/slc5_amd64_gcc434/external/openssl/0.9.7m/lib
+# BADPYTHON
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/uscmst1/prod/sw/cms/slc5_amd64_gcc434/external/openssl/0.9.7m/lib:/uscmst1/prod/sw/cms/slc5_amd64_gcc434/external/bz2lib/1.0.5/lib
 python2.6 Unpacker.py --sandbox=$SANDBOX --package=JobPackage.pkl --index=$INDEX
 
 cd job
 export PYTHONPATH=$PYTHONPATH:$PWD
 python2.6 WMCore/WMRuntime/Startup.py
-cp WMTaskSpace/Report.pkl ../
+if [ -f WMTaskSpace/cmsRun1/Report.pkl ]
+then
+  cp WMTaskSpace/cmsRun1/Report.pkl ../
+fi
+if [ -f WMTaskSpace/mergeReco/Report.pkl ]
+then
+  cp WMTaskSpace/mergeReco/Report.pkl ../
+fi
+if [ -f WMTaskSpace/mergeAlcaReco/Report.pkl ]
+then
+  cp WMTaskSpace/mergeAlcaReco/Report.pkl ../
+fi
 
 sleep 90
 

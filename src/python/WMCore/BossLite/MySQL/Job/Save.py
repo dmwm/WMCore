07129107539c2cbea5e9372b2a72d1ac87124476
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:58:42 +0000

    Lists converted to strings before save/update the object into the database.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7357 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/BossLite/MySQL/Job/Save.py
+++ src/python/WMCore/BossLite/MySQL/Job/Save.py
@@ -6,12 +6,14 @@ MySQL implementation of BossLite.Jobs.Save
 """
 
 __all__ = []
-__revision__ = "$Id: Save.py,v 1.1 2010/03/30 10:13:03 mnorman Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Save.py,v 1.2 2010/05/09 14:56:12 spigafi Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
+from WMCore.BossLite.Common.System import strToList, listToStr
 
 class Save(DBFormatter):
+    # WHERE id = :id
     sql = """UPDATE bl_job SET job_id = :jobId, task_id = :taskId,
                 name = :name, executable = :executable, events = :events,
                 arguments = :arguments, stdin = :standardInput,
@@ -19,10 +21,35 @@ class Save(DBFormatter):
                 input_files = :inputFiles, output_files = :outputFiles,
                 dls_destination = :dlsDestination, submission_number = :submissionNumber,
                 closed = :closed
-                WHERE id = :id
+                WHERE job_id = :jobId AND task_id = :taskId
                 """
-
-
+    
+    def preFormat(self, entry):
+        """
+        This method maps database fields with object dictionary and 
+        it translate python List and timestamps in well formatted string
+        """
+        
+        result = {}  
+        
+        result['id']               = entry['id']
+        result['jobId']            = entry['jobId']
+        result['taskId']           = entry['taskId']
+        result['name']             = entry['name']
+        result['executable']       = entry['executable']
+        result['events']           = entry['events']
+        result['arguments']        = entry['arguments']
+        result['standardInput']    = entry['standardInput']
+        result['standardOutput']   = entry['standardOutput']
+        result['standardError']    = entry['standardError']
+        result['inputFiles']       = listToStr(entry['inputFiles'])
+        result['outputFiles']      = listToStr(entry['outputFiles'])
+        result['dlsDestination']   = listToStr(entry['dlsDestination'])
+        result['submissionNumber'] = entry['submissionNumber']
+        result['closed']           = entry['closed']
+            
+        return result
+    
     def execute(self, binds, conn = None, transaction = False):
         """
         This assumes that you are passing in binds in the same format
@@ -30,7 +57,11 @@ class Save(DBFormatter):
         too long a function for me to want to write in Perugia while
         parsing the binds
         """
-        self.dbi.processData(self.sql, binds, conn = conn,
-                             transaction = transaction)
+        
+        ppBinds = self.preFormat(binds)
+        
+        self.dbi.processData(self.sql, ppBinds, conn = conn,
+                         transaction = transaction)
+        
         return
 

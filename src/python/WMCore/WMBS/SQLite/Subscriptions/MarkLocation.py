215a27e3ab199af3160341225da0b2ba47736ead
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:47:07 +0000

    Inherit from the MySQL version of this object.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6380 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Subscriptions/MarkLocation.py
+++ src/python/WMCore/WMBS/SQLite/Subscriptions/MarkLocation.py
@@ -3,38 +3,12 @@
 _MarkLocation_
 
 SQLite implementation of Subscription.MarkLocation
-
-Insert a record to wmbs_subscription_location
-
-CREATE TABLE wmbs_subscription_location (
-             subscription     INT(11)      NOT NULL,
-             location         INT(11)      NOT NULL,
-             valid            BOOLEAN      NOT NULL DEFAULT TRUE,
-             FOREIGN KEY(subscription)  REFERENCES wmbs_subscription(id)
-               ON DELETE CASCADE,
-             FOREIGN KEY(location)     REFERENCES wmbs_location(id)
-               ON DELETE CASCADE)
 """
-__all__ = []
-__revision__ = "$Id: MarkLocation.py,v 1.4 2009/05/09 11:42:27 sfoulkes Exp $"
-__version__ = "$Revision: 1.4 $"
 
-from WMCore.Database.DBFormatter import DBFormatter
+__revision__ = "$Id: MarkLocation.py,v 1.5 2010/02/09 17:44:04 sfoulkes Exp $"
+__version__ = "$Revision: 1.5 $"
 
-class MarkLocation(DBFormatter):
-    sql = """insert into wmbs_subscription_location 
-            (subscription, location, valid)
-        values (
-            :subscription, 
-            (select id from wmbs_location where site_name = :location),
-            :valid
-        )"""
-    
-    def execute(self, subscription=None, location=None, valid = None, 
-                conn = None, transaction = False):
-        
-        result = self.dbi.processData(self.sql, 
-                                      self.getBinds(subscription=subscription, 
-                                              location=location, valid=valid), 
-                         conn = conn, transaction = transaction)
-        return self.format(result)
+from WMCore.WMBS.MySQL.Subscriptions.MarkLocation import MarkLocation as MySQLMarkLocation
+
+class MarkLocation(MySQLMarkLocation):
+    pass

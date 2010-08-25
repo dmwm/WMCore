74cb19e061374e659feb767a1d1eb51668aacfe1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:33:21 +0000

    cleaned this up. these tests didn't actually have any assertions in them...
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5324 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMComponent_t/HTTPFrontend_t/HTTPFrontend_t.py
+++ test/python/WMComponent_t/HTTPFrontend_t/HTTPFrontend_t.py
@@ -1,25 +1,77 @@
-from MessageService.MessageService import MessageService
-from WMComponent_t.HTTPFrontend_t.MsgServiceApp import MsgServiceApp
-from WMCore.Database.Transaction import Transaction
+#from MessageService.MessageService import MessageService
+#from WMComponent_t.HTTPFrontend_t.MsgServiceApp import MsgServiceApp
+
+import os
+
+import unittest
 import threading
 
-msApp = MsgServiceApp("HTTPFrontend_t")
-ms = msApp.msgService()
-print "PUBLISH"
-myThread = threading.currentThread()
-myThread.transaction = Transaction(myThread.dbi)
-myThread.transaction.begin()
-ms.subscribeTo("HTTPFrontendStart")
 
-ms.purgeMessages()
-ms.publish({'name' : 'HTTPFrontendStart', 'payload' : 'GO', 'delay' : '00:00:00', 'instant' : True})
-myThread.transaction.commit()
-ms.finish()
+from WMCore.WMFactory import WMFactory
+from WMCore.Database.Transaction import Transaction
 
-myThread.transaction.begin()
-print str(ms.pendingMsgs())
-myThread.transaction.commit()
+class MsgServiceApp(unittest.TestCase):
+    """
+    TestCase for modules
+    """
+    def __init__(self, name):
+        self.name = name
+        self._maxMessage = 10
 
 
-print ms.get(False)
+
+    def setUp(self):
+        """
+        setup for test.
+        """
+        
+        self.testInit.setLogging()
+        self.testInit.setDatabaseConnection()
+        self.testInit.setSchema(customModules = 
+                                ["WMCore.ThreadPool",
+                                 "WMCore.MsgService",
+                                 ],
+                                useDefault = False)
+        self.config = self.testInit.getConfiguration(os.path.join(os.getenv('WMCOREBASE'), \
+                        'src/python/WMComponent/ErrorHandler/DefaultConfig.py'))
+
+    def msgService(self):
+        """ returns a MsgService instance """
+        # load a message service as we want to check if total failure
+        # messages are returned
+
+        myThread = threading.currentThread()
+        myThread.transaction = Transaction(myThread.dbi)
+        myThread.transaction.begin()
+        factory = WMFactory('msgService', 'WMCore.MsgService.'+myThread.dialect)
+        msgService =  factory.loadObject("MsgService")
+        msgService.registerAs(self.name)
+        # subscribe to what we want to test.
+        myThread.transaction.commit()
+        return msgService
+
+    def tearDown(self):
+        """
+        Database deletion
+        """
+        self.testInit.clearDatabase()
+
+    def testB(self):
+        ms = self.msgService()
+        
+        myThread = threading.currentThread()
+        myThread.transaction = Transaction(myThread.dbi)
+        myThread.transaction.begin()
+        ms.subscribeTo("HTTPFrontendStart")
+        
+        ms.purgeMessages()
+        ms.publish({'name' : 'HTTPFrontendStart', 'payload' : 'GO', 'delay' : '00:00:00', 'instant' : True})
+        myThread.transaction.commit()
+        ms.finish()
+        
+        myThread.transaction.begin()
+        #print str(ms.pendingMsgs())
+        myThread.transaction.commit()
+        
+        
 

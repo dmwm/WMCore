b270edfd41ae106258797411ac95534e9c7f7511
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:41:49 +0000

    fixing typo
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4554 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/ThreadPool/SQLite/Destroy.py
+++ src/python/WMCore/ThreadPool/SQLite/Destroy.py
@@ -6,11 +6,11 @@ Sqlite implementation of Job.ChangeState
 """
 
 __all__ = []
-__revision__ = "$Id: Destroy.py,v 1.1 2009/07/15 21:46:27 meloam Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Destroy.py,v 1.2 2009/07/15 21:47:13 meloam Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
-from WMCore.ThreadPool.Mysql.Destroy import Destroy as BaseDAO
+from WMCore.ThreadPool.MySQL.Destroy import Destroy as BaseDAO
 
 class Destroy(BaseDAO):
     pass

b5fcd9764b9b1df439823f8a30dc5a4f925c9c26
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 05:15:36 +0000

    This is the long-awaited (by some) upgrade to the JobCreator, package codename: Taizong
    It deals with the problem of the JobCreator having to pause while working through a large
    chunk of jobs by using generators to iterate through SQL, pulling out a chunk of jobs with
    each iteration, and then running those all the way through.  This should mean that instead
    of everything holding while it processes all jobs, it should slowly move jobs forward as
    it proceeds.  This requires the JobCreator to handle subscriptions in an iterative manner.
    
    In order to specify the number of files to be pulled out at a time in the JobSplitting,
    you want to work with the fileLoadLimit config value for the JobSubmitter.  By default it
    pulls out a hundred records at a time.  For a particular workflow this can be overridden
    by messing with the workflow splitting params, although I'm not sure you want to do this.
    
    It should work with everything, but is not well tested.  Use at your own risk.
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8364 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/JobCreator/JobCreatorWorker.py
+++ src/python/WMComponent/JobCreator/JobCreatorWorker.py
@@ -11,13 +11,19 @@
 The JobCreator Poller for the JSM
 """
 __all__ = []
-__revision__ = "$Id: JobCreatorWorker.py,v 1.15 2010/07/08 14:13:07 mnorman Exp $"
-__version__ = "$Revision: 1.15 $"
+__revision__ = "$Id: JobCreatorWorker.py,v 1.16 2010/08/02 21:14:22 mnorman Exp $"
+__version__ = "$Revision: 1.16 $"
 
 import threading
 import logging
 import os
 import os.path
+
+
+import gc
+import sys
+import objgraph
+import pdb
 
 import cPickle
 
@@ -33,6 +39,29 @@ from WMCore.JobStateMachine.ChangeState                 import ChangeState
 from WMCore.WMSpec.Makers.Interface.CreateWorkArea      import CreateWorkArea
 
 from WMCore.Agent.Configuration import Configuration
+
+
+def _VmB(VmKey):
+    '''Private.
+    '''
+    _proc_status = '/proc/%d/status' % os.getpid()
+    _scale = {'kB': 1024.0, 'mB': 1024.0*1024.0,
+              'KB': 1024.0, 'MB': 1024.0*1024.0}
+    
+    # get pseudo file  /proc/<pid>/status
+    try:
+        t = open(_proc_status)
+        v = t.read()
+        t.close()
+    except:
+        return 0.0  # non-Linux?
+    # get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
+    i = v.index(VmKey)
+    v = v[i:].split(None, 3)  # whitespace
+    if len(v) < 3:
+        return 0.0  # invalid format?
+    # convert Vm value to bytes
+    return float(v[1]) * _scale[v[2]]
 
 def retrieveWMSpec(subscription):
     """
@@ -80,6 +109,20 @@ def retrieveJobSplitParams(wmWorkload, task):
 
 
 
+def runSplitter(jobFactory, splitParams):
+        """
+        _runSplitter_
+
+        Run the jobSplitting as a coroutine method, yielding values as required
+        """
+
+        groups = ['test']
+        while groups != []:
+            groups = jobFactory(**splitParams)
+            yield groups
+
+
+
 
     
 
@@ -105,13 +148,6 @@ class JobCreatorWorker:
         # WMCore splitter factory for splitting up jobs.
         self.splitterFactory = SplitterFactory()
 
-        #Dictionaries to be filled later
-        self.sites         = {}
-        self.slots         = {}
-        self.workflows     = {}
-        self.subscriptions = {}
-
-
         config = Configuration()
         config.section_("JobStateMachine")
         config.JobStateMachine.couchurl      = configDict["couchURL"]
@@ -123,6 +159,7 @@ class JobCreatorWorker:
         #Variables
         self.jobCacheDir    = configDict['jobCacheDir']
         self.defaultJobType = configDict['defaultJobType']
+        self.limit          = configDict.get('fileLoadLimit', 500)
 
 
         
@@ -142,6 +179,7 @@ class JobCreatorWorker:
         myThread = threading.currentThread()
 
         for entry in parameters:
+            # This retrieves a single subscription
             subscriptionID = entry.get('subscription')
 
             if subscriptionID == -1:
@@ -157,7 +195,14 @@ class JobCreatorWorker:
             wmbsSubscription.load()
             wmbsSubscription["workflow"].load()
             workflow         = wmbsSubscription["workflow"]
+
+            #pdb.set_trace()
+            
             wmWorkload       = retrieveWMSpec(wmbsSubscription)
+
+
+
+            
 
             if not workflow.task or not wmWorkload:
                 wmTask = None
@@ -175,53 +220,134 @@ class JobCreatorWorker:
             # My hope is that the job factory is smart enough only to split un-split jobs
             wmbsJobFactory = self.splitterFactory(package = "WMCore.WMBS",
                                                   subscription = wmbsSubscription,
-                                                  generators=seederList)
+                                                  generators=seederList,
+                                                  limit = self.limit)
             splitParams = retrieveJobSplitParams(wmWorkload, workflow.task)
             logging.debug("Split Params: %s" % splitParams)
-            wmbsJobGroups = wmbsJobFactory(**splitParams)
-            #logging.debug("Job Groups %s" % wmbsJobGroups)
-            logging.info("Have jobGroups")
-            
-            # Now we get to find out what job they are.
-            countJobs = self.daoFactory(classname = "Jobs.GetNumberOfJobsPerWorkflow")
-            jobNumber = countJobs.execute(workflow = workflow.id)
-            
+
+            continueSubscription = True
+            #wmbsJobGroups        = []
             myThread.transaction.commit()
+
+            # Turn on the jobFactory
+            wmbsJobFactory.open()
+
+            # Create a function to hold it
+            jobSplittingFunction = runSplitter(jobFactory = wmbsJobFactory,
+                                               splitParams = splitParams)
+
+            while continueSubscription:
+                # This loop runs over the jobFactory,
+                # using yield statements and a pre-existing proxy to
+                # generate and process new jobs
+
+                # First we need the jobs.
+                
+                try:
+                    wmbsJobGroups = jobSplittingFunction.next()
+                    logging.error("Retrieved %i jobGroups from jobSplitter" % (len(wmbsJobGroups)))
+                except StopIteration:
+                    # If you receive a stopIteration, we're done
+                    logging.error("Completed iteration over subscription %i" % (subscriptionID))
+                    continueSubscription = False
+                    continue
+
+                # wmbsJobGroups = wmbsJobFactory(**splitParams)
+                # logging.debug("Job Groups %s" % wmbsJobGroups)
+                logging.info("Have jobGroups")
             
-            for wmbsJobGroup in wmbsJobGroups:
+                # Now we get to find out what job they are.
+                countJobs = self.daoFactory(classname = "Jobs.GetNumberOfJobsPerWorkflow")
+                jobNumber = countJobs.execute(workflow = workflow.id)
                 
-                # Create a directory
-                self.createWorkArea.processJobs(jobGroup = wmbsJobGroup,
-                                                startDir = self.jobCacheDir,
-                                                workflow = workflow,
-                                                wmWorkload = wmWorkload)
                 
-                for job in wmbsJobGroup.jobs:
-                    jobNumber += 1
-                    # We better save the whole job
-                    # First, add the necessary components
-                    if wmTask:
-                        # If we managed to load the task,
-                        # so the url should be valid
-                        job['spec']    = workflow.spec
-                        job['sandbox'] = wmTask.data.input.sandbox
-                        job['task']    = wmTask.getPathName()
-                    job['counter']  = jobNumber
-                    cacheDir = job.getCache()
-                    job['cache_dir'] = cacheDir
-                    output = open(os.path.join(cacheDir, 'job.pkl'),'w')
-                    cPickle.dump(job, output)
-                    output.close()
+            
+                for wmbsJobGroup in wmbsJobGroups:
+                
+                    # Create a directory
+                    self.createWorkArea.processJobs(jobGroup = wmbsJobGroup,
+                                                    startDir = self.jobCacheDir,
+                                                    workflow = workflow,
+                                                    wmWorkload = wmWorkload)
+                    
+                    for job in wmbsJobGroup.jobs:
+                        jobNumber += 1
+                        self.saveJob(job = job, workflow = workflow,
+                                     wmTask = wmTask, jobNumber = jobNumber)
+                        ## We better save the whole job
+                        ## First, add the necessary components
+                        #if wmTask:
+                        #    # If we managed to load the task,
+                        #    # so the url should be valid
+                        #    job['spec']    = workflow.spec
+                        #    job['sandbox'] = wmTask.data.input.sandbox
+                        #    job['task']    = wmTask.getPathName()
+                        #job['counter']  = jobNumber
+                        #cacheDir = job.getCache()
+                        #job['cache_dir'] = cacheDir
+                        #output = open(os.path.join(cacheDir, 'job.pkl'),'w')
+                        #cPickle.dump(job, output)
+                        #output.close()
 
-                self.createJobGroup(wmbsJobGroup)
+                    self.createJobGroup(wmbsJobGroup)
 
-                logging.info("Finished call for jobGroup %i" \
-                             %(wmbsJobGroup.exists()))
+                    logging.info("Finished call for jobGroup %i" \
+                                 %(wmbsJobGroup.exists()))
+
+
+                # END: while loop over jobSplitter
+
+
+
+            #wmbsJobFactory.close()
+            #logging.error("Ending a loop")
+            #logging.error(gc.get_count())
+            #logging.error(gc.get_referrers())
+            #logging.error(objgraph.show_most_common_types(limit=50))
+            #logging.error("About to get memory references")
+            #logging.error(_VmB('VmSize:'))
+            #logging.error(_VmB('VmRSS:'))
+            #logging.error(_VmB('VmStk:'))
+            #pdb.set_trace()
+            wmbsJobGroups  = None
+            wmTask         = None
+            wmWorkload     = None
+            splitParams    = None
+            wmbsJobFactory = None
+            gc.collect()
+            #logging.error("About to get memory references: Part 2")
+            #logging.error(_VmB('VmSize:'))
+            #logging.error(_VmB('VmRSS:'))
+            #logging.error(_VmB('VmStk:'))
+            #logging.error(gc.get_count())
+
 
         return parameters
 
 
+    def saveJob(self, job, workflow, wmTask = None, jobNumber = 0):
+        """
+        _saveJob_
 
+        Actually do the mechanics of saving the job to a pickle file
+        """
+
+
+        if wmTask:
+            # If we managed to load the task,
+            # so the url should be valid
+            job['spec']    = workflow.spec
+            job['sandbox'] = wmTask.data.input.sandbox
+            job['task']    = wmTask.getPathName()
+        job['counter']  = jobNumber
+        cacheDir = job.getCache()
+        job['cache_dir'] = cacheDir
+        output = open(os.path.join(cacheDir, 'job.pkl'),'w')
+        cPickle.dump(job, output)
+        output.close()
+
+
+        return
 
     
 
@@ -247,6 +373,9 @@ class JobCreatorWorker:
 
 
         return
+
+
+    
 
 
 

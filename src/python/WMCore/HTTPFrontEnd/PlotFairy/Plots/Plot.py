c16ef503f41644f7d1d5b5e853005fa9fe09cae7
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:28:11 +0000

    Add base class for plot plugins.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5250 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/HTTPFrontEnd/PlotFairy/Plots/Plot.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+'''
+The Plot class is a base class for PlotFairy plots to inherit from. Authors of
+new plots should override the plot(self, data) method. Plots should be 
+instantiated via a factory, and be stateless.
+'''
+
+class Plot(object):
+    def __call__(self, input):
+        '''
+        Plot objects will be instantiated either directly or via a factory. 
+        They will then be called to create the plot for some given input data.
+        We use the __call__ method as a way of enforcing good behaviour, and 
+        hopefully minimising memory leakage. 
+        '''
+        plot = self.plot(input)
+        matplotlib.pyplot.close()
+        return plot
+        
+    def plot(self):
+        '''
+        Create the matplotlib object and return it - override!
+        '''
+        return None

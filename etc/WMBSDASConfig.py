c20954690645a3c57a3236b2f32fbdfee1da38ae
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:48:43 +0000

    Updated to work with wmcoreD.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6402 4525493e-7705-40b1-a816-d608a930855b

--- etc/WMBSDASConfig.py
+++ etc/WMBSDASConfig.py
@@ -10,10 +10,24 @@ import WMCore.WMInit
 
 config = Configuration()
 
+config.section_("Agent")
+config.Agent.hostName = "cmssrv52.fnal.gov"
+config.Agent.contact = "sfoulkes@fnal.gov"
+config.Agent.teamName = "DMWM"
+config.Agent.agentName = "ReRecoDOMINATOR"
+
+config.section_("General")
+config.General.workDir = "/home/sfoulkes/WMAgent/work"
+
+config.section_("CoreDatabase")
+config.CoreDatabase.socket = "/opt/MySQL.5.0/var/lib/mysql/mysql.sock"
+config.CoreDatabase.connectUrl = "mysql://sfoulkes:@localhost/ProdAgentDB_sfoulkes"
+
 config.component_("Webtools")
 config.Webtools.application = "WMBSMonitoring"
-config.Webtools.host = "cmssrv52.fnal.gov"
-config.Webtools.port = 8085
+config.Webtools.host = "cmssrv18.fnal.gov"
+config.Webtools.port = 8087
+config.Webtools.componentDir = config.General.workDir + "/Webtools"
 
 config.component_("WMBSMonitoring")
 config.WMBSMonitoring.templates = WMCore.WMInit.getWMBASE() + '/src/templates/WMCore/WebTools'
@@ -24,16 +38,14 @@ config.WMBSMonitoring.instance = "ReReco WMAGENT"
 config.WMBSMonitoring.couchURL = "http://cmssrv52:5984/_utils/document.html?tier1_skimming/"
 
 config.WMBSMonitoring.section_('views')
-# These are all the active pages that Root.py should instantiate
-active = config.WMBSMonitoring.views.section_('active')
-wmbs = active.section_('wmbs')
-# The class to load for this view/page
-wmbs.object = 'WMCore.WebTools.RESTApi'
-wmbs.templates = WMCore.WMInit.getWMBASE() + '/src/templates/WMCore/WebTools/'
-wmbs.database = 'mysql://sfoulkes:@localhost/WMAgentDB_sfoulkes'
-wmbs.dbsocket = '/opt/MySQL-5.1/var/lib/mysql/mysql.sock'
+config.WMBSMonitoring.views.section_('active')
+config.WMBSMonitoring.views.active.section_('wmbs')
+config.WMBSMonitoring.views.active.wmbs.section_('model')
+config.WMBSMonitoring.views.active.wmbs.section_('formatter')
+config.WMBSMonitoring.views.active.wmbs.object = 'WMCore.WebTools.RESTApi'
+config.WMBSMonitoring.views.active.wmbs.templates = WMCore.WMInit.getWMBASE() + '/src/templates/WMCore/WebTools/'
+config.WMBSMonitoring.views.active.wmbs.database = 'mysql://sfoulkes:@localhost/WMAgentDB_sfoulkes'
+config.WMBSMonitoring.views.active.wmbs.dbsocket = '/opt/MySQL.5.0/var/lib/mysql/mysql.sock'
+config.WMBSMonitoring.views.active.wmbs.model.object = 'WMCore.HTTPFrontEnd.WMBS.WMBSRESTModel'
+config.WMBSMonitoring.views.active.wmbs.formatter.object = 'WMCore.WebTools.DASRESTFormatter'
 
-wmbs.section_('model')
-wmbs.model.object = 'WMCore.HTTPFrontEnd.WMBS.WMBSRESTModel'
-wmbs.section_('formatter')
-wmbs.formatter.object = 'WMCore.WebTools.DASRESTFormatter'

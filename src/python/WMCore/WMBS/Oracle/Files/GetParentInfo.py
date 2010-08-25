e8d99263036454169b047c0b528ff398f7055519
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:44:46 +0000

    Use the MySQL version of this query.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7970 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Files/GetParentInfo.py
+++ src/python/WMCore/WMBS/Oracle/Files/GetParentInfo.py
@@ -5,39 +5,10 @@ _GetParentInfo_
 Oracle implementation of Files.GetParentInfo
 """
 
-__revision__ = "$Id: GetParentInfo.py,v 1.2 2009/12/23 17:49:36 sfoulkes Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: GetParentInfo.py,v 1.3 2010/06/28 19:06:36 sfoulkes Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.WMBS.MySQL.Files.GetParentInfo import GetParentInfo as GetParentInfoMySQL
 
 class GetParentInfo(GetParentInfoMySQL):
-    sql = """SELECT wmbs_file_details.id,
-                    wmbs_file_details.lfn,
-                    wmbs_file_details.merged,
-                    wmbs_file_gpdetails.lfn AS gplfn,
-                    wmbs_file_gpdetails.merged AS gpmerged,
-                    redneck_parent_output.output_fileset AS redneck_parent_fileset,
-                    redneck_child_output.output_fileset AS redneck_child_fileset
-                    FROM wmbs_file_details
-               INNER JOIN wmbs_file_parent ON
-                 wmbs_file_details.id = wmbs_file_parent.parent
-               LEFT OUTER JOIN wmbs_file_parent wmbs_file_gparent ON
-                 wmbs_file_parent.parent = wmbs_file_gparent.child
-               LEFT OUTER JOIN wmbs_file_details wmbs_file_gpdetails ON
-                 wmbs_file_gparent.parent = wmbs_file_gpdetails.id
-               INNER JOIN wmbs_fileset_files ON
-                 wmbs_file_parent.child = wmbs_fileset_files.fileid
-               INNER JOIN wmbs_workflow_output ON
-                 wmbs_fileset_files.fileset = wmbs_workflow_output.output_fileset
-               INNER JOIN wmbs_subscription ON
-                 wmbs_subscription.workflow = wmbs_workflow_output.workflow_id
-               LEFT OUTER JOIN wmbs_workflow_output wmbs_pworkflow_output ON
-                 wmbs_subscription.fileset = wmbs_pworkflow_output.output_fileset
-               LEFT OUTER JOIN wmbs_workflow_output redneck_parent_output ON
-                 wmbs_pworkflow_output.workflow_id = redneck_parent_output.workflow_id AND
-                 wmbs_pworkflow_output.output_parent = redneck_parent_output.output_identifier
-               LEFT OUTER JOIN wmbs_workflow_output redneck_child_output ON
-                 wmbs_pworkflow_output.workflow_id = redneck_child_output.workflow_id AND
-                 wmbs_pworkflow_output.output_identifier = redneck_child_output.output_parent
-               WHERE wmbs_file_parent.child =
-                 (SELECT id FROM wmbs_file_details WHERE lfn = :child_lfn)"""
+    pass

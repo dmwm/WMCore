dba34e47db150551b4c9edc587ee3c2a4b1f8d94
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 05:19:38 +0000

    Specialized DAO for LumiBased splitting
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8417 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/Oracle/Files/GetBulkRunLumi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+_GetBulkRunLumi_
+
+Oracle implementation of GetBulkRunLumi
+"""
+
+__revision__ = "$Id: GetBulkRunLumi.py,v 1.1 2010/08/05 16:29:32 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.WMBS.MySQL.Files.GetBulkRunLumi import GetBulkRunLumi as MySQLGetBulkRunLumi
+
+class GetBulkRunLumi(MySQLGetBulkRunLumi):
+    """
+    Oracle implementation
+
+
+    """

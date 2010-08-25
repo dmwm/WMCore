ac420fba55346aa08c3b8812f09c7578ab534982
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:58:51 +0000

    Adapted to load datasetAlgo pair instead of just dataset
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6545 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSUpload/Database/MySQL/FindUploadableDatasets.py
+++ src/python/WMComponent/DBSUpload/Database/MySQL/FindUploadableDatasets.py
@@ -5,8 +5,8 @@ _DBSUpload.FindUploadableDatasets_
 Find the datasets that have files that needs to be uploaded to DBS
 
 """
-__revision__ = "$Id: FindUploadableDatasets.py,v 1.7 2009/07/20 18:02:53 mnorman Exp $"
-__version__ = "$Revision: 1.7 $"
+__revision__ = "$Id: FindUploadableDatasets.py,v 1.8 2010/02/24 21:36:59 mnorman Exp $"
+__version__ = "$Revision: 1.8 $"
 __author__ = "anzar@fnal.gov"
 
 import threading
@@ -15,15 +15,22 @@ from WMCore.Database.DBFormatter import DBFormatter
 
 class FindUploadableDatasets(DBFormatter):
     
-    sql = """SELECT ds.id as ID, ds.Path as Path, ds.Algo as Algo, ds.AlgoInDBS as AlgoInDBS 
-    						FROM dbsbuffer_dataset ds WHERE UnMigratedFiles > 0"""
-    sql = """SELECT das.dataset_id as ID, ds.Path as Path, das.algo_id as Algo, das.in_dbs as in_dbs 
+
+    sql = """SELECT das.dataset_id AS ID, ds.Path as Path, das.algo_id as Algo, das.in_dbs as in_dbs,
+               da.in_dbs AS algo_in_dbs, das.id AS das_id,
+               (SELECT COUNT(*) FROM dbsbuffer_algo_dataset_assoc das2 WHERE das2.dataset_id = das.dataset_id AND das2.in_dbs = 1) AS dataset_in_dbs,
+               da.app_name AS ApplicationName, 
+               da.app_ver AS ApplicationVersion, 
+               da.app_fam AS ApplicationFamily, 
+               da.PSet_Hash as PSetHash,
+               da.Config_Content as PSetContent
              FROM dbsbuffer_algo_dataset_assoc AS das
              INNER JOIN dbsbuffer_dataset AS ds
-               ON das.dataset_id = ds.ID
-             WHERE das.ID IN (SELECT df.dataset_algo FROM dbsbuffer_file AS df WHERE df.status = 'NOTUPLOADED')
-    """
-    #sql = """SELECT ds.id as ID, ds.Path as Path, ds.Algo as Algo, ds.AlgoInDBS as AlgoInDBS FROM dbsbuffer_dataset ds """
+              ON das.dataset_id = ds.ID
+             INNER JOIN dbsbuffer_algo AS da
+              ON das.algo_id = da.ID
+             WHERE das.in_dbs = 0"""
+
     
     def __init__(self):
         myThread = threading.currentThread()
@@ -33,21 +40,33 @@ class FindUploadableDatasets(DBFormatter):
         ret=[]
         for r in results:
             entry={}
+            entry['Dataset']=long(r['id'])
             entry['ID']=long(r['id'])
             entry['Path']=r['path']
+            entry['DAS_ID'] = long(r['das_id'])
             if not r['algo'] == None:
                 entry['Algo'] = int(r['algo'])
             else:
                 entry['Algo'] = None
-            if not r['in_dbs'] == None:
-                entry['AlgoInDBS'] = int(r['in_dbs'])
+            if not r['algo_in_dbs'] == None:
+                entry['AlgoInDBS'] = int(r['algo_in_dbs'])
             else:
                 entry['AlgoInDBS'] = None
+            if int(r['dataset_in_dbs']) > 0:
+                entry['DatasetInDBS'] = True
+            else:
+                entry['DatasetInDBS'] = False
             path = r['path']
-            entry['PrimaryDataset']   = path.split('/')[1]
-            entry['ProcessedDataset'] = path.split('/')[2]
-            entry['DataTier']         = path.split('/')[3]
+            entry['PrimaryDataset']     = path.split('/')[1]
+            entry['ProcessedDataset']   = path.split('/')[2]
+            entry['DataTier']           = path.split('/')[3]
+            entry['ApplicationName']    = r['applicationname']
+            entry['ApplicationVersion'] = r['applicationversion']
+            entry['ApplicationFamily']  = r['applicationfamily']
+            entry['PSetHash']           = r['psethash']
+            entry['PSetContent']        = r['psetcontent']
             ret.append(entry)
+
         return ret
  
     def execute(self, conn=None, transaction = False):
--- src/python/WMComponent/DBSUpload/Database/Oracle/FindUploadableDatasets.py
+++ src/python/WMComponent/DBSUpload/Database/Oracle/FindUploadableDatasets.py
@@ -5,8 +5,8 @@ _DBSUpload.FindUploadableDatasets_
 Find the datasets that have files that needs to be uploaded to DBS
 
 """
-__revision__ = "$Id: FindUploadableDatasets.py,v 1.2 2009/07/20 18:07:26 mnorman Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: FindUploadableDatasets.py,v 1.3 2010/02/24 21:36:59 mnorman Exp $"
+__version__ = "$Revision: 1.3 $"
 __author__ = "anzar@fnal.gov"
 
 from WMComponent.DBSUpload.Database.MySQL.FindUploadableDatasets import FindUploadableDatasets as MySQLFindUploadableDatasets
@@ -17,10 +17,3 @@ class FindUploadableDatasets(MySQLFindUploadableDatasets):
 Oracle implementation for finding datasets that have files that need to be uploaded into DBS
     """
 
-
-    sql = """SELECT das.dataset_id as ID, ds.Path as Path, das.algo_id as Algo, das.in_dbs as in_dbs 
-             FROM dbsbuffer_algo_dataset_assoc das
-             INNER JOIN dbsbuffer_dataset ds
-               ON das.dataset_id = ds.ID
-             WHERE das.ID IN (SELECT df.dataset_algo FROM dbsbuffer_file df WHERE df.status = 'NOTUPLOADED')
-             """

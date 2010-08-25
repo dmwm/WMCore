17815d7aa6ce358ec88c8395b876fd2de6a3121e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:19:03 +0000

    Use the MySQL version of this object.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4213 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Jobs/Save.py
+++ src/python/WMCore/WMBS/Oracle/Jobs/Save.py
@@ -6,18 +6,10 @@ Oracle implementation of Jobs.Save
 """
 
 __all__ = []
-__revision__ = "$Id: Save.py,v 1.3 2009/01/11 17:49:37 sfoulkes Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: Save.py,v 1.4 2009/05/18 17:31:41 sfoulkes Exp $"
+__version__ = "$Revision: 1.4 $"
 
 from WMCore.WMBS.MySQL.Jobs.Save import Save as SaveJobMySQL
 
 class Save(SaveJobMySQL):
-    sql = """UPDATE wmbs_job SET JOBGROUP = :jobgroup, NAME = :name,
-             LAST_UPDATE = :timestamp WHERE ID = :jobid"""
-    
-    def execute(self, jobid, jobgroup, name, conn = None, transaction = False):
-        binds = self.getBinds(jobgroup = jobgroup, name = name,
-                              timestamp = self.timestamp(), jobid = jobid)
-
-        self.dbi.processData(self.sql, binds, conn =  conn,
-                             transaction = transaction)
+    pass

3af8350c6ea92e7929cc6fcfbc9a1bac49706301
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:23:00 +0000

    Minor fix of SQLite implementation of Workflow.LoadFromTask
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7684 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Workflow/LoadFromTask.py
+++ src/python/WMCore/WMBS/SQLite/Workflow/LoadFromTask.py
@@ -6,11 +6,11 @@ SQLite implementation of Workflow.LoadFromTask
 """
 
 __all__ = []
-__revision__ = "$Id: LoadFromTask.py,v 1.1 2010/06/01 17:22:39 riahi Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: LoadFromTask.py,v 1.2 2010/06/01 21:23:18 riahi Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WMBS.MySQL.Workflow.LoadFromTask import LoadFromTask as LoadFromTaskMySQL
 
-class LoadFromName(LoadFromTaskMySQL):
+class LoadFromTask(LoadFromTaskMySQL):
     sql = LoadFromTaskMySQL.sql
 

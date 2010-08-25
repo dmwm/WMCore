c4020e86618ea0f4eb7afcd811b8c45f29dbcf72
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:26:30 +0000

    DAO for loading all blocks belonging to a particular Dataset-Algo and that are in 'Open' status
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7730 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMComponent/DBSUpload/Database/SQLite/LoadBlocksByDAS.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+_LoadBlocksByDAS_
+
+SQLite implementation of LoadBlocksByDAS
+"""
+
+__revision__ = "$Id: LoadBlocksByDAS.py,v 1.1 2010/06/04 19:00:35 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMComponent.DBSUpload.Database.MySQL.LoadBlocksByDAS import LoadBlocksByDAS as MySQLLoadBlocksByDAS
+
+class LoadBlocksByDAS(MySQLLoadBlocksByDAS):
+    """
+    _LoadBlocksByDAS_
+
+    Untested; same as MySQL
+    """

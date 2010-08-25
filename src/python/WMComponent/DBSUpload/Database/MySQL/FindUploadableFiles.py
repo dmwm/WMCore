710b5874530b86b022c5dcdbfd9b1d72954d1d4e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:58:55 +0000

    Adapted to load files, datasets, and and algos all together
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6546 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSUpload/Database/MySQL/FindUploadableFiles.py
+++ src/python/WMComponent/DBSUpload/Database/MySQL/FindUploadableFiles.py
@@ -5,8 +5,8 @@ _DBSUpload.FindUploadableFiles_
 Find the files in a datasets that needs to be uploaded to DBS
 
 """
-__revision__ = "$Id: FindUploadableFiles.py,v 1.10 2009/09/25 14:44:42 sfoulkes Exp $"
-__version__ = "$Revision: 1.10 $"
+__revision__ = "$Id: FindUploadableFiles.py,v 1.11 2010/02/24 21:38:45 mnorman Exp $"
+__version__ = "$Revision: 1.11 $"
 __author__ = "anzar@fnal.gov"
 
 import threading
@@ -14,31 +14,61 @@ import base64
 from WMCore.Database.DBFormatter import DBFormatter
 
 class FindUploadableFiles(DBFormatter):
-    sql = """SELECT dbsfile.id as ID FROM dbsbuffer_file dbsfile
-             WHERE dbsfile.dataset_algo = (SELECT ID FROM dbsbuffer_algo_dataset_assoc WHERE dataset_id =:dataset)
+    sql = """SELECT dbsfile.id as ID, das.dataset_id AS dataset, ds.Path as Path, das.algo_id as Algo, das.in_dbs as in_dbs,
+               das.id AS das_id,
+               da.app_name AS ApplicationName, 
+               da.app_ver AS ApplicationVersion, 
+               da.app_fam AS ApplicationFamily, 
+               da.PSet_Hash as PSetHash,
+               da.Config_Content as PSetContent
+             FROM dbsbuffer_file dbsfile
+             INNER JOIN dbsbuffer_algo_dataset_assoc das ON dbsfile.dataset_algo = das.id
+             INNER JOIN dbsbuffer_dataset ds ON ds.id = das.dataset_id
+             INNER JOIN dbsbuffer_algo da ON da.id = das.algo_id
+             WHERE das.in_dbs = 1
              AND dbsfile.status =:status
-             LIMIT :maxfiles""" 
+             """ 
 
     def __init__(self):
         myThread = threading.currentThread()
         DBFormatter.__init__(self, myThread.logger, myThread.dbi)
     
-    def getBinds(self, dataset, maxfiles):
-        binds =  { 'dataset': dataset['ID'], 'status':'NOTUPLOADED', 'maxfiles': maxfiles }
+    def getBinds(self):
+        binds =  {'status':'NOTUPLOADED'}
         return binds
 
     def makeFile(self, results):
         ret=[]
         for r in results:
-                entry={}
-                entry['ID']=long(r['id'])
-                ret.append(entry)
+            entry={}
+            entry['ID']=long(r['id'])
+            entry['Path']=r['path']
+            entry['DAS_ID'] = long(r['das_id'])
+            if not r['algo'] == None:
+                entry['Algo'] = int(r['algo'])
+            else:
+                entry['Algo'] = None
+            path = r['path']
+            entry['PrimaryDataset']     = path.split('/')[1]
+            entry['ProcessedDataset']   = path.split('/')[2]
+            entry['DataTier']           = path.split('/')[3]
+            entry['ApplicationName']    = r['applicationname']
+            entry['ApplicationVersion'] = r['applicationversion']
+            entry['ApplicationFamily']  = r['applicationfamily']
+            entry['PSetHash']           = r['psethash']
+            entry['PSetContent']        = r['psetcontent']
+            entry['Dataset']            = r['dataset']
+            ret.append(entry)
+
         return ret
 
-    def execute(self, datasetInfo=None, maxfiles=10, conn=None, transaction = False):
 
-        binds = self.getBinds(datasetInfo, maxfiles)
+    def execute(self, conn=None, transaction = False):
+
+        binds = self.getBinds()
         result = self.dbi.processData(self.sql, binds, 
                          conn = conn, transaction = transaction)
+        
         return self.makeFile(self.formatDict(result))
+
     
--- src/python/WMComponent/DBSUpload/Database/Oracle/FindUploadableFiles.py
+++ src/python/WMComponent/DBSUpload/Database/Oracle/FindUploadableFiles.py
@@ -5,9 +5,8 @@ _DBSUpload.FindUploadableFiles_
 Find the files in a datasets that needs to be uploaded to DBS
 
 """
-__revision__ = "$Id: FindUploadableFiles.py,v 1.3 2009/09/03 12:53:47 sfoulkes Exp $"
-__version__ = "$Revision: 1.3 $"
-__author__ = "anzar@fnal.gov"
+__revision__ = "$Id: FindUploadableFiles.py,v 1.4 2010/02/24 21:38:52 mnorman Exp $"
+__version__ = "$Revision: 1.4 $"
 
 from WMComponent.DBSUpload.Database.MySQL.FindUploadableFiles import FindUploadableFiles as MySQLFindUploadableFiles
 
@@ -16,10 +15,4 @@ class FindUploadableFiles(MySQLFindUploadableFiles):
     """
 Oracle implementation to find files in datasets that need to be uploaded to DBS
     """
-    sql = """SELECT * FROM
-               (SELECT dbsbuffer_file.id FROM dbsbuffer_file
-                  INNER JOIN dbsbuffer_algo_dataset_assoc ON
-                    dbsbuffer_file.dataset_algo = dbsbuffer_algo_dataset_assoc.id
-                WHERE status = :status AND
-                      dbsbuffer_algo_dataset_assoc.dataset_id = :dataset)
-             WHERE rownum <= :maxfiles"""
+

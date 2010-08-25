f45267359ada67398bbd574be0793f8608669cfe
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:45:54 +0000

    Strangely enough, this DAO does exactly what the name implies
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7182 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/Oracle/Jobs/GetNumberOfJobsPerWorkflow.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+_GetNumberOfJobsPerWorkflow_
+
+Oracle implementation of Jobs.GetNumberOfJobsPerWorkflow
+"""
+
+__all__ = []
+__revision__ = "$Id: GetNumberOfJobsPerWorkflow.py,v 1.1 2010/04/26 20:35:54 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+import logging
+
+from WMCore.WMBS.MySQL.Jobs.GetNumberOfJobsPerWorkflow import \
+     GetNumberOfJobsPerWorkflow as MySQLGetNumberOfJobsPerWorkflow
+
+
+class GetNumberOfJobsPerWorkflow(MySQLGetNumberOfJobsPerWorkflow):
+    """
+    Same as MySQL
+
+    """

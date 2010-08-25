f475aeb4f0f4e79b475d913a7f11aea2d322e2e4
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:04:59 +0000

    SQLite doesn't seem to like whitespace.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3997 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/JobGroup/IsComplete.py
+++ src/python/WMCore/WMBS/SQLite/JobGroup/IsComplete.py
@@ -5,15 +5,13 @@ Sqlite implementation of JobGroup.IsComplete
 """
 
 __all__ = []
-__revision__ = "$Id: IsComplete.py,v 1.1 2009/03/24 22:00:46 sryu Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: IsComplete.py,v 1.2 2009/03/31 16:25:16 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WMBS.MySQL.JobGroup.IsComplete import IsComplete as IsCompleteJobGroupMySQL
 
 class IsComplete(IsCompleteJobGroupMySQL):
-    sql = """select (
-                select count(id) from wmbs_job where jobgroup=:jobgroup 
-                ) as ac, 
-                select count(job) from wmbs_group_job_complete where jobgroup=:jobgroup
-                ) as cm 
+    sql = """select
+               (select count(id) from wmbs_job where jobgroup=:jobgroup) as ac,
+               (select count(job) from wmbs_group_job_complete where jobgroup=:jobgroup) as cm
           """

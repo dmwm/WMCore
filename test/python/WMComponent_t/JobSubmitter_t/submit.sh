379d6fa120b163e23ec008872c64e8d9b45da044
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:12:17 +0000

    Ugly changes to make this work with our current setup.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6726 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMComponent_t/JobSubmitter_t/submit.sh
+++ test/python/WMComponent_t/JobSubmitter_t/submit.sh
@@ -6,6 +6,9 @@ INDEX=$2
 
 ls
 . /uscmst1/prod/sw/cms/slc5_amd64_gcc434/external/python/2.6.4-cms2/etc/profile.d/init.sh 
+. /uscmst1/prod/sw/cms/setup/shrc prod
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/uscmst1/prod/sw/cms/slc5_amd64_gcc434/external/openssl/0.9.7m/lib" > ~/.bashrc
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/uscmst1/prod/sw/cms/slc5_amd64_gcc434/external/openssl/0.9.7m/lib
 python2.6 Unpacker.py --sandbox=$SANDBOX --package=JobPackage.pkl --index=$INDEX
 
 cd job

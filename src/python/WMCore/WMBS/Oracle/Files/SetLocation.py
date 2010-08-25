1854cd53a38446d2eeba1f64d35bd16d8bed5ab2
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:40:33 +0000

    Change this to take a file id instead of a lfn.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5431 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/Oracle/Files/SetLocation.py
+++ src/python/WMCore/WMBS/Oracle/Files/SetLocation.py
@@ -1,15 +1,17 @@
 """
-Oracle implementation of SetFileLocation
+_SetLocation_
+
+Oracle implementation of Files.SetLocation
 """
 
+__revision__ = "$Id: SetLocation.py,v 1.7 2009/10/22 18:38:18 sfoulkes Exp $"
+__version__ = "$Revision: 1.7 $"
+
 from WMCore.WMBS.MySQL.Files.SetLocation import SetLocation as SetLocationMySQL
-from sets import Set
 
 class SetLocation(SetLocationMySQL):
-    
-    sql = """insert into wmbs_file_location (fileid, location) 
-                SELECT wmbs_file_details.id, wmbs_location.id from wmbs_file_details, wmbs_location 
-                 WHERE wmbs_file_details.lfn = :lfn
-                 AND wmbs_location.site_name = :location 
-                 AND NOT EXISTS (SELECT * FROM wmbs_file_location WHERE fileid = wmbs_file_details.id
-                 and location = wmbs_location.id)"""
+    sql = """INSERT INTO wmbs_file_location (fileid, location) 
+                SELECT :fileid, wmbs_location.id from wmbs_location 
+                  WHERE wmbs_location.site_name = :location AND NOT EXISTS
+                    (SELECT * FROM wmbs_file_location
+                       WHERE fileid = :fileid AND location = wmbs_location.id)"""

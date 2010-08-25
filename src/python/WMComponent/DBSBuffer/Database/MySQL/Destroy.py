148987395f43308432c1c30a223568f889348f99
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:56:24 +0000

    updating dbsbuffer to be able to handle partial database blowing away
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4775 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DBSBuffer/Database/MySQL/Destroy.py
+++ src/python/WMComponent/DBSBuffer/Database/MySQL/Destroy.py
@@ -5,8 +5,8 @@ _Destroy_
 Implementation of DBSBuffer.Destroy for SQLite
 """
 
-__revision__ = "$Id: Destroy.py,v 1.1 2009/07/13 19:57:44 sfoulkes Exp $"
-__version__ = "$Revision: 1.1 $s"
+__revision__ = "$Id: Destroy.py,v 1.2 2009/08/12 17:24:52 meloam Exp $"
+__version__ = "$Revision: 1.2 $s"
 
 import threading
 
@@ -23,11 +23,11 @@ class Destroy(DBCreator):
         myThread = threading.currentThread()
         DBCreator.__init__(self, myThread.logger, myThread.dbi)
         
-        self.create["08dbsbuffer_dataset"] = "DROP TABLE dbsbuffer_dataset" 
-        self.create["07dbsbuffer_algo"] = "DROP TABLE dbsbuffer_algo"
-        self.create["06dbsbuffer_algo_dataset_assoc"] = "DROP TABLE dbsbuffer_algo_dataset_assoc"
-        self.create["05dbsbuffer_file"] = "DROP TABLE dbsbuffer_file"
-        self.create["04dbsbuffer_file_parent"] = "DROP TABLE dbsbuffer_file_parent"
-        self.create["03dbsbuffer_file_runlumi_map"] = "DROP TABLE dbsbuffer_file_runlumi_map"
-        self.create["02dbsbuffer_location"] = "DROP TABLE dbsbuffer_location"
-        self.create["01dbsbuffer_file_location"] = "DROP TABLE dbsbuffer_file_location"
+        self.delete["08dbsbuffer_dataset"] = "DROP TABLE dbsbuffer_dataset" 
+        self.delete["07dbsbuffer_algo"] = "DROP TABLE dbsbuffer_algo"
+        self.delete["06dbsbuffer_algo_dataset_assoc"] = "DROP TABLE dbsbuffer_algo_dataset_assoc"
+        self.delete["05dbsbuffer_file"] = "DROP TABLE dbsbuffer_file"
+        self.delete["04dbsbuffer_file_parent"] = "DROP TABLE dbsbuffer_file_parent"
+        self.delete["03dbsbuffer_file_runlumi_map"] = "DROP TABLE dbsbuffer_file_runlumi_map"
+        self.delete["02dbsbuffer_location"] = "DROP TABLE dbsbuffer_location"
+        self.delete["01dbsbuffer_file_location"] = "DROP TABLE dbsbuffer_file_location"
--- src/python/WMComponent/DBSBuffer/Database/Oracle/Destroy.py
+++ src/python/WMComponent/DBSBuffer/Database/Oracle/Destroy.py
@@ -5,8 +5,8 @@ _Destroy_
 Implementation of DBSBuffer.Destroy for Oracle
 """
 
-__revision__ = "$Id: Destroy.py,v 1.1 2009/07/14 19:20:42 sfoulkes Exp $"
-__version__ = "$Revision: 1.1 $s"
+__revision__ = "$Id: Destroy.py,v 1.2 2009/08/12 17:24:51 meloam Exp $"
+__version__ = "$Revision: 1.2 $s"
 
 import threading
 
@@ -23,23 +23,23 @@ class Destroy(DBCreator):
         myThread = threading.currentThread()
         DBCreator.__init__(self, myThread.logger, myThread.dbi)
         
-        self.create["08dbsbuffer_dataset"] = "DROP TABLE dbsbuffer_dataset" 
-        self.create["07dbsbuffer_algo"] = "DROP TABLE dbsbuffer_algo"
-        self.create["06dbsbuffer_algo_dataset_assoc"] = "DROP TABLE dbsbuffer_algo_dataset_assoc"
-        self.create["05dbsbuffer_file"] = "DROP TABLE dbsbuffer_file"
-        self.create["04dbsbuffer_file_parent"] = "DROP TABLE dbsbuffer_file_parent"
-        self.create["03dbsbuffer_file_runlumi_map"] = "DROP TABLE dbsbuffer_file_runlumi_map"
-        self.create["02dbsbuffer_location"] = "DROP TABLE dbsbuffer_location"
-        self.create["01dbsbuffer_file_location"] = "DROP TABLE dbsbuffer_file_location"
+        self.delete["08dbsbuffer_dataset"] = "DROP TABLE dbsbuffer_dataset" 
+        self.delete["07dbsbuffer_algo"] = "DROP TABLE dbsbuffer_algo"
+        self.delete["06dbsbuffer_algo_dataset_assoc"] = "DROP TABLE dbsbuffer_algo_dataset_assoc"
+        self.delete["05dbsbuffer_file"] = "DROP TABLE dbsbuffer_file"
+        self.delete["04dbsbuffer_file_parent"] = "DROP TABLE dbsbuffer_file_parent"
+        self.delete["03dbsbuffer_file_runlumi_map"] = "DROP TABLE dbsbuffer_file_runlumi_map"
+        self.delete["02dbsbuffer_location"] = "DROP TABLE dbsbuffer_location"
+        self.delete["01dbsbuffer_file_location"] = "DROP TABLE dbsbuffer_file_location"
 
-        self.create["01dbsbuffer_dataset_seq"] = "DROP SEQUENCE dbsbuffer_dataset_seq"
-        self.create["02dbsbuffer_algo_seq"] = "DROP SEQUENCE dbsbuffer_algo_seq"
-        self.create["03dbsbuffer_file_seq"] = "DROP SEQUENCE dbsbuffer_file_seq"
-        self.create["04dbsbuffer_location_seq"] = "DROP SEQUENCE dbsbuffer_location_seq"
-        self.create["05dbsbuffer_algo_dataset_assoc_seq"] = "DROP SEQUENCE dbsbuffer_algdset_assoc_seq"
+        self.delete["01dbsbuffer_dataset_seq"] = "DROP SEQUENCE dbsbuffer_dataset_seq"
+        self.delete["02dbsbuffer_algo_seq"] = "DROP SEQUENCE dbsbuffer_algo_seq"
+        self.delete["03dbsbuffer_file_seq"] = "DROP SEQUENCE dbsbuffer_file_seq"
+        self.delete["04dbsbuffer_location_seq"] = "DROP SEQUENCE dbsbuffer_location_seq"
+        self.delete["05dbsbuffer_algo_dataset_assoc_seq"] = "DROP SEQUENCE dbsbuffer_algdset_assoc_seq"
 
-        self.create["01dbsbuffer_dataset_trg"] = "DROP TRIGGER dbsbuffer_dataset_trg"
-        self.create["02dbsbuffer_algo_trg"] = "DROP TRIGGER dbsbuffer_algo_trg"
-        self.create["03dbsbuffer_file_trg"] = "DROP TRIGGER dbsbuffer_file_trg"
+        self.delete["01dbsbuffer_dataset_trg"] = "DROP TRIGGER dbsbuffer_dataset_trg"
+        self.delete["02dbsbuffer_algo_trg"] = "DROP TRIGGER dbsbuffer_algo_trg"
+        self.delete["03dbsbuffer_file_trg"] = "DROP TRIGGER dbsbuffer_file_trg"
         #self.create["04dbsbuffer_location_trg"] = "DROP TRIGGER dbsbuffer_location_trg"
-        self.create["05dbsbuffer_algo_dataset_assoc_trg"] = "DROP TRIGGER dbsbuffer_algdset_assoc_trg"
+        self.delete["05dbsbuffer_algo_dataset_assoc_trg"] = "DROP TRIGGER dbsbuffer_algdset_assoc_trg"

28cb818012e3207ebd37895536fba77e84a4c23b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:29:07 +0000

    Add support for combining job reports.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6954 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMRuntime/Startup.py
+++ src/python/WMCore/WMRuntime/Startup.py
@@ -3,14 +3,14 @@
 _Startup_
 
 Runtime environment startup script
-
 """
-import os
-import WMCore.WMRuntime.Bootstrap as Bootstrap
 
+import os
+
+import WMCore.WMRuntime.Bootstrap as Bootstrap
+import WMCore.FwkJobReport.Report as Report
 
 if __name__ == '__main__':
-
     job = Bootstrap.loadJobDefinition()
     task = Bootstrap.loadTask(job)
     monitor = Bootstrap.setupMonitoring()
@@ -26,11 +26,15 @@ if __name__ == '__main__':
     if monitor.isAlive():
         monitor.shutdown()
 
+    print "Combining reports..."
+    finalReport = Report()
+    taskSteps = task.listAllStepNames()
+    print "Have the following steps: %s" % taskSteps
+    for taskStep in taskSteps:
+        if os.path.exists("./%s/Report.pkl" % taskStep):
+            stepReport = Report(taskStep)
+            print "Loading %s" % taskStep
+            stepReport.unpersist("./%s/Report.pkl" % taskStep)
+            finalReport.setStep(taskStep, stepReport.retrieveStep(taskStep))
 
-
-
-
-
-
-
-
+    finalReport.persist("Report.pkl")

353b7687594563257d9afb0fa4bf632b876f378e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:57:15 +0000

    Now handles multiple locations and will break files into jobs based on file_per_job
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7337 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/JobSplitting/EndOfRun.py
+++ src/python/WMCore/JobSplitting/EndOfRun.py
@@ -6,8 +6,8 @@ If a subscription's fileset is closed, make a job that will run over any availab
 files
 """
 
-__revision__ = "$Id: EndOfRun.py,v 1.6 2010/01/15 14:30:46 sfoulkes Exp $"
-__version__  = "$Revision: 1.6 $"
+__revision__ = "$Id: EndOfRun.py,v 1.7 2010/05/07 14:37:27 mnorman Exp $"
+__version__  = "$Revision: 1.7 $"
 
 import logging
 from WMCore.JobSplitting.JobFactory import JobFactory
@@ -23,36 +23,55 @@ class EndOfRun(JobFactory):
         _algorithm_
 
         An end-of-run job splitting algorithm, will return a job with all
-        unacquired ("available") files if the fileset is marked as closed
-        returns nothing otherwise
-        """
-       
-        #  //
-        # // Resulting job set (shouldnt this be a JobGroup??)
-        #//
-        self.newGroup()
+        unacquired ('available') files if the fileset is marked as closed
+        returns nothing otherwise.
 
+        Can take files_per_job as an argument
+        """
+
+        filesPerJob  = int(kwargs.get("files_per_job", 999999))
+       
         #  //
         # // get the fileset
         #//
         fileset = self.subscription.getFileset()
+
         try:
             fileset.load()
         except AttributeError, ae:
-            pass    
+            pass
 
-        if (not fileset.open):
-            availFiles = self.subscription.availableFiles()
+        # Return if you have the fileset still open
+        if fileset.open:
+            logging.error("Fileset was open")
+            return
 
-            baseName = makeUUID()
-            self.newJob(name = '%s-endofrun' % (baseName,))
-            
-            if (len(availFiles) == 0):
-                # no files to acquire
-                return []
-            
-            for f in availFiles:
+        # Get a dictionary of sites, files
+        locationDict = self.sortByLocation()
+
+        # Get a new jobGroup
+        self.newGroup()
+
+        jobCount = 0
+        baseName = makeUUID()
+
+        # Unlike FileBased, all these jobs will be in one group
+        for location in locationDict.keys():
+            # For each location, though, we'll need new jobs.
+            fileList    = locationDict[location]
+            filesInJob  = 0
+            if len(fileList) == 0:
+                #No files for this location
+                #This isn't supposed to happen, but better safe then sorry
+                continue
+            for f in fileList:
+                if filesInJob == 0 or filesInJob >= filesPerJob:
+                    self.newJob(name = '%s-endofrun-%i' %(baseName, jobCount))
+                    filesInJob = 0
+                    jobCount += 1
                 f.loadData(parentage = 1)
                 self.currentJob.addFile(f)
+                filesInJob += 1
 
-            return
+        return
+

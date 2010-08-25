de28505b1376f0fc3e04fdc41cd0476f50949138
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:30:21 +0000

    change node name to se
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6971 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMQuality/Emulators/PhEDExClient/PhEDEx.py
+++ src/python/WMQuality/Emulators/PhEDExClient/PhEDEx.py
@@ -3,8 +3,8 @@
     Mocked Phedex interface
 """
 
-__revision__ = "$Id: PhEDEx.py,v 1.3 2010/03/22 16:04:27 sryu Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: PhEDEx.py,v 1.4 2010/04/06 20:41:38 sryu Exp $"
+__version__ = "$Revision: 1.4 $"
 
 # need to clean this up at some point
 
@@ -47,7 +47,7 @@ class PhEDEx:
                                                   'subscription' : []})
                 subs = data['phedex']['dataset'][-1]['subscription']
                     #FIXME: Take from self.locations
-                subs.append({'node': 'SiteA', 'custodial': 'n', 'suspend_until': None,
+                subs.append({'se': 'SiteA', 'custodial': 'n', 'suspend_until': None,
                              'level': 'dataset', 'move': 'n', 'request': '47983',
                              'time_created': '1232989000', 'priority': 'low',
                              'time_update': None, 'node_id': '781',
@@ -66,9 +66,9 @@ class PhEDEx:
                 blocks.append({"bytes":"10438786614", "files":"5", "is_open":"n",
                                "name": args['block'],
                                "id":"454370", "subscription"
-                                                  :[ {'node' : x } for x in locations]
+                                                  :[ {'se' : x } for x in locations]
                                                         #{"priority":"normal", "request":"51253", "time_created":"1245165314",
-                                                        #   "move":"n", "suspend_until":None, "node":"SiteA",
+                                                        #   "move":"n", "suspend_until":None, "se":"SiteA",
                                                         #   "time_update":"1228905272", "group":None, "level":"block",
                                                         #   "node_id":"641", "custodial":"n", "suspended":"n"}]
                                                     })

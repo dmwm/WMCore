79f58c538a61f46b32f418755fadcfac26d44683
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:12:24 +0000

    DAO object to set cache_dir for jobs; can work in bulk mode
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5017 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/Oracle/Jobs/SetCache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+_SetCache_
+
+Oracle implementation of Jobs.SetCache
+"""
+
+__all__ = []
+__revision__ = "$Id: SetCache.py,v 1.1 2009/09/09 19:24:03 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.WMBS.MySQL.Jobs.SetCache import SetCache as MySQLSetCache
+
+class SetCache(MySQLSetCache):
+    """
+    Identical to MySQL version for now
+
+    """

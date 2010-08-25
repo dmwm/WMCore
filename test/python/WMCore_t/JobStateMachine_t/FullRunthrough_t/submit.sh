1ef192ebdc0b0f6a80de972a497d23e0741c118b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:45:46 +0000

    Support files for the fullRunthrough test
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5506 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/python/WMCore_t/JobStateMachine_t/FullRunthrough_t/submit.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+jid=$1
+submitNode=$2
+sandbox=$3
+
+echo "I am running job "$jid" from submit node "$submitNode" on node "$HOSTNAME
+echo "I will get the sandbox from "$sandbox
+
+exit 0

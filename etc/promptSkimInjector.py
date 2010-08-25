9804f1729de253b4512f7ea1cf6733009e163c00
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:27:20 +0000

    Add code to actually copy some data.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7741 4525493e-7705-40b1-a816-d608a930855b

--- etc/promptSkimInjector.py
+++ etc/promptSkimInjector.py
@@ -2,11 +2,49 @@
 """
 _promptSkimInjector_
 
+Recreate portions of the T0AST schema that are necessary to test PromptSkimming
+for one run and copy all necessary data as well.
 """
 
-from WMCore.Database.DBFactory import DBFactory
+import logging
+import sys
 
-def determineSchema(remoteT0AST, tableName):
+from WMCore.Database.DBFactory import DBFactory
+from WMCore.WMBS.Oracle.Create import Create
+
+def copyTableSubset(remoteT0AST, localT0AST, tableName, selectSQL, selectBinds):
+    """
+    _copyTableSubset_
+
+    Copy a subset of the data out of a remote T0AST instance into a local T0AST
+    instance.
+    """
+    print "\nCopying %s..." % tableName,
+
+    resultSets = remoteT0AST.processData(selectSQL, selectBinds)
+    results = []
+    for resultSet in resultSets:
+        results.extend(resultSet.fetchall())
+
+    numCols = len(results[0])
+
+    insert = "INSERT INTO %s VALUES(" % tableName
+    for i in range(numCols):
+        insert += ":p_%i, " % i
+    insert = insert[:-2] + ")"
+
+    binds = []
+    for result in results:
+        newBind = {}
+        for i in range(numCols):
+            newBind["p_%i" % i] = result[i]
+        binds.append(newBind)
+
+    localT0AST.processData(insert, binds)
+    print "done."
+    return
+
+def determineSchema(remoteT0AST):
     """
     _determineSchema_
 
@@ -14,7 +52,8 @@ def determineSchema(remoteT0AST, tableName):
     is returned as a dictionary keyed by tablename.  The values are a list of
     columns where each column is a dictionary with information about the column.
     """
-    sqlQuery = "SELECT tname, colno, cname, coltype, width FROM col WHERE tname = %s" % tableName
+    sqlQuery = """SELECT table_name, column_id, column_name, data_type, data_length
+                  FROM all_tab_columns WHERE owner = 'CMS_T0AST_1'"""
     resultSets = remoteT0AST.processData(sqlQuery)
 
     results = []
@@ -53,7 +92,7 @@ def createSchema(localT0AST, tableInfo):
     """
     _createSchema_
 
-    Create the Oracle schema in a SQLite database.
+    Create the T0AST schema in the local database instance.
     """
     for tableName in tableInfo.keys():
         columns = tableInfo[tableName]
@@ -87,20 +126,28 @@ def createSchema(localT0AST, tableInfo):
 
     return
 
-def copyData(remoteT0AST, localT0AST, tableNames):
+def copyData(remoteT0AST, localT0AST, tableInfo, runNumber):
     """
     _copyData_
 
-    Copy data between the Oracle and SQLite databases.
+    Copy data from the remote T0AST to the localT0AST for all the tables
+    in the tableInfo parameter.  If the table has a RUN_ID column limit rows
+    copied to those from the given run.
     """
-    print "Copying some data:"
-    
-    tableInfo = []
-    for tableName in tableNames:
-        tableInfo.append(determineSchema(remoteT0AST, tableName))
-    
+    print "\nCopying T0AST metadata for run %s:" % runNumber
+       
     for tableName in tableInfo.keys():
+        hasRun = False
+        tableCols = tableInfo[tableName]
+        for tableCol in tableCols:
+            if tableCol["NAME"] == "RUN_ID":
+                hasRun = True
+                break
+        
         rowQuery = "SELECT count(*) FROM %s" % tableName
+        if hasRun:
+            rowQuery += " WHERE run_id = %s" % runNumber
+            
         resultSets = remoteT0AST.processData(rowQuery)
         result = resultSets[0].fetchall()[0][0]
 
@@ -116,15 +163,19 @@ def copyData(remoteT0AST, localT0AST, tableNames):
             insertQuery += "%s," % column["NAME"]
 
         selectQuery = selectQuery[:-1] + " FROM %s" % tableName
-        insertQuery = insertQuery[:-1] + ") VALUES ("
+        if hasRun:
+            selectQuery += " WHERE run_id = %s" % runNumber            
 
+        insertQuery = insertQuery[:-1] + ") VALUES ("
         for i in range(len(columns)):
             insertQuery += ":p_%s," % i
 
         insertQuery = insertQuery[:-1] + ")"
 
-        t0astDBConn.execute(selectQuery)
-        results = t0astDBConn.fetchall()
+        resultSets = remoteT0AST.processData(selectQuery)
+        results = []
+        for resultSet in resultSets:
+            results.extend(resultSet.fetchall())
 
         for result in results:
             bindVars = {}
@@ -135,15 +186,98 @@ def copyData(remoteT0AST, localT0AST, tableNames):
 
             localT0AST.processData(insertQuery, bindVars, None, None)
 
-localT0ASTUrl = "oracle://sfoulkes:sfoulkes_cms2008@cmscald:1521"
+if len(sys.argv) != 4:
+    print "Usage:"
+    print "  ./promptSkimInjector LOCAL_T0AST_URL PROD_T0AST_READER_URL RUN_NUMBER"
+    print ""
+    sys.exit(0)
 
-# Connect to remote T0AST
-# Connect to local T0AST
-# Install WMBS in local t0ast
+localT0ASTUrl = sys.argv[1]
+remoteT0ASTUrl = sys.argv[2]
+runNum = int(sys.argv[3])
 
-tableNames = ["processed_dataset", "primary_dataset", "data_tier",
-              "dataset_path", "run_status", "block_status",
-              "block_migrate_status", "run_stream_cmssw_assoc",
-              "cmssw_version", "t1skim_config", "phedex_subscription",
-              "storage_node"]
+localDBFactory = DBFactory(logging, localT0ASTUrl)
+remoteDBFactory = DBFactory(logging, remoteT0ASTUrl)
+
+localDbi = localDBFactory.connect()
+remoteDbi = remoteDBFactory.connect()
+
+print "\nCreating WMBS Schema in local Oracle...",
+wmbsCreate = Create(logging, localDbi)
+wmbsCreate.execute()
+print "done."
+
+copyTableNames = ["processed_dataset", "primary_dataset", "data_tier",
+                  "dataset_path", "run_status", "block_status",
+                  "block_migrate_status", "run_stream_cmssw_assoc",
+                  "cmssw_version", "t1skim_config", "phedex_subscription",
+                  "storage_node", "run", "block_run_assoc"]
+
+createTableNames = ["processed_dataset", "primary_dataset", "data_tier",
+                    "dataset_path", "run_status", "block_status",
+                    "block_migrate_status", "run_stream_cmssw_assoc",
+                    "cmssw_version", "t1skim_config", "phedex_subscription",
+                    "storage_node", "run", "block_run_assoc", "block",
+                    "wmbs_file_dataset_path_assoc", "wmbs_file_block_assoc"]
+
+tableInfo = determineSchema(remoteDbi)
+for tableName in tableInfo.keys():
+    if tableName.lower() not in createTableNames:
+        del tableInfo[tableName]
+
+createSchema(localDbi, tableInfo)
+
+for tableName in tableInfo.keys():
+    if tableName.lower() not in copyTableNames:
+        del tableInfo[tableName]
+copyData(remoteDbi, localDbi, tableInfo, runNum)
+
+blockSelect = """SELECT id, dataset_path_id, block_size, file_count, status,
+                        migrate_status, delete_status, name, export_start_time,
+                        export_end_time FROM block
+                   INNER JOIN block_run_assoc ON
+                     block.id = block_run_assoc.block_id
+                 WHERE block_run_assoc.run_id = :runid"""
+
+copyTableSubset(remoteDbi, localDbi, "block", blockSelect, {"runid": runNum})
+
+fileBlockSelect = """SELECT DISTINCT(file_id), block_id FROM wmbs_file_block_assoc
+                       INNER JOIN wmbs_file_runlumi_map ON
+                         wmbs_file_block_assoc.file_id = wmbs_file_runlumi_map.fileid
+                     WHERE wmbs_file_runlumi_map.run = :runid"""
+
+copyTableSubset(remoteDbi, localDbi, "wmbs_file_block_assoc", fileBlockSelect,
+                {"runid": runNum})
+
+fileDatasetSelect = """SELECT DISTINCT(file_id), dataset_path_id FROM wmbs_file_dataset_path_assoc
+                         INNER JOIN wmbs_file_runlumi_map ON
+                           wmbs_file_dataset_path_assoc.file_id = wmbs_file_runlumi_map.fileid
+                       WHERE wmbs_file_runlumi_map.run = :runid"""
+
+copyTableSubset(remoteDbi, localDbi, "wmbs_file_dataset_path_assoc", fileDatasetSelect,
+                {"runid": runNum})
+
+fileDetailsSelect = """SELECT DISTINCT(id), lfn, filesize, events, first_event, last_event, merged
+                         FROM wmbs_file_details
+                         INNER JOIN wmbs_file_runlumi_map ON
+                           wmbs_file_details.id = wmbs_file_runlumi_map.fileid
+                       WHERE wmbs_file_runlumi_map.run = :runid"""
+
+copyTableSubset(remoteDbi, localDbi, "wmbs_file_details", fileDetailsSelect,
+                {"runid": runNum})
+
+fileRunSelect = "SELECT fileid, run, lumi FROM wmbs_file_runlumi_map WHERE run = :runid"
+
+copyTableSubset(remoteDbi, localDbi, "wmbs_file_runlumi_map", fileRunSelect, {"runid": runNum})
+
+fileParentsSelect = """SELECT child, parent FROM wmbs_file_parent
+                         INNER JOIN
+                           (SELECT DISTINCT(id) AS id FROM wmbs_file_details
+                              INNER JOIN wmbs_file_runlumi_map ON
+                                wmbs_file_details.id = wmbs_file_runlumi_map.fileid
+                            WHERE wmbs_file_runlumi_map.run = :runid) run_files ON
+                           wmbs_file_parent.parent = run_files.id"""
+
+copyTableSubset(remoteDbi, localDbi, "wmbs_file_parent", fileParentsSelect,
+                {"runid": runNum})
 

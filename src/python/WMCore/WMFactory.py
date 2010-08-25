956065804ffda0b9fdb97282aba29a09df747c0a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:55:07 +0000

    Add support for instantiating factory objects with a dict of arguments as well as lists
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4756 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMFactory.py
+++ src/python/WMCore/WMFactory.py
@@ -1,53 +1,53 @@
 #!/usr/bin/env python
 """
 A factory Class that is 'not thread safe' but is intended to work in threads
-(no sharing). The class dynamically loads objects from files when needed and 
+(no sharing). The class dynamically loads objects from files when needed and
 caches them (or not). It is a generalized factory object. If needed this class
 can be made threadsafe.
 """
 
-__revision__ = "$Id: WMFactory.py,v 1.10 2009/07/22 21:55:15 mnorman Exp $"
-__version__ = "$Revision: 1.10 $"
+__revision__ = "$Id: WMFactory.py,v 1.11 2009/08/10 18:58:33 evansde Exp $"
+__version__ = "$Revision: 1.11 $"
 __author__ = "fvlingen@caltech.edu"
 
 import logging
 import threading
 
 from WMCore.WMException import WMException
-from WMCore.WMExceptions import WMEXCEPTION 
+from WMCore.WMExceptions import WMEXCEPTION
 from sets import Set
 class WMFactory:
     """
-    A factory Class that is 'not thread safe' but is intended to work in 
-    threads (no sharing). The class dynamically loads objects from files 
+    A factory Class that is 'not thread safe' but is intended to work in
+    threads (no sharing). The class dynamically loads objects from files
     when needed and caches them.
     """
 
     def __init__(self, name, namespace = ''):
         """
         Initializes the factory, and checks if this thread already
-        has an attribute for storing registries. It uses the reserved 
+        has an attribute for storing registries. It uses the reserved
         'registries' attribute in the thread.
         """
-        self.namespace = namespace 
+        self.namespace = namespace
         self.objectList = {}
         msg = """
-Creating factory with name: %s associated to 
+Creating factory with name: %s associated to
 namespace (package): %s """ % (name, str(namespace))
         logging.debug(msg)
         myThread = threading.currentThread()
         if not hasattr(myThread, "factory"):
             myThread.factory = {}
-        myThread.factory[name] = self 
+        myThread.factory[name] = self
 
     def loadObject(self, classname, args = None, storeInCache = True, \
         getFromCache = False, listFlag = False):
         """
-        Dynamically loads the object from file.        
-        For this to work the class name has to 
+        Dynamically loads the object from file.
+        For this to work the class name has to
         be the same as the file name (minus the .py)
-        
-        Be default objects are loaded from cache. However if you 
+
+        Be default objects are loaded from cache. However if you
         want several different instances of the same object in one
         thread, you set cache to False.
         """
@@ -59,7 +59,7 @@ namespace (package): %s """ % (name, str(namespace))
             logging.debug("Not in cache")
         logging.debug("Searching class name: "+ classname)
         #try:
-        # check if we need to include the namespace 
+        # check if we need to include the namespace
         if self.namespace == '':
             module = classname
             #FIXME: hoky way of doing this! Change this please!
@@ -78,6 +78,8 @@ namespace (package): %s """ % (name, str(namespace))
             #Or anywhere you need arguments of the form (a,b,c,...)
             if type(args) == list and listFlag:
                 classinstance = obj(*args)
+            elif type(args) == dict:
+                classinstance = obj(**args)
             else:
                 #But if you actually need to pass a list, better do it the old fashioned way
                 classinstance = obj(args)

19ef065052a0cda225cedf4bbb51af48c40ce9af
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:37:07 +0000

    Remove FNAL specific python settings.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7871 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/JobSubmitter/submit.sh
+++ src/python/WMComponent/JobSubmitter/submit.sh
@@ -30,14 +30,10 @@ command -v python2.6 > /dev/null
 rc=$?
 if [[ $rc != 0 ]]
 then
-	# python2.6 isn't here, go through (hopefully to be removed) site-specific setup
-	echo "WMAgent couldn't find python2.6, attempting site-specific magic"
-	if [ -e /uscmst1/prod/sw/cms/slc5_amd64_gcc434/external/python/2.6.4-cms2/etc/profile.d/init.sh ]
+	if [ -e $VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/python/2.6.4/etc/profile.d/init.sh ]
 	then
-		# FNAL-specific setup
-		echo "WMAgent thinks it's running at FNAL, trying FNAL-specific bootstrapping"
-		. /uscmst1/prod/sw/cms/slc5_amd64_gcc434/external/python/2.6.4-cms2/etc/profile.d/init.sh 
-		export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/uscmst1/prod/sw/cms/slc5_amd64_gcc434/external/openssl/0.9.7m/lib:/uscmst1/prod/sw/cms/slc5_amd64_gcc434/external/bz2lib/1.0.5/lib
+		. $VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/python/2.6.4-cms2/etc/profile.d/init.sh 
+		export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/openssl/0.9.7m/lib:$VO_CMS_SW_DIR/COMP/slc5_amd64_gcc434/external/bz2lib/1.0.5/lib
 	else
 		echo "WMAgent Error: Python2.6 isn't available on this worker node." >&2
 		echo "WMAgent Error: WMCore/WMAgent REQUIRES python2.6" >&2

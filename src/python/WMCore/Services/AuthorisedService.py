b896cf82b3f95c50bcd06a911b6e822c78cc9871
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:37:17 +0000

    don't need to call getCert again here
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4487 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Services/AuthorisedService.py
+++ src/python/WMCore/Services/AuthorisedService.py
@@ -6,26 +6,27 @@ An AuthorisedService is the same as a Service but sends a cert/key with the url
 opener to access secured resources.
 """
 
-__revision__ = "$Id: AuthorisedService.py,v 1.8 2009/07/11 08:36:56 metson Exp $"
-__version__ = "$Revision: 1.8 $"
+__revision__ = "$Id: AuthorisedService.py,v 1.9 2009/07/13 17:55:47 sryu Exp $"
+__version__ = "$Revision: 1.9 $"
 
 import datetime, os, urllib, time
 
 from WMCore.WMException import WMException
-from WMCore.WMExceptions import WMEXCEPTION
 from WMCore.Services.Service import Service
 from WMCore.Services.Requests import SecureRequests
 
 class AuthorisedService(SecureRequests, Service):
+    """
+    _AuthorisedService_
+    
+    (Warning: Order of inheritance is important since both has parent have 
+     the same method (_getURLOpener))
+    TODO: better exception handling - make clear what exception is throwms
+    """
     def __init__(self, dict={}):
-        Service.__init__(self, dict)
         try:
-            if not dict.has_key('key') or not dict.has_key('cert'):
-                key, cert = getKeyCert()
-                self.setdefault("cert", cert)
-                self.setdefault("key", key)
-                
-        except:
-            self.logger.exception('Service requires a host certificate and key')
-            raise WMException('Service requires a host certificate and key', 
-                              "WMCORE-11")
+            Service.__init__(self, dict)    
+        except WMException, ex:
+            msg = str(ex)
+            self["logger"].exception(msg)
+            raise WMException(msg)

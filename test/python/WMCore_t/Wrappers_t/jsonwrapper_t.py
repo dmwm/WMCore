a0f40a4e0b981c089d4ec888c3bbec0160225268
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:17:02 +0000

    Update the test to cover new cjson load/dump calls
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5946 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/Wrappers_t/jsonwrapper_t.py
+++ test/python/WMCore_t/Wrappers_t/jsonwrapper_t.py
@@ -3,16 +3,16 @@
 Unit tests for json wrapper.
 """
 
-__revision__ = "$Id: jsonwrapper_t.py,v 1.1 2009/12/15 19:08:58 valya Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: jsonwrapper_t.py,v 1.2 2010/01/11 14:00:21 metson Exp $"
+__version__ = "$Revision: 1.2 $"
 __author__ = "Valentin Kuznetsov"
 
 import unittest
 import json
-__test = 0
+__test = False
 try:
     import cjson
-    __test = 1
+    __test = True
 except:
     print "No cjson module is found, skip the test"
 
@@ -28,7 +28,7 @@ class TestWrapper(unittest.TestCase):
 
     def test_cjson(self):
         """
-        Test default json implementation.
+        Test cjson implementation.
         """
         json_wrap._module = "cjson"
         result = json_wrap.dumps(self.record)
@@ -40,7 +40,64 @@ class TestWrapper(unittest.TestCase):
         result = json_wrap.loads(data)
         expect = json.loads(data)
         self.assertEqual(expect, result)
+    
+    def test_json(self):
+        """
+        Test default json implementation.
+        """
+        json_wrap._module = "json"
+        result = json_wrap.dumps(self.record)
+        expect = json.dumps(self.record)
+        self.assertEqual(expect, result)
 
+        data   = result
+
+        result = json_wrap.loads(data)
+        expect = json.loads(data)
+        self.assertEqual(expect, result)
+
+    def test_string_compare(self):
+        """
+        Test that cjson and json libraries do the same thing.
+        """
+        json_wrap._module = "cjson"
+        cj_result = json_wrap.dumps(self.record)
+        json_wrap._module = "json"
+        dj_result = json_wrap.dumps(self.record)
+        self.assertEqual(dj_result, cj_result)
+
+        data   = dj_result
+        
+        json_wrap._module = "cjson"
+        cj_result = json_wrap.loads(data)
+        json_wrap._module = "json"
+        dj_result = json_wrap.loads(data)
+        self.assertEqual(dj_result, cj_result)
+        
+    def test_file_compare(self):
+        #write self.record to a file via cjson
+        f = open('/tmp/jsonwrappertest', 'w')
+        json_wrap._module = "cjson"
+        json_wrap.dump(self.record, f)
+        f.close()
+        #read the file with the json
+        f = open('/tmp/jsonwrappertest', 'r')
+        json_wrap._module = "json" 
+        data = json_wrap.load(f)
+        f.close()
+        self.assertEqual(data, self.record) 
+        
+        #write self.record to a file via json
+        f = open('/tmp/jsonwrappertest', 'w')
+        json_wrap.dump(self.record, f)
+        f.close()
+        #read the file with cjson
+        f = open('/tmp/jsonwrappertest', 'r')
+        json_wrap._module = "cjson" 
+        data = json_wrap.load(f)
+        f.close()
+        self.assertEqual(data, self.record)
+        
 if __name__ == "__main__":
     if  __test:
         unittest.main()

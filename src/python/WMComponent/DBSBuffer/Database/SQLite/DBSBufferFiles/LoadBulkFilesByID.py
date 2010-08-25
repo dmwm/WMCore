362d8d84c06bdecf4dff79378ebc961fdd6aa2fe
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:05:17 +0000

    Very long, horribly complicated DAO for bulk file loading FOR DBSUPLOAD ONLY.  Does not work without AddToBuffer
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7446 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMComponent/DBSBuffer/Database/SQLite/DBSBufferFiles/LoadBulkFilesByID.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+_GetByID_
+
+SQLite implementation of DBSBufferFiles.GetByID
+"""
+
+__revision__ = "$Id: LoadBulkFilesByID.py,v 1.1 2010/05/14 18:53:59 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+import logging
+
+from WMComponent.DBSBuffer.Database.MySQL.DBSBufferFiles import LoadBulkFilesByID as MySQLLoadBulkFilesByID
+
+class LoadBulkFilesByID(MySQLLoadBulkFilesByID):
+    """
+    Same as MySQL
+
+    """

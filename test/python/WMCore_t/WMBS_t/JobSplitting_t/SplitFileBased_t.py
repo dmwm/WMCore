7c51cd15ad97c7aa9f93db85e72e269fa616f51b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:26:57 +0000

    Update to use the new merge query.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6925 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WMBS_t/JobSplitting_t/SplitFileBased_t.py
+++ test/python/WMCore_t/WMBS_t/JobSplitting_t/SplitFileBased_t.py
@@ -5,8 +5,8 @@ _SplitFileBased_t_
 Unit tests for the split file job splitting algorithm.
 """
 
-__revision__ = "$Id: SplitFileBased_t.py,v 1.6 2009/12/16 18:40:38 sfoulkes Exp $"
-__version__ = "$Revision: 1.6 $"
+__revision__ = "$Id: SplitFileBased_t.py,v 1.7 2010/03/31 21:33:37 sfoulkes Exp $"
+__version__ = "$Revision: 1.7 $"
 
 import unittest
 import os
@@ -62,7 +62,6 @@ class SplitFileBasedTest(unittest.TestCase):
         self.testInit.clearDatabase()        
         return
 
-
     def stuffWMBS(self):
         """
         _stuffWMBS_
@@ -73,131 +72,212 @@ class SplitFileBasedTest(unittest.TestCase):
         also injected.  Also files are added to the "Mergeable" subscription as
         well as to the output fileset for their jobgroups.
         """
-        bunkFileset = Fileset(name = "bunkFileset")
-        bunkFileset.create()
-
-        bunkWorkflow = Workflow(name = "bunkWorkflow", spec = "bunk",
-                                owner = "Steve", task = "someTask")
-        bunkWorkflow.create()
-        
-        bunkSubscription = Subscription(fileset = bunkFileset,
-                                        workflow = bunkWorkflow)
-        bunkSubscription.create()
-
         changeStateDAO = self.daoFactory(classname = "Jobs.ChangeState")
 
-        jobGroup1 = JobGroup(subscription = bunkSubscription)
+        inputFileset = Fileset(name = "inputFileset")
+        inputFileset.create()
+
+        inputWorkflow = Workflow(name = "inputWorkflow", spec = "input",
+                                owner = "Steve", task = "Test")
+        inputWorkflow.create()
+        
+        inputSubscription = Subscription(fileset = inputFileset,
+                                        workflow = inputWorkflow)
+        inputSubscription.create()
+
+        parentFile1 = File(lfn = "parentFile1")
+        parentFile1.create()
+        parentFile2 = File(lfn = "parentFile2")
+        parentFile2.create() 
+        parentFile3 = File(lfn = "parentFile3")
+        parentFile3.create()
+        parentFile4 = File(lfn = "parentFile4")
+        parentFile4.create()        
+
+        jobGroup1 = JobGroup(subscription = inputSubscription)
         jobGroup1.create()
-        newJob = Job()
-        newJob["outcome"] = "success"
-        newJob.create(jobGroup1)
-        newJob.save()
-        newJob["state"] = "cleanout"        
-        changeStateDAO.execute([newJob])                
-        jobGroup2 = JobGroup(subscription = bunkSubscription)
+        jobGroup2 = JobGroup(subscription = inputSubscription)
         jobGroup2.create()
-        newJob = Job()
-        newJob["outcome"] = "success"        
-        newJob.create(jobGroup2)
-        newJob.save()        
-        newJob["state"] = "cleanout"        
-        changeStateDAO.execute([newJob])                
-        jobGroup3 = JobGroup(subscription = bunkSubscription)
-        jobGroup3.create()
-        newJob = Job()
-        newJob["outcome"] = "success"        
-        newJob.create(jobGroup3)
-        newJob.save()        
-        newJob["state"] = "cleanout"        
-        changeStateDAO.execute([newJob])                
-        jobGroup4 = JobGroup(subscription = bunkSubscription)
-        jobGroup4.create()
-        newJob = Job()
-        newJob.create(jobGroup4)
-        newJob["state"] = "executing"        
-        changeStateDAO.execute([newJob])                
+        
+        testJob1 = Job()
+        testJob1.addFile(parentFile1)
+        testJob1.create(jobGroup1)
+        testJob1["state"] = "cleanout"
+        testJob1["oldstate"] = "new"
+        testJob1["couch_record"] = "somejive"
+        testJob1["retry_count"] = 0
+        testJob1["outcome"] = "success"
+        testJob1.save()
+        changeStateDAO.execute([testJob1])
+        
+        testJob2 = Job()
+        testJob2.addFile(parentFile2)
+        testJob2.create(jobGroup1)
+        testJob2["state"] = "cleanout"
+        testJob2["oldstate"] = "new"
+        testJob2["couch_record"] = "somejive"
+        testJob2["retry_count"] = 0
+        testJob2["outcome"] = "success"
+        testJob2.save()        
+        changeStateDAO.execute([testJob2])
+
+        testJob3 = Job()
+        testJob3.addFile(parentFile3)
+        testJob3.create(jobGroup2)
+        testJob3["state"] = "cleanout"
+        testJob3["oldstate"] = "new"
+        testJob3["couch_record"] = "somejive"
+        testJob3["retry_count"] = 0
+        testJob3["outcome"] = "success"
+        testJob3.save()        
+        changeStateDAO.execute([testJob3])
+
+        testJob4 = Job()
+        testJob4.addFile(parentFile4)
+        testJob4.create(jobGroup2)
+        testJob4["state"] = "cleanout"
+        testJob4["oldstate"] = "new"
+        testJob4["couch_record"] = "somejive"
+        testJob4["retry_count"] = 0
+        testJob4["outcome"] = "failure"
+        testJob4.save()        
+        changeStateDAO.execute([testJob4])
+
+        # We'll simulate a failed split by event job that the merger should
+        # ignore.
+        parentFile5 = File(lfn = "parentFile5")
+        parentFile5.create()        
+
+        testJob5 = Job()
+        testJob5.addFile(parentFile5)
+        testJob5.create(jobGroup2)
+        testJob5["state"] = "cleanout"
+        testJob5["oldstate"] = "new"
+        testJob5["couch_record"] = "somejive"
+        testJob5["retry_count"] = 0
+        testJob5["outcome"] = "success"
+        testJob5.save()        
+        changeStateDAO.execute([testJob5])
+
+        testJob6 = Job()
+        testJob6.addFile(parentFile5)
+        testJob6.create(jobGroup2)
+        testJob6["state"] = "cleanout"
+        testJob6["oldstate"] = "new"
+        testJob6["couch_record"] = "somejive"
+        testJob6["retry_count"] = 0
+        testJob6["outcome"] = "failure"
+        testJob6.save()        
+        changeStateDAO.execute([testJob6])                
+
+        badFile1 = File(lfn = "badFile1", size = 10241024, events = 10241024, first_event = 0)
+        badFile1.addRun(Run(1, *[45]))
+        badFile1.create()
+        badFile1.addParent(parentFile5["lfn"])
 
         file1 = File(lfn = "file1", size = 1024, events = 1024, first_event = 0)
         file1.addRun(Run(1, *[45]))
+        file1.create()
+        file1.addParent(parentFile1["lfn"])
         file2 = File(lfn = "file2", size = 1024, events = 1024, first_event = 1024)
         file2.addRun(Run(1, *[45]))
+        file2.create()
+        file2.addParent(parentFile1["lfn"])
         file3 = File(lfn = "file3", size = 1024, events = 1024, first_event = 2048)
         file3.addRun(Run(1, *[45]))
+        file3.create()
+        file3.addParent(parentFile1["lfn"])
         file4 = File(lfn = "file4", size = 1024, events = 1024, first_event = 3072)        
         file4.addRun(Run(1, *[45]))
+        file4.create()
+        file4.addParent(parentFile1["lfn"]) 
 
         fileA = File(lfn = "fileA", size = 1024, events = 1024, first_event = 0)
         fileA.addRun(Run(1, *[46]))
+        fileA.create()
+        fileA.addParent(parentFile2["lfn"])
         fileB = File(lfn = "fileB", size = 1024, events = 1024, first_event = 1024)
         fileB.addRun(Run(1, *[46]))
+        fileB.create()
+        fileB.addParent(parentFile2["lfn"])
         fileC = File(lfn = "fileC", size = 1024, events = 1024, first_event = 2048)
         fileC.addRun(Run(1, *[46]))
-
+        fileC.create()
+        fileC.addParent(parentFile2["lfn"])
+        
         fileI = File(lfn = "fileI", size = 1024, events = 1024, first_event = 0)
         fileI.addRun(Run(2, *[46]))
+        fileI.create()
+        fileI.addParent(parentFile3["lfn"])
         fileII = File(lfn = "fileII", size = 1024, events = 1024, first_event = 1024)
         fileII.addRun(Run(2, *[46]))
+        fileII.create()
+        fileII.addParent(parentFile3["lfn"])
         fileIII = File(lfn = "fileIII", size = 1024, events = 1024, first_event = 2048)
         fileIII.addRun(Run(2, *[46]))
+        fileIII.create()
+        fileIII.addParent(parentFile3["lfn"])        
         fileIV = File(lfn = "fileIV", size = 1024, events = 1024, first_event = 3072)        
-        fileIV.addRun(Run(2, *[46]))        
+        fileIV.addRun(Run(2, *[46]))
+        fileIV.create()
+        fileIV.addParent(parentFile3["lfn"])
 
         fileX = File(lfn = "badFileA", size = 1024, events = 1024, first_event = 0)
         fileX.addRun(Run(1, *[47]))
+        fileX.create()
+        fileX.addParent(parentFile4["lfn"])
         fileY = File(lfn = "badFileB", size = 1024, events = 1024, first_event = 1024)
         fileY.addRun(Run(1, *[47]))
+        fileY.create()
+        fileY.addParent(parentFile4["lfn"])        
         fileZ = File(lfn = "badFileC", size = 1024, events = 1024, first_event = 2048)
         fileZ.addRun(Run(1, *[47]))
+        fileZ.create()
+        fileZ.addParent(parentFile4["lfn"])
 
         jobGroup1.output.addFile(file1)
         jobGroup1.output.addFile(file2)
         jobGroup1.output.addFile(file3)
         jobGroup1.output.addFile(file4)        
+        jobGroup1.output.addFile(fileA)
+        jobGroup1.output.addFile(fileB)
+        jobGroup1.output.addFile(fileC)
         jobGroup1.output.commit()
 
-        jobGroup2.output.addFile(fileA)
-        jobGroup2.output.addFile(fileB)
-        jobGroup2.output.addFile(fileC)
+        jobGroup2.output.addFile(fileI)
+        jobGroup2.output.addFile(fileII)
+        jobGroup2.output.addFile(fileIII)
+        jobGroup2.output.addFile(fileIV)        
+        jobGroup2.output.addFile(fileX)
+        jobGroup2.output.addFile(fileY)
+        jobGroup2.output.addFile(fileZ)
+        jobGroup2.output.addFile(badFile1)
         jobGroup2.output.commit()
-        
-        jobGroup3.output.addFile(fileI)
-        jobGroup3.output.addFile(fileII)
-        jobGroup3.output.addFile(fileIII)
-        jobGroup3.output.addFile(fileIV)        
-        jobGroup3.output.commit()                
-
-        jobGroup4.output.addFile(fileX)
-        jobGroup4.output.addFile(fileY)
-        jobGroup4.output.addFile(fileZ)
-        jobGroup4.output.commit()
 
         self.mergeFileset = Fileset(name = "mergeFileset")
         self.mergeFileset.create()
+        self.bogusFileset = Fileset(name = "bogusFileset")
+        self.bogusFileset.create()        
 
         mergeWorkflow = Workflow(name = "mergeWorkflow", spec = "bunk2",
-                                 owner = "Steve", task = "someTask")
+                                 owner = "Steve", task="Test")
         mergeWorkflow.create()
         
         self.mergeSubscription = Subscription(fileset = self.mergeFileset,
                                               workflow = mergeWorkflow,
                                               split_algo = "SplitFileBased")
         self.mergeSubscription.create()
-        
-        self.mergeFileset.addFile(file1)
-        self.mergeFileset.addFile(file2)
-        self.mergeFileset.addFile(file3)
-        self.mergeFileset.addFile(file4)
-        self.mergeFileset.addFile(fileA)
-        self.mergeFileset.addFile(fileB)
-        self.mergeFileset.addFile(fileC)
-        self.mergeFileset.addFile(fileI)
-        self.mergeFileset.addFile(fileII)
-        self.mergeFileset.addFile(fileIII)
-        self.mergeFileset.addFile(fileIV)
-        self.mergeFileset.addFile(fileX)
-        self.mergeFileset.addFile(fileY)
-        self.mergeFileset.addFile(fileZ)        
+        self.bogusSubscription = Subscription(fileset = self.bogusFileset,
+                                              workflow = mergeWorkflow,
+                                              split_algo = "SplitFileBased")
+
+        for file in [file1, file2, file3, file4, fileA, fileB, fileC, fileI,
+                     fileII, fileIII, fileIV, fileX, fileY, fileZ, badFile1]:
+            self.mergeFileset.addFile(file)
+            self.bogusFileset.addFile(file)
+
         self.mergeFileset.commit()
+        self.bogusFileset.commit()
 
         return
 

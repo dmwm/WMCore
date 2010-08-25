13d54e3e48351887f87e152c738e10ffa72e4b41
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:04:37 +0000

    Add a close() method and properly handle column names.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@3991 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Database/ResultSet.py
+++ src/python/WMCore/Database/ResultSet.py
@@ -5,14 +5,18 @@ A class to read in a SQLAlchemy result proxy and hold the data, such that the
 SQLAlchemy result sets (aka cursors) can be closed. Make this class look as much
 like the SQLAlchemy class to minimise the impact of adding this class.
 """
-__revision__ = "$Id: ResultSet.py,v 1.1 2009/03/31 09:43:01 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+
+__revision__ = "$Id: ResultSet.py,v 1.2 2009/03/31 14:35:47 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
 
 class ResultSet:
     def __init__(self):
         self.data = []
         self.keys = []
-        
+
+    def close(self):
+        return
+    
     def fetchone(self):
         return self.data[0]
     
@@ -22,5 +26,5 @@ class ResultSet:
     def add(self, resultproxy):
         for r in resultproxy:
             if len(self.keys) == 0:
-                self.keys = r.keys
+                self.keys.extend(r.keys())
             self.data.append(r)

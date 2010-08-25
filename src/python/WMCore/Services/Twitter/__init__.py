54ca2d7b20396c411ecb438e2d99d7e3a22b37fd
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:50:48 +0000

    Exactly what it says on the tin. http://code.google.com/p/python-twitter/ v0.6
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4691 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/Services/Twitter/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+_SiteDB_
+
+SiteDB interface package
+
+"""
+__all__ = []
+__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: __init__.py,v 1.1 2009/07/30 19:02:02 mnorman Exp $"
+

58a6a84820ecd0da324060ed2c2e245f8dbeeca4
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:32:22 +0000

    get the Params directly from ReReco library
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7807 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMQuality/Emulators/WMSpecGenerator/Samples/ReRecoParams.py
+++ src/python/WMQuality/Emulators/WMSpecGenerator/Samples/ReRecoParams.py
@@ -1,29 +1,7 @@
 # this is for actual test
-MinBiasWithoutEmulator = {
-    "CmsPath": "/uscmst1/prod/sw/cms",
-    "AcquisitionEra": "WMAgentCommissioning10",
-    "Requestor": "sfoulkes@fnal.gov",
-    "InputDataset": "/MinimumBias/Commissioning10-v4/RAW",
-    "CMSSWVersion": "CMSSW_3_5_8_patch3",
-    "ScramArch": "slc5_ia32_gcc434",
-    "ProcessingVersion": "v20scf",
-    "SkimInput": "output",
-    "GlobalTag": "GR10_P_v4::All",
-    
-    "ProcessingConfig": "http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/Configuration/GlobalRuns/python/rereco_FirstCollisions_MinimumBias_35X.py?revision=1.8",
-    "SkimConfig": "http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/Configuration/DataOps/python/prescaleskimmer.py?revision=1.1",
-    
-    "CouchUrl": "http://dmwmwriter:gutslap!@cmssrv52.fnal.gov:5984",
-    "CouchDBName": "wmagent_config_cache",
-    "Scenario": ""
-#     "scenario": "cosmics",
-#     "processingOutputModules": {"outputRECORECO": {"dataTier": "RECO", "filterName": ""},
-#                                 "outputALCARECOALCARECO": {"dataTier": "ALCARECO", "filterName": ""}},
-#     "skimOutputModules": {},
-#     "processingConfig": "",
-#     "skimConfig": ""
-    
-    }
+from WMCore.WMSpec.StdSpecs.ReReco import getTestArguments
+
+MinBiasWithoutEmulator = getTestArguments()
 
 
 # This for the emulator test.

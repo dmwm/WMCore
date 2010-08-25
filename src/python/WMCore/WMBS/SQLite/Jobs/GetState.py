19b03746583fdb08efd90db12ac98c175605012e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:36:03 +0000

    Added a method to find the state for a job in wmbs_job
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4468 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/SQLite/Jobs/GetState.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+_GetState_
+
+SQLite implementation of Jobs.GetState
+"""
+
+__all__ = []
+__revision__ = "$Id: GetState.py,v 1.1 2009/07/10 20:02:49 mnorman Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.WMBS.MySQL.Jobs.GetState import GetState as MySQLGetState
+
+class GetState(MySQLGetState):
+    """
+    Right now this is the same as the MySQL version.
+
+    """

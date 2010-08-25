428fb6e325138bd68af4bd4cd06aa75994fd2ab1
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:16:58 +0000

    Added JSONEncoder/JSONDecoder wrapper; added load method for backward compatibility with json, cjson doesn't provide it
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5945 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Wrappers/jsonwrapper/__init__.py
+++ src/python/WMCore/Wrappers/jsonwrapper/__init__.py
@@ -7,8 +7,8 @@ So far we use simplejson (json) and cjson, other modules can be
 added in addition.
 """
 
-__revision__ = "$Id: __init__.py,v 1.2 2009/12/16 14:22:15 valya Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: __init__.py,v 1.3 2010/01/10 16:36:32 valya Exp $"
+__version__ = "$Revision: 1.3 $"
 __author__ = "Valentin Kuznetsov"
 __all__ = ['loads', 'dumps']
 
@@ -32,6 +32,15 @@ def loads(idict, **kwargs):
         return json.loads(idict, **kwargs)
     elif _module == 'cjson':
         return cjson.decode(idict)
+
+def load(source):
+    """
+    Use json.load for back-ward compatibility, since cjson doesn't
+    provide this method. The load method works on file-descriptor
+    objects.
+    """
+    return json.load(source)
+
 def dumps(idict, **kwargs):
     """
     Based on default _module invoke appropriate JSON encoding API call
@@ -41,3 +50,35 @@ def dumps(idict, **kwargs):
     elif _module == 'cjson':
         return cjson.encode(idict)
 
+def dump(doc, source):
+    """
+    Use json.dump for back-ward compatibility, since cjson doesn't
+    provide this method. The dump method works on file-descriptor
+    objects.
+    """
+    return json.dump(doc, source)
+
+
+class JSONEncoder(object):
+    def __init__(self):
+        self.encoder = json.JSONEncoder()
+
+    def encode(self, idict):
+        if  _module == 'cjson':
+            return cjson.encode(idict)
+        return self.encoder.encode(idict)
+
+    def iterencode(self, idict):
+        return self.encoder.iterencode(idict)
+
+class JSONDecoder(object):
+    def __init__(self):
+        self.decoder = json.JSONDecoder()
+
+    def decode(self, idict):
+        if  _module == 'cjson':
+            return cjson.decode(idict)
+        return self.decoder.decode(idict)
+
+    def raw_decode(self, idict):
+        return self.decoder.raw_decode(idict)

788565ee4d5f6f150a7f7fed0d72b27e2d6389ef
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:44:32 +0000

    Use the correct SQL call.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7967 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/SQLite/Subscriptions/SucceededJobs.py
+++ src/python/WMCore/WMBS/SQLite/Subscriptions/SucceededJobs.py
@@ -6,12 +6,12 @@ SQLite implementation of Subscriptions.SucceededJobs
 """
 
 __all__ = []
-__revision__ = "$Id: SucceededJobs.py,v 1.1 2010/06/01 21:14:18 riahi Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: SucceededJobs.py,v 1.2 2010/06/28 19:04:09 sfoulkes Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WMBS.MySQL.Subscriptions.SucceededJobs import SucceededJobs as SucceededJobsMySQL
 
 class SucceededJobs(SucceededJobsMySQL):
-    sql = LoadFromTaskMySQL.sql
+    sql = SucceededJobsMySQL.sql
 
 

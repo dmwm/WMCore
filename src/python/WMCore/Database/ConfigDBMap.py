1ec5066539a66aafceadfcf0ec855ece7c6f719e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 02:59:51 +0000

    update comments
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6559 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Database/ConfigDBMap.py
+++ src/python/WMCore/Database/ConfigDBMap.py
@@ -1,10 +1,26 @@
 class ConfigDBMapInterface(object):
+    """
+    Interface for convertiong the configuration to 
+    db url string and option dictionary
+    
+    the implementation of this class should create db url string.
+    and option for DBFactory creation.
+    For details of options, reference WMCore.Database.DBFactory 
+    """
     
     def __init__(self, config):
         self.config = config
     
     def getDBUrl(self):
+        """
+        this should return db string
+        i.e. mysql://username@hostname.fnal.gov:3306/TestDB
+        """
         raise NotImplementedError, "getDBUrl is not implemented"
     
     def getOption(self):
-        raise NotImplementedError, "getDBUrl is not implemented"
+        """
+        this should return options of dict format
+        i.e. {'engine_parameters': {'pool_size': 10}}
+        """
+        raise NotImplementedError, "getOption is not implemented"

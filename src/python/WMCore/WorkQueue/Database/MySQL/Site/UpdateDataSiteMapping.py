50b2a73f4039500db3ff0183e28b652997be4d80
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 03:21:53 +0000

    MySql doesn't handle the delete and insert within the same transaction when there is row deleted get reinserted. oracle handles that. - Maybe there is better way to do this
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@6856 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WorkQueue/Database/MySQL/Site/UpdateDataSiteMapping.py
+++ src/python/WMCore/WorkQueue/Database/MySQL/Site/UpdateDataSiteMapping.py
@@ -3,8 +3,8 @@ MySQL implementation of Site.UpdateBlockSiteMapping
 """
 
 __all__ = []
-__revision__ = "$Id: UpdateDataSiteMapping.py,v 1.3 2010/03/19 17:30:39 swakef Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: UpdateDataSiteMapping.py,v 1.4 2010/03/29 21:11:56 sryu Exp $"
+__version__ = "$Revision: 1.4 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -13,7 +13,7 @@ class UpdateDataSiteMapping(DBFormatter):
     deleteSql = """DELETE FROM wq_data_site_assoc WHERE data_id =
                   (SELECT id FROM wq_data WHERE name = :data)"""
 
-    insertSQL = """INSERT INTO wq_data_site_assoc (data_id, site_id)
+    insertSQL = """INSERT IGNORE INTO wq_data_site_assoc (data_id, site_id)
                         VALUES(
                             (SELECT id FROM wq_data WHERE name = :data),
                             (SELECT id from wq_site WHERE name = :site))"""
--- src/python/WMCore/WorkQueue/Database/Oracle/Site/UpdateDataSiteMapping.py
+++ src/python/WMCore/WorkQueue/Database/Oracle/Site/UpdateDataSiteMapping.py
@@ -3,12 +3,14 @@ SQLite implementation of site.UpdateDataSiteMapping
 """
 
 __all__ = []
-__revision__ = "$Id: UpdateDataSiteMapping.py,v 1.1 2009/09/03 15:44:20 swakef Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: UpdateDataSiteMapping.py,v 1.2 2010/03/29 21:12:14 sryu Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WorkQueue.Database.MySQL.Site.UpdateDataSiteMapping \
     import UpdateDataSiteMapping as UpdateDataSiteMappingMySQL
 
 class UpdateDataSiteMapping(UpdateDataSiteMappingMySQL):
-    deleteSql = UpdateDataSiteMappingMySQL.deleteSql
-    insertSQL = UpdateDataSiteMappingMySQL.insertSQL
+    insertSQL = """INSERT INTO wq_data_site_assoc (data_id, site_id)
+                        VALUES(
+                            (SELECT id FROM wq_data WHERE name = :data),
+                            (SELECT id from wq_site WHERE name = :site))"""

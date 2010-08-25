f308f6b147296271d71b5ba68c38801d4680f3fd
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 04:19:32 +0000

    Duplicate lines can be inserted.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@7638 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WorkQueue/Database/SQLite/Site/UpdateDataSiteMapping.py
+++ src/python/WMCore/WorkQueue/Database/SQLite/Site/UpdateDataSiteMapping.py
@@ -3,12 +3,12 @@ SQLite implementation of site.UpdateBlockSiteMapping
 """
 
 __all__ = []
-__revision__ = "$Id: UpdateDataSiteMapping.py,v 1.1 2009/09/03 15:44:21 swakef Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: UpdateDataSiteMapping.py,v 1.2 2010/05/27 18:19:34 swakef Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.WorkQueue.Database.MySQL.Site.UpdateDataSiteMapping \
     import UpdateDataSiteMapping as UpdateDataSiteMappingMySQL
 
 class UpdateDataSiteMapping(UpdateDataSiteMappingMySQL):
-    deleteSql = UpdateDataSiteMappingMySQL.deleteSql
-    insertSQL = UpdateDataSiteMappingMySQL.insertSQL
+    deleteSql = UpdateDataSiteMappingMySQL.deleteSql.replace('IGNORE', 'OR IGNORE', 1)
+    insertSQL = UpdateDataSiteMappingMySQL.insertSQL.replace('IGNORE', 'OR IGNORE', 1)

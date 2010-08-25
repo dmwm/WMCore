8f8669dafd6365b6d5e8a811ec3969a915207f18
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:54:08 +0000

    first tests of the json-roundtripping code
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4741 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/python/WMCore_t/Services_t/Requests_t.py
@@ -0,0 +1,33 @@
+'''
+Created on Aug 6, 2009
+
+@author: meloam
+'''
+import unittest
+import WMCore.Services.Requests as Requests
+from sets import Set
+from WMCore.DataStructs.Run import Run
+class testJSONRequests(unittest.TestCase):
+    def roundTrip(self,data):
+        self.assertEqual( data, self.request.decode(self.request.encode(data)) )
+        
+    def setUp(self):
+        self.request = Requests.JSONRequests()
+    
+    def testSet1(self):
+        self.roundTrip(Set([]))
+    def testSet2(self):
+        self.roundTrip(Set([1,2,3,4]))
+    def testSet3(self):
+        self.roundTrip(Set(['a','b','c','d']))
+    def testSet4(self):
+        self.roundTrip(Set([1,2,3,4,'a','b']))
+    def testRun1(self):
+        self.roundTrip(Run(1))
+    def testRun2(self):
+        self.roundTrip(Run(1,1))
+    def testRun3(self):
+        self.roundTrip(Run(1,2,3))
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()

1291a5672da619a0d5e602011b7a0cd6f00ea9fe
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 01:31:41 +0000

    Initial revision.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@5299 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/ProcessPool/ProcessPool.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+"""
+_ProcessPool_
+
+"""
+
+import subprocess
+import sys
+import simplejson
+import logging
+import os
+import threading
+
+from logging.handlers import RotatingFileHandler
+
+from WMCore.WMFactory import WMFactory
+from WMCore.WMInit import WMInit
+
+class ProcessPool:
+    def __init__(self, slaveClassName, totalSlaves, componentDir, config):
+        """
+        __init__
+
+        Constructor for the process pool.  The slave class name must be based
+        inside the WMComponent namespace.  For examples, the JobAccountant would
+        pass in "JobAccountant.AccountantWorker" to run the AccountantWorker
+        class.  All log files will be stored in the component directory that is
+        passed in.  Each slave will have its own log file.  The config is used
+        to get database connection parameters.
+        """
+        self.enqueueIndex = 0
+        self.dequeueIndex = 0
+
+        self.jsonDecoder = simplejson.JSONDecoder()
+        self.jsonEncoder = simplejson.JSONEncoder()
+
+        self.workers = []
+        slaveArgs = ["python", __file__, slaveClassName]
+        if hasattr(config.CoreDatabase, "socket"):
+            socket = config.CoreDatabase.socket
+        else:
+            socket = None
+            
+        dbConfig = {"dialect": config.CoreDatabase.dialect,
+                    "connectUrl": config.CoreDatabase.connectUrl,
+                    "socket": socket,
+                    "componentDir": componentDir}
+        encodedDBConfig = self.jsonEncoder.encode(dbConfig)
+        while totalSlaves > 0:
+            slaveProcess = subprocess.Popen(slaveArgs, stdin = subprocess.PIPE,
+                                            stdout = subprocess.PIPE)
+            slaveProcess.stdin.write("%s\n" % encodedDBConfig)
+            slaveProcess.stdin.flush()
+            self.workers.append(slaveProcess)
+            totalSlaves -= 1
+            
+        return
+
+    def enqueue(self, work):
+        """
+        __enqeue__
+
+        Assign work to the workers processes.  The work parameters must be a
+        list where each item in the list can be serialized into JSON.
+        """
+        for someWork in work:
+            encodedWork = self.jsonEncoder.encode(someWork)
+
+            worker = self.workers[self.enqueueIndex]
+            self.enqueueIndex = (self.enqueueIndex + 1) % len(self.workers)
+            worker.stdin.write("%s\n" % encodedWork)
+            worker.stdin.flush()
+
+        return
+
+    def dequeue(self, totalItems = 1):
+        """
+        __dequeue__
+
+        Retrieve completed work from the slave workers.  This method will block
+        until enough work has been completed.
+        """
+        completedWork = []
+
+        while totalItems > 0:
+            worker = self.workers[self.dequeueIndex]
+            self.dequeueIndex = (self.dequeueIndex + 1) % len(self.workers)
+
+            try:
+                output = worker.stdout.readline()
+
+                if output == None:
+                    continue
+
+                completedWork.append(self.jsonDecoder.decode(output))
+                totalItems -= 1
+            except Exception, e:
+                logging.error("Exception while getting slave output: %s" % e)
+                break
+
+        return completedWork
+
+def setupLogging(componentDir):
+    """
+    _setupLogging_
+
+    Setup logging for the slave process.  Each slave process will have its own
+    log file.
+    """
+    logFile = "%s/ComponentLog.%s" % (componentDir, os.getpid())
+
+    logHandler = RotatingFileHandler(logFile, "a", 1000000000, 3)
+    logFormatter = logging.Formatter("%(asctime)s:%(levelname)s:%(module)s:%(message)s")
+    logHandler.setFormatter(logFormatter)
+    logging.getLogger().addHandler(logHandler)
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    myThread = threading.currentThread()
+    myThread.logger = logging.getLogger()
+    return
+
+def setupDB(config, wmInit):
+    """
+    _setupDB_
+
+    Create the database connections.
+    """
+    if config.has_key("socket"):
+        socket = config["socket"]
+    else:
+        socket = None
+        
+    connectUrl = config["connectUrl"]
+    dialect = config["dialect"]
+        
+    wmInit.setDatabaseConnection(dbConfig = connectUrl,
+                                 dialect = dialect,
+                                 socketLoc = socket)
+    return
+    
+if __name__ == "__main__":
+    """
+    __main__
+
+    Entry point for the slave process.  The slave's classname will be passed in
+    on the command line.  The database connection parameters as well as the
+    name of the directory that the log files will be stored in will be passed
+    in through stdin as a JSON object.
+    """
+    slaveClassName = sys.argv[1]
+
+    jsonEncoder = simplejson.JSONEncoder()
+    jsonDecoder = simplejson.JSONDecoder()
+
+    encodedConfig = sys.stdin.readline()
+    config = jsonDecoder.decode(encodedConfig)
+
+    wmInit = WMInit()
+    setupLogging(config["componentDir"])
+    setupDB(config, wmInit)
+
+    wmFactory = WMFactory(name = "slaveFactory", namespace = "WMComponent")
+    slaveClass = wmFactory.loadObject(classname = slaveClassName)
+
+    while(True):
+        encodedInput = sys.stdin.readline()
+
+        try:
+            input = jsonDecoder.decode(encodedInput)
+        except Exception, e:
+            break
+
+        output = slaveClass(parameters = input)
+
+        if output != None:
+            encodedOutput = jsonEncoder.encode(output)
+            sys.stdout.write("%s\n" % encodedOutput)
+            sys.stdout.flush()
+
---
+++ src/python/WMCore/ProcessPool/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+__init__
+
+"""
+
+__revision__ = "$Id: __init__.py,v 1.1 2009/10/13 19:29:48 sfoulkes Exp $"
+__version__ = "$Revision: 1.1 $"
+
+
+

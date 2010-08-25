d912604b1f6f99a9b8aa97a1e31a02021827b4ff
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-08-25 00:48:55 +0000

    Tests for partial CMS names. Split tests up a bit
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@4664 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/Lexicon_t.py
+++ test/python/WMCore_t/Lexicon_t.py
@@ -6,8 +6,8 @@ General test of Lexicon
 
 """
 
-__revision__ = "$Id: Lexicon_t.py,v 1.1 2009/02/03 17:47:08 metson Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: Lexicon_t.py,v 1.2 2009/07/27 16:07:43 metson Exp $"
+__version__ = "$Revision: 1.2 $"
 
 import logging
 import unittest
@@ -15,20 +15,37 @@ import unittest
 from WMCore.Lexicon import *
 
 class LexiconTest(unittest.TestCase):
-    def runTest(self):
+    def testGoodSiteTier(self):
+        # Check that valid tiers work
         assert sitetier('T0'), 'valid tier not validated'
         assert sitetier('T1'), 'valid tier not validated'
         assert sitetier('T2'), 'valid tier not validated'
         assert sitetier('T3'), 'valid tier not validated'
         
+    def testBadSiteTier(self):    
+        # Check that invalid tiers raise an exception
         self.assertRaises(AssertionError, sitetier, 'T4')
         self.assertRaises(AssertionError, sitetier, 'D0')
-        
+    
+    def testGoodCMSName(self):    
+        # Check that full names work
         assert cmsname('T0_CH_CERN'), 'valid CMS name not validated'
         assert cmsname('T2_UK_SGrid_Bristol'), 'valid CMS name not validated'
+        
+    def testPartialCMSName(self):    
+        # Check that partial names work
+        assert cmsname('T2'), 'partial CMS name not validated'
+        assert cmsname('T2_'), 'partial CMS name not validated'
+        assert cmsname('T2_UK'), 'partial CMS name not validated'
+        assert cmsname('T2_UK_'), 'partial CMS name not validated'
+        assert cmsname('T2_UK_SGrid'), 'partial CMS name not validated'
+        assert cmsname('T2_UK_SGrid_'), 'partial CMS name not validated'
+        
+    def testBadCMSName(self):        
+        # Check that invalid names raise an exception
         self.assertRaises(AssertionError, cmsname, 'T5_UK_SGrid_Bristol')
         self.assertRaises(AssertionError, cmsname, 'D2_UK_SGrid_Bristol')
-        self.assertRaises(AssertionError, cmsname, 'T2_UK')
+        #self.assertRaises(AssertionError, cmsname, 'T2_UK')
             
 if __name__ == "__main__":
     unittest.main() 

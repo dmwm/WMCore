493d533774ef4417c02396bf6d3ecc5000f4efa9
sfoulkes <sfoulkes@4525493e-7705-40b1-a816-d608a930855b>
2010-08-30 11:01:49 +0000

    Another BossAir DAO from Matt.  Fixes #230
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8742 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/BossAir/MySQL/UpdateJobs.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+_NewJobs_
+
+MySQL implementation for updating jobs
+"""
+
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class UpdateJobs(DBFormatter):
+    """
+    _UpdateJobs_
+
+    Update jobs with new values
+    """
+
+
+    sql = """UPDATE bl_runjob SET wmbs_id = :jobid, grid_id = :gridid,
+               bulk_id = :bulkid,
+               sched_status = (SELECT id FROM bl_status WHERE name = :status),
+               retry_count = :retry_count
+               WHERE id = :id
+               """
+
+
+
+    def execute(self, jobs, conn = None, transaction = False):
+        """
+        _execute_
+
+        Update jobs with new values.
+        Mostly a maintenance script
+        """
+
+        if len(jobs) == 0:
+            return
+
+        binds = []
+        for job in jobs:
+            binds.append({'jobid': job['jobid'], 'gridid': job.get('gridid', None), 'bulkid': job.get('bulkid', None),
+                          'status': job.get('status', None), 'retry_count': job['retry_count'], 'id': job['id']})
+
+        result = self.dbi.processData(self.sql, binds, conn = conn,
+                                      transaction = transaction)
+
+        return

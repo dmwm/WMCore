0bab3d9146ab5bb4ee0fb07069f7b7037dc07b81
sfoulkes <sfoulkes@4525493e-7705-40b1-a816-d608a930855b>
2010-08-31 15:37:10 +0000

    Remove the dependency on TestInit.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8776 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/RequestManager/RequestDB/Connection.py
+++ src/python/WMCore/RequestManager/RequestDB/Connection.py
@@ -7,19 +7,10 @@ get a database connection and DAOFactory instance for the RequestDB package
 """
 
 import logging
-#CHANGED FVL (1 line)
 import threading
 
 from WMCore.DAOFactory import DAOFactory
-from WMQuality.TestInit import TestInit
 from WMCore.WMFactory import WMFactory
-
-
-def initReqDB():
-    # setup connection
-    testInit = TestInit('RequestDBConnection')
-    testInit.setLogging(logLevel = logging.DEBUG)
-    testInit.setDatabaseConnection()
 
 def getConnection():
     """

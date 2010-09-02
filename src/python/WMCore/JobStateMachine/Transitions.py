ba5a69a7f35e3741bfe2a0c3d78d0a11f2abbd26
sfoulkes <sfoulkes>
2010-09-02 16:48:46 +0000

    Add a killed state to the JSM.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8794 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/JobStateMachine/Transitions.py
+++ src/python/WMCore/JobStateMachine/Transitions.py
@@ -5,29 +5,30 @@ _Transitions_
 Controls what state transitions are allowed.
 """
 
-
 class Transitions(dict):
     """
     All allowed state transitions in the JSM.
     """
     def __init__(self):
         self.setdefault('none', ['new'])
-        self.setdefault('new', ['created', 'createfailed'])
-        self.setdefault('created', ['executing', 'submitfailed'])
-        self.setdefault('executing', ['complete', 'jobfailed'])
+        self.setdefault('new', ['created', 'createfailed', 'killed'])
+        self.setdefault('created', ['executing', 'submitfailed', 'killed'])
+        self.setdefault('executing', ['complete', 'jobfailed', 'killed'])
         self.setdefault('complete', ['jobfailed', 'success'])
-        self.setdefault('createfailed', ['createcooloff', 'exhausted'])
-        self.setdefault('submitfailed', ['submitcooloff', 'exhausted'])
-        self.setdefault('jobfailed', ['jobcooloff', 'exhausted'])
-        self.setdefault('createcooloff', ['new'])
-        self.setdefault('submitcooloff', ['created'])
-        self.setdefault('jobcooloff', ['created'])
+        self.setdefault('createfailed', ['createcooloff', 'exhausted', 'killed'])
+        self.setdefault('submitfailed', ['submitcooloff', 'exhausted', 'killed'])
+        self.setdefault('jobfailed', ['jobcooloff', 'exhausted', 'killed'])
+        self.setdefault('createcooloff', ['new', 'killed'])
+        self.setdefault('submitcooloff', ['created', 'killed'])
+        self.setdefault('jobcooloff', ['created', 'killed'])
         self.setdefault('success', ['cleanout'])
         self.setdefault('exhausted', ['cleanout'])
-
+        self.setdefault('killed', ['cleanout'])
 
     def states(self):
         """
+        _states_
+        
         Return a list of all known states, derive it in case we add new final
         states other than cleanout.
         """

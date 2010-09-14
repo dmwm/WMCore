b98102cab4dec487c1267f5af4b050eaeaec3bf9
Steve Foulkes <sfoulkes@fnal.gov>
2010-09-14 21:27:07 +0000

    Revert BossAir related changes due to mass confusion.
    From: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@8938 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/JobTracker/JobTrackerPoller.py
+++ src/python/WMComponent/JobTracker/JobTrackerPoller.py
@@ -19,41 +19,45 @@ from WMCore.WMFactory         import WMFactory
 
 from WMCore.JobStateMachine.ChangeState import ChangeState
 
-from WMCore.BossAir.BossAirAPI          import BossAirAPI
-
 class JobTrackerPoller(BaseWorkerThread):
     """
-    _JobTrackerPoller_
-    
-    Polls the BossAir database for complete jobs
-    Handles completed jobs
+    Polls for Error Conditions, handles them
     """
-
-    
     def __init__(self, config):
         """
         Initialise class members
         """
-
         BaseWorkerThread.__init__(self)
         self.config = config
 
         myThread = threading.currentThread()
 
-        
-        self.changeState   = ChangeState(self.config)
-        self.bossAir       = BossAirAPI(config = config)
-        self.daoFactory    = DAOFactory(package = "WMCore.WMBS",
-                                        logger = myThread.logger,
-                                        dbinterface = myThread.dbi)
+        #I'm not entirely happy with this
+        #But I think we need to keep global track of the failed jobs
+        self.failedJobs    = {}
+        self.runTimeLimit  = self.config.JobTracker.runTimeLimit
+        self.idleTimeLimit = self.config.JobTracker.idleTimeLimit
+        self.heldTimeLimit = self.config.JobTracker.heldTimeLimit
+        self.unknTimeLimit = self.config.JobTracker.unknTimeLimit
 
-        self.jobListAction = self.daoFactory(classname = "Jobs.GetAllJobs")
+        self.trackerInst   = None
+        self.changeState   = None
 
+        self.daoFactory = DAOFactory(package = "WMCore.WMBS",
+                                     logger = myThread.logger,
+                                     dbinterface = myThread.dbi)
+
+        # Create a factory to load plugins
+        self.pluginFactory = WMFactory("plugins",
+                                       self.config.JobTracker.pluginDir)
     
     def setup(self, parameters = None):
         """
         Load DB objects required for queries
         """
+
+        self.trackerInst = self.loadTracker()
+        self.changeState = ChangeState(self.config)
 
         return
 
@@ -98,57 +102,134 @@ class JobTrackerPoller(BaseWorkerThread):
         and passes that off to tracking.
         """
 
-        passedJobs = []
-        failedJobs = []
-        
-
-        # Get all jobs WMBS thinks are running
-        jobList = self.jobListAction.execute(state = "Executing")
+        jobListAction = self.daoFactory(classname = "Jobs.GetAllJobs")
+        jobList       = jobListAction.execute(state = "Executing")
 
         if jobList == []:
-            # No jobs: do nothing
+            return
+        
+        jobDictList = []
+        for job in jobList:
+            jobDictList.append({'id': job})
+
+        #locListAction = self.daoFactory(classname = "Jobs.GetLocation")
+        #locDict = locListAction.execute(jobid = jobDictList)
+        #logging.error("Have locations in trackJobs")
+        #logging.error(locDict)
+
+        #trackDict = self.getInfo(locDict)
+        trackDict = self.getInfo(jobDictList)
+
+        if 'TrackerFailure' in trackDict.keys():
+            # Then we experienced a tracker failure
+            # Do NOTHING
+            # The tracker could just be overloaded.
+            # Wait for it to sort itself out.
+            logging.error('Experienced tracker failure of type %s' % (trackDict['TrackerFailure']))
+            logging.error('Passing for this round')
             return
 
+        #logging.error("Have info")
+        #logging.info(trackDict)
+        
+        passedJobs, failedJobs = self.parseJobs(trackDict)
 
-        # Now get all jobs that BossAir thinks are complete
-        completeJobs = self.bossAir.getComplete()
+        #logging.error("In trackJobs")
+        #logging.error(passedJobs)
+        #logging.error(failedJobs)
 
-        for job in completeJobs:
-            if job['status'].lower() == 'timeout':
+        self.failJobs(failedJobs)
+        self.passJobs(passedJobs)
+
+
+
+        return
+
+    def getInfo(self, jobDict):
+        """
+        _getInfo_
+
+        Gets info on the individual jobs.  jobDict should be
+        of form [{'id': id, 'site_name': site_name}]
+        
+        """
+        trackerDict = self.trackerInst(jobDict)
+
+        return trackerDict
+
+    def parseJobs(self, trackDict):
+        """
+        _parseJobs_
+        
+        This parses the information that comes back from the tracker plugin
+        The tracker plugin should report information in the following form.
+        trackDict = {'jobName': {jobInfo}}
+        jobInfo = {'Status': string, 'StatusTime': int since entering status,
+        'StatusReason': string}
+        """
+
+        passedJobs = []
+        failedJobs = []
+
+
+        for job in trackDict.keys():
+            if trackDict[job]['Status'] == 'Running' and trackDict[job]['StatusTime'] > self.runTimeLimit:
                 failedJobs.append(job)
-            else:
+            elif trackDict[job]['Status'] == 'Idle' and trackDict[job]['StatusTime'] > self.idleTimeLimit:
+                failedJobs.append(job)
+            elif trackDict[job]['Status'] == 'Held' and trackDict[job]['StatusTime'] > self.heldTimeLimit:
+                failedJobs.append(job)
+            elif trackDict[job]['Status'] == 'Unknown' and trackDict[job]['StatusTime'] > self.unknTimeLimit:
+                failedJobs.append(job)
+            elif trackDict[job]['Status'] == 'NA' or trackDict[job]['Status'] == 'Complete':
+                # Well, then we're not sure what happened to it.
+                # Pass this on to the JobAccountant on
+                # the assumption that it finished
                 passedJobs.append(job)
 
 
-        # Assume all these jobs "passed" if they aren't in timeout
-        self.passJobs(passedJobs)
-        self.failJobs(failedJobs)
-
-        return
+        return passedJobs, failedJobs
 
 
     def failJobs(self, failedJobs):
         """
         _failJobs_
 
-        Dump those jobs that have failed due to timeout
+        Dump those jobs that have failed
         """
         if len(failedJobs) == 0:
             return
 
-        myThread = threading.currentThread()
-
+        #Kill them on the system
+        self.trackerInst.kill(failedJobs)
 
         # Load DAOs
         setFWJRAction = self.daoFactory(classname = "Jobs.SetFWJRPath")
         loadAction    = self.daoFactory(classname = "Jobs.LoadFromID")
 
-        jrBinds = []
+        #Mark 'em as failed
+        listOfJobs = []
+        binds      = []
+        jrBinds    = []
+        for jobID in failedJobs:
+            binds.append({"jobid": jobID})
+            
+        results = loadAction.execute(jobID = binds)
 
-        for job in failedJobs:
+        if not type(results) == list:
+            results = [results]
+
+        for entry in results:
+            # One job per entry
+            tmpJob = Job(id = entry['id'])
+            tmpJob.update(entry)
+            listOfJobs.append(tmpJob)
+
+
+        for job in listOfJobs:
             jrPath = os.path.join(job.getCache(),
                                   'Report.%i.pkl' % (job['retry_count']))
-            jrBinds.append({'jobid': job['id'], 'fwjrpath': jrPath})
+            jrBinds.append({'jobid': jobID, 'fwjrpath': jrPath})
             #job.setFWJRPath(os.path.join(job.getCache(),
             #                             'Report.%i.pkl' % (job['retry_count'])))
 
@@ -156,11 +237,8 @@ class JobTrackerPoller(BaseWorkerThread):
         # Set all paths at once
         myThread.transaction.begin()
         setFWJRAction.execute(binds = jrBinds)
-
-        print "Should be about to fail jobs"
-
         
-        self.changeState.propagate(failedJobs, 'jobfailed', 'executing')
+        self.changeState.propagate(listOfJobs, 'jobfailed', 'executing')
         myThread.transaction.commit()
 
 
@@ -185,9 +263,25 @@ class JobTrackerPoller(BaseWorkerThread):
         loadAction    = self.daoFactory(classname = "Jobs.LoadFromID")
         setFWJRAction = self.daoFactory(classname = "Jobs.SetFWJRPath")
 
+        binds = []
+        for jobID in passedJobs:
+            binds.append({"jobid": jobID})
+            
+        results = loadAction.execute(jobID = binds)
+        
+        listOfJobs = []
         jrBinds    = []
 
-        for job in passedJobs:
+        if not type(results) == list:
+            results = [results]
+        
+        for entry in results:
+            # One job per entry
+            tmpJob = Job(id = entry['id'])
+            tmpJob.update(entry)
+            listOfJobs.append(tmpJob)
+
+        for job in listOfJobs:
             jrPath = os.path.join(job.getCache(),
                                   'Report.%i.pkl' % (job['retry_count']))
             jrBinds.append({'jobid': job['id'], 'fwjrpath': jrPath})
@@ -197,12 +291,39 @@ class JobTrackerPoller(BaseWorkerThread):
         setFWJRAction.execute(binds = jrBinds)
 
 
-        logging.debug("Propagating jobs in jobTracker")
-        self.changeState.propagate(passedJobs, 'complete', 'executing')
+        logging.info("Propagating jobs in jobTracker")
+        self.changeState.propagate(listOfJobs, 'complete', 'executing')
         myThread.transaction.commit()
 
         return
                 
+    
 
+
+    def loadTracker(self, trackerName = None):
+        """
+        Gets the tracker plugin and returns an instance of that tracker. 
+        Pluging should be triggered through call()
+        """
+
+        if not trackerName:
+            trackerName = self.config.JobTracker.trackerName
+
+
+        loadedClass = self.pluginFactory.loadObject(classname = trackerName,
+                                                    args = self.config)
+
+        return loadedClass
+
+
+    def killJobs(self, jobList = []):
+        """
+        Uses the tracker plugin to kill jobs
+
+        """
+        self.trackerInst.getClassAds()
+        self.trackerInst.kill(jobList)
+
+        return
 
         

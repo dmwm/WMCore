0f0010d1cb3510c595322126d4582476d5f8a44a
Steve Foulkes <sfoulkes@fnal.gov>
2010-10-05 15:38:24 +0000

    Comment out missing templates.
    From: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@10544 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/HTTPFrontEnd/REST/Templates/__init__.py
+++ src/python/WMCore/HTTPFrontEnd/REST/Templates/__init__.py
@@ -6,8 +6,8 @@ Templates used by REST service
 
 
 
-from WMCore.HTTPFrontEnd.REST.Templates.templateAuth import templateAuth
+#from WMCore.HTTPFrontEnd.REST.Templates.templateAuth import templateAuth
 from WMCore.HTTPFrontEnd.REST.Templates.templateBottom import templateBottom
-from WMCore.HTTPFrontEnd.REST.Templates.templateDirContent import templateDirContent
-from WMCore.HTTPFrontEnd.REST.Templates.templateStartpage import templateStartpage
+#from WMCore.HTTPFrontEnd.REST.Templates.templateDirContent import templateDirContent
+#from WMCore.HTTPFrontEnd.REST.Templates.templateStartpage import templateStartpage
 from WMCore.HTTPFrontEnd.REST.Templates.templateTop import templateTop

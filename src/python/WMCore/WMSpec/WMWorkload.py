9d93a7aa58e043e5dc10be8c07f03d033980a090
Steve Foulkes <sfoulkes@fnal.gov>
2010-11-09 19:19:53 +0000

    Fix setting the min merge size, port changes from trunk.
    From: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/branches/WMCORE_0_5_X@10843 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMSpec/WMWorkload.py
+++ src/python/WMCore/WMSpec/WMWorkload.py
@@ -555,7 +555,7 @@ class WMWorkloadHelper(PersistencyHelper):
                                             max_merge_events = maxEvents)
             for stepName in task.listAllStepNames():
                 stepHelper = task.getStepHelper(stepName)                
-                if stepHelper.stepType() == "CMSSW":
+                if stepHelper.stepType() == "StageOut" and stepHelper.minMergeSize() != -1:
                     stepHelper.setMinMergeSize(minSize)
 
             self.setMergeParameters(minSize, maxSize, maxEvents, task)

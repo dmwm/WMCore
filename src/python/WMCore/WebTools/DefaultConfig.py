35e062a9b77d20c4b9ee96b38d24300654b92df4
Steve Foulkes <sfoulkes@fnal.gov>
2010-11-22 20:30:02 +0000

    Revert the last patch from #472 as it unexpectedly broke all of webtools stuff. More comments in trac.
    From: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@10923 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/DefaultConfig.py
+++ src/python/WMCore/WebTools/DefaultConfig.py
@@ -37,7 +37,7 @@ config.Webtools.application = 'WebtoolsDocs'
 # This is the config for the application
 config.component_('WebtoolsDocs')
 # Define the default location for templates for the app
-config.WebtoolsDocs.templates = os.path.join(getWMBASE(), 'src/templates/WMCore/WebTools')
+config.WebtoolsDocs.templates = os.path.join(getWMBASE(), '/src/templates/WMCore/WebTools')
 config.WebtoolsDocs.admin = 'your@email.com'
 config.WebtoolsDocs.title = 'CMS WMCore/WebTools Documentation'
 config.WebtoolsDocs.description = 'Documentation on the WMCORE/WebTools'
@@ -55,18 +55,20 @@ config.WebtoolsDocs.section_('views')
 active = config.WebtoolsDocs.views.section_('active')
 # This is the Security config the application will use
 config.component_('SecurityModule')
-# You can turn off security by setting
-#config.SecurityModule.dangerously_insecure = True
-# There should be a proper HMAC key file here, use this file as an 
-# example
-config.SecurityModule.key_file = os.path.join(getWMBASE(), 'src/python/WMCore/WebTools/DefaultConfig.py')
-#
-# I could secure all the pages in the web app to these settings using a default
-# configuration. This can still be over-ridden on a per page basis.
+#config.SecurityModule.app_url = 'https://cmsweb.cern.ch/myapp'
+#config.SecurityModule.app_url = 'http://%s:%d' % (config.Webtools.host,config.Webtools.port)
+config.SecurityModule.mount_point = 'auth'
+config.SecurityModule.store = 'filestore'
+config.SecurityModule.store_path = '/tmp/security-store'
+#config.CernOpenID.store.database = 'sqlite://'
+config.SecurityModule.session_name = 'SecurityModule'
+config.SecurityModule.oid_server = 'http://localhost:8400/'
+config.SecurityModule.handler = 'WMCore.WebTools.OidDefaultHandler'
+# I could secure all the pages in the web app to these settings like so
 #default = config.SecurityModule.section_('default')
-#default.role = ''
-#default.group = ''
-#default.site = ''
+#default.role = ['Admin']
+#default.group = ['CMS']
+#default.site = ['T2_BR_UERJ']
 
 # The section name is also the location the class will be located
 # e.g. http://localhost:8080/documentation

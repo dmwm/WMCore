86d2448c8de9b3f7cd6316840a837e5329cd1cec
Matt Norman <mnorman@fnal.gov>
2010-11-30 20:53:38 +0000

    Add failure information to the FWJR for LogCollect jobs.
    From: Matt Norman <mnorman@fnal.gov>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/branches/WMCORE_0_6_X@10983 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMSpec/Steps/Diagnostics/LogCollect.py
+++ src/python/WMCore/WMSpec/Steps/Diagnostics/LogCollect.py
@@ -30,31 +30,43 @@ class LCExceptionHandler(DiagnosticHandler):
         Twiddle thumbs, contemplate navel, toss coin
 
         """
-        jobRepXml = os.path.join(executor.step.builder.workingDir,
-                                 executor.step.output.jobReport)
+        msg         = "Error in LogCollect: %s\n" % (errCode)
+        description = "Misc. LogCollect error: %s\n" % (errCode)
+        if args.get('ex', None):
+            msg += str(args.get('ex'))
+        if args.get('ExceptionInstance', False):
+            msg += str(args.get('ExceptionInstance'))
 
-        if not os.path.exists(jobRepXml):
-            # no report => Error
-            msg = "No Job Report Found: %s" % jobRepXml
-            executor.report.addError(50115, "MissingJobReport", msg)
-            return
-        
-        # job report XML exists, load the exception information from it
-        executor.report.parse(jobRepXml)
+
+
+        #jobRepXml = os.path.join(executor.step.builder.workingDir,
+        #                         executor.step.output.jobReport)
+        #
+        #if not os.path.exists(jobRepXml):
+        #    # no report => Error
+        #    msg = "No Job Report Found: %s" % jobRepXml
+        #    executor.report.addError(50115, "MissingJobReport", msg)
+        #    return
+        #
+        ## job report XML exists, load the exception information from it
+        #executor.report.parse(jobRepXml)
         
         
         # make sure the report has the error in it
         errSection = getattr(executor.report.report, "errors", None)
         if errSection == None:
-            msg = "Job Report contains no error report, but StageOutManager exited non-zero: %s" % errCode
+            msg = "Job Report contains no error report, but LogCollect exited non-zero: %s" % errCode
             executor.report.addError(50116, "MissingErrorReport", msg)
             return
 
         else:
             #check exit code in report is non zero
             if executor.report.report.status == 0:
-                msg = "Job Report contains no error report, but StageOutManager exited non-zero: %s" % errCode
+                msg = "Job Report contains no error report, but LogCollect exited non-zero: %s" % errCode
                 executor.report.addError(50116, "MissingErrorReport", msg)
+
+        executor.report.addError(executor.step._internal_name,
+                                 errCode, description, msg)
         return
 
 class LogCollect(Diagnostic):

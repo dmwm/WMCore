68ad7057ae548ee1b02dd70b51a4e84d2435ef3a
Matt Norman <mnorman@fnal.gov>
2010-12-06 17:56:34 +0000

    Missed two files from the BossAir monitoring patch.
    From: Matt Norman <mnorman@fnal.gov>
    From: Seangchan Ryu <sryu@fnal.gov>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/branches/WMCORE_0_6_X@11056 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/BossAir/MySQL/JobStatusForMonitoring.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+"""
+_JobStatusMonitoring_
+
+MySQL implementation for loading a job by scheduler status
+"""
+
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class JobStatusForMonitoring(DBFormatter):
+    """
+    _LoadForMonitoring_
+
+    Load all jobs with a certain scheduler status including
+    all the joined information.
+    """
+
+
+    sql = """SELECT count(rj.wmbs_id) AS num_jobs, st.name AS status,
+                    wl.plugin AS plugin
+               FROM bl_runjob rj
+               INNER JOIN bl_status st ON rj.sched_status = st.id
+               INNER JOIN wmbs_job wj ON wj.id = rj.wmbs_id
+               INNER JOIN wmbs_jobgroup wjg ON wjg.id = wj.jobgroup
+               INNER JOIN wmbs_subscription ws ON ws.id = wjg.subscription
+               INNER JOIN wmbs_workflow wf ON wf.id = ws.workflow
+               LEFT OUTER JOIN wmbs_location wl ON wl.id = wj.location
+               WHERE rj.status = :complete
+               GROUP BY plugin, status
+    """
+
+    def mappedStatusFormat(self, results):
+        """
+        convert each indiviual batch system plugin status to a common status.
+        Warning: This assumes all the plugin are under WMCore/BossAir/Plugins/
+        and its module name and class name should be the same.
+        
+        __import__ doesn't reload if the module exist. 
+        (so doesn't need to keep track what is already imported. 
+        The performance difference should be small. If desired, maintain 
+        the cache to keep track imported plugIns
+        """
+        commonStates = {}
+        for data in results:
+            module = __import__("WMCore.BossAir.Plugins.%s" % data['plugin'], 
+                                globals(), locals(), [data['plugin']])
+            plugIn = getattr(module, data['plugin'])
+            state = plugIn.stateMap().get(data['status'])
+            commonStates.setdefault(state, 0)                             
+            commonStates[state] += data['num_jobs']
+        
+        return [{'status': status, 'num_jobs': numJobs} 
+                  for status, numJobs in commonStates.items()]
+        
+
+    def execute(self, commonFormat = True, conn = None, transaction = False):
+        """
+        _execute_
+
+        Load all jobs either running or not (running by default)
+        """
+        complete = '1'
+        binds = {'complete': complete}
+
+
+        result = self.dbi.processData(self.sql, binds, conn = conn,
+                                      transaction = transaction)
+        if commonFormat:
+            return self.mappedStatusFormat(self.formatDict(result))
+        else:
+            return self.formatDict(result)
+

d769a2a5dc94f2ad1ee043c83b3f4fe1ef2906dc
Matt Norman <mnorman@fnal.gov>
2010-12-14 21:45:18 +0000

    Fix BossAir Oracle issues.  Fixes #856.
    From: Matt Norman <mnorman@fnal.gov>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@11140 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/BossAir/MySQL/LoadRunning.py
+++ src/python/WMCore/BossAir/MySQL/LoadRunning.py
@@ -15,9 +15,9 @@ class LoadRunning(DBFormatter):
     Load all bl_runjob that are active
 
     """
-    sql = """SELECT rj.wmbs_id AS jobid, rj.grid_id AS gridid, rj.bulk_id AS bulkid, 
-               st.name AS status, rj.retry_count AS retry_count, rj.id AS id, 
-               rj.status_time AS status_time, wu.cert_dn AS user
+    sql = """SELECT rj.wmbs_id jobid, rj.grid_id gridid, rj.bulk_id bulkid, 
+               st.name status, rj.retry_count retry_count, rj.id id, 
+               rj.status_time status_time, wu.cert_dn AS userdn
              FROM bl_runjob rj 
              INNER JOIN bl_status st ON rj.sched_status = st.id
              INNER JOIN wmbs_users wu ON wu.id = rj.user_id

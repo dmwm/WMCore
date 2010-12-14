7f425ea964e33dbad94c4e8d24b44ce8b0eef66d
Matt Norman <mnorman@fnal.gov>
2010-12-14 19:47:31 +0000

    The ResourceControl should account for site white/black lists when determining which sites are empty.  Also throw in some BossAir fixes.
    From: Matt Norman <mnorman@fnal.gov>
    From: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@11125 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/MySQL/Subscriptions/GetValidation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+_GetValidation_
+
+MySQL implementation of Subscription.GetValidation
+"""
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class GetValidation(DBFormatter):
+    sql = """SELECT wmbs_location.site_name, wmbs_subscription_validation.valid
+                    FROM wmbs_location
+               INNER JOIN wmbs_subscription_validation ON
+                 wmbs_location.id = wmbs_subscription_validation.location_id AND
+                 wmbs_subscription_validation.subscription_id = :id"""
+                 
+    def execute(self, id, conn = None, transaction = False):
+        results = self.dbi.processData(self.sql, {"id": id},
+                                       transaction = transaction)
+        formattedResults = self.formatDict(results)
+        for formattedResult in formattedResults:
+            if formattedResult["valid"] == 1:
+                formattedResult = True
+            else:
+                formattedResult = False
+
+        return formattedResults

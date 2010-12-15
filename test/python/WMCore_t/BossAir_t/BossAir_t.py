7a8faa524bbe6e22f4fa7cfda8fa425356f53506
Matt Norman <mnorman@fnal.gov>
2010-12-15 17:46:39 +0000

    Fold the JobStatus config into the JobStatusLite section.  Fixes #834.
    From: Matt Norman <mnorman@fnal.gov>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/branches/WMCORE_0_6_X@11159 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/BossAir_t/BossAir_t.py
+++ test/python/WMCore_t/BossAir_t/BossAir_t.py
@@ -216,15 +216,11 @@ class BossAirTest(unittest.TestCase):
         config.JobStateMachine.couchDBName     = "mnorman_test"
 
 
-        # JobStatus
-        config.component_('JobStatus')
-        config.JobStatus.stateTimeouts  = {'Pending': 20, 'Running': 86400}
-        config.JobStatus.pollInterval   = 1
-
-        # JobStatusLite (LEGACY)
+        # JobStatusLite
         config.component_('JobStatusLite')
         config.JobStatusLite.componentDir = os.path.join(os.getcwd(), 'Components')
-
+        config.JobStatusLite.stateTimeouts = {'Pending': 20, 'Running': 86400}
+        config.JobStatusLite.pollInterval = 1
 
 
         return config
@@ -829,7 +825,7 @@ class BossAirTest(unittest.TestCase):
         # So at the end of several polling cycles, the jobs should all
         # be done, but be in the failed status (they timed out)
 
-        time.sleep(10)
+        time.sleep(20)
 
 
         myThread.workerThreadManager.terminateWorkers()

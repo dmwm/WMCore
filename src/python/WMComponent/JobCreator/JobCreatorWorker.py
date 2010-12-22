a225a3f8fd4b08fed903f6dabb66fd890935945a
sfoulkes <sfoulkes>
2010-12-22 19:39:18 +0000

    Revert "Hacks to work around JobCreator and work killing bugs."
    This reverts commit 01d3b577387adf370a0ef63e46bc3577ff6bbe2e.
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/branches/WMCORE_0_6_X@11230 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/JobCreator/JobCreatorWorker.py
+++ src/python/WMComponent/JobCreator/JobCreatorWorker.py
@@ -290,7 +290,6 @@ class JobCreatorWorker:
             jobSplittingFunction = runSplitter(jobFactory = wmbsJobFactory,
                                                splitParams = splitParams)
             while continueSubscription:
-                continueSubscription = False
                 # This loop runs over the jobFactory,
                 # using yield statements and a pre-existing proxy to
                 # generate and process new jobs
--- src/python/WMComponent/WorkQueueManager/WorkQueueManagerReqMgrPoller.py
+++ src/python/WMComponent/WorkQueueManager/WorkQueueManagerReqMgrPoller.py
@@ -171,7 +171,7 @@ class WorkQueueManagerReqMgrPoller(BaseWorkerThread):
             updated_elements = sum([list(x['Elements']) for x in updated], [])
             self.wq.setReqMgrUpdate(now, *[y['Id'] for y in updated_elements])
 
-            #self.wq.deleteFinisedWork(updated)
+            self.wq.deleteFinisedWork(updated)
 
     def reportRequestStatusToReqMgr(self, request, status, message = None):
         """Change state in RequestManager

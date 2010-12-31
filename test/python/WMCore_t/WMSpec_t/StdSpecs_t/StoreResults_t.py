f0d2222582b75d9dbc76a701548693ddc0d5c24e
Eric Vaandering <ewv@fnal.gov>
2010-12-31 00:12:04 +0000

    Add StoreResults workflow.
    Closes #641
    
    From: Eric Vaandering <ewv@fnal.gov>
    Signed-off-by: Stuart Wakefield <stuart.wakefield@imperial.ac.uk>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@11241 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/python/WMCore_t/WMSpec_t/StdSpecs_t/StoreResults_t.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+"""
+_StoreResults_t_
+
+Unit tests for the StoreResults workflow.
+"""
+
+import unittest
+import os
+import threading
+
+from WMCore.WMBS.Fileset import Fileset
+from WMCore.WMBS.Subscription import Subscription
+from WMCore.WMBS.Workflow import Workflow
+
+from WMCore.WorkQueue.WMBSHelper import WMBSHelper
+from WMCore.WMSpec.StdSpecs.StoreResults import getTestArguments, storeResultsWorkload
+
+from WMQuality.TestInit import TestInit
+
+class StoreResultsTest(unittest.TestCase):
+    def setUp(self):
+        """
+        _setUp_
+
+        Initialize the database.
+        """
+        self.testInit = TestInit(__file__)
+        self.testInit.setLogging()
+        self.testInit.setDatabaseConnection()
+        self.testInit.setSchema(customModules = ["WMCore.WMBS"],
+                                useDefault = False)
+        return
+
+    def tearDown(self):
+        """
+        _tearDown_
+
+        Clear out the database.
+        """
+        self.testInit.clearDatabase()
+        return
+
+    def testStoreResults(self):
+        """
+        _testStoreResults_
+
+        Create a StoreResults workflow and verify it installs into WMBS
+        correctly.
+        """
+        testWorkload = storeResultsWorkload("TestWorkload", getTestArguments())
+        testWorkload.setSpecUrl("somespec")
+        testWorkload.setOwnerDetails("ewv@fnal.gov", "DMWM")
+
+        testWMBSHelper = WMBSHelper(testWorkload, "SomeBlock")
+        testWMBSHelper.createSubscription()
+
+        testWorkflow = Workflow(name = "TestWorkload",
+                                task = "/TestWorkload/StoreResults")
+        testWorkflow.load()
+
+        self.assertEqual(len(testWorkflow.outputMap.keys()), 2,
+                         "Error: Wrong number of WF outputs.")
+
+        goldenOutputMods = ["Merged"]
+        for goldenOutputMod in goldenOutputMods:
+            mergedOutput = testWorkflow.outputMap[goldenOutputMod]["merged_output_fileset"]
+            unmergedOutput = testWorkflow.outputMap[goldenOutputMod]["output_fileset"]
+
+            mergedOutput.loadData()
+            unmergedOutput.loadData()
+
+            self.assertEqual(mergedOutput.name, "/TestWorkload/StoreResults/merged-%s" % goldenOutputMod,
+                             "Error: Merged output fileset is wrong: %s" % mergedOutput.name)
+            self.assertEqual(unmergedOutput.name, "/TestWorkload/StoreResults/merged-%s" % goldenOutputMod,
+                             "Error: Unmerged output fileset is wrong: %s."%unmergedOutput.name)
+
+        logArchOutput = testWorkflow.outputMap["logArchive"]["merged_output_fileset"]
+        unmergedLogArchOutput = testWorkflow.outputMap["logArchive"]["output_fileset"]
+        logArchOutput.loadData()
+        unmergedLogArchOutput.loadData()
+
+
+        self.assertEqual(logArchOutput.name, "/TestWorkload/StoreResults/merged-logArchive",
+                         "Error: LogArchive output fileset is wrong.")
+        self.assertEqual(unmergedLogArchOutput.name, "/TestWorkload/StoreResults/merged-logArchive",
+                         "Error: LogArchive output fileset is wrong.")
+
+
+        topLevelFileset = Fileset(name = "TestWorkload-StoreResults-SomeBlock")
+        topLevelFileset.loadData()
+
+        procSubscription = Subscription(fileset = topLevelFileset, workflow = testWorkflow)
+        procSubscription.loadData()
+
+        self.assertEqual(procSubscription["type"], "Merge",
+                         "Error: Wrong subscription type.")
+        self.assertEqual(procSubscription["split_algo"], "ParentlessMergeBySize",
+                         "Error: Wrong split algo.")
+
+
+        return
+
+if __name__ == '__main__':
+    unittest.main()

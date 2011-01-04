2fb947e943a897af636c9a433ba01dc897bb2e42
Seangchan Ryu <sryu@fnal.gov>
2011-01-04 22:56:10 +0000

    Fix progress bar in WorkQueue.
    Fixes #895
    
    From: Seangchan Ryu <sryu@fnal.gov>
    Signed-off-by: Stuart Wakefield <stuart.wakefield@imperial.ac.uk>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/branches/WMCORE_0_6_X@11246 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WorkQueue/Database/MySQL/Monitor/Workloads/WorkloadsWithProgress.py
+++ src/python/WMCore/WorkQueue/Database/MySQL/Monitor/Workloads/WorkloadsWithProgress.py
@@ -17,7 +17,8 @@ from WMCore.WorkQueue.Database import States
 class WorkloadsWithProgress(DBFormatter):
     sql = """SELECT ws.id as spec_id, ws.name as spec_name, 
                     url, owner, CAST(SUM(we.num_jobs) AS SIGNED) as total, 
-                    CAST(SUM(we.percent_complete) AS SIGNED) as done  
+                    CAST(SUM(we.percent_complete * we.num_jobs / 100) AS SIGNED)
+                    as done
              From wq_wmspec ws 
              INNER JOIN wq_wmtask wt ON (wt.wmspec_id = ws.id)
              INNER JOIN wq_element we ON (we.wmtask_id = wt.id) 

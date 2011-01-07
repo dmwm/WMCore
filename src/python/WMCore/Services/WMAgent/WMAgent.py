605fe28ccf2024c0fe579d9c3d36fbda8ac77dd9
Seangchan Ryu <sryu@fnal.gov>
2011-01-07 17:01:28 +0000

    Propagate agent component hartbeat's to request overview
    Closes #383
    
    From: Seangchan Ryu <sryu@fnal.gov>
    Signed-off-by: Stuart Wakefield <stuart.wakefield@imperial.ac.uk>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@11257 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/Services/WMAgent/WMAgent.py
@@ -0,0 +1,60 @@
+import os
+import pwd
+import logging
+from WMCore.Services.Service import Service
+from WMCore.Wrappers import JsonWrapper
+
+class WMAgent(Service):
+
+    """
+    API for dealing with retrieving information from WMAgent RESTModel Service
+    """
+
+    def __init__(self, dict={}):
+        """
+        responseType will be either xml or json
+        """
+
+        #if self.responseType == 'json':
+            #self.parser = JSONParser()
+        #elif self.responseType == 'xml':
+            #self.parser = XMLParser()
+
+        dict.setdefault("accept_type", "application/json")
+        dict.setdefault("content_type", "application/json")
+        self.encoder = JsonWrapper.dumps
+        self.decoder = JsonWrapper.loads
+
+        Service.__init__(self, dict)
+
+    def _getResult(self, callname, clearCache = True,
+                   args = None, verb="GET", contentType = None):
+        """
+        _getResult_
+
+        retrieve JSON/XML formatted information given the service name and the
+        argument dictionaries
+
+        TODO: Probably want to move this up into Service
+        """
+        result = ''
+        # make base file name from call name.
+        file = callname.replace("/", "_")
+        if clearCache:
+            self.clearCache(file, args, verb)
+
+        # can't pass the decoder here since refreshCache wright to file
+        f = self.refreshCache(file, callname, args, encoder = self.encoder,
+                              verb = verb, contentType = contentType)
+        result = f.read()
+        f.close()
+        result = self.decoder(result)
+
+        return result
+
+    def getAgentStatus(self, detail = False):
+        """
+        """
+        callname = 'agentstatus'
+        args = {'detail': detail}
+        return self._getResult(callname, args = args)

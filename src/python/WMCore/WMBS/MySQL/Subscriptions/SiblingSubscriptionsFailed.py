dfbcbe237cba6900ad2ad6f6eb8458831105bee4
Steve Foulkes <sfoulkes@fnal.gov>
2011-01-11 14:22:18 +0000

    Handle failed files properly for WMBS cleanup
    Previously if downstream processing failed files would be left behind
    in the database and would prevent subscription cleanup from running
    
    Fixes #915
    
    From: Steve Foulkes <sfoulkes@fnal.gov>
    Signed-off-by: Stuart Wakefield <stuart.wakefield@imperial.ac.uk>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@11269 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/WMBS/MySQL/Subscriptions/SiblingSubscriptionsFailed.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+_SiblingSubscriptionsFailed_
+
+MySQL implementation of Subscription.SiblingSubscriptionsFailed
+"""
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class SiblingSubscriptionsFailed(DBFormatter):
+    # Find files that have failed processing and mark them as complete for the
+    # cleanup subscription.  These files should not be cleaned up as they may be
+    # used to investigate why the job failed.  This should only be run after the
+    # input fileset has been closed.
+    sql = """SELECT DISTINCT wmbs_fileset_files.fileid AS fileid,
+                             :subscription AS subscription FROM wmbs_fileset_files
+               INNER JOIN wmbs_subscription ON
+                 wmbs_fileset_files.fileset = wmbs_subscription.fileset
+               INNER JOIN wmbs_sub_files_failed ON
+                 wmbs_fileset_files.fileid = wmbs_sub_files_failed.fileid AND
+                 wmbs_subscription.id = wmbs_sub_files_failed.subscription
+             WHERE wmbs_fileset_files.fileset = :fileset"""
+
+    delete = """DELETE FROM wmbs_sub_files_available
+                WHERE subscription = :subscription AND
+                      fileid = :fileid"""
+
+    insert = """INSERT INTO wmbs_sub_files_complete (subscription, fileid) VALUES
+                  (:subscription, :fileid)"""
+
+    def execute(self, subscription, fileset, conn = None, transaction = False):
+        results = self.dbi.processData(self.sql, {"fileset": fileset,
+                                                  "subscription": subscription},
+                                       conn = conn, transaction = transaction)
+        failedFiles = self.formatDict(results)
+
+        if len(failedFiles) > 0:
+            self.dbi.processData(self.delete, failedFiles, conn = conn,
+                                 transaction = transaction)
+            self.dbi.processData(self.insert, failedFiles, conn = conn,
+                                 transaction = transaction)
+        return
---
+++ src/python/WMCore/WMBS/Oracle/Subscriptions/SiblingSubscriptionsFailed.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+_SiblingSubscriptionsFailed_
+
+Oracle implementation of Subscription.SiblingSubscriptionsFailed
+"""
+
+from WMCore.WMBS.MySQL.Subscriptions.SiblingSubscriptionsFailed import \
+    SiblingSubscriptionsFailed as SiblingFailedMySQL
+
+class SiblingSubscriptionsFailed(SiblingFailedMySQL):
+    pass

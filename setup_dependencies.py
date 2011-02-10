e4e70ceed6053f159c9dda62b666773d93630de6
Simon Metson <simonmetson@googlemail.com>
2011-02-10 23:45:27 +0000

    Fix up workqueue dependencies, fixes #904
    From: Simon Metson <simonmetson@googlemail.com>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@11566 4525493e-7705-40b1-a816-d608a930855b

--- setup_dependencies.py
+++ setup_dependencies.py
@@ -5,22 +5,28 @@ A system can depend on packages or other systems.
 
 """
 dependencies = {
-               'wmc-base':{
+                'wmc-base':{
+                        'packages' : ['WMCore.DataStructs'],
                         'modules': ['WMCore.WMFactory', 'WMCore.WMException', 'WMCore.Configuration',
                                     'WMCore.WMExceptions', 'WMCore.WMFactory', 'WMCore.Lexicon',
-                                    'WMCore.WMBase', 'WMCore.WMLogging']
+                                    'WMCore.WMBase', 'WMCore.WMLogging', 'WMCore.Algorithms.Permissions'],
                         },
-               'wmc-database':{
+                'wmc-component':{
+                        'packages': ['WMCore.MsgService', 'WMCore.WorkerThreads', 'WMCore.ThreadPool'],
+                        'modules': ['WMComponent.__init__'],
+                        'systems': ['wmc-base']
+                        },
+                'wmc-database':{
                         'packages': ['WMCore.Wrappers', 'WMCore.GroupUser', 'WMCore.DataStructs', 'WMCore.Database',
                                     'WMCore.Algorithms', 'WMCore.Services', 'WMCore.Cache'],
                         'modules': ['WMCore.Action', 'WMCore.WMConnectionBase', 'WMCore.DAOFactory', 'WMCore.WMInit'],
                         'systems':['wmc-base']
                         },
-               'wmc-runtime':{
-                        'packages': ['WMCore.WMRuntime', 'WMCore.WMSpec'],
+                'wmc-runtime':{
+                        'packages': ['WMCore.WMRuntime', 'WMCore.WMSpec', 'PSetTweaks', 'WMCore.FwkJobReport'],],
                         'systems':['wmc-base']
                         },
-               'wmc-web':{
+                'wmc-web':{
                         'packages': ['WMCore.WebTools', 'WMCore.Agent', 'WMCore.WorkerThreads'],
                         'systems':['wmc-database', 'wmc-base'],
                         'statics': ['src/javascript/WMCore/WebTools',
@@ -31,20 +37,27 @@ dependencies = {
                                 'src/templates/WMCore/WebTools',
                                 'src/templates/WMCore/WebTools/Masthead',]
                         },
-               'reqmgr':{
+                'reqmgr':{
                         'packages': ['WMCore.RequestManager', 'WMCore.HTTPFrontEnd'],
                         'systems':['wmc-web', 'wmc-runtime'],
                         'statics': ['src/javascript/WMCore/WebTools/RequestManager',
                                     'src/templates/WMCore/WebTools/RequestManager',
                                     'src/html/RequestManager'],
                         },
-               'workqueue':{
-                        'packages': ['WMComponent.WorkQueueManager', 'WMCore.WorkQueue'],
-                        'systems': ['wmc-web', 'wmc-database', 'wmc-base'],
+                'workqueue':{
+                        'packages': ['WMComponent.WorkQueueManager', 'WMCore.WorkerThreads',
+                                    'WMCore.WorkQueue','WMCore.Services', 'WMCore.Wrappers',
+                                    'WMQuality.Emulators',' WMCore.WMSpec'],
+                        'modules' : ['WMQuality.__init__'],
+                        'systems': ['wmc-web', 'wmc-database', 'wmc-base', 'wmc-component'],
                         'statics': ['src/templates/WMCore/WebTools/WorkQueue',]
                         },
-               'wmagent':{
-                        'packages': [],
+                'wmagent':{
+                        'packages': ['WMCore.Agent', 'WMCore.WMBS', 'WMCore.Algorithms',
+                                    'WMCore.JobStateMachine', 'WMComponent.DBSBuffer',
+                                    'WMCore.HTTPFrontEnd', 'WMCore.ThreadPool',
+                                    'WMCore.BossAir',
+                                    'WMCore.ResourceControl'],
                         'systems':['wmc-web', 'wmc-database', 'workqueue', 'wmc-runtime'],
                         'statics': ['src/javascript/WMCore/WebTools/Agent',
                                 'src/javascript/WMCore/WebTools/WMBS',

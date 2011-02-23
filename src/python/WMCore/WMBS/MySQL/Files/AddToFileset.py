b9d1ac3b956bebac43ab9efe1105234d7a1effb2
Matt Norman <mnorman@fnal.gov>
2011-02-23 22:44:53 +0000

    Files.AddToFileset Should Use Insert Ignore.  Fixes #1155.
    From: Matt Norman <mnorman@fnal.gov>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@11639 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Files/AddToFileset.py
+++ src/python/WMCore/WMBS/MySQL/Files/AddToFileset.py
@@ -10,25 +10,19 @@ import time
 from WMCore.Database.DBFormatter import DBFormatter
 
 class AddToFileset(DBFormatter):
-    sql = """INSERT INTO wmbs_fileset_files (fileid, fileset, insert_time)
+    sql = """INSERT IGNORE INTO wmbs_fileset_files (fileid, fileset, insert_time)
                SELECT wmbs_file_details.id, :fileset, :insert_time
                FROM wmbs_file_details
                WHERE wmbs_file_details.lfn = :lfn
-               AND NOT EXISTS (SELECT fileid FROM wmbs_fileset_files wff2 WHERE
-                                wff2.fileid = wmbs_file_details.id
-                                AND wff2.fileset = :fileset)"""
+               """
 
-    sqlAvail = """INSERT INTO wmbs_sub_files_available (subscription, fileid)
+    sqlAvail = """INSERT IGNORE INTO wmbs_sub_files_available (subscription, fileid)
                     SELECT wmbs_subscription.id AS subscription,
                            wmbs_file_details.id AS fileid FROM wmbs_subscription
                       INNER JOIN wmbs_file_details ON
                         wmbs_file_details.lfn = :lfn
-                    WHERE wmbs_subscription.fileset = :fileset AND NOT EXISTS
-                      (SELECT * FROM wmbs_sub_files_available
-                       WHERE fileid = (SELECT id FROM wmbs_file_details
-                                     WHERE lfn = :lfn) AND
-                             subscription = (SELECT id FROM wmbs_subscription
-                                             WHERE fileset = :fileset LIMIT 1))"""
+                    WHERE wmbs_subscription.fileset = :fileset 
+                    """
         
     def execute(self, file = None, fileset = None, conn = None,
                 transaction = False):

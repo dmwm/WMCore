e81f1b605b7eb818d2c9e5511eb28247a3381e50
spiga <spiga@cern.ch>
2011-03-03 13:30:52 +0000

    addding missing analysisrequest, fixes #206
    From: spiga <spiga@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@11765 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/RequestManager/RequestMaker/Processing/AnalysisRequest.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+_AnalysisRequest_
+"""
+
+from WMCore.RequestManager.RequestMaker.RequestMakerInterface import RequestMakerInterface
+from WMCore.RequestManager.DataStructs.RequestSchema import RequestSchema
+from WMCore.RequestManager.RequestMaker.Registry import registerRequestType
+from WMCore.WMSpec.StdSpecs.Analysis import AnalysisWorkloadFactory
+
+
+class AnalysisRequest(RequestMakerInterface):
+    """
+    _AnalysisRequest_
+
+    """
+    def __init__(self):
+        RequestMakerInterface.__init__(self)
+
+    def makeWorkload(self, schema):
+        factory = AnalysisWorkloadFactory()
+        return factory(schema['RequestName'], schema).data
+
+class AnalysisSchema(RequestSchema):
+    def __init__(self):
+        RequestSchema.__init__(self)
+
+        self.validateFields = []
+        self.optionalFields = []
+
+        self.validateFields = [
+            "CMSSWVersion",
+            "ScramArch",
+            "InputDataset"
+            ]
+        self.optionalFields = [
+            "SiteWhitelist",
+            "SiteBlacklist",
+            "BlockWhitelist",
+            "BlockBlacklist",
+            "RunWhitelist",
+            "RunBlacklist",
+            "CouchUrl",
+            "CouchDBName",
+            "DbsUrl",
+            ]
+
+    def validate(self):
+        RequestSchema.validate(self)
+
+registerRequestType("Analysis", AnalysisRequest, AnalysisSchema)

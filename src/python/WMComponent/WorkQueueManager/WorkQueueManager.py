ae6a49c2d34482920fd2d90e831a013d9c3ed94d
Stuart Wakefield <stuart.wakefield@imperial.ac.uk>
2011-03-09 21:00:16 +0000

    Remove WorkQueue cli dependency on WMComponent
    Closes #1220
    
    From: Stuart Wakefield <stuart.wakefield@imperial.ac.uk>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@11861 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/WorkQueueManager/WorkQueueManager.py
+++ src/python/WMComponent/WorkQueueManager/WorkQueueManager.py
@@ -4,12 +4,7 @@ WorkQueuemanager component
 
 Runs periodic tasks for WorkQueue
 """
-
-
-
-
 import threading
-from os import path
 
 from WMCore.Agent.Harness import Harness
 
@@ -19,11 +14,7 @@ from WMComponent.WorkQueueManager.WorkQueueManagerLocationPoller import WorkQueu
 from WMComponent.WorkQueueManager.WorkQueueManagerCleaner import WorkQueueManagerCleaner
 from WMComponent.WorkQueueManager.WorkQueueManagerWMBSFileFeeder import WorkQueueManagerWMBSFileFeeder
 
-from WMCore.WorkQueue.WorkQueue import localQueue, globalQueue, WorkQueue
-
-# Should probably import this but don't want to create the dependency
-WORKQUEUE_REST_NAMESPACE = 'WMCore.HTTPFrontEnd.WorkQueue.WorkQueueRESTModel'
-WORKQUEUE_MONITOR_NAMESPACE = 'WMCore.HTTPFrontEnd.WorkQueue.WorkQueueMonitorPage'
+from WMCore.WorkQueue.WorkQueueUtils import queueFromConfig, queueConfigFromConfigObject
 
 class WorkQueueManager(Harness):
     """WorkQueuemanager component
@@ -34,87 +25,14 @@ class WorkQueueManager(Harness):
     def __init__(self, config):
         # call the base class
         Harness.__init__(self, config)
-        self.config = self.setConfig(config)
-        self.logger = None
+        self.config = queueConfigFromConfigObject(config)
 
-
-    def setConfig(self, config):
-        """
-        Set various defaults if not provided by the user
-        """
-        wqManager = config.section_('WorkQueueManager')
-        if not hasattr(wqManager, 'componentDir'):
-            wqManager.componentDir = path.join(config.General.WorkDir,
-                                               'WorkQueueManager')
-        if not hasattr(wqManager, 'namespace'):
-            wqManager.namespace = 'WMComponent.WorkQueueManager.WorkQueueManager'
-        if not hasattr(wqManager, 'logLevel'):
-            wqManager.logLevel = 'INFO'
-        if not hasattr(wqManager, 'pollInterval'):
-            wqManager.pollInterval = 600
-
-        # WorkQueue config
-        if not hasattr(wqManager, 'queueParams'):
-            wqManager.queueParams = {}
-        qConfig = wqManager.queueParams
-        qConfig.setdefault('CacheDir', path.join(wqManager.componentDir, 'wf'))
-
-        if hasattr(wqManager, 'couchurl'):
-            wqManager.queueParams['CouchUrl'] = wqManager.couchurl
-        if hasattr(wqManager, 'dbname'):
-            wqManager.queueParams['DbName'] = wqManager.dbname
-        if hasattr(wqManager, 'inboxDatabase'):
-            wqManager.queueParams['InboxDbName'] = wqManager.inboxDatabase
-            
-        qConfig["BossAirConfig"] = getattr(config.WorkQueueManager, "BossAirConfig", None)
-        qConfig["JobDumpConfig"] = getattr(config.WorkQueueManager, "JobDumpConfig", None)
-
-        try:
-            monitorURL = ''
-            queueFlag = False
-            for webapp in config.listWebapps_():
-                webapp = config.webapp_(webapp)
-                for page in webapp.section_('views').section_('active'):
-                    
-                    if not queueFlag and hasattr(page, "model") \
-                       and page.section_('model').object == WORKQUEUE_REST_NAMESPACE:
-                        qConfig['QueueURL'] = 'http://%s:%s/%s/%s' % (webapp.Webtools.host,
-                                                                  webapp.Webtools.port,
-                                                                  webapp._internal_name.lower(),
-                                                                  page._internal_name)
-                        queueFlag = True
-                        
-                    if page.object == WORKQUEUE_MONITOR_NAMESPACE:
-                        monitorURL = 'http://%s:%s/%s/%s' % (webapp.Webtools.host,
-                                                          webapp.Webtools.port,
-                                                          webapp._internal_name.lower(),
-                                                          page._internal_name)
-            if not queueFlag:
-                raise RuntimeError
-            
-        except RuntimeError:
-            msg = """Unable to determine WorkQueue QueueURL, Either:
-            Configure a WorkQueueRESTModel webapp_ section or,
-            Add a WorkQueueManager.queueParams.QueueURL setting."""
-            raise RuntimeError, msg
-
-        if not qConfig.has_key('Teams') and hasattr(config.Agent, 'teamName'):
-            qConfig['Teams'] = config.Agent.teamName
-
-        # ReqMgr params
-        if not hasattr(wqManager, 'reqMgrConfig'):
-            wqManager.reqMgrConfig = {}
-        wqManager.reqMgrConfig['QueueURL'] = qConfig['QueueURL']
-        wqManager.reqMgrConfig['MonitorURL'] = monitorURL
-
-        return config
 
     def preInitialization(self):
         print "WorkQueueManager.preInitialization"
 
         # Add event loop to worker manager
         myThread = threading.currentThread()
-        self.logger = myThread.logger
         pollInterval = self.config.WorkQueueManager.pollInterval
 
         ### Global queue special functions
@@ -122,9 +40,9 @@ class WorkQueueManager(Harness):
 
             # Get work from ReqMgr, report back & delete finished requests
             myThread.workerThreadManager.addWorker(
-                                WorkQueueManagerReqMgrPoller( 
-                                        self.instantiateQueue(self.config),
-                                        getattr(self.config.WorkQueueManager, 
+                                WorkQueueManagerReqMgrPoller(
+                                        queueFromConfig(self.config),
+                                        getattr(self.config.WorkQueueManager,
                                                 'reqMgrConfig', {})
                                         ),
                                  pollInterval)
@@ -134,37 +52,24 @@ class WorkQueueManager(Harness):
 
             # pull work from parent queue
             myThread.workerThreadManager.addWorker(
-                                WorkQueueManagerWorkPoller(self.instantiateQueue(self.config)), 
+                                WorkQueueManagerWorkPoller(queueFromConfig(self.config)),
                                 pollInterval)
 
             # inject acquired work into wmbs
             myThread.workerThreadManager.addWorker(
-                                WorkQueueManagerWMBSFileFeeder(self.instantiateQueue(self.config)), 
+                                WorkQueueManagerWMBSFileFeeder(queueFromConfig(self.config)),
                                 pollInterval)
 
         ### general functions
 
         # Data location updates
         myThread.workerThreadManager.addWorker(
-                                    WorkQueueManagerLocationPoller(self.instantiateQueue(self.config)),
+                                    WorkQueueManagerLocationPoller(queueFromConfig(self.config)),
                                     pollInterval)
 
         # Clean finished work & apply end policies
         myThread.workerThreadManager.addWorker(
-                                WorkQueueManagerCleaner(self.instantiateQueue(self.config)), 
+                                WorkQueueManagerCleaner(queueFromConfig(self.config)),
                                 pollInterval)
 
         return
-
-    def instantiateQueue(self, config):
-        """
-        Create an appropriate queue
-        """
-        config.WorkQueueManager.queueParams.setdefault('logger', self.logger)
-        self.logger.info("Creating %s queue" % config.WorkQueueManager.level)
-        if config.WorkQueueManager.level == 'GlobalQueue':
-            return globalQueue(**config.WorkQueueManager.queueParams)
-        elif config.WorkQueueManager.level == 'LocalQueue':
-            return localQueue(**config.WorkQueueManager.queueParams)
-        else:
-            return WorkQueue(**config.WorkQueueManager.queueParams)
--- src/python/WMCore/WorkQueue/WorkQueueUtils.py
+++ src/python/WMCore/WorkQueue/WorkQueueUtils.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 """Various helper functions for workqueue"""
 
-__all__ = ['get_remote_queue', 'get_dbs', 'sitesFromStorageEelements']
+__all__ = ['get_remote_queue', 'get_dbs', 'sitesFromStorageEelements',
+           'queueConfigFromConfigObject', 'queueFromConfig']
+
+# Should probably import this but don't want to create the dependency
+WORKQUEUE_REST_NAMESPACE = 'WMCore.HTTPFrontEnd.WorkQueue.WorkQueueRESTModel'
+WORKQUEUE_MONITOR_NAMESPACE = 'WMCore.HTTPFrontEnd.WorkQueue.WorkQueueMonitorPage'
 
 __queues = {}
 def get_remote_queue(queue, logger):
@@ -45,3 +50,93 @@ def sitesFromStorageEelements(ses):
         else:
             result.add(site)
     return list(result)
+
+def queueFromConfig(config):
+    """Create a queue from the config object"""
+    config = queueConfigFromConfigObject(config)
+    if config.WorkQueueManager.level == 'GlobalQueue':
+        from WMCore.WorkQueue.WorkQueue import globalQueue
+        return globalQueue(**config.WorkQueueManager.queueParams)
+    elif config.WorkQueueManager.level == 'LocalQueue':
+        from WMCore.WorkQueue.WorkQueue import localQueue
+        return localQueue(**config.WorkQueueManager.queueParams)
+    else:
+        from WMCore.WorkQueue.WorkQueue import WorkQueue
+        return WorkQueue(**config.WorkQueueManager.queueParams)
+
+def queueConfigFromConfigObject(config):
+    """From a config object create a config dict suitable for a queue object"""
+    from os import path
+    wqManager = config.section_('WorkQueueManager')
+    if not hasattr(wqManager, 'componentDir'):
+        wqManager.componentDir = path.join(config.General.WorkDir,
+                                           'WorkQueueManager')
+    if not hasattr(wqManager, 'namespace'):
+        wqManager.namespace = 'WMComponent.WorkQueueManager.WorkQueueManager'
+    if not hasattr(wqManager, 'logLevel'):
+        wqManager.logLevel = 'INFO'
+    if not hasattr(wqManager, 'pollInterval'):
+        wqManager.pollInterval = 600
+
+    # WorkQueue config
+    if not hasattr(wqManager, 'queueParams'):
+        wqManager.queueParams = {}
+    qConfig = wqManager.queueParams
+    qConfig.setdefault('CacheDir', path.join(wqManager.componentDir, 'wf'))
+
+    if hasattr(wqManager, 'couchurl'):
+        wqManager.queueParams['CouchUrl'] = wqManager.couchurl
+    if hasattr(wqManager, 'dbname'):
+        wqManager.queueParams['DbName'] = wqManager.dbname
+    if hasattr(wqManager, 'inboxDatabase'):
+        wqManager.queueParams['InboxDbName'] = wqManager.inboxDatabase
+
+    qConfig["BossAirConfig"] = getattr(config.WorkQueueManager, "BossAirConfig", None)
+    qConfig["JobDumpConfig"] = getattr(config.WorkQueueManager, "JobDumpConfig", None)
+
+    try:
+        monitorURL = ''
+        queueFlag = False
+        for webapp in config.listWebapps_():
+            webapp = config.webapp_(webapp)
+            for page in webapp.section_('views').section_('active'):
+
+                if not queueFlag and hasattr(page, "model") \
+                   and page.section_('model').object == WORKQUEUE_REST_NAMESPACE:
+                    qConfig['QueueURL'] = 'http://%s:%s/%s/%s' % (webapp.Webtools.host,
+                                                              webapp.Webtools.port,
+                                                              webapp._internal_name.lower(),
+                                                              page._internal_name)
+                    queueFlag = True
+
+                if page.object == WORKQUEUE_MONITOR_NAMESPACE:
+                    monitorURL = 'http://%s:%s/%s/%s' % (webapp.Webtools.host,
+                                                      webapp.Webtools.port,
+                                                      webapp._internal_name.lower(),
+                                                      page._internal_name)
+        if not queueFlag:
+            raise RuntimeError
+
+    except RuntimeError:
+        msg = """Unable to determine WorkQueue QueueURL, Either:
+        Configure a WorkQueueRESTModel webapp_ section or,
+        Add a WorkQueueManager.queueParams.QueueURL setting."""
+        raise RuntimeError, msg
+
+    if not qConfig.has_key('Teams') and hasattr(config.Agent, 'teamName'):
+        qConfig['Teams'] = config.Agent.teamName
+    if not 'logger' in qConfig:
+        import threading
+        myThread = threading.currentThread()
+        if not hasattr(myThread, 'logger'):
+            import logging
+            myThread.logger = logging.getLogger()
+        qConfig['logger'] = myThread.logger
+
+    # ReqMgr params
+    if not hasattr(wqManager, 'reqMgrConfig'):
+        wqManager.reqMgrConfig = {}
+    wqManager.reqMgrConfig['QueueURL'] = qConfig['QueueURL']
+    wqManager.reqMgrConfig['MonitorURL'] = monitorURL
+
+    return config

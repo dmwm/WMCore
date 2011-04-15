4c6a2ee6a0ef728885b8d255320525cd028aeaf5
Eric Vaandering <ewv@fnal.gov>
2011-04-15 16:26:44 +0000

    Don't print out contents of files into log file fixes #1457
    From: Eric Vaandering <ewv@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/branches/WMCORE_0_7_x@12328 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WebTools/RESTApi.py
+++ src/python/WMCore/WebTools/RESTApi.py
@@ -2,7 +2,7 @@
 """
 _RESTApi_
 
-A standard class implementing a REST interface. You should configure the 
+A standard class implementing a REST interface. You should configure the
 application to point at this class, with a model and formatter class configured:
 
 active.section_('rest')
@@ -19,6 +19,7 @@ active.rest.formatter.templates = '/templates/WMCore/WebTools/'
 
 """
 
+import cgi
 
 
 
@@ -33,28 +34,28 @@ class RESTApi(WebAPI):
     """
     __version__ = 1
     def __init__(self, config = {}):
-        
+
         self._set_formatter(config)
-        
+
         self._set_model(config)
-        
+
         self.__doc__ = self.model.__doc__
-        
+
         WebAPI.__init__(self, config)
         self.methods.update({'handler':{'args':[],
                                  'call':self.model.handler,
                                  'version': 1}})
-                            
+
         # TODO: implement HEAD & TRACE
         self.supporttypes = self.formatter.supporttypes.keys()
-        
+
     def _set_model(self, config):
         """
         Load the model object, and assign it to the RESTAPI
         """
         factory = WMFactory('webtools_factory')
         self.model = factory.loadObject(config.model.object, config)
-        
+
     def _set_formatter(self, config):
         """
         Load the formatter object, and assign it to the RESTAPI
@@ -68,17 +69,17 @@ class RESTApi(WebAPI):
         Return the auto-generated documentation for the API
         """
         return self._buildResponse()
-    
+
     @expose
     def default(self, *args, **kwargs):
         """
         Return either the documentation or run the appropriate method.
         """
         return self._buildResponse(args, kwargs)
-    
+
     def _buildResponse(self, args=[], kwargs={}):
         """
-        Set the headers for the response appropriately and format the response 
+        Set the headers for the response appropriately and format the response
         data (e.g. serialise to XML, JSON, RSS/ATOM) or return the documentation
         if no method is specified.
         """
@@ -89,10 +90,10 @@ class RESTApi(WebAPI):
             for m in dir(self.formatter):
                 prop = dir(self.formatter.__getattribute__(m))
                 if 'exposed' in prop:
-                    
+
                     types.append(m)
-            
-            return self.templatepage('RESTAPI', model = self.model, 
+
+            return self.templatepage('RESTAPI', model = self.model,
                                  types = types,
                                  title = self.config.title,
                                  description = self.config.description)
@@ -101,11 +102,14 @@ class RESTApi(WebAPI):
                                                             args, kwargs)
             return self._formatResponse(data, expires,
                                        kwargs.get('return_type', None))
-            
+
         except HTTPError, h:
             # If something raises an HTTPError assume it's something that should
             # go to the client
             response.status = h[0]
+            for kwarg in kwargs.keys():
+                if isinstance(kwargs[kwarg], cgi.FieldStorage):
+                    kwargs[kwarg] = 'FieldStorage class, not printed.'
             self.debug('call to %s with args: %s kwargs: %s resulted in %s' % (request.method, args, kwargs, h[1]))
             return self._formatResponse({'exception': h[0],
                                         'type': 'HTTPError',
@@ -115,25 +119,27 @@ class RESTApi(WebAPI):
             # If something raises a generic exception assume the details are private
             # and should not go to the client
             response.status = 500
+            for kwarg in kwargs.keys():
+                if isinstance(kwargs[kwarg], cgi.FieldStorage):
+                    kwargs[kwarg] = 'FieldStorage class, not printed.'
             self.debug('call to %s with args: %s kwargs: %s resulted in %s' % (request.method, args, kwargs, str(e)))
             return self._formatResponse({'exception': 500,
-                                        'type': e.__class__.__name__, 
-                                        'message': 'Server Error'}, expires=0, 
+                                        'type': e.__class__.__name__,
+                                        'message': 'Server Error'}, expires=0,
                                         format=kwargs.get('return_type', None))
-    
+
     def _formatResponse(self, data, expires, format=None):
         """
-        data format can be anything API provides, but it will make sense 
+        data format can be anything API provides, but it will make sense
         to have either dict format or list of dict format.
         """
-        
+
         acchdr = request.headers.elements('Accept')
         if format:
-            datatype = format 
+            datatype = format
         elif len(acchdr) == 1 and '*/*' == str(acchdr[0]):
             datatype = '*/*'
         else:
             datatype = accept(self.supporttypes)
-            
+
         return self.formatter.format(data, datatype, expires)
-        

fd9e2edc5ee6411323aad8ebdcf00cae3d5f50eb
Simon Metson <simonmetson@googlemail.com>
2011-04-20 20:33:56 +0000

    Fix unit tests to deal with changes to NullAuth.  Fixes #1459.
    From: Simon Metson <simonmetson@googlemail.com>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@12399 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/WebTools_t/RESTFormat_t.py
+++ test/python/WMCore_t/WebTools_t/RESTFormat_t.py
@@ -22,61 +22,60 @@ from WMQuality.WebTools.RESTServerSetup import DefaultConfig
 from WMCore.Wrappers import JsonWrapper
 
 class RESTFormatTest(RESTBaseUnitTest):
-    
+
     def initialize(self):
         self.config = DefaultConfig('WMCore_t.WebTools_t.DummyRESTModel')
         do_debug = False
 
+        self.config.Webtools.environment = 'development'
         if do_debug:
-            self.config.Webtools.environment = 'development'
             self.config.Webtools.error_log_level = logging.DEBUG
             self.config.Webtools.access_log_level = logging.DEBUG
         else:
-            self.config.Webtools.environment = 'production'
             self.config.Webtools.error_log_level = logging.WARNING
             self.config.Webtools.access_log_level = logging.WARNING
-            
+
         self.urlbase = self.config.getServerUrl()
-    
+
     def testUnsupportedFormat(self):
         # test not accepted type should return 406 error
         url = self.urlbase +'list1/'
         methodTest('GET', url, accept='text/das', output={'code':406})
-    
+
     def testSupportedFormat(self):
         rf = RESTFormatter(config=self.config.Webtools)
         url = self.urlbase +'list1/'
-        
+
         for type in rf.supporttypes.keys():
             # test accepted type should return 200 error
             methodTest('GET', url, accept=type, output={'code':200})
-    
+
     def testEncodedInput(self):
         type = 'text/plain'
-       
+
         url = self.urlbase + 'list3?a=a%&b=b'
-        methodTest('GET', url, accept=type, 
+        methodTest('GET', url, accept=type,
                          output={'code':200, 'data':"{'a': 'a%', 'b': 'b'}"})
-       
-        request_input={'a':'%', 'b':'b'}  
-           
+
+        request_input={'a':'%', 'b':'b'}
+
         #methodTest encoded input with urlencode
         url = self.urlbase +'list3'
-        methodTest('GET', url, accept=type, request_input=request_input, 
+        methodTest('GET', url, accept=type, request_input=request_input,
                  output={'code':200, 'data':"{'a': '%', 'b': 'b'}"})
-    
+
     def testReturnFormat(self):
         return_type = 'application/json'
-        
+
         url = self.urlbase +'list3?a=a%&b=b'
-        methodTest('GET', url, accept=return_type, 
+        methodTest('GET', url, accept=return_type,
                          output={'code':200, 'data':'{"a": "a%", "b": "b"}'})
-                
+
         url = self.urlbase + 'list?input_int=a&input_str=a'
         expected_data = '''{"exception": 400, "message": "Invalid input", "type": "HTTPError"}'''
         methodTest('GET', url, accept=return_type,
                          output={'code':400, 'data':expected_data})
-       
+
     def testNoArgMethods(self):
         """
         list1 takes no arguments, it should raise an error if called with one. Require json output.
@@ -85,8 +84,8 @@ class RESTFormatTest(RESTBaseUnitTest):
         url = self.urlbase + 'list1?int=a'
         expected_data = """{"exception": 400, "message": "Invalid input", "type": "HTTPError"}"""
         methodTest('GET', url, accept=return_type, output={'code':400, 'data':expected_data})
-       
-        
+
+
 if __name__ == "__main__":
-    unittest.main() 
-        
+    unittest.main()
+

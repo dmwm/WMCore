4c46d2dd8455bbfe56f3e30e7f57579dae577ef0
Matt Norman <mnorman@fnal.gov>
2011-05-02 13:48:33 +0000

    Update the "last_update" field in a fileset when closing it.  Fixes #1514.
    From: Matt Norman <mnorman@fnal.gov>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/branches/WMCORE_0_7_x@12483 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Fileset/MarkOpen.py
+++ src/python/WMCore/WMBS/MySQL/Fileset/MarkOpen.py
@@ -7,16 +7,16 @@ MySQL implementation of Fileset.MarkOpen
 
 __all__ = []
 
-
+import time
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class MarkOpen(DBFormatter):
-    sql = "UPDATE wmbs_fileset SET open = :p_1 WHERE name = :p_2"
+    sql = "UPDATE wmbs_fileset SET open = :p_1, last_update = :time WHERE name = :p_2"
     
     def execute(self, fileset = None, isOpen = True, conn = None,
                 transaction = False):
-        bindVars = {"p_1": int(isOpen), "p_2": fileset} 
+        bindVars = {"p_1": int(isOpen), "p_2": fileset, "time": int(time.time())} 
         self.dbi.processData(self.sql, bindVars, conn = conn,
                              transaction = transaction)
         return

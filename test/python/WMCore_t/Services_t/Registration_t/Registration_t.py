08e69c077ee7194b539273b2614febbceedc05bd
Simon Metson <simonmetson@googlemail.com>
2011-05-05 20:04:09 +0000

    patch-series-metson/
    From: Simon Metson <simonmetson@googlemail.com>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/branches/WMCORE_0_7_x@12539 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/Services_t/Registration_t/Registration_t.py
+++ test/python/WMCore_t/Services_t/Registration_t/Registration_t.py
@@ -17,56 +17,49 @@ from WMQuality.TestInitCouchApp import TestInitCouchApp
 class RegistrationTest(unittest.TestCase):
     """
     Provide setUp and tearDown for Reader package module
-    
+
     """
     def setUp(self):
         """
         setUP global values
         """
         testname = self.id().split('.')[-1]
-        
+
         logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
                     filename='service_unittests.log',
                     filemode='w')
-        
+
         logger_name = 'Service%s' % testname.replace('test', '', 1)
-        
+
         self.logger = logging.getLogger(logger_name)
-        
+
         self.cache_path = tempfile.mkdtemp()
         self.testInit = TestInitCouchApp("RegistrationTest")
-        self.testInit.setupCouch("regsvc")
+        self.couch_db = "regsvc"
+        self.testInit.setupCouch(self.couch_db)
 
     def tearDown(self):
         self.testInit.tearDownCouch()
-        
+
     def testPush(self):
         reg_info ={
-                   "url": "https://globaldbs",
+                   "location": "https://globaldbs",
                    "admin": "joe.bloggs@cern.ch",
                    "type": "DBS",
                    "name": "Global DBS",
                    "timeout": 2
         }
-        
-        reg = Registration({'inputdata': reg_info,
-                            'endpoint': '%s/regsvc/' % self.testInit.couchUrl,
-                            'cert': os.getcwd() + '/' +__file__,
-                            'key': os.getcwd() + '/' +__file__,
-                            'logger': self.logger, 
-                            'cachepath' : self.cache_path,
-                            'req_cache_path': '%s/requests' % self.cache_path,})
-        reg.refreshCache()
-        
+
+        reg = Registration({'server': self.testInit.couchUrl, 'database': self.couch_db}, reg_info)
+
         json = BasicAuthJSONRequests(self.testInit.couchUrl)
-        data = json.get('/regsvc/' + \
-                       str(reg['inputdata']['url'].__hash__()))
+        data = json.get('/%s/%s' % (self.couch_db, str(reg_info['location'].__hash__())))
         for k, v in reg_info.items():
             if k != 'timestamp':
                 assert data[0][k] == v, \
-                "Registration incomplete: %s should equal %s for key %s" % (data[0][k], v, k) 
-        
+                "Registration incomplete: %s should equal %s for key %s" % (data[0][k], v, k)
+
 if __name__ == '__main__':
-    unittest.main()        
+    unittest.main()

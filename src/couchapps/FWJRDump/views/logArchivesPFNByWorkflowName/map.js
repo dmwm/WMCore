b090e1b49e915e0c971daff9d0cf169762df6186
Danielle Spiga <spiga@cern.ch>
2011-05-16 15:49:23 +0000

    Add a couch view to retrieve the PFNs of archived logs.  Fixes #1303.
    From: Danielle Spiga <spiga@cern.ch>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@12673 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/couchapps/FWJRDump/views/logArchivesPFNByWorkflowName/map.js
@@ -0,0 +1,28 @@
+function(doc) {
+  if (doc['type'] == 'fwjr') {
+    for (var stepName in doc['fwjr'].steps) {
+      if (stepName != 'logArch1') {
+        continue;
+      }
+      var request = doc['fwjr'].task.split('/')[1]
+
+      var step = doc['fwjr']['steps'][stepName];      
+      for (var stepAttribute in step) {
+        if (stepAttribute != 'output') {
+          continue;
+        }
+
+        for (var outputModule in step[stepAttribute]) {
+          if (outputModule != 'logArchive') {
+            continue;
+          }
+
+          if (step[stepAttribute][outputModule].length > 0 &&
+              'pfn' in step['output']['logArchive'][0]) {
+            emit([request], {"jobid" : doc['jobid'], "pfn" :  step["output"]["logArchive"][0]["pfn"]});
+          }
+        }
+      }
+    }
+  }
+}

17817ffbeef7da98314b8d97987e58dfa32dffa1
Rick Wilkinson <rpw@fnal.gov>
2011-06-15 13:40:19 +0000

    Improve the outputDatasetsByPrepID REST call in ReqMgr.  Fixes #1679.
    From: Rick Wilkinson <rpw@fnal.gov>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@13022 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/HTTPFrontEnd/RequestManager/ReqMgrRESTModel.py
+++ src/python/WMCore/HTTPFrontEnd/RequestManager/ReqMgrRESTModel.py
@@ -130,7 +130,7 @@ class ReqMgrRESTModel(RESTModel):
                        args = [], secured=True, expires = 0)
         self._addMethod('GET', 'outputDatasetsByRequestName', self.getOutputForRequest,
                        args = ['requestName'], secured=True, expires = 0)
-        self._addMethod('GET', 'outputDatasestByPrepID', self.getOutputForPrepID,
+        self._addMethod('GET', 'outputDatasetsByPrepID', self.getOutputForPrepID,
                        args = ['prepID'], secured=True, expires = 0)        
         
         cherrypy.engine.subscribe('start_thread', self.initThread)
@@ -198,12 +198,16 @@ class ReqMgrRESTModel(RESTModel):
         return helper.listOutputDatasets()
 
     def getOutputForPrepID(self, prepID):
-        """Return the datasets produced by this prep ID."""
-        requestName = GetRequest.getRequestByPrepID(prepID)
-        if requestName == None:
-            return []
-        helper = Utilities.loadWorkload(requestName)
-        return helper.listOutputDatasets()        
+        """Return the datasets produced by this prep ID. in a dict of requestName:dataset list"""
+        requestIDs = GetRequest.getRequestByPrepID(prepID)
+        result = {}
+        print requestIDs
+        for requestID in requestIDs:
+            request = GetRequest.getRequest(requestID)
+            requestName = request["RequestName"]
+            helper = Utilities.loadWorkload(request)
+            result[requestName] =  helper.listOutputDatasets()
+        return result
 
     def getAssignment(self, teamName=None, request=None):
         """ If a team name is passed in, get all assignments for that team.

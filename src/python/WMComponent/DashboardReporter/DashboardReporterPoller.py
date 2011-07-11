0ee71270126c805e0fa8d9906558e7ba17181c9b
Steve Foulkes <sfoulkes@fnal.gov>
2011-07-11 19:59:37 +0000

    Changes for 0.7.12.
    From: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/branches/WMCORE_0_7_x@13300 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/DashboardReporter/DashboardReporterPoller.py
+++ src/python/WMComponent/DashboardReporter/DashboardReporterPoller.py
@@ -277,7 +277,7 @@ class DashboardReporterPoller(BaseWorkerThread):
         package['taskId']         = 'wmagent_%s' % name
         package['jobId']          = 'taskMeta'
         package['JSTool']         = 'WMAgent'
-        package['JSToolVersion']  = '0.7.11'
+        package['JSToolVersion']  = '0.7.12'
         package['TaskType']       = 'reprocessing'
         package['CMSUser']        = user
         package['Workflow']       = name

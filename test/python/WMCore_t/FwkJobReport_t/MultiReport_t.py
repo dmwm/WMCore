71d574c36302a36b7bcfb73f432ad5504fe91dbc
Dave Evans <evansde@fnal.gov>
2011-07-13 15:26:49 +0000

    Fix the multicore FWJR unit test.  Fixes #1909.
    From: Dave Evans <evansde@fnal.gov>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@13315 4525493e-7705-40b1-a816-d608a930855b

--- test/python/WMCore_t/FwkJobReport_t/MultiReport_t.py
+++ test/python/WMCore_t/FwkJobReport_t/MultiReport_t.py
@@ -20,7 +20,7 @@ class MultiReport_t(unittest.TestCase):
     def setUp(self):
         
         self.directory = os.path.dirname(inspect.getsourcefile(findThisModule))
-        self.multiReport = "%s/CMSSWMulticoreTopReport.xml" % self.directory
+        self.multiReport = os.path.join(self.directory, "CMSSWMulticoreTopReport.xml")
         self.report = Report("MultiReport_t")
         
         
@@ -31,10 +31,26 @@ class MultiReport_t(unittest.TestCase):
         """
         test reading the multi report file, which should in turn read the processing report
         """
-        multiXmlToJobReport(self.report, self.multiReport, directory = self.directory)
+        if not os.path.exists(self.multiReport):
+            msg = "Multi Report file not found: %s" % self.multiReport
+            self.fail(msg)
+        try:
+            multiXmlToJobReport(self.report, self.multiReport, directory = self.directory)
+        except Exception, ex:
+            msg = "Error calling multiXmlToJobReport: %s" % str(ex)
+            self.fail(msg)
 
+        self.failUnless(hasattr(self.report.report, "input"))
+        self.failUnless(hasattr(self.report.report.input, "source"))
+        self.failUnless(hasattr(self.report.report.input.source, "files"))        
+        self.failUnless(hasattr(self.report.report.input.source.files, "fileCount"))        
+        self.failUnless(hasattr(self.report.report, "output"))        
+        
+        self.assertEqual(self.report.report.input.source.files.fileCount, 1)
+        self.failUnless("outputRECORECO" in self.report.report.output.listSections_())
+        self.failUnless("outputALCARECORECO" in self.report.report.output.listSections_())        
 
-        print self.report.data
+        
 
     
 if __name__ == '__main__':

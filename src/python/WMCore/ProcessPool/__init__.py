3c1e291a219ff0a79f83cc055cd9e02e8a440f8b
sfoulkes <sfoulkes>
2011-09-15 18:49:03 +0000

    Revert "From 533a4d7019adc29e85d825e8caabc7e6bda93e0e Mon Sep 17 00:00:00 2001"
    This reverts commit b71bc79130c5a14ebc67fc2318cae22d4458c380.
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@13881 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/WMCore/ProcessPool/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+__init__
+
+"""
+
+
+
+
+
+
---
+++ test/python/WMCore_t/ProcessPool_t/ProcessPoolTestWorker.py
@@ -0,0 +1,23 @@
+"""
+_ProcessPoolTestWorker_
+
+"""
+
+class ProcessPoolTestWorker:
+    def __init__(self, **kwargs):
+        """
+        __init__
+
+        """
+        return
+
+    def __call__(self, parameters):
+        """
+        __call__
+
+        """
+        returnList = []
+        for parameter in parameters:
+            returnList.append(parameter)
+
+        return returnList

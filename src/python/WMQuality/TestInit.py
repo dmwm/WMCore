58d3f53d9d5d33e87cf31d33b8d750670f28bb22
Hassen Riahi <riahi@cern.ch>
2011-10-05 20:52:12 +0000

    Unit tests for the CouchDB component harness.  Fixes #2178.
    From: Hassen Riahi <riahi@cern.ch>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@14151 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMQuality/TestInit.py
+++ src/python/WMQuality/TestInit.py
@@ -139,6 +139,8 @@ class TestInit:
             return 'SQLite'
         elif dialectPart == 'oracle':
             return 'Oracle'
+        elif dialectPart == 'http':
+            return 'CouchDB'
         else:
             raise RuntimeError, "Unrecognized dialect %s" % dialectPart
         

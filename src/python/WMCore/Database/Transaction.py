c4c2170ac3dbaf656c599b354257a24666f7df94
Steve Foulkes <sfoulkes@fnal.gov>
2011-10-12 19:01:28 +0000

    Nuke the code in the transactions class that will retry on errors,  it wasn't working well and no one uses it.  Nuke the unit test as well,  this is tested in a better fashion elsewhere.  Fixes #2621.
    From: Steve Foulkes <sfoulkes@fnal.gov>
    Signed-off-by: Matt Norman <mnorman@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@14239 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/Database/Transaction.py
+++ src/python/WMCore/Database/Transaction.py
@@ -32,11 +32,6 @@ class Transaction(WMObject):
         self.conn = None
         self.transaction = None
 
-        # retries when disconnect
-        self.retries = 5
-        # buffer for losing connection
-        self.sqlBuffer = []
-       
     def begin(self):
         if self.conn == None:
             self.conn = self.dbi.connection()
@@ -48,34 +43,13 @@ class Transaction(WMObject):
 
         return
    
-    def processData(self, sql, binds={}, redo = True):
+    def processData(self, sql, binds={}):
         """
         Propagates the request to the proper dbcore backend,
         and performs checks for lost (or closed) connection.
         """
-        self.sqlBuffer.append( (sql, binds) )
-        # but we might dealing with a closed or
-        # lost connection.
-        if not self.conn == None:
-            if self.conn.closed:
-                self.conn = None
-        try:
-            result = self.dbi.processData(sql, binds, conn = self.conn, \
-                transaction = True)
-        except Exception, ex:
-            logging.warning("Problem connecting to database: "+str(ex))
-            logging.warning("Be patient!")
-            logging.warning("Trying to close existing connection")
-            try:
-                self.conn.close()
-            except:
-                pass
-            logging.warning("Trying to reconnect")
-            if redo:
-                result = self.redo()
-            else:
-                raise ex
-
+        result = self.dbi.processData(sql, binds, conn = self.conn,
+                                      transaction = True)
         return result
 
     def commit(self):
@@ -83,25 +57,8 @@ class Transaction(WMObject):
         Commit the transaction and return the connection to the pool
         """
         if not self.transaction == None:
-            try:
-                self.transaction.commit()
-            except Exception, ex:
-                print "PROBLEM: %s" % str(ex)
-                logging.warning("Problem connecting to database: "+str(ex))
-                logging.warning("Be patient!")
-                logging.warning("Trying to close existing connection")
-                try:
-                    self.conn.close()
-                except:
-                    pass
-                logging.warning("Trying to reconnect")
-                self.redo()
-                self.transaction.commit()
-        else:
-            logging.debug("Transaction::commit(%s) had null transaction object" %id(self))
-            
-            
-        self.sqlBuffer = []
+            self.transaction.commit()
+
         if not self.conn == None:
             self.conn.close()
         self.conn = None
@@ -112,8 +69,6 @@ class Transaction(WMObject):
         To be called if there is an exception and you want to roll back the
         transaction and return the connection to the pool
         """
-        self.sqlBuffer = []
-
         if self.transaction:
             self.transaction.rollback()
 
@@ -139,49 +94,3 @@ class Transaction(WMObject):
         except:
             pass
         return
-        
-    def redo(self):
-        """
-        Tries to re-execute all statements that where not committed,
-        before the connection was lost.
-        """
-        #Added by mnorman for testing.  Do NOT commit
-        #for sql, binds in self.sqlBuffer:
-        #    print 'Going to attempt redo for %s, %s' %(sql, binds)
-        tries = 0
-        result = None
-        connectionProblem = True
-        fatalError = False
-        ## try several time to connect.
-        waitTime = 1
-        reportedError = ''
-        while tries < self.retries and connectionProblem:
-            #try reconnecting
-            self.begin()
-            try:
-                for sql, binds in self.sqlBuffer:
-                    result = self.dbi.processData(sql, binds, \
-                        conn = self.conn, transaction = True)
-                tries = self.retries
-                connectionProblem = False
-            except Exception,ex:
-                logging.warning("Again error. "+str(ex))
-                logging.warning(str(self.retries - tries)+" tries left")
-                reportedError = str(ex)
-                #if connection problem:
-                tries += 1
-                if tries > self.retries:
-                    fatalError = True
-                ##else:
-                ##connectionProblem = False
-                ##tries = self.retries
-            # wait a few seconds if connection failed again:
-            # multiply wait time.
-            if connectionProblem:
-                waitTime = waitTime * 3
-                logging.warning("Waiting: "+str(waitTime)+" seconds before retry")
-                time.sleep(waitTime)
-        if connectionProblem or fatalError:
-            raise WMException(WMEXCEPTION['WMCORE-12']+str(reportedError), \
-                'WMCORE-12')
-        return result

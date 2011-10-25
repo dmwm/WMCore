d44164ad2c92bd048b6b10ff5726d0a1e0d69e8e
Marco Mascheroni <marco.mascheroni@cern.ch>
2011-10-25 19:11:43 +0000

    Modified outputPFNByWorkflowName view to support changes in #2610. Fixes #2706
    From: Marco Mascheroni <marco.mascheroni@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@14338 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapps/FWJRDump/views/outputPFNByWorkflowName/map.js
+++ src/couchapps/FWJRDump/views/outputPFNByWorkflowName/map.js
@@ -6,21 +6,29 @@ function(doc) {
 
     var request = doc['fwjr'].task.split('/')[1]
     var res = ''
-
     for (var stepName in doc['fwjr']['steps']) {
       var stepOutput = doc['fwjr']['steps'][stepName]['output']
       for (var outputModuleName in stepOutput) {
-        if (outputModuleName == 'logArchive') {
-          continue;
-        };
-        for (var outputFileIndex in stepOutput[outputModuleName]) {
-          var outputFile = stepOutput[outputModuleName][outputFileIndex];
-          if (outputFile['OutputPFN'] != '') {
-            res = {"jobid" : doc['jobid'], "pfn" : outputFile['OutputPFN'], "checksums" : outputFile['checksums']};
+        if (stepName=='cmsRun1') {
+          for (var outputFileIndex in stepOutput[outputModuleName]) {
+            var outputFile = stepOutput[outputModuleName][outputFileIndex];
+            //if res contain something it is from the async step. Priority to it!
+            if (outputFile['lfn'] != '' && res == '') {
+              res = {"jobid" : doc['jobid'], "lfn" : outputFile['lfn'], "location" : outputFile['location'], "checksums" : outputFile['checksums']};
+            }
+          }
+        }
+        if (stepName=='asynStageOut1') {
+          for (var outputFileIndex in stepOutput[outputModuleName]) {
+            var outputFile = stepOutput[outputModuleName][outputFileIndex];
+            if (outputFile['OutputPFN'] != '') {
+              res = {"jobid" : doc['jobid'], "pfn" : outputFile['OutputPFN'], "checksums" : outputFile['checksums']};
+            }
           }
         }
       }
     }
+
    if (res != '') {
     emit([request], res);
     }

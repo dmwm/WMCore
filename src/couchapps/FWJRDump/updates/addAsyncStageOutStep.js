c45345a846d5c7df1e6cec3f37f8471fe2bc652e
spiga <spiga@cern.ch>
2011-10-27 09:24:34 +0000

    Fixes #2712 patch-wmcore-riahi/
    From: spiga <spiga@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@14357 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapps/FWJRDump/updates/addAsyncStageOutStep.js
+++ src/couchapps/FWJRDump/updates/addAsyncStageOutStep.js
@@ -3,8 +3,10 @@ function (doc,req) {
 	if (doc.type == "fwjr"){
 
 		checksumDic = {'adler32': req.query.adler, 'cksum': req.query.cksum};
-		doc.fwjr.steps['asynStageOut1'] = {'status': 0};
-		doc.fwjr.steps['asynStageOut1']['output'] = {'output':[{'OutputPFN': req.query.pfn,
+		doc.fwjr.steps['asyncStageOut1'] = {'status': 0};
+		doc.fwjr.steps['asyncStageOut1']['output'] = {'output':[{'lfn': req.query.lfn,
+                                                                       'location': req.query.location,
+                                                                       'OutputPFN': req.query.pfn,
                                                                        'checksums': checksumDic }]};
 
 	return [doc, "OK"];

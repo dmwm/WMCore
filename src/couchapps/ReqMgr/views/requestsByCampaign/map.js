0f4b2df9bcf26d9852069bdbb01f0390374c6e92
Mattia Cinquilli <mcinquil@cern.ch>
2011-11-01 16:27:20 +0000

    Remove the user name from the requestsByCampaign view.  Fixes #2695.
    From: Mattia Cinquilli <mcinquil@cern.ch>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@14404 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapps/ReqMgr/views/requestsByCampaign/map.js
+++ src/couchapps/ReqMgr/views/requestsByCampaign/map.js
@@ -1,7 +1,7 @@
 function(user_request) {
   if (user_request.Requestor && user_request.Campaign) {
-    emit([user_request.Requestor, user_request.Campaign],
-        {'_id': user_request['_id'], 'RequestName' : user_request['RequestName'], 'OriginalRequestName' : user_request['OriginalRequestName']}
+    emit([user_request.Campaign],
+        {'_id': user_request['_id'], 'RequestName' : user_request['RequestName'], 'OriginalRequestName' : user_request['OriginalRequestName'], 'Submission' : user_request['Submission']}
         );
   }
 }

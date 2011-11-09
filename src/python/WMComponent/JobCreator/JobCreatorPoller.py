5bd8320e7c21f32631de198877c285424264fcc6
Matt Norman <mnorman@fnal.gov>
2011-11-09 17:21:10 +0000

    Fix MySQL AutoIncrement Couch Load.  Fixes #2818.
    From: Matt Norman <mnorman@fnal.gov>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@14491 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/JobCreator/JobCreatorPoller.py
+++ src/python/WMComponent/JobCreator/JobCreatorPoller.py
@@ -376,12 +376,25 @@ class JobCreatorPoller(BaseWorkerThread):
             jobsdatabase     = couchdb.connectDatabase("%s/jobs" % config.JobStateMachine.couchDBName)
             try:
                 jobID = jobsdatabase.loadView("JobDump",
-                                              "highestJobID")['rows'][0]['value']
+                                              "highestJobID",
+                                              options = {'reduce': False,
+                                                         'limit': 1,
+                                                         'descending': True}
+                                              )['rows'][0]['id']
+                jobID = int(jobID)
             except IndexError:
                 # In this case, there are no jobs in couch
                 jobID = 0
+            except ValueError:
+                # This is a weird error - there's a document in the couch server
+                # without a job ID as id
+                jobID = 0
+                logging.error("Encountered a document in the JobDump database with an invalid ID!")
+                logging.error("ID: %s" % jobID)
+                pass
 
             # Now increment the MySQL AutoIncrement
+            logging.info("About to handle MySQL AutoIncrement with jobID %i" % jobID)
             incrementDAO.execute(input = jobID)
 
         return

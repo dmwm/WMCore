16afddbc4f86cbe3b3122c08f67a4d97d5cebee0
Marco Mascheroni <marco.mascheroni@cern.ch>
2011-11-11 23:21:56 +0000

    outputFileIndex is not defined. (Re) Fixes #2761
    From: Marco Mascheroni <marco.mascheroni@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@14531 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapps/FWJRDump/views/outputLFNByWorkflowName/map.js
+++ src/couchapps/FWJRDump/views/outputLFNByWorkflowName/map.js
@@ -10,10 +10,11 @@ function(doc) {
         for (var stepName in doc['fwjr']['steps']) {
             var stepOutput = doc['fwjr']['steps'][stepName]['output'];
             for (var outputModuleName in stepOutput) {
-                var outputFile = stepOutput[outputModuleName][outputFileIndex];
                 for (var outputFileIndex in stepOutput[outputModuleName]) {
+                    var outputFile = stepOutput[outputModuleName][outputFileIndex];
                     if (stepName=='asyncStageOut1' && outputFile['lfn']!='') {
                         emit([request], {"jobid" : doc['jobid'], "lfn" : outputFile['lfn'], "location" : outputFile['location'], "checksums" : outputFile['checksums']});
+                        return;
                     }
                     else if (stepName=='cmsRun1' && outputFile['lfn']!='') {
                         res = {"jobid" : doc['jobid'], "lfn" : outputFile['lfn'], "location" : outputFile['location'], "checksums" : outputFile['checksums']};

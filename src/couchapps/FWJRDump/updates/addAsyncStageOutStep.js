b9a30b35ca46c72cabad96fb164369faac861cb9
Hassen Riahi <riahi@cern.ch>
2011-11-11 15:42:38 +0000

    Fix multiple output jobs in Async Stageout.  Fixes #2331.
    From: Hassen Riahi <riahi@cern.ch>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@14524 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapps/FWJRDump/updates/addAsyncStageOutStep.js
+++ src/couchapps/FWJRDump/updates/addAsyncStageOutStep.js
@@ -1,14 +1,21 @@
 function (doc,req) {
 
 	if (doc.type == "fwjr"){
-
-		checksumDic = {'adler32': req.query.adler, 'cksum': req.query.cksum};
-		doc.fwjr.steps['asyncStageOut1'] = {'status': 0};
-		doc.fwjr.steps['asyncStageOut1']['output'] = {'output':[{'lfn': req.query.lfn,
-                                                                       'location': req.query.location,
-                                                                       'OutputPFN': req.query.pfn,
-                                                                       'checksums': checksumDic }]};
-
+		var checksumDic = {'adler32': req.query.adler, 'cksum': req.query.cksum};
+                var asynStepFound = false
+		for (var stepName in doc['fwjr']['steps']) {
+                	if (stepName == "asynStageOut1") { 
+				doc.fwjr.steps['asynStageOut1']['output']['output'].push({'OutputPFN': req.query.pfn,
+                                                        			          'checksums': checksumDic });	
+                                asynStepFound = true;
+                                break;
+			}
+		}
+                if (!asynStepFound ){
+			doc.fwjr.steps['asynStageOut1'] = {'status': 0};
+			doc.fwjr.steps['asynStageOut1']['output'] = {'output':[{'OutputPFN': req.query.pfn,
+                   	                                                        'checksums': checksumDic }]};
+                }
 	return [doc, "OK"];
 	}
 }

c50049dfd4462f4207e8694a60b655bb230b5db9
spiga <spiga@cern.ch>
2011-11-28 14:12:29 +0000

    patch-wmcore-riahi/ fixes #2894
    From: spiga <spiga@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@14646 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapps/FWJRDump/updates/addAsyncStageOutStep.js
+++ src/couchapps/FWJRDump/updates/addAsyncStageOutStep.js
@@ -4,16 +4,19 @@ function (doc,req) {
 		var checksumDic = {'adler32': req.query.adler, 'cksum': req.query.cksum};
                 var asynStepFound = false
 		for (var stepName in doc['fwjr']['steps']) {
-                	if (stepName == "asynStageOut1") { 
-				doc.fwjr.steps['asynStageOut1']['output']['output'].push({'OutputPFN': req.query.pfn,
-                                                        			          'checksums': checksumDic });	
+                        if (stepName == "asyncStageOut1") {
+				doc.fwjr.steps['asyncStageOut1']['output']['output'].push({'lfn': req.query.lfn,
+                                                                                          'location': req.query.location,
+                                                                                          'OutputPFN': req.query.pfn,
+                                                                                          'checksums': checksumDic });
                                 asynStepFound = true;
                                 break;
 			}
 		}
                 if (!asynStepFound ){
-			doc.fwjr.steps['asynStageOut1'] = {'status': 0};
-			doc.fwjr.steps['asynStageOut1']['output'] = {'output':[{'OutputPFN': req.query.pfn,
+			doc.fwjr.steps['asyncStageOut1'] = {'status': 0};
+			doc.fwjr.steps['asyncStageOut1']['output'] = {'output':[{'lfn': req.query.lfn,
+                                                                                'location': req.query.location,									                                                                       'OutputPFN': req.query.pfn,
                    	                                                        'checksums': checksumDic }]};
                 }
 	return [doc, "OK"];

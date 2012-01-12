4bbf54744a33a644663290848cf5dcca7c85153f
Matt Norman <mnorman@fnal.gov>
2012-01-12 16:32:08 +0000

    Don't reset the subscription status when adding files to a fileset if the files are already associated with the fileset.  From #3080.
    From: Matt Norman <mnorman@fnal.gov>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@14936 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMBS/MySQL/Files/AddDupsToFileset.py
+++ src/python/WMCore/WMBS/MySQL/Files/AddDupsToFileset.py
@@ -10,6 +10,11 @@ import time
 from WMCore.Database.DBFormatter import DBFormatter
 
 class AddDupsToFileset(DBFormatter):
+    existSQL = """SELECT lfn FROM wmbs_file_details wfd
+                    INNER JOIN wmbs_fileset_files wff ON wff.fileid = wfd.id
+                    WHERE wff.fileset = :fileset
+                    AND wfd.lfn = :lfn"""
+    
     sql = """INSERT IGNORE INTO wmbs_fileset_files (fileid, fileset, insert_time)
                SELECT wmbs_file_details.id, :fileset, :insert_time
                FROM wmbs_file_details
@@ -42,9 +47,24 @@ class AddDupsToFileset(DBFormatter):
                           wmbs_fileset_files.fileset != :fileset)""" 
         
     def execute(self, file, fileset, workflow, conn = None, transaction = False):
-        binds = []
+        binds      = []
         availBinds = []
+        existBinds = []
         timestamp = int(time.time())
+        for fileLFN in file:
+            existBinds.append({'lfn': fileLFN, 'fileset': fileset})
+        existResult = self.dbi.processData(self.existSQL, existBinds,
+                                           conn = conn,
+                                           transaction = transaction)
+        existsLFNs = self.formatDict(existResult)
+        for result in existsLFNs:
+            file.remove(result.get('lfn'))
+
+        if len(file) < 1:
+            # We removed all the files
+            # There's nothing more for us to do.
+            return
+        
         for fileLFN in file:
             binds.append({"lfn": fileLFN, "fileset": fileset,
                           "insert_time": timestamp, "workflow": workflow})

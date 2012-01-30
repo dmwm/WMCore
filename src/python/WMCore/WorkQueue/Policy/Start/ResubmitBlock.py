72612aed95cc5d7884e8a54cc76f942ab21fce27
Steve Foulkes <sfoulkes@fnal.gov>
2012-01-30 15:38:57 +0000

    Don't fail if a task doesn't have an ACDC section.  Fixes #2980.
    From: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@15047 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WorkQueue/Policy/Start/ResubmitBlock.py
+++ src/python/WMCore/WorkQueue/Policy/Start/ResubmitBlock.py
@@ -48,6 +48,9 @@ class ResubmitBlock(StartPolicyInterface):
         chunkSize = 200
         
         acdcInfo = task.getInputACDC()
+        if acdcInfo == None:
+            return []
+        
         acdc = DataCollectionService(acdcInfo["server"], acdcInfo["database"])
         if self.data:
             acdcBlockSplit = ACDCBlock.splitBlockName(self.data.keys()[0])

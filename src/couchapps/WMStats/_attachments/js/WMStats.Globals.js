a1bef117152a190b9128d5c7b20192df598e2405
Seangchan Ryu <sryu@fnal.gov>
2012-01-30 16:26:39 -0600

    add show/hide columns (filter is not working yet). Need to be refactored

---
+++ src/couchapps/WMStats/_attachments/js/WMStats.Globals.js
@@ -0,0 +1,6 @@
+/*
+ * Define Global values for WMStats client application
+ */
+WMStats.namespace("Globals")
+WMStats.Globals.couchDBName = "wmstats"
+WMStats.Globals.couchDBViewPath = "/wmstats/_design/WMStats/_view/"
--- src/couchapps/WMStats/_attachments/js/WMStats.Table.js
+++ src/couchapps/WMStats/_attachments/js/WMStats.Table.js
@@ -1,119 +1,24 @@
 WMStats.namespace("Table")
 
-WMStats.Table.convertCampaignData = function(data, baseColumns) {
-    var rows =[]
-    for (var i in data) {
-        tableRow = data[i].value;
-        for (var j = 0; j < baseColumns.length; j ++) {
-            tableRow[baseColums[j]] = data[j].key;
-        }
-        rows.push(tableRow)
-    }
-    return rows
-}
-
-
-
-WMStats.Table.convertToColumnsAndRows = function(data, baseColumns) {
-    /*
-     * *
-     *  convert couch db data to columns and rows.
-     *  i.e. couchdb format is 
-     *  {"rows":[ {"key":["A", "B", "C"],"value":{"a":0,"b":0, ...}},
-     *            {"key":["D", "E", "F"],"value":{"a":1,"b":1, ...}},
-     *  baseColumns is the list of column names for key value above
-     *  baseColums = ["campaign", "team", "type"] then 
-     *  "A" is campaign name "B" is team name, "C" is test name 
-     *  
-     *  this will return
-     *  {'columns': ["campaign", "team", "type", "a", "b", ...],
-     *   'rows': [["A", "B", "C", 0, 0, ...],
-     *            ["D", "E", "F", 1, 1, ...],
-     *           ]
-     * */
-    var columns = [];
-    var rows = [];
+WMStats.Table = function(config) {
+    var _defaultConfig = {"bProcessing": true,
+                          "sDom": 'C<"clear">lfrtip',
+                          };
     
-    if (data.rows.length) {
-        
-        for (var i = 0; i < data.rows[0].key.length; i++) {
-            columns.push(baseColumns[i]);
+    function _updateConfig(config) {
+        for (var prop in config) {
+            _defaultConfig[prop] = config[prop];
         }
-         
-        for (status in data.rows[0].value) {
-            columns.push(status);    
-        }        
     }
     
-    for (var j in data.rows) {
-        dataRow = [];
-        for (var k in data.rows[j].key){
-            dataRow.push(data.rows[j].key[k]);
-        }
-        for (var status in data.rows[j].value){
-            dataRow.push(data.rows[j].value[status]);
-        }
-        rows.push(dataRow);
+    function _create(selector) {
+        return $(selector).dataTable(_defaultConfig).columnFilter();
     }
     
-    return {'columns': columns, 'rows': rows}
-}
-
-
-WMStats.Table.convertToRows = function(requestData, columnFilter) {
-
-    if (!columnFilter) {
-        // default column filter
-        columnFilter = function(dataRow) {
-            var row = [];
-            for(var field in dataRow) {
-                row.push(dataRow[field]);
+    if (config) {_updateConfig(config)}
+    
+    return {'config': _defaultConfig,
+            'updateConfig': _updateConfig,
+            'create': _create
             }
-            return row;
-        }
-    }
-    
-    var data = requestData.getDataByRequest();
-    var rows = [];
-    for (var workflow in data) {
-        rows.push(columnFilter(data[workflow]));       
-    }
-    return rows;
 }
-
-columnFilter = function(rowData) {
-    
-}
-
-WMStats.Table.create = function(selector, data, config) {
-    /*
-     * create the Table with given data on the specified selector 
-     * (selector is css/jquery selector)
-     */
-    
-    function generateConfig() {
-        /*
-         * jquery.dataTables config
-         * Reference: http://datatables.net/ref
-         * */
-        
-        // default table config
-        var tableConfig = {'bProcessing': true}; 
-      
-        if (config) {
-            tableConfig = config;
-        }
-        
-        tableConfig.aoColumns = [];                   
-        for (var i in data.columns) {
-            tableConfig.aoColumns.push({'sTitle': data.columns[i]});
-        }
-        tableConfig.aaData = data.rows;
-        return tableConfig; 
-    }
-    
-    var oTableConfig = generateConfig();
-    var oTable = $(selector).dataTable(oTableConfig);
-    return oTable;
-}
-    

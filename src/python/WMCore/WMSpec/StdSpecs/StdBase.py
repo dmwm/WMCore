1832df867b0547b0199648d55341d4ab62a7202b
Dirk Hufnagel <hufangel@cern.ch>
2012-04-02 15:43:15 +0000

    Fixes for Config.DP workflow output handling.  Fixes #3421.
    From: Dirk Hufnagel <hufangel@cern.ch>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@15584 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMCore/WMSpec/StdSpecs/StdBase.py
+++ src/python/WMCore/WMSpec/StdSpecs/StdBase.py
@@ -135,19 +135,20 @@ class StdBase(object):
             outputModules = configCache.getOutputModuleInfo()
         else:
             if scenarioFunc in [ "promptReco", "expressProcessing" ]:
-                for output in scenarioArgs.get('outputs',[]):
-                    dataTier = output['dataTier']
+                for output in scenarioArgs.get('outputs', []):
                     moduleLabel = output['moduleLabel']
-                    filterName = output.get('filterName', None)
-                    outputModules[moduleLabel] = {'dataTier' : dataTier,
-                                                  'filterName' : filterName}
+                    outputModules[moduleLabel] = { 'dataTier' : output['dataTier'] }
+                    if output.has_key('primaryDataset'):
+                        outputModules[moduleLabel]['primaryDataset'] = output['primaryDataset']
+                    if output.has_key('filterName'):
+                        outputModules[moduleLabel]['filterName'] = output['filterName']
             elif scenarioFunc == "alcaSkim":
                 for alcaSkim in scenarioArgs.get('skims',[]):
-                    dataTier = "ALCARECO"
                     moduleLabel = "ALCARECOStream%s" % alcaSkim
-                    filterName = alcaSkim
-                    outputModules[moduleLabel] = {'dataTier' : dataTier,
-                                                  'filterName' : filterName}
+                    outputModules[moduleLabel] = { 'dataTier' : "ALCARECO",
+                                                   'primaryDataset' : scenarioArgs.get('primaryDataset'),
+                                                   'filterName' : alcaSkim }
+                    
 
         return outputModules
 
@@ -255,9 +256,7 @@ class StdBase(object):
                                          block_whitelist = self.blockWhitelist,
                                          run_blacklist = self.runBlacklist,
                                          run_whitelist = self.runWhitelist)
-            elif inputStep == None:
-                procTask.setInputStep(inputStep)
-            else:
+            elif inputStep != None and inputModule != None:
                 procTask.setInputReference(inputStep, outputModule = inputModule)
 
         if primarySubType:
@@ -277,11 +276,6 @@ class StdBase(object):
         procTaskStageHelper.setMinMergeSize(self.minMergeSize, self.maxMergeEvents)
         procTaskCmsswHelper.cmsswSetup(self.frameworkVersion, softwareEnvironment = "",
                                        scramArch = self.scramArch)
-        if configDoc != None and configDoc != "":
-            procTaskCmsswHelper.setConfigCache(couchURL, configDoc, couchDBName)
-        else:
-            procTaskCmsswHelper.setDataProcessingConfig(scenarioName, scenarioFunc,
-                                                        **scenarioArgs)
 
         configOutput = self.determineOutputModules(scenarioFunc, scenarioArgs,
                                                    configDoc, couchURL, couchDBName)
@@ -289,10 +283,24 @@ class StdBase(object):
         for outputModuleName in configOutput.keys():
             outputModule = self.addOutputModule(procTask,
                                                 outputModuleName,
-                                                self.inputPrimaryDataset,
-                                                configOutput[outputModuleName]["dataTier"],
-                                                configOutput[outputModuleName]["filterName"])
+                                                configOutput[outputModuleName].get('primaryDataset',
+                                                                                   self.inputPrimaryDataset),
+                                                configOutput[outputModuleName]['dataTier'],
+                                                configOutput[outputModuleName].get('filterName', None))
             outputModules[outputModuleName] = outputModule
+
+        if configDoc != None and configDoc != "":
+            procTaskCmsswHelper.setConfigCache(couchURL, configDoc, couchDBName)
+        else:
+            # delete dataset information from scenarioArgs
+            if scenarioArgs.has_key('outputs'):
+                for output in scenarioArgs['outputs']:
+                    del output['primaryDataset']
+            if scenarioArgs.has_key('primaryDataset'):
+                del scenarioArgs['primaryDataset']
+
+            procTaskCmsswHelper.setDataProcessingConfig(scenarioName, scenarioFunc,
+                                                        **scenarioArgs)
 
         return outputModules
 

3b7721688e63683648d5b4ef089aa1324b9e8b5d
Seangchan Ryu <sryu@fnal.gov>
2012-05-03 14:59:51 -0500

    partial work

--- src/couchapps/WMStats/_attachments/js/WMStats.Globals.js
+++ src/couchapps/WMStats/_attachments/js/WMStats.Globals.js
@@ -4,11 +4,20 @@
  * PromptScheme specific config
  */
 WMStats.namespace("Globals")
-WMStats.Globals = {
-    REQ_DETAIL_URL_PREFIX: "/reqmgr/view/details/",
-    WORKLOAD_SUMMARY_URL_PREFIX: "/couchdb/workloadsummary/_design/WorkloadSummary/_show/histogramByWorkflow/",
-    // set the flag to prevent sending ajax call by clicking table rows 
-    RequestViewTableClickFlag: false,
-    AJAX_LOADING_STATUS: {beforeSend: function(){$('#loading_page').show().addClass('front')}, 
-                          complete: function(){$('#loading_page').hide()}}
-}
+WMStats.Globals = function(){
+    function _getCouchDB() {
+        pathList = window.location.pathname.split('/_design/WMStats');
+        if (pathList.length > 1) {
+            // accessing through 
+        } else {
+        }
+    return {
+        REQ_DETAIL_URL_PREFIX: "/reqmgr/view/details/",
+        WORKLOAD_SUMMARY_URL_PREFIX: "/couchdb/workloadsummary/_design/WorkloadSummary/_show/histogramByWorkflow/",
+        AJAX_LOADING_STATUS: {beforeSend: function(){$('#loading_page').show().addClass('front')}, 
+                              complete: function(){$('#loading_page').hide()}},
+        getCouchDB: _couchDB
+    }
+}()
+
+WMStats.Globals

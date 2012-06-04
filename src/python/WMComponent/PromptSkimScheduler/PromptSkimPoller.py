6ef5e82a6142b58ea856f6f85cabfed3108aa867
Diego Ballesteros <dballest@fnal.gov>
2012-06-04 21:07:26 +0000

    Fix races in PromptSkimming between workflow cleanup and data transfer in the T0.  Fixes #3787.
    From: Diego Ballesteros <dballest@fnal.gov>
    Signed-off-by: Steve Foulkes <sfoulkes@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/WMCore/trunk@15961 4525493e-7705-40b1-a816-d608a930855b

--- src/python/WMComponent/PromptSkimScheduler/PromptSkimPoller.py
+++ src/python/WMComponent/PromptSkimScheduler/PromptSkimPoller.py
@@ -11,6 +11,7 @@ import logging
 import sys
 import os
 import traceback
+import re
 
 from WMCore.WorkerThreads.BaseWorkerThread import BaseWorkerThread
 
@@ -19,6 +20,7 @@ from WMCore.DAOFactory import DAOFactory
 from WMCore.Database.DBFactory import DBFactory
 
 from T0.State.Database.Reader import ListBlock
+from T0.State.Database.Reader import ListRuns
 from T0.State.Database.Writer import InsertBlock
 
 from T0.GenericTier0.Tier0DB import Tier0DB
@@ -56,6 +58,8 @@ class PromptSkimPoller(BaseWorkerThread):
         self.daoFactory = DAOFactory(package = "WMCore.WMBS", logger = logging,
                                      dbinterface = myThread.dbi)
         self.runConfigCache = None
+        self.handleWorkflowInjection = not getattr(self.config.JobArchiver,
+                                                   'handleInjected', True)
 
         # Scram arch and path to cmssw needed to generate workflows.
         self.scramArch = self.config.PromptSkimScheduler.scramArch
@@ -97,6 +101,7 @@ class PromptSkimPoller(BaseWorkerThread):
         Poll for transferred blocks and complete runs.
         """
         self.pollForTransferredBlocks()
+        self.markInjected()
         return
 
     def getRunConfig(self, runNumber):
@@ -347,4 +352,76 @@ class PromptSkimPoller(BaseWorkerThread):
                 myThread.transaction.commit()
 
         self.t0astDBConn.commit()
+        return
+
+    def markInjected(self):
+        """
+        _markInjected_
+
+        Mark promptSkimming workflows as injected only when we know
+        that we have received all the blocks available
+        for the corresponding Run/PD
+        """
+
+        if not self.handleWorkflowInjection:
+            logging.debug("Component will not check workflows for injection status")
+            return
+
+        #Poll the workqueue/wmbs for workflows
+        myThread = threading.currentThread()
+        getAction  = self.daoFactory(classname = "Workflow.GetInjectedWorkflows")
+        markAction = self.daoFactory(classname = "Workflow.MarkInjectedWorkflows")
+        result = getAction.execute()
+
+        # Check each result to see if it is injected:
+        injected = []
+        for name in result:
+            try:
+                if self.workQueue.getWMBSInjectionStatus(workflowName = name):
+                    injected.append(name)
+            except:
+                # Do nothing if it complains
+                pass
+        logging.debug("Found the following workflows to be marked as injected: %s"
+                        % injected)
+
+        runs = {}
+        pattern = r'^Run[0-9]+-[A-Za-z0-9_\-]+-'
+        for name in injected:
+            #Ignore any workflow that doesn't match a promptSkimName
+            if not re.match(pattern, name):
+                logging.error("Non-PromptSkim workflow detected in the agent, will ignore it")
+                logging.error("Why is this workflow in this agent?")
+                logging.error("If needed to run, mark it as injected manually on wmbs")
+                logging.error("Workflow name: %s" % name)
+                continue
+            parts = name.split("-")
+            runNumber = int(parts[0][3:])
+            if runNumber not in runs:
+                runs[runNumber] = []
+            runs[runNumber].append(name)
+        logging.debug("Got the following runs %s" % runs.keys())
+
+        #Poll T0AST for those workflows that can be marked as injected
+        toInject = []
+        acceptedStates = ['Complete']
+        if len(runs) > 0:
+            runStates = ListRuns.listRunState(self.t0astDBConn, runs.keys())
+        for run in runs:
+            logging.debug("Run %d is in state %s" % (run, runStates[run]))
+            if runStates[run] not in acceptedStates:
+                #Then we are in a state where there may be more blocks
+                #left to be produced
+                logging.debug("Not marking any workflows for run %d as injected" % run)
+                continue
+            toInject.extend(runs[run])
+
+        #Mark what is ready as injected
+        logging.debug("Marking the following workflows as injected: %s"
+                    % toInject)
+
+        if len(toInject) > 0:
+            myThread.transaction.begin()
+            markAction.execute(names = toInject, injected = True)
+            myThread.transaction.commit()
         return

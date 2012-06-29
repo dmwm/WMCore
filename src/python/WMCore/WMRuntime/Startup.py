a7a1df060558d9271ab1d2f294e4830ac4fcf467
Andrew Melo <andrew.melo@gmail.com>
2012-06-29 14:17:53 -0500

    Revert "Adding timing information to startup.py"
    This reverts commit 68499276a76c88ade4e74291f049dbcf91eb02e7.

--- src/python/WMCore/WMRuntime/Startup.py
+++ src/python/WMCore/WMRuntime/Startup.py
@@ -6,32 +6,31 @@ Runtime environment startup script
 """
 
 import os
-import datetime
 import WMCore.WMRuntime.Bootstrap as Bootstrap
 
 if __name__ == '__main__':
-    print "Startup.py : %s : loading job definition" % datetime.isoformat()
+    print "Startup.py : loading job definition"
     job = Bootstrap.loadJobDefinition()
-    print "Startup.py : %s : loading task" % datetime.isoformat()
+    print "Startup.py : loading task"
     task = Bootstrap.loadTask(job)
-    print "Startup.py : %s : setting up monitoring" % datetime.isoformat()
+    print "Startup.py : setting up monitoring"
     logLocation = "Report.%i.pkl" % job['retry_count']
     Bootstrap.createInitialReport(job = job,
                                   task = task,
                                   logLocation = logLocation)
     monitor = Bootstrap.setupMonitoring(logPath = logLocation)
 
-    print "Startup.py : %s : setting up logging" % datetime.isoformat()
+    print "Startup.py : setting up logging"
     Bootstrap.setupLogging(os.getcwd())
 
-    print "Startup.py : %s : building task" % datetime.isoformat()
+    print "Startup.py : building task"
     task.build(os.getcwd())
-    print "Startup.py : %s : executing task" % datetime.isoformat()
+    print "Startup.py : executing task"
     task.execute(job)
-    print "Startup.py : %s : completing task" % datetime.isoformat()
+    print "Startup.py : completing task"
     task.completeTask(jobLocation = os.getcwd(),
                       logLocation = logLocation)
-    print "Startup.py : %s : shutting down monitor" % datetime.isoformat()
+    print "Startup.py : shutting down monitor"
     os.fchmod(1, 0664)
     os.fchmod(2, 0664)
     if monitor.isAlive():

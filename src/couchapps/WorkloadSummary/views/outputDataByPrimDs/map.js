d9df12003e7c16f9ea7a8fa08ec2950c4cf95445
Diego Ballesteros <dballest@fnal.gov>
2012-07-17 13:01:56 -0500

    Make workflowtype key more descriptive
    Instead of using 0 and 1 for PromptSkims and Tier1Promptreco,
    use the strings "PromptSkim" and "T1Reco" as identifiers

--- src/couchapps/WorkloadSummary/views/outputDataByPrimDs/map.js
+++ src/couchapps/WorkloadSummary/views/outputDataByPrimDs/map.js
@@ -3,8 +3,10 @@
  * Run<runnumber>-<PrimDs>-<ProcessedDataset>-Tier1PromptReco-<GUID>
  * Run<runnumber>-<PrimDs>-<ProcessedDataset>-<SkimName>-<GUID>
  * It returns them with the following key:
- * [<PrimDs>,<isTier1Reco>, <runnumber>]
- *
+ * [<PrimDs>,<WorkflowType>, <runnumber>]
+ * The possible workflow types are:
+ *    T1Reco for the first pattern
+ *    PromptSkim for the second pattern
  */
 function(doc) {
     var regexp = /^Run(\d+)-(\w+)-(\w+)(-\w+)?-v(\d+)-(\w+)/;
@@ -12,7 +14,7 @@ function(doc) {
     if (match && match.length == 7){
        var runNumber = parseInt(match[1]);
        var primDs = match[2];
-       var isReco = (match[4] != null) ? 0 : 1;
+       var isReco = (match[4] != null) ? "PromptSkim" : "T1Reco";
        emit([primDs, isReco, runNumber], null);
     }
 }

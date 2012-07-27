8a5f4d661a0c834cb39d4ac29c918c427af68fcc
Seangchan Ryu <sryu@fnal.gov>
2012-07-27 15:33:55 -0500

    add acive request tier0 support, layout change and hanle no request case

--- src/couchapps/WMStats/_attachments/js/GUI/HTMLList/import.js
+++ src/couchapps/WMStats/_attachments/js/GUI/HTMLList/import.js
@@ -1,6 +1,16 @@
 WMStats.Globals.importScripts([
     //"js/GUI/HTMLList/WMStats.HTMLList.js",
     "js/GUI/HTMLList/WMStats.JobDetailList.js",
-    "js/GUI/HTMLList/WMStats.RequestDetailList.js",
     "js/GUI/HTMLList/WMStats.RequestDataList.js"
 ])
+
+if (WMStats.Globals.VARIANT == "tier1") {
+    //import tier1 specific table
+    WMStats.Globals.importScripts(["js/GUI/HTMLList/WMStats.RequestDetailList.js"])
+} else if (WMStats.Globals.VARIANT == "tier0") {
+    //import tie0 specific table
+    WMStats.Globals.importScripts(["js/GUI/HTMLList/T0/WMStats.RequestDetailList.js"])
+} else if (WMStats.Globals.VARIANT == "analysis") {
+    //import analysis specific table
+    WMStats.Globals.importScripts(["js/GUI/HTMLList/WMStats.RequestDetailList.js"])
+} 

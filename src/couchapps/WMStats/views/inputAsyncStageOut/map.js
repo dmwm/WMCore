8b41c192225edb927fbff21801cfda25cf285af2
Hassen Riahi <Hassen.Riahi@cern.ch>
2012-09-06 12:56:29 +0200

    Remove unneeded parameter.

--- src/couchapps/WMStats/views/inputAsyncStageOut/map.js
+++ src/couchapps/WMStats/views/inputAsyncStageOut/map.js
@@ -2,18 +2,16 @@ function(doc) {
         if ( doc.type == "jobsummary" ) {
                 workflow = doc['workflow'];
                 job = doc['_id'];
-                job_end_time = doc['timestamp'];
                 if (doc['state']=='success') {
 			for (module in doc['output']) {
 		                if (doc['output'][module]['type'] == 'output') {
-			                emit(job_end_time,
+			                emit(doc.timestamp,
 					{'workflow' : workflow,
 			  		 'jobid' : job,
        		                         '_id': doc['output'][module]['lfn'],
 				     	 'checksums': doc['output'][module]['checksums'],
 	                              	 'size': doc['output'][module]['size'],
-	                 	  	 'source' : doc['output'][module]['location'],
-                                         'job_end_time': job_end_time})
+	                 	  	 'source' : doc['output'][module]['location']})
 				}
 			}
 		}

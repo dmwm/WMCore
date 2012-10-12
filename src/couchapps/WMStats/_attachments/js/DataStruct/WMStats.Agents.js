51955be3bbf7fc4d6a90785e80ca85bd63deeefd
Seangchan Ryu <sryu@fnal.gov>
2012-10-12 14:08:56 -0500

    add local queue pull warning: need to define for different case and add wmbs job id in jobsummary doc (Tier0 ops request)

--- src/couchapps/WMStats/_attachments/js/DataStruct/WMStats.Agents.js
+++ src/couchapps/WMStats/_attachments/js/DataStruct/WMStats.Agents.js
@@ -15,5 +15,30 @@ WMStats.Agents = function (couchData) {
                                  }
     if (couchData) agentData.setData(couchData);
     
+    agentData.getAlertList = function() {
+        var currentTime = Math.round(new Date().getTime() / 1000);
+        var dataList = this.getData();
+        var agentPollingCycle = 600;
+    
+        function getStatus(agentInfo) {
+            var lastUpdatedDuration = currentTime - agentInfo.timestamp;
+            if (lastUpdatedDuration > agentPollingCycle * 2) {
+                return {status: "agent_down", 
+                        message: WMStats.Utils.foramtDuration(lastUpdatedDuration)};
+            };
+            if (agentInfo.down_components.length > 0) {
+                return {status: "component_down",
+                        message: agentInfo.down_components};
+            };
+            return {status: "ok", 
+                    message: WMStats.Utils.foramtDuration(lastUpdatedDuration)};
+        };
+        
+        for (var index in dataList) {
+            dataList[index]['alert'] = getStatus(dataList[index]);
+        }
+        return dataList;
+    }
+
     return agentData
 }

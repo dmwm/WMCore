8e89393cf0ae9b0ea840760cb337b9081f70009b
Diego Ballesteros <dballest@fnal.gov>
2012-11-30 07:45:54 -0600

    Change approach to getting tasks by dataset
    It was abusing of couchdb reduce by creating growing lists
    (doesn't matter if bounded). Change the approach to using
    a list with generates the desired mapping of tasks
    that produced a dataset.

--- src/couchapps/FWJRDump/views/outputByWorkflowName/reduce.js
+++ src/couchapps/FWJRDump/views/outputByWorkflowName/reduce.js
@@ -1,7 +1,6 @@
 function (key, values, rereduce) {
-  var output = {'size': 0, 'events': 0, 'count': 0, 'dataset': null, 'tasks': []};
+  var output = {'size': 0, 'events': 0, 'count': 0, 'dataset': null};
 
-  var storedTasks = {}
   for (var someValue in values) {
     output['dataset'] = values[someValue]['dataset'];
     output['size'] += values[someValue]['size'];
@@ -12,22 +11,6 @@ function (key, values, rereduce) {
     }
     else {
       output['count'] += 1;
-    }
-
-    if (rereduce) {
-      for (var i = 0; i < values[someValue]['tasks'].length; i++) {
-        var task = values[someValue]['tasks'][i]
-        if (!(task in storedTasks)){
-          output['tasks'].push(task)
-          storedTasks[task] = true
-        }
-      }
-    }
-    else {
-      if (!(values[someValue]['task'] in storedTasks)){
-        storedTasks[values[someValue]['task']] = true
-        output['tasks'].push(values[someValue]['task'])
-      }
     }
   }
 

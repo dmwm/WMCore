606862c5155deeee491d84535aacdbe095d54219
Seangchan Ryu <sryu@fnal.gov>
2013-01-24 15:54:12 -0600

    fix prep id search and make detail page not draggable

--- src/couchapps/WMStats/_attachments/js/Views/HTMLList/T1/WMStats.RequestDetailList.js
+++ src/couchapps/WMStats/_attachments/js/Views/HTMLList/T1/WMStats.RequestDetailList.js
@@ -41,6 +41,7 @@ WMStats.namespace('RequestDetailList');
             htmlstr += "<li><b>campaign:</b> " + reqDoc.campaign + "</li>";
             htmlstr += "<li><b>workflow:</b> " + WMStats.Utils.formatReqDetailUrl(reqDoc.workflow) + "</li>";
             htmlstr += "<li><b>agent url:</b> " + reqDoc.agent_url + "</li>";
+            htmlstr += "<li><b>prep id:</b> " + reqDoc.prep_id + "</li>";
             htmlstr += "<li><b>teams:</b> " + reqDoc.teams + "</li>";
             htmlstr += "<li><b>requetor:</b> " + reqDoc.requestor + "</li>";
             htmlstr += "<li><b>request date:</b> " + reqDoc.request_date + "</li>";

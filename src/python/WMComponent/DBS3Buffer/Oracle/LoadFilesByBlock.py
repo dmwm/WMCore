5a4c3ddd1a21f60c12bdd79dae90ace9226a78f8
Stephen Foulkes <sfoulkes@fnal.gov>
2013-02-04 15:20:23 -0600

    Fixes from the testing of DBS3 upload.

---
+++ src/python/WMComponent/DBS3Buffer/Oracle/LoadFilesByBlock.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+_LoadFilesByBlock_
+
+Oracle implementation of DBS3Buffer.LoadFilesByBlock
+"""
+
+from WMComponent.DBS3Buffer.MySQL.LoadFilesByBlock import LoadFilesByBlock as MySQLLoadFilesByBlock
+
+class LoadFilesByBlock(MySQLLoadFilesByBlock):
+    pass

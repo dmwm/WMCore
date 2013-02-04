a8280d4cbe012835f58e8e20f175291944098de2
Stephen Foulkes <sfoulkes@fnal.gov>
2013-02-04 15:20:23 -0600

    Fixes from the testing of DBS3 upload.

--- src/python/WMComponent/DBS3Buffer/MySQL/GetOpenBlocks.py
+++ src/python/WMComponent/DBS3Buffer/MySQL/GetOpenBlocks.py
@@ -2,21 +2,49 @@
 """
 _GetOpenBlocks_
 
-MySQL implementation of DBSBufferFiles.GetOpenBlocks
+MySQL implementation of DBS3Buffer.GetOpenBlocks
 """
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class GetOpenBlocks(DBFormatter):
-    sql = """SELECT DISTINCT blockname AS blockname, create_time AS create_time
-             FROM dbsbuffer_block
-             WHERE status = 'Open' OR status = 'Pending'"""
+    sql = """SELECT dbsbuffer_block.blockname AS blockname, dbsbuffer_block.create_time AS create_time,
+                    pending_parents.parent_count FROM dbsbuffer_block
+               LEFT OUTER JOIN
+                 (SELECT dbsbuffer_block.id AS block_id, COUNT(dbsbuffer_block.id) AS parent_count FROM dbsbuffer_block
+                    INNER JOIN dbsbuffer_file ON
+                      dbsbuffer_block.id = dbsbuffer_file.block_id
+                    LEFT OUTER JOIN dbsbuffer_file_parent ON
+                      dbsbuffer_file.id = dbsbuffer_file_parent.child
+                    LEFT OUTER JOIN dbsbuffer_file dbsbuffer_file_parent_detail ON
+                       dbsbuffer_file_parent.parent = dbsbuffer_file_parent_detail.id
+                    LEFT OUTER JOIN dbsbuffer_block dbsbuffer_block_parent ON
+                      dbsbuffer_file_parent_detail.block_id = dbsbuffer_block_parent.id
+                    WHERE dbsbuffer_block_parent.status3 = 'Pending'
+                    GROUP BY dbsbuffer_block.id) pending_parents ON
+                   dbsbuffer_block.id = pending_parents.block_id
+             WHERE dbsbuffer_block.status = 'Open' OR dbsbuffer_block.status = 'Pending' AND
+                   (pending_parents.parent_count IS NULL or pending_parents.parent_count = 0)"""    
 
     # Find only blocks that have been closed and uploaded to DBS2 but are still
     # waiting to be uploaded to DBS3.
-    sql3 = """SELECT DISTINCT blockname AS blockname, create_time AS create_time
-              FROM dbsbuffer_block
-              WHERE status = 'InDBS' AND (status3 = 'Open' OR status3 = 'Pending')"""    
+    sql3 = """SELECT dbsbuffer_block.blockname AS blockname, dbsbuffer_block.create_time AS create_time,
+                     pending_parents.parent_count FROM dbsbuffer_block
+                LEFT OUTER JOIN
+                  (SELECT dbsbuffer_block.id AS block_id, COUNT(dbsbuffer_block.id) AS parent_count FROM dbsbuffer_block
+                     INNER JOIN dbsbuffer_file ON
+                       dbsbuffer_block.id = dbsbuffer_file.block_id
+                     LEFT OUTER JOIN dbsbuffer_file_parent ON
+                       dbsbuffer_file.id = dbsbuffer_file_parent.child
+                     LEFT OUTER JOIN dbsbuffer_file dbsbuffer_file_parent_detail ON
+                        dbsbuffer_file_parent.parent = dbsbuffer_file_parent_detail.id
+                     LEFT OUTER JOIN dbsbuffer_block dbsbuffer_block_parent ON
+                       dbsbuffer_file_parent_detail.block_id = dbsbuffer_block_parent.id
+                     WHERE dbsbuffer_block_parent.status3 = 'Pending'
+                     GROUP BY dbsbuffer_block.id) pending_parents ON
+                    dbsbuffer_block.id = pending_parents.block_id
+              WHERE dbsbuffer_block.status = 'Closed' AND (dbsbuffer_block.status3 = 'Pending' OR dbsbuffer_block.status3 = 'Open') AND
+                    (pending_parents.parent_count IS NULL or pending_parents.parent_count = 0)"""
 
     def execute(self, dbs3OnlyUpload, conn = None, transaction = False):
         if dbs3OnlyUpload:
--- src/python/WMComponent/DBS3Buffer/MySQL/UpdateBlocks.py
+++ src/python/WMComponent/DBS3Buffer/MySQL/UpdateBlocks.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 """
-_DBS3Buffer.SetBlockStatus_
+_UpdateBlocks_
 
-Create new block in dbsbuffer_block
-Update file to reflect block information
+MySQL implementation of DBS3Buffer.UpdateBlocks
 """
-
-import threading
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -17,27 +14,18 @@ class UpdateBlocks(DBFormatter):
 
     sql3  = """UPDATE dbsbuffer_block SET status3 = :status WHERE blockname = :block"""
 
-    def __init__(self, logger = None, dbi = None):
-        myThread = threading.currentThread()
-        DBFormatter.__init__(self, myThread.logger, myThread.dbi)
-
-
     def execute(self, blocks, dbs3UploadOnly = False, conn = None, transaction = False):
-        """
-        _execute_
-
-        Changed to expect a DBSBlock object
-        """
         bindVars = []
 
-        for block in blocks:
-            bindVars.append({"block": block.getName(), "location": block.getLocation(),
-                             "status": block.status, "time": block.getStartTime()})
-
         if dbs3UploadOnly:
+            for block in blocks:
+                bindVars.append({"block": block.getName(), "status": block.status})
             self.dbi.processData(self.sql3, bindVars, conn = conn,
                                  transaction = transaction)
         else:
+            for block in blocks:
+                bindVars.append({"block": block.getName(), "location": block.getLocation(),
+                                 "status": block.status, "time": block.getStartTime()})            
             self.dbi.processData(self.sql, bindVars, conn = conn,
                                  transaction = transaction)            
 

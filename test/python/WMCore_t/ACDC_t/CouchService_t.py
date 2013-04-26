60143085f3e309731bd020eefd5cdea05b3132e7
Diego Ballesteros <dballest@fnal.gov>
2013-04-26 16:27:40 -0500

    ACDC Server CouchApp
    - Add the necessary dependencies for the acdcserver
    service for cmsweb deployment.
    - Add two new views to the ACDC couchapp which will allow cleaning operations
      based on request name and document age. Also include a basic rewrite function and
      a validate function which allows editing by the same rules as the ReqMgr couchapp.
    - Add a timestamp to all fileset ACDC records.
    - Add a script to cleanup the acdcserver in a crontab.

--- test/python/WMCore_t/ACDC_t/CouchService_t.py
+++ test/python/WMCore_t/ACDC_t/CouchService_t.py
@@ -9,11 +9,13 @@ Copyright (c) 2010 Fermilab. All rights reserved.
 
 import unittest
 import random
+import time
 
 from WMCore.ACDC.CouchService import CouchService
 from WMCore.ACDC.CouchFileset import CouchFileset
 from WMCore.ACDC.CouchCollection import CouchCollection
 
+from WMCore.Database.CMSCouch import CouchServer
 from WMCore.DataStructs.File import File
 from WMCore.Services.UUID import makeUUID
 
@@ -39,6 +41,66 @@ class CouchService_t(unittest.TestCase):
         """
         self.testInit.tearDownCouch()
         return
+
+    def populateCouchDB(self):
+        """
+        _populateCouchDB_
+
+        Populate the ACDC records
+        """
+        svc = CouchService(url = self.testInit.couchUrl,
+                           database = self.testInit.couchDbName)
+
+        ownerA = svc.newOwner("somegroup", "someuserA")
+        ownerB = svc.newOwner("somegroup", "someuserB")
+
+        testCollectionA = CouchCollection(database = self.testInit.couchDbName,
+                                          url = self.testInit.couchUrl,
+                                          name = "Thunderstruck")
+        testCollectionA.setOwner(ownerA)
+        testCollectionB = CouchCollection(database = self.testInit.couchDbName,
+                                          url = self.testInit.couchUrl,
+                                          name = "Struckthunder")
+        testCollectionB.setOwner(ownerA)
+        testCollectionC = CouchCollection(database = self.testInit.couchDbName,
+                                          url = self.testInit.couchUrl,
+                                          name = "Thunderstruck")
+        testCollectionC.setOwner(ownerB)
+        testCollectionD = CouchCollection(database = self.testInit.couchDbName,
+                                          url = self.testInit.couchUrl,
+                                          name = "Thunderstruck")
+        testCollectionD.setOwner(ownerB)
+
+        testFilesetA = CouchFileset(database = self.testInit.couchDbName,
+                                    url = self.testInit.couchUrl,
+                                    name = "TestFilesetA")
+        testCollectionA.addFileset(testFilesetA)
+        testFilesetB = CouchFileset(database = self.testInit.couchDbName,
+                                    url = self.testInit.couchUrl,
+                                    name = "TestFilesetB")
+        testCollectionB.addFileset(testFilesetB)
+        testFilesetC = CouchFileset(database = self.testInit.couchDbName,
+                                    url = self.testInit.couchUrl,
+                                    name = "TestFilesetC")
+        testCollectionC.addFileset(testFilesetC)
+        testFilesetD = CouchFileset(database = self.testInit.couchDbName,
+                                    url = self.testInit.couchUrl,
+                                    name = "TestFilesetD")
+        testCollectionC.addFileset(testFilesetD)
+
+        testFiles = []
+        for i in range(5):
+            testFile = File(lfn = makeUUID(), size = random.randint(1024, 4096),
+                            events = random.randint(1024, 4096))
+            testFiles.append(testFile)
+
+        testFilesetA.add(testFiles)
+        time.sleep(1)
+        testFilesetB.add(testFiles)
+        time.sleep(1)
+        testFilesetC.add(testFiles)
+        time.sleep(2)
+        testFilesetD.add(testFiles)
 
     def testListCollectionsFilesets(self):
         """
@@ -135,6 +197,57 @@ class CouchService_t(unittest.TestCase):
 
         svc.removeOwner(owner2)
         self.failUnless(len(svc.listOwners()) == 0)
+        return
+
+    def testTimestampAccounting(self):
+        """
+        _testTimestampAccounting_
+
+        Check the correct functioning of the timestamp view in the ACDC
+        couchapp and the function to remove old filesets.
+        """
+        self.populateCouchDB()
+        svc = CouchService(url = self.testInit.couchUrl,
+                           database = self.testInit.couchDbName)
+
+        currentTime = time.time()
+        database = CouchServer(self.testInit.couchUrl).connectDatabase(self.testInit.couchDbName)
+        results = database.loadView("ACDC", "byTimestamp", {"endkey" : currentTime})
+        self.assertEqual(len(results["rows"]), 4)
+        results = database.loadView("ACDC", "byTimestamp", {"endkey" : currentTime - 2})
+        self.assertEqual(len(results["rows"]), 3)
+        results = database.loadView("ACDC", "byTimestamp", {"endkey" : currentTime - 3})
+        self.assertEqual(len(results["rows"]), 2)
+        results = database.loadView("ACDC", "byTimestamp", {"endkey" : currentTime - 4})
+        self.assertEqual(len(results["rows"]), 1)
+        results = database.loadView("ACDC", "byTimestamp", {"endkey" : currentTime - 5})
+        self.assertEqual(len(results["rows"]), 0)
+        svc.removeOldFilesets(0)
+        results = database.loadView("ACDC", "byTimestamp", {"endkey" : currentTime})
+        self.assertEqual(len(results["rows"]), 0)
+        return
+
+    def testRemoveByCollectionName(self):
+        """
+        _testRemoveByCollectionName_
+
+        Check the function to obliterate all the filesets of a collection
+        """
+        self.populateCouchDB()
+        svc = CouchService(url = self.testInit.couchUrl,
+                           database = self.testInit.couchDbName)
+        database = CouchServer(self.testInit.couchUrl).connectDatabase(self.testInit.couchDbName)
+
+        results = database.loadView("ACDC", "byCollectionName", keys = ["Thunderstruck"])
+        self.assertTrue(len(results["rows"]) > 0)
+        svc.removeFilesetsByCollectionName("Thunderstruck")
+        results = database.loadView("ACDC", "byCollectionName", keys = ["Thunderstruck"])
+        self.assertEqual(len(results["rows"]), 0)
+        results = database.loadView("ACDC", "byCollectionName", keys = ["Struckthunder"])
+        self.assertTrue(len(results["rows"]) > 0)
+        svc.removeFilesetsByCollectionName("Struckthunder")
+        results = database.loadView("ACDC", "byCollectionName", keys = ["Struckthunder"])
+        self.assertEqual(len(results["rows"]), 0)
         return
 
 if __name__ == '__main__':

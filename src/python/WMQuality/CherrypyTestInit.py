c49e52d0dfaee857c281a75e900a527329b4defc
Seangchan Ryu <sryu@fnal.gov>
2013-05-23 16:06:41 -0500

    add WMCore.REST unittest framework

---
+++ src/python/WMQuality/CherrypyTestInit.py
@@ -0,0 +1,47 @@
+import threading
+import traceback
+import cherrypy
+import cherrypy.process.wspbus as cherrybus
+
+def start(server):
+    try:
+        server.start(blocking=False)
+    except RuntimeError, e:
+        # there appears to be worker threads from a previous test
+        # hanging out. Try to slay them so that we can keep going
+        print "Failed to load cherrypy with exception: %s\n" % e
+        print "The threads are: \n%s\n" % threading.enumerate()
+        print "The previous test was %s\n" % server.getLastTest()
+        print traceback.format_exc()
+        server.stop()
+        raise e
+
+def stop(server):
+    server.stop()
+    server.setLastTest()
+    # there was a ton of racy failures in REST tools because of
+    # how much global state cherrypy has. this resets it
+
+    # Also, it sucks I had to copy/paste this from
+    # https://bitbucket.org/cherrypy/cherrypy/src/9720342ad159/cherrypy/__init__.py
+    # but reload() doesn't have the right semantics
+
+    cherrybus.bus = cherrybus.Bus()
+    cherrypy.engine = cherrybus.bus
+    cherrypy.engine.timeout_monitor = cherrypy._TimeoutMonitor(cherrypy.engine)
+    cherrypy.engine.timeout_monitor.subscribe()
+
+    cherrypy.engine.autoreload = cherrypy.process.plugins.Autoreloader(cherrypy.engine)
+    cherrypy.engine.autoreload.subscribe()
+
+    cherrypy.engine.thread_manager = cherrypy.process.plugins.ThreadManager(cherrypy.engine)
+    cherrypy.engine.thread_manager.subscribe()
+
+    cherrypy.engine.signal_handler = cherrypy.process.plugins.SignalHandler(cherrypy.engine)
+    cherrypy.engine.subscribe('log', cherrypy._buslog)
+
+    from cherrypy import _cpserver
+    cherrypy.server = _cpserver.Server()
+    cherrypy.server.subscribe()
+    cherrypy.checker = cherrypy._cpchecker.Checker()
+    cherrypy.engine.subscribe('start', cherrypy.checker)
---
+++ src/python/WMQuality/REST/ServerSetup.py
@@ -0,0 +1,60 @@
+import os
+import cherrypy
+import logging
+from functools import wraps
+
+from WMCore.REST.Main import RESTMain
+from WMCore.REST.Test import fake_authz_headers, fake_authz_key_file
+from WMCore.Services.Requests import JSONRequests
+
+class RESTMainTestServer(object):
+    
+    def __init__(self, cfg, statedir, testName):
+        self.server = RESTMain(cfg, statedir)
+        self.testName = testName
+        self.cofig = cfg
+        self.port = cfg.main.port
+        self.host = '127.0.0.1'
+        self.serverUrl = "http://%s:%s/%s/" % (self.host, self.port, cfg.main.application)
+        ## test permission
+        #test_authz_key = fake_authz_key_file()
+        #self.header = fake_authz_headers(test_authz_key.data, roles = {"Global Admin": {'group': ['global']}})
+        
+        self.jsonSender = JSONRequests(self.serverUrl)
+        
+    def getLastTest(self):
+        global lastTest
+        return lastTest
+
+    def setLastTest(self):
+        global lastTest
+        lastTest = self.testName
+        
+    def start(self, blocking = True):
+        
+        self.server.validate_config()
+        self.server.setup_server()
+        self.server.install_application()
+        cherrypy.config.update({'server.socket_port': self.port})
+        cherrypy.config.update({'server.socket_host': self.host})
+        cherrypy.config.update({'request.show_tracebacks': True})
+        cherrypy.config.update({'environment': 'test_suite'})
+        for app in cherrypy.tree.apps.values():
+            app.config["/"]["request.show_tracebacks"] = True
+            
+        cherrypy.server.httpserver = None
+        cherrypy.engine.start()
+        if blocking:
+            cherrypy.engine.block()
+
+    def stop(self):
+        """
+        Stop the server
+        """
+        cherrypy.engine.exit()
+        cherrypy.engine.stop()
+
+        # Ensure the next server that's started gets fresh objects
+        for name, server in getattr(cherrypy, 'servers', {}).items():
+            server.unsubscribe()
+            del cherrypy.servers[name]

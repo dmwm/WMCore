768a8fcf7aa6c4b258580c9bad435efabad71161
Seangchan Ryu <sryu@fnal.gov>
2013-05-31 22:23:22 -0500

    add failed alert

--- src/couchapps/WMStats/_attachments/js/Views/HTMLList/T1/WMStats.RequestAlertGUI.js
+++ src/couchapps/WMStats/_attachments/js/Views/HTMLList/T1/WMStats.RequestAlertGUI.js
@@ -1,47 +1,38 @@
 WMStats.namespace("RequestAlertGUI");
 
 WMStats.RequestAlertGUI = function (requestData, containerDiv) {
-
+    
+    function displayAlert(errorArray, title) {
+        if (errorArray.length > 0) {
+            htmlList += '<fieldset><legend>' + title + '</legend> <div><ul>';
+        
+            for (var i in errorArray) {
+                var workflow = errorArray[i].getName();
+                var summary = errorArray[i].getSummary();
+                var cooloff = summary.getTotalCooloff()
+                var failure =summary.getTotalFailure();
+                var success = summary.getJobStatus("success");
+                htmlList += ('<li> <a class="requestAlert">' + workflow + "</a>: status:" + errorArray[i].getLastState() + ", cooloff " + cooloff + " failure:" + failure + " success:" + success + '</li>');
+            }
+            htmlList += "</ul></div></fieldset>";
+            
+            errorFlag = true;
+        };
+    };
+    
     var alertRequests = requestData.getRequestAlerts();
     var notPulledRequests = requestData.requestNotPulledAlert();
     var configError = alertRequests.configError;
     var siteError = alertRequests.siteError;
+    var failed = alertRequests.failed;
     var errorFlag = false;
-    var numError = configError.length + siteError.length;
+    var numError = configError.length + siteError.length + failed.length;
     var htmlList = "";
-    if (configError.length > 0) {
-        htmlList = '<fieldset>\
-                        <legend>Config Error</legend> <div><ul>';
     
-        for (var i in configError) {
-            var workflow = configError[i].getName();
-            var summary = configError[i].getSummary();
-            var cooloff = summary.getTotalCooloff()
-            var failure =summary.getTotalFailure();
-            var success = summary.getJobStatus("success");
-            htmlList += ('<li> <a class="requestAlert">' + workflow + "</a>: status:" + configError[i].getLastState() + ", cooloff " + cooloff + " failure:" + failure + " success:" + success + '</li>');
-        }
-        htmlList += "</ul></div></fieldset>";
-        
-        errorFlag = true;
-    }
-    
-    if (siteError.length > 0) {
-        htmlList += '<fieldset>\
-                        <legend>Site Error</legend> <div><ul>';
-    
-        for (var i in siteError) {
-            var workflow = siteError[i].getName();
-            var summary = siteError[i].getSummary();
-            var cooloff = summary.getTotalCooloff()
-            var failure =summary.getTotalFailure();
-            var success = summary.getJobStatus("success");
-            htmlList += ('<li> <a class="requestAlert">' + workflow + "</a>:  status:" + siteError[i].getLastState() + ", cooloff " + cooloff + " failure:" + failure + " success:" + success + '</li>');
-        }
-        htmlList += "</ul></div></fieldset>";
-        errorFlag = true;
-    }
-    
+    displayAlert(configError, "Config Error");
+    displayAlert(siteError, "Site Error");
+    displayAlert(failed, "failed");
+
     $(containerDiv).addClass("request_error_box");
     if (errorFlag) {
         $(containerDiv).removeClass("stable warning").addClass("error").html(htmlList);

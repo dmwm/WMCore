9a2203fc100beb9963e16dc52f1373c0763b85f2
Seangchan Ryu <sryu@fnal.gov>
2013-10-29 09:27:07 -0500

    fixes #4832

--- etc/WMAgentConfig.py
+++ etc/WMAgentConfig.py
@@ -216,7 +216,7 @@ config.ErrorHandler.logLevel = globalLogLevel
 config.ErrorHandler.maxRetries = maxJobRetries
 config.ErrorHandler.pollInterval = 240
 config.ErrorHandler.readFWJR = True
-config.ErrorHandler.failureExitCodes = [50660, 50661, 50664, 134]
+config.ErrorHandler.failureExitCodes = [50660, 50661, 50664]
 config.ErrorHandler.maxFailTime = 120000
 
 config.component_("RetryManager")

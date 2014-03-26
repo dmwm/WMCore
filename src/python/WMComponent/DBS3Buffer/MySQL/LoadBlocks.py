7e129b6af3d8f84c2430ce0cf9125ba65b97a729
Seangchan Ryu <sryu@fnal.gov>
2014-03-26 11:07:15 +0100

    fix missing workflow retrieval

--- src/python/WMComponent/DBS3Buffer/MySQL/LoadBlocks.py
+++ src/python/WMComponent/DBS3Buffer/MySQL/LoadBlocks.py
@@ -10,7 +10,8 @@ from WMCore.Database.DBFormatter import DBFormatter
 class LoadBlocks(DBFormatter):
     sql = """SELECT DISTINCT dbb.blockname as blockname, dbb.create_time as create_time,
                 dbb.status AS status, dbb.status3 AS status3,
-                dbl.se_name AS location, dbf3.dataset_algo AS das
+                dbl.se_name AS location, dbf3.dataset_algo AS das,
+                dbw.name AS workflow
               FROM dbsbuffer_block dbb
               INNER JOIN dbsbuffer_file dbf3 ON
                 dbf3.block_id = dbb.id

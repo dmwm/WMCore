8a199412f4f0c9a942180501e53122bf9168a3fc
Valentin Kuznetsov <vkuznet@gmail.com>
2014-12-17 12:22:32 -0500

    Expand ajax action to provide users with confirmation

--- src/html/ReqMgr/javascript/ajax_utils.js
+++ src/html/ReqMgr/javascript/ajax_utils.js
@@ -4,7 +4,30 @@ function ajaxRequest(path, parameters) {
     new Ajax.Updater('response', path,
     { method: 'post' ,
       parameters : parameters,
-      onException: function() {return;},
-      onComplete : function() {return;}
+      onCreate: function() {
+          var doc = document.getElementById('confirmation');
+          doc.innerHTML='Your request has been submitted';
+          doc.className='tools-alert tools-alert-blue confirmation shadow';
+      },
+      onException: function() {
+          var doc = document.getElementById('confirmation');
+          doc.innerHTML='ERROR! Your request has been failed';
+          doc.className='tools-alert tools-alert-red confirmation shadow';
+          setTimeout(function(){
+              var doc = document.getElementById('confirmation');
+              doc.innerHTML='';
+              doc.className='';
+          }, 5000);
+      },
+      onComplete : function() {
+          var doc = document.getElementById('confirmation');
+          doc.innerHTML='SUCCESS! Your request has been proceesed';
+          doc.className='tools-alert tools-alert-green confirmation shadow';
+          setTimeout(function(){
+              var doc = document.getElementById('confirmation');
+              doc.innerHTML='';
+              doc.className='';
+          }, 5000);
+      }
     });
 }

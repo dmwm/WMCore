8ba20ade9a98f404f5d590d786f8a2e560a076d4
Valentin Kuznetsov <vkuznet@gmail.com>
2014-12-17 09:34:33 -0500

    Remove ajaxScript since it is obsolete now

--- src/html/ReqMgr/javascript/ajax_utils.js
+++ src/html/ReqMgr/javascript/ajax_utils.js
@@ -8,19 +8,3 @@ function ajaxRequest(path, parameters) {
       onComplete : function() {return;}
     });
 }
-function ajaxScript(base) {
-    // base is a URL base, e.g. https://cmsweb.cern.ch
-    // get placehoder for code textarea
-    var cid=document.getElementById('code');
-    // get value of snippets drop-down menu (see create.tmpl)
-    var val=document.getElementById('snippets').value;
-    new Ajax.Updater('response', base+'/scripts',
-    { method: 'get' ,
-      parameters : {'name': val},
-      onException: function() {return;},
-      onSuccess : function(response) {
-          // update code textarea with returned value of script server API
-          cid.value=response.responseText;
-      }
-    });
-}

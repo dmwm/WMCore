56f636df45cdc0abd4225923109f2297b35bd2c0
Seangchan Ryu <sryu@fnal.gov>
2015-01-16 13:32:24 -0600

    remove couch view from acdc

--- src/couchapps/ACDC/views/owner_coll_fileset_files/map.js
+++
@@ -1,9 +0,0 @@
-function(doc) {
-  for (var lfn in doc.files) {
-    filesetFile = doc.files[lfn];
-    var locationCopy = filesetFile["locations"].slice();
-    locationCopy.sort();
-    emit([doc.owner.group, doc.owner.user, doc.collection_name, doc.fileset_name, 
-          locationCopy, filesetFile["lfn"]], filesetFile);
-  }
-}

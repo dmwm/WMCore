f9c2dcd4dd8bdbd63d79d3a2e4c43ee4ec1e89f3
Seangchan Ryu <sryu@fnal.gov>
2015-01-22 11:20:23 -0600

    remove reqmgr view

--- src/couchapps/ReqMgr/views/bystatusandtime/map.js
+++
@@ -1,7 +0,0 @@
-function(doc) {
-  var updateTime = null;
-  if (doc.RequestTransition) {
-  	updateTime = doc.RequestTransition[doc.RequestTransition.length - 1].UpdateTime;
-  }
-  emit([doc.RequestStatus, updateTime], null);
-}

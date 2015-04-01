a5c920a681155bbea26f5f678d28575bd9833fdf
Valentin Kuznetsov <vkuznet@gmail.com>
2015-04-01 10:38:56 -0400

    Implement dynamic creation of logdb_agent and upload requests view

--- src/python/WMCore/Services/LogDB/LogDBBackend.py
+++ src/python/WMCore/Services/LogDB/LogDBBackend.py
@@ -8,7 +8,7 @@ Interface to LogDB persistent storage
 import time
 import datetime
 
-from WMCore.Database.CMSCouch import CouchServer, CouchNotFoundError
+from WMCore.Database.CMSCouch import CouchServer, CouchNotFoundError, CouchConflictError
 from WMCore.Wrappers import JsonWrapper as json
 from WMCore.Services.LogDB.LogDBExceptions import LogDBError
 
@@ -34,6 +34,13 @@ def tstamp():
     tstamp = '%s.%s' % (base, now.microsecond)
     return float(tstamp)
 
+def design_doc():
+    """Return basic design document"""
+    rmap = dict(map="function(doc){ if(doc.request) emit(doc.request, doc)}")
+    views = dict(requests=rmap)
+    doc = dict(_id="_design/LogDB", views=views)
+    return doc
+
 class LogDBBackend(object):
     """
     Represents persistent storage for LogDB
@@ -44,14 +51,30 @@ class LogDBBackend(object):
         else:
             import logging
             self.logger = logging
-        
+
         self.db_url = db_url
         self.server = CouchServer(db_url)
-        dbs = self.server.listDatabases()
-        create = False if db_name in self.server.listDatabases() else True 
-        self.db = self.server.connectDatabase(db_name, create=create, size = 10000)
-        self.design = kwds.get('design', 'LogDB')
-        self.view = kwds.get('view', 'requests')
+        self.db_name = db_name
+        self.dbs = {} # map of agent db server, will be filled dynamically
+        self.design = kwds.get('design', 'LogDB') # name of design document
+        self.view = kwds.get('view', 'requests') # name of view to look-up requests
+
+    def dbserver(self, agent):
+        """Get or construct pointer to agent db server"""
+        if  agent not in self.dbs:
+            db_name = '%s_%s' % (self.db_name, agent)
+            create = False if db_name in self.server.listDatabases() else True
+            self.dbs[agent] = self.server.connectDatabase(db_name, create=create, size = 10000)
+            uri = '/%s/_design/%s' % (db_name, self.design)
+            data = design_doc()
+            try:
+                # insert design doc, if fails due to conflict continue
+                # conflict may happen due to concurrent client connection who
+                # created first this doc
+                self.dbs[agent].put(uri, data)
+            except CouchConflictError:
+                pass
+        return self.dbs[agent]
 
     def check(self, request, agent):
         """Check that given request/agent names are valid"""
@@ -67,14 +90,14 @@ class LogDBBackend(object):
         """Post new entry into LogDB for given request/agent pair"""
         self.check(request, agent)
         data = {"request":request, "ts":tstamp(), "msg":msg, "type":mtype}
-        res = self.db.commitOne(data)
+        res = self.dbserver(agent).commitOne(data)
         return res
 
     def get(self, request, agent, mtype="comment"):
         """Retrieve all entries from LogDB for given request/agent pair"""
         self.check(request, agent)
         spec = {'key':request}
-        docs = self.db.loadView(self.design, self.view, spec)
+        docs = self.dbserver(agent).loadView(self.design, self.view, spec)
         return docs
 
     def delete(self, request, agent):

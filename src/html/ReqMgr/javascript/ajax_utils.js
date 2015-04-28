92abaa45f982b6621c747f4dc84adcac6b28323c
Seangchan Ryu <sryu@fnal.gov>
2015-04-27 22:53:56 -0500

    fix reqmgr2 jQuery call

--- src/html/ReqMgr/javascript/ajax_utils.js
+++ src/html/ReqMgr/javascript/ajax_utils.js
@@ -5,8 +5,8 @@ function cleanConfirmation () {
 }
 function errorMessage(err) {
     // extract X-Error-Detail from server error
-    var pat = 'X-Error-Detail:'
-    var arr = err.split('\n')
+    var pat = 'X-Error-Detail:';
+    var arr = err.split('\n');
     var msg = "<h3>Server error message</h3>";
     for(var i=0; i<arr.length; i++) {
         if(arr[i].startsWith(pat)) {
@@ -95,8 +95,26 @@ function ajaxRequest(path, parameters, verb) {
         } else {
             //doc.innerHTML='WARNING! Your request has been processed with status code '+ arg1.status+' and '+msg+' '+arg2;
             //doc.className='tools-alert tools-alert-yellow confirmation fadeout shadow';
-            var headers = xhr.getAllResponseHeaders();
+            var headers = arg1.getAllResponseHeaders();
             errorMessage(headers);
         }
     });
 }
+
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};

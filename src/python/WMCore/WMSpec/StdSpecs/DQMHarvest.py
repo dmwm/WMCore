b0c77a0054d01d293355b308ea4492479f8f6f1d
amaltaro <alan.malta@cern.ch>
2015-05-15 13:52:51 +0200

    DQMHarvest spec remake; Template updated

--- src/python/WMCore/WMSpec/StdSpecs/DQMHarvest.py
+++ src/python/WMCore/WMSpec/StdSpecs/DQMHarvest.py
@@ -1,6 +1,7 @@
 from WMCore.Lexicon import dataset, block
 from WMCore.WMSpec.StdSpecs.StdBase import StdBase
 from WMCore.WMSpec.WMWorkloadTools import makeList
+from WMCore.WMSpec.WMSpecErrors import WMSpecFactoryException
 
 class DQMHarvestWorkloadFactory(StdBase):
     """
@@ -16,66 +17,48 @@ class DQMHarvestWorkloadFactory(StdBase):
         """
         StdBase.__call__(self, workloadName, arguments)
 
-        (self.inputPrimaryDataset, self.inputProcessedDataset, self.inputDataTier) = self.inputDataset[1:].split("/")
-
-        workload = self.createWorkload()
+        self.workload = self.createWorkload()
         
-        workload.setLFNBase(self.mergedLFNBase, self.unmergedLFNBase)
-        workload.setDashboardActivity("DQMHarvest")
-        self.reportWorkflowToDashboard(workload.getDashboardActivity())
+        self.workload.setDashboardActivity("harvesting")
+        self.reportWorkflowToDashboard(self.workload.getDashboardActivity())
 
-        mergeTask = workload.newTask("DQMHarvest")
-        self.addDashboardMonitoring(mergeTask)
-        mergeTaskCmssw = mergeTask.makeStep("cmsRun1")
-        mergeTaskCmssw.setStepType("CMSSW")
+        self.workload.setWorkQueueSplitPolicy("DatasetBlock", "FileBased", {"files_per_job": 99999} )
 
-        mergeTaskStageOut = mergeTaskCmssw.addStep("stageOut1")
-        mergeTaskStageOut.setStepType("StageOut")
+        # TODO maybe it can be removed since LFNBase shouldn't be needed here
+        self.workload.setLFNBase(self.mergedLFNBase, self.unmergedLFNBase)
+
+        # also creates the logCollect job by default
+        self.addDQMHarvestTask(uploadProxy = self.dqmUploadProxy,
+                               periodic_harvest_interval= self.periodicHarvestInterval,
+                               dqmHarvestUnit = self.dqmHarvestUnit)
         
-        mergeTaskLogArch = mergeTaskCmssw.addStep("logArch1")
-        mergeTaskLogArch.setStepType("LogArchive")
-
-        mergeTask.setSiteWhitelist(self.siteWhitelist)
-        mergeTask.setSiteBlacklist(self.siteBlacklist)
-
-        self.addLogCollectTask(mergeTask, taskName = "DQMHarvestMergeLogCollect")
-        
-        mergeTask.setTaskType("Merge")
-        mergeTask.applyTemplates()
-        
-        mergeTask.addInputDataset(primary = self.inputPrimaryDataset,
-                                  processed = self.inputProcessedDataset,
-                                  tier = self.inputDataTier,
-                                  dbsurl = self.dbsUrl,
-                                  block_blacklist = self.blockBlacklist,
-                                  block_whitelist = self.blockWhitelist,
-                                  run_blacklist = self.runBlacklist,
-                                  run_whitelist = self.runWhitelist)
-
-        splitAlgo = "ParentlessMergeBySize"
-        mergeTask.setSplittingAlgorithm(splitAlgo,
-                                        max_merge_size = self.maxMergeSize,
-                                        min_merge_size = self.minMergeSize,
-                                        max_merge_events = self.maxMergeEvents)
-        
-        mergeTaskCmsswHelper = mergeTaskCmssw.getTypeHelper()
-        mergeTaskCmsswHelper.cmsswSetup(self.frameworkVersion, softwareEnvironment = "",
-                                        scramArch = self.scramArch)
-        mergeTaskCmsswHelper.setGlobalTag(self.globalTag)
-        mergeTaskCmsswHelper.setSkipBadFiles(True)
-        mergeTaskCmsswHelper.setDataProcessingConfig("do_not_use", "merge")
-        
-        self.addOutputModule(mergeTask, "Merged",
-                             primaryDataset = self.inputPrimaryDataset,
-                             dataTier = self.dataTier,
-                             filterName = None,
-                             forceMerged = True)
-
         # setting the parameters which need to be set for all the tasks
         # sets acquisitionEra, processingVersion, processingString
-        workload.setTaskPropertiesFromWorkload()
+        self.workload.setTaskPropertiesFromWorkload()
         
-        return workload
+        return self.workload
+
+    def validateSchema(self, schema):
+        """
+        _validateSchema_
+
+        Standard StdBase schema validation, plus verification
+        of the DQMConfigCacheID.
+        """
+        StdBase.validateSchema(self, schema)
+
+        if not schema.get("DQMUploadUrl", None):
+            msg = "DQMUploadUrl parameter has not been provided in the request"
+            raise WMSpecFactoryException(message = msg)
+        if not schema.get("DQMConfigCacheID", None):
+            msg = "DQMConfigCacheID parameter has not been provided in the request"
+            raise WMSpecFactoryException(message = msg)
+
+        couchUrl = schema.get("ConfigCacheUrl", None)
+        self.validateConfigCacheExists(configID = schema["DQMConfigCacheID"],
+                                       couchURL = couchUrl,
+                                       couchDBName = schema["CouchDBName"],
+                                       getOutputModules = False)
 
     @staticmethod
     def getWorkloadArguments():
@@ -85,9 +68,8 @@ class DQMHarvestWorkloadFactory(StdBase):
         specArgs = {"RequestType" : {"default" : "DQMHarvest"},
                     "InputDataset" : {"default" : None, "optional" : False,
                                       "validate" : dataset},
-                    "DataTier" : {"default" : "DQMIO"},
                     "UnmergedLFNBase" : {"default" : "/store/unmerged"},
-                    "MergedLFNBase" : {"default" : "/store/results"},
+                    "MergedLFNBase" : {"default" : "/store/data"},
                     "MinMergeSize" : {"default" : 2 * 1024 * 1024 * 1024, "type" : int,
                                       "validate" : lambda x : x > 0},
                     "MaxMergeSize" : {"default" : 4 * 1024 * 1024 * 1024, "type" : int,
@@ -106,3 +88,91 @@ class DQMHarvestWorkloadFactory(StdBase):
         baseArgs.update(specArgs)
         StdBase.setDefaultArgumentsProperty(baseArgs)
         return baseArgs
+
+
+    def addDQMHarvestTask(self, uploadProxy = None, periodic_harvest_interval = 0,
+                          periodic_harvest_sibling = False, parentStepName = "cmsRun1",
+                          doLogCollect = True, dqmHarvestUnit = "byRun"):
+        """
+        _addDQMHarvestTask_
+
+        Create a DQM harvest task that does not depend on previous tasks.
+        """
+        if periodic_harvest_interval:
+            harvestType = "Periodic"
+        else:
+            harvestType = "EndOfRun"
+
+        harvestTask = self.workload.newTask("%sDQMHarvest" % harvestType)
+
+        self.addDashboardMonitoring(harvestTask)
+        harvestTaskCmssw = harvestTask.makeStep("cmsRun1")
+        harvestTaskCmssw.setStepType("CMSSW")
+
+        harvestTaskUpload = harvestTaskCmssw.addStep("upload1")
+        harvestTaskUpload.setStepType("DQMUpload")
+        harvestTaskLogArch = harvestTaskCmssw.addStep("logArch1")
+        harvestTaskLogArch.setStepType("LogArchive")
+
+        harvestTask.setTaskLogBaseLFN(self.unmergedLFNBase)
+        if doLogCollect:
+            self.addLogCollectTask(harvestTask, taskName = "%sDQMHarvestLogCollect" % harvestType)
+
+        harvestTask.setTaskType("Harvesting")
+        harvestTask.applyTemplates()
+
+        harvestTaskCmsswHelper = harvestTaskCmssw.getTypeHelper()
+        harvestTaskCmsswHelper.cmsswSetup(self.frameworkVersion, softwareEnvironment = "",
+                                          scramArch = self.scramArch)
+
+        harvestTaskCmsswHelper.setErrorDestinationStep(stepName = harvestTaskLogArch.name())
+        harvestTaskCmsswHelper.setGlobalTag(self.globalTag)
+        harvestTaskCmsswHelper.setOverrideCatalog(self.overrideCatalog)
+
+        harvestTaskCmsswHelper.setUserLFNBase("/")
+
+        (self.inputPrimaryDataset, self.inputProcessedDataset, self.inputDataTier) = self.inputDataset[1:].split("/")
+        harvestTask.addInputDataset(primary = self.inputPrimaryDataset,
+                                    processed = self.inputProcessedDataset,
+                                    tier = self.inputDataTier,
+                                    dbsurl = self.dbsUrl,
+                                    block_whitelist = self.blockWhitelist,
+                                    black_blacklist = self.blockBlacklist,
+                                    run_whitelist =self.runWhitelist,
+                                    run_blacklist = self.runBlacklist)
+
+        harvestTask.setSplittingAlgorithm("Harvest",
+                                          periodic_harvest_interval = periodic_harvest_interval,
+                                          periodic_harvest_sibling = periodic_harvest_sibling,
+                                          dqmHarvestUnit = dqmHarvestUnit)
+
+        if self.dqmConfigCacheID is not None:
+            if getattr(self, "configCacheUrl", None) is not None:
+                harvestTaskCmsswHelper.setConfigCache(self.configCacheUrl, self.dqmConfigCacheID, self.couchDBName)
+            else:
+                harvestTaskCmsswHelper.setConfigCache(self.couchURL, self.dqmConfigCacheID, self.couchDBName)
+            harvestTaskCmsswHelper.setDatasetName(self.inputDataset)
+        else:
+            scenarioArgs = { 'globalTag' : self.globalTag,
+                             'datasetName' : self.inputDataset,
+                             'runNumber' : self.runNumber,
+                             'dqmSeq' : self.dqmSequences }
+            if self.globalTagConnect:
+                scenarioArgs['globalTagConnect'] = self.globalTagConnect
+            if self.inputDataTier == "DQMIO":
+                scenarioArgs['newDQMIO'] = True
+            harvestTaskCmsswHelper.setDataProcessingConfig(self.procScenario,
+                                                           "dqmHarvesting",
+                                                           **scenarioArgs)
+
+        harvestTaskUploadHelper = harvestTaskUpload.getTypeHelper()
+        harvestTaskUploadHelper.setProxyFile(uploadProxy)
+        harvestTaskUploadHelper.setServerURL(self.dqmUploadUrl)
+
+        # if this was a Periodic harvesting add another for EndOfRun
+        if periodic_harvest_interval:
+            self.addDQMHarvestTask(uploadProxy = uploadProxy, periodic_harvest_interval = 0,
+                                   periodic_harvest_sibling = True, parentStepName = parentStepName,
+                                   doLogCollect = doLogCollect, dqmHarvestUnit = dqmHarvestUnit)
+
+        return

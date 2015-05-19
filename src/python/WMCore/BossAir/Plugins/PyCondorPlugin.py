67c6b001ad9bcc67e1f128dfd6904298598ad2c4
Alexander Richards <a.richards@imperial.ac.uk>
2015-05-19 11:01:53 +0200

    reverting changes that proved not to be cause of problem

--- src/python/WMCore/BossAir/Plugins/PyCondorPlugin.py
+++ src/python/WMCore/BossAir/Plugins/PyCondorPlugin.py
@@ -1011,8 +1011,8 @@ class PyCondorPlugin(BasePlugin):
 
             jdl.append("priority = %i\n" % (task_priority + prio*self.maxTaskPriority))
 
-#            jdl.append("+PostJobPrio1 = -%d\n" % len(job.get('potentialSites', [])))
-#            jdl.append("+PostJobPrio2 = -%d\n" % job['taskID'])
+            jdl.append("+PostJobPrio1 = -%d\n" % len(job.get('potentialSites', [])))
+            jdl.append("+PostJobPrio2 = -%d\n" % job['taskID'])
 
             jdl.append("+WMAgent_JobID = %s\n" % job['jobid'])
             jdl.append("job_machine_attrs = GLIDEIN_CMSSite\n")

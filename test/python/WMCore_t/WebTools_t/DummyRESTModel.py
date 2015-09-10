04b5d2679e7f45db0373e3966451afadef157e66
Valentin Kuznetsov <vkuznet@gmail.com>
2015-09-10 13:48:54 -0400

    Change type to instance as suggested by pylint

--- test/python/WMCore_t/WebTools_t/DummyRESTModel.py
+++ test/python/WMCore_t/WebTools_t/DummyRESTModel.py
@@ -138,7 +138,7 @@ class DummyRESTModel(RESTModel):
         return aList
 
     def listTypeValidate(self, request_input):
-        if type(request_input["aList"]) != list:
+        if not isinstance(request_input["aList"], list):
             request_input["aList"] = [int(request_input["aList"])]
         else:
             request_input["aList"] = map(int, request_input["aList"])
@@ -160,7 +160,7 @@ class DummyRESTModel(RESTModel):
             pass
         # Checks its first request_input contains a int
         try:
-            assert type(request_input['input_int']) == type(123)
+            assert isinstance(request_input['input_int'], type(123))
         except AssertionError:
             raise AssertionError('val_1 failed: %s not int' % type(request_input['input_int']))
         return request_input

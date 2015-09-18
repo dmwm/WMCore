2280bec4ffdb85c1152c620eb7e8ea72f86d3986
yuyi@fnal.gov <yuyi@fnal.gov>
2015-09-18 16:03:26 -0500

    add DBSRESTFormatter.py
    move upclass init to inside of __init__ and add _future__

---
+++ src/python/WMCore/WebTools/DBSRESTFormatter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+_DBSRESTFormatter_
+
+A basic REST formatter. The formatter takes the data from the API call, turns it into the
+appropriate format and sets the CherryPy header appropriately.
+
+Could add YAML via http://pyyaml.org/
+"""
+from __future__ import (division, print_function)
+from WMCore.WebTools.RESTFormatter import RESTFormatter
+
+class DBSRESTFormatter(RESTFormatter):
+    def __init__(self, config):
+	RESTFormatter.__init__(self, config)
+        self.supporttypes = {'application/xml': self.xml,
+                   'application/atom+xml': self.atom,
+                   'text/json': self.json,
+                   'text/x-json': self.json,
+                   'application/json': self.json,
+                   'text/html': self.to_string,
+                   'text/plain': self.to_string,
+                   '*/*': self.json}

c353c834d74a46e30867c15a13c207039fd161f6
Valentin Kuznetsov <vkuznet@gmail.com>
2015-09-18 21:37:59 -0400

    Revert changes upon Eric's request

--- src/python/WMCore/Services/DBS/DBSReader.py
+++ src/python/WMCore/Services/DBS/DBSReader.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 """
 _DBSReader_
-
 Readonly DBS Interface
-
 """
 import urlparse
 
 from WMCore.Services.DBS.DBSErrors import DBSReaderError
 from WMCore.Services.EmulatorSwitch import emulatorHook
-from WMCore.Services.DBS.DBS3Reader import DBS3Reader
+
 
 @emulatorHook
 def DBSReader(endpoint, **kwargs):
@@ -17,13 +15,17 @@ def DBSReader(endpoint, **kwargs):
     
     msg = ''
     try:
-        dbs = DBS3Reader(endpoint, **kwargs)
+        dbs = _getDBS3Reader(endpoint, **kwargs)
         # if this doesn't throw endpoint is dbs3
         dbs.dbs.serverinfo()
         return dbs
     except Exception as ex:
         msg += 'Instantiating DBS3Reader failed with %s\n' % str(ex)
-    raise DBSReaderError("Can't contact DBS at %s, kwargs %s, got errors %s" \
-            % (endpoint, kwargs, msg))
+    raise DBSReaderError("Can't contact DBS at %s, got errors %s" % (endpoint, msg))
+
+
+def _getDBS3Reader(endpoint, **kwargs):
+    from WMCore.Services.DBS.DBS3Reader import DBS3Reader as DBSReader
+    return DBSReader(endpoint, **kwargs)
 
 __all__ = [DBSReader]

949550dba899388ad7147d05737f23e2f39759c3
Valentin Kuznetsov <vkuznet@gmail.com>
2015-09-24 12:05:57 -0400

    New base class to deal with database(s) in unit tests

---
+++ src/python/WMCore/Database/DBTest.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#pylint: disable=
+"""
+File       : DBTest.py
+Author     : Valentin Kuznetsov <vkuznet AT gmail dot com>
+Description: Base class for WMCore database related unit tests.
+             It defines test database name based on user preferences specified via
+             environment variables and setup logic to setup/tear down actions
+             based on database.
+"""
+from __future__ import print_function
+
+# system modules
+import os
+import sys
+import unittest
+
+class DBTest(unittest.TestCase):
+    def __init__(self, methodName='runTest'):
+        super(WMCoreUnitTest, self).__init__(methodName)
+        self.dbName = os.environ.get('WMCORE_TEST_DATABASE', \
+                'unittest_%s' % self.__class__.__name__)
+        print("WMCoreTestDB test %s database" % self.dbName)
+        enforce = os.environ.get('WMCORE_TEST_DATABASE_DELETE', False)
+        if  enforce:
+            print("WMCORE_TEST_DATABASE_DELETE=%s, will delete %s" % (enforce, self.dbName))
+            self.deleteDatabase()
+            self.createDatabase()
+
+    def setUp(self):
+        """Setup database for unittests"""
+        if  self.dbName.startswith('unittest'):
+            self.deleteDatabase()
+            self.createDatabase()
+
+    def tearDown(self):
+        """Tear down database"""
+        if self.dbName.startswith('unittest'):
+            self.deleteDatabase()
+
+    def createDatabase(self):
+        """Abstract method to create test database, must be implemented in sub-classes"""
+        print("Create %s" % self.dbName)
+
+    def deleteDatabase(self):
+        """Abstract method to delete test database, must be implemented in sub-classes"""
+        print("Delete %s" % self.dbName)

9db14e9cf8528cf67ee9461a4d84988b7a0af19e
amaltaro <alan.malta@cern.ch>
2015-09-28 14:40:17 +0200

    Fixed byinputdataset view

--- src/couchapps/ReqMgr/views/byinputdataset/map.js
+++ src/couchapps/ReqMgr/views/byinputdataset/map.js
@@ -1,6 +1,21 @@
-
 function(doc) {
-  if (doc.InputDataset){
-    emit(doc.InputDataset, null) ;
-  } 
+  if (doc.InputDataset && doc.InputDataset !== "None"){
+      emit(doc.InputDataset, null) ;
+  }
+  
+  if (doc.TaskChain){
+  	for (var i = 0; i < doc.TaskChain; i++) {
+  		if (doc["Task" + (i+1)].InputDataset && doc.InputDataset !== "None") {
+  			emit(doc["Task" + (i+1)].InputDataset, null);
+  		}
+  	} 
+  }
+  
+  if (doc.StepChain){
+  	for (var i = 0; i < doc.StepChain; i++) {
+  		if (doc["Step" + (i+1)].InputDataset && doc.InputDataset !== "None") {
+  			emit(doc["Step" + (i+1)].InputDataset, null);
+  		}
+  	} 
+  }  
 }

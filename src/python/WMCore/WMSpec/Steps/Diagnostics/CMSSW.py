969ec3fbaa6e845586d596ff67bcb1b94cb8e08d
Eric Vaandering <ericvaandering@gmail.com>
2015-12-03 14:56:20 -0600

    Fixup pylint

--- src/python/WMCore/WMSpec/Steps/Diagnostics/CMSSW.py
+++ src/python/WMCore/WMSpec/Steps/Diagnostics/CMSSW.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#pylint: disable=W0212
+# pylint: disable=W0212
 # W0212: Names are not accessible for WMTask objects, etc.
 """
 _CMSSW_
@@ -9,28 +9,32 @@ Diagnostic implementation for a CMSSW job
 
 """
 
-import os.path
 import logging
-from WMCore.WMSpec.Steps.Diagnostic import Diagnostic, DiagnosticHandler
-from WMCore.FwkJobReport.Report import FwkJobReportException
+import os.path
 
 import WMCore.Algorithms.BasicAlgos as BasicAlgos
+from WMCore.FwkJobReport.Report import FwkJobReportException
+from WMCore.WMSpec.Steps.Diagnostic import Diagnostic, DiagnosticHandler
+
 
 class Exit127(DiagnosticHandler):
     """
     Handle non-existant executable
 
     """
+
     def __call__(self, errCode, executor, **args):
         msg = "Executable Not Found"
         executor.report.addError(executor.step._internal_name,
                                  50110, "ExecutableNotFound", msg)
+
 
 class Exit126(DiagnosticHandler):
     """
     Handle bad permissions
 
     """
+
     def __call__(self, errCode, executor, **args):
         msg = "Executable permissions not executable"
         if args.get('ExceptionInstance', False):
@@ -38,11 +42,13 @@ class Exit126(DiagnosticHandler):
         executor.report.addError(executor.step._internal_name,
                                  50111, "ExecutableBadPermissions", msg)
 
+
 class Exit50513(DiagnosticHandler):
     """
     Handle SCRAM script failure
 
     """
+
     def __call__(self, errCode, executor, **args):
         """
         Added for Steve to handle SCRAM script failure
@@ -79,9 +85,8 @@ class CMSDefaultHandler(DiagnosticHandler):
     Default handler for miscellaneous CMSSW errors.
     """
 
-
     def __call__(self, errCode, executor, **args):
-        logging.critical("%s Diagnostic Handler invoked" % self.__class__.__name__)
+        logging.critical("%s Diagnostic Handler invoked", self.__class__.__name__)
         msg = "Error in CMSSW: %s\n" % (errCode)
         jobRepXml = os.path.join(executor.step.builder.workingDir,
                                  executor.step.output.jobReport)
@@ -129,13 +134,11 @@ class CMSDefaultHandler(DiagnosticHandler):
             msg += "".join(logTail)
 
         # make sure the report has the error in it
-        errSection = getattr(executor.report.report, "errors", None)
+        dummy = getattr(executor.report.report, "errors", None)  # Seems to do nothing
         executor.report.addError(executor.step._internal_name,
                                  errCode, description, msg)
 
         return
-
-
 
 
 class CMSRunHandler(DiagnosticHandler):
@@ -145,6 +148,7 @@ class CMSRunHandler(DiagnosticHandler):
     Base util for a cmsRun bork, check job report exists and contains appropriate error
 
     """
+
     def __init__(self, code, desc):
         DiagnosticHandler.__init__(self)
         self.code = code
@@ -165,7 +169,6 @@ class CMSRunHandler(DiagnosticHandler):
                 pass
             reportStep = executor.report.retrieveStep(executor.step._internal_name)
             reportStep.status = self.code
-
 
         errLog = os.path.join(os.path.dirname(jobRepXml),
                               '%s-stderr.log' % (executor.step._internal_name))
@@ -194,6 +197,7 @@ class CMSRunHandler(DiagnosticHandler):
         print executor.report.report.errors
         return
 
+
 class EDMExceptionHandler(DiagnosticHandler):
     """
     _EDMExceptionHandler_
@@ -204,6 +208,7 @@ class EDMExceptionHandler(DiagnosticHandler):
     If the job report isnt there, thats a specific failure
 
     """
+
     def __call__(self, errCode, executor, **args):
         """
         _operator()_
@@ -218,7 +223,6 @@ class EDMExceptionHandler(DiagnosticHandler):
                               '%s-stderr.log' % (executor.step._internal_name))
         outLog = os.path.join(os.path.dirname(jobRepXml),
                               '%s-stdout.log' % (executor.step._internal_name))
-
 
         addOn = '\n'
         if os.path.exists(errLog):
@@ -264,7 +268,7 @@ class EDMExceptionHandler(DiagnosticHandler):
             return
 
         else:
-            #check exit code in report is non zero
+            # check exit code in report is non zero
             if executor.report.report.status == 0:
                 msg = "Job Report contains no error report, but cmsRun exited non-zero: %s" % errCode
                 msg += addOn
@@ -279,7 +283,6 @@ class EDMExceptionHandler(DiagnosticHandler):
         return
 
 
-
 class CMSSW(Diagnostic):
     """
     Master diagnostic class
@@ -292,19 +295,17 @@ class CMSSW(Diagnostic):
         self.handlers[50513] = Exit50513()
         self.handlers[126] = Exit126()
         self.handlers[127] = Exit127()
-        self.handlers[65]  = CMSRunHandler(8001, "CMSExeption")
-        self.handlers[66]  = CMSRunHandler(8002, "StdExeption")
-        self.handlers[67]  = CMSRunHandler(8003, "UnknownExeption")
-        self.handlers[68]  = CMSRunHandler(8004, "StdBadAlloc")
-        self.handlers[88]  = CMSRunHandler(7000, "CommandLineProcessing")
-        self.handlers[89]  = CMSRunHandler(7001, "ConfigFileNotFound")
-        self.handlers[90]  = CMSRunHandler(7002, "ConfigFileReadError")
-
+        self.handlers[65] = CMSRunHandler(8001, "CMSExeption")
+        self.handlers[66] = CMSRunHandler(8002, "StdExeption")
+        self.handlers[67] = CMSRunHandler(8003, "UnknownExeption")
+        self.handlers[68] = CMSRunHandler(8004, "StdBadAlloc")
+        self.handlers[88] = CMSRunHandler(7000, "CommandLineProcessing")
+        self.handlers[89] = CMSRunHandler(7001, "ConfigFileNotFound")
+        self.handlers[90] = CMSRunHandler(7002, "ConfigFileReadError")
 
         self.defaultHandler = CMSDefaultHandler()
-
 
         # for all the exception codes between 1 and 225, use a default that attempts to read the code
         # from the job report
         catchAll = EDMExceptionHandler()
-        [ self.handlers.__setitem__(x, catchAll) for x in range(0, 255) if x not in self.handlers ]
+        [self.handlers.__setitem__(x, catchAll) for x in range(0, 255) if x not in self.handlers]

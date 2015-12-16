2a7083aa5ac4353438829f7e6c68fec68c392462
Eric Vaandering <ericvaandering@gmail.com>
2015-12-16 14:09:45 -0600

    A program to be run interactively to add to or replace the JSON files in test/data used to mock the DBS emulator

---
+++ src/python/WMQuality/Emulators/DBSClient/MockedDBSphys03Calls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from __future__ import (division, print_function)
+
+endpoint = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSReader'
+
+datasets = []
+
+calls = [
+    ['listBlockOrigin', {'block_name': '/HighPileUp/Run2011A-v1/RAW#fabf118a-cbbf-11e0-80a9-003048caabcd'}],
+    ['listBlockOrigin', {
+        'block_name': '/GenericTTbar/hernan-140317_231446_crab_JH_ASO_test_T2_ES_CIEMAT_5000_100_140318_0014-ea0972193530f531086947d06eb0f121/USER#fb978442-a61b-413a-b4f4-526e6cdb142e'}],
+    ['listBlockOrigin', {
+        'block_name': '/GenericTTbar/hernan-140317_231446_crab_JH_ASO_test_T2_ES_CIEMAT_5000_100_140318_0014-ea0972193530f531086947d06eb0f121/USER#0b04d417-d734-4ef2-88b0-392c48254dab'}],
+]

a13d6991153c72175b7e541f4d09208b441d61cb
Eric Vaandering <ericvaandering@gmail.com>
2016-01-20 13:26:11 -0600

    Remove possibility of cjson and simplejson from json wrapper and cleanup the tests and remove cjson from the tests

--- test/python/WMCore_t/Wrappers_t/JsonWrapper_t.py
+++ test/python/WMCore_t/Wrappers_t/JsonWrapper_t.py
@@ -3,135 +3,38 @@
 Unit tests for json wrapper.
 """
 
-import nose
+import json
 import unittest
-import tempfile
-import os
-
-try:
-    import simplejson as json
-except:
-    import json
 
 import WMCore.Wrappers.JsonWrapper as json_wrap
+
 
 class TestWrapper(unittest.TestCase):
     """
     JSON wrapper unit tests.
     """
+
     def setUp(self):
         """Init parameters"""
-        self.record = {"test":1}
-
-    def test_cjson(self):
-        """
-        Test cjson implementation.
-        """
-        try:
-            import cjson
-        except:
-            raise nose.SkipTest
-
-        json_wrap._module = "cjson"
-        result = json_wrap.dumps(self.record)
-        expect = json.dumps(self.record)
-        self.assertEqual(expect, result)
-
-        data   = result
-
-        result = json_wrap.loads(data)
-        expect = json.loads(data)
-        self.assertEqual(expect, result)
-
-        try:
-            json_wrap.loads("bogusbogus")
-        except cjson.DecodeError as ex:
-            self.assertEqual(ex.args, ("cannot parse JSON description: bogusbogus",))
+        self.record = {"test": 1}
 
     def test_json(self):
         """
         Test default json implementation.
         """
-        json_wrap._module = "json"
         result = json_wrap.dumps(self.record)
         expect = json.dumps(self.record)
         self.assertEqual(expect, result)
 
-        data   = result
+        data = result
 
         result = json_wrap.loads(data)
         expect = json.loads(data)
         self.assertEqual(expect, result)
 
-        try:
+        with self.assertRaises(ValueError):
             json_wrap.loads("bogusbogus")
-        except ValueError as ex:
-            self.assertEqual(ex.args, ("No JSON object could be decoded: bogusbogus",))
 
-    def test_string_compare(self):
-        """
-        Test that cjson and json libraries do the same thing.
-        """
-        try:
-            import cjson
-        except:
-            raise nose.SkipTest
-
-        json_wrap._module = "cjson"
-        json_wrap._module = "cjson"
-        cj_result = json_wrap.dumps(self.record)
-        json_wrap._module = "json"
-        dj_result = json_wrap.dumps(self.record)
-        self.assertEqual(dj_result, cj_result)
-
-        data   = dj_result
-
-        json_wrap._module = "cjson"
-        cj_result = json_wrap.loads(data)
-        json_wrap._module = "json"
-        dj_result = json_wrap.loads(data)
-        self.assertEqual(dj_result, cj_result)
-
-    def test_file_compare(self):
-        """
-        _test_file_compare_
-
-        """
-        try:
-            import cjson
-        except:
-            raise nose.SkipTest
-
-        f = tempfile.NamedTemporaryFile()
-        f.delete = False
-
-        json_wrap._module = "cjson"
-        #write self.record to a file via cjson
-        json_wrap._module = "cjson"
-        json_wrap.dump(self.record, f)
-        f.close()
-        #read the file with the json
-        f = open(f.name, 'r')
-        json_wrap._module = "json"
-        data = json_wrap.load(f)
-        f.close()
-        self.assertEqual(data, self.record)
-        #Clean up
-        os.remove(f.name)
-
-        #write self.record to a file via json
-        f = tempfile.NamedTemporaryFile()
-        f.delete = False
-        json_wrap.dump(self.record, f)
-        f.close()
-        #read the file with cjson
-        f = open(f.name, 'r')
-        json_wrap._module = "cjson"
-        data = json_wrap.load(f)
-        f.close()
-        self.assertEqual(data, self.record)
-        #Clean up
-        os.remove(f.name)
 
 if __name__ == "__main__":
     unittest.main()

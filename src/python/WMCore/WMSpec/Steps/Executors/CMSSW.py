6544dbb27b9e3ab96a64915f89320ccfc9a482a1
Emilis Rupeika <phonixreal@gmail.com>
2016-05-26 19:35:16 +0200

    Merge Justas' patch for untarring with python 2.6 for older releases

--- src/python/WMCore/WMSpec/Steps/Executors/CMSSW.py
+++ src/python/WMCore/WMSpec/Steps/Executors/CMSSW.py
@@ -378,7 +378,13 @@ if [ -n "$USER_TARBALL" ] ; then
     EXIT_STATUS=$?
     if [ $EXIT_STATUS -ne 0 ]; then
        echo "***\nCouldn't untar sandbox with python2: $EXIT_STATUS\n";
-       exit 74;
+       echo "***\nWill try with python2.6 as it might be an old CMSSW release!"
+       python2.6 -m WMCore.WMRuntime.Scripts.UnpackUserTarball $USER_TARBALL $USER_FILES
+       EXIT_STATUS=$?
+       if [ $EXIT_STATUS -ne 0 ]; then
+          echo "***\nCouldn't untar sandbox with python2.6: $EXIT_STATUS\n";
+          exit 74;
+       fi
     fi
 fi
 echo "Completed SCRAM project"

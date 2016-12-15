6516f34bcae847be3eafe999c519e26719ae682c
amaltaro <alan.malta@cern.ch>
2016-12-15 22:26:58 +0100

    Pylint fixes

--- src/python/WMCore/WMRuntime/Startup.py
+++ src/python/WMCore/WMRuntime/Startup.py
@@ -17,10 +17,8 @@ if __name__ == '__main__':
     task = Bootstrap.loadTask(job)
     print("Startup.py : %s : setting up monitoring" % time.strftime("%Y-%m-%dT%H:%M:%S"))
     logLocation = "Report.%i.pkl" % job['retry_count']
-    Bootstrap.createInitialReport(job = job,
-                                  task = task,
-                                  logLocation = logLocation)
-    monitor = Bootstrap.setupMonitoring(logPath = logLocation)
+    Bootstrap.createInitialReport(job=job, logLocation=logLocation)
+    monitor = Bootstrap.setupMonitoring(logPath=logLocation)
 
     print("Startup.py : %s : setting up logging" % time.strftime("%Y-%m-%dT%H:%M:%S"))
     Bootstrap.setupLogging(os.getcwd())
@@ -30,8 +28,8 @@ if __name__ == '__main__':
     print("Startup.py : %s : executing task" % time.strftime("%Y-%m-%dT%H:%M:%S"))
     task.execute(job)
     print("Startup.py : %s : completing task" % time.strftime("%Y-%m-%dT%H:%M:%S"))
-    task.completeTask(jobLocation = os.getcwd(),
-                      logLocation = logLocation)
+    task.completeTask(jobLocation=os.getcwd(),
+                      logLocation=logLocation)
     print("Startup.py : %s : shutting down monitor" % time.strftime("%Y-%m-%dT%H:%M:%S"))
     os.fchmod(1, 0o664)
     os.fchmod(2, 0o664)

92a6e363c82b560d41cd0040b211b6e916972725
Eric Vaandering <ericvaandering@gmail.com>
2017-01-13 10:00:18 -0600

    Build a map of scram arch to required OS and vice versa

--- src/python/WMCore/WMRuntime/Tools/Scram.py
+++ src/python/WMCore/WMRuntime/Tools/Scram.py
@@ -34,6 +34,14 @@ import sys
 import subprocess
 import logging
 
+ARCH_TO_OS = {'slc5': ['rhel6'], 'slc6': ['rhel6'], 'slc7': ['rhel7']}
+
+OS_TO_ARCH = {}
+for arch, oses in ARCH_TO_OS.iteritems():
+    for osName in oses:
+        if osName not in OS_TO_ARCH:
+            OS_TO_ARCH[osName] = []
+        OS_TO_ARCH[osName].append(arch)
 
 def testWriter(func, *args):
     """

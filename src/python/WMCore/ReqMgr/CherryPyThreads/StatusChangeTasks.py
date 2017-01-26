317ddf4c5c70015e9f94ad608e46daed0d776ab0
Alan Malta Rodrigues <amaltaro@uscmsmac02.dyndns.cern.ch>
2017-01-26 10:54:18 +0100

    Pylint stuff

--- src/python/WMCore/ReqMgr/CherryPyThreads/StatusChangeTasks.py
+++ src/python/WMCore/ReqMgr/CherryPyThreads/StatusChangeTasks.py
@@ -1,7 +1,6 @@
-'''
+"""
 Created on May 19, 2015
-
-'''
+"""
 from __future__ import (division, print_function)
 
 from Utils.CherryPyPeriodicTask import CherryPyPeriodicTask
@@ -9,8 +8,8 @@ from WMCore.ReqMgr.DataStructs.RequestStatus import AUTO_TRANSITION
 from WMCore.Services.WorkQueue.WorkQueue import WorkQueue
 from WMCore.Services.RequestDB.RequestDBWriter import RequestDBWriter
 
+
 def moveForwardStatus(reqDBWriter, wfStatusDict):
-    
     for status, nextStatus in AUTO_TRANSITION.iteritems():
         requests = reqDBWriter.getRequestByStatus([status])
         for wf in requests:
@@ -23,7 +22,7 @@ def moveForwardStatus(reqDBWriter, wfStatusDict):
                 reqDBWriter.updateRequestStatus(wf, stateFromGQ)
                 print("%s in %s moved to %s" % (wf, status, stateFromGQ))
                 continue
-            
+
             try:
                 i = nextStatus.index(stateFromGQ)
             except ValueError:
@@ -34,19 +33,21 @@ def moveForwardStatus(reqDBWriter, wfStatusDict):
                 reqDBWriter.updateRequestStatus(wf, "aborted-completed")
                 print("%s in %s moved to %s" % (wf, status, "aborted-completed"))
             else:
-                for j in range(i+1):
+                for j in range(i + 1):
                     reqDBWriter.updateRequestStatus(wf, nextStatus[j])
                     print("%s in %s moved to %s" % (wf, status, nextStatus[j]))
     return
 
+
 def moveToArchivedForNoJobs(reqDBWriter, wfStatusDict):
-    '''
+    """
     Handle the case when request is aborted/rejected before elements are created in GQ
-    '''
+    """
+
     statusTransition = {"aborted": ["aborted-completed", "aborted-archived"],
                         "aborted-completed": ["aborted-archived"],
                         "rejected": ["rejected-archived"]}
-    
+
     for status, nextStatusList in statusTransition.items():
         requests = reqDBWriter.getRequestByStatus([status])
         count = 0
@@ -54,20 +55,16 @@ def moveToArchivedForNoJobs(reqDBWriter, wfStatusDict):
             # check whether wq elements exists for given request
             # if not, it means 
             if wf not in wfStatusDict:
-                for nextStatus in nextStatusList: 
+                for nextStatus in nextStatusList:
                     reqDBWriter.updateRequestStatus(wf, nextStatus)
                     count += 1
         print("Total %s-archived: %d" % (status, count))
-    
+
     return
-            
-            
 
 
 class StatusChangeTasks(CherryPyPeriodicTask):
-
     def __init__(self, rest, config):
-
         CherryPyPeriodicTask.__init__(self, config)
 
     def setConcurrentTasks(self, config):
@@ -80,14 +77,13 @@ class StatusChangeTasks(CherryPyPeriodicTask):
         """
         gather active data statistics
         """
-        
+
         reqDBWriter = RequestDBWriter(config.reqmgrdb_url)
         gqService = WorkQueue(config.workqueue_url)
-        
+
         wfStatusDict = gqService.getWorkflowStatusFromWQE()
-        
+
         moveForwardStatus(reqDBWriter, wfStatusDict)
-        moveToArchivedForNoJobs(reqDBWriter, wfStatusDict) 
-                  
+        moveToArchivedForNoJobs(reqDBWriter, wfStatusDict)
+
         return
-                    

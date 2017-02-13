ac522d712cf7eb9d7123bbf21ab007ee46a8ad69
Alan Malta Rodrigues <amaltaro@uscmsmac02.dyndns.cern.ch>
2017-02-13 15:16:13 +0100

    pylint stuff
    fix unit tests
    
    remove defaultdict

--- test/python/WMCore_t/Cache_t/GenericDataCache_t.py
+++ test/python/WMCore_t/Cache_t/GenericDataCache_t.py
@@ -10,6 +10,11 @@ import time
 from WMCore.Cache.GenericDataCache import GenericDataCache, CacheExistException, \
                           CacheWithWrongStructException, MemoryCacheStruct
 
+
+class Foo(object):
+    pass
+
+
 class GenericDataCacheTest(unittest.TestCase):
 
     def testBasic(self):
@@ -18,9 +23,10 @@ class GenericDataCacheTest(unittest.TestCase):
 
         Basic stuff.
         """
-        mc = MemoryCacheStruct(1, lambda x: int(time.time()), {'x':1})
+        mc = MemoryCacheStruct(1, lambda x: int(time.time()), kwargs={'x':1})
+        self.assertIsNone(mc.data)
         self.assertEqual(mc.lastUpdated, -1)
-        
+
         GenericDataCache.registerCache("test", mc)
         with self.assertRaises(CacheExistException):
             GenericDataCache.registerCache("test", mc)
@@ -32,8 +38,30 @@ class GenericDataCacheTest(unittest.TestCase):
         after = mc2.getData()
         self.assertFalse(before == after)
         self.assertFalse(mc2.lastUpdate == -1)
-        
+
         return
+
+    def testBasicInit(self):
+        """
+        _testBasicInit_
+
+        Test init values
+        """
+        mc = MemoryCacheStruct(0, lambda x: x, initCacheValue=Foo())
+        self.assertIsInstance(mc.data, Foo)
+
+        mc1 = MemoryCacheStruct(0, lambda x: sum(x), [], kwargs={'x': [1, 2]})
+        self.assertEqual(mc1.data, [])
+        after = mc1.getData()
+        self.assertEqual(after, 3)
+
+        mc2 = MemoryCacheStruct(0, lambda x: x, {}, kwargs={'x': {'one':1, 'two':2}})
+        self.assertEqual(mc2.data, {})
+        after = mc2.getData()
+        self.assertItemsEqual(after.keys(), ['one', 'two'])
+
+        return
+
 
 if __name__ == "__main__":
     unittest.main()

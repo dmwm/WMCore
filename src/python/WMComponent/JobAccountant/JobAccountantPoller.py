023a3ca9aee75b85dc03b87783c669f177c91e42
Alan Malta Rodrigues <amaltaro@uscmsmac02.dyndns.cern.ch>
2017-03-20 11:35:18 +0100

    Remove alert system code; pylint fixes
    Fix more pylint

--- src/python/WMComponent/JobAccountant/JobAccountantPoller.py
+++ src/python/WMComponent/JobAccountant/JobAccountantPoller.py
@@ -5,17 +5,16 @@ _JobAccountantPoller_
 Poll WMBS for complete jobs and process their framework job reports.
 """
 
-import time
 import threading
 import logging
 
 from Utils.IteratorTools import grouper
 from WMCore.WorkerThreads.BaseWorkerThread import BaseWorkerThread
-from WMCore.Agent.Harness import Harness
 from WMCore.Database.CouchUtils import CouchConnectionError
 from WMCore.DAOFactory import DAOFactory
 from WMComponent.JobAccountant.AccountantWorker import AccountantWorker
 from WMCore.WMException import WMException
+
 
 class JobAccountantPollerException(WMException):
     """
@@ -26,43 +25,39 @@ class JobAccountantPollerException(WMException):
     from the worker).
     """
 
+
 class JobAccountantPoller(BaseWorkerThread):
     def __init__(self, config):
         BaseWorkerThread.__init__(self)
         self.config = config
         self.accountantWorkSize = getattr(self.config.JobAccountant, 'accountantWorkSize', 100)
-        # initialize the alert framework (if available - config.Alert present)
-        #    self.sendAlert will be then be available
-        self.initAlerts(compName = "JobAccountant")
 
         return
 
-    def setup(self, parameters = None):
+    def setup(self, parameters=None):
         """
         _setup_
 
         Instantiate the requisite number of accountant workers and create a
         processpool with them.  Also instantiate all the DAOs that we will use.
         """
-        #self.accountantWorker = AccountantWorker(couchURL = self.config.JobStateMachine.couchurl,
-        #                                         couchDBName = self.config.JobStateMachine.couchDBName)
-        self.accountantWorker = AccountantWorker(config = self.config)
+        self.accountantWorker = AccountantWorker(config=self.config)
 
         myThread = threading.currentThread()
-        daoFactory = DAOFactory(package = "WMCore.WMBS", logger = myThread.logger,
-                                dbinterface = myThread.dbi)
-        self.getJobsAction = daoFactory(classname = "Jobs.GetFWJRByState")
+        daoFactory = DAOFactory(package="WMCore.WMBS", logger=myThread.logger,
+                                dbinterface=myThread.dbi)
+        self.getJobsAction = daoFactory(classname="Jobs.GetFWJRByState")
         return
 
-    def algorithm(self, parameters = None):
+    def algorithm(self, parameters=None):
         """
         _algorithm_
 
         Poll WMBS for jobs in the 'Complete' state and then pass them to the
         accountant worker.
         """
-        completeJobs = self.getJobsAction.execute(state = "complete")
-        logging.info("Found %d completed jobs" % len(completeJobs))
+        completeJobs = self.getJobsAction.execute(state="complete")
+        logging.info("Found %d completed jobs", len(completeJobs))
 
         if len(completeJobs) == 0:
             logging.debug("No work to do; exiting")
@@ -79,7 +74,7 @@ class JobAccountantPoller(BaseWorkerThread):
             except CouchConnectionError as ex:
                 msg = "Caught CouchConnectionError exception. Waiting until the next polling cycle.\n"
                 msg += str(ex)
-                logging.exception(msg)
+                logging.error(msg)
                 myThread = threading.currentThread()
                 if getattr(myThread, 'transaction', None) is not None:
                     myThread.transaction.rollback()
@@ -89,8 +84,7 @@ class JobAccountantPoller(BaseWorkerThread):
                     myThread.transaction.rollback()
                 msg = "Hit general exception in JobAccountantPoller while using worker.\n"
                 msg += str(ex)
-                logging.error(msg)
-                self.sendAlert(6, msg = msg)
+                logging.exception(msg)
                 raise JobAccountantPollerException(msg)
 
         return

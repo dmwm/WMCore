d57654f1f83e4b727369d60f3bf1b2145fe03413
ticoann <sryu@fnal.gov>
2017-03-22 13:40:42 -0500

    change drain status warning

--- src/couchapps/WMStats/_attachments/js/DataStruct/WMStats.Agents.js
+++ src/couchapps/WMStats/_attachments/js/DataStruct/WMStats.Agents.js
@@ -20,43 +20,57 @@ WMStats.Agents = function (couchData) {
         var currentTime = Math.round(new Date().getTime() / 1000);
         var dataList = this.getData();
         var agentPollingCycle = 600;
-        agentData.agentNumber = {error: 0, warning:0, stable:0};
+        agentData.agentNumber = {error: 0, warning:0, stable:0, drain: 0};
         
         function getStatus(agentInfo) {
             var lastUpdatedDuration = currentTime - agentInfo.timestamp;
             var dataUpdateDuration = -1; 
             if (agentInfo.data_last_update) {
-            	var dataUpdateDuration = currentTime - agentInfo.data_last_update; 
+                var dataUpdateDuration = currentTime - agentInfo.data_last_update; 
             }
-            var report = {};
+            var report = {"status": "stable", "message": ""};
+            
+            // drain case            
+            if (agentInfo.drain_mode) {
+                report.status = "drain";
+                report.message += "Draining Agent; ";
+            }
+            
+            // warning case
+            
+            if (agentInfo.couch_process_warning) {
+                agentData.agentNumber.error += 1;
+                report.status = "error";
+                report.message += "couchdb process maxed out: " + agentInfo.couch_process_warning;
+            }
+            
+            if (agentInfo.disk_warning && (agentInfo.disk_warning.length > 0)) {
+                agentData.agentNumber.warning += 1;
+                report.status = "warning";
+                report.message += "disk is almost full; ";
+            }
+
             if (lastUpdatedDuration > agentPollingCycle * 2) {
                 agentData.agentNumber.error += 1;
-                report = {status: "error", 
-                          message: "Data is not updated: AnalyticsDataCollector Down"};
-            } else if (agentInfo.down_components.length > 0) {
-				agentData.agentNumber.error += 1;
-                report = {status: "error",
-                          message:"Components or Thread down"};
-            } else if (agentInfo.data_error && (agentInfo.data_error !== "ok")) {
-            	agentData.agentNumber.error += 1;
-            	report = {status: "error",
-                          message: "Data collect error"};
-            } else if (agentInfo.couch_process_warning) {
-            	agentData.agentNumber.error += 1;
-            	report = {status: "error",
-                          message: "couchdb process maxed out: " + agentInfo.couch_process_warning};
-            } else if (agentInfo.drain_mode) {
-                agentData.agentNumber.warning += 1;
-                report = {status: "warning",
-                          message: "Draining Agent"};
-            } else if (agentInfo.disk_warning && (agentInfo.disk_warning.length > 0)) {
-            	agentData.agentNumber.warning += 1;
-            	report = {status: "warning",
-                          message: "disk is almost full" };
-            } else {
+                report.status = "error"; 
+                report.message += "Data is not updated: AnalyticsDataCollector Down; ";
+            }
+            
+            if (agentInfo.down_components.length > 0) {
+                agentData.agentNumber.error += 1;
+                report.status = "error";
+                report.message += "Components or Thread down; ";
+            }
+            
+            if (agentInfo.data_error && (agentInfo.data_error !== "ok")) {
+                agentData.agentNumber.error += 1;
+                report.status = "error";
+                report.message += "Data collect error; ";
+            }
+            // If there is no drain/warning or errors set the message OK
+            if (report.status === "stable") {
                 agentData.agentNumber.stable += 1;
-                report = {status: "ok", 
-                          message: "OK"};
+                report.message = "OK";
             };
             
             report["agent_update"] =  WMStats.Utils.foramtDuration(lastUpdatedDuration);

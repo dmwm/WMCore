d3df6d57e0e1e31ecfeddd8e63c324b1681ba24b
goughes <goughes@purdue.edu>
2017-04-26 15:26:55 -0400

    Update docstring and comments

--- src/python/WMComponent/AgentStatusWatcher/DrainStatusPoller.py
+++ src/python/WMComponent/AgentStatusWatcher/DrainStatusPoller.py
@@ -1,5 +1,7 @@
 """
-Monitoring the agent drain status using CheckDrainStatus
+Thread used to monitor the agent draining process. Should
+eventually report no issues meaning that the agent is ready
+to be shutdown and a new version be put in place.
 """
 __all__ = []
 
@@ -11,6 +13,7 @@ from WMComponent.AnalyticsDataCollector.DataCollectAPI import isDrainMode
 
 class DrainStatusPoller(BaseWorkerThread):
     """
+    Collects information related to the agent drain status
     """
     # class variable that contains drain statistics
     drainStats = {}
@@ -31,7 +34,7 @@ class DrainStatusPoller(BaseWorkerThread):
 
             try:
                 DrainStatusPoller.drainStats = self.collectDrainInfo()
-                logging.info("finished collecting agent drain status")
+                logging.info("Finished collecting agent drain status.")
 
             except Exception as ex:
                 msg = "Error occurred, will retry later:\n"
@@ -43,7 +46,7 @@ class DrainStatusPoller(BaseWorkerThread):
 
     def collectDrainInfo(self):
         """
-        call CheckDrainStatus here
+        Call methods to check the drain status
         """
         # return empty dict until real stats are queried
         results = {}

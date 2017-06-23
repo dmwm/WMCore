16118aab3254029e039b895cb774aeb24b61eb98
ticoann <sryu@fnal.gov>
2017-06-23 10:16:10 -0500

    fix python3 compatibility (sorted)

--- test/python/WMCore_t/DataStructs_t/Run_t.py
+++ test/python/WMCore_t/DataStructs_t/Run_t.py
@@ -106,8 +106,7 @@ class RunTest(unittest.TestCase):
         run2 = Run(2, 4, 5, 6)
         run3 = Run(3, 7, 8, 9)
 
-        runlist = [run2, run3, run1]
-        runlist.sort()
+        runlist = sorted([run2, run3, run1])
         self.assertEqual(runlist[0], run1)
         self.assertEqual(runlist[1], run2)
         self.assertEqual(runlist[2], run3)
@@ -116,15 +115,12 @@ class RunTest(unittest.TestCase):
         run5 = Run(4, 4, 5, 6)
         run6 = Run(4, 7, 8, 9)
 
-        runlist = [run5, run6, run4]
-        runlist.sort()
+        runlist = sorted([run5, run6, run4])
         self.assertEqual(runlist[0], run4)
         self.assertEqual(runlist[1], run5)
         self.assertEqual(runlist[2], run6)
 
-        runlist = [run5, run1, run3, run4, run6, run2]
-
-        runlist.sort()
+        runlist = sorted([run5, run1, run3, run4, run6, run2])
         self.assertEqual(runlist[0], run1)
         self.assertEqual(runlist[1], run2)
         self.assertEqual(runlist[2], run3)
@@ -138,8 +134,7 @@ class RunTest(unittest.TestCase):
         run10 = Run(9, 3, 4, 5)
         run11 = Run(9, 8, 9, 10)
 
-        runlist = [run10, run8, run11, run7, run9]
-        runlist.sort()
+        runlist = sorted([run10, run8, run11, run7, run9])
         self.assertEqual(runlist[0], run7)
         self.assertEqual(runlist[1], run8)
         self.assertEqual(runlist[2], run9)

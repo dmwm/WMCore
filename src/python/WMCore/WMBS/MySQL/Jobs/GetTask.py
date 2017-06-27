0772be584aa8d0bef473bab6e1ae69dfd809cd8a
Eric Vaandering <ericvaandering@gmail.com>
2017-06-27 16:34:59 -0500

    Preemptive pylint of GetTask.py

--- src/python/WMCore/WMBS/MySQL/Jobs/GetTask.py
+++ src/python/WMCore/WMBS/MySQL/Jobs/GetTask.py
@@ -7,21 +7,18 @@ MySQL implementation of Jobs.Task
 
 __all__ = []
 
-
-
 import logging
 
 from WMCore.Database.DBFormatter import DBFormatter
 
-class GetTask(DBFormatter):
 
+class GetTask(DBFormatter):
     sql = """SELECT wmwf.task AS task, wmj.id AS jobid FROM wmbs_workflow wmwf
                INNER JOIN wmbs_subscription wms ON wmwf.id = wms.workflow
                INNER JOIN wmbs_jobgroup wmjg ON wms.id = wmjg.subscription
                INNER JOIN wmbs_job wmj ON wmjg.id = wmj.jobgroup
                WHERE wmj.id = :jobid
                """
-
 
     def format(self, results):
         """
@@ -37,12 +34,10 @@ class GetTask(DBFormatter):
 
         return final
 
-
-    def execute(self, jobID, conn = None, transaction = False):
+    def execute(self, jobID, conn=None, transaction=False):
         """
         Should handle bulk and regular attempts to find the task
         """
-
 
         if type(jobID) == list:
             if len(jobID) == 0:
@@ -56,6 +51,6 @@ class GetTask(DBFormatter):
             logging.error('Incompatible jobid in GetTask')
             return
 
-        results = self.dbi.processData(self.sql, binds, conn = conn, transaction = transaction)
+        results = self.dbi.processData(self.sql, binds, conn=conn, transaction=transaction)
 
         return self.format(results)

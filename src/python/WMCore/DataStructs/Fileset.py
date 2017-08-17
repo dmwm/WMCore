33e428e5756dc479da95d387506190fab566e115
Roman <roman.tulinov@cern.ch>
2017-08-17 05:45:32 +0200

    Return negative if x<y, zero if x==y, positive if x>y
    itemgetter used instead of cmp
    
    the commented line with old cmp function is deleted

--- src/python/WMCore/DataStructs/Fileset.py
+++ src/python/WMCore/DataStructs/Fileset.py
@@ -6,6 +6,7 @@ Data object that contains a set of files
 
 """
 from __future__ import print_function
+from operator import itemgetter
 __all__ = []
 
 
@@ -70,7 +71,7 @@ class Fileset(WMObject):
             files = list(self.getFiles(type='set'))
 
             try:
-                files.sort(lambda x, y: cmp(x['lfn'], y['lfn']))
+        	files.sort(key=itemgetter('lfn'))
             except Exception as e:
                 print('Problem with listFiles for fileset:', self.name)
                 print(files.pop())

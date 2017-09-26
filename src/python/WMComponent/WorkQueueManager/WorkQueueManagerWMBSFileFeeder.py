4ad22d2222ebe5022683b6e3633eead0431ef8a0
ticoann <sryu@fnal.gov>
2017-09-26 11:22:28 -0500

    add temp exclude

--- src/python/WMComponent/WorkQueueManager/WorkQueueManagerWMBSFileFeeder.py
+++ src/python/WMComponent/WorkQueueManager/WorkQueueManagerWMBSFileFeeder.py
@@ -65,6 +65,36 @@ class WorkQueueManagerWMBSFileFeeder(BaseWorkerThread):
 
         abortedAndForceCompleteRequests = self.abortedAndForceCompleteWorkflowCache.getData()
 
+        tempACDCExceptions = ["mcremone_ACDC0_task_EGM-RunIISummer17GS-00002__v1_T_170924_053538_530",
+                              "mcremone_ACDC0_task_EGM-RunIISummer17GS-00002__v1_T_170924_053544_9947",
+                              "mcremone_ACDC0_task_EGM-RunIISummer17GS-00002__v1_T_170924_053551_9974",
+                              "mcremone_ACDC0_task_EGM-RunIISummer17GS-00002__v1_T_170924_053600_3305",
+                              "mcremone_ACDC0_task_EGM-RunIISummer17GS-00002__v1_T_170924_053608_5854",
+                              "mcremone_ACDC0_task_EGM-RunIISummer17GS-00002__v1_T_170924_053617_8169",
+                              "mcremone_ACDC0_task_EGM-RunIISummer17GS-00002__v1_T_170924_053625_8322",
+                              "mcremone_ACDC0_task_EGM-RunIISummer17GS-00002__v1_T_170924_053632_9709",
+                              "mcremone_ACDC0_task_EGM-RunIISummer17GS-00002__v1_T_170924_053641_9747",
+                              "mcremone_ACDC0_task_EGM-RunIISummer17GS-00002__v1_T_170924_053649_646",
+                              "mcremone_ACDC0_Run2016G-v1-SingleMuon-07Aug17_8029_170924_054940_8115",
+                            # prebello_Run2016G-v1-SingleMuon-07Aug17_8029_170811_175009_9489
+                            "mcremone_ACDC0_Run2016G-v1-SingleMuon-07Aug17_8029_170924_054953_3843",
+                            "mcremone_ACDC0_Run2016G-v1-SingleMuon-07Aug17_8029_170924_055006_2236",
+                            "mcremone_ACDC0_Run2016G-v1-SingleMuon-07Aug17_8029_170924_055016_487",
+                            "mcremone_ACDC0_Run2016G-v1-SingleMuon-07Aug17_8029_170924_055027_1392",
+                            "mcremone_ACDC0_Run2016G-v1-SingleMuon-07Aug17_8029_170924_055034_1805",
+                            "mcremone_ACDC0_Run2016G-v1-SingleMuon-07Aug17_8029_170924_055041_5982",
+                            "mcremone_ACDC0_Run2016G-v1-SingleMuon-07Aug17_8029_170924_055048_4811",
+                            "mcremone_ACDC0_Run2016G-v1-SingleMuon-07Aug17_8029_170924_055055_4038",
+                            "mcremone_ACDC0_Run2016G-v1-SingleMuon-07Aug17_8029_170924_055102_5520",
+                            "mcremone_ACDC0_Run2016G-v1-SingleMuon-07Aug17_8029_170924_055110_2575",
+                            "mcremone_ACDC0_Run2016G-v1-SingleMuon-07Aug17_8029_170924_055117_4664",
+                            # fabozzi_Run2016F-v1-MuonEG-07Aug17_8029_170831_192544_4761
+                            "mcremone_ACDC0_Run2016F-v1-MuonEG-07Aug17_8029_170924_054427_8461",
+                            # fabozzi_Run2016G-v1-HTMHT-07Aug17_8029_170831_185809_149
+                            "mcremone_ACDC0_Run2016G-v1-HTMHT-07Aug17_8029_170924_054805_2608",
+                            ]
+        abortedAndForceCompleteRequests.extend(tempACDCExceptions)
+        self.queue.logger.info("Excluding following Workflows %s", abortedAndForceCompleteRequests)
         previousWorkList = self.queue.getWork(resources, jobCounts, excludeWorkflows=abortedAndForceCompleteRequests)
         self.queue.logger.info("%s of units of work acquired for file creation"
                                % len(previousWorkList))

cdc724e0840e643159be44dbf35beb26947e5034
ticoann <sryu@fnal.gov>
2017-11-06 11:55:27 -0600

    escape html in error detail

--- src/couchapps/WMStats/_attachments/js/WMStats.Utils.js
+++ src/couchapps/WMStats/_attachments/js/WMStats.Utils.js
@@ -228,27 +228,27 @@ WMStats.Utils.delay =  (function(){
 })();
 
 WMStats.Utils.getInputDatasets = function(reqDoc) {
-     if (reqDoc.inputdataset && reqDoc.inputdataset !== "None"){
-     	return [reqDoc.inputdataset];
-     }
-     if (reqDoc.TaskChain) {
+    if (reqDoc.inputdataset && reqDoc.inputdataset !== "None"){
+        return [reqDoc.inputdataset];
+    }
+    if (reqDoc.TaskChain) {
         var inputDatasets = [];
-     	for (var i = 0; i < reqDoc.TaskChain; i++) {
-  			if (reqDoc["Task" + (i+1)].InputDataset && reqDoc["Task" + (i+1)].InputDataset !== "None") {
-  				inputDatasets.push(reqDoc["Task" + (i+1)].InputDataset);
-  			}
-  		}
-  		return inputDatasets;
-  	 }
-  	 if (reqDoc.StepChain) {
+        for (var i = 0; i < reqDoc.TaskChain; i++) {
+            if (reqDoc["Task" + (i+1)].InputDataset && reqDoc["Task" + (i+1)].InputDataset !== "None") {
+                inputDatasets.push(reqDoc["Task" + (i+1)].InputDataset);
+            }
+        }
+        return inputDatasets;
+    }
+    if (reqDoc.StepChain) {
         var inputDatasets = [];
-     	for (var i = 0; i < reqDoc.StepChain; i++) {
-  			if (reqDoc["Step" + (i+1)].InputDataset && reqDoc["Step" + (i+1)].InputDataset !== "None") {
-  				inputDatasets.push(reqDoc["Step" + (i+1)].InputDataset);
-  			}
-  		}
-  		return inputDatasets;
-  	}
+        for (var i = 0; i < reqDoc.StepChain; i++) {
+            if (reqDoc["Step" + (i+1)].InputDataset && reqDoc["Step" + (i+1)].InputDataset !== "None") {
+                inputDatasets.push(reqDoc["Step" + (i+1)].InputDataset);
+            }
+        }
+    return inputDatasets;
+    }
 };
 
 WMStats.Utils.formatStateTransition = function(stateTransition) {
@@ -259,4 +259,21 @@ WMStats.Utils.formatStateTransition = function(stateTransition) {
 	
   	return WMStats.Utils.expandFormat(stateTransition, "detail", transFunc);
   	
+};
+
+WMStats.Utils.entityMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;',
+    '`': '&#x60;',
+    '=': '&#x3D;'
+};
+
+WMStats.Utils.escapeHtml = function(string) {
+    return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+        return WMStats.Utils.entityMap[s];
+    });
 };

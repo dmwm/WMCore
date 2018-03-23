78c4235eee7f432565cc1f328fc5f84902bc37d4
Alan Malta Rodrigues <alan.malta@cern.ch>
2018-03-23 16:49:22 +0100

    ResourceControl API to add standalone PNNs into the database
    Updated oracle insert pnn on wmbs_pnns; created index for it
    
    index already created in wmbsBase
    
    remove index creation from wmbsBase as well
    
    Remove SiteDB SE-related code

---
+++ src/python/WMCore/WMBS/Oracle/Locations/AddPNNs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Oracle implementation of AddPNNs
+"""
+
+from WMCore.WMBS.MySQL.Locations.AddPNNs import AddPNNs as AddPNNsMySQL
+
+
+class AddPNNs(AddPNNsMySQL):
+
+    sql = """INSERT /*+ IGNORE_ROW_ON_DUPKEY_INDEX (wmbs_pnns (pnn)) */
+               INTO wmbs_pnns (id, pnn) VALUES (wmbs_pnns_SEQ.nextval, :pnn)"""

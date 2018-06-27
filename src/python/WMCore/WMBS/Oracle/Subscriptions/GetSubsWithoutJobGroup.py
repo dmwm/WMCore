cb142aeec710a8db4ab95285d39bacd740fa311d
amaltaro <alan.malta@cern.ch>
2018-06-27 13:11:31 +0200

    New DAO to check subscriptions with no jobgroup associated; new condition for pulling work from GQ
    fix format returned
    
    fix typo
    
    fetch task name and parse it to figure out top level
    
    pylint

---
+++ src/python/WMCore/WMBS/Oracle/Subscriptions/GetSubsWithoutJobGroup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from __future__ import division
+
+from WMCore.WMBS.MySQL.Subscriptions.GetSubsWithoutJobGroup import GetSubsWithoutJobGroup as GetSubsWithoutJobGroupMySQL
+
+
+class GetSubsWithoutJobGroup(GetSubsWithoutJobGroupMySQL):
+    """ Same implementation as in MySQL"""
+    pass

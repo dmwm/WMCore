b2a758c61e5aa1129f511deeceae832659991e26
Alan Malta Rodrigues <alan.malta@cern.ch>
2019-02-14 21:32:10 +0100

    Remove MonteCarlo, MonteCarloFromGEN and ReDigi specs
    Remove them in other src/ places as well
    
    Fixed WMQuality code and getTestArguments
    
    set step arguments for StepChain tests
    
    Fixed StepName in testArgs; fixed Pileup for MC in StepChain

--- src/python/WMCore/ReqMgr/DataStructs/RequestType.py
+++ src/python/WMCore/ReqMgr/DataStructs/RequestType.py
@@ -4,14 +4,10 @@ List of valid request types.
 """
 
 REQUEST_TYPES = [
-    "MonteCarlo",
     "ReReco",
     "StoreResults",
     "Resubmission",
-    "ReDigi",
-    "MonteCarloFromGEN",
     "TaskChain",
-    "PrivateMC",
     "DQMHarvest",
     "StepChain"
 ]

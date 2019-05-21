fd3facb4d347e7289160658727a92ff3bf6da617
Valentin Kuznetsov <vkuznet@gmail.com>
2019-05-21 10:54:43 -0400

    Code re-factoring and add timerange for user activities under throttling

--- src/python/Utils/Throttled.py
+++ src/python/Utils/Throttled.py
@@ -7,7 +7,7 @@ Should be used in Web server APIs which wants to throttle clients.
 from __future__ import division
 
 # standard modules
-import logging
+import time
 import threading
 
 # cherrypy modules
@@ -20,32 +20,24 @@ class _ThrottleCounter(object):
     enter/exit methods to work with `with` context.
     """
 
-    def __init__(self, throttle, user, debug=False):
+    def __init__(self, throttle, user, trange):
         self.throttle = throttle
         self.user = user
-        self.debug = debug
+        self.trange = trange
 
     def __enter__(self):
         "Define enter method for `with` context"
-        ctr = self.throttle._incUser(self.user)
-        if self.debug:
-            msg = "Entering throttled function with counter %d for user %s" \
-                    % (ctr, self.user)
-            self.throttle.logger.debug(msg)
-        if ctr >= self.throttle.getLimit():
+        ctr = self.throttle._incUser(self.user, self.trange)
+        if ctr > self.throttle.getLimit():
             self.throttle._decUser(self.user)
             msg = "The current number of active operations for this resource"
-            msg += " exceeds the limit of %d for user %s" \
-                   % (self.throttle.getLimit(), self.user)
-            raise cherrypy.HTTPError("500, Internal Server Error", msg)
+            msg += " exceeds the limit of %d for user %s in last %s sec" \
+                   % (self.throttle.getLimit(), self.user, self.trange)
+            raise cherrypy.HTTPError("500 Internal Server Error", msg)
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         "Define exit method for `with` context"
-        ctr = self.throttle._decUser(self.user)
-        if self.debug:
-            msg = "Exiting throttled function with counter %d for user %s" \
-                  % (ctr, self.user)
-            self.throttle.logger.debug(msg)
+        pass
 
 
 class UserThrottle(object):
@@ -55,51 +47,64 @@ class UserThrottle(object):
 
     def __init__(self, limit=3):
         self.lock = threading.Lock()
-        self.tls = threading.local()
         self.users = {}
+        self.users_time = {}
         self.limit = limit
-        self.logger = logging.getLogger("WMCore.UserThrottle")
 
     def getLimit(self):
         "Return throttle limit"
         return self.limit
 
-    def throttleContext(self, user, debug=False):
-        "defint throttle context"
-        self.users.setdefault(user, 0)
-        return _ThrottleCounter(self, user, debug)
+    def throttleContext(self, user, trange=60):
+        """
+        Define throttle context via _ThrottleCounter class
+        which will count number of requests given user made in trange
+        :param user: user name
+        :param trange: time range while keep user access history
+        """
+        return _ThrottleCounter(self, user, trange)
 
-    def make_throttled(self, debug=False):
+    def make_throttled(self, trange=60):
         "decorator for throttled context"
         def throttled_decorator(fn):
             def throttled_wrapped_function(*args, **kw):
                 username = cherrypy.request.user.get('login', 'Unknown') \
                         if hasattr(cherrypy.request, 'user') else 'Unknown'
-                with self.throttleContext(username, debug):
+                with self.throttleContext(username, trange):
                     return fn(*args, **kw)
             return throttled_wrapped_function
         return throttled_decorator
 
-    def _incUser(self, user):
-        "increment user count"
-        retval = 0
-        with self.lock:
-            retval = self.users[user]
-            if getattr(self.tls, 'count', None) is None:
-                self.tls.count = 0
-            self.tls.count += 1
-            if self.tls.count == 1:
-                self.users[user] = retval + 1
-        return retval
+    def reset(self, user):
+        "Reset user activities, i.e. access counter and last time of access"
+        self.users[user] = 0
+        self.users_time[user] = time.time()
 
-    def _decUser(self, user):
-        "decrecrement user count"
-        retval = 0
+    def _incUser(self, user, trange=60):
+        "increment user count"
         with self.lock:
-            retval = self.users[user]
-            self.tls.count -= 1
-            if self.tls.count == 0:
-                self.users[user] = retval - 1
-        return retval
+            self.users.setdefault(user, 0)
+            self.users_time.setdefault(user, time.time())
+            last_time = self.users_time[user]
+            # increase counter within our trange
+            if abs(time.time()-last_time) < trange:
+                self.users[user] += 1
+            else:
+                self.reset(user)
+        return self.users[user]
+
+    def _decUser(self, user, trange=60):
+        "decrecrement user count"
+        with self.lock:
+            # decrease counter outside of our trange
+            last_time = self.users_time[user]
+            if abs(time.time()-last_time) < trange:
+                # gradually decrease user counter based on his/her elapsed time
+                step = int(trange - abs(time.time()-last_time))
+                self.users[user] -= step
+                if self.users[user] < 0:
+                    self.reset(user)
+            else:
+                self.reset(user)
 
 global_user_throttle = UserThrottle()
--- test/python/Utils_t/Throttled_t.py
+++ test/python/Utils_t/Throttled_t.py
@@ -7,30 +7,34 @@ Unittests for Utilities functions
 from __future__ import division, print_function
 import time
 import unittest
-from Utils.Throttled import global_user_throttle
+from Utils.Throttled import global_user_throttle, UserThrottle
+from cherrypy import HTTPError
 
-
-@global_user_throttle.make_throttled()
-def throttled():
-    "Test function for throttled"
-    return time.time()
 
 
 class UtilitiesTests(unittest.TestCase):
     """
     unittest for Utilities functions
     """
+    def setUp(self):
+        self.limit = 100
+        self.thr = UserThrottle(limit=self.limit)
+        self.thr3 = UserThrottle(limit=3)
+        self.thr10 = UserThrottle(limit=10)
 
     def testthrottledTimes(self):
         """
         Test that throttled function behave slower than normal
         """
-        rdim = 1000
+        @self.thr.make_throttled()
+        def throttled():
+            "Test function for throttled"
+            return time.time()
         thr_times = []
         xxx_times = []
-        for _ in range(rdim):
+        for _ in range(self.limit-1):
             thr_times.append(throttled())
-        for _ in range(rdim):
+        for _ in range(self.limit-1):
             xxx_times.append(time.time())
         thr_range = max(thr_times) - min(thr_times)
         nor_range = max(xxx_times) - min(xxx_times)
@@ -38,6 +42,42 @@ class UtilitiesTests(unittest.TestCase):
         print("normal    times: %s" % nor_range)
         self.assertEqual(thr_range > nor_range, True)
         self.assertEqual((thr_range - nor_range) > nor_range, True)
+
+    def testthrottledTimeRange(self):
+        """
+        Test that throttled within certain time range
+        """
+
+        @self.thr10.make_throttled()
+        def throttled():
+            "Test function for throttled"
+            return time.time()
+
+        @self.thr3.make_throttled(trange=2)
+        def throttled2():
+            "Test function for throttled"
+            return time.time()
+
+        def test(dim):
+            for _ in range(dim):
+                throttled2()
+        self.assertRaises(HTTPError, test, 5)
+        # once assertion happens user will be allowed to make N-additional
+        # calls based on elapsed time of throttler
+        time.sleep(1)
+        throttled2()
+
+        # here we should be fine because throttled function decorated with
+        # limit=10
+        for _ in range(5):
+            throttled()
+
+        # sleep 2 sec (our trange) and use again our throttled2 function
+        # this time it should be fine
+        time.sleep(2)
+        throttled2()
+        # but we we'll call it multiple times we'll get back exception
+        self.assertRaises(HTTPError, test, 5)
 
 if __name__ == '__main__':
     unittest.main()

f302b1bd69a5bf4ac60b44453d84af5b125ef472
Alan Malta Rodrigues <alan.malta@cern.ch>
2019-11-12 13:05:59 +0100

    Bump WMAgent deployment to 1.2.8

--- deploy/deploy-wmagent.sh
+++ deploy/deploy-wmagent.sh
@@ -25,8 +25,8 @@
 ### Usage:               -c <central_services> Url to central services hosting central couchdb (e.g. alancc7-cloud1.cern.ch)
 ### Usage:
 ### Usage: deploy-wmagent.sh -w <wma_version> -d <deployment_tag> -t <team_name> [-s <scram_arch>] [-r <repository>] [-n <agent_number>] [-c <central_services_url>]
-### Usage: Example: sh deploy-wmagent.sh -w 1.2.6.patch1 -d HG1909e -t production -n 30
-### Usage: Example: sh deploy-wmagent.sh -w 1.2.4.patch3 -d HG1908a -t testbed-vocms001 -p "9315 9364 9367" -r comp=comp.amaltaro -c cmsweb-testbed.cern.ch
+### Usage: Example: sh deploy-wmagent.sh -w 1.2.8 -d HG1911c -t production -n 30
+### Usage: Example: sh deploy-wmagent.sh -w 1.2.8 -d HG1911c -t testbed-vocms001 -p "9403" -r comp=comp.amaltaro -c cmsweb-testbed.cern.ch
 ### Usage:
 
 IAM=`whoami`

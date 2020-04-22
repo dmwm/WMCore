2d8f3e7277dbf569b368f4eabf581b54683f2f2f
tivanov <tivanov@tot-t590>
2020-04-22 18:41:51 -0400

    Add and earlier status ('closed-out') to fix the parentage dependencies

--- src/python/WMCore/ReqMgr/CherryPyThreads/StepChainParentageFixTask.py
+++ src/python/WMCore/ReqMgr/CherryPyThreads/StepChainParentageFixTask.py
@@ -35,7 +35,7 @@ class StepChainParentageFixTask(CherryPyPeriodicTask):
         super(StepChainParentageFixTask, self).__init__(config)
         self.reqmgrDB = RequestDBWriter(config.reqmgrdb_url)
         self.dbsSvc = DBS3Reader(config.dbs_url, logger=self.logger)
-        self.statusToCheck = ["announced", "normal-archived"]
+        self.statusToCheck = ["closed-out", "announced", "normal-archived"]
 
     def setConcurrentTasks(self, config):
         """

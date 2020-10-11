2cbeafe0ae25c5586425033ff3dbe4958d3fce06
Alan Malta Rodrigues <alan.malta@cern.ch>
2020-10-11 19:43:25 +0200

    code reformat

--- src/python/WMComponent/WorkQueueManager/WorkQueueManager.py
+++ src/python/WMComponent/WorkQueueManager/WorkQueueManager.py
@@ -5,17 +5,17 @@ WorkQueuemanager component
 Runs periodic tasks for WorkQueue
 """
 from __future__ import print_function
+
 import threading
 
-from WMCore.Agent.Harness import Harness
-
-from WMComponent.WorkQueueManager.WorkQueueManagerWorkPoller import WorkQueueManagerWorkPoller
-from WMComponent.WorkQueueManager.WorkQueueManagerReqMgrPoller import WorkQueueManagerReqMgrPoller
-from WMComponent.WorkQueueManager.WorkQueueManagerLocationPoller import WorkQueueManagerLocationPoller
 from WMComponent.WorkQueueManager.WorkQueueManagerCleaner import WorkQueueManagerCleaner
+from WMComponent.WorkQueueManager.WorkQueueManagerLocationPoller import WorkQueueManagerLocationPoller
+from WMComponent.WorkQueueManager.WorkQueueManagerReqMgrPoller import WorkQueueManagerReqMgrPoller
 from WMComponent.WorkQueueManager.WorkQueueManagerWMBSFileFeeder import WorkQueueManagerWMBSFileFeeder
-
+from WMComponent.WorkQueueManager.WorkQueueManagerWorkPoller import WorkQueueManagerWorkPoller
+from WMCore.Agent.Harness import Harness
 from WMCore.WorkQueue.WorkQueueUtils import queueFromConfig, queueConfigFromConfigObject
+
 
 class WorkQueueManager(Harness):
     """WorkQueuemanager component
@@ -27,7 +27,6 @@ class WorkQueueManager(Harness):
         # call the base class
         Harness.__init__(self, config)
         self.config = queueConfigFromConfigObject(config)
-
 
     def preInitialization(self):
         print("WorkQueueManager.preInitialization")
@@ -42,40 +41,40 @@ class WorkQueueManager(Harness):
 
             # Get work from ReqMgr, report back & delete finished requests
             myThread.workerThreadManager.addWorker(
-                                WorkQueueManagerReqMgrPoller(
-                                        queueFromConfig(self.config),
-                                        getattr(self.config.WorkQueueManager,
-                                                'reqMgrConfig', {})
-                                        ),
-                                 pollInterval)
+                    WorkQueueManagerReqMgrPoller(
+                            queueFromConfig(self.config),
+                            getattr(self.config.WorkQueueManager,
+                                    'reqMgrConfig', {})
+                    ),
+                    pollInterval)
 
         ### local queue special function
         elif self.config.WorkQueueManager.level == 'LocalQueue':
 
             # pull work from parent queue
             myThread.workerThreadManager.addWorker(
-                                WorkQueueManagerWorkPoller(queueFromConfig(self.config),
-                                                           self.config),
-                                pollInterval)
+                    WorkQueueManagerWorkPoller(queueFromConfig(self.config),
+                                               self.config),
+                    pollInterval)
 
             # inject acquired work into wmbs
             myThread.workerThreadManager.addWorker(
-                                WorkQueueManagerWMBSFileFeeder(queueFromConfig(self.config),
-                                                               self.config),
-                                pollInterval)
+                    WorkQueueManagerWMBSFileFeeder(queueFromConfig(self.config),
+                                                   self.config),
+                    pollInterval)
 
         ### general functions
 
         # Data location updates
         myThread.workerThreadManager.addWorker(
-                                    WorkQueueManagerLocationPoller(queueFromConfig(self.config),
-                                                                   self.config),
-                                    dataLocationInterval)
+                WorkQueueManagerLocationPoller(queueFromConfig(self.config),
+                                               self.config),
+                dataLocationInterval)
 
         # Clean finished work & apply end policies
         myThread.workerThreadManager.addWorker(
-                                WorkQueueManagerCleaner(queueFromConfig(self.config),
-                                                        self.config),
-                                pollInterval)
+                WorkQueueManagerCleaner(queueFromConfig(self.config),
+                                        self.config),
+                pollInterval)
 
         return

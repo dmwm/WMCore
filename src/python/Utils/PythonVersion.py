72a8f08053bed57db39c8a2569491e0fecaea714
Dario Mapelli <mapelli.dario@gmail.com>
2021-05-27 11:21:10 +0200

    [py2py3] new module: Utils/PythonVersion.py

---
+++ src/python/Utils/PythonVersion.py
@@ -0,0 +1,10 @@
+"""
+Easily get the version of the python interpreter at runtime
+"""
+
+from __future__ import division # Jenkins CI
+
+import sys
+
+PY3 = sys.version_info[0] == 3
+PY2 = sys.version_info[0] == 2

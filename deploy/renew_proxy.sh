7d0433c7dfff27b5a495de3ba77f61ca44675ea4
Alan Malta Rodrigues <alan.malta@cern.ch>
2022-02-22 18:45:24 -0500

    Explicitly create proxies with 2048 bits

--- deploy/renew_proxy.sh
+++ deploy/renew_proxy.sh
@@ -7,5 +7,5 @@ if [[ "$credname" == amaltaroCERN ]]; then
     source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh
 fi
 myproxy-get-delegation -v -l amaltaro -t 168 -s 'myproxy.cern.ch' -k $credname -n \
-  -o /data/certs/mynewproxy.pem && voms-proxy-init -rfc -voms cms:/cms/Role=production -valid 168:00 \
+  -o /data/certs/mynewproxy.pem && voms-proxy-init -rfc -voms cms:/cms/Role=production -valid 168:00 -bits 2048 \
   -noregen -cert /data/certs/mynewproxy.pem -key /data/certs/mynewproxy.pem -out /data/certs/myproxy.pem

8ca8dd17cea26c574f29fbf6d3d9f2432f4ccec1
Alan Malta Rodrigues <alan.malta@cern.ch>
2022-06-21 21:50:13 -0400

    remove all the CMS authz logic from the CouchDB validate functions

--- src/couchapps/WorkQueue/validate_doc_update.js
+++ src/couchapps/WorkQueue/validate_doc_update.js
@@ -5,21 +5,4 @@ function(newDoc, oldDoc, userCtx) {
       throw({forbidden: 'Do not create deleted docs'});
     }
 
-    var validation = require("lib/validate").init(newDoc, oldDoc, userCtx);
-
-    // Admins can do anything
-    if (validation.isAdmin()) {
-        return true;
-    }
-
-    // Either Developer or DataOps Operator/Manager required
-    if (validation.hasGroupRole("dataops", "developer") ||
-        validation.hasGroupRole("dataops", "production-operator") ||
-        validation.hasGroupRole("facops", "web-service")) {
-        return true;
-    }
-
-    // authentication failed
-    log("Authentication failed: " + toJSON(userCtx));
-    throw {forbidden: "User not validated for action"};
 }

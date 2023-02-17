dc66a1e38b61011ccf02f59678423d0223f63fc6
Alan Malta Rodrigues <alan.malta@cern.ch>
2023-02-17 15:40:07 -0500

    Fix manage path in the restartComponent script
    Valentin.s review

--- deploy/restartComponent.sh
+++ deploy/restartComponent.sh
@@ -3,16 +3,17 @@
 HOST=`hostname`
 DATENOW=`date +%s`
 
+# Get a few environment variables in, like $install and $manage
+source /data/admin/wmagent/env.sh
+
 # Figure whether it's a python2 or python3 agent
-if [ -d "/data/srv/wmagent/current/install/wmagent" ]; then
-  WMA_PATH="/data/srv/wmagent/current/install/wmagent"
-else
-  WMA_PATH="/data/srv/wmagent/current/install/wmagentpy3"
+if [ ! -d "$install" ]; then
+  install="/data/srv/wmagent/current/install/wmagentpy3"
 fi
 
 COMPONENTS="ErrorHandler JobSubmitter AgentStatusWatcher"
 for comp in $COMPONENTS; do
-  COMPLOG=$WMA_PATH/$comp/ComponentLog
+  COMPLOG=$install/$comp/ComponentLog
   LASTCHANGE=`stat -c %Y $COMPLOG`
   INTERVAL=`expr $DATENOW - $LASTCHANGE`
   if (("$INTERVAL" >= 1800)); then
@@ -22,9 +23,8 @@ for comp in $COMPONENTS; do
       exit 1
     fi
 
-    . /data/admin/wmagent/env.sh
     TAIL_LOG=`tail -n100 $COMPLOG`
-    $WMA_PATH/manage execute-agent wmcoreD --restart --components=$comp
+    $manage execute-agent wmcoreD --restart --components=$comp
     echo -e "ComponentLog quiet for $INTERVAL secs\n\nTail of the log is:\n$TAIL_LOG" |
       mail -s "$HOST : $comp restarted" alan.malta@cern.ch,todor.trendafilov.ivanov@cern.ch
   fi

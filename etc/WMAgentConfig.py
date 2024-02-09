f55430647077812b4ea12ecaec9b6735fa143362
Antonio Linares Sancho <132582167+LinaresToine@users.noreply.github.com>
2024-02-09 05:01:36 -0600

    Fix syntax error

--- etc/WMAgentConfig.py
+++ etc/WMAgentConfig.py
@@ -260,15 +260,16 @@ config.RetryManager.section_("SquaredAlgo")
 config.RetryManager.SquaredAlgo.section_("default")
 config.RetryManager.SquaredAlgo.default.coolOffTime = retryAlgoParams
 
+#Include modifiers in RetryManager
 config.RetryManager.modifiers = {"default" : "DefaultModifier", "memory" : "MemoryModifier"}
 config.RetryManager.section_("DefaultModifier")
 config.RetryManager.DefaultModifier.section_("default")
 config.RetryManager.DefaultModifier.default.requiresModify = False
 config.RetryManager.section_("MemoryModifier")
 config.RetryManager.MemoryModifier.section_("memory")
-config.RetryManager.MemoryModifier.default.requiresModify = True
-config.RetryManager.MemoryModifier.default.maxMemory = 20 # in GB
-config.RetryManager.MemoryModifier.default.addedMemory = 2 # in GB
+config.RetryManager.MemoryModifier.memory.requiresModify = True
+config.RetryManager.MemoryModifier.memory.maxMemory = 20 # in GB
+config.RetryManager.MemoryModifier.memory.addedMemory = 2 # in GB
 
 config.component_("JobArchiver")
 config.JobArchiver.namespace = "WMComponent.JobArchiver.JobArchiver"

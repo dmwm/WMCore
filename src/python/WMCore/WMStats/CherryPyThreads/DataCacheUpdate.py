046a26541bab460f162b7858b1c353f9d4933c40
Alan Malta Rodrigues <alan.malta@cern.ch>
2024-02-20 17:39:00 -0500

    Memory trace for DataCacheUpdate CP thread

--- src/python/WMCore/WMStats/CherryPyThreads/DataCacheUpdate.py
+++ src/python/WMCore/WMStats/CherryPyThreads/DataCacheUpdate.py
@@ -1,6 +1,7 @@
 from __future__ import (division, print_function)
 
 import time
+import tracemalloc
 from WMCore.REST.CherryPyPeriodicTask import CherryPyPeriodicTask
 from WMCore.WMStats.DataStructs.DataCache import DataCache
 from WMCore.Services.WMStats.WMStatsReader import WMStatsReader
@@ -12,6 +13,7 @@ class DataCacheUpdate(CherryPyPeriodicTask):
         self.getJobInfo = getattr(config, "getJobInfo", False)
 
         super(DataCacheUpdate, self).__init__(config)
+        tracemalloc.start()
 
     def setConcurrentTasks(self, config):
         """
@@ -27,6 +29,7 @@ class DataCacheUpdate(CherryPyPeriodicTask):
         try:
             tStart = time.time()
             if DataCache.islatestJobDataExpired():
+                snapshot1 = tracemalloc.take_snapshot()
                 wmstatsDB = WMStatsReader(config.wmstats_url, reqdbURL=config.reqmgrdb_url,
                                           reqdbCouchApp="ReqMgr", logger=self.logger)
                 self.logger.info("Getting active data with job info for statuses: %s", WMSTATS_JOB_INFO)
@@ -34,9 +37,21 @@ class DataCacheUpdate(CherryPyPeriodicTask):
                 self.logger.info("Getting active data with NO job info for statuses: %s", WMSTATS_NO_JOB_INFO)
                 tempData = wmstatsDB.getActiveData(WMSTATS_NO_JOB_INFO, jobInfoFlag=False)
                 jobData.update(tempData)
+                snapshot2 = tracemalloc.take_snapshot()
                 self.logger.info("Running setlatestJobData...")
                 DataCache.setlatestJobData(jobData)
                 self.logger.info("DataCache is up-to-date with %d requests data", len(jobData))
+                snapshot3 = tracemalloc.take_snapshot()
+
+                self.logger.info("Memory stats between snapshot2 and snapshot1")
+                top_stats = snapshot2.compare_to(snapshot1, 'lineno')
+                for thisStat in top_stats[:5]:
+                    self.logger.info(thisStat)
+
+                self.logger.info("Memory stats between snapshot3 and snapshot2")
+                top_stats = snapshot3.compare_to(snapshot2, 'lineno')
+                for thisStat in top_stats[:5]:
+                    self.logger.info(thisStat)
         except Exception as ex:
             self.logger.exception("Exception updating DataCache. Error: %s", str(ex))
         self.logger.info("Total time loading data from ReqMgr2 and WMStats: %s", time.time() - tStart)

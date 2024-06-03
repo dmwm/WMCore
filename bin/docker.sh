112fbca88f9cedc730708a6dc58c6135f4cf5d88
Valentin Kuznetsov <vkuznet@gmail.com>
2024-06-03 14:18:35 -0400

    New docker wrapper script to manage docker image actions (build, build-stable, push, remove)

---
+++ bin/docker.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# Author: Valentin Kuznetsov <vkuznet@gmail.com>
+# helper script to handle docker actions
+
+if [ $# -ne 4 ]; then
+     echo "Usage: docker.sh <action> <service> <tag> <registry> "
+     echo "Supported actions: build, build-stable, push, remove, delete"
+     exit 1;
+fi
+
+# helper function to match the pattern X.Y.Z where X, Y, and Z are numbers
+function match_tag {
+  local tag=$1
+  if [[ $tag =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then
+    return 0
+  elif [[ $tag =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# make input variable assignments
+action=$1
+service=$2
+tag=$3
+registry=$4
+rurl=$registry/cmsweb/$service
+
+# check if action is supported
+case "$action" in
+  "build"|"build-stable"|"push"|"remove"|"delete")
+    ;;
+  *)
+    echo "action: '$action' is not supported"
+    exit 1
+    ;;
+esac
+
+# determine suffix of image tag
+suffix=""
+if match_tag "$tag"; then
+    suffix="-stable"
+fi
+
+# check build/push actions and build the image(s) if it will be required
+if [ "$action" == "build" ]; then
+    echo "check if image with service=$service and tag=$tag exist ..."
+    image_exist=`docker images | grep $tag | grep $service`
+    if [ -z "$image_exist" ]; then
+        echo "action: docker build --build-arg TAG=${tag} --tag ${rurl}:${tag}"
+        docker build --build-arg TAG=${tag} --tag ${rurl}:${tag} .
+    fi
+    docker images | grep $tag | grep $service
+    exit 0
+fi
+if [ "$action" == "build-stable" ]; then
+    if [ -z "$suffix" ]; then
+        echo "Building stable image for tag=$tag is not appropriate as tag is not matched X.Y.Z or X.Y.Z.P pattern"
+        exit 1
+    fi
+    echo "check if image with service=$service and tag=${tag}${suffix} exist ..."
+    image_exist=`docker images | grep ${tag}${suffix} | grep $service`
+    if [ -z "$image_exist" ]; then
+        echo "action: docker build --build-arg TAG=${tag} --tag ${rurl}:${tag}${suffix}"
+        docker build --build-arg TAG=${tag} --tag ${rurl}:${tag}${suffix} .
+    fi
+    docker images | grep ${tag}${suffix} | grep $service
+    exit 0
+fi
+if [ "$action" == "push" ]; then
+    image_exist=`docker images | grep $tag | grep $service`
+    if [ -z "$image_exit" ]; then
+        echo "Image ${rurl}:${tag}${suffix} is not found"
+        exit 1
+    fi
+    echo "action: docker push ${rurl}:${tag}"
+    docker push ${rurl}:${tag}
+    exit 0
+fi
+if [ "$action" == "remove" ] || [ "$action" == "delete" ]; then
+    echo "action: docker image rm -f ${rurl}:${tag}${suffix}"
+    image_exist=`docker images | grep ${tag}${suffix} | grep $service`
+    if [ -z "$image_exit" ]; then
+        echo "Image ${rurl}:${tag}${suffix} is not found"
+        exit 1
+    fi
+    docker rmi ${rurl}:${tag}
+    exit 0
+fi

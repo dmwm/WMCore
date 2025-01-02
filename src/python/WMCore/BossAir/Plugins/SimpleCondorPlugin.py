843f0e060df705df3299504b0742d2876e9b5abb
Andrea Piccinelli <andrea8395@gmail.com>
2025-01-02 12:38:41 +0100

    Enabling token auth in SimpleCondorPlugin

--- src/python/WMCore/BossAir/Plugins/SimpleCondorPlugin.py
+++ src/python/WMCore/BossAir/Plugins/SimpleCondorPlugin.py
@@ -520,6 +520,8 @@ class SimpleCondorPlugin(BasePlugin):
                 ad['Requirements'] = self.reqStr
 
             #ad['My.x509userproxy'] = classad.quote(self.x509userproxy)
+            # Allow oauth based token authentication
+            ad['use_oauth_services'] = "cms"
             sites = ','.join(sorted(job.get('possibleSites')))
             ad['My.DESIRED_Sites'] = classad.quote(str(sites))
             sites = ','.join(sorted(job.get('potentialSites')))

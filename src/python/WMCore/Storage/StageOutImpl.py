036c95fd973ea70efb1cdb98b2cf5b2ad6f8bdd4
Andrea Piccinelli <andrea8395@gmail.com>
2025-02-17 17:03:04 +0100

    unit tests update -- unit test

--- src/python/WMCore/Storage/StageOutImpl.py
+++ src/python/WMCore/Storage/StageOutImpl.py
@@ -212,6 +212,7 @@ class StageOutImpl:
         # //
         # // Create the command to be used.
         # //
+        # // This will actually only enforce the definition of the bearer token variables, still x509 auth activated as a backup
         try:
             command = self.createStageOutCommand(sourcePFN, targetPFN, options, checksums, auth_method="TOKEN")
         except TypeError as ex:
@@ -224,12 +225,9 @@ class StageOutImpl:
         stageOutEx = None  # variable to store the possible StageOutError
         for retryCount in range(self.numRetries + 1):
             try:
-                logging.info("Running the stage out with tokens (attempt %d)...", retryCount + 1)
+                logging.info("Running the stage out with the available auth method (attempt %d)...", retryCount + 1)
                 logging.info("Command to run: %s", command)
                 self.executeCommand(command)
-                # to remove once understood gfal behavior
-                debcommand = self.createDebuggingCommand(sourcePFN, targetPFN, options, checksums, auth_method="TOKEN")
-                self.executeCommand(debcommand)
                 logging.info("\nStage-out succeeded with the current environment.")
                 break
 
@@ -237,27 +235,25 @@ class StageOutImpl:
                 msg = "Attempt {} to stage out failed with default setup.\n".format(retryCount)
                 msg += "Error details:\n{}\n".format(str(ex))
                 logging.error(msg)
-
                 logging.info("Retrying with authentication-safe logic...")
 
                 # Authentication-safe fallback logic
                 if os.getenv("X509_USER_PROXY"):
-                    logging.info("Retrying with X509_USER_PROXY after unsetting BEARER_TOKEN...")
+                    logging.info("Retrying with X509_USER_PROXY with BEARER_TOKEN unset...")
                     command = self.createStageOutCommand(sourcePFN, targetPFN, options, checksums, auth_method="X509", force_method=True)
                     try:
                         self.executeCommand(command)
-                        logging.info("Stage-out succeeded with X509 after unsetting BEARER_TOKEN.")
+                        logging.info("Stage-out succeeded with X509 with BEARER_TOKEN unset.")
                         return
                     except StageOutError as fallbackEx:
                         logging.warning("Fallback with X509_USER_PROXY failed:\n%s", str(fallbackEx))
 
                 if os.getenv("BEARER_TOKEN") or os.getenv("BEARER_TOKEN_FILE"):
-                    logging.info("Retrying with BEARER_TOKEN after unsetting X509_USER_PROXY...")
-                    os.system("unset X509_USER_PROXY")
+                    logging.info("Retrying with BEARER_TOKEN with X509_USER_PROXY unset...")
                     command = self.createStageOutCommand(sourcePFN, targetPFN, options, checksums, auth_method="TOKEN", force_method=True)
                     try:
                         self.executeCommand(command)
-                        logging.info("Stage-out succeeded with TOKEN after unsetting X509_USER_PROXY.")
+                        logging.info("Stage-out succeeded with TOKEN with X509_USER_PROXY unset.")
                         return
                     except StageOutError as fallbackEx:
                         logging.warning("Fallback with BEARER_TOKEN failed:\n%s", str(fallbackEx))
@@ -271,6 +267,9 @@ class StageOutImpl:
         # This block will now always be executed after retries are exhausted
         if stageOutEx is not None:
             logging.error("Maximum number of retries exhausted. Further details on the failed command reported below.")
-            command = self.createDebuggingCommand(sourcePFN, targetPFN, options, checksums, auth_method="TOKEN")
+            try:
+                command = self.createDebuggingCommand(sourcePFN, targetPFN, options, checksums, auth_method="TOKEN")
+            except TypeError as ex:
+                command = self.createDebuggingCommand(sourcePFN, targetPFN, options, checksums)
             self.executeCommand(command)
             raise stageOutEx from None

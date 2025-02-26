03db59837b76afeeaba50d04e9afb25ec906e5d5
Valentin Kuznetsov <vkuznet@gmail.com>
2025-02-26 10:15:10 -0500

    Remove hard-coded emails and read them from WMA_SECRETS_FILE

--- deploy/restartComponent.sh
+++ deploy/restartComponent.sh
@@ -8,9 +8,14 @@
 
 HOST=$(hostname)
 DATENOW=$(date +%s)
-DEST_WM=cms-wmcore-team
-DEST_T0=cms-tier0-operations
-DEST_PNR=cms-comp-ops-pnr-alarms
+# look-up alert emails from WMA secret file
+EMAIL_ALERT_WM=`grep EMAIL_ALERT_WM $WMA_SECRETS_FILE`
+EMAIL_ALERT_T0=`grep EMAIL_ALERT_T0 $WMA_SECRETS_FILE`
+EMAIL_ALERT_PNR=`grep EMAIL_ALERT_PNR $WMA_SECRETS_FILE`
+
+[[ -z $EMAIL_ALERT_WM ]] && {echo "ERROR: unable to find EMAIL_ALERT_WM in $WMA_SECRETS_FILE"; exit 1;}
+[[ -z $EMAIL_ALERT_T0 ]] && {echo "ERROR: unable to find EMAIL_ALERT_T0 in $WMA_SECRETS_FILE"; exit 1;}
+[[ -z $EMAIL_ALERT_PNR ]] && {echo "ERROR: unable to find EMAIL_ALERT_PNR in $WMA_SECRETS_FILE"; exit 1;}
 
 [[ -z $WMA_INSTALL_DIR ]] && { echo "ERROR: Trying to run without having the full WMAgent environment set!";  exit 1 ;}
 
@@ -36,7 +41,7 @@ for comp in $comps; do
     echo -e "Restarting component: $comp"
     manage execute-agent wmcoreD --restart --components=$comp
     echo -e "ComponentLog quiet for $INTERVAL secs\n\nTail of the log is:\n$TAIL_LOG" |
-      mail -s "$HOST : $comp restarted" $DEST_WM@cern.ch $DEST_T0@cern.ch $DEST_PNR@cern.ch
+      mail -s "$HOST : $comp restarted" $EMAIL_ALERT_WM $EMAIL_ALERT_T0 $EMAIL_ALERT_PNR
   fi
 done
 

c1f3a6f0b17c87e36c63469a5c6d0be3ece654dd
Andrea Piccinelli <andrea8395@gmail.com>
2025-03-11 12:35:25 +0100

    first refactoring for GFAL2Impl.py

--- src/python/WMCore/Storage/Backends/GFAL2Impl.py
+++ src/python/WMCore/Storage/Backends/GFAL2Impl.py
@@ -31,6 +31,41 @@ class GFAL2Impl(StageOutImpl):
         self.copyOpts = '-t 2400 -T 2400 -p -v --abort-on-failure {checksum} {options} {source} {destination}'
         self.copyCommand = self.setups.format('. $JOBSTARTDIR/startup_environment.sh; date; gfal-copy ' + self.copyOpts)
 
+        #Overriding the default debugging template
+        self.debuggingTemplate = "#!/bin/bash\n"
+        self.debuggingTemplate += """
+        echo
+        echo
+        echo "-----------------------------------------------------------"
+        echo "==========================================================="
+        echo
+        echo "Debugging information on failing gfal-copy command"
+        echo
+        echo "Current date and time: $(date +"%Y-%m-%d %H:%M:%S")"
+        echo "gfal-copy command which failed: {copy_command}"
+        echo "Hostname:   $(hostname -f)"
+        echo "OS:  $(uname -r -s)"
+        echo
+        echo "GFAL environment variables:"
+        env | grep ^GFAL
+        echo
+        echo "PYTHON environment variables:"
+        env | grep ^PYTHON
+        echo
+        echo "LD_* environment variables:"
+        env | grep ^LD_
+        echo
+        echo "gfal-copy location: $(which gfal-copy)"
+        echo "Source PFN: {source}"
+        echo "Target PFN: {destination}"
+        echo
+        echo "VOMS proxy info:"
+        voms-proxy-info -all
+        echo "==========================================================="
+        echo "-----------------------------------------------------------"
+        echo
+        """
+
     def adjustSetup(self, authmethod=None, forcemethod=False):
         """
         Adjust the `self.setups` based on the selected authentication method and regenerate commands.
@@ -110,7 +145,6 @@ class GFAL2Impl(StageOutImpl):
         else:
             return self.removeCommand.format(self.createFinalPFN(pfn))
 
-
     def buildCopyCommandDict(self, sourcePFN, targetPFN, options=None, checksums=None):
         """
         Build the gfal-cp command for stageOut
@@ -144,10 +178,51 @@ class GFAL2Impl(StageOutImpl):
         copyCommandDict['destination'] = self.createFinalPFN(targetPFN)
 
         return copyCommandDict
+    
+    def defineStageOutCommand(self, sourcePFN, targetPFN, options=None, checksums=None, authmethod=None, forcemethod=False, debug=False):
+        """
+        Create gfal-cp command for stageOut, adding the actual gfal-cp call to execute it if debug is False,
+
+        :sourcePFN: str, PFN of the source file
+        :targetPFN: str, destination PFN
+        :options: str, additional options for gfal-cp
+        :checksums: dict, collect checksums according to the algorithms saved as keys
+        :authmethod: str, the authentication method to be preferentially used ("X509", "TOKEN", or None)
+        :forcemethod: bool, whether to force the use of the preferred authentication method, disabling the other
+        :debug: bool, whether to add the actual gfal-cp command to the output
+        """
+        # Adjust the setup
+        self.adjustSetup(authmethod, forcemethod)
+
+        # Construct the gfal-cp command
+        copyCommandDict = self.buildCopyCommandDict(sourcePFN, targetPFN, options, checksums)
+        copyCommand = self.copyCommand.format_map(copyCommandDict)
+        logging.info("============ defineStageOutCommand within GFAL2Impl =============")
+        logging.info("copyCommand: %s", copyCommand)
+
+        if not debug:
+            result = "#!/bin/bash\n" + copyCommand
+            if _CheckExitCodeOption:
+                result += """
+                EXIT_STATUS=$?
+                echo "gfal-copy exit status: $EXIT_STATUS"
+                if [[ $EXIT_STATUS != 0 ]]; then
+                    echo "ERROR: gfal-copy exited with $EXIT_STATUS"
+                    echo "Cleaning up failed file:"
+                    {remove_command}
+                fi
+                exit $EXIT_STATUS
+                """.format(remove_command=self.createRemoveFileCommand(targetPFN))
+        else:
+            result = self.debuggingTemplate.format(copy_command=copyCommand, source=copyCommandDict['source'], destination=copyCommandDict['destination'])
+            
+        logging.info("============ end of createStageOutCommand within GFAL2Impl =============")
+        
+        return result
 
     def createStageOutCommand(self, sourcePFN, targetPFN, options=None, checksums=None, authmethod=None, forcemethod=False):
         """
-        Create gfal-cp command for stageOut
+        Create gfal-cp command for stageOut via defineStageOutCommand in debug=False mode
 
         :sourcePFN: str, PFN of the source file
         :targetPFN: str, destination PFN
@@ -156,44 +231,7 @@ class GFAL2Impl(StageOutImpl):
         :authmethod: str, the authentication method to be preferentially used ("X509", "TOKEN", or None)
         :forcemethod: bool, whether to force the use of the preferred authentication method, disabling the other
         """
-        # Adjust the setup
-        self.adjustSetup(authmethod, forcemethod)
-
-        # Construct the gfal-cp command
-        copyCommandDict = self.buildCopyCommandDict(sourcePFN, targetPFN, options, checksums)
-        copyCommand = self.copyCommand.format_map(copyCommandDict)
-        logging.info("============ createStageOutCommand within GFAL2Impl =============")
-        logging.info("copyCommand: %s", copyCommand)
-
-        #Debugging for GFAL env variables
-        try:
-            # Execute the shell command
-            gfal_env_output = subprocess.check_output("env | grep ^GFAL", shell=True, text=True, stderr=subprocess.STDOUT)
-            # Log the output
-            logging.info('GFAL environment variables:\n%s', gfal_env_output)
-        except subprocess.CalledProcessError as e:
-            # Log an error if the command fails
-            logging.error('Failed to retrieve GFAL environment variables: %s', e.output.strip())
-        except Exception as e:
-            logging.error('An unexpected error occurred: %s', str(e))
-
-        result = "#!/bin/bash\n" + copyCommand
-
-        if _CheckExitCodeOption:
-            result += """
-            EXIT_STATUS=$?
-            echo "gfal-copy exit status: $EXIT_STATUS"
-            if [[ $EXIT_STATUS != 0 ]]; then
-                echo "ERROR: gfal-copy exited with $EXIT_STATUS"
-                echo "Cleaning up failed file:"
-                {remove_command}
-            fi
-            exit $EXIT_STATUS
-            """.format(remove_command=self.createRemoveFileCommand(targetPFN))
-            
-        logging.info("============ end of createStageOutCommand within GFAL2Impl =============")
-        
-        return result
+        return self.defineStageOutCommand(sourcePFN, targetPFN, options, checksums, authmethod, forcemethod, debug=False)
 
     def createDebuggingCommand(self, sourcePFN, targetPFN, options=None, checksums=None, authmethod=None, forcemethod=False):
         """
@@ -207,52 +245,7 @@ class GFAL2Impl(StageOutImpl):
         :authmethod: str, the authentication method to be used ("X509", "TOKEN", or None)
         :forcemethod: bool, whether to force the use of the preferred authentication method, disabling the other
         """
-
-        # Adjust the setup
-        self.adjustSetup(authmethod, forcemethod)
-
-        # Build the gfal-cp command for debugging purposes
-        copyCommandDict = self.buildCopyCommandDict(sourcePFN, targetPFN, options, checksums)
-        copyCommand = self.copyCommand.format_map(copyCommandDict)
-
-        result = "#!/bin/bash\n"
-        result += """
-        echo
-        echo
-        echo "-----------------------------------------------------------"
-        echo "==========================================================="
-        echo
-        echo "Debugging information on failing gfal-copy command"
-        echo
-        echo "Current date and time: $(date +"%Y-%m-%d %H:%M:%S")"
-        echo "gfal-copy command which failed: {copy_command}"
-        echo "Hostname:   $(hostname -f)"
-        echo "OS:  $(uname -r -s)"
-        echo
-        echo "GFAL environment variables:"
-        env | grep ^GFAL
-        echo
-        echo "PYTHON environment variables:"
-        env | grep ^PYTHON
-        echo
-        echo "LD_* environment variables:"
-        env | grep ^LD_
-        echo
-        echo "gfal-copy location: $(which gfal-copy)"
-        echo "Source PFN: {source}"
-        echo "Target PFN: {destination}"
-        echo
-        echo "Information for credentials in the environment"
-        echo "Bearer token content: $BEARER_TOKEN"
-        echo "Bearer token file: $BEARER_TOKEN_FILE"
-        echo
-        echo "VOMS proxy info:"
-        voms-proxy-info -all
-        echo "==========================================================="
-        echo "-----------------------------------------------------------"
-        echo
-        """.format(copy_command=copyCommand, source=copyCommandDict['source'], destination=copyCommandDict['destination'])
-        return result
+        return self.defineStageOutCommand(sourcePFN, targetPFN, options, checksums, authmethod, forcemethod, debug=True)
 
     def removeFile(self, pfnToRemove):
         """

091dbc6164c8ed25aa89f60fe8f5cefc65854e51
Alan Malta Rodrigues <alan.malta@cern.ch>
2025-04-21 14:18:03 -0400

    REMOVE: temporary SQL files to test unit tests
    REMOVE: add tier0 SQL files
    
    Add testdb SQL schema
    
    Indexes and primary key fixes for wmbs SQL files
    
    Remove primary keys for TestDB tables
    
    Add SQL files to new structure
    
    REMOVE-ME: removed tablespace from wmbs indexes
    
    REMOVE-ME: fix initial data for wmbs
    
    REMOVE-ME: more consistent database schema for Oracle
    
    REMOVE-ME: make wmbs_job.retry_count larger to support 99999
    
    REMOVE-ME: sync last sql schema changes
    
    REMOVE-ME: update tier0 create tables file

---
+++ sql/oracle/resourcecontrol/create_resourcecontrol.sql
@@ -0,0 +1,12 @@
+-- Oracle tables for WMCore.ResourceControl component
+
+CREATE TABLE rc_threshold (
+    site_id       NUMBER(11)    NOT NULL,
+    sub_type_id   NUMBER(11)    NOT NULL,
+    pending_slots NUMBER(11)    NOT NULL,
+    max_slots     NUMBER(11)    NOT NULL,
+    CONSTRAINT rc_threshold_fk1 FOREIGN KEY (site_id) 
+        REFERENCES wmbs_location(id) ON DELETE CASCADE,
+    CONSTRAINT rc_threshold_fk2 FOREIGN KEY (sub_type_id) 
+        REFERENCES wmbs_sub_types(id) ON DELETE CASCADE
+); 

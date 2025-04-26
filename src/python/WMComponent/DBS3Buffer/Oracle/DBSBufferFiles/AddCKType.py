3447ab0609b7bd1b06c59cd617390d6e83db95fd
Alan Malta Rodrigues <alan.malta@cern.ch>
2025-04-26 09:08:55 -0400

    Stop using SEQ.nextval in the WMComponent DAOs, as it uses autoincrement now

--- src/python/WMComponent/DBS3Buffer/Oracle/DBSBufferFiles/AddCKType.py
+++ src/python/WMComponent/DBS3Buffer/Oracle/DBSBufferFiles/AddCKType.py
@@ -11,5 +11,5 @@ Oracle implementation of AddCKType
 from WMComponent.DBS3Buffer.MySQL.DBSBufferFiles.AddCKType import AddCKType as MySQLAddCKType
 
 class AddCKType(MySQLAddCKType):
-    sql = """INSERT INTO dbsbuffer_checksum_type (id, type)
-               VALUES (dbsbuffer_checksum_type_seq.nextval, :cktype)"""
+    sql = """INSERT INTO dbsbuffer_checksum_type (type)
+             VALUES (:cktype)"""

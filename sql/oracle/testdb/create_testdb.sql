f8593cfc78a7e4903ce185141ee41f3ab3f02174
Alan Malta Rodrigues <alan.malta@cern.ch>
2025-05-19 22:47:48 -0400

    Refactor all Oracle SQL files to use slash as statement terminator
    update create_wmbs_tables with slash terminator
    
    Increase id column length for t0_deployment_id

--- sql/oracle/testdb/create_testdb.sql
+++ sql/oracle/testdb/create_testdb.sql
@@ -6,29 +6,40 @@ CREATE TABLE test_tablea (
     column1 NUMBER(11),
     column2 NUMBER(11),
     column3 VARCHAR2(255)
-);
+)
+/
 
 -- Create test_tableb
 CREATE TABLE test_tableb (
     column1 VARCHAR2(255),
     column2 NUMBER(11),
     column3 VARCHAR2(255)
-);
+)
+/
 
 -- Create test_tablec
 CREATE TABLE test_tablec (
     column1 VARCHAR2(255),
     column2 VARCHAR2(255),
     column3 VARCHAR2(255)
-);
+)
+/
 
 -- Create test_bigcol
 CREATE TABLE test_bigcol (
     column1 NUMBER(35)
-);
+)
+/
 
 -- Add comments to tables
-COMMENT ON TABLE test_tablea IS 'Test table A with integer and varchar columns';
-COMMENT ON TABLE test_tableb IS 'Test table B with varchar and integer columns';
-COMMENT ON TABLE test_tablec IS 'Test table C with varchar columns';
-COMMENT ON TABLE test_bigcol IS 'Test table with large decimal column';
+COMMENT ON TABLE test_tablea IS 'Test table A with integer and varchar columns'
+/
+
+COMMENT ON TABLE test_tableb IS 'Test table B with varchar and integer columns'
+/
+
+COMMENT ON TABLE test_tablec IS 'Test table C with varchar columns'
+/
+
+COMMENT ON TABLE test_bigcol IS 'Test table with large decimal column'
+/

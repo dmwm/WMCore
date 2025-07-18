fbad960483a371c10c12acd43b9bf149fcc84807
Todor Ivanov <todor.trendafilov.ivanov@cern.ch>
2025-07-18 12:42:26 +0200

    Turn reset failure info message to warnings for all components

--- src/python/WMComponent/RetryManager/RetryManagerPoller.py
+++ src/python/WMComponent/RetryManager/RetryManagerPoller.py
@@ -182,7 +182,7 @@ class RetryManagerPoller(BaseWorkerThread):
         # Reset its own watchdog timer at the end of the run cycle
         logging.info(f"Resetting {componentName(self)} watchdog timer.")
         if resetWatchdogTimer(self.config, componentName(self)):
-            logging.info(f"Failed to reset {componentName(self)} watchdog timer. The component might be restarted soon.")
+            logging.warning(f"Failed to reset {componentName(self)} watchdog timer. The component might be restarted soon.")
 
 
     def processRetries(self, jobs, cooloffType):

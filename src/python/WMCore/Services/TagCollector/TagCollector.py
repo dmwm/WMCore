637403671c39152cd245fb303384f971d70e6765
Antonio <alinares@alinaresdesktop.dyndns.cern.ch>
2025-11-12 11:10:49 +0100

    Parse cvmfs releasesMap file into xml file

--- src/python/WMCore/Services/TagCollector/TagCollector.py
+++ src/python/WMCore/Services/TagCollector/TagCollector.py
@@ -6,6 +6,7 @@ from Utils.Utilities import decodeBytesToUnicode
 standard_library.install_aliases()
 
 import logging
+
 from urllib.parse import urlparse
 
 from collections import defaultdict
@@ -39,6 +40,72 @@ class TagCollector(Service):
         super(TagCollector, self).__init__(configDict)
         self['logger'].debug("Initializing TagCollector with url: %s", self['endpoint'])
 
+        self.cvmfsReleasesMap = "/cvmfs/cms.cern.ch/releases.map"
+        self.tmpReleasesXML = "/tmp/ReleasesXML"
+
+    def parseCvmfsReleasesXML(self, releasesMap, releasesXML):
+        """
+        Parses the ReleasesXML file from the releases.map in cvmfs
+        """
+        production = "type=Production;"
+        announced="state=Announced;"
+        anyarch=False
+        architecture=""
+        archs = {}
+        rels = []
+
+        with open(releasesMap, "r", encoding="utf-8") as releasesFile:
+            for line in releasesFile:
+                if not anyarch and 'prodarch=1;' not in line:
+                    continue
+
+                if production and production not in line:
+                    continue
+
+                if announced and announced not in line:
+                    continue
+
+                if architecture and architecture not in line:
+                    continue
+
+                data = {}
+                for item in line.split(";"):
+                    if "=" not in item:
+                        continue
+                    k, v = item.split("=")
+                    data[k] = v
+
+                if "architecture" in data and "label" in data and "type" in data and "state" in data:
+                    if not anyarch and data["label"] in rels:
+                        continue
+
+                    rels.append(data["label"])
+                    arch = data["architecture"]
+
+                    if arch not in archs:
+                        archs[arch] = []
+
+                    extraTag = ""
+                    if "default_micro_arch" in data:
+                        extraTag = ' default_micro_arch="%s"' % data["default_micro_arch"]
+
+                    data["extra_tag"] = extraTag
+                    archs[arch].append(
+                        """<project label="%(label)s" type="%(type)s" state="%(state)s"%(extra_tag)s/>"""
+                        % data
+                    )
+
+        with open(releasesXML, "w", encoding="utf-8") as xml:
+            xml.write("<projects>\n")
+            for arch in archs:
+                xml.write('  <architecture name="%s">\n' % arch)
+                for rel in archs[arch]:
+                    xml.write("    %s\n" % rel)
+                xml.write("  </architecture>\n")
+            xml.write("</projects>\n")
+
+        return
+
     def _getResult(self, callname="", clearCache=False,
                    args=None, verb="GET", encoder=None, decoder=None,
                    contentType=None):
@@ -50,23 +117,38 @@ class TagCollector(Service):
 
         TODO: Probably want to move this up into Service
         """
-        if not args:
-            args = self.tcArgs
+        try:
+            if not args:
+                args = self.tcArgs
 
-        cFile = '%s_%s'% (self.cFileUrlPath, callname.replace("/", "_"))
-        # If no callname or url path, the base host is getting queried
-        if cFile == '_':
-            cFile = 'baseRequest'
+            cFile = '%s_%s'% (self.cFileUrlPath, callname.replace("/", "_"))
+            # If no callname or url path, the base host is getting queried
+            if cFile == '_':
+                cFile = 'baseRequest'
 
-        if clearCache:
-            self.clearCache(cFile, args, verb)
+            if clearCache:
+                self.clearCache(cFile, args, verb)
 
-        # Note cFile is just the base name pattern, args 
-        # are also considered for the end filename in the method below
-        f = self.refreshCache(cFile, callname, args, encoder=encoder, decoder=decodeBytesToUnicode,
-                              verb=verb, contentType=contentType)
-        result = f.read()
-        f.close()
+            # Note cFile is just the base name pattern, args 
+            # are also considered for the end filename in the method below
+            f = self.refreshCache(cFile, callname, args, encoder=encoder, decoder=decodeBytesToUnicode,
+                                verb=verb, contentType=contentType)
+            result = f.read()
+            f.close()
+        except:
+            logging.error('Something went wrong accessing ReleasesXML from cmssdt, perhaps the service is temporarily down')
+            logging.info('Retrying to access ReleasesXML from cvmfs')
+        
+            try:
+                self.parseCvmfsReleasesXML(releasesMap=self.cvmfsReleasesMap, releasesXML=self.tmpReleasesXML)
+                with open('/tmp/ReleasesXML', 'r', encoding='utf-8') as f:
+                    result = f.read()
+                f.close()
+            except:
+                logging.error('Something went wrong parsing /cvmfs/cms.cern.ch/releases.map into XML format, perhaps cvmfs is not mounted')
+                logging.exception('Unable to access ReleasesXML from cmssdt and cvmfs')
+                raise
+
 
         # overhead from REST model which returns results as strings or None
         # therefore they can be encoded by JSON to None, etc.
